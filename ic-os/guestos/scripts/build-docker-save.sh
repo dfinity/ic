7c0125deb8d8c49fda211fac49b4f95cd655f2eb
Marko Kosmerl <marko.kosmerl@dfinity.org>
2021-12-09 09:40:01 +0000

    Adding clear output for image and sha256 digest

--- ic-os/guestos/scripts/build-docker-save.sh
+++ ic-os/guestos/scripts/build-docker-save.sh
@@ -5,11 +5,19 @@
 #
 # Arguments to this script are passed verbatim to "docker build".
 
+CI_JOB_NAME=${CI_JOB_NAME:-}
+# We implicitly pull dependent image.
+ARGS=(--pull)
+if [ "$CI_JOB_NAME" == "guest-os-updateimg-build-determinism" ]; then
+    # Don't use cache when in CI.
+    ARGS+=(--no-cache)
+fi
+
 DOCKER_ID=$(
     # Account for two different output formats of docker command:
     # "classic" docker and "buildkit" docker
-    echo "docker build $@" >&2
-    docker build --no-cache "$@" 2>&1 | tee /dev/fd/2 | sed -e 's/Successfully built //' -e t -e 's/.*writing image sha256:\([0-9a-f]\{64\}\) .*/\1/' -e t -e d
+    echo "docker build ${ARGS[@]} $@" >&2
+    docker build "${ARGS[@]}" "$@" 2>&1 | tee /dev/fd/2 | sed -e 's/Successfully built //' -e t -e 's/.*writing image sha256:\([0-9a-f]\{64\}\) .*/\1/' -e t -e d
 )
 
 docker save "${DOCKER_ID}"
