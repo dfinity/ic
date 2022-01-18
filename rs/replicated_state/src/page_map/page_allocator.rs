d975c0fb7f4770a2919057033904fdb152e9afcd
Ulan Degenbaev <ulan.degenbaev@dfinity.org>
2022-01-18 15:31:34 +0000

    EXC-523: Temporarily enable canister sandbox on Linux

--- rs/replicated_state/src/page_map/page_allocator.rs
+++ rs/replicated_state/src/page_map/page_allocator.rs
@@ -19,12 +19,9 @@ pub mod mmap;
 #[cfg(target_os = "linux")]
 mod default_implementation {
     pub use super::mmap::{MmapBasedPage, MmapBasedPageAllocator};
-    use super::{HeapBasedPage, HeapBasedPageAllocator};
     // Exported publicly for benchmarking.
-    // TODO(EXC-523): Enable with mmap-based page allocator with
-    // sandboxing.
-    pub type DefaultPageImpl = HeapBasedPage;
-    pub type DefaultPageAllocatorImpl = HeapBasedPageAllocator;
+    pub type DefaultPageImpl = MmapBasedPage;
+    pub type DefaultPageAllocatorImpl = MmapBasedPageAllocator;
 }
 #[cfg(not(target_os = "linux"))]
 mod default_implementation {
