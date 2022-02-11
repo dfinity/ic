b9d3cf52f2b10ec848b1a7982c9f48988e688b34
Roman Kashitsyn <roman.kashitsyn@dfinity.org>
2022-02-11 11:49:16 +0000

    infra: disable unhelpful clippy warnings

--- gitlab-ci/src/rust_lint/lint.sh
+++ gitlab-ci/src/rust_lint/lint.sh
@@ -3,7 +3,13 @@ set -xeuo pipefail
 
 cd "$CI_PROJECT_DIR/rs"
 cargo fmt -- --check
-cargo clippy --locked --all-features --tests --benches -- -D warnings -D clippy::all -D clippy::mem_forget -C debug-assertions=off
+cargo clippy --locked --all-features --tests --benches -- \
+    -D warnings \
+    -D clippy::all \
+    -D clippy::mem_forget \
+    -A clippy::redundant_closure \
+    -A clippy::too_many_arguments \
+    -C debug-assertions=off
 
 if cargo tree -e features | grep -q 'serde feature "rc"'; then
     echo 'The serde "rc" feature seems to be enabled. Instead, the module "serde_arc" in "ic-utils" should be used.'
