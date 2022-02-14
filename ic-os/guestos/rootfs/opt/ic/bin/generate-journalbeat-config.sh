a00bffeb2ceafebedeed44b54b2b3be745832e23
Ian Blenke <ian.blenke@dfinity.org>
2022-02-14 08:57:16 +0000

    Revert "Adding defaults for journalbeat config"

--- ic-os/guestos/rootfs/opt/ic/bin/generate-journalbeat-config.sh
+++ ic-os/guestos/rootfs/opt/ic/bin/generate-journalbeat-config.sh
@@ -64,37 +64,6 @@ fi
 JOURNALBEAT_HOSTS="${journalbeat_hosts}"
 JOURNALBEAT_TAGS="${journalbeat_tags}"
 
-testnet_prefixes=(
-    "2a00:fb01:400:42"
-    "2607:fb58:9005:42"
-    "2607:f6f0:3004:1"
-    "2001:4d78:40d:0"
-    "fd00:2:1:1"
-)
-
-MAINNET="telemetry01.mainnet.dfinity.network telemetry02.mainnet.dfinity.network telemetry03.mainnet.dfinity.network"
-TESTNET="telemetry01.testnet.dfinity.network telemetry02.testnet.dfinity.network telemetry03.testnet.dfinity.network"
-
-if [ -z "${JOURNALBEAT_HOSTS}" ] || (echo ${JOURNALBEAT_HOSTS} | grep -iE 'mainnet|mercury|stage' >/dev/null); then
-    # Set default journalbeat hosts
-    # Detect if this is a testnet node or a mainnet node
-    ipv6_prefix="$(ip -6 addr show | awk '(/inet6/) && (! /fe80|::1/) { print $2 }' | cut -d: -f1-4)"
-    # Default to mainnet:
-    JOURNALBEAT_HOSTS="${MAINNET}"
-    for testnet_prefix in ${testnet_prefixes[@]}; do
-        if [ "$testnet_prefix" = "$ipv6_prefix" ]; then
-            # If a testnet node, use this as the default instead:
-            JOURNALBEAT_HOSTS="${TESTNET}"
-        fi
-    done
-fi
-
-# XXX
-# This function is a temporary hack, it takes care of harmonizing the
-# configuration on all nodes. To be removed before next node upgrade.
-echo "journalbeat_hosts=${JOURNALBEAT_HOSTS}" >/boot/config/journalbeat.conf
-# XXX
-
 if [ "${JOURNALBEAT_HOSTS}" != "" ]; then
     # Covert string into comma separated array
     journalbeat_hosts_array=$(for host in ${JOURNALBEAT_HOSTS}; do echo -n "\"${host}\", "; done | sed -E "s@, \$@@g")
