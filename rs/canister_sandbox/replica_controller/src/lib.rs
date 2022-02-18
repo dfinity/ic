de3afa8aa9fa68138f689308fe37628480729102
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2022-02-18 17:34:37 +0000

    Remove sandbox launch_as_thread

--- rs/canister_sandbox/replica_controller/src/lib.rs
+++ rs/canister_sandbox/replica_controller/src/lib.rs
@@ -1,7 +1,6 @@
 pub mod active_execution_state_registry;
 pub mod controller_service_impl;
 pub mod launch_as_process;
-pub mod launch_as_thread;
 mod process_exe_and_args;
 pub mod process_os_metrics;
 pub mod sandboxed_execution_controller;
