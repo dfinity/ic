f9286dfd91afe965b8102d7f5a9ef8eb29e67412
Marko Kosmerl <marko.kosmerl@dfinity.org>
2022-03-16 12:42:17 +0000

    Use rclone instead of Gitlab's artifacts

--- gitlab-ci/src/artifacts/find-build-id.sh
+++ gitlab-ci/src/artifacts/find-build-id.sh
@@ -7,8 +7,12 @@ set -eux
 # TODO: "broken-blockmaster" condition is a hack to fix the broken hack of !2067.
 # TODO: CI_MERGE_REQUEST_TITLE conditional is a stopgap solution for IDX-2123.
 if [[ "${CI_PARENT_PIPELINE_SOURCE:-}" = "merge_request_event" && "${CI_COMMIT_REF_NAME:-}" != "broken-blockmaker" && ! "${CI_MERGE_REQUEST_TITLE}" =~ \[rc\]|\[benchmark\] ]]; then
-    cd "${CI_PROJECT_DIR}"
-    placebo -c build-id --inputs_hash
+    if [ -n "${VERSION:-}" ]; then
+        echo "$VERSION"
+    else
+        cd "${CI_PROJECT_DIR}"
+        placebo -c build-id --inputs_hash
+    fi
 else
     git rev-parse --verify HEAD
 fi
