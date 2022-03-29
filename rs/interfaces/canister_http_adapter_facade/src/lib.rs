e99b239f77a69b16f80d18ec146d0067b735c1ed
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2022-03-29 15:40:36 +0000

    [NET-949] Add the shim type that will be used by consensus

--- rs/interfaces/canister_http_adapter_facade/src/lib.rs
+++ rs/interfaces/canister_http_adapter_facade/src/lib.rs
@@ -1,24 +1,25 @@
+use ic_types::canister_http::{CanisterHttpRequest, CanisterHttpResponseContent};
+
 /// The error type that can be returned on "send".
 #[derive(Debug, PartialEq)]
 pub enum SendError<Request> {
     /// The "bridge" is full. Some responses must be consumes before new
     /// requests are send.
     Full(Request),
-    /// The "bridge" is closed. Sending and receiving fails.
-    Closed(Request),
 }
 
 /// The error type that can be returned on "try_receive".
 #[derive(Debug, PartialEq)]
-pub enum ReceiveError {
+pub enum TryReceiveError {
     /// No new response are available.
     Empty,
-    /// The "channel" is disconnected.
-    Disconnected,
 }
 
 /// Abstract interface for non-blocking channel.
 pub trait NonBlockingChannel<Request, Response> {
     fn send(&mut self, request: Request) -> Result<(), SendError<Request>>;
-    fn try_receive(&mut self) -> Result<Response, ReceiveError>;
+    fn try_receive(&mut self) -> Result<Response, TryReceiveError>;
 }
+
+pub type CanisterHttpClient =
+    Box<dyn NonBlockingChannel<CanisterHttpRequest, CanisterHttpResponseContent> + Send>;
