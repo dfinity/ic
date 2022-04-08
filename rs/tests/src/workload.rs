fe069a9751753f021dc8aaa4610358870e923d3f
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2022-04-08 15:02:09 +0000

    Workload aggregate errors Metrics

--- rs/tests/src/workload.rs
+++ rs/tests/src/workload.rs
@@ -1,6 +1,7 @@
 use ic_agent::{export::Principal, Agent};
 use leaky_bucket::RateLimiter;
 use slog::info;
+use std::collections::HashMap;
 use std::{
     cmp::{max, min},
     time::{Duration, Instant},
@@ -80,7 +81,8 @@ pub struct WorkloadError;
 
 /// Defines result of the workload execution.
 pub struct Metrics {
-    total_calls: Counter,
+    errors_max: Counter,
+    errors_map: HashMap<String, Counter>,
     success_calls: Counter,
     failure_calls: Counter,
     min_request_duration: Duration,
@@ -89,8 +91,10 @@ pub struct Metrics {
 
 impl Default for Metrics {
     fn default() -> Metrics {
+        let errors_max = 10000;
         Metrics {
-            total_calls: 0,
+            errors_max,
+            errors_map: HashMap::with_capacity(errors_max),
             success_calls: 0,
             failure_calls: 0,
             min_request_duration: Duration::MAX,
@@ -100,6 +104,10 @@ impl Default for Metrics {
 }
 
 impl Metrics {
+    pub fn total_calls(&self) -> Counter {
+        self.success_calls + self.failure_calls()
+    }
+
     pub fn success_calls(&self) -> Counter {
         self.success_calls
     }
@@ -114,6 +122,28 @@ impl Metrics {
 
     pub fn max_request_duration(&self) -> Duration {
         self.max_request_duration
+    }
+
+    pub fn errors(&self) -> &HashMap<String, Counter> {
+        &self.errors_map
+    }
+
+    pub fn process_request(&mut self, request: RequestResult) {
+        self.min_request_duration = min(self.min_request_duration, request.duration);
+        self.max_request_duration = max(self.max_request_duration, request.duration);
+        match request.call_status {
+            CallStatus::Success => self.success_calls += 1,
+            CallStatus::Failure(err) => {
+                self.failure_calls += 1;
+                *self.errors_map.entry(err).or_insert(0) += 1;
+                if self.errors_map.len() > self.errors_max {
+                    panic!(
+                        "Hash table holding errors exceeded predefined max_size={}.",
+                        self.errors_max
+                    );
+                }
+            }
+        }
     }
 }
 
@@ -260,15 +290,9 @@ async fn collect_results(
     requests_count: usize,
 ) -> Metrics {
     let mut metrics = Metrics::default();
-    while let Some(result) = receiver.recv().await {
-        metrics.total_calls += 1;
-        metrics.min_request_duration = min(metrics.min_request_duration, result.duration);
-        metrics.max_request_duration = max(metrics.max_request_duration, result.duration);
-        match result.call_status {
-            CallStatus::Success => metrics.success_calls += 1,
-            CallStatus::Failure(_) => metrics.failure_calls += 1,
-        }
-        if metrics.total_calls == requests_count {
+    while let Some(request) = receiver.recv().await {
+        metrics.process_request(request);
+        if metrics.total_calls() == requests_count {
             info!(
                 log,
                 "Reached desired number of requests={}.", requests_count
@@ -277,4 +301,97 @@ async fn collect_results(
         }
     }
     metrics
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_request_with_success_result() {
+        let mut metrics = Metrics::default();
+        let request = RequestResult {
+            duration: Duration::from_secs(1),
+            call_status: CallStatus::Success,
+        };
+
+        metrics.process_request(request);
+
+        assert_eq!(1, metrics.success_calls());
+        assert_eq!(0, metrics.failure_calls());
+        assert_eq!(0, metrics.errors().len());
+        assert_eq!(metrics.min_request_duration, Duration::from_secs(1));
+        assert_eq!(metrics.max_request_duration, Duration::from_secs(1));
+    }
+
+    #[test]
+    fn test_process_request_with_failure_results() {
+        let mut metrics = Metrics::default();
+        let request_1 = RequestResult {
+            duration: Duration::from_secs(3),
+            call_status: CallStatus::Failure("some_error".to_string()),
+        };
+        // Request with max duration.
+        let request_2 = RequestResult {
+            duration: Duration::from_secs(4),
+            call_status: CallStatus::Failure("some_error".to_string()),
+        };
+        // Request with min duration.
+        let request_3 = RequestResult {
+            duration: Duration::from_secs(1),
+            call_status: CallStatus::Failure("some_other_error".to_string()),
+        };
+
+        metrics.process_request(request_1);
+        metrics.process_request(request_2);
+        metrics.process_request(request_3);
+
+        let errors = metrics.errors();
+        assert_eq!(0, metrics.success_calls());
+        assert_eq!(3, metrics.failure_calls());
+        assert_eq!(errors["some_error"], 2);
+        assert_eq!(errors["some_other_error"], 1);
+        assert_eq!(metrics.min_request_duration, Duration::from_secs(1));
+        assert_eq!(metrics.max_request_duration, Duration::from_secs(4));
+    }
+
+    #[test]
+    #[should_panic(expected = "Hash table holding errors exceeded predefined max_size=1.")]
+    fn test_process_request_with_overflow() {
+        let mut metrics = Metrics {
+            errors_max: 1,
+            ..Default::default()
+        };
+        let request_1 = RequestResult {
+            duration: Duration::from_secs(3),
+            call_status: CallStatus::Failure("some_error".to_string()),
+        };
+        let request_2 = RequestResult {
+            duration: Duration::from_secs(4),
+            call_status: CallStatus::Failure("some_other_error".to_string()),
+        };
+
+        metrics.process_request(request_1);
+        // This request causes an overflow in the hash table.
+        metrics.process_request(request_2);
+    }
+
+    #[tokio::test]
+    async fn test_collect_result() {
+        let requests_count = 1;
+        let (sender, receiver) = tokio::sync::mpsc::channel(requests_count);
+        let log = slog::Logger::root(slog::Discard, slog::o!());
+        let request = RequestResult {
+            duration: Duration::from_secs(1),
+            call_status: CallStatus::Success,
+        };
+        sender
+            .send(request)
+            .await
+            .expect("Sending request's result to the channel has failed.");
+
+        let metrics = collect_results(log, receiver, requests_count).await;
+
+        assert_eq!(metrics.success_calls(), 1);
+    }
 }
--- rs/tests/src/workload_counter_canister_test.rs
+++ rs/tests/src/workload_counter_canister_test.rs
@@ -3,17 +3,22 @@
 Title:: Use workload to execute update/query calls on counter canisters.
 
 Goal:: Ensure that at a moderate rate of requests per second, workload sends update/query requests to counter canisters successfully.
+Update calls increments canisters counter. Query calls (with non-existing methods) on canisters are expected to fail.
 
 Runbook::
 0. Set up an application subnet with N nodes.
 1. Install X counter canisters on this subnet.
 2. Instantiate and start the workload.
-   Workload sends update/query requests in a round-robin fashion:
-   [update[canister_id_0], query[canister_id_1], query[canister_id_0], update[canister_id_1]].
+   Workload sends update/query requests to counter canisters in a round-robin fashion:
+   [
+       update[canister_id_0, "write"], // should be successful
+       query[canister_id_1, "non_existing_method_a"], // should fail
+       query[canister_id_0, "non_existing_method_b"], // should fail
+       update[canister_id_1, "write"], // should be successful
+    ].
    These requests are equally distributed between nodes of the subnet.
-3. Query the number of executed update calls on the counter canisters.
-4. Assert that number of update calls on each canister = rps * workload_execution_sec / (2 * X).
-   Factor 2 is present above as half of the calls are `query` and half are `update` (see Item 2.)
+3. Assert the expected number of failed query calls on each canister.
+4. Assert the expected number of successful update calls on each canister.
 
 end::catalog[] */
 
@@ -31,8 +36,12 @@ use std::time::Duration;
 
 const NODES_COUNT: usize = 3;
 const CANISTERS_COUNT: usize = 2;
-const RPS: usize = 20;
-const DURATION: Duration = Duration::from_secs(20);
+const RPS: usize = 60;
+const DURATION: Duration = Duration::from_secs(30);
+const NON_EXISTING_METHOD_A: &str = "non_existing_method_a";
+const NON_EXISTING_METHOD_B: &str = "non_existing_method_b";
+const MAX_RETRIES: u32 = 10;
+const RETRY_WAIT: Duration = Duration::from_secs(10);
 
 pub fn config() -> InternetComputer {
     InternetComputer::new().add_subnet(Subnet::new(SubnetType::Application).add_nodes(NODES_COUNT))
@@ -46,69 +55,73 @@ pub async fn test(handle: IcHandle, ctx: &ic_fondue::pot::Context) {
     let mut rng = ctx.rng.clone();
     let endpoints: Vec<_> = handle.as_permutation(&mut rng).collect();
     assert_endpoints_reachability(endpoints.as_slice(), EndpointsStatus::AllReachable).await;
-    // Install canisters.
+    info!(ctx.logger, "All nodes are reachable, IC setup succeeded.");
+    // Step 1: Install X counter canisters on the subnet.
     let mut agents = Vec::new();
     let mut canisters = Vec::new();
     for ep in endpoints.iter() {
         agents.push(assert_create_agent(ep.url.as_str()).await);
     }
-    let install_agent = &agents[0];
+    info!(ctx.logger, "Installing canisters...");
+    let install_agent = agents[0].clone();
     for _ in 0..CANISTERS_COUNT {
-        canisters.push(install_counter_canister(install_agent).await);
+        canisters.push(install_counter_canister(&install_agent).await);
     }
-    // Initialize requests' plan for the workload.
+    info!(
+        ctx.logger,
+        "{} canisters installed successfully.",
+        canisters.len()
+    );
+    assert_eq!(canisters.len(), CANISTERS_COUNT);
+    // Step 2: Instantiate and start the workload.
     let payload: Vec<u8> = vec![0; 12];
     let plan = RoundRobinPlan::new(vec![
         Request::Update(CallSpec::new(canisters[0], "write", payload.clone())),
-        Request::Query(CallSpec::new(canisters[1], "read", payload.clone())),
-        Request::Query(CallSpec::new(canisters[0], "read", payload.clone())),
+        Request::Query(CallSpec::new(
+            canisters[1],
+            NON_EXISTING_METHOD_A,
+            payload.clone(),
+        )),
+        Request::Query(CallSpec::new(
+            canisters[0],
+            NON_EXISTING_METHOD_B,
+            payload.clone(),
+        )),
         Request::Update(CallSpec::new(canisters[1], "write", payload.clone())),
     ]);
-
     let workload = Workload::new(agents, RPS, DURATION, plan, ctx.logger.clone());
-
     let metrics = workload
         .execute()
         .await
-        .expect("Workload execution failed.");
-
-    info!(
-        ctx.logger,
-        "Min request duration={} ms.",
-        metrics.min_request_duration().as_millis()
-    );
-    info!(
-        ctx.logger,
-        "Max request duration={} ms.",
-        metrics.max_request_duration().as_millis()
-    );
-
-    // Assert metrics from workload's perspective.
+        .expect("Workload execution has failed.");
+    // Step 3: Assert the expected number of failed query calls on each canister.
+    info!(ctx.logger, "Asserting metrics are correct...");
     let requests_count = RPS * DURATION.as_secs() as usize;
-    info!(ctx.logger, "Expected requests count={}", requests_count);
-    assert_eq!(
-        metrics.success_calls(),
-        requests_count,
-        "Not all calls were successful."
-    );
-    assert_eq!(
-        metrics.failure_calls(),
-        0,
-        "Number of failed calls should be zero."
-    );
-
-    // Assert metrics from counter canisters' perspective (expected counter value).
-    let agent = assert_create_agent(endpoints[0].url.as_str()).await;
-    let expected_count = RPS * DURATION.as_secs() as usize / CANISTERS_COUNT / 2;
+    // 1/2 requests are query (failure) and 1/2 are update (success).
+    let expected_failure_calls = requests_count / 2;
+    let expected_success_calls = requests_count / 2;
+    let errors = metrics.errors();
+    assert_eq!(errors.len(), 2);
+    // Error messages should contain the name of failed method call.
+    assert!(errors.keys().any(|k| k.contains(NON_EXISTING_METHOD_A)));
+    assert!(errors.keys().any(|k| k.contains(NON_EXISTING_METHOD_B)));
+    assert!(errors
+        .values()
+        .all(|k| *k == expected_failure_calls / CANISTERS_COUNT));
+    assert_eq!(expected_failure_calls, metrics.failure_calls());
+    assert_eq!(expected_success_calls, metrics.success_calls());
+    assert_eq!(requests_count, metrics.total_calls());
+    // Step 4: Assert the expected number of update calls on each canister.
+    let expected_canister_counter = expected_success_calls / CANISTERS_COUNT;
     for canister in canisters.iter() {
         assert_canister_counter_with_retries(
             &ctx.logger,
-            &agent,
+            &install_agent,
             canister,
             payload.clone(),
-            expected_count,
-            10,
-            10,
+            expected_canister_counter,
+            MAX_RETRIES,
+            RETRY_WAIT,
         )
         .await;
     }
@@ -143,15 +156,13 @@ async fn assert_canister_counter_with_retries(
     canister_id: &Principal,
     payload: Vec<u8>,
     expected_count: usize,
-    max_retries: u64,
-    retry_wait_sec: u64,
+    max_retries: u32,
+    retry_wait: Duration,
 ) {
-    for i in 0..max_retries + 1 {
+    for i in 1..=1 + max_retries {
         debug!(
             log,
-            "Reading counter value from canister with id={}, attempt {}.",
-            canister_id,
-            i + 1
+            "Reading counter value from canister with id={}, attempt {}.", canister_id, i
         );
         let res = agent
             .query(canister_id, "read")
@@ -176,8 +187,8 @@ async fn assert_canister_counter_with_retries(
                 log,
                 "Counter value != expected_count, {} != {}", counter, expected_count
             );
-            debug!(log, "Retrying in {} secs...", retry_wait_sec);
-            tokio::time::sleep(std::time::Duration::from_secs(retry_wait_sec)).await;
+            debug!(log, "Retrying in {} secs...", retry_wait.as_secs());
+            tokio::time::sleep(retry_wait).await;
         }
     }
     panic!(
