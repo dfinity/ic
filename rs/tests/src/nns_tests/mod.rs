766e2ec8151ea704ad8d97a4d6ec6b0c7dbe56da
Yvonne-Anne Pignolet <yvonneanne@dfinity.org>
2022-04-12 10:17:53 +0000

    Cleanup system tests: move NNS tests to one directory

---
+++ rs/tests/src/nns_tests/mod.rs
@@ -0,0 +1,7 @@
+pub mod create_subnet;
+pub mod cycles_minting;
+pub mod nns_canister_upgrade;
+pub mod nns_follow;
+pub mod nns_uninstall_canister_by_proposal;
+pub mod nns_voting;
+pub mod node_removal_from_registry;
