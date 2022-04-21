eab9be79c53a88627881258b399ac9967aae7a60
Mykola Dzham <mykola.dzham@dfinity.org>
2022-04-21 06:56:27 +0000

    Always use git sha as the Build ID for now.

--- gitlab-ci/src/artifacts/find-build-id.sh
+++ gitlab-ci/src/artifacts/find-build-id.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
-#
-# Finds the Build ID by calculating the sha's of all inputs listed in Capsule.toml under 'build-id'.
 
 set -eux
 
-# TODO: "broken-blockmaster" condition is a hack to fix the broken hack of !2067.
-# TODO: CI_MERGE_REQUEST_TITLE conditional is a stopgap solution for IDX-2123.
-if [[ "${CI_PARENT_PIPELINE_SOURCE:-}" = "merge_request_event" && "${CI_COMMIT_REF_NAME:-}" != "broken-blockmaker" && ! "${CI_MERGE_REQUEST_TITLE}" =~ \[rc\]|\[benchmark\] ]]; then
-    if [ -n "${VERSION:-}" ]; then
-        echo "$VERSION"
-    else
-        cd "${CI_PROJECT_DIR}"
-        placebo -c build-id --inputs_hash
-    fi
-else
-    git rev-parse --verify HEAD
-fi
+echo "${VERSION:-$(git rev-parse --verify HEAD)}"
