83c592bc7e0fd9d0918f882d1b18d9d054358e01
Ulan Degenbaev <ulan.degenbaev@dfinity.org>
2022-04-27 20:07:14 +0000

    RUN-105: Create the initial `execution` module structure

---
+++ rs/execution_environment/src/execution/heartbeat.rs
@@ -0,0 +1,4 @@
+// This module defines how `canister_heartbeat` messages are executed.
+// See https://smartcontracts.org/docs/interface-spec/index.html#_heartbeat.
+//
+// TODO(RUN-60): Move heartbeat execution functions here.
