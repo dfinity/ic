49cd98b73a6b9a35a56ff318c033449db0da5124
Alexandra Zapuc <alexandra.zapuc@dfinity.org>
2022-05-04 16:59:19 +0000

    EXC-1035: Canister invariant callbacks

--- rs/replicated_state/src/canister_state.rs
+++ rs/replicated_state/src/canister_state.rs
@@ -282,18 +282,18 @@ impl CanisterState {
             )));
         }
 
-        let num_contexts = self
+        let num_callbacks = self
             .system_state
             .call_context_manager()
-            .map(|ccm| ccm.call_contexts().len())
+            .map(|ccm| ccm.callbacks().len())
             .unwrap_or(0);
         let num_responses = self.system_state.queues().input_queues_response_count();
         let num_reservations = self.system_state.queues().input_queues_reservation_count();
-        if num_contexts != num_reservations + num_responses {
+        if num_callbacks != num_reservations + num_responses {
             return Err(StateError::InvariantBroken(format!(
-                "Canister {}: Number of call contexts ({}) is different than the accumulated number of reservations and responses ({})",
+                "Canister {}: Number of callbacks ({}) is different than the accumulated number of reservations and responses ({})",
                 self.canister_id(),
-                num_contexts,
+                num_callbacks,
                 num_reservations + num_responses
             )));
         }
