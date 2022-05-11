60dda0549217d3030b6718a9303da01c8f2b688d
Roman Kashitsyn <roman.kashitsyn@dfinity.org>
2022-05-11 20:31:41 +0000

    bazel build: bazelize ic-base-types

--- rs/types/base_types/protobuf_generator/src/main.rs
+++ rs/types/base_types/protobuf_generator/src/main.rs
@@ -2,8 +2,15 @@ use ic_base_types_protobuf_generator::generate_prost_files;
 use std::path::PathBuf;
 
 fn main() {
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR")
-        .expect("CARGO_MANIFEST_DIR env variable is not defined");
+    let manifest_dir = match std::env::var("CARGO_MANIFEST_DIR") {
+        Ok(path) => PathBuf::from(path),
+        Err(_) => match std::env::var("BUILD_WORKSPACE_DIRECTORY") {
+            Ok(path) => PathBuf::from(path).join("rs/types/base_types/protobuf_generator"),
+            Err(_) => panic!(
+                "Neither CARGO_MANIFEST_DIR nor BUILD_WORKSPACE_DIRECTORY env variable is defined"
+            ),
+        },
+    };
     let out = PathBuf::from(&manifest_dir).join("../gen");
     let def = PathBuf::from(&manifest_dir).join("../proto");
     match std::fs::remove_dir_all(&out) {
