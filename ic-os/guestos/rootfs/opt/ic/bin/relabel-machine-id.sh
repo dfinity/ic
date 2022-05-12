5bc5027d7deff6091f5a612573fc06e3fc09401b
Eero Kelly <eero.kelly@dfinity.org>
2022-05-12 23:27:14 +0000

    Fix SELinux context of /etc/machine-id on first boot

--- ic-os/guestos/rootfs/opt/ic/bin/relabel-machine-id.sh
+++ ic-os/guestos/rootfs/opt/ic/bin/relabel-machine-id.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# On anything but first boot, machine-id is set up in initramfs and bind-mounted
-# over to the rootfs. Since policy is not loaded in initramfs yet, it has
-# wrong security context. Fix this up, but ignore failures (this will not
-# work on first boot).
-restorecon -v /etc/machine-id || true
+# machine-id is set up in initramfs and bind-mounted over to the rootfs. Since
+# policy is not loaded in initramfs yet, it has wrong security context.
+restorecon -v /etc/machine-id
