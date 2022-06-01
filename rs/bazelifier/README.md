d8f0dce9ed49773f19aaa6fdb326fcdc4c2cafce
Jude Taylor <jude.taylor@dfinity.org>
2022-06-01 18:28:19 +0000

    Generate tests in bazelifier

--- rs/bazelifier/README.md
+++ rs/bazelifier/README.md
@@ -3,14 +3,31 @@ This is a tool that turns a crate into a BUILD file.
 ## Usage
 
 ```
-$ bazel run //rs/bazelifier -- path/to/some/Cargo.toml
+bazelifier
+
+USAGE:
+    bazelifier [OPTIONS] <CARGO_FILE>
+
+ARGS:
+    <CARGO_FILE>    Cargo.toml file to convert
+
+OPTIONS:
+    -f, --force      Overwrite any existing BUILD file
+    -h, --help       Print help information
+    -n, --dry-run    Show the generated file instead of writing it
+    -t, --tests      Generate rust_test invocations
+```
+
+Example:
+
+```
+# show a generated BUILD.bazel file with tests for the crypto crate
+$ bazel run bazelifier -- rs/crypto/Cargo.toml -n -t
 ```
 
 ## Limitations
 
 - Can't translate target specific dependencies. The tool will warn you when it encounters any.
 - Can't always distinguish between regular and proc macro dependencies. You may have to move some of the entries, but `bazel` will tell you what to do in such cases.
-- Will not overwrite an existing BUILD.bazel, since the one it generates is not guaranteed to be correct.
-- Doesn't run buildifier to format the generated file afterward, you'll have to do it manually.
-- Doesn't generate a tests section.
+- Doesn't run buildifier to format the generated file afterward. You'll have to do it manually, or let `pre-commit` run buildifier automatically.
 - Doesn't modify the workspace file. If a new external dependency is needed, you'll have to do it manually.
--- rs/bazelifier/src/main.rs
+++ rs/bazelifier/src/main.rs
@@ -1,7 +1,11 @@
-// unused struct fields are needed for serde to get the names right
-#![allow(dead_code)]
-
-use std::{collections::BTreeMap, fmt::Display, io::Write, path::Path, process::Command};
+use std::{
+    borrow::Cow,
+    collections::{btree_map, BTreeMap, BTreeSet},
+    fmt::Display,
+    io::Write,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
 use askama::Template;
 use clap::Parser;
@@ -12,9 +16,17 @@ struct Options {
     /// Overwrite any existing BUILD file
     #[clap(short, long)]
     force: bool,
+    /// Show the generated file instead of writing it
+    #[clap(short = 'n', long)]
+    dry_run: bool,
+    /// Generate rust_test invocations
+    #[clap(short = 't', long = "tests")]
+    gen_tests: bool,
     /// Cargo.toml file to convert
     cargo_file: String,
 }
+
+type Deps = BTreeSet<String>;
 
 enum BuildType {
     Lib,
@@ -30,6 +42,12 @@ impl Display for BuildType {
     }
 }
 
+impl Default for BuildType {
+    fn default() -> Self {
+        Self::Lib
+    }
+}
+
 #[derive(Deserialize, Debug)]
 struct Crate {
     package: Package,
@@ -37,6 +55,8 @@ struct Crate {
     lib: Option<LibSection>,
     #[serde(default)]
     dependencies: BTreeMap<String, Dep>,
+    #[serde(default, rename = "dev-dependencies")]
+    dev_dependencies: BTreeMap<String, Dep>,
     #[serde(default)]
     target: toml::map::Map<String, toml::Value>,
 }
@@ -50,7 +70,6 @@ struct LibSection {
 #[derive(Deserialize, Debug)]
 struct Package {
     name: String,
-    version: String,
     edition: String,
 }
 
@@ -65,9 +84,8 @@ enum Dep {
 
 #[derive(Deserialize, Debug)]
 struct NormalDep {
-    version: String,
-    #[serde(default)]
-    features: Vec<String>,
+    #[serde(rename = "version")]
+    _version: String,
     #[serde(default)]
     package: Option<String>,
 }
@@ -79,124 +97,204 @@ struct LocalDep {
 
 #[derive(Deserialize, Debug)]
 struct GitDep {
-    git: String,
-    branch: Option<String>,
-    rev: Option<String>,
-    tag: Option<String>,
+    #[serde(rename = "git")]
+    _git: String,
 }
 
-#[derive(Template)]
+#[derive(Template, Default)]
 #[template(path = "buildfile.template", escape = "none")]
-struct Buildfile<'a> {
+struct BuildFile<'a> {
     build_type: BuildType,
-    target_name: &'a str,
-    crate_name: &'a str,
-    edition: &'a str,
-    deps: Vec<String>,
-    macro_deps: Vec<String>,
+    deps: Deps,
+    dev_deps: Deps,
+    macro_deps: Deps,
+    macro_dev_deps: Deps,
     aliases: BTreeMap<String, String>,
+    target_name: Cow<'a, str>,
+    edition: &'a str,
+    crate_name: String,
+    gen_tests: bool,
+    integration_tests: Vec<BuildTest>,
+}
+
+struct BuildTest {
+    name: String,
+    filepath: String,
 }
 
 static MACRO_CRATES: &[&str] = &[
     "async-trait",
+    "debug_stub_derive",
     "derive_more",
     "fe-derive",
+    "hex-literal",
     "paste",
     "proptest-derive",
     "slog_derive",
     "strum_macros",
 ];
 
-fn main() -> eyre::Result<()> {
-    let Options {
-        force,
-        cargo_file: tomlfile,
-    } = Options::parse();
+struct Bazelifier {
+    opts: Options,
+    rs_dir: PathBuf,
+    manifest_dir: PathBuf,
+    pkg: Crate,
+}
 
-    // if running with bazel run, move to the requested cwd
+impl Bazelifier {
+    fn new() -> eyre::Result<Self> {
+        let opts = Options::parse();
+        let abs = std::fs::canonicalize(&opts.cargo_file)?;
+        let manifest_dir = abs.parent().unwrap();
+
+        let git_dir = String::from_utf8_lossy(
+            &Command::new("git")
+                .args(["rev-parse", "--show-toplevel"])
+                .output()?
+                .stdout,
+        )
+        .trim()
+        .to_string();
+
+        let rs_dir = Path::new(&git_dir).join("rs");
+        let contents = std::fs::read_to_string(&abs)?;
+        let pkg = toml::from_str::<Crate>(&contents)?;
+
+        Ok(Self {
+            opts,
+            manifest_dir: manifest_dir.into(),
+            rs_dir,
+            pkg,
+        })
+    }
+
+    fn process_deps(
+        &self,
+        deps_iter: btree_map::Iter<String, Dep>,
+        deps: &mut Deps,
+        macro_deps: &mut Deps,
+        aliases: &mut BTreeMap<String, String>,
+    ) -> eyre::Result<()> {
+        for (dep_name, dep) in deps_iter {
+            let dep_text = match dep {
+                Dep::VersionExtra(NormalDep {
+                    package: Some(alias),
+                    ..
+                }) => {
+                    aliases.insert(format!("@crate_index//:{}", alias), dep_name.clone());
+                    format!("@crate_index//:{}", alias)
+                }
+                Dep::Version { .. } | Dep::VersionExtra { .. } | Dep::Git { .. } => {
+                    format!("@crate_index//:{}", dep_name)
+                }
+                Dep::Local(LocalDep { path }) => format!(
+                    "//rs/{}",
+                    pathdiff::diff_paths(
+                        self.manifest_dir.join(path).canonicalize()?,
+                        &self.rs_dir
+                    )
+                    .unwrap()
+                    .display()
+                ),
+            };
+            if MACRO_CRATES.contains(&&**dep_name) {
+                macro_deps.insert(dep_text);
+            } else {
+                deps.insert(dep_text);
+            }
+        }
+        Ok(())
+    }
+
+    fn generate_tests(&self, bf: &mut BuildFile) -> eyre::Result<()> {
+        self.process_deps(
+            self.pkg.dev_dependencies.iter(),
+            &mut bf.dev_deps,
+            &mut bf.macro_dev_deps,
+            &mut bf.aliases,
+        )?;
+
+        bf.dev_deps = bf.dev_deps.difference(&bf.deps).cloned().collect();
+        bf.macro_dev_deps = bf
+            .macro_dev_deps
+            .difference(&bf.macro_deps)
+            .cloned()
+            .collect();
+
+        if let Ok(tests_dir) = std::fs::read_dir(self.manifest_dir.join("tests")) {
+            for file in tests_dir {
+                let file = file?;
+                if !file.path().extension().map_or(false, |x| x == "rs") {
+                    continue;
+                }
+                if let Some(stem) = file.path().file_stem() {
+                    let stem = stem.to_string_lossy();
+                    bf.integration_tests.push(BuildTest {
+                        name: stem.to_string(),
+                        filepath: format!("tests/{}.rs", stem),
+                    });
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn run(self) -> eyre::Result<()> {
+        let buildfile_path = self.manifest_dir.join("BUILD.bazel");
+        if !self.opts.dry_run && !self.opts.force && buildfile_path.exists() {
+            eprintln!(
+                "{} already exists, refusing to overwrite it",
+                buildfile_path.display()
+            );
+            std::process::exit(1);
+        }
+
+        if !self.pkg.target.is_empty() {
+            eprintln!("WARNING: Cargo.toml has a target-specific dependencies section. Please add those dependencies manually to BUILD.bazel.");
+        }
+
+        let lib_build_type = if self.pkg.lib.as_ref().map_or(false, |x| x.proc_macro) {
+            BuildType::ProcMacro
+        } else {
+            BuildType::Lib
+        };
+        let mut bf = BuildFile {
+            edition: &self.pkg.package.edition,
+            build_type: lib_build_type,
+            target_name: self.manifest_dir.file_name().unwrap().to_string_lossy(),
+            crate_name: self.pkg.package.name.replace('-', "_"),
+            gen_tests: self.opts.gen_tests,
+            ..Default::default()
+        };
+
+        self.process_deps(
+            self.pkg.dependencies.iter(),
+            &mut bf.deps,
+            &mut bf.macro_deps,
+            &mut bf.aliases,
+        )?;
+
+        if self.opts.gen_tests {
+            self.generate_tests(&mut bf)?;
+        }
+
+        if self.opts.dry_run {
+            std::io::stdout().write_all(bf.render()?.as_bytes())?;
+        } else {
+            std::fs::File::create(&buildfile_path)?.write_all(bf.render()?.as_bytes())?;
+
+            println!("Created {}", buildfile_path.display());
+        }
+
+        Ok(())
+    }
+}
+
+fn main() -> eyre::Result<()> {
     if let Some(wd) = std::env::var_os("BUILD_WORKING_DIRECTORY") {
         std::env::set_current_dir(wd)?;
     }
 
-    let abs = std::fs::canonicalize(&tomlfile)?;
-    let manifest_dir = abs.parent().unwrap();
-    let buildfile_path = manifest_dir.join("BUILD.bazel");
-    if !force && buildfile_path.exists() {
-        eprintln!(
-            "{} already exists, refusing to overwrite it",
-            buildfile_path.display()
-        );
-        std::process::exit(1);
-    }
-    let contents = std::fs::read_to_string(&abs)?;
-    let pkg = toml::from_str::<Crate>(&contents)?;
-
-    if !pkg.target.is_empty() {
-        eprintln!("WARNING: Cargo.toml has a target-specific dependencies section. Please double check the generated BUILD.bazel file before committing");
-    }
-
-    let git_dir = String::from_utf8_lossy(
-        &Command::new("git")
-            .args(["rev-parse", "--show-toplevel"])
-            .output()?
-            .stdout,
-    )
-    .trim()
-    .to_string();
-
-    let rs_dir = Path::new(&git_dir).join("rs");
-
-    let target_name = manifest_dir.file_name().unwrap().to_string_lossy();
-    let crate_name = pkg.package.name.replace('-', "_");
-
-    let mut deps = vec![];
-    let mut macro_deps = vec![];
-    let mut aliases = BTreeMap::new();
-    let build_type = if pkg.lib.map_or(false, |x| x.proc_macro) {
-        BuildType::ProcMacro
-    } else {
-        BuildType::Lib
-    };
-
-    for (dep_name, dep) in pkg.dependencies {
-        let dep_text = match dep {
-            Dep::VersionExtra(NormalDep {
-                package: Some(alias),
-                ..
-            }) => {
-                aliases.insert(format!("@crate_index//:{}", alias), dep_name.clone());
-                format!("@crate_index//:{}", alias)
-            }
-            Dep::Version { .. } | Dep::VersionExtra { .. } | Dep::Git { .. } => {
-                format!("@crate_index//:{}", dep_name)
-            }
-            Dep::Local(LocalDep { path }) => format!(
-                "//rs/{}",
-                pathdiff::diff_paths(manifest_dir.join(path).canonicalize()?, &rs_dir)
-                    .unwrap()
-                    .display()
-            ),
-        };
-        if MACRO_CRATES.contains(&&*dep_name) {
-            macro_deps.push(dep_text);
-        } else {
-            deps.push(dep_text);
-        }
-    }
-
-    let buildfile = Buildfile {
-        build_type,
-        target_name: &*target_name,
-        crate_name: &crate_name,
-        edition: &pkg.package.edition,
-        deps,
-        macro_deps,
-        aliases,
-    };
-
-    std::fs::File::create(&buildfile_path)?.write_all(buildfile.render()?.as_bytes())?;
-
-    println!("Created {}", buildfile_path.display());
-
-    Ok(())
+    let bzl = Bazelifier::new()?;
+    bzl.run()
 }
