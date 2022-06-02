0aa781c0ffd46e9822b6d092a56145fb6dfee977
Ali Piccioni <ali.piccioni@dfinity.org>
2022-06-02 15:52:43 +0000

    Send build metadata to Buildbuddy

---
+++ bazel/workspace_status.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+function remove_url_credentials() {
+    which perl >/dev/null && perl -pe 's#//.*?:.*?@#//#' || cat
+}
+
+repo_url=$(git config --get remote.origin.url | remove_url_credentials)
+echo "REPO_URL $repo_url"
+
+commit_sha=$(git rev-parse HEAD)
+echo "COMMIT_SHA $commit_sha"
+
+git_branch=$(git rev-parse --abbrev-ref HEAD)
+echo "GIT_BRANCH $git_branch"
+
+git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
+echo "GIT_TREE_STATUS $git_tree_status"
