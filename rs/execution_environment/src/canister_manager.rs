0f64952e66fea6d21f2b8abe737588dcc9191b94
Alexandra Zapuc <alexandra.zapuc@dfinity.org>
2022-06-02 11:20:28 +0000

    EXC-1117: Fix metric cycles_consumned_since_replica_started

--- rs/execution_environment/src/canister_manager.rs
+++ rs/execution_environment/src/canister_manager.rs
@@ -24,6 +24,7 @@ use ic_replicated_state::{
     SchedulerState, SystemState,
 };
 use ic_state_layout::{CanisterLayout, CheckpointLayout, RwPolicy};
+use ic_types::nominal_cycles::NominalCycles;
 use ic_types::{
     ingress::{IngressState, IngressStatus},
     messages::{Payload, RejectContext, Response as CanisterResponse, StopCanisterContext},
@@ -987,7 +988,19 @@ impl CanisterManager {
         // - its cycles are discarded.
 
         // Take out the canister from `ReplicatedState`.
-        let _canister_to_delete = state.take_canister_state(&canister_id_to_delete).unwrap();
+        let canister_to_delete = state.take_canister_state(&canister_id_to_delete).unwrap();
+        // Leftover cycles in the balance are considered `consumed`.
+        let consumed_cycles_by_canister_to_delete =
+            NominalCycles::from(canister_to_delete.system_state.balance())
+                + canister_to_delete
+                    .system_state
+                    .canister_metrics
+                    .consumed_cycles_since_replica_started;
+
+        state
+            .metadata
+            .subnet_metrics
+            .consumed_cycles_by_deleted_canisters += consumed_cycles_by_canister_to_delete;
 
         let layout = canister_layout(state.path(), &canister_id_to_delete);
         layout
