cb772766b7d3227fccd12fe58489a7bfaf04ba9c
Rüdiger Kapitza <ruediger.kapitza@dfinity.org>
2022-06-02 11:02:19 +0000

    Prepare nginx conf for ro rootfs

--- ic-os/boundary-guestos/rootfs/opt/ic/bin/setup-nginx.sh
+++ ic-os/boundary-guestos/rootfs/opt/ic/bin/setup-nginx.sh
@@ -3,47 +3,52 @@
 set -ex
 
 function copy_nns_url() {
+    NGINX_RUN="$1"
     if [ -e /boot/config/nns_public_key.pem ]; then
-        mkdir -p /run/ic-node/etc/nginx
-        cp /boot/config/nns_public_key.pem /run/ic-node/etc/nginx/ic_public_key.pem
-        mount --bind /run/ic-node/etc/nginx/ic_public_key.pem /etc/nginx/ic_public_key.pem
+        cp /boot/config/nns_public_key.pem "${NGINX_RUN}"/ic_public_key.pem
+        mount --bind "${NGINX_RUN}"/ic_public_key.pem /etc/nginx/ic_public_key.pem
     fi
 }
 
 function copy_certs() {
+    NGINX_RUN="$1"
     CERT_DIR=/boot/config/certs
+
     if [[ -f ${CERT_DIR}/fullchain.pem ]] && [[ -f ${CERT_DIR}/privkey.pem ]] && [[ -f ${CERT_DIR}/chain.pem ]]; then
         echo "Using certificates ${CERT_DIR}/fullchain.pem ${CERT_DIR}/privkey.pem ${CERT_DIR}/chain.pem"
-        cp ${CERT_DIR}/fullchain.pem /etc/nginx/certs/fullchain.pem
-        cp ${CERT_DIR}/privkey.pem /etc/nginx/keys/privkey.pem
-        cp ${CERT_DIR}/chain.pem /etc/nginx/certs/chain.pem
+
+        cp ${CERT_DIR}/fullchain.pem "${NGINX_RUN}"/certs/fullchain.pem
+        cp ${CERT_DIR}/privkey.pem "${NGINX_RUN}"/keys/privkey.pem
+        cp ${CERT_DIR}/chain.pem "${NGINX_RUN}"/certs/chain.pem
+
+        mount --bind "${NGINX_RUN}"/certs /etc/nginx/certs
+        mount --bind "${NGINX_RUN}"/keys /etc/nginx/keys
     else
         echo "Not copying certificates"
     fi
 }
 
 function copy_deny_list() {
+    NGINX_RUN="$1"
     DENY_LIST=/boot/config/denylist.map
     if [[ -f ${DENY_LIST} ]]; then
-        cp $DENY_LIST /etc/nginx/denylist.map
+        cp "${DENY_LIST}" "${NGINX_RUN}"/denylist.map
+        mount --bind "${NGINX_RUN}"/denylist.map /etc/nginx/denylist.map
     fi
 }
 
-function restore_context() {
-    restorecon -v /etc/nginx/ic_public_key.pem
-    restorecon -v /etc/nginx/certs/fullchain.pem
-    restorecon -v /etc/nginx/keys/privkey.pem
-    restorecon -v /etc/nginx/certs/chain.pem
-    restorecon -v /etc/nginx/conf.d/*.conf
-    restorecon -v /etc/nginx/denylist.map
-}
-
 function setup_domain_name() {
+    NGINX_RUN="$1"
+
+    cp /etc/nginx/conf.d/* "$NGINX_RUN"/conf.d/
+    mount --bind "${NGINX_RUN}"/conf.d /etc/nginx/conf.d
+
     source /boot/config/nginxdomain.conf
     if [[ -z "$DOMAIN" ]] || [[ -z "$TLD" ]]; then
         echo "\$DOMAIN or \$TLD variable not set. Nginx won't be configured. " 1>&2
         exit 1
     fi
+
     pushd /etc/nginx/conf.d
     for filename in ./001-mainnet-nginx.conf ./002-rosetta-nginx.conf ./999-test-ic-nginx.conf.exclude; do
         sed -i -e "s/{{DOMAIN}}/${DOMAIN}/g" -e "s/{{TLD}}/${TLD}/g" ${filename}
@@ -52,10 +57,12 @@ function setup_domain_name() {
 }
 
 function enable_dev_mode() {
+    NGINX_RUN="$1"
+
     # The boundary node image is built in prod configuration. Any changes need
     # to enable development mode at runtime should go in here.
     if [ $(cat /boot/config/deployment_type) == "dev" ]; then
-        pushd /etc/nginx/conf.d
+        pushd "$NGINX_RUN"/conf.d
         mv ./999-test-ic-nginx.conf.exclude ./999-test-ic-nginx.conf
         mv ./001-mainnet-nginx.conf ./001-mainnet-nginx.conf.exclude
         mv ./002-rosetta-nginx.conf ./002-rosetta-nginx.conf.exclude
@@ -63,9 +70,21 @@ function enable_dev_mode() {
     fi
 }
 
-copy_nns_url
-copy_certs
-copy_deny_list
-setup_domain_name
-enable_dev_mode
+function restore_context() {
+    restorecon -v /etc/nginx/ic_public_key.pem \
+        /etc/nginx/certs/fullchain.pem \
+        /etc/nginx/keys/privkey.pem \
+        /etc/nginx/certs/chain.pem \
+        /etc/nginx/conf.d/*.conf \
+        /etc/nginx/denylist.map
+}
+
+# Place to assemble nginx related configuration for the current run
+NGINX_RUN="/run/ic-node/etc/nginx"
+
+copy_nns_url "$NGINX_RUN"
+copy_certs "$NGINX_RUN"
+copy_deny_list "$NGINX_RUN"
+setup_domain_name "$NGINX_RUN"
+enable_dev_mode "$NGINX_RUN"
 restore_context
