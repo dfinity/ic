0ccde7f2d647f7fa6c8a0452b34bdc55d38ba27d
Or Ricon <rikonor@gmail.com>
2022-06-07 23:01:56 +0300

    clean up shell script a bit

--- gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh
+++ gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh
@@ -8,24 +8,30 @@ if (($# < 1)); then
     exit 1
 fi
 
+function disk_image_exists() {
+    local GIT_SHA=$1
+
+    URLS=(
+        "https://download.dfinity.systems/blessed/ic/${GIT_SHA}/guest-os/disk-img/disk-img.tar.gz"
+        "https://download.dfinity.systems/blessed/ic/${GIT_SHA}/guest-os/disk-img/disk-img.tar.zst"
+        "https://download.dfinity.systems/ic/${GIT_SHA}/guest-os/disk-img.tar.gz"
+        "https://download.dfinity.systems/ic/${GIT_SHA}/guest-os/disk-img.tar.zst"
+        "https://download.dfinity.systems/ic/${GIT_SHA}/guest-os/disk-img/disk-img.tar.gz"
+        "https://download.dfinity.systems/ic/${GIT_SHA}/guest-os/disk-img/disk-img.tar.zst"
+    )
+
+    for url in ${URLS[@]}; do
+        curl -sL -I --fail "${url}" -o /dev/null
+        if [[ "$?" == "0" ]]; then
+            return 0
+        fi
+    done
+
+    return 1
+}
+
 branch_name=${1:-}
 count=${2:-1}
-
-function disk_image_exists() {
-    git_sha=$1
-    curl --output /dev/null --silent --head --fail \
-        "https://download.dfinity.systems/blessed/ic/$git_sha/guest-os/disk-img/disk-img.tar.gz" \
-        || curl --output /dev/null --silent --head --fail \
-            "https://download.dfinity.systems/ic/$git_sha/guest-os/disk-img/disk-img.tar.gz" \
-        || curl --output /dev/null --silent --head --fail \
-            "https://download.dfinity.systems/ic/$git_sha/guest-os/disk-img.tar.gz" \
-        || curl --output /dev/null --silent --head --fail \
-            "https://download.dfinity.systems/blessed/ic/$git_sha/guest-os/disk-img/disk-img.tar.zst" \
-        || curl --output /dev/null --silent --head --fail \
-            "https://download.dfinity.systems/ic/$git_sha/guest-os/disk-img/disk-img.tar.zst" \
-        || curl --output /dev/null --silent --head --fail \
-            "https://download.dfinity.systems/ic/$git_sha/guest-os/disk-img.tar.zst"
-}
 
 for git_sha in $(git log --format=format:%H "$branch_name" --max-count=50); do
     test "$count" = 0 && exit 0
