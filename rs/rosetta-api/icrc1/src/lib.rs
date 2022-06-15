8ace494208e34717195317e99e43a23d6b6e6671
Roman Kashitsyn <roman.kashitsyn@dfinity.org>
2022-06-15 12:15:58 +0000

    [ROSETTA1-296] ICRC-1 block encoding and decoding

--- rs/rosetta-api/icrc1/src/lib.rs
+++ rs/rosetta-api/icrc1/src/lib.rs
@@ -1,4 +1,5 @@
 use candid::CandidType;
+use ciborium::tag::Required;
 use ic_base_types::PrincipalId;
 use ic_ledger_core::{
     archive::ArchiveCanisterWasm,
@@ -10,8 +11,10 @@ use ic_ledger_core::{
     tokens::Tokens,
 };
 use serde::{Deserialize, Serialize};
+use serde_bytes::ByteBuf;
 use std::borrow::Cow;
 use std::collections::{BTreeMap, HashMap, VecDeque};
+use std::convert::TryFrom;
 use std::time::Duration;
 
 const TRANSACTION_WINDOW: Duration = Duration::from_secs(24 * 60 * 60);
@@ -26,27 +29,114 @@ pub type Subaccount = [u8; 32];
     Serialize, Deserialize, CandidType, Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord,
 )]
 pub struct Account {
-    pub owner: PrincipalId,
+    pub of: PrincipalId,
     pub subaccount: Option<Subaccount>,
+}
+
+fn ser_compact_account<S>(acc: &Account, s: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::ser::Serializer,
+{
+    CompactAccount::from(acc.clone()).serialize(s)
+}
+
+fn de_compact_account<'de, D>(d: D) -> Result<Account, D::Error>
+where
+    D: serde::de::Deserializer<'de>,
+{
+    use serde::de::Error;
+    let compact_account = CompactAccount::deserialize(d)?;
+    Account::try_from(compact_account).map_err(D::Error::custom)
+}
+
+/// A compact representation of an Account.
+///
+/// Instead of encoding accounts as structs with named fields,
+/// we encode them as tuples with variables number of elements.
+/// ```text
+/// [bytes] <=> Account { of: bytes, subaccount : None }
+/// [x: bytes, y: bytes] <=> Account { of: x, subaccount: Some(y) }
+/// ```
+#[derive(Serialize, Deserialize)]
+#[serde(transparent)]
+struct CompactAccount(Vec<ByteBuf>);
+
+impl From<Account> for CompactAccount {
+    fn from(acc: Account) -> Self {
+        let mut components = vec![ByteBuf::from(acc.of.to_vec())];
+        if let Some(sub) = acc.subaccount {
+            components.push(ByteBuf::from(sub.to_vec()))
+        }
+        CompactAccount(components)
+    }
+}
+
+impl TryFrom<CompactAccount> for Account {
+    type Error = String;
+    fn try_from(compact: CompactAccount) -> Result<Account, String> {
+        let elems = compact.0;
+        if elems.is_empty() {
+            return Err("account tuple must have at least one element".to_string());
+        }
+        if elems.len() > 2 {
+            return Err(format!(
+                "account tuple must have at most two elements, got {}",
+                elems.len()
+            ));
+        }
+
+        let principal = PrincipalId::try_from(&elems[0][..])
+            .map_err(|e| format!("invalid principal: {}", e))?;
+        let subaccount = if elems.len() > 1 {
+            Some(Subaccount::try_from(&elems[1][..]).map_err(|_| {
+                format!(
+                    "invalid subaccount: expected 32 bytes, got {}",
+                    elems[1].len()
+                )
+            })?)
+        } else {
+            None
+        };
+
+        Ok(Account {
+            of: principal,
+            subaccount,
+        })
+    }
 }
 
 #[derive(
     Serialize, Deserialize, CandidType, Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord,
 )]
+#[serde(tag = "op")]
 pub enum Operation {
+    #[serde(rename = "mint")]
     Mint {
+        #[serde(serialize_with = "ser_compact_account")]
+        #[serde(deserialize_with = "de_compact_account")]
         to: Account,
-        amount: Tokens,
+        #[serde(rename = "amt")]
+        amount: u64,
     },
+    #[serde(rename = "xfer")]
     Transfer {
+        #[serde(serialize_with = "ser_compact_account")]
+        #[serde(deserialize_with = "de_compact_account")]
         from: Account,
+        #[serde(serialize_with = "ser_compact_account")]
+        #[serde(deserialize_with = "de_compact_account")]
         to: Account,
-        amount: Tokens,
-        fee: Tokens,
+        #[serde(rename = "amt")]
+        amount: u64,
+        fee: u64,
     },
+    #[serde(rename = "burn")]
     Burn {
+        #[serde(serialize_with = "ser_compact_account")]
+        #[serde(deserialize_with = "de_compact_account")]
         from: Account,
-        amount: Tokens,
+        #[serde(rename = "amt")]
+        amount: u64,
     },
 }
 
@@ -54,8 +144,10 @@ pub enum Operation {
     Serialize, Deserialize, CandidType, Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord,
 )]
 pub struct Transaction {
-    operation: Operation,
-    created_at_time: TimeStamp,
+    #[serde(flatten)]
+    pub operation: Operation,
+    #[serde(rename = "ts")]
+    pub created_at_time: u64,
 }
 
 impl LedgerTransaction for Transaction {
@@ -63,13 +155,16 @@ impl LedgerTransaction for Transaction {
 
     fn burn(from: Account, amount: Tokens, created_at_time: TimeStamp) -> Self {
         Self {
-            operation: Operation::Burn { from, amount },
-            created_at_time,
+            operation: Operation::Burn {
+                from,
+                amount: amount.get_e8s(),
+            },
+            created_at_time: created_at_time.as_nanos_since_unix_epoch(),
         }
     }
 
     fn created_at_time(&self) -> TimeStamp {
-        self.created_at_time
+        TimeStamp::from_nanos_since_unix_epoch(self.created_at_time)
     }
 
     fn hash(&self) -> HashOf<Self> {
@@ -87,9 +182,9 @@ impl LedgerTransaction for Transaction {
                 to,
                 amount,
                 fee,
-            } => balances.transfer(from, to, *amount, *fee),
-            Operation::Burn { from, amount } => balances.burn(from, *amount),
-            Operation::Mint { to, amount } => balances.mint(to, *amount),
+            } => balances.transfer(from, to, Tokens::from_e8s(*amount), Tokens::from_e8s(*fee)),
+            Operation::Burn { from, amount } => balances.burn(from, Tokens::from_e8s(*amount)),
+            Operation::Mint { to, amount } => balances.mint(to, Tokens::from_e8s(*amount)),
         }
     }
 }
@@ -98,20 +193,32 @@ impl LedgerTransaction for Transaction {
     Serialize, Deserialize, CandidType, Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord,
 )]
 pub struct Block {
+    #[serde(rename = "phash")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub parent_hash: Option<HashOf<EncodedBlock>>,
+    #[serde(rename = "tx")]
     pub transaction: Transaction,
-    pub timestamp: TimeStamp,
+    #[serde(rename = "ts")]
+    pub timestamp: u64,
 }
+
+type TaggedBlock = Required<Block, 55799>;
 
 impl BlockType for Block {
     type Transaction = Transaction;
 
     fn encode(self) -> EncodedBlock {
-        unimplemented!("block encoding is not decided yet")
+        let mut bytes = vec![];
+        let value: TaggedBlock = Required(self);
+        ciborium::ser::into_writer(&value, &mut bytes).expect("bug: failed to encode a block");
+        EncodedBlock::from_vec(bytes)
     }
 
-    fn decode(_encoded_block: EncodedBlock) -> Result<Self, String> {
-        unimplemented!("block encoding is not decided yet")
+    fn decode(encoded_block: EncodedBlock) -> Result<Self, String> {
+        let bytes = encoded_block.into_vec();
+        let tagged_block: TaggedBlock = ciborium::de::from_reader(&bytes[..])
+            .map_err(|e| format!("failed to decode a block: {}", e))?;
+        Ok(tagged_block.0)
     }
 
     fn parent_hash(&self) -> Option<HashOf<EncodedBlock>> {
@@ -119,7 +226,7 @@ impl BlockType for Block {
     }
 
     fn timestamp(&self) -> TimeStamp {
-        self.timestamp
+        TimeStamp::from_nanos_since_unix_epoch(self.timestamp)
     }
 
     fn from_transaction(
@@ -130,7 +237,7 @@ impl BlockType for Block {
         Self {
             parent_hash,
             transaction,
-            timestamp,
+            timestamp: timestamp.as_nanos_since_unix_epoch(),
         }
     }
 }
