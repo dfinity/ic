f5004532853b72777abc6c84eeea7c3f52ca4f22
Yvonne-Anne Pignolet <yvonneanne@dfinity.org>
2022-06-30 19:38:17 +0000

    Improve wasm generator nightly tests runs (timeout, logging)

--- rs/tests/src/wasm_generator_test.rs
+++ rs/tests/src/wasm_generator_test.rs
@@ -7,6 +7,7 @@ use ic_fondue::ic_manager::IcHandle;
 use ic_fondue::{self};
 use ic_registry_subnet_type::SubnetType;
 use ic_utils::interfaces::ManagementCanister;
+use slog::info;
 use std::env;
 use std::fs;
 use std::path::Path;
@@ -16,13 +17,22 @@ pub fn config() -> InternetComputer {
 }
 
 pub fn test(handle: IcHandle, ctx: &ic_fondue::pot::Context) {
-    let rt = tokio::runtime::Runtime::new().expect("Could not create tokio runtime.");
-    rt.block_on(async move {
-        let mut rng = ctx.rng.clone();
-        let node = util::get_random_application_node_endpoint(&handle, &mut rng);
-        node.assert_ready(ctx).await;
+    let mut rng = ctx.rng.clone();
+    let endpoints: Vec<_> = handle.as_permutation(&mut rng).collect();
+    let node = util::get_random_application_node_endpoint(&handle, &mut rng);
+    util::block_on(async move {
+        info!(ctx.logger, "Asserting reachability of all nodes..");
+        // Assert all nodes are reachable via http:://[IPv6]:8080/api/v2/status
+        util::assert_endpoints_reachability(
+            endpoints.as_slice(),
+            util::EndpointsStatus::AllReachable,
+        )
+        .await;
+
+        info!(ctx.logger, "All nodes are reachable, creating agent..");
         let agent = util::assert_create_agent(node.url.as_str()).await;
 
+        info!(ctx.logger, "Reading canister paths..");
         // A path of a directory containing canisters has to be passed in to the test
         // through an env variable named `RANDOM_CANISTERS_BASE_DIR`.
         let canisters_base_dir = env::var("RANDOM_CANISTERS_BASE_DIR")
@@ -31,8 +41,10 @@ pub fn test(handle: IcHandle, ctx: &ic_fondue::pot::Context) {
             fs::read_dir(canisters_base_dir).expect("directory with random canisters is incorrect");
 
         for p in can_paths {
+            info!(ctx.logger, "Installing canister {:?}..", p);
             let cid =
                 install_random_canister(&agent, &p.expect("canister path incorrect").path()).await;
+            info!(ctx.logger, "Send query to canister");
             // Verify that the compute function exported by the installed canister can be
             // called.
             agent
