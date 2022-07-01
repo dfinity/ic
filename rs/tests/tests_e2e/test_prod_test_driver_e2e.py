60eaebbcdf0d5a2e8d6e80991a8dc3fe0abaaeb4
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2022-07-01 07:57:43 +0000

    Fix + add tests to prod-test-driver

--- rs/tests/tests_e2e/test_prod_test_driver_e2e.py
+++ rs/tests/tests_e2e/test_prod_test_driver_e2e.py
@@ -8,6 +8,8 @@ import tempfile
 import unittest
 from ast import Dict
 from pathlib import Path
+from typing import List
+from typing import Optional
 
 import expected_results
 
@@ -84,6 +86,7 @@ class TestRunSystemTests(unittest.TestCase):
     def test__suite_with_all_tests_passing(self):
         self._run_suite_to_completion(
             suite_name="suite_to_succeed",
+            additional_args=["--keep_artifacts", "--keep_tmp_dirs"],
             expected_suite_contract=expected_results.suite_contract_to_succeed,
             expected_suite_result=expected_results.suite_result_to_succeed,
             expected_exit_code=0,
@@ -92,6 +95,7 @@ class TestRunSystemTests(unittest.TestCase):
     def test__suite_with_some_tests_failing(self):
         self._run_suite_to_completion(
             suite_name="suite_to_fail",
+            additional_args=["--keep_artifacts", "--keep_tmp_dirs"],
             expected_suite_contract=expected_results.suite_contract_to_fail,
             expected_suite_result=expected_results.suite_result_to_fail,
             expected_exit_code=1,
@@ -100,31 +104,63 @@ class TestRunSystemTests(unittest.TestCase):
     def test__suite_with_test_timeout(self):
         self._run_suite_to_completion(
             suite_name="suite_to_timeout",
+            additional_args=["--keep_artifacts", "--keep_tmp_dirs"],
             expected_suite_contract=expected_results.suite_contract_to_timeout,
             expected_suite_result=expected_results.suite_result_to_timeout,
             expected_exit_code=124,
         )
 
-    def _run_suite_with_early_failure(self, expected_error):
+    def test__suite_with_include_pattern_case_1(self):
+        include_pattern = "test_success_1"
+        # test_success_2 is excluded from pot_success_1
+        self._run_suite_to_completion(
+            suite_name="suite_to_succeed",
+            additional_args=[f"--include-pattern={include_pattern}", "--keep_artifacts", "--keep_tmp_dirs"],
+            expected_suite_contract=expected_results.suite_contract_include_pattern_case_1,
+            expected_suite_result=expected_results.suite_result_include_pattern_case_1,
+            expected_exit_code=0,
+        )
+
+    def test__suite_with_include_pattern_case_2(self):
+        include_pattern = "success"
+        # test_fail_1 is excluded from pot_fail_1
+        # test_fail_1 is also excluded from pot_fail_2, thus the pot_fail_2 itself is excluded from the suite (as it becomes empty)
+        # As all failing tests are excluded, the suite should succeed.
+        self._run_suite_to_completion(
+            suite_name="suite_to_fail",
+            additional_args=[f"--include-pattern={include_pattern}", "--keep_artifacts", "--keep_tmp_dirs"],
+            expected_suite_contract=expected_results.suite_contract_include_pattern_case_2,
+            expected_suite_result=expected_results.suite_result_include_pattern_case_2,
+            expected_exit_code=0,
+        )
+
+    def _run_suite_with_early_failure(self, expected_error: str):
         process = subprocess.run([f"{self.script_path}", "--keep_artifacts"], env=self.env, capture_output=True)
         # Assert
         stderr = process.stderr.decode()
         self.assertEqual(process.returncode, 1)
         self.assertTrue(expected_error in stderr)
 
-    def _run_suite_to_completion(self, suite_name, expected_exit_code, expected_suite_contract, expected_suite_result):
-        # We need to keep the artifacts, otherwise the first test will erase them and consequent tests will fail.
-        process = subprocess.run(
-            [f"{self.script_path}", f"--suite={suite_name}", "--keep_artifacts", "--keep_tmp_dirs"], env=self.env
-        )
+    def _run_suite_to_completion(
+        self,
+        suite_name: str,
+        additional_args: Optional[List[str]],
+        expected_exit_code: int,
+        expected_suite_contract: Dict,
+        expected_suite_result: Dict,
+    ):
+        cmd = [f"{self.script_path}", f"--suite={suite_name}"]
+        if additional_args is not None:
+            cmd += additional_args
+        process = subprocess.run(cmd, env=self.env)
         # Assert correct exit code.
         self.assertEqual(process.returncode, expected_exit_code)
         # Assert correct suite_execution_contract.json content.
         actual_suite_contract = self._find_parse_json_by_name(self.suite_contract_wildcard)
-        self.assertDictEqual(expected_suite_contract, actual_suite_contract)
+        self.assertDictEqual(expected_suite_contract, actual_suite_contract)  # type: ignore
         # Assert correct test-result.json content.
         actual_suite_result = self._find_parse_json_by_name(self.suite_result_wildcard)
-        self.assertDictEqual(expected_suite_result, actual_suite_result)
+        self.assertDictEqual(expected_suite_result, actual_suite_result)  # type: ignore
 
 
 if __name__ == "__main__":
