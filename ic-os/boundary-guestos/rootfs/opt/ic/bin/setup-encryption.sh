4c25fd11687865b0da971c2fc081ba6aeef30e6b
Anna Galanou <anna.galanou@dfinity.org>
2022-07-05 10:59:27 +0000

    Setup and enable /boot/config encryption and place key file under /boot/grub

--- ic-os/boundary-guestos/rootfs/opt/ic/bin/setup-encryption.sh
+++ ic-os/boundary-guestos/rootfs/opt/ic/bin/setup-encryption.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+KEYFILE=/boot/grub/store.keyfile
+
 if [ -e /dev/vda10 ]; then
     exit 0
 fi
@@ -10,9 +12,11 @@ echo "- - L" | sfdisk --force --no-reread -a /dev/vda
 
 # Generate a key and initialize encrypted store with it.
 partprobe /dev/vda
-umask 0077
-dd if=/dev/random of=/boot/config/store.keyfile bs=16 count=1
+if [[ ! -f "${KEYFILE}" ]]; then
+    umask 0077
+    dd if=/dev/random of="$KEYFILE" bs=16 count=1
+fi
 # Set minimal iteration count -- we already use a random key with
 # maximal entropy, pbkdf doesn't gain anything (besides slowing
 # down boot by a couple seconds which needlessly annoys for testing).
-cryptsetup luksFormat --type luks2 --pbkdf pbkdf2 --pbkdf-force-iterations 1000 /dev/vda10 /boot/config/store.keyfile
+cryptsetup luksFormat --type luks2 --pbkdf pbkdf2 --pbkdf-force-iterations 1000 /dev/vda10 "${KEYFILE}"
