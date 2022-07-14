ef5d854a03c482a4b7ca22cfc810e2479b69d9f6
Helge Bahmann <helge.bahmann@dfinity.org>
2022-07-14 16:25:18 +0200

    Support building upgrade images in bazel

---
+++ toolchains/sysimage/build_upgrade_image.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#
+# Builds an upgrade image from individual partition images.
+#
+# Call example:
+#   build_upgrade_image -o upgrade.tar.gz -b boot.img.tar -r root.img.tar -v version.txt -c gzip
+#
+import argparse
+import atexit
+import shutil
+import subprocess
+import sys
+import tempfile
+
+COMPRESSOR_PROGRAMS = {
+    "gz": "gzip",
+    "gzip": "gzip",
+    "zstd": "zstd --threads=0 -10",
+}
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--out", help="Target (tar) file to write upgrade package to", type=str)
+    parser.add_argument("-b", "--boot", help="The (tarred) boot filesystem image", type=str)
+    parser.add_argument("-r", "--root", help="The (tarred) root filesystem image", type=str)
+    parser.add_argument("-v", "--versionfile", help="The version file in the upgrade image", type=str)
+    parser.add_argument("-c", "--compression", help="Compression format of upgrade package", type=str, default="gz")
+
+    args = parser.parse_args(sys.argv[1:])
+
+    out_file = args.out
+    root_image = args.root
+    boot_image = args.boot
+    version_file = args.versionfile
+    compression = args.compression
+
+    tmpdir = tempfile.mkdtemp()
+    atexit.register(lambda: shutil.rmtree(tmpdir))
+
+    subprocess.run(["tar", "xf", boot_image, "--transform=s/partition.img/boot.img/", "-C", tmpdir], check=True)
+
+    subprocess.run(["tar", "xf", root_image, "--transform=s/partition.img/root.img/", "-C", tmpdir], check=True)
+    shutil.copy(version_file, tmpdir + "/VERSION.TXT", follow_symlinks=False)
+    subprocess.run(
+        [
+            "tar",
+            "cf",
+            out_file,
+            "--sort=name",
+            "--owner=root:0",
+            "--group=root:0",
+            "--mtime=UTC 1970-01-01 00:00:00",
+            "--use-compress-program=" + COMPRESSOR_PROGRAMS[compression],
+            "--sparse",
+            "-C",
+            tmpdir,
+            ".",
+        ],
+        check=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
