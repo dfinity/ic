f5263ab7db2805c14fc5347ece708d464618aeea
Tim Gretler <tim.gretler@dfinity.org>
2022-07-25 20:05:59 +0000

    Remove docker from bitcoin test

--- rs/tests/src/btc_integration/btc.rs
+++ rs/tests/src/btc_integration/btc.rs
@@ -24,7 +24,8 @@ use crate::driver::test_env_api::{
     retry, HasPublicApiUrl, HasTopologySnapshot, IcNodeContainer, SshSession, ADMIN, RETRY_BACKOFF,
     RETRY_TIMEOUT,
 };
-use crate::driver::universal_vm::UniversalVms;
+use crate::driver::universal_vm::{DeployedUniversalVm, UniversalVms};
+
 use crate::nns::NnsExt;
 use crate::util::{self, *};
 use crate::{
@@ -52,13 +53,8 @@ pub fn config(env: TestEnv) {
     // docker bitcoind image uses 8332 for the rpc server
     // https://en.bitcoinwiki.org/wiki/Running_Bitcoind
     let activate_script = r#"#!/bin/sh
-cp /config/bitcoin.conf /tmp/bitcoin.conf
-docker run  --name=bitcoind-node -d \
-  -p 8332:8332 \
-  -p 18444:18444 \
-  -v /tmp:/bitcoin/.bitcoin \
-  kylemanna/bitcoind
-"#;
+        nix-env -iA nixos.bitcoind
+    "#;
     let config_dir = env
         .single_activate_script_config_dir(UNIVERSAL_VM_NAME, activate_script)
         .unwrap();
@@ -75,6 +71,13 @@ docker run  --name=bitcoind-node -d \
     rpcuser=btc-dev-preview
     rpcpassword=Wjh4u6SAjT4UMJKxPmoZ0AN2r9qbE-ksXQ5I2_-Hm4w=
     rpcauth=btc-dev-preview:8555f1162d473af8e1f744aa056fd728$afaf9cb17b8cf0e8e65994d1195e4b3a4348963b08897b4084d210e5ee588bcb
+    
+    [regtest]
+    rpcport=8332
+    rpcbind=[::]:8332 
+    rpcallowip=0.0.0.0/0
+    rpcallowip=::/0
+    bind=[::]:18444    
     "#
     .as_bytes()).unwrap();
     bitcoin_conf.sync_all().unwrap();
@@ -86,8 +89,10 @@ docker run  --name=bitcoind-node -d \
 
     let deployed_universal_vm = env.get_deployed_universal_vm(UNIVERSAL_VM_NAME).unwrap();
     let universal_vm = deployed_universal_vm.get_vm().unwrap();
-    let btc_node_ipv6 = universal_vm.ipv6;
 
+    start_bitcoind(deployed_universal_vm);
+
+    let btc_node_ipv6 = universal_vm.ipv6;
     InternetComputer::new()
         .with_bitcoind_addr(SocketAddr::new(IpAddr::V6(btc_node_ipv6), 18444))
         .add_subnet(Subnet::new(SubnetType::System).add_nodes(1))
@@ -106,6 +111,11 @@ docker run  --name=bitcoind-node -d \
         .expect("failed to setup IC under test");
 }
 
+fn start_bitcoind(vm: DeployedUniversalVm) {
+    vm.block_on_bash_script(ADMIN, "bitcoind -conf=/config/bitcoin.conf -daemon")
+        .unwrap();
+}
+
 fn get_bitcoind_log(env: &TestEnv) {
     let f = || -> Result<(), anyhow::Error> {
         let r = {
@@ -114,12 +124,13 @@ fn get_bitcoind_log(env: &TestEnv) {
             // Give log file user permisison to copy it from the host.
             universal_vm.block_on_bash_script(
                 ADMIN,
-                "sudo chown -R $(id -u):$(id -g) /tmp/regtest/debug.log",
+                "sudo chown -R $(id -u):$(id -g) /home/admin/.bitcoin/regtest/debug.log",
             )?;
 
             // log file is mapped from docker contrainer to tmp directory
             let session = universal_vm.block_on_ssh_session(ADMIN).unwrap();
-            let (mut remote_file, _) = session.scp_recv(Path::new("/tmp/regtest/debug.log"))?;
+            let (mut remote_file, _) =
+                session.scp_recv(Path::new("/home/admin/.bitcoin/regtest/debug.log"))?;
 
             let mut buf = String::new();
             remote_file.read_to_string(&mut buf)?;
