1d0b76eccdbdaf4834f647331f86606609aed705
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2022-07-29 07:58:42 +0000

    Add Rust canister that makes ECDSA calls

---
+++ rs/rust_canisters/ecdsa/README.md
@@ -0,0 +1,32 @@
+ECDSA Canister
+================================
+
+Build
+-----
+
+```bash
+# Build the Wasm binary from `ic/rs`
+cargo build --target wasm32-unknown-unknown --release --bin ecdsa-canister
+
+# Optional: install ic-cdk-optimizer
+cargo install ic-cdk-optimizer
+
+# Reduce the Wasm binary size
+ic-cdk-optimizer target/wasm32-unknown-unknown/release/ecdsa-canister.wasm --output ecdsa-canister.wasm
+```
+
+Run
+---
+
+```bash
+# Enable ECDSA signing on a subnet:
+NNS_URL='http://[2001:4d78:40d:0:5000:67ff:fe4f:650d]:8080' # An NNS node
+SUBNET=ovko3-ja43o-cjmxw-ayco4-67g7l-nckpi-rplb7-cw3tc-vyoum-35bip-lqe
+ic-admin --nns-url $NNS_URL propose-to-update-subnet --subnet $SUBNET --ecdsa-keys-to-generate Secp256k1:test_key  --test-neuron-proposer
+ic-admin --nns-url $NNS_URL propose-to-update-subnet --subnet $SUBNET --ecdsa-key-signing-enable Secp256k1:test_key  --test-neuron-proposer
+
+# Payload (a json string) has to be encoded in hex.
+PAYLOAD=$(didc encode '(record {derivation_path = vec {}; key_name = "test_key" })' -d rust_canisters/ecdsa/ecdsa.did -t '(Options)')
+# Run workload
+cargo run --release --bin ic-workload-generator $NNS_URL -r 10 -n 300 -m Update --call-method "get_sig" --payload $PAYLOAD --canister ecdsa-canister.wasm
+```
---
+++ rs/rust_canisters/ecdsa/src/main.rs
@@ -0,0 +1,82 @@
+use candid::{candid_method, CandidType, Encode};
+use dfn_core::api::{call_bytes, Funds};
+use ic_cdk::api::print;
+use ic_cdk_macros::update;
+use ic_ic00_types::{EcdsaCurve, EcdsaKeyId, Method as Ic00Method, SignWithECDSAArgs, IC_00};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, CandidType)]
+struct Options {
+    derivation_path: Vec<Vec<u8>>,
+    key_name: String,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            derivation_path: vec![],
+            key_name: "test_key".to_string(),
+        }
+    }
+}
+
+#[candid_method(update)]
+#[update]
+async fn get_sig(options: Options) {
+    print(format!(
+        "calling get sig with key {} and derivation path {:?}",
+        options.key_name, options.derivation_path,
+    ));
+    let response = call_bytes(
+        IC_00,
+        &Ic00Method::SignWithECDSA.to_string(),
+        &Encode!(&SignWithECDSAArgs {
+            message_hash: vec![0; 32],
+            derivation_path: options.derivation_path,
+            key_id: EcdsaKeyId {
+                curve: EcdsaCurve::Secp256k1,
+                name: options.key_name,
+            },
+        })
+        .unwrap(),
+        Funds::new(1_000_000_000_000),
+    )
+    .await;
+    print(format!("got result {:?}", response));
+}
+
+// When run on native this prints the candid service definition of this
+// canister, from the methods annotated with `candid_method` above.
+//
+// Note that `cargo test` calls `main`, and `export_service` (which defines
+// `__export_service` in the current scope) needs to be called exactly once. So
+// in addition to `not(target_arch = "wasm32")` we have a `not(test)` guard here
+// to avoid calling `export_service`, which we need to call in the test below.
+#[cfg(not(any(target_arch = "wasm32", test)))]
+fn main() {
+    // The line below generates did types and service definition from the
+    // methods annotated with `candid_method` above. The definition is then
+    // obtained with `__export_service()`.
+    candid::export_service!();
+    std::print!("{}", __export_service());
+}
+
+#[cfg(any(target_arch = "wasm32", test))]
+fn main() {}
+
+#[test]
+fn check_candid_file() {
+    let candid = String::from_utf8(std::fs::read("ecdsa.did").unwrap()).unwrap();
+
+    // See comments in main above
+    candid::export_service!();
+    let expected = __export_service();
+
+    if candid != expected {
+        panic!(
+            "Generated candid definition does not match ecdsa.did. Run `cargo \
+            run --bin ecdsa-canister > rust_canisters/ecdsa/ecdsa.did` to update \
+            the candid file."
+        )
+    }
+}
