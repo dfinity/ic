a0a55f1490a177b912d955454269aecfde9fb982
Daniel Stefan Dietiker <stefan@dfinity.org>
2022-08-04 22:27:00 +0000

    [VER-1804] use public cdn in upgrade/downgrade test

--- rs/tests/src/orchestrator/utils/upgrade.rs
+++ rs/tests/src/orchestrator/utils/upgrade.rs
@@ -33,19 +33,22 @@ pub(crate) fn get_update_image_url(image_type: UpdateImageType, git_revision: &s
     match image_type {
         UpdateImageType::Image => {
             format!(
-                "http://download.proxy-global.dfinity.network:8080/ic/{}/guest-os/update-img/update-img.tar.gz",
+                "https://download.dfinity.systems/ic/{}/guest-os/update-img/update-img.tar.gz",
+                // "http://download.proxy-global.dfinity.network:8080/ic/{}/guest-os/update-img/update-img.tar.gz",
                 git_revision
             )
         }
         UpdateImageType::ImageTest => {
             format!(
-                "http://download.proxy-global.dfinity.network:8080/ic/{}/guest-os/update-img/update-img-test.tar.gz",
+                "https://download.dfinity.systems/ic/{}/guest-os/update-img/update-img-test.tar.gz",
+                // "http://download.proxy-global.dfinity.network:8080/ic/{}/guest-os/update-img/update-img-test.tar.gz",
                 git_revision
             )
         }
         UpdateImageType::Sha256 => {
             format!(
-                "http://download.proxy-global.dfinity.network:8080/ic/{}/guest-os/update-img/SHA256SUMS",
+                "https://download.dfinity.systems/ic/{}/guest-os/update-img/SHA256SUMS",
+                // "http://download.proxy-global.dfinity.network:8080/ic/{}/guest-os/update-img/SHA256SUMS",
                 git_revision
             )
         }
