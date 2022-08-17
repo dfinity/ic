2979dd7c5a788fa4bb56df11feb7898f95285c12
Ali Piccioni <ali.piccioni@dfinity.org>
2022-08-17 10:33:46 +0000

    Bazelize a few more dev crates

--- rs/replica/tests/cli.rs
+++ rs/replica/tests/cli.rs
@@ -2,10 +2,18 @@ use assert_cmd::Command;
 use ic_config::SAMPLE_CONFIG;
 use predicates::prelude::*;
 
+fn new_replica_command() -> Command {
+    match Command::cargo_bin("replica") {
+        // When in Cargo environment. This should be removed after Bazel the migration is complete.
+        Ok(v) => v,
+        // When in Bazel environment
+        Err(_) => Command::new("rs/replica/replica"),
+    }
+}
+
 #[test]
 fn nonexistent_config_causes_failure() {
-    Command::cargo_bin("replica")
-        .unwrap()
+    new_replica_command()
         .arg("does_not_exist.toml")
         .assert()
         .stderr(predicate::str::contains("No such file or directory"))
@@ -14,8 +22,7 @@ fn nonexistent_config_causes_failure() {
 
 #[test]
 fn too_many_arguments_causes_exit_code_1() {
-    Command::cargo_bin("replica")
-        .unwrap()
+    new_replica_command()
         .args(&["arg1", "arg2"])
         .assert()
         .stderr(predicate::str::starts_with(
@@ -26,8 +33,7 @@ fn too_many_arguments_causes_exit_code_1() {
 
 #[test]
 fn help_arg_prints_help() {
-    Command::cargo_bin("replica")
-        .unwrap()
+    new_replica_command()
         .arg("--help")
         .assert()
         .stdout(predicate::str::contains(
@@ -38,8 +44,7 @@ fn help_arg_prints_help() {
 
 #[test]
 fn can_read_config_from_stdin() {
-    Command::cargo_bin("replica")
-        .unwrap()
+    new_replica_command()
         .arg("-")
         .write_stdin("garbage garbage")
         .assert()
@@ -49,8 +54,7 @@ fn can_read_config_from_stdin() {
 
 #[test]
 fn can_read_config_from_arg() {
-    Command::cargo_bin("replica")
-        .unwrap()
+    new_replica_command()
         .arg("-")
         .write_stdin("--config=garbage garbage")
         .assert()
@@ -60,8 +64,7 @@ fn can_read_config_from_arg() {
 
 #[test]
 fn can_print_sample_config() {
-    Command::cargo_bin("replica")
-        .unwrap()
+    new_replica_command()
         .arg("--sample-config")
         .assert()
         .stdout(predicate::str::contains(SAMPLE_CONFIG))
