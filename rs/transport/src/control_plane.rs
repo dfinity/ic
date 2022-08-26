1890414203fadbc7e0c52bf05eb5e9cfd56edfc5
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2022-08-26 15:04:24 +0000

    [NET-1166] Add a unit test that checks a connection is establish in case if a single transient client handshake error

--- rs/transport/src/control_plane.rs
+++ rs/transport/src/control_plane.rs
@@ -541,6 +541,8 @@ mod tests {
     use ic_base_types::{NodeId, RegistryVersion};
     use ic_config::transport::TransportConfig;
     use ic_crypto::utils::TempCryptoComponent;
+    use ic_crypto_tls_interfaces::{TlsClientHandshakeError, TlsHandshake};
+    use ic_crypto_tls_interfaces_mocks::MockTlsHandshake;
     use ic_interfaces_transport::{Transport, TransportEvent, TransportEventHandler};
     use ic_logger::ReplicaLogger;
     use ic_metrics::MetricsRegistry;
@@ -553,6 +555,7 @@ mod tests {
     use std::net::SocketAddr;
     use std::str::FromStr;
     use std::sync::Arc;
+    use tokio::net::TcpStream;
     use tokio::sync::mpsc::{channel, Sender};
     use tower::{util::BoxCloneService, Service, ServiceExt};
     use tower_test::mock::Handle;
@@ -560,11 +563,44 @@ mod tests {
     const NODE_ID_1: NodeId = NODE_1;
     const NODE_ID_2: NodeId = NODE_2;
     const REG_V1: RegistryVersion = RegistryVersion::new(1);
-    const FLOW_TAG_1: u32 = 1234;
-    const FLOW_TAG_2: u32 = 1235;
+    const FLOW_TAG: u32 = 1234;
 
     const PORT_1: u16 = 65001;
     const PORT_2: u16 = 65002;
+
+    fn setup_test_peer<F>(
+        log: ReplicaLogger,
+        rt_handle: tokio::runtime::Handle,
+        node_id: NodeId,
+        port: u16,
+        registry_version: RegistryVersion,
+        registry_and_data: &mut RegistryAndDataProvider,
+        mut crypto_factory: F,
+    ) -> (Arc<dyn Transport>, Handle<TransportEvent, ()>, SocketAddr)
+    where
+        F: FnMut(&mut RegistryAndDataProvider, NodeId) -> Arc<dyn TlsHandshake + Send + Sync>,
+    {
+        let crypto = crypto_factory(registry_and_data, node_id);
+        let config = TransportConfig {
+            node_ip: "0.0.0.0".to_string(),
+            legacy_flow_tag: FLOW_TAG,
+            listening_port: port,
+            send_queue_size: 10,
+        };
+        let peer = create_transport(
+            node_id,
+            config,
+            registry_version,
+            MetricsRegistry::new(),
+            crypto,
+            rt_handle,
+            log,
+        );
+        let addr = SocketAddr::from_str(&format!("127.0.0.1:{}", port)).unwrap();
+        let (event_handler, mock_handle) = setup_mock_event_handler();
+        peer.set_event_handler(event_handler);
+        (peer, mock_handle, addr)
+    }
 
     #[test]
     fn test_start_connection_between_two_peers() {
@@ -646,6 +682,121 @@ mod tests {
         temp_crypto
     }
 
+    #[test]
+    fn test_single_transient_failure_of_tls_client_handshake() {
+        with_test_replica_logger(|log| {
+            let mut registry_and_data = RegistryAndDataProvider::new();
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let rt_handle = rt.handle().clone();
+
+            let crypto_factory_with_single_tls_handshake_client_failures =
+                |registry_and_data: &mut RegistryAndDataProvider, node_id: NodeId| {
+                    let mut mock_client_tls_handshake = MockTlsHandshake::new();
+                    let rt_handle = rt_handle.clone();
+
+                    let crypto = Arc::new(temp_crypto_component_with_tls_keys_in_registry(
+                        registry_and_data,
+                        node_id,
+                    ));
+
+                    mock_client_tls_handshake
+                        .expect_perform_tls_client_handshake()
+                        .times(1)
+                        .returning(
+                            move |_tcp_stream: TcpStream,
+                                  _server: NodeId,
+                                  _registry_version: RegistryVersion| {
+                                Err(TlsClientHandshakeError::HandshakeError {
+                                    internal_error: "transient".to_string(),
+                                })
+                            },
+                        );
+
+                    mock_client_tls_handshake
+                        .expect_perform_tls_client_handshake()
+                        .times(1)
+                        .returning(
+                            move |tcp_stream: TcpStream,
+                                  server: NodeId,
+                                  registry_version: RegistryVersion| {
+                                let rt_handle = rt_handle.clone();
+                                let crypto = crypto.clone();
+
+                                tokio::task::block_in_place(move || {
+                                    let rt_handle = rt_handle.clone();
+
+                                    rt_handle.block_on(async move {
+                                        crypto
+                                            .perform_tls_client_handshake(
+                                                tcp_stream,
+                                                server,
+                                                registry_version,
+                                            )
+                                            .await
+                                    })
+                                })
+                            },
+                        );
+
+                    Arc::new(mock_client_tls_handshake) as Arc<dyn TlsHandshake + Send + Sync>
+                };
+
+            let crypto_factory = |registry_and_data: &mut RegistryAndDataProvider,
+                                  node_id: NodeId| {
+                Arc::new(temp_crypto_component_with_tls_keys_in_registry(
+                    registry_and_data,
+                    node_id,
+                )) as Arc<dyn TlsHandshake + Send + Sync>
+            };
+
+            let (peer_1, mut mock_handle_peer_1, peer_1_addr) = setup_test_peer(
+                log.clone(),
+                rt.handle().clone(),
+                NODE_1,
+                PORT_1,
+                REG_V1,
+                &mut registry_and_data,
+                crypto_factory_with_single_tls_handshake_client_failures,
+            );
+            let (peer_2, mut mock_handle_peer_2, peer_2_addr) = setup_test_peer(
+                log,
+                rt.handle().clone(),
+                NODE_2,
+                PORT_2,
+                REG_V1,
+                &mut registry_and_data,
+                crypto_factory,
+            );
+
+            registry_and_data.registry.update_to_latest_version();
+            let (connected_1, mut done_1) = channel(1);
+            let (connected_2, mut done_2) = channel(1);
+            rt.spawn(async move {
+                let (event, rsp) = mock_handle_peer_1.next_request().await.unwrap();
+                if let TransportEvent::PeerUp(_) = event {
+                    connected_1.try_send(true).unwrap()
+                }
+                rsp.send_response(());
+            });
+            rt.spawn(async move {
+                let (event, rsp) = mock_handle_peer_2.next_request().await.unwrap();
+                if let TransportEvent::PeerUp(_) = event {
+                    connected_2.try_send(true).unwrap()
+                }
+                rsp.send_response(());
+            });
+            assert!(peer_1
+                .start_connection(&NODE_ID_2, peer_2_addr, REG_V1)
+                .is_ok());
+
+            assert!(peer_2
+                .start_connection(&NODE_ID_1, peer_1_addr, REG_V1)
+                .is_ok());
+            assert_eq!(done_1.blocking_recv(), Some(true));
+            assert_eq!(done_2.blocking_recv(), Some(true));
+        });
+    }
+
     fn setup_mock_event_handler() -> (TransportEventHandler, Handle<TransportEvent, ()>) {
         let (service, handle) = tower_test::mock::pair::<TransportEvent, ()>();
 
@@ -684,7 +835,7 @@ mod tests {
         let peer_a_config = TransportConfig {
             node_ip: "0.0.0.0".to_string(),
             listening_port: PORT_1,
-            legacy_flow_tag: FLOW_TAG_1,
+            legacy_flow_tag: FLOW_TAG,
             send_queue_size,
         };
 
@@ -702,7 +853,7 @@ mod tests {
         let peer_b_config = TransportConfig {
             node_ip: "0.0.0.0".to_string(),
             listening_port: PORT_2,
-            legacy_flow_tag: FLOW_TAG_2,
+            legacy_flow_tag: FLOW_TAG,
             send_queue_size,
         };
 
