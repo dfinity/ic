d2e4409a361195af0da13ab07c8435e1b01e5e7e
Maximillian Murphy <max@dfinity.org>
2022-08-31 12:33:52 +0000

    Save sns canister ids

--- rs/sns/cli/src/deploy.rs
+++ rs/sns/cli/src/deploy.rs
@@ -1,15 +1,29 @@
 //! Contains the logic for deploying SNS canisters
 
+use anyhow::anyhow;
+use candid::parser::value::IDLField;
 use candid::parser::value::IDLValue;
+use candid::types::internal::Label as IDLLabel;
 use candid::Decode;
 use candid::Encode;
+use candid::IDLArgs;
 use ic_base_types::{CanisterId, PrincipalId};
 use ic_nns_constants::ROOT_CANISTER_ID as NNS_ROOT_CANISTER_ID;
 use ic_nns_constants::SNS_WASM_CANISTER_ID;
 use ic_sns_init::pb::v1::SnsInitPayload;
 use ic_sns_init::{SnsCanisterIds, SnsCanisterInitPayloads};
 use ic_sns_wasm::pb::v1::DeployNewSnsRequest;
+use serde_json::Value as JsonValue;
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io::{BufWriter, Read, Seek, SeekFrom, Write};
+use std::path::Path;
 use std::str::FromStr;
+
+#[cfg(test)]
+use std::io::BufReader;
+#[cfg(test)]
+use tempfile::NamedTempFile;
 
 use crate::{call_dfx, get_identity, hex_encode_candid, DeployArgs};
 
@@ -139,7 +153,7 @@ impl SnsWasmSnsDeployer {
             .expect("Couldn't decode DeployNewSnsRequest")
         );
 
-        call_dfx(&[
+        let output = call_dfx(&[
             "canister",
             "--network",
             &self.args.network,
@@ -152,7 +166,183 @@ impl SnsWasmSnsDeployer {
             "deploy_new_sns",
             &request_idl,
         ]);
+        if !output.status.success() {
+            panic!("Failed to create SNS");
+        }
+        Self::save_canister_ids(&output.stdout, &self.args.network)
+            .expect("Failed to save to canister_ids.json");
     }
+
+    pub fn get_canisters_record(idl: &IDLArgs) -> anyhow::Result<&Vec<IDLField>> {
+        // TODO: Please god tell me there is a way of getting this by parsing directly to DeployNewSnsResponse.
+        // Variants tried:
+        // `let args: DeployNewSnsResponse = ...` <-- doesn't compile
+        // `let response: DeployNewSnsResponse = Decode!(buffer, DeployNewSnsResponse).expect("Could not parse DeployNewSnsResponse");` <-- panics - does it expect binary?
+
+        if let Some(IDLValue::Record(fields)) = &idl.args.get(0) {
+            let canisters: &IDLValue = fields
+                .iter()
+                .find(|field| {
+                    if let IDLLabel::Named(name) = &field.id {
+                        name == "canisters"
+                    } else {
+                        false
+                    }
+                })
+                .map(|field| &field.val)
+                .ok_or_else(|| anyhow!("Response does not provide canister IDs"))?;
+            let canisters: &IDLValue = if let IDLValue::Opt(canisters) = canisters {
+                canisters
+            } else {
+                return Err(anyhow!("Expected canisters to be wrapped in an opt."));
+            };
+            let canisters: &Vec<IDLField> = if let IDLValue::Record(canisters) = canisters {
+                canisters
+            } else {
+                return Err(anyhow!("Expected canisters to be a record"));
+            };
+            Ok(canisters)
+        } else {
+            Err(anyhow!("Expected response to be a record"))
+        }
+    }
+
+    pub fn canister_ids_as_json(idl: &[IDLField], network_name: &str) -> JsonValue {
+        fn canister_label_to_string(label: &IDLLabel) -> String {
+            if let IDLLabel::Named(name) = label {
+                format!("sns_{}", name)
+            } else {
+                panic!("The canister name is not a name; do you have the required did files installed?")
+            }
+        }
+        fn canister_principal_to_string(principal: &IDLValue) -> String {
+            let principal = if let IDLValue::Opt(principal) = principal {
+                principal
+            } else {
+                panic!(
+                    "Expected the IDL principal to be an opt but got: {:?}",
+                    principal
+                );
+            };
+            if let IDLValue::Principal(principal) = **principal {
+                principal.to_text()
+            } else {
+                panic!(
+                    "Expected the IDL principal to be a principal but got: {:?}",
+                    principal
+                );
+            }
+        }
+        let structure: HashMap<String, HashMap<String, String>> = idl
+            .iter()
+            .map(|IDLField { id, val }| {
+                (
+                    canister_label_to_string(id),
+                    HashMap::from([(network_name.to_string(), canister_principal_to_string(val))]),
+                )
+            })
+            .collect();
+        serde_json::to_value(structure).unwrap()
+    }
+
+    /// Merges the given JSON into a JSON file.
+    /// - If the file is missing or empty, the JSON is simply written to the file.
+    pub fn merge_into_json_file<P>(path: P, value: &JsonValue) -> anyhow::Result<()>
+    where
+        P: AsRef<Path>,
+    {
+        // Read the file, keeping the file pointer for the later write.
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        // Merge in the JSON value
+        // ... If there is existing JSON we need to hold it in this scope:
+        #[allow(unused_assignments)]
+        let mut modified_contents = JsonValue::Null;
+        // ... Get a pointer to the updated data:
+        let new_json = if contents.is_empty() {
+            value
+        } else {
+            modified_contents = serde_json::from_str(&contents)?;
+            json_patch::merge(&mut modified_contents, value);
+            &modified_contents
+        };
+        // Truncate the file and write the new contents.
+        file.set_len(0)?;
+        file.seek(SeekFrom::Start(0))?;
+
+        let mut writer = BufWriter::new(file);
+        writeln!(&mut writer, "{}", &serde_json::to_string_pretty(&new_json)?)?;
+        writer.flush()?;
+        Ok(())
+    }
+
+    /// Records the created canister IDs in dfx.JSON
+    pub fn save_canister_ids(buffer: &[u8], network: &str) -> anyhow::Result<()> {
+        let candid_str = std::str::from_utf8(buffer)?;
+        let args: IDLArgs = candid_str.parse()?;
+        let canisters_in_idl = Self::get_canisters_record(&args)?;
+        let new_canisters_json = Self::canister_ids_as_json(canisters_in_idl, network);
+        Self::merge_into_json_file("canister_ids.json", &new_canisters_json)
+    }
+}
+#[test]
+fn should_save_canister_ids() {
+    let sample_response = r#"
+				(
+					record {
+						subnet_id = opt principal "5y6g2-wypgr-jrg42-gwkse-r33lb-4hohc-okizp-sg7ng-kq4x7-vzbno-qqe";
+						error = null;
+						canisters = opt record {
+							root = opt principal "q3fc5-haaaa-aaaaa-aaahq-cai";
+							swap = opt principal "si2b5-pyaaa-aaaaa-aaaja-cai";
+							ledger = opt principal "sbzkb-zqaaa-aaaaa-aaaiq-cai";
+							governance = opt principal "sgymv-uiaaa-aaaaa-aaaia-cai";
+						};
+					},
+				)
+       "#;
+    let network_name = "foo";
+    let expected_json_str = r#"
+      { "sns_root": { "foo": "q3fc5-haaaa-aaaaa-aaahq-cai" }
+      , "sns_swap": { "foo": "si2b5-pyaaa-aaaaa-aaaja-cai" }
+      , "sns_ledger": { "foo": "sbzkb-zqaaa-aaaaa-aaaiq-cai" }
+      , "sns_governance": { "foo": "sgymv-uiaaa-aaaaa-aaaia-cai" }
+      }
+    "#;
+    let expected_json = serde_json::from_str(expected_json_str).unwrap();
+
+    fn assert_same_json(expected: &JsonValue, actual: &JsonValue, message: &str) {
+        let diff = json_patch::diff(expected, actual);
+        assert_eq!(json_patch::Patch(Vec::new()), diff, "{}", message);
+    }
+
+    // Test the individual steps:
+    // .. First parse the response and prepare the JSON representation:
+    let candid_str = sample_response.to_string();
+    let args: IDLArgs = candid_str.parse().expect("Malformed input");
+    let canisters_in_idl = SnsWasmSnsDeployer::get_canisters_record(&args).unwrap();
+    let new_canisters_json =
+        SnsWasmSnsDeployer::canister_ids_as_json(canisters_in_idl, network_name);
+    // ... verify that the representation has no semantic differences
+    assert_same_json(
+        &expected_json,
+        &new_canisters_json,
+        "The jsonification is wrong",
+    );
+    // ... verify that writing to an empty file dumps the data without modification
+    let file = NamedTempFile::new().unwrap();
+    SnsWasmSnsDeployer::merge_into_json_file(&file.path(), &new_canisters_json)
+        .expect("Failed to save changes to file");
+    let file_content =
+        serde_json::from_reader(&mut BufReader::new(file.reopen().unwrap())).unwrap();
+    assert_same_json(&expected_json, &file_content, "Save to file doesn't work");
+
+    //SnsWasmSnsDeployer::save_canister_ids(sample_response.as_bytes(), "foo");
 }
 
 /// Responsible for deploying SNS canisters
