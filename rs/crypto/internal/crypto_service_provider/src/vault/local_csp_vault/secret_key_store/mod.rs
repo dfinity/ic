8baa578ec06ec71446c1051ed50850d82145aa1b
Grégory Demay <gregory.demay@dfinity.org>
2022-09-20 07:56:57 +0000

    CRP-1326 Remove unused methods in SecretKeyStoreCspVault

--- rs/crypto/internal/crypto_service_provider/src/vault/local_csp_vault/secret_key_store/mod.rs
+++ rs/crypto/internal/crypto_service_provider/src/vault/local_csp_vault/secret_key_store/mod.rs
@@ -4,8 +4,6 @@ use crate::vault::local_csp_vault::LocalCspVault;
 use crate::SecretKeyStore;
 use ic_types::crypto::KeyId;
 
-use crate::secret_key_store::{Scope, SecretKeyStoreError};
-use crate::types::CspSecretKey;
 use rand::{CryptoRng, Rng};
 
 #[cfg(test)]
@@ -16,18 +14,5 @@ impl<R: Rng + CryptoRng + Send + Sync, S: SecretKeyStore, C: SecretKeyStore> Sec
 {
     fn sks_contains(&self, id: &KeyId) -> Result<bool, CspSecretKeyStoreContainsError> {
         Ok(self.sks_read_lock().contains(id))
-    }
-
-    fn insert_secret_key(
-        &self,
-        id: KeyId,
-        key: CspSecretKey,
-        scope: Option<Scope>,
-    ) -> Result<(), SecretKeyStoreError> {
-        self.sks_write_lock().insert(id, key, scope)
-    }
-
-    fn get_secret_key(&self, id: &KeyId) -> Option<CspSecretKey> {
-        self.sks_read_lock().get(id)
     }
 }
