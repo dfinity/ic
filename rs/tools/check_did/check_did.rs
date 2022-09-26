d7cbc2a2abce6f1dbab31003ec8a78e888960b37
Roman Kashitsyn <roman.kashitsyn@dfinity.org>
2022-09-26 22:06:49 +0000

    FI-180: add a bazel test to check did compatiblity

---
+++ rs/tools/check_did/check_did.rs
@@ -0,0 +1,26 @@
+use candid::utils::{service_compatible, CandidSource};
+use std::path::PathBuf;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} new.did old.did", args[0]);
+        std::process::exit(1);
+    }
+
+    let new_did_file = PathBuf::from(&args[1]);
+    let old_did_file = PathBuf::from(&args[2]);
+
+    if let Err(e) = service_compatible(
+        CandidSource::File(&new_did_file),
+        CandidSource::File(&old_did_file),
+    ) {
+        panic!(
+            "The new interface at {} is not compatible with the old interface at {}:\n{}",
+            new_did_file.display(),
+            old_did_file.display(),
+            e,
+        );
+    }
+}
