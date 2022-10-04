ec2dd111ff859e7e589f3deac32a1613632cae67
Daniel Bloom <daniel.bloom@dfinity.org>
2022-10-04 20:31:33 +0000

    Remove all nginx `mount --bind`s

--- ic-os/boundary-guestos/rootfs/etc/nginx/ic/ic_router_control_plane_watcher.sh
+++ ic-os/boundary-guestos/rootfs/etc/nginx/ic/ic_router_control_plane_watcher.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-ROUTES_DIR=/var/cache/ic_routes
-mkdir -p "$ROUTES_DIR"
-python3 ic_router_control_plane.py "$ROUTES_DIR" nginx_table.conf ic_router_table.js trusted_certs.pem --generate_upstream_declarations=True --deny_node_socket_addrs=
-inotifywait -q -m -e modify -e create -e close_write --format "%w%f" "$ROUTES_DIR" \
+readonly ROUTES_DIR='/var/cache/ic_routes'
+readonly IC_ROUTING='/var/opt/nginx/ic'
+readonly TRUSTED_CERTS="${IC_ROUTING}/trusted_certs.pem"
+readonly NGINX_TABLE="${IC_ROUTING}/nginx_table.conf"
+readonly IC_ROUTER_TABLE="${IC_ROUTING}/ic_router_table.js"
+
+function rewrite() {
+    python3 ic_router_control_plane.py "${ROUTES_DIR}" "${NGINX_TABLE}" "${IC_ROUTER_TABLE}" "${TRUSTED_CERTS}" --generate_upstream_declarations=True --deny_node_socket_addrs=
+    service nginx reload
+}
+
+mkdir -p "${ROUTES_DIR}"
+rewrite
+inotifywait -q -m -e modify -e create -e close_write --format "%w%f" "${ROUTES_DIR}" \
     | while read -r path; do
-        echo $path changed
+        echo "${path} changed"
         sleep 1
-        python3 ic_router_control_plane.py "$ROUTES_DIR" nginx_table.conf ic_router_table.js trusted_certs.pem --generate_upstream_declarations=True --deny_node_socket_addrs=
-        service nginx reload
+        rewrite
     done
