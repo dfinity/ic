dae94a107e21d799f60097a9ca66143b4b3ca041
Daniel Bloom <daniel.bloom@dfinity.org>
2022-10-06 16:53:23 +0000

    Allow `Accept-Ranges`

--- rs/tests/src/boundary_nodes_integration/boundary_nodes.rs
+++ rs/tests/src/boundary_nodes_integration/boundary_nodes.rs
@@ -1606,7 +1606,7 @@ pub fn direct_to_replica_options_test(env: TestEnv) {
                 ("Access-Control-Allow-Methods", &allowed_methods),
                 ("Access-Control-Allow-Credentials", "true"),
                 ("Access-Control-Allow-Headers", "DNT,User-Agent,X-Requested-With,If-None-Match,If-Modified-Since,Cache-Control,Content-Type,Range,Cookie"),
-                ("Access-Control-Expose-Headers", "Content-Length,Content-Range"),
+                ("Access-Control-Expose-Headers", "Accept-Ranges,Content-Length,Content-Range"),
                 ("Access-Control-Max-Age", "600"),
             ] {
                 let hdr = res
