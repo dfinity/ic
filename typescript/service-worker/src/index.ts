71da558c7e4522b59795780fe44ab05d3882d50c
Nathan Mc Grath <nathan.mcgrath@dfinity.org>
2022-10-10 17:50:14 +0200

    fix sw reloading issues

--- typescript/service-worker/src/index.ts
+++ typescript/service-worker/src/index.ts
@@ -20,27 +20,23 @@ window.addEventListener('load', async () => {
       `This web browser cannot interact with the Internet Computer securely.  (No: ${unsupported})
        Please try new web browser software.`
     );
-  } else {
-    console.log(
-      `Installing a service worker ${process.env.VERSION} to proxy and validate content...`
-    );
-    // Ok, let's install the service worker...
-    // note: if the service worker was already installed, when the browser requested <domain>/, it would have
-    // proxied the response from <domain>/<canister-id>/, so this bootstrap file would have never been
-    // retrieved from the boundary nodes
-    try {
-      await navigator.serviceWorker.register('sw.js');
-    } catch (e) {
-      await navigator.serviceWorker.register('/sw.js');
-    }
 
-    // delays code execution until serviceworker is ready
-    await navigator.serviceWorker.ready;
-    const registration = await navigator.serviceWorker.getRegistration();
-    if (registration.active && !navigator.serviceWorker.controller) {
-      // There's an active SW, but no controller for this tab. The service worker events are also _not_ fired.
-      // This happens after a hard refresh --> Perform a soft reload to load everything from the SW.
-      window.location.reload();
-    }
+    return;
   }
+
+  console.log(
+    `Installing a service worker ${process.env.VERSION} to proxy and validate content...`
+  );
+
+  // Ok, let's install the service worker...
+  // note: if the service worker was already installed, when the browser requested <domain>/, it would have
+  // proxied the response from <domain>/<canister-id>/, so this bootstrap file would have never been
+  // retrieved from the boundary nodes
+  await navigator.serviceWorker.register('/sw.js');
+
+  // delays code execution until serviceworker is ready
+  await navigator.serviceWorker.ready;
+
+  // // reload the page so the service worker can intercept the requests
+  window.location.reload();
 });
