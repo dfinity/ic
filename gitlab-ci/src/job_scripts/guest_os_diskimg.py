e89cd15e52eac773e9701f513629ce421f8aaf10
Jude Taylor <jude.taylor@dfinity.org>
2022-10-12 18:17:53 +0000

    Build guestos in pipeline using bazel (re-merge)

--- gitlab-ci/src/job_scripts/guest_os_diskimg.py
+++ gitlab-ci/src/job_scripts/guest_os_diskimg.py
@@ -1,6 +1,6 @@
 import logging
-from os import environ
 from os import getenv
+from pathlib import Path
 
 from ci import buildevent
 from ci import cwd
@@ -11,26 +11,20 @@ from ci import sh
 def run():
     build_extra_suffix = getenv("BUILD_EXTRA_SUFFIX", "")
     build_out = f"build-out/disk-img{build_extra_suffix}"
-    build_tmp = f"build-tmp{build_extra_suffix}"
     upload_target = f"guest-os/disk-img{build_extra_suffix}"
     version = ENV.build_id
     logging.info(f"Build ID: {version}")
 
-    with cwd("ic-os/guestos"):
-        # lib/guest-os-diskimg.sh fails if these are not set, which they aren't when running locally
-        offline_defaults = {"BUILD_EXTRA_ARGS": "", "BUILD_EXTRA_SUFFIX": "", "CI_JOB_ID": ""}
-        # override those variables with the already-set ones if there are any
-        script_env = {**offline_defaults, **environ.copy()}
+    build_mode = "dev" if "-dev" in build_extra_suffix else "prod"
+    malicious_mode = "-malicious" if "-malicious" in build_extra_suffix else ""
 
-        sh(
-            f"{ENV.top}/gitlab-ci/src/job_scripts/lib/guest-os-diskimg.sh",
-            build_out,
-            build_tmp,
-            upload_target,
-            version,
-            environ.get("CDPRNET", ""),
-            env=script_env,
-        )
+    with cwd("ic-os/guestos"):
+        sh(f"{ENV.top}/gitlab-ci/src/job_scripts/lib/guest-os-diskimg.sh", build_out, build_mode, malicious_mode)
+
+        version_txt = Path(f"{ENV.top}/version.txt")
+        if version_txt.is_file():
+            legacy_version = version
+            version = version_txt.read_text().strip()
 
         if ENV.is_gitlab:
             with buildevent("rclone"):
@@ -41,5 +35,14 @@ def run():
                     build_out,
                     upload_target,
                 )
+            if legacy_version:
+                with buildevent("rclone"):
+                    sh(
+                        f"{ENV.top}/gitlab-ci/src/artifacts/rclone_upload.py",
+                        f"--version={legacy_version}",
+                        "--verbose",
+                        build_out,
+                        upload_target,
+                    )
 
     logging.info(f"Build ID (real or fake git revision): {version}")
