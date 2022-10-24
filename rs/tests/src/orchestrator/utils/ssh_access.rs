0881f121e09f631e2fceb30bfe3d3038e69eb272
Leo Eichhorn <leo.eichhorn@dfinity.org>
2022-10-24 09:20:03 +0000

    Remove unwraps in ssh access

--- rs/tests/src/orchestrator/utils/ssh_access.rs
+++ rs/tests/src/orchestrator/utils/ssh_access.rs
@@ -246,19 +246,26 @@ pub(crate) async fn fail_updating_ssh_keys_for_all_unassigned_nodes(
 }
 
 pub(crate) fn execute_bash_command(sess: &Session, command: String) -> Result<String, String> {
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("bash").unwrap();
-    channel.write_all(command.as_bytes()).unwrap();
-    channel.flush().unwrap();
-    channel.send_eof().unwrap();
+    let mut channel = sess.channel_session().map_err(|e| e.to_string())?;
+    channel.exec("bash").map_err(|e| e.to_string())?;
+    channel
+        .write_all(command.as_bytes())
+        .map_err(|e| e.to_string())?;
+    channel.flush().map_err(|e| e.to_string())?;
+    channel.send_eof().map_err(|e| e.to_string())?;
     let mut out = String::new();
-    channel.read_to_string(&mut out).unwrap();
+    channel
+        .read_to_string(&mut out)
+        .map_err(|e| e.to_string())?;
     let mut err_str = String::new();
     match channel.exit_status() {
         Ok(status) => match status {
             0 => Ok(out),
             _ => {
-                channel.stderr().read_to_string(&mut err_str).unwrap();
+                channel
+                    .stderr()
+                    .read_to_string(&mut err_str)
+                    .map_err(|e| e.to_string())?;
                 Err(format!(
                     "Error in: {}\nErr code: {}\nstdout: \n{}\nstderr: \n{}",
                     command, status, out, err_str
@@ -266,7 +273,10 @@ pub(crate) fn execute_bash_command(sess: &Session, command: String) -> Result<St
             }
         },
         Err(e) => {
-            channel.stderr().read_to_string(&mut err_str).unwrap();
+            channel
+                .stderr()
+                .read_to_string(&mut err_str)
+                .map_err(|e| e.to_string())?;
             Err(format!(
                 "Error in: {}\nError: {}\nstdout: \n{}\nstderr: \n{}",
                 command, e, out, err_str
