c84444a8dc3394f73395017f8be741b597866636
Mykola Dzham <mykola.dzham@dfinity.org>
2022-11-03 16:19:47 +0000

    Use gzip_compress() instead of calling gzip from genrule.

--- gitlab-ci/src/job_scripts/lib/guest-os-diskimg.sh
+++ gitlab-ci/src/job_scripts/lib/guest-os-diskimg.sh
@@ -24,9 +24,9 @@ fi
 BUILD_MODE="${BUILD_MODE}${MALICIOUS_MODE}"
 
 # shellcheck disable=SC2086
-bazel ${BAZEL_STARTUP_ARGS:-} build ${BAZEL_CI_CONFIG:-} ${BAZEL_EXTRA_ARGS:-} "//ic-os/guestos/${BUILD_MODE}:disk-img.tar_gz" "//ic-os/guestos/${BUILD_MODE}:disk-img.tar_zst"
+bazel ${BAZEL_STARTUP_ARGS:-} build ${BAZEL_CI_CONFIG:-} ${BAZEL_EXTRA_ARGS:-} "//ic-os/guestos/${BUILD_MODE}:disk-img.tar.gz" "//ic-os/guestos/${BUILD_MODE}:disk-img.tar_zst"
 
-cp -fv "$ROOT_DIR"/"$(bazel ${BAZEL_STARTUP_ARGS:-} cquery ${BAZEL_CI_CONFIG:-} ${BAZEL_EXTRA_ARGS:-} --output=files "//ic-os/guestos/${BUILD_MODE}:disk-img.tar_gz")" "$BUILD_OUT"/disk-img.tar.gz
+cp -fv "$ROOT_DIR"/"$(bazel ${BAZEL_STARTUP_ARGS:-} cquery ${BAZEL_CI_CONFIG:-} ${BAZEL_EXTRA_ARGS:-} --output=files "//ic-os/guestos/${BUILD_MODE}:disk-img.tar.gz")" "$BUILD_OUT"/disk-img.tar.gz
 cp -fv "$ROOT_DIR"/"$(bazel ${BAZEL_STARTUP_ARGS:-} cquery ${BAZEL_CI_CONFIG:-} ${BAZEL_EXTRA_ARGS:-} --output=files "//ic-os/guestos/${BUILD_MODE}:disk-img.tar_zst")" "$BUILD_OUT"/disk-img.tar.zst
 
 if [ -n "${CI_JOB_ID:-}" ]; then
