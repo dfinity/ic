a3ca5b8718a7b142678e0646a9d36de9d4e08d2e
Bas van Dijk <bas@dfinity.org>
2022-11-22 10:29:48 +0000

    rs/tests: bazelify create_subnet_pre_master

---
+++ rs/tests/bin/create_subnet_pre_master_test.rs
@@ -0,0 +1,15 @@
+#[rustfmt::skip]
+
+use anyhow::Result;
+
+use ic_tests::driver::new::group::SystemTestGroup;
+use ic_tests::nns_tests::create_subnet;
+use ic_tests::systest;
+
+fn main() -> Result<()> {
+    SystemTestGroup::new()
+        .with_setup(create_subnet::pre_master_config)
+        .add_test(systest!(create_subnet::test))
+        .execute_from_args()?;
+    Ok(())
+}
