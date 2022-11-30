697e315fce974eab8209900c1a69e0e93eb7ed51
Grégory Demay <gregory.demay@dfinity.org>
2022-11-30 16:18:01 +0000

    fix(crypto) CRP-1806 add scope to existing MEGaPrivateKey in secret key store

--- rs/crypto/internal/crypto_service_provider/src/secret_key_store/proto_store.rs
+++ rs/crypto/internal/crypto_service_provider/src/secret_key_store/proto_store.rs
@@ -1,5 +1,6 @@
 //! Filesystem-backed secret key store
 #![allow(clippy::unwrap_used)]
+use crate::canister_threshold::IDKG_MEGA_SCOPE;
 use crate::key_id::KeyId;
 use crate::secret_key_store::{
     Scope, SecretKeyStore, SecretKeyStoreError, SecretKeyStorePersistenceError,
@@ -86,17 +87,30 @@ impl ProtoSecretKeyStore {
     // merging.
     fn migrate_to_current_version(sks_proto: pb::SecretKeyStore) -> SecretKeys {
         match sks_proto.version {
-            CURRENT_SKS_VERSION => ProtoSecretKeyStore::sks_proto_to_secret_keys(&sks_proto),
+            3 => ProtoSecretKeyStore::sks_proto_to_secret_keys(&sks_proto),
+            CURRENT_SKS_VERSION => {
+                let mut secret_keys = SecretKeys::new();
+                for (key_id_hex, sk_proto) in sks_proto.key_id_to_secret_key_v1.iter() {
+                    let key_id = key_id_from_hex(key_id_hex);
+                    let csp_key = Self::parse_csp_secret_key(&sk_proto.csp_secret_key, &key_id);
+                    let scope = match &csp_key {
+                        CspSecretKey::MEGaEncryptionK256(_) => {
+                            let _ensure_scope_is_valid = Self::parse_scope(&sk_proto.scope);
+                            Some(IDKG_MEGA_SCOPE)
+                        }
+                        _ => Self::parse_scope(&sk_proto.scope),
+                    };
+                    secret_keys.insert(key_id, (csp_key, scope));
+                }
+                secret_keys
+            }
             0 => {
                 let mut secret_keys = SecretKeys::new();
                 for (key_id_string, key_bytes) in sks_proto.key_id_to_csp_secret_key.iter() {
                     let key_id = KeyId::try_from(key_id_string.as_str()).unwrap_or_else(|err| {
                         panic!("Failed to create KeyId: {}", err);
                     });
-                    let mut csp_key: CspSecretKey = serde_cbor::from_slice(key_bytes)
-                        .unwrap_or_else(|e| {
-                            panic!("Error deserializing key with ID {}: {}", key_id, e)
-                        });
+                    let mut csp_key = Self::parse_csp_secret_key(key_bytes, &key_id);
                     if let CspSecretKey::FsEncryption(CspFsEncryptionKeySet::Groth20_Bls12_381(
                         key_set,
                     )) = &csp_key
@@ -115,16 +129,11 @@ impl ProtoSecretKeyStore {
                 }
                 secret_keys
             }
-
             1 => {
                 let mut secret_keys = SecretKeys::new();
                 for (key_id_hex, sk_proto) in sks_proto.key_id_to_secret_key_v1.iter() {
                     let key_id = key_id_from_hex(key_id_hex);
-                    let mut csp_key = serde_cbor::from_slice(&sk_proto.csp_secret_key)
-                        .unwrap_or_else(|e| {
-                            panic!("Error deserializing key with ID {}: {}", key_id, e)
-                        });
-
+                    let mut csp_key = Self::parse_csp_secret_key(&sk_proto.csp_secret_key, &key_id);
                     if let CspSecretKey::FsEncryption(CspFsEncryptionKeySet::Groth20_Bls12_381(
                         key_set,
                     )) = &csp_key
@@ -134,14 +143,7 @@ impl ProtoSecretKeyStore {
                             CspFsEncryptionKeySet::Groth20WithPop_Bls12_381(key_set_with_pop),
                         );
                     }
-                    let maybe_scope = if sk_proto.scope.is_empty() {
-                        None
-                    } else {
-                        Some(
-                            Scope::from_str(&sk_proto.scope)
-                                .unwrap_or_else(|_| panic!("Unknown scope: {}", sk_proto.scope)),
-                        )
-                    };
+                    let maybe_scope = Self::parse_scope(&sk_proto.scope);
                     secret_keys.insert(key_id, (csp_key, maybe_scope));
                 }
                 secret_keys
@@ -153,29 +155,39 @@ impl ProtoSecretKeyStore {
         }
     }
 
-    fn sks_proto_to_secret_keys(sks_proto: &pb::SecretKeyStore) -> SecretKeys {
-        if sks_proto.version != CURRENT_SKS_VERSION {
-            panic!(
-                "Unexpected SecretKeyStore-proto version: {}",
-                sks_proto.version
+    fn parse_csp_secret_key(key_bytes: &[u8], key_id: &KeyId) -> CspSecretKey {
+        serde_cbor::from_slice(key_bytes)
+            .unwrap_or_else(|e| panic!("Error deserializing key with ID {}: {}", key_id, e))
+    }
+
+    fn parse_scope(scope_proto: &str) -> Option<Scope> {
+        if scope_proto.is_empty() {
+            None
+        } else {
+            Some(
+                Scope::from_str(scope_proto)
+                    .unwrap_or_else(|_| panic!("Unknown scope: {}", scope_proto)),
             )
         }
+    }
+
+    fn sks_proto_to_secret_keys(sks_proto: &pb::SecretKeyStore) -> SecretKeys {
+        Self::ensure_version_is_supported(sks_proto.version);
         let mut secret_keys = SecretKeys::new();
         for (key_id_hex, sk_proto) in sks_proto.key_id_to_secret_key_v1.iter() {
             let key_id = key_id_from_hex(key_id_hex);
-            let csp_key = serde_cbor::from_slice(&sk_proto.csp_secret_key)
-                .unwrap_or_else(|e| panic!("Error deserializing key with ID {}: {}", key_id, e));
-            let maybe_scope = if sk_proto.scope.is_empty() {
-                None
-            } else {
-                Some(
-                    Scope::from_str(&sk_proto.scope)
-                        .unwrap_or_else(|_| panic!("Unknown scope: {}", sk_proto.scope)),
-                )
-            };
+            let csp_key = Self::parse_csp_secret_key(&sk_proto.csp_secret_key, &key_id);
+            let maybe_scope = Self::parse_scope(&sk_proto.scope);
             secret_keys.insert(key_id, (csp_key, maybe_scope));
         }
         secret_keys
+    }
+
+    fn ensure_version_is_supported(version: u32) {
+        let supported_versions = vec![CURRENT_SKS_VERSION, 3];
+        if !supported_versions.contains(&version) {
+            panic!("Unexpected SecretKeyStore-proto version: {}", version)
+        }
     }
 
     fn secret_keys_to_sks_proto(
@@ -314,7 +326,12 @@ where
 pub mod tests {
     use super::super::test_utils;
     use super::*;
-    use crate::secret_key_store::test_utils::TempSecretKeyStore;
+    use crate::canister_threshold::IDKG_MEGA_SCOPE;
+    use crate::secret_key_store::test_utils::{make_key_id, make_secret_key, TempSecretKeyStore};
+    use crate::secret_key_store::{
+        scope::ConstScope, Scope, SecretKeyStore, SecretKeyStoreError,
+        SecretKeyStorePersistenceError,
+    };
     use crate::types::CspSecretKey;
     use ic_crypto_internal_basic_sig_ed25519::types as ed25519_types;
     use ic_crypto_internal_csp_test_utils::files::mk_temp_dir_with_permissions;
@@ -397,53 +414,85 @@ pub mod tests {
     }
 
     #[test]
-    fn should_deserialize_existing_secret_key_store() {
-        let temp_dir: TempDir = mk_temp_dir_with_permissions(0o700);
-        copy_file_to_dir(
-            path_to_existing_secret_key_store().as_path(),
-            temp_dir.path(),
-        );
+    fn should_deserialize_all_existing_secret_key_stores() {
+        for version in SecretKeyStoreVersion::all_versions() {
+            let (_temp_dir, secret_key_store) = existing_secret_key_store_in_temp_dir(&version);
+            let guard = secret_key_store.keys.read();
+            assert_eq!(guard.keys().len(), 5);
 
-        let secret_key_store = ProtoSecretKeyStore::open(temp_dir.path(), "sks_data.pb", None);
-        let guard = secret_key_store.keys.read();
-        assert_eq!(guard.keys().len(), 5);
-
-        let test_vecs = vec![
-            TestVector::mega_encryption(),
-            TestVector::tls(),
-            TestVector::ed25519(),
-            TestVector::multi_bls(),
-        ];
-        for test_vec in test_vecs {
-            let secret_key = secret_key_store
-                .get(&test_vec.key_id)
-                .expect("secret key not found");
-            assert_eq!(secret_key, test_vec.secret_key);
+            let test_vecs = vec![
+                TestVector::mega_encryption(),
+                TestVector::tls(),
+                TestVector::ed25519(),
+                TestVector::multi_bls(),
+            ];
+            for test_vec in test_vecs {
+                let secret_key = secret_key_store
+                    .get(&test_vec.key_id)
+                    .expect("secret key not found");
+                assert_eq!(secret_key, test_vec.secret_key);
+            }
+            // We test for FSEncryption separately because the structure of the private key is way more complex
+            // and so recreating it from scratch from some byte arrays would be very time-consuming
+            // (e.g., d_t contains 32 96-byte arrays and d_h contains 256 96-byte arrays)
+            let fs_key_id =
+                KeyId::from_hex("bdf42c6970fdeb0dc16c8175430b8f8428a2a1cd387da5ca805eaeb461c2518b")
+                    .expect("invalid key id");
+            let fs_key = secret_key_store
+                .get(&fs_key_id)
+                .expect("Secret key for FS encryption not found");
+            assert!(matches!(
+                fs_key,
+                CspSecretKey::FsEncryption(CspFsEncryptionKeySet::Groth20WithPop_Bls12_381(..))
+            ));
         }
-        // We test for FSEncryption separately because the structure of the private key is way more complex
-        // and so recreating it from scratch from some byte arrays would be very time-consuming
-        // (e.g., d_t contains 32 96-byte arrays and d_h contains 256 96-byte arrays)
-        let fs_key_id =
-            KeyId::from_hex("bdf42c6970fdeb0dc16c8175430b8f8428a2a1cd387da5ca805eaeb461c2518b")
-                .expect("invalid key id");
-        let fs_key = secret_key_store
-            .get(&fs_key_id)
-            .expect("Secret key for FS encryption not found");
-        assert!(matches!(
-            fs_key,
-            CspSecretKey::FsEncryption(CspFsEncryptionKeySet::Groth20WithPop_Bls12_381(..))
-        ));
+    }
+
+    #[test]
+    fn should_downgrade_secret_key_store_to_current_sks_version() {
+        //TODO CRP-1806: remove this test when version bumped to 3
+        //TODO CRP-1806: change `supported_versions` to only contain CURRENT_SKS_VERSION
+        assert_eq!(CURRENT_SKS_VERSION, 2);
+        let (_temp_dir, mut secret_key_store) =
+            existing_secret_key_store_in_temp_dir(&SecretKeyStoreVersion::V3);
+
+        let seed = 42;
+        let (key_id, secret_key) = (make_key_id(seed), make_secret_key(seed));
+        secret_key_store
+            .insert(
+                key_id,
+                secret_key.clone(),
+                Some(Scope::Const(ConstScope::Test0)),
+            )
+            .expect("could not write generated key");
+
+        assert_eq!(
+            secret_key_store.get(&key_id).expect("could not read SKS"),
+            secret_key
+        );
+        assert_secret_key_store_proto_has_correct_version(
+            secret_key_store.proto_file_path(),
+            &SecretKeyStoreVersion::V2,
+        );
+    }
+
+    #[test]
+    fn should_have_scope_for_mega_private() {
+        let (_temp_dir, secret_key_store) =
+            existing_secret_key_store_in_temp_dir(&SecretKeyStoreVersion::V2);
+        let (_csp_key, scope) = with_read_lock(&secret_key_store.keys, |keys| {
+            keys.get(&TestVector::mega_encryption().key_id)
+                .map(|(csp_key, scope)| (csp_key.to_owned(), scope.to_owned()))
+        })
+        .expect("missing MEGa private key");
+
+        assert_eq!(scope, Some(IDKG_MEGA_SCOPE));
     }
 
     #[test]
     fn should_fail_to_write_to_read_only_secret_key_store_directory() {
-        let temp_dir: TempDir = mk_temp_dir_with_permissions(0o700);
-        copy_file_to_dir(
-            path_to_existing_secret_key_store().as_path(),
-            temp_dir.path(),
-        );
-
-        let mut secret_key_store = ProtoSecretKeyStore::open(temp_dir.path(), "sks_data.pb", None);
+        let (temp_dir, mut secret_key_store) =
+            existing_secret_key_store_in_temp_dir(&SecretKeyStoreVersion::V2);
         let mut seed = ChaCha20Rng::seed_from_u64(42);
         let key_id = KeyId::from(seed.gen::<[u8; 32]>());
         let key = CspSecretKey::Ed25519(ed25519_types::SecretKeyBytes(
@@ -463,7 +512,7 @@ pub mod tests {
             if msg.to_lowercase().contains("secret key store internal error writing protobuf using tmp file: permission denied")
         ));
 
-        fs::set_permissions(temp_dir.path(), fs::Permissions::from_mode(0o700)).expect(
+        fs::set_permissions(temp_dir.path(), Permissions::from_mode(0o700)).expect(
             "failed to change permissions of temp_dir so that writing is possible \
                 again, so that the directory can automatically be cleaned up",
         );
@@ -474,19 +523,25 @@ pub mod tests {
     fn should_fail_to_read_from_secret_key_store_with_no_read_permissions() {
         let temp_dir: TempDir = mk_temp_dir_with_permissions(0o700);
         copy_file_to_dir(
-            path_to_existing_secret_key_store().as_path(),
+            path_to_existing_secret_key_store(&SecretKeyStoreVersion::V2).as_path(),
             temp_dir.path(),
         );
 
         // remove read permissions from the secret key store file, causing the subsequent call to
         // open the key store to panic (since it tries to read the key store)
         fs::set_permissions(
-            temp_dir.path().join("sks_data.pb"),
+            temp_dir.path().join(existing_secret_key_store_file_name(
+                &SecretKeyStoreVersion::V2,
+            )),
             Permissions::from_mode(0o000),
         )
         .expect("Could not set the permissions of the secret key store file.");
 
-        let _secret_key_store = ProtoSecretKeyStore::open(temp_dir.path(), "sks_data.pb", None);
+        let _secret_key_store = ProtoSecretKeyStore::open(
+            temp_dir.path(),
+            &existing_secret_key_store_file_name(&SecretKeyStoreVersion::V2),
+            None,
+        );
     }
 
     fn copy_file_to_dir(source_file: &Path, target_dir: &Path) {
@@ -503,10 +558,62 @@ pub mod tests {
     // (See for example the test `should_collect_correctly_key_count_metrics_for_all_keys`).
     // A break point was set before the crypto component went out-of-scoped to be able to copy the
     // secret key store file somewhere else.
-    fn path_to_existing_secret_key_store() -> PathBuf {
+    fn path_to_existing_secret_key_store(version: &SecretKeyStoreVersion) -> PathBuf {
         let mut path = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-        path.push("test_resources/sks_data.pb");
+        path.push(format!(
+            "test_resources/{}",
+            existing_secret_key_store_file_name(version)
+        ));
         path
+    }
+
+    fn existing_secret_key_store_file_name(version: &SecretKeyStoreVersion) -> String {
+        let version_suffix = match version {
+            SecretKeyStoreVersion::V2 => "v2",
+            SecretKeyStoreVersion::V3 => "v3",
+        };
+        format!("sks_data_{}.pb", version_suffix)
+    }
+
+    fn existing_secret_key_store_in_temp_dir(
+        version: &SecretKeyStoreVersion,
+    ) -> (TempDir, ProtoSecretKeyStore) {
+        let temp_dir: TempDir = mk_temp_dir_with_permissions(0o700);
+        let sks_path = path_to_existing_secret_key_store(version);
+        assert_secret_key_store_proto_has_correct_version(sks_path.as_path(), version);
+        copy_file_to_dir(sks_path.as_path(), temp_dir.path());
+
+        let secret_key_store = ProtoSecretKeyStore::open(
+            temp_dir.path(),
+            &existing_secret_key_store_file_name(version),
+            None,
+        );
+        (temp_dir, secret_key_store)
+    }
+
+    enum SecretKeyStoreVersion {
+        V2,
+        V3,
+    }
+
+    impl SecretKeyStoreVersion {
+        fn all_versions() -> Vec<SecretKeyStoreVersion> {
+            vec![SecretKeyStoreVersion::V2, SecretKeyStoreVersion::V3]
+        }
+    }
+
+    fn assert_secret_key_store_proto_has_correct_version(
+        sks_data_file: &Path,
+        version: &SecretKeyStoreVersion,
+    ) {
+        let data = fs::read(sks_data_file).expect("error reading SKS");
+        let sks_proto = pb::SecretKeyStore::decode(&*data).expect("error parsing SKS data");
+        let actual_version = sks_proto.version;
+        let expected_version = match version {
+            SecretKeyStoreVersion::V2 => 2,
+            SecretKeyStoreVersion::V3 => 3,
+        };
+        assert_eq!(actual_version, expected_version);
     }
 
     struct TestVector {
