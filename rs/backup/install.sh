75b9a598ed4467aeb5f490c1d967d8c030757ef3
Zoran Cvetkov <zoran.cvetkov@dfinity.org>
2022-11-30 13:32:35 +0000

    fix(OR-136): Backup tool in Rust - fixes and improvements

--- rs/backup/install.sh
+++ rs/backup/install.sh
@@ -8,37 +8,44 @@
 # required binaries on the backup pod: bash, curl, gunzip
 #
 
-BACKUP_EXE="ic-backup"
+TMP_DIR=$(mktemp -d)
+BACKUP_EXE_NAME="ic-backup"
+BACKUP_EXE="${TMP_DIR}/${BACKUP_EXE_NAME}"
 BACKUP_EXE_GZ="${BACKUP_EXE}.gz"
-CONFIG_FILE="config.json5"
-SERVICE_CONFIG_FILE="ic-backup.service"
+CONFIG_FILE_NAME="config.json5"
+CONFIG_FILE="${TMP_DIR}/${CONFIG_FILE_NAME}"
+SERVICE_CONFIG_FILE="${TMP_DIR}/ic-backup.service"
 NNS_URL="https://ic0.app"
-INSTALL_DIR="/usr/local/bin"
-PUBLIC_KEY_FILE="ic_public_key.pem"
+PUBLIC_KEY_NAME="ic_public_key.pem"
+PUBLIC_KEY_FILE="${TMP_DIR}/${PUBLIC_KEY_NAME}"
 NODES_SYNCING=5
-SYNCING_PERIOD=3600 # 1 hour
-REPLAY_PERIOD=28800 # 8 hours
+SYNCING_PERIOD=1800 # 1/2 hour
+REPLAY_PERIOD=14400 # 4 hours
 BACKUP_INSTANCE=$(hostname -a)
 
 DEFAULT_BUILD_ID="7657b1f1762ddfc39d5ac63e00c7c54ecf2cf132"
-echo "Please enter the BUILD_ID of the proper ic-backup version:"
+echo "Enter the BUILD_ID of the proper ic-backup version:"
 echo "(default: ${DEFAULT_BUILD_ID}):"
 read BUILD_ID
 if [ -z "${BUILD_ID// /}" ]; then
     BUILD_ID=${DEFAULT_BUILD_ID}
 fi
 
-DEFAULT_USER_ID="backup"
-echo "Please enter backup process USER_ID"
+DEFAULT_USER_ID="op"
+echo "Enter the local USER_ID that will run the backup:"
 echo "(default: ${DEFAULT_USER_ID}):"
 read USER_ID
 if [ -z "${USER_ID// /}" ]; then
     USER_ID=${DEFAULT_USER_ID}
 fi
 
-BACKUP_HOME=$(grep ${USER_ID} /etc/passwd | cut -d ":" -f6)
+BACKUP_HOME=$(grep "^${USER_ID}:" /etc/passwd | cut -d ":" -f6)
+if [ -z "${BACKUP_HOME// /}" ]; then
+    echo "Error: User ${USER_ID} is not found!"
+    exit 1
+fi
 
-DEFAULT_WORK_DIR="${BACKUP_HOME}/backup-main"
+DEFAULT_WORK_DIR="/var/backups/ic"
 echo "Please enter backup work directory"
 echo "(default: ${DEFAULT_WORK_DIR}):"
 read WORK_DIR
@@ -55,9 +62,7 @@ echo
 
 ROOT_DIR="${WORK_DIR}/backup"
 
-rm -rf ${BACKUP_EXE} ${BACKUP_EXE_GZ}
-
-DOWNLOAD_URL="https://download.dfinity.systems/ic/${BUILD_ID}/release/${BACKUP_EXE_GZ}"
+DOWNLOAD_URL="https://download.dfinity.systems/ic/${BUILD_ID}/release/${BACKUP_EXE_NAME}.gz"
 echo "Downloading: ${DOWNLOAD_URL}"
 curl -L ${DOWNLOAD_URL} --output ${BACKUP_EXE_GZ}
 
@@ -68,13 +73,20 @@ read -r -d '' CONFIG <<-EOM
 {
     "backup_instance": "${BACKUP_INSTANCE}",
     "nns_url": "${NNS_URL}",
-    "nns_pem": "${WORK_DIR}/$PUBLIC_KEY_FILE",
+    "nns_pem": "${WORK_DIR}/$PUBLIC_KEY_NAME",
     "root_dir": "${ROOT_DIR}",
-    "excluded_dirs": ["backups", "diverged_checkpoints", "diverged_state_markers", "fs_tmp", "tip", "tmp"],
+    "excluded_dirs": [
+        "backups", 
+        "diverged_checkpoints", 
+        "diverged_state_markers",
+        "fs_tmp", 
+        "tip", 
+        "tmp"
+    ],
     "ssh_private_key": "${BACKUP_HOME}/.ssh/id_ed25519_backup",
+    "disk_threshold_warn": 75,
     "slack_token": "<INSERT SLACK TOKEN>",
-    "subnets":
-    [
+    "subnets": [
         {
             "subnet_id": "<INSERT 1. SUBNET_ID>",
             "initial_replica_version": "<INSERT REPLICA_VERSION>",
@@ -127,13 +139,14 @@ mkdir -p ${ROOT_DIR}
 cp ${BACKUP_EXE} ${WORK_DIR}
 cp ${CONFIG_FILE} ${WORK_DIR}
 cp ${PUBLIC_KEY_FILE} ${WORK_DIR}
+echo "Installing system config:"
 sudo cp ${SERVICE_CONFIG_FILE} /etc/systemd/system
-rm ${BACKUP_EXE} ${CONFIG_FILE} ${PUBLIC_KEY_FILE} ${SERVICE_CONFIG_FILE}
+rm -rf ${TMP_DIR}
 
 echo
 echo
 echo
-echo "Please edit the config file ${CONFIG_FILE} placed in ${WORK_DIR}"
+echo "Please edit the config file ${CONFIG_FILE_NAME} placed in ${WORK_DIR}"
 echo
 echo "then start the backup service with:"
 echo "sudo systemctl start ic-backup.service"
