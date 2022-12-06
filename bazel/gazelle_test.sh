2a79080eab727b0fbb6dc0126d05284fbd53d6da
Ali Piccioni <ali.piccioni@dfinity.org>
2022-12-06 09:36:09 +0000

    Add gazelle test

---
+++ bazel/gazelle_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BUILD_WORKSPACE_DIRECTORY="$(dirname $(readlink $WORKSPACE))"
+export BUILD_WORKSPACE_DIRECTORY
+
+if ! "$GAZELLE_BIN" update -mode=diff; then
+    echo "Some gazelle managed build files need to be regenerated" >&2
+    echo "run bazel run //:gazelle" >&2
+    echo "for more help go to #project-bazel" >&2
+    exit 1
+fi
