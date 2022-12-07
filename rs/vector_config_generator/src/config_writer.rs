46df49f01fe488e2862e21a594321649ed0fdae4
Gilles Marin <mrngilles@gmail.com>
2022-12-07 10:34:38 +0000

    Add OldMachinesFilter

--- rs/vector_config_generator/src/config_writer.rs
+++ rs/vector_config_generator/src/config_writer.rs
@@ -4,6 +4,7 @@
 use std::{
     collections::{BTreeMap, BTreeSet},
     fmt::Debug,
+    net::IpAddr,
     path::{Path, PathBuf},
     sync::{Arc, RwLock},
 };
@@ -34,6 +35,27 @@ impl TargetGroupFilter for NodeIDRegexFilter {
     }
 }
 
+/// OldMachinesFilter will remove target groups with a target containing a 5000 as the
+/// 5th field of their IPv6 address.
+/// These machines were previously deployed manually by the DFINITY team, and are in
+/// the process of being redeployed, so this filter should not be relevant for too long.
+///
+/// Feel free to inspect the machines and remove this filter when the redeployment is done
+/// for all DCs.
+#[derive(Debug)]
+pub struct OldMachinesFilter {}
+
+impl TargetGroupFilter for OldMachinesFilter {
+    fn filter(&self, target_group: TargetGroup) -> bool {
+        target_group
+            .targets
+            .iter()
+            // Maps addresses to true if they are new
+            .map(|sockaddr| !matches!(sockaddr.ip(), IpAddr::V6(a) if a.segments()[4] == 0x5000))
+            .all(|is_new| is_new)
+    }
+}
+
 #[derive(Debug)]
 pub struct TargetGroupFilterList {
     filters: Vec<Box<dyn TargetGroupFilter>>,
@@ -54,8 +76,145 @@ impl TargetGroupFilter for TargetGroupFilterList {
             self.filters
                 .iter()
                 .map(|f| f.filter(target_group.clone()))
-                .any(|status| status)
+                .all(|status| status)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{collections::BTreeSet, net::SocketAddrV6, str::FromStr};
+
+    use ic_types::{NodeId, PrincipalId, SubnetId};
+    use regex::Regex;
+    use service_discovery::TargetGroup;
+
+    use crate::config_writer::TargetGroupFilter;
+
+    use super::{NodeIDRegexFilter, OldMachinesFilter, TargetGroupFilterList};
+
+    fn create_dummy_target_group(ipv6: &str) -> TargetGroup {
+        let mut targets = BTreeSet::new();
+        targets.insert(std::net::SocketAddr::V6(
+            SocketAddrV6::from_str(ipv6).unwrap(),
+        ));
+        TargetGroup {
+            node_id: NodeId::from(PrincipalId::new_anonymous()),
+            ic_name: "mercury".into(),
+            targets,
+            subnet_id: Some(SubnetId::from(PrincipalId::new_anonymous())),
+            dc_id: None,
+            operator_id: None,
+        }
+    }
+
+    #[test]
+    fn old_machine_filter_test() {
+        let filter = OldMachinesFilter {};
+
+        let new_tg = create_dummy_target_group("[2a02:800:2:2003:6801:f6ff:fec4:4c86]:9091");
+        assert!(filter.filter(new_tg));
+
+        let old_tg = create_dummy_target_group("[2a02:800:2:2003:5000:f6ff:fec4:4c86]:9091");
+        assert!(!filter.filter(old_tg));
+    }
+
+    #[test]
+    fn old_machine_filter_test_no_targets() {
+        let filter = OldMachinesFilter {};
+        let tg = TargetGroup {
+            node_id: NodeId::from(PrincipalId::new_anonymous()),
+            ic_name: "mercury".into(),
+            targets: BTreeSet::new(),
+            subnet_id: Some(SubnetId::from(PrincipalId::new_anonymous())),
+            dc_id: None,
+            operator_id: None,
+        };
+        assert!(filter.filter(tg));
+    }
+
+    #[test]
+    fn node_id_regex_filter_test() {
+        let filter = NodeIDRegexFilter::new(Regex::new("^i").unwrap());
+
+        let accepted_tg = TargetGroup {
+            node_id: NodeId::from(
+                PrincipalId::from_str(
+                    "iylgr-zpxwq-kqgmf-4srtx-o4eey-d6bln-smmq6-we7px-ibdea-nondy-eae",
+                )
+                .unwrap(),
+            ),
+            ic_name: "mercury".into(),
+            targets: BTreeSet::new(),
+            subnet_id: Some(SubnetId::from(PrincipalId::new_anonymous())),
+            dc_id: None,
+            operator_id: None,
+        };
+        assert!(filter.filter(accepted_tg));
+
+        let rejected_tg = TargetGroup {
+            node_id: NodeId::from(
+                PrincipalId::from_str(
+                    "x33ed-h457x-bsgyx-oqxqf-6pzwv-wkhzr-rm2j3-npodi-purzm-n66cg-gae",
+                )
+                .unwrap(),
+            ),
+            ic_name: "mercury".into(),
+            targets: BTreeSet::new(),
+            subnet_id: Some(SubnetId::from(PrincipalId::new_anonymous())),
+            dc_id: None,
+            operator_id: None,
+        };
+        assert!(!filter.filter(rejected_tg));
+    }
+
+    #[test]
+    fn filter_list_test() {
+        let filter_vec: Vec<Box<dyn TargetGroupFilter>> = vec![
+            Box::new(NodeIDRegexFilter::new(Regex::new("^i").unwrap())),
+            Box::new(OldMachinesFilter {}),
+        ];
+        let filterlist = TargetGroupFilterList::new(filter_vec);
+
+        let accepted_tg = TargetGroup {
+            node_id: NodeId::from(
+                PrincipalId::from_str(
+                    "iylgr-zpxwq-kqgmf-4srtx-o4eey-d6bln-smmq6-we7px-ibdea-nondy-eae",
+                )
+                .unwrap(),
+            ),
+            ic_name: "mercury".into(),
+            targets: BTreeSet::new(),
+            subnet_id: Some(SubnetId::from(PrincipalId::new_anonymous())),
+            dc_id: None,
+            operator_id: None,
+        };
+        assert!(filterlist.filter(accepted_tg));
+
+        let rejected_tg_1 = TargetGroup {
+            node_id: NodeId::from(
+                PrincipalId::from_str(
+                    "x33ed-h457x-bsgyx-oqxqf-6pzwv-wkhzr-rm2j3-npodi-purzm-n66cg-gae",
+                )
+                .unwrap(),
+            ),
+            ic_name: "mercury".into(),
+            targets: BTreeSet::new(),
+            subnet_id: Some(SubnetId::from(PrincipalId::new_anonymous())),
+            dc_id: None,
+            operator_id: None,
+        };
+        assert!(!filterlist.filter(rejected_tg_1));
+
+        let old_tg = create_dummy_target_group("[2a02:800:2:2003:5000:f6ff:fec4:4c86]:9091");
+        assert!(!filterlist.filter(old_tg));
+    }
+
+    #[test]
+    fn filter_list_test_empty() {
+        let filterlist = TargetGroupFilterList::new(vec![]);
+        let tg = create_dummy_target_group("[2a02:800:2:2003:6801:f6ff:fec4:4c86]:9091");
+        assert!(filterlist.filter(tg));
     }
 }
 
--- rs/vector_config_generator/src/main.rs
+++ rs/vector_config_generator/src/main.rs
@@ -21,7 +21,9 @@ use service_discovery::{
 };
 use slog::{info, o, Drain, Logger};
 
-use crate::config_writer::{NodeIDRegexFilter, TargetGroupFilter, TargetGroupFilterList};
+use crate::config_writer::{
+    NodeIDRegexFilter, OldMachinesFilter, TargetGroupFilter, TargetGroupFilterList,
+};
 
 mod config_writer;
 mod vector_configuration;
@@ -61,6 +63,8 @@ fn main() -> Result<()> {
             filter_node_id_regex.clone(),
         )));
     };
+
+    filters_vec.push(Box::new(OldMachinesFilter {}));
 
     let filters = TargetGroupFilterList::new(filters_vec);
 
