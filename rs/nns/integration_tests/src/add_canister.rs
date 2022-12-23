9cc751afa4a52bc4ccdf0a958296917e3be34f3a
Andriy Berestovskyy <andriy.berestovskyy@dfinity.org>
2022-12-23 00:16:02 +0000

    Fix NNS tests due to the bigger Universal Canister size

--- rs/nns/integration_tests/src/add_canister.rs
+++ rs/nns/integration_tests/src/add_canister.rs
@@ -39,7 +39,7 @@ fn add_nns_canister_via_governance_proposal() {
             wasm_module: UNIVERSAL_CANISTER_WASM.to_vec(),
             arg: vec![],
             query_allocation: Some(Nat::from(34)),
-            memory_allocation: Some(Nat::from(1234567)),
+            memory_allocation: Some(Nat::from(12345678)),
             compute_allocation: Some(Nat::from(12)),
             initial_cycles: 1 << 45,
             authz_changes: Vec::new(),
