4f3dd02ecc9d7219d1bd4becc5909684377b2668
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2022-12-28 17:36:01 +0000

    Increase upgrade_downgrade_app_subnet group timeout to 15 minutes

--- rs/tests/bin/upgrade_downgrade_app_subnet_test.rs
+++ rs/tests/bin/upgrade_downgrade_app_subnet_test.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use std::time::Duration;
 
 use ic_tests::driver::new::group::SystemTestGroup;
 use ic_tests::orchestrator::upgrade_downgrade::{config, upgrade_downgrade_app_subnet};
@@ -6,9 +7,9 @@ use ic_tests::systest;
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
+        .with_overall_timeout(Duration::from_secs(15 * 60))
         .with_setup(config)
         .add_test(systest!(upgrade_downgrade_app_subnet))
         .execute_from_args()?;
-
     Ok(())
 }
