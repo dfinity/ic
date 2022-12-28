f88bd3c1baab328286ab8a6de883501bf9233513
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2022-12-28 18:16:33 +0000

    Increate create_subnet_hourly_test group timeout to 15 minutes

--- rs/tests/bin/create_subnet_hourly_test.rs
+++ rs/tests/bin/create_subnet_hourly_test.rs
@@ -1,6 +1,5 @@
-#[rustfmt::skip]
-
 use anyhow::Result;
+use std::time::Duration;
 
 use ic_tests::driver::new::group::SystemTestGroup;
 use ic_tests::nns_tests::create_subnet;
@@ -8,7 +7,10 @@ use ic_tests::systest;
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
+        .with_overall_timeout(Duration::from_secs(15 * 60))
+        // expected to take 250..450 seconds
         .with_setup(create_subnet::hourly_config)
+        // expected to take up to 200 seconds
         .add_test(systest!(create_subnet::test))
         .execute_from_args()?;
     Ok(())
