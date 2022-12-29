c5f17dfb40c3696efbae274a24f723b921151127
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2022-12-29 15:17:21 +0000

    Introduce ict

---
+++ rs/tests/ict/cmd/root.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"regexp"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+)
+
+func NewRootCmd() *cobra.Command {
+	var version = "0.1.0"
+	var rootCmd = &cobra.Command{
+		Version: version,
+		Use:     "ict",
+		Long:    "ict " + version + "\nA simple CLI for running system_tests in Bazel.",
+		Example: "ict test //rs/tests:basic_health_test",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Print help by default, i.e. if no args are provided.
+			if len(args) == 0 {
+				cmd.Help()
+				return
+			}
+		},
+	}
+	// rootCmd.SetOutput(color.Output)
+	cobra.AddTemplateFunc("StyleHeading", color.New(color.FgGreen).SprintFunc())
+	usageTemplate := rootCmd.UsageTemplate()
+	usageTemplate = strings.NewReplacer(
+		`Usage:`, `{{StyleHeading "Usage:"}}`,
+		`Examples:`, `{{StyleHeading "Examples:"}}`,
+		`Aliases:`, `{{StyleHeading "Aliases:"}}`,
+		`Available Commands:`, `{{StyleHeading "Available Commands:"}}`,
+		`Global Flags:`, `{{StyleHeading "Global Flags:"}}`,
+		// The following one steps on "Global Flags:"
+		// `Flags:`, `{{StyleHeading "Flags:"}}`,
+	).Replace(usageTemplate)
+	re := regexp.MustCompile(`(?m)^Flags:\s*$`)
+	usageTemplate = re.ReplaceAllLiteralString(usageTemplate, `{{StyleHeading "Flags:"}}`)
+	rootCmd.SetUsageTemplate(usageTemplate)
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	return rootCmd
+}
---
+++ rs/tests/ict/cmd/testCmd.go
@@ -0,0 +1,63 @@
+package cmd
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var RED = "\033[1;31m"
+var GREEN = "\033[1;32m"
+var CYAN = "\033[0;36m"
+var NC = "\033[0m"
+
+type Config struct {
+	isCacheTestResult bool
+	testTmpDir        string
+	isDryRun          bool
+}
+
+func TestCommandWithConfig(cfg *Config) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		target := args[0]
+		cache_test_results := "--cache_test_results="
+		if cfg.isCacheTestResult {
+			cache_test_results += "yes"
+		} else {
+			cache_test_results += "no"
+		}
+		command := []string{"bazel", "test", target, "--config=systest", cache_test_results}
+		if cfg.testTmpDir != "" {
+			command = append(command, "--test_tmpdir="+cfg.testTmpDir)
+		}
+		// Print Bazel command for debugging puroposes.
+		cmd.Println(CYAN + "Raw Bazel command to be invoked: \n$" + strings.Join(command, " ") + NC)
+		if cfg.isDryRun {
+			return nil
+		} else {
+			// Start Bazel test Command with stdout, stderr streaming.
+			testCmd := exec.Command(command[0], command[1:]...)
+			testCmd.Stdout = os.Stdout
+			testCmd.Stderr = os.Stderr
+			return testCmd.Run()
+		}
+	}
+}
+
+func NewTestCmd() *cobra.Command {
+	var cfg = Config{}
+	var testCmd = &cobra.Command{
+		Use:     "test",
+		Aliases: []string{"system_test", "t"},
+		Short:   "Run system_test target with Bazel",
+		Example: "ict test //rs/tests:basic_health_test",
+		Args:    cobra.ExactArgs(1),
+		RunE:    TestCommandWithConfig(&cfg),
+	}
+	testCmd.Flags().BoolVarP(&cfg.isDryRun, "dry-run", "n", false, "Print raw Bazel command to be invoked.")
+	testCmd.Flags().BoolVarP(&cfg.isCacheTestResult, "cache_test_results", "c", false, "Bazel's cache_test_results, see --cache_test_results tag in Bazel docs.")
+	testCmd.PersistentFlags().StringVarP(&cfg.testTmpDir, "test_tmpdir", "t", "", "Dir for storing test results, see --test-tmpdir tag in Bazel docs.")
+	return testCmd
+}
