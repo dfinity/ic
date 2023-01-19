9476e8ae447b5a8c8684fae3ce1e32cd5a4ab40c
Jack Lloyd <jack.lloyd@dfinity.org>
2023-01-19 13:08:46 +0000

    chore(crypto): CRP-1868 Add SecretBytes container

---
+++ rs/crypto/secrets_containers/src/secret_bytes.rs
@@ -0,0 +1,66 @@
+/// Sensitive data held as a Vec of `u8`s.
+use core::fmt::{self, Debug};
+use zeroize::{Zeroize, ZeroizeOnDrop};
+
+/// Sensitive data held as a Vec of `u8`s.
+///
+/// This type is very similar to the SecretVec type, also in this
+/// crate, with the main difference being that its serialization and
+/// deserialization are as compact as (and identical to) just encoding
+/// a bytestring directly.
+#[derive(Clone, Eq, PartialEq, Zeroize, ZeroizeOnDrop)]
+pub struct SecretBytes {
+    secret: Vec<u8>,
+}
+
+impl SecretBytes {
+    /// Constructs a `SecretBytes` from the provided `secret`
+    pub fn new(secret: Vec<u8>) -> Self {
+        Self { secret }
+    }
+
+    /// Constructs a `SecretBytes` from the provided (non-owned) `secret`.
+    ///
+    /// Note: It is the responsibility of the caller to clear the memory
+    /// used by `secret`.
+    pub fn new_from_unowned(secret: &[u8]) -> Self {
+        Self::new(secret.to_vec())
+    }
+
+    /// Provides read-only access to the secret vec.
+    pub fn expose_secret(&self) -> &[u8] {
+        &self.secret
+    }
+}
+
+impl Debug for SecretBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "REDACTED SecretBytes of length {}", self.secret.len())
+    }
+}
+
+impl serde::Serialize for SecretBytes {
+    fn serialize<S: serde::ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_bytes(&self.secret)
+    }
+}
+
+impl<'a> serde::Deserialize<'a> for SecretBytes {
+    fn deserialize<D: serde::de::Deserializer<'a>>(deserializer: D) -> Result<Self, D::Error> {
+        struct Visitor;
+
+        impl<'de> serde::de::Visitor<'de> for Visitor {
+            type Value = SecretBytes;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(formatter, "a bytestring")
+            }
+
+            fn visit_bytes<E: serde::de::Error>(self, v: &[u8]) -> Result<Self::Value, E> {
+                Ok(SecretBytes::new_from_unowned(v))
+            }
+        }
+
+        deserializer.deserialize_bytes(Visitor)
+    }
+}
--- rs/crypto/secrets_containers/src/tests.rs
+++ rs/crypto/secrets_containers/src/tests.rs
@@ -125,10 +125,26 @@ mod secret_array {
 
         let serialized = serde_cbor::to_vec(&sec).expect("failed to serialize SecretArray");
 
+        assert_eq!(serialized.len(), 66);
+
         let deserialized: SecretArray<SECRET_SIZE> =
             serde_cbor::from_slice(&serialized[..]).expect("failed to deserialize SecretArray");
 
         assert_eq!(sec, deserialized);
+    }
+
+    #[test]
+    fn should_serialization_be_equivalent_to_compact_bytestring() {
+        const SECRET_SIZE: usize = 32;
+
+        let sec = SecretArray::new_and_dont_zeroize_argument(&[137; SECRET_SIZE]);
+
+        let serialized = serde_cbor::to_vec(&sec).expect("failed to serialize SecretArray");
+
+        let bytes = serde_bytes::ByteBuf::from(vec![137; SECRET_SIZE]);
+        let expected = serde_cbor::to_vec(&bytes).expect("failed to serialize array");
+
+        assert_eq!(serialized, expected);
     }
 
     #[test]
@@ -294,6 +310,8 @@ mod secret_vec {
 
         let serialized = serde_cbor::to_vec(&sec).expect("failed to serialize SecretVec");
 
+        assert_eq!(serialized.len(), 144);
+
         let deserialized =
             serde_cbor::from_slice(&serialized[..]).expect("failed to deserialize SecretVec");
 
@@ -315,6 +333,168 @@ mod secret_vec {
         {
             let stuff = SuperType {
                 secret: SecretVec::new_and_dont_zeroize_argument(&input),
+                _public: [1; 32],
+            };
+
+            ptr = stuff.secret.expose_secret().as_ptr();
+        }
+
+        // Can't test for 0, since memory may already be used elsewhere.
+        assert_ne!(
+            unsafe { core::slice::from_raw_parts(ptr, SECRET_SIZE) },
+            input
+        );
+    }
+}
+
+mod secret_bytes {
+    use crate::SecretBytes;
+
+    #[test]
+    fn should_expose_secret_as_expected() {
+        const SECRET_SIZE: usize = 64;
+        const SECRET_BYTE: u8 = 137;
+        let input = vec![SECRET_BYTE; SECRET_SIZE];
+
+        let sec = SecretBytes::new_from_unowned(&input);
+
+        assert_eq!(input, *sec.expose_secret());
+    }
+
+    #[test]
+    fn should_redact_debug_logging() {
+        const SECRET_SIZE: usize = 64;
+        const SECRET_BYTE: u8 = 137;
+        let input = vec![SECRET_BYTE; SECRET_SIZE];
+
+        let sec = SecretBytes::new_from_unowned(&input);
+
+        let debug_string = format!("{:?}", sec);
+
+        assert!(debug_string.contains("REDACTED"));
+        assert!(!debug_string.contains(&SECRET_BYTE.to_string()[..]));
+    }
+
+    #[test]
+    fn should_move_input_after_new() {
+        const SECRET_SIZE: usize = 64;
+        let input = vec![137; SECRET_SIZE];
+        let input_ptr = input.as_ptr();
+        let input_clone = input.clone();
+
+        let sec = SecretBytes::new(input);
+
+        assert_eq!(sec.expose_secret(), input_clone);
+        assert_eq!(sec.expose_secret().as_ptr(), input_ptr);
+    }
+
+    #[test]
+    fn should_clear_memory_after_dropping() {
+        const SECRET_SIZE: usize = 64;
+        let input = vec![137; SECRET_SIZE];
+
+        let ptr: *const u8;
+
+        {
+            let sec = SecretBytes::new_from_unowned(&input);
+            ptr = sec.expose_secret().as_ptr();
+            assert_eq!(
+                unsafe { core::slice::from_raw_parts(ptr, SECRET_SIZE) },
+                input
+            );
+        }
+
+        // Can't test for 0, since memory may already be used elsewhere.
+        assert_ne!(
+            unsafe { core::slice::from_raw_parts(ptr, SECRET_SIZE) },
+            input
+        );
+    }
+
+    #[test]
+    fn should_clear_memory_after_move_into_function() {
+        const SECRET_SIZE: usize = 64;
+        let input = vec![137; SECRET_SIZE];
+
+        let sec = SecretBytes::new_from_unowned(&input);
+        let ptr = sec.expose_secret().as_ptr();
+        assert_eq!(
+            unsafe { core::slice::from_raw_parts(ptr, SECRET_SIZE) },
+            input
+        );
+
+        fn move_into_me(moved_sec: SecretBytes) {
+            let input = vec![137; SECRET_SIZE]; // just non-DRY this, so it can live in the fn
+            let ptr = moved_sec.expose_secret().as_ptr();
+            assert_eq!(
+                unsafe { core::slice::from_raw_parts(ptr, SECRET_SIZE) },
+                input
+            );
+        }
+        move_into_me(sec);
+
+        // Can't test for 0, since memory may already be used elsewhere.
+        assert_ne!(
+            unsafe { core::slice::from_raw_parts(ptr, SECRET_SIZE) },
+            input
+        );
+    }
+
+    #[test]
+    fn should_compare_equal_for_clones() {
+        const SECRET_SIZE: usize = 64;
+
+        let sec = SecretBytes::new_from_unowned(&[137; SECRET_SIZE]);
+        let sec2 = sec.clone();
+
+        assert_eq!(sec, sec2);
+    }
+
+    #[test]
+    fn should_deserialize_from_serialized() {
+        const SECRET_SIZE: usize = 64;
+
+        let sec = SecretBytes::new_from_unowned(&[137; SECRET_SIZE]);
+
+        let serialized = serde_cbor::to_vec(&sec).expect("failed to serialize SecretBytes");
+
+        assert_eq!(serialized.len(), 66);
+
+        let deserialized =
+            serde_cbor::from_slice(&serialized[..]).expect("failed to deserialize SecretBytes");
+
+        assert_eq!(sec, deserialized);
+    }
+
+    #[test]
+    fn should_have_compact_bytestring_serialization() {
+        const SECRET_SIZE: usize = 32;
+
+        let sec = SecretBytes::new_from_unowned(&[137; SECRET_SIZE]);
+
+        let serialized = serde_cbor::to_vec(&sec).expect("failed to serialize SecretBytes");
+
+        let bytes = serde_bytes::ByteBuf::from(vec![137; SECRET_SIZE]);
+        let expected = serde_cbor::to_vec(&bytes).expect("failed to serialize array");
+
+        assert_eq!(serialized, expected);
+    }
+
+    #[test]
+    fn should_clear_memory_when_composed() {
+        const SECRET_SIZE: usize = 64;
+        let input = vec![137; SECRET_SIZE];
+
+        let ptr: *const u8;
+
+        struct SuperType {
+            secret: SecretBytes,
+            _public: [u8; 32],
+        }
+
+        {
+            let stuff = SuperType {
+                secret: SecretBytes::new_from_unowned(&input),
                 _public: [1; 32],
             };
 
