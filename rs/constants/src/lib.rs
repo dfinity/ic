59cef6d01c529ea21104c5e9109b56bbdbde5e90
Alin Sinpalean <alin.sinpalean@dfinity.org>
2023-01-20 14:47:27 +0000

    [Hotfix] fix: Introduce a hard limit on ingress history messages

--- rs/constants/src/lib.rs
+++ rs/constants/src/lib.rs
@@ -34,6 +34,16 @@ pub const PERMITTED_DRIFT_AT_VALIDATOR: Duration = Duration::from_secs(30);
 /// shouldn't impact other parts of the system.
 pub const PERMITTED_DRIFT_AT_ARTIFACT_MANAGER: Duration = Duration::from_secs(60);
 
+/// The maximum number of messages that can be present in the ingress history
+/// at any one time.
+///
+/// The value is the product of the default `max_ingress_messages_per_block`
+/// configured in the subnet record; and the `MAX_INGRESS_TTL` (assuming a block
+/// rate of 1 block per second). Times 2, since we could theoretically have
+/// `MAX_INGRESS_TTL` worth of `Received` messages; plus the same number of
+/// messages in terminal states.
+pub const INGRESS_HISTORY_MAX_MESSAGES: usize = 2 * 1000 * MAX_INGRESS_TTL.as_secs() as usize;
+
 /// Message count limit for `System` subnet outgoing streams used for throttling
 /// the matching input stream.
 pub const SYSTEM_SUBNET_STREAM_MSG_LIMIT: usize = 100;
