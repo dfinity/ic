e113d54d71432518a495093c9c908ec148de96e0
Leon Tan <leon.tan@dfinity.org>
2023-01-27 11:23:04 +0000

    test(consensus): Small changes to catch up tests

--- rs/tests/bin/catch_up_impossible_test.rs
+++ rs/tests/bin/catch_up_impossible_test.rs
@@ -5,11 +5,16 @@ use anyhow::Result;
 use ic_tests::consensus::catch_up_test::{config_catch_up_impossible, test_catch_up_impossible};
 use ic_tests::driver::new::group::SystemTestGroup;
 use ic_tests::systest;
+use std::time::Duration;
+
+const TIMEOUT: Duration = Duration::from_secs(15 * 60);
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_setup(config_catch_up_impossible)
         .add_test(systest!(test_catch_up_impossible))
+        .with_timeout_per_test(TIMEOUT)
+        .with_overall_timeout(TIMEOUT)
         .execute_from_args()?;
 
     Ok(())
