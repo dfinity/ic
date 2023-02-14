b5340b1a8c0dcde2c98db58b3dc7c18333a51786
Mahir Eusufzai <mahir.eusufzai@dfinity.org>
2023-02-14 22:30:50 +0000

    [5/N][Onchain Observability Canister][NET-1334] Define Report type and implement Signable trait

--- rs/monitoring/onchain_observability/adapter/src/main.rs
+++ rs/monitoring/onchain_observability/adapter/src/main.rs
@@ -10,26 +10,32 @@ use ic_base_types::NodeId;
 use ic_canister_client::{Agent, Sender};
 use ic_config::registry_client::DataProviderConfig;
 use ic_crypto::CryptoComponent;
-use ic_interfaces::crypto::{BasicSigner, KeyManager};
-use ic_logger::{error, info, new_replica_logger_from_config, ReplicaLogger};
+use ic_interfaces::crypto::{BasicSigner, ErrorReproducibility, KeyManager};
+use ic_logger::{error, info, new_replica_logger_from_config, warn, ReplicaLogger};
 use ic_metrics::MetricsRegistry;
 use ic_onchain_observability_adapter::{
     get_peer_ids, Config, Flags, MetricsParseError, SampledMetricsCollector,
 };
 use ic_registry_client::client::RegistryClientImpl;
 use ic_registry_local_store::LocalStoreImpl;
-use ic_types::messages::MessageId;
+use ic_types::{
+    crypto::CryptoError,
+    messages::MessageId,
+    onchain_observability::{PeerReport, Report, SignedReport},
+};
 use serde_json::to_string_pretty;
 use std::{
     collections::{HashMap, HashSet},
     sync::Arc,
-    time::Instant,
+    time::SystemTime,
 };
 use tokio::{
     runtime::Handle,
     time::{interval, MissedTickBehavior},
 };
 use url::Url;
+
+const MAX_CRYPTO_SIGNATURE_ATTEMPTS: u64 = 5;
 
 #[tokio::main]
 pub async fn main() {
@@ -74,15 +80,18 @@ pub async fn main() {
     let mut sampling_interval = interval(config.sampling_interval_sec);
     sampling_interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
     let mut sampler = SampledMetricsCollector::new_with_client(http_client);
-    let mut start_time = Instant::now();
-
+    let mut start_time = SystemTime::now();
     loop {
         sampling_interval.tick().await;
         if let Err(e) = sampler.sample().await {
             error!(logger, "sampling failed {:?}", e);
         }
-        if start_time.elapsed() >= config.report_length_sec {
-            let end_time = Instant::now();
+        if start_time
+            .elapsed()
+            .expect("Negative system time must not happen")
+            >= config.report_length_sec
+        {
+            let end_time = SystemTime::now();
             let up_time_peer_labels = sampler.aggregate();
             // TODO NET-1328 - remove panic and handle failed conversion gracefully
             let uptime: HashMap<NodeId, f32> = up_time_peer_labels
@@ -100,10 +109,30 @@ pub async fn main() {
                 "Completed Report: interval{:?}-{:?} uptime% {:?}", start_time, end_time, uptime
             );
 
-            sampler.clear();
-            start_time = Instant::now();
+            let report = prepare_report(node_id, start_time, end_time, uptime);
 
-            //TODO send report
+            for signature_attempts in 0..MAX_CRYPTO_SIGNATURE_ATTEMPTS {
+                match sign_report(crypto_component.clone(), report.clone(), node_id).await {
+                    Ok(_signed_report) => {
+                        /* send report */
+                        break;
+                    }
+                    Err(e) => {
+                        if e.is_reproducible()
+                            || signature_attempts == MAX_CRYPTO_SIGNATURE_ATTEMPTS - 1
+                        {
+                            // "Reproducible" represents a fundamental issue with the crypto setup
+                            // TODO (NET-1338) convert to metric counter
+                            error!(logger, "Failed to sign report, skipping {:?}", e);
+                            break;
+                        }
+                        // Otherwise, if we receive sporadic error, re-try a limited number of times.
+                        warn!(logger, "Received sporadic crypto signature failure {:?}", e);
+                    }
+                }
+            }
+            sampler.clear();
+            start_time = SystemTime::now();
         }
     }
 }
@@ -177,6 +206,57 @@ async fn create_canister_client(crypto: Arc<CryptoComponent>, url: Url, node_id:
         sign: Arc::new(sign_cmd),
     };
     Agent::new(url, sender)
+}
+
+// Prepare a report in the format expected by the observability canister.
+// The report will be sent along with a signature allowing canister to
+// verify its authentictiy.
+fn prepare_report(
+    reporting_node_id: NodeId,
+    start_time: SystemTime,
+    end_time: SystemTime,
+    uptime_percent: HashMap<NodeId, f32>,
+) -> Report {
+    // First, prepare the peer data
+    let peer_reports = uptime_percent
+        .iter()
+        .map(|(peer_id, uptime)| PeerReport {
+            peer_id_binary: peer_id.get().to_vec(),
+            peer_uptime_percent: *uptime,
+        })
+        .collect();
+
+    // Next, append the reporting-node-specific fields
+    Report {
+        start_time,
+        end_time,
+        reporting_node_id_binary: reporting_node_id.get().to_vec(),
+        replica_last_start: SystemTime::now(), // TODO (NET-1331) add real value
+        peer_report: peer_reports,
+    }
+}
+
+async fn sign_report(
+    crypto: Arc<CryptoComponent>,
+    report: Report,
+    reporting_node_id: NodeId,
+) -> Result<SignedReport, CryptoError> {
+    let report_clone = report.clone();
+    match tokio::task::spawn_blocking(move || {
+        crypto
+            .sign_basic(
+                &report_clone,
+                reporting_node_id,
+                crypto.registry_client().get_latest_version(),
+            )
+            .map(|value| value.get().0)
+    })
+    .await
+    .unwrap()
+    {
+        Ok(signature) => Ok(SignedReport { report, signature }),
+        Err(e) => Err(e),
+    }
 }
 
 // Peer label is in form of "{NODE IP}_{NODE ID PREFIX}" so we can take the prefix
