0a95e3670527802c46fc7ad02277b03aab3ddda2
Adrian Alić <adrian.alic@dfinity.org>
2023-02-17 12:41:09 +0000

    docs: Fix outdated/broken links to developer docs

--- rs/types/types/src/messages/http.rs
+++ rs/types/types/src/messages/http.rs
@@ -21,7 +21,7 @@ use serde::{Deserialize, Serialize};
 use std::{collections::BTreeSet, convert::TryFrom, error::Error, fmt};
 
 /// Describes the fields of a canister update call as defined in
-/// `<https://sdk.dfinity.org/docs/interface-spec/index.html#api-update>`.
+/// `<https://internetcomputer.org/docs/current/references/ic-interface-spec#http-call>`.
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub struct HttpCanisterUpdate {
@@ -88,8 +88,8 @@ impl HttpCallContent {
     }
 }
 
-/// Describes the fields of a canister query call (a query from a user to a
-/// canister) as defined in `<https://sdk.dfinity.org/docs/interface-spec/index.html#api-query>`.
+/// Describes the fields of a canister query call (a query from a user to a canister) as
+/// defined in `<https://internetcomputer.org/docs/current/references/ic-interface-spec#http-query>`.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct HttpUserQuery {
     pub canister_id: Blob,
@@ -127,7 +127,7 @@ impl HttpQueryContent {
     }
 }
 
-/// A `read_state` request as defined in `<https://sdk.dfinity.org/docs/interface-spec/index.html#api-request-read-state>`.
+/// A `read_state` request as defined in `<https://internetcomputer.org/docs/current/references/ic-interface-spec#http-read-state>`.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct HttpReadState {
     pub sender: Blob,
@@ -212,7 +212,7 @@ impl HttpReadState {
 }
 
 /// A request envelope as defined in
-/// `<https://sdk.dfinity.org/docs/interface-spec/index.html#authentication>`.
+/// `<https://internetcomputer.org/docs/current/references/ic-interface-spec#authentication>`.
 ///
 /// The content is either [`HttpCallContent`], [`HttpQueryContent`] or
 /// [`HttpReadStateContent`].
@@ -430,7 +430,7 @@ impl From<CanisterIdError> for HttpRequestError {
 }
 
 /// Describes a delegation map as defined in
-/// `<https://sdk.dfinity.org/docs/interface-spec/index.html#certification-delegation>`.
+/// `<https://internetcomputer.org/docs/current/references/ic-interface-spec#certification-delegation>`.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub struct Delegation {
@@ -504,7 +504,7 @@ impl SignedBytesWithoutDomainSeparator for Delegation {
 }
 
 /// Describes a delegation as defined in
-/// `<https://sdk.dfinity.org/docs/interface-spec/index.html#certification-delegation>`.
+/// `<https://internetcomputer.org/docs/current/references/ic-interface-spec#certification-delegation>`.
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub struct SignedDelegation {
@@ -579,7 +579,7 @@ pub struct HttpReadStateResponse {
     pub certificate: Blob,
 }
 
-/// A `Certificate` as defined in `<https://sdk.dfinity.org/docs/interface-spec/index.html#_certificate>`
+/// A `Certificate` as defined in `<https://internetcomputer.org/docs/current/references/ic-interface-spec#certificate>`
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Certificate {
     pub tree: MixedHashTree,
@@ -588,7 +588,7 @@ pub struct Certificate {
     pub delegation: Option<CertificateDelegation>,
 }
 
-/// A `CertificateDelegation` as defined in `<https://smartcontracts.org/docs/interface-spec/index.html#certification-delegation>`
+/// A `CertificateDelegation` as defined in `<https://internetcomputer.org/docs/current/references/ic-interface-spec#certification-delegation>`
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct CertificateDelegation {
     pub subnet_id: Blob,
