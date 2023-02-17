5976636eef6892fee4e3b8d8fcf70c82ceefe4ce
John Plevyak <jplevyak@gmail.com>
2023-02-17 23:28:06 +0000

    i
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- bin/dotbootstrap.sh
+++ bin/dotbootstrap.sh
@@ -2,3 +2,4 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
 curl --proto =https --tlsv1.2 -sSf https://sh.rustup.rs
+echo "scp -r jplevyak@jplevyak.homeip.net:.vim ~/.vim"
