f1abf86421e10deb1b8b839ab58724db774d8f71
Franz-Stefan Preiss <franzstefan@dfinity.org>
2023-03-09 10:27:13 +0000

    chore(crypto): CRP-1725 remove obsolete read_node_public_keys and store_node_public_keys

--- rs/crypto/internal/crypto_service_provider/src/public_key_store/mod.rs
+++ rs/crypto/internal/crypto_service_provider/src/public_key_store/mod.rs
@@ -1,11 +1,6 @@
 //! Interfaces for saving and retrieving public keys
-use crate::PUBLIC_KEY_STORE_DATA_FILENAME;
-use ic_protobuf::crypto::v1::NodePublicKeys;
 use ic_protobuf::registry::crypto::v1::{PublicKey as PublicKeyProto, X509PublicKeyCert};
 use ic_types::Time;
-use prost::Message;
-use std::fs;
-use std::path::Path;
 
 pub mod proto_pubkey_store;
 
@@ -14,35 +9,6 @@ pub mod temp_pubkey_store;
 
 #[cfg(test)]
 pub mod mock_pubkey_store;
-
-/// Error while reading or writing public keys
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum PublicKeyStoreError {
-    ParsingError(String),
-    SerialisationError(String),
-    IOError(String),
-}
-
-/// Write the node public keys to local storage
-pub fn store_node_public_keys(
-    crypto_root: &Path,
-    node_pks: &NodePublicKeys,
-) -> Result<(), PublicKeyStoreError> {
-    let pk_file = crypto_root.join(PUBLIC_KEY_STORE_DATA_FILENAME);
-
-    ic_utils::fs::write_protobuf_using_tmp_file(pk_file, node_pks)
-        .map_err(|err| PublicKeyStoreError::IOError(err.to_string()))
-}
-
-/// Read the node public keys from local storage
-pub fn read_node_public_keys(crypto_root: &Path) -> Result<NodePublicKeys, PublicKeyStoreError> {
-    let pk_file = crypto_root.join(PUBLIC_KEY_STORE_DATA_FILENAME);
-    match fs::read(pk_file) {
-        Ok(data) => NodePublicKeys::decode(&*data)
-            .map_err(|err| PublicKeyStoreError::ParsingError(err.to_string())),
-        Err(err) => Err(PublicKeyStoreError::IOError(err.to_string())),
-    }
-}
 
 #[derive(Debug)]
 pub enum PublicKeySetOnceError {
