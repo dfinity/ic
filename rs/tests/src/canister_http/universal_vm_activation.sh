ba84b84ed8ff819188e90abe8ea143e3550857f9
Michael Weigelt <michael.weigelt@dfinity.org>
2023-03-27 10:31:06 +0000

    [VER-2232] Embed docker containers in UVM config images

--- rs/tests/src/canister_http/universal_vm_activation.sh
+++ rs/tests/src/canister_http/universal_vm_activation.sh
@@ -19,18 +19,20 @@ export ipv6
 echo "IPv6 is $ipv6"
 
 ipv4=""
-while true; do
-    ipv4=$(ip -4 address | grep "scope global dynamic" | awk '{$1=$1};1' | awk -F '[ \t]+|/' '{print $2}')
-    if [[ $ipv4 == '' ]]; then
-        sleep 5s
-        echo "IPv4 not found. Retrying in 5 seconds ..."
-    else
-        echo "Found IPv4 $ipv4"
-        break
-    fi
-done
-export ipv4
-echo "IPv4 is $ipv4"
+if ip link show dev enp2s0 >/dev/null; then
+    while true; do
+        ipv4=$(ip -4 address | grep "scope global dynamic" | awk '{$1=$1};1' | awk -F '[ \t]+|/' '{print $2}')
+        if [[ $ipv4 == '' ]]; then
+            sleep 5s
+            echo "IPv4 not found. Retrying in 5 seconds ..."
+        else
+            echo "Found IPv4 $ipv4"
+            break
+        fi
+    done
+    export ipv4
+fi
+echo "IPv4 is ${ipv4:-disabled}"
 
 # 2 - generate ipv6 service cert with root cert and key, using `minica`
 mkdir certs
@@ -40,22 +42,28 @@ cp /config/key.pem minica-key.pem
 chmod -R 755 ./
 
 echo "Making certs directory in $(pwd)"
+docker load -i /config/minica.tar
+docker tag bazel/image:image minica
 docker run \
     -v "$(pwd)":/output \
-    registry.gitlab.com/dfinity-lab/open/public-docker-registry/ryantk/minica \
-    -ip-addresses="$ipv6,$ipv4"
+    minica \
+    -ip-addresses="$ipv6${ipv4:+,$ipv4}"
 
-mv $ipv6 ipv6 # updateing service certificate folder name so it can be fed to ssl-proxy container
+mv $ipv6 ipv6 # updating service certificate folder name so it can be fed to ssl-proxy container
 chmod -R 755 ipv6
-mv $ipv4 ipv4
-chmod -R 755 ipv4
+if [[ -n $ipv4 ]]; then
+    mv $ipv4 ipv4
+    chmod -R 755 ipv4
+fi
 
 # 3 - setting up httpbin on port 20443
+docker load -i /config/httpbin.tar
+docker tag bazel/image:image httpbin
 docker run \
     --rm \
     -d \
     -p 20443:80 \
     -v "$(pwd)/ipv6":/certs \
     --name httpbin \
-    registry.gitlab.com/dfinity-lab/open/public-docker-registry/dfinity/httpbin \
+    httpbin \
     --cert-file /certs/cert.pem --key-file /certs/key.pem --port 80
