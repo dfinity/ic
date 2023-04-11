a53f0002f30fd5e0898722a25c47410267007701
Mario Pastorelli <mario.pastorelli@dfinity.org>
2023-04-11 10:25:26 +0000

    feat(FI-688): add webserver and arguments to ICRC Rosetta

--- rs/rosetta-api/icrc1/rosetta/src/main.rs
+++ rs/rosetta-api/icrc1/rosetta/src/main.rs
@@ -1,1 +1,52 @@
-fn main() {}
+use anyhow::{Context, Result};
+use axum::{http::StatusCode, routing::get, Json, Router};
+use clap::Parser;
+use std::net::TcpListener;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The port to which Rosetta will bind.
+    /// If not set then it will be 0.
+    #[arg(short, long)]
+    port: Option<u16>,
+
+    /// The file where the port to which Rosetta will bind
+    /// will be written.
+    #[arg(short = 'P', long)]
+    port_file: Option<PathBuf>,
+}
+
+impl Args {
+    /// Return the port to which Rosetta should bind to.
+    fn get_port(&self) -> u16 {
+        match (&self.port, &self.port_file) {
+            (None, None) => 8080,
+            (None, Some(_)) => 0,
+            (Some(port), _) => *port,
+        }
+    }
+}
+
+async fn health() -> (StatusCode, Json<()>) {
+    (StatusCode::OK, Json(()))
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let app = Router::new().route("/health", get(health));
+
+    let tcp_listener = TcpListener::bind(format!("0.0.0.0:{}", args.get_port()))?;
+
+    if let Some(port_file) = args.port_file {
+        std::fs::write(port_file, tcp_listener.local_addr()?.to_string())?;
+    }
+
+    axum::Server::from_tcp(tcp_listener)?
+        .serve(app.into_make_service())
+        .await
+        .context("Unable to start the Rosetta server")
+}
