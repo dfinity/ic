9c6fe71bf24527bb20c0f4f67f75d9d4dddc9cdc
Enzo Desiage <enzo.desiage@dfinity.org>
2023-04-12 08:01:22 +0000

    IDX-2719 - Fix - add support for smoke target for macos nproc [hotfix]

--- bazel/pre-commit/shellcheck.sh
+++ bazel/pre-commit/shellcheck.sh
@@ -9,7 +9,7 @@ cd "$REPO_PATH"
 
 # TODO IDX-2796 - enable for the whole repository
 # Run shellcheck for all testnet/tests/scripts/*.sh
-find testnet/tests/scripts/ -maxdepth 1 -type f -name "*.sh" -print0 | xargs -0 -P "$(nproc)" "$SHELLCHECK" --source-path=include
+find testnet/tests/scripts/ -maxdepth 1 -type f -name "*.sh" -exec "$SHELLCHECK" --source-path=include {} \+
 
 # Run shellcheck in for testnet/tests/scripts/test_modules/*.sh
-find testnet/tests/scripts/test_modules/ -maxdepth 1 -type f -name "*.sh" -print0 | xargs -0 -P "$(nproc)" "$SHELLCHECK" --source-path=include
+find testnet/tests/scripts/test_modules/ -maxdepth 1 -type f -name "*.sh" -exec "$SHELLCHECK" --source-path=include {} \+
