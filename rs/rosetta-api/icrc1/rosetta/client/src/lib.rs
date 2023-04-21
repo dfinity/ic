e86df1801d6432045c5106aa63c41787ea76cc1f
Mario Pastorelli <mario.pastorelli@dfinity.org>
2023-04-21 12:22:16 +0000

    feat(FI-703) [icrc rosetta] add rosetta-client and one example

---
+++ rs/rosetta-api/icrc1/rosetta/client/src/lib.rs
@@ -0,0 +1,47 @@
+use ic_icrc_rosetta::common::types::{MetadataRequest, NetworkListResponse};
+use reqwest::{Client, Url};
+use url::ParseError;
+
+pub struct RosettaClient {
+    pub url: Url,
+    pub http_client: Client,
+}
+
+impl RosettaClient {
+    pub fn from_url(url: Url) -> Self {
+        Self {
+            url,
+            http_client: Client::new(),
+        }
+    }
+
+    pub fn from_str_url(url: &str) -> Result<Self, ParseError> {
+        let url = Url::parse(url)?;
+        Ok(Self::from_url(url))
+    }
+
+    fn url(&self, path: &str) -> Url {
+        self.url
+            .join(path)
+            .unwrap_or_else(|e| panic!("Failed to join {} with path {}: {}", self.url, path, e))
+    }
+
+    pub async fn health(&self) -> reqwest::Result<()> {
+        self.http_client
+            .get(self.url("/health"))
+            .send()
+            .await?
+            .json()
+            .await
+    }
+
+    pub async fn network_list(&self) -> reqwest::Result<NetworkListResponse> {
+        self.http_client
+            .post(self.url("/network/list"))
+            .json(&MetadataRequest { metadata: None })
+            .send()
+            .await?
+            .json()
+            .await
+    }
+}
---
+++ rs/rosetta-api/icrc1/rosetta/tests/system_tests.rs
@@ -0,0 +1,52 @@
+use candid::Principal;
+use ic_base_types::CanisterId;
+use ic_icrc_rosetta::common::types::NetworkIdentifier;
+use ic_icrc_rosetta_client::RosettaClient;
+use ic_icrc_rosetta_runner::start_rosetta;
+use ic_starter_tests::{start_replica, ReplicaBins, ReplicaStarterConfig};
+use std::path::PathBuf;
+
+fn path_from_env(var: &str) -> PathBuf {
+    std::fs::canonicalize(
+        std::env::var(var).unwrap_or_else(|_| panic!("Environment variable {} is not set", var)),
+    )
+    .unwrap()
+}
+
+fn replica_bins() -> ReplicaBins {
+    let canister_launcher = path_from_env("CANISTER_LAUNCHER");
+    let replica_bin = path_from_env("REPLICA_BIN");
+    let sandbox_launcher = path_from_env("SANDBOX_LAUNCHER");
+    let starter_bin = path_from_env("STARTER_BIN");
+    ReplicaBins {
+        canister_launcher,
+        replica_bin,
+        sandbox_launcher,
+        starter_bin,
+    }
+}
+
+fn rosetta_bin() -> PathBuf {
+    path_from_env("ROSETTA_BIN_PATH")
+}
+
+#[tokio::test]
+async fn test_network_list() {
+    let context = start_replica(&replica_bins(), &ReplicaStarterConfig::default())
+        .await
+        .expect("Unable to start the replica");
+    let replica_url = format!("http://localhost:{}", context.port);
+
+    let context = start_rosetta(&rosetta_bin(), Principal::anonymous(), replica_url).await;
+    let client = RosettaClient::from_str_url(&format!("http://0.0.0.0:{}", context.port))
+        .expect("Unable to parse url");
+    let network_list = client
+        .network_list()
+        .await
+        .expect("Unable to call network_list")
+        .network_identifiers;
+    let expected = NetworkIdentifier::for_ledger_id(
+        CanisterId::try_from(Principal::anonymous().as_slice()).unwrap(),
+    );
+    assert_eq!(network_list, vec![expected]);
+}
