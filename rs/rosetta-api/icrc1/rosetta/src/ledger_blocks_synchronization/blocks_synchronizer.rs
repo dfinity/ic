de3a69bbb07925be7fdb5d526e41a557a681fb09
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2023-04-21 14:09:31 +0000

    feat(FI-704) Synchronize block tip

--- rs/rosetta-api/icrc1/rosetta/src/ledger_blocks_synchronization/blocks_synchronizer.rs
+++ rs/rosetta-api/icrc1/rosetta/src/ledger_blocks_synchronization/blocks_synchronizer.rs
@@ -1,36 +1,103 @@
-use std::{ops::RangeInclusive, sync::Arc};
-
 use crate::common::storage::{storage_client::StorageClient, types::RosettaBlock};
 use candid::Nat;
 use ic_crypto_tree_hash::{LookupStatus, MixedHashTree};
 use ic_icrc1::hash::Hash;
 use icrc_ledger_agent::Icrc1Agent;
 use icrc_ledger_types::icrc3::blocks::{GetBlocksRequest, GetBlocksResponse};
+use num_traits::ToPrimitive;
 use serde_bytes::ByteBuf;
+use std::{cmp, collections::HashMap, ops::RangeInclusive, sync::Arc};
 use LookupStatus::Found;
 
-// The blocks synchronizer starts by getting the most recent block and starts fetching the blockchain starting from the top of the blockchain
-// It verifies fetched blocks and stores verified blocks in the database
-// TODO: Make BlocksSynchronizer fetch blocks continously instead of returning once it has finished synching from the tip
+// The Range of indices to be synchronized
+// Contains the hashes of the top and end of the index range, which is used to ensure the fetched block interval is valid
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct SyncRange {
+    index_range: RangeInclusive<u64>,
+    leading_block_hash: ByteBuf,
+    trailing_parent_hash: Option<ByteBuf>,
+}
+
+impl SyncRange {
+    fn new(
+        lowest_index: u64,
+        _highest_index: u64,
+        leading_block_hash: ByteBuf,
+        trailing_parent_hash: Option<ByteBuf>,
+    ) -> Self {
+        Self {
+            index_range: RangeInclusive::new(lowest_index, _highest_index),
+            leading_block_hash,
+            trailing_parent_hash,
+        }
+    }
+}
+
+/// The blocks synchronizer starts by getting the most recent block and starts fetching the blockchain starting from the top of the blockchain
+/// It verifies fetched blocks and stores verified blocks in the database
 pub async fn start_synching_blocks(
-    agent: &Icrc1Agent,
+    agent: Arc<Icrc1Agent>,
     storage_client: Arc<StorageClient>,
     maximum_blocks_per_request: u64,
 ) -> anyhow::Result<()> {
-    // Get the most recent hash and index of the icrc ledger and start fetching from the top of the chain
-    let (tip_block_index, tip_block_hash) = get_blockchain_tip_data(agent).await?;
+    // Check whether the blocks storage is empty.
+    // Conduct a full sync of the icrc ledger if storage is empty otherwise only sync the blocks that are missing between the tip of the blockchain and the highest block in storage
+    let (tip_block_index, tip_block_hash) = fetch_blockchain_tip_data(agent.clone()).await?;
 
-    // The leading index/hash is the highest block index/hash that is requested by the icrc ledger
-    let mut index_interval = RangeInclusive::new(
-        tip_block_index.saturating_sub(maximum_blocks_per_request),
-        tip_block_index,
+    // The starting point of the synchronization process is either 0 if the database is empty or the highest stored block index plus one
+    // The trailing parent hash is either None if the database is empty or the block hash of the block with the highest block index in storage
+    let sync_range = storage_client.get_block_with_highest_block_idx()?.map_or(
+        SyncRange::new(0, tip_block_index, ByteBuf::from(tip_block_hash), None),
+        |block| {
+            SyncRange::new(
+                // If storage is up to date then the start index is the same as the tip of the ledger
+                block.index + 1,
+                tip_block_index,
+                ByteBuf::from(tip_block_hash),
+                Some(block.block_hash),
+            )
+        },
     );
-    let mut leading_block_hash = Some(ByteBuf::from(tip_block_hash));
+
+    // Do not make a sync call if the storage is up to date with the replica's ledger
+    if !sync_range.index_range.is_empty() {
+        sync_blocks_interval(
+            agent.clone(),
+            storage_client.clone(),
+            maximum_blocks_per_request,
+            sync_range,
+        )
+        .await?;
+    }
+    Ok(())
+}
+
+/// Syncs a specific blocks interval, validates it and stores it in storage
+/// Expects the blocks interval to exist on the ledger
+async fn sync_blocks_interval(
+    agent: Arc<Icrc1Agent>,
+    storage_client: Arc<StorageClient>,
+    maximum_blocks_per_request: u64,
+    sync_range: SyncRange,
+) -> anyhow::Result<()> {
+    // The leading index/hash is the highest block index/hash that is requested by the icrc ledger
+    let mut next_index_interval = RangeInclusive::new(
+        cmp::max(
+            sync_range
+                .index_range
+                .end()
+                .saturating_sub(maximum_blocks_per_request),
+            *sync_range.index_range.start(),
+        ),
+        *sync_range.index_range.end(),
+    );
+    let mut leading_block_hash = Some(sync_range.leading_block_hash);
 
     // Start fetching blocks starting from the tip of the blockchain and store them in the database
-    // Only the genesis block has a parent hash that is none
-    while leading_block_hash.is_some() {
-        let fetched_blocks = fetch_blocks_interval(agent, index_interval.clone()).await?;
+    loop {
+        // The fetch_blocks_interval function guarentees that all blocks that were asked for are fetched if they exist on the ledger
+        let fetched_blocks =
+            fetch_blocks_interval(agent.clone(), next_index_interval.clone()).await?;
 
         // Verify that the fetched blocks are valid
         // Leading block hash of a non empty fetched blocks can never be None -> Unwrap is safe
@@ -41,67 +108,159 @@ pub async fn start_synching_blocks(
             // Abort synchronization if blockchain is not valid
             return Err(anyhow::Error::msg(format!(
                 "The fetched blockchain contains invalid blocks in index range {} to {}",
-                index_interval.start(),
-                index_interval.end()
+                next_index_interval.start(),
+                next_index_interval.end()
             )));
         }
 
-        // Set variables for next loop iteration
         leading_block_hash = fetched_blocks[0].parent_hash.clone();
-        index_interval = RangeInclusive::new(
-            index_interval
-                .start()
-                .saturating_sub(fetched_blocks.len() as u64),
-            index_interval
-                .end()
-                .saturating_sub(fetched_blocks.len() as u64),
-        );
+        let number_of_blocks_fetched = fetched_blocks.len();
 
         // Store the fetched blocks in the database
-        storage_client.store_blocks(fetched_blocks)?;
-    }
+        storage_client.store_blocks(fetched_blocks.clone())?;
 
+        // If the interval of the last iteration started at the target height, then all blocks above and including the target height have been synched
+        if *next_index_interval.start() == *sync_range.index_range.start() {
+            // All blocks were fetched, now the parent hash of the lowest block fetched has to match the hash of the highest block in the database or None (If database was empty)
+            if leading_block_hash == sync_range.trailing_parent_hash {
+                break;
+            } else {
+                return Err(anyhow::Error::msg(format!(
+                    "Hash of block {} in database does not match parent hash of fetched block {}",
+                    next_index_interval.start().saturating_sub(1),
+                    next_index_interval.start()
+                )));
+            }
+        }
+
+        // Set variables for next loop iteration
+        let interval_start = cmp::max(
+            next_index_interval
+                .start()
+                .saturating_sub(number_of_blocks_fetched as u64),
+            *sync_range.index_range.start(),
+        );
+        let interval_end = cmp::max(
+            next_index_interval
+                .end()
+                .saturating_sub(number_of_blocks_fetched as u64),
+            *sync_range.index_range.start(),
+        );
+        next_index_interval = RangeInclusive::new(interval_start, interval_end);
+    }
     Ok(())
 }
 
-// Fetches all blocks given a certain interval. The interval is expected to be smaller or equal to the maximum number of blocks than can be requested
-pub async fn fetch_blocks_interval(
-    agent: &Icrc1Agent,
+/// Fetches all blocks given a certain interval. The interval is expected to be smaller or equal to the maximum number of blocks than can be requested
+/// Guarentees to return only if all blocks in the given interval were fetched
+async fn fetch_blocks_interval(
+    agent: Arc<Icrc1Agent>,
     index_range: RangeInclusive<u64>,
 ) -> anyhow::Result<Vec<RosettaBlock>> {
-    // Construct the request object for the icrc1 agent
-    let get_blocks_request = GetBlocksRequest {
-        start: Nat::from(*index_range.start()),
-        // To include the block at end_index we have to add one, since the index starts at 0
-        length: Nat::from(*index_range.end() - *index_range.start() + 1),
+    // Construct a hashmap which maps block indices to blocks. Blocks that have not been fetched are None
+    let mut fetched_blocks_result: HashMap<u64, Option<RosettaBlock>> = HashMap::new();
+
+    // Initialize fetched blocks map with None as no blocks have been fetched yet
+    index_range.for_each(|index| {
+        fetched_blocks_result.insert(index, None);
+    });
+
+    // Missing blocks are those block indices where the value in the hashmap is missing
+    let missing_blocks = |blocks: &HashMap<u64, Option<RosettaBlock>>| {
+        blocks
+            .iter()
+            .filter_map(
+                |(key, value)| {
+                    if value.is_none() {
+                        Some(*key)
+                    } else {
+                        None
+                    }
+                },
+            )
+            .collect::<Vec<u64>>()
     };
 
-    let mut fetched_blocks_result = vec![];
+    // Extract all block index intervals that can be fetch
+    let fetchable_intervals = |blocks: &HashMap<u64, Option<RosettaBlock>>| {
+        // Get all the missing block indices and sort them
+        let mut missing = missing_blocks(blocks);
+        missing.sort();
 
-    // Fetch blocks with a given request from the Icrc1Agent
-    let blocks_response: GetBlocksResponse =
-        agent.get_blocks(get_blocks_request).await.map_err(|_| {
-            let error_msg = format!(
-                "Icrc1Agent could not fetch blocks in interval {} to {}",
-                index_range.start().clone(),
-                index_range.end().clone()
-            );
-            anyhow::Error::msg(error_msg)
-        })?;
+        // If all blocks have been fetched return an empty vector
+        if missing.is_empty() {
+            return vec![];
+        }
 
-    // Convert all Generic Blocks into RosettaBlocks
-    for (index, block) in blocks_response.blocks.into_iter().enumerate() {
-        // The index of the RosettaBlock is the starting index of the request plus the position of current block in the response object
-        fetched_blocks_result.push(RosettaBlock::from_generic_block(
-            block,
-            *index_range.start() + index as u64,
-        )?);
+        let mut block_ranges = vec![];
+        let mut start = missing[0];
+
+        // It is possible that the replica returns block intervals that contain patches --> Find all missing indices and aggregate them in the longest consecutive intervals
+        for i in 1..missing.len() {
+            if missing[i] != missing[i - 1] + 1 {
+                block_ranges.push(RangeInclusive::new(start, missing[i - 1]));
+                start = missing[i];
+            }
+        }
+        block_ranges.push(RangeInclusive::new(start, missing[missing.len() - 1]));
+        block_ranges
+    };
+
+    // Ensure that this function only returns once all blocks have been collected
+    while !missing_blocks(&fetched_blocks_result).is_empty() {
+        // Calculate all longest consecutive block index intervals
+        for interval in fetchable_intervals(&fetched_blocks_result) {
+            let get_blocks_request = GetBlocksRequest {
+                start: Nat::from(*interval.start()),
+                // To include the block at end_index we have to add one, since the index starts at 0
+                length: Nat::from(*interval.end() - *interval.start() + 1),
+            };
+
+            // Fetch blocks with a given request from the Icrc1Agent
+            let blocks_response: GetBlocksResponse =
+                agent.get_blocks(get_blocks_request).await.map_err(|_| {
+                    let error_msg = format!(
+                        "Icrc1Agent could not fetch blocks in interval {} to {}",
+                        interval.start().clone(),
+                        interval.end().clone()
+                    );
+                    anyhow::Error::msg(error_msg)
+                })?;
+
+            // Convert all Generic Blocks into RosettaBlocks
+            for (index, block) in blocks_response.blocks.into_iter().enumerate() {
+                // The index of the RosettaBlock is the starting index of the request plus the position of current block in the response object
+                let block_index = blocks_response
+                    .first_index
+                    .0
+                    .to_u64()
+                    .ok_or_else(|| anyhow::Error::msg("Could not convert Nat to u64"))?
+                    + index as u64;
+                fetched_blocks_result.insert(
+                    block_index,
+                    Some(RosettaBlock::from_generic_block(block, block_index)?),
+                );
+            }
+        }
     }
 
-    Ok(fetched_blocks_result)
+    // Get all the blocks from the hashmap
+    let mut result = fetched_blocks_result
+        .into_values()
+        .map(|block| {
+            block.ok_or_else(|| anyhow::Error::msg("Could not fetch all requested blocks"))
+        })
+        .collect::<Result<Vec<RosettaBlock>, anyhow::Error>>()?;
+
+    // The blocks may not have been fetched in order
+    result.sort_by(|a, b| a.index.partial_cmp(&b.index).unwrap());
+
+    Ok(result)
 }
 
-async fn get_blockchain_tip_data(agent: &Icrc1Agent) -> anyhow::Result<(u64, Hash)> {
+/// Fetches the data certificate from the ledger and validates it
+/// Returns the tip index and hash of the ledger
+async fn fetch_blockchain_tip_data(agent: Arc<Icrc1Agent>) -> anyhow::Result<(u64, Hash)> {
     // Fetch the data certificate from the icrc ledger
     let data_certificate = agent
         .get_data_certificate()
@@ -153,9 +312,9 @@ async fn get_blockchain_tip_data(agent: &Icrc1Agent) -> anyhow::Result<(u64, Has
 }
 
 pub mod blocks_verifier {
+    use crate::common::storage::types::RosettaBlock;
     use serde_bytes::ByteBuf;
 
-    use crate::common::storage::types::RosettaBlock;
     pub fn is_valid_blockchain(
         blockchain: &Vec<RosettaBlock>,
         leading_block_hash: &ByteBuf,
