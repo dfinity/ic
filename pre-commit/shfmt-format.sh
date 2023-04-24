4b4178cd49d389d5159c9d18bb12444dedcaa0b2
Mykola Dzham <Mykola.Dzham@dfinity.org>
2023-04-24 23:21:50 +0200

    [hotfix] Do not touch internal git files in pre-commit check.

--- pre-commit/shfmt-format.sh
+++ pre-commit/shfmt-format.sh
@@ -6,4 +6,4 @@ SHFMT="$(readlink "$shfmt_path")"
 REPO_PATH="$(dirname "$(readlink "$WORKSPACE")")"
 cd "$REPO_PATH"
 
-find . -type f -name "*.sh" -exec "$SHFMT" -w -i 4 -bn -ci {} \+
+find . -path ./.git -prune -o -type f -name "*.sh" -exec "$SHFMT" -w -i 4 -bn -ci {} \+
--- pre-commit/shfmt-lint.sh
+++ pre-commit/shfmt-lint.sh
@@ -6,7 +6,7 @@ SHFMT="$(readlink "$shfmt_path")"
 REPO_PATH="$(dirname "$(readlink "$WORKSPACE")")"
 cd "$REPO_PATH"
 
-if ! find . -type f -name "*.sh" -exec "$SHFMT" -d -w -i 4 -bn -ci {} \+; then
+if ! find . -path ./.git -prune -o -type f -name "*.sh" -exec "$SHFMT" -d -w -i 4 -bn -ci {} \+; then
     cat >&2 <<EOF
 
 [-] Linting Bash script failed
