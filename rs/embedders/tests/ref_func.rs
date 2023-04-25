ea0582e5e6bafb8fb0d8b8f824b4fb884e8665ee
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2023-04-25 14:14:43 +0000

    RUN-624: Update all function indices.

---
+++ rs/embedders/tests/ref_func.rs
@@ -0,0 +1,120 @@
+//! Test to validate that all existing function references are properly updated
+//! after instrumentation.
+
+use ic_test_utilities::{mock_time, wasmtime_instance::WasmtimeInstanceBuilder};
+use ic_types::{
+    methods::{FuncRef, WasmMethod},
+    Cycles, PrincipalId,
+};
+
+fn run_go_export(wat: &str) {
+    const LARGE_INSTRUCTION_LIMIT: u64 = 1_000_000_000_000;
+
+    let mut instance = WasmtimeInstanceBuilder::new()
+        .with_wat(wat)
+        .with_api_type(ic_system_api::ApiType::update(
+            mock_time(),
+            vec![],
+            Cycles::from(0_u128),
+            PrincipalId::new_user_test_id(0),
+            0.into(),
+        ))
+        .with_num_instructions(LARGE_INSTRUCTION_LIMIT.into())
+        .build();
+
+    instance
+        .run(FuncRef::Method(WasmMethod::Update("go".to_string())))
+        .unwrap();
+}
+
+#[test]
+fn direct_call() {
+    run_go_export(
+        r#"
+		(module
+			(func $reply (import "ic0" "msg_reply"))
+			(func $f (result i32) (i32.const 123))
+			(func $go (export "canister_update go")
+				(call $f)
+				(i32.const 123)
+				(i32.ne)
+				(if (then unreachable))
+				(call $reply)
+		    )
+		)
+	"#,
+    );
+}
+
+#[test]
+fn element() {
+    run_go_export(
+        r#"
+		(module
+			(func $reply (import "ic0" "msg_reply"))
+			(func $f (result i32) (i32.const 123))
+
+			(type $f_type (func (result i32)))
+			(table 1 funcref)
+			(elem (i32.const 0) $f)
+
+			(func $go (export "canister_update go")
+				(call_indirect (type $f_type) (i32.const 0))
+				(i32.const 123)
+				(i32.ne)
+				(if (then unreachable))
+				(call $reply)
+		    )
+		)
+	"#,
+    );
+}
+
+#[test]
+fn element_const_expr() {
+    run_go_export(
+        r#"
+		(module
+			(func $reply (import "ic0" "msg_reply"))
+			(func $f (result i32) (i32.const 123))
+
+			(type $f_type (func (result i32)))
+			(table 1 funcref)
+			(elem (i32.const 0) funcref (ref.func $f))
+
+			(func $go (export "canister_update go")
+				(call_indirect (type $f_type) (i32.const 0))
+				(i32.const 123)
+				(i32.ne)
+				(if (then unreachable))
+				(call $reply)
+		    )
+		)
+	"#,
+    );
+}
+
+#[test]
+fn global() {
+    run_go_export(
+        r#"
+		(module
+			(func $reply (import "ic0" "msg_reply"))
+			(func $f (result i32) (i32.const 123))
+
+			(type $f_type (func (result i32)))
+			(table 1 funcref)
+			(global $f_global funcref (ref.func $f))
+
+			(func $go (export "canister_update go")
+				(table.set (i32.const 0) (global.get $f_global))
+				(call_indirect (type $f_type) (i32.const 0))
+				(i32.const 123)
+				(i32.ne)
+				(if (then unreachable))
+				(call $reply)
+		    )
+		)
+	"#,
+    );
+}
