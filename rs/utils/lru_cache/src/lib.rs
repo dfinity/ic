b02e753cc6730a13af4292aee0e050f4c8bf7763
Andriy Berestovskyy <andriy.berestovskyy@dfinity.org>
2023-04-29 16:53:29 +0000

    RUN-609 Add more query cache metrics

--- rs/utils/lru_cache/src/lib.rs
+++ rs/utils/lru_cache/src/lib.rs
@@ -59,26 +59,30 @@ where
         self.cache.get(key)
     }
 
-    /// Inserts or updates the item with the given key.
-    /// It also marks the item as the most-recently used.
-    /// The size parameter specifies the size of the item,
-    /// which must not exceed `MAX_SIZE = (2^63 - 1)`.
-    pub fn push(&mut self, key: K, value: V) {
+    /// Pushes a key-value pair into the cache. If an entry with key `k` already exists in
+    /// the cache or other cache entries are evicted (due to the cache capacity),
+    /// then it returns the old entry's key-value pairs. Otherwise, returns an empty vector.
+    pub fn push(&mut self, key: K, value: V) -> Vec<(K, V)> {
         let size = key.count_bytes() + value.count_bytes();
         assert!(size <= MAX_SIZE);
-        if let Some((prev_key, prev_value)) = self.cache.push(key, value) {
-            let prev_size = prev_key.count_bytes() + prev_value.count_bytes();
-            debug_assert!(self.size >= prev_size);
+
+        let removed_entry = self.cache.push(key, value);
+        if let Some((removed_key, removed_value)) = &removed_entry {
+            let removed_size = removed_key.count_bytes() + removed_value.count_bytes();
+            debug_assert!(self.size >= removed_size);
             // This cannot underflow because we know that `self.size` is
             // the sum of sizes of all items in the cache.
-            self.size -= prev_size;
+            self.size -= removed_size;
         }
         // This cannot overflow because we know that
         // `self.size <= self.capacity <= MAX_SIZE`
         // and `size <= MAX_SIZE == usize::MAX / 2`.
         self.size += size;
-        self.evict();
+        let mut evicted_entries = self.evict();
         self.check_invariants();
+
+        evicted_entries.extend(removed_entry);
+        evicted_entries
     }
 
     /// Removes and returns the value corresponding to the key from the cache or
@@ -103,7 +107,9 @@ where
     }
 
     /// Evicts as many items as needed to restore the capacity guarantee.
-    fn evict(&mut self) {
+    /// Returns the vector of evicted key-value pairs.
+    fn evict(&mut self) -> Vec<(K, V)> {
+        let mut ret = vec![];
         while self.size > self.capacity {
             match self.cache.pop_lru() {
                 Some((key, value)) => {
@@ -112,10 +118,13 @@ where
                     // This cannot underflow because we know that `self.size` is
                     // the sum of sizes of all items in the cache.
                     self.size -= size;
+
+                    ret.push((key, value));
                 }
                 None => break,
             }
         }
+        ret
     }
 
     fn check_invariants(&self) {
@@ -158,14 +167,21 @@ mod tests {
 
         assert!(lru.get(&Key(0)).is_none());
 
-        lru.push(Key(0), ValueSize(42, 10));
+        let evicted = lru.push(Key(0), ValueSize(42, 10));
         assert_eq!(*lru.get(&Key(0)).unwrap(), ValueSize(42, 10));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(Key(0), ValueSize(42, 11));
+        let evicted = lru.push(Key(0), ValueSize(42, 11));
         assert!(lru.get(&Key(0)).is_none());
+        // The new entry does not fit into the cache.
+        assert_eq!(
+            evicted,
+            vec![(Key(0), ValueSize(42, 11)), (Key(0), ValueSize(42, 10))]
+        );
 
-        lru.push(Key(0), ValueSize(24, 10));
+        let evicted = lru.push(Key(0), ValueSize(24, 10));
         assert_eq!(*lru.get(&Key(0)).unwrap(), ValueSize(24, 10));
+        assert_eq!(evicted, vec![]);
     }
 
     #[test]
@@ -192,33 +208,48 @@ mod tests {
 
         assert!(lru.get(&Key(0)).is_none());
 
-        lru.push(Key(0), ValueSize(42, 10));
+        let evicted = lru.push(Key(0), ValueSize(42, 10));
         assert_eq!(*lru.get(&Key(0)).unwrap(), ValueSize(42, 10));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(Key(1), ValueSize(20, 0));
+        let evicted = lru.push(Key(1), ValueSize(20, 0));
         assert_eq!(*lru.get(&Key(0)).unwrap(), ValueSize(42, 10));
         assert_eq!(*lru.get(&Key(1)).unwrap(), ValueSize(20, 0));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(Key(2), ValueSize(10, 10));
+        let evicted = lru.push(Key(2), ValueSize(10, 10));
         assert!(lru.get(&Key(0)).is_none());
         assert_eq!(*lru.get(&Key(1)).unwrap(), ValueSize(20, 0));
         assert_eq!(*lru.get(&Key(2)).unwrap(), ValueSize(10, 10));
+        // The least recently used is the first entry.
+        assert_eq!(evicted, vec![(Key(0), ValueSize(42, 10))]);
 
-        lru.push(Key(3), ValueSize(30, 10));
+        let evicted = lru.push(Key(3), ValueSize(30, 10));
         assert!(lru.get(&Key(1)).is_none());
         assert!(lru.get(&Key(2)).is_none());
         assert_eq!(*lru.get(&Key(3)).unwrap(), ValueSize(30, 10));
+        // Now the second and third entries are evicted.
+        assert_eq!(
+            evicted,
+            vec![(Key(1), ValueSize(20, 0)), (Key(2), ValueSize(10, 10))]
+        );
 
-        lru.push(Key(3), ValueSize(60, 5));
+        let evicted = lru.push(Key(3), ValueSize(60, 5));
         assert_eq!(*lru.get(&Key(3)).unwrap(), ValueSize(60, 5));
+        assert_eq!(evicted, vec![(Key(3), ValueSize(30, 10))]);
 
-        lru.push(Key(4), ValueSize(40, 5));
+        let evicted = lru.push(Key(4), ValueSize(40, 5));
         assert_eq!(*lru.get(&Key(3)).unwrap(), ValueSize(60, 5));
         assert_eq!(*lru.get(&Key(4)).unwrap(), ValueSize(40, 5));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(Key(4), ValueSize(40, 10));
+        let evicted = lru.push(Key(4), ValueSize(40, 10));
         assert!(lru.get(&Key(3)).is_none());
         assert_eq!(*lru.get(&Key(4)).unwrap(), ValueSize(40, 10));
+        assert_eq!(
+            evicted,
+            vec![(Key(3), ValueSize(60, 5)), (Key(4), ValueSize(40, 5))]
+        );
     }
 
     #[test]
@@ -227,55 +258,88 @@ mod tests {
 
         assert!(lru.get(&ValueSize(0, 10)).is_none());
 
-        lru.push(ValueSize(0, 10), ValueSize(42, 0));
+        let evicted = lru.push(ValueSize(0, 10), ValueSize(42, 0));
         assert_eq!(*lru.get(&ValueSize(0, 10)).unwrap(), ValueSize(42, 0));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(ValueSize(1, 0), ValueSize(20, 0));
+        let evicted = lru.push(ValueSize(1, 0), ValueSize(20, 0));
         assert_eq!(*lru.get(&ValueSize(0, 10)).unwrap(), ValueSize(42, 0));
         assert_eq!(*lru.get(&ValueSize(1, 0)).unwrap(), ValueSize(20, 0));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(ValueSize(2, 10), ValueSize(10, 0));
+        let evicted = lru.push(ValueSize(2, 10), ValueSize(10, 0));
         assert!(lru.get(&ValueSize(0, 10)).is_none());
         assert_eq!(*lru.get(&ValueSize(1, 0)).unwrap(), ValueSize(20, 0));
         assert_eq!(*lru.get(&ValueSize(2, 10)).unwrap(), ValueSize(10, 0));
+        // The least recently used is the first entry.
+        assert_eq!(evicted, vec![(ValueSize(0, 10), ValueSize(42, 0))]);
 
-        lru.push(ValueSize(3, 10), ValueSize(30, 0));
+        let evicted = lru.push(ValueSize(3, 10), ValueSize(30, 0));
         assert!(lru.get(&ValueSize(1, 0)).is_none());
         assert!(lru.get(&ValueSize(2, 10)).is_none());
         assert_eq!(*lru.get(&ValueSize(3, 10)).unwrap(), ValueSize(30, 0));
+        // Now the second and third entries are evicted.
+        assert_eq!(
+            evicted,
+            vec![
+                (ValueSize(1, 0), ValueSize(20, 0)),
+                (ValueSize(2, 10), ValueSize(10, 0))
+            ]
+        );
 
-        lru.push(ValueSize(3, 5), ValueSize(60, 0));
+        let evicted = lru.push(ValueSize(3, 5), ValueSize(60, 0));
         assert_eq!(*lru.get(&ValueSize(3, 5)).unwrap(), ValueSize(60, 0));
+        assert_eq!(evicted, vec![(ValueSize(3, 10), ValueSize(30, 0))]);
 
-        lru.push(ValueSize(4, 5), ValueSize(40, 0));
+        let evicted = lru.push(ValueSize(4, 5), ValueSize(40, 0));
         assert_eq!(*lru.get(&ValueSize(3, 5)).unwrap(), ValueSize(60, 0));
         assert_eq!(*lru.get(&ValueSize(4, 5)).unwrap(), ValueSize(40, 0));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(ValueSize(4, 10), ValueSize(40, 0));
+        let evicted = lru.push(ValueSize(4, 10), ValueSize(40, 0));
         assert!(lru.get(&ValueSize(3, 5)).is_none());
         assert_eq!(*lru.get(&ValueSize(4, 10)).unwrap(), ValueSize(40, 0));
+        assert_eq!(
+            evicted,
+            vec![
+                (ValueSize(3, 5), ValueSize(60, 0)),
+                (ValueSize(4, 5), ValueSize(40, 0))
+            ]
+        );
     }
 
     #[test]
     fn lru_cache_key_and_value_eviction() {
         let mut lru = LruCache::<ValueSize, ValueSize>::new(NumBytes::new(10));
 
-        lru.push(ValueSize(0, 5), ValueSize(42, 5));
+        let evicted = lru.push(ValueSize(0, 5), ValueSize(42, 5));
         assert_eq!(*lru.get(&ValueSize(0, 5)).unwrap(), ValueSize(42, 5));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(ValueSize(1, 0), ValueSize(20, 0));
+        let evicted = lru.push(ValueSize(1, 0), ValueSize(20, 0));
         assert_eq!(*lru.get(&ValueSize(0, 5)).unwrap(), ValueSize(42, 5));
         assert_eq!(*lru.get(&ValueSize(1, 0)).unwrap(), ValueSize(20, 0));
+        assert_eq!(evicted, vec![]);
 
-        lru.push(ValueSize(2, 5), ValueSize(10, 5));
+        let evicted = lru.push(ValueSize(2, 5), ValueSize(10, 5));
         assert!(lru.get(&ValueSize(0, 5)).is_none());
         assert_eq!(*lru.get(&ValueSize(1, 0)).unwrap(), ValueSize(20, 0));
         assert_eq!(*lru.get(&ValueSize(2, 5)).unwrap(), ValueSize(10, 5));
+        // The least recently used is the first entry.
+        assert_eq!(evicted, vec![(ValueSize(0, 5), ValueSize(42, 5))]);
 
-        lru.push(ValueSize(3, 5), ValueSize(30, 5));
+        let evicted = lru.push(ValueSize(3, 5), ValueSize(30, 5));
         assert!(lru.get(&ValueSize(1, 0)).is_none());
         assert!(lru.get(&ValueSize(2, 5)).is_none());
         assert_eq!(*lru.get(&ValueSize(3, 5)).unwrap(), ValueSize(30, 5));
+        // Now the second and third entries are evicted.
+        assert_eq!(
+            evicted,
+            vec![
+                (ValueSize(1, 0), ValueSize(20, 0)),
+                (ValueSize(2, 5), ValueSize(10, 5))
+            ]
+        );
     }
 
     #[test]
