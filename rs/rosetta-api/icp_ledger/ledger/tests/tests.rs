7df1e52c9d9aec6eaa34be8f662243902303c8eb
Mario Pastorelli <mario.pastorelli@dfinity.org>
2023-05-15 16:09:53 +0000

    fix [icp ledger] support old init

--- rs/rosetta-api/icp_ledger/ledger/tests/tests.rs
+++ rs/rosetta-api/icp_ledger/ledger/tests/tests.rs
@@ -1,5 +1,9 @@
+use std::collections::{HashMap, HashSet};
+
+use candid::Encode;
 use ic_ledger_core::Tokens;
-use icp_ledger::LedgerCanisterInitPayload;
+use ic_state_machine_tests::{PrincipalId, StateMachine};
+use icp_ledger::{AccountIdentifier, InitArgs, LedgerCanisterInitPayload, LedgerCanisterPayload};
 
 fn ledger_wasm() -> Vec<u8> {
     ic_test_utilities_load_wasm::load_wasm(
@@ -97,4 +101,44 @@ fn block_hashes_are_stable() {
 #[test]
 fn check_transfer_model() {
     ic_icrc1_ledger_sm_tests::check_transfer_model(ledger_wasm(), encode_init_args);
+}
+
+#[test]
+fn check_old_init() {
+    let env = StateMachine::new();
+    let old_init = Encode!(&InitArgs {
+        archive_options: None,
+        minting_account: AccountIdentifier::new(PrincipalId::new_user_test_id(1), None),
+        icrc1_minting_account: None,
+        initial_values: HashMap::new(),
+        max_message_size_bytes: None,
+        transaction_window: None,
+        send_whitelist: HashSet::new(),
+        transfer_fee: None,
+        token_symbol: Some("ICP".into()),
+        token_name: Some("Internet Computer".into()),
+    })
+    .unwrap();
+    env.install_canister(ledger_wasm(), old_init, None)
+        .expect("Unable to install the Ledger canister with the old init");
+}
+
+#[test]
+fn check_new_init() {
+    let env = StateMachine::new();
+    let new_init = Encode!(&LedgerCanisterPayload::Init(InitArgs {
+        archive_options: None,
+        minting_account: AccountIdentifier::new(PrincipalId::new_user_test_id(1), None),
+        icrc1_minting_account: None,
+        initial_values: HashMap::new(),
+        max_message_size_bytes: None,
+        transaction_window: None,
+        send_whitelist: HashSet::new(),
+        transfer_fee: None,
+        token_symbol: Some("ICP".into()),
+        token_name: Some("Internet Computer".into()),
+    }))
+    .unwrap();
+    env.install_canister(ledger_wasm(), new_init, None)
+        .expect("Unable to install the Ledger canister with the new init");
 }
