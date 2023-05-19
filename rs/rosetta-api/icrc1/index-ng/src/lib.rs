701658397f0d7789cc4ad4fc6e10804da281e5d5
Mario Pastorelli <mario.pastorelli@dfinity.org>
2023-05-19 13:41:43 +0000

    feat(FI-725) [icrc index-ng] add get_account_transactions endpoint

--- rs/rosetta-api/icrc1/index-ng/src/lib.rs
+++ rs/rosetta-api/icrc1/index-ng/src/lib.rs
@@ -1,5 +1,8 @@
-use candid::{CandidType, Deserialize, Principal};
+use candid::{CandidType, Deserialize, Nat, Principal};
+use icrc_ledger_types::icrc1::account::Account;
+use icrc_ledger_types::icrc1::transfer::BlockIndex;
 use icrc_ledger_types::icrc3::blocks::GenericBlock;
+use icrc_ledger_types::icrc3::transactions::Transaction;
 
 #[derive(CandidType, Debug, Deserialize)]
 pub enum IndexArg {
@@ -19,3 +22,36 @@ pub struct GetBlocksResponse {
     // The blocks in the requested range.
     pub blocks: Vec<GenericBlock>,
 }
+
+#[derive(CandidType, Debug, Deserialize, PartialEq, Eq)]
+pub struct GetAccountTransactionsArgs {
+    pub account: Account,
+    // The txid of the last transaction seen by the client.
+    // If None then the results will start from the most recent
+    // txid. If set then the results will start from the next
+    // most recent txid after start (start won't be included).
+    pub start: Option<BlockIndex>,
+    // Maximum number of transactions to fetch.
+    pub max_results: Nat,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Eq)]
+pub struct TransactionWithId {
+    pub id: BlockIndex,
+    pub transaction: Transaction,
+}
+
+#[derive(CandidType, Debug, Deserialize, PartialEq, Eq)]
+pub struct GetAccountTransactionsResponse {
+    pub transactions: Vec<TransactionWithId>,
+    // The txid of the oldest transaction the account has
+    pub oldest_tx_id: Option<BlockIndex>,
+}
+
+#[derive(CandidType, Debug, Deserialize, PartialEq, Eq)]
+pub struct GetAccountTransactionsError {
+    pub message: String,
+}
+
+pub type GetAccountTransactionsResult =
+    Result<GetAccountTransactionsResponse, GetAccountTransactionsError>;
