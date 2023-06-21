21259cd6d1e982503c4ed6421c9fbf58ac64e750
Linwei Shang <linwei.shang@dfinity.org>
2023-06-21 15:10:59 -0400

    feat: Bitcoin API handles cycles cost under the hood. (#406)
    * bitcoin fee
    
    * bump ver & changelog

--- bitcoin/mod.rs
+++ bitcoin/mod.rs
@@ -1,4 +1,6 @@
 //! The IC Bitcoin API.
+//!
+//! Check [Bitcoin integration](https://internetcomputer.org/docs/current/developer-docs/integrations/bitcoin/bitcoin-how-it-works/#api) for more details.
 
 use crate::api::call::{call_with_payment128, CallResult};
 use candid::Principal;
@@ -6,11 +8,32 @@ use candid::Principal;
 mod types;
 pub use types::*;
 
+const GET_UTXO_MAINNET: u128 = 10_000_000_000;
+const GET_UTXO_TESTNET: u128 = 4_000_000_000;
+
+const GET_CURRENT_FEE_PERCENTILES_MAINNET: u128 = 100_000_000;
+const GET_CURRENT_FEE_PERCENTILES_TESTNET: u128 = 40_000_000;
+
+const GET_BALANCE_MAINNET: u128 = 100_000_000;
+const GET_BALANCE_TESTNET: u128 = 40_000_000;
+
+const SEND_TRANSACTION_SUBMISSION_MAINNET: u128 = 5_000_000_000;
+const SEND_TRANSACTION_SUBMISSION_TESTNET: u128 = 2_000_000_000;
+
+const SEND_TRANSACTION_PAYLOAD_MAINNET: u128 = 20_000_000;
+const SEND_TRANSACTION_PAYLOAD_TESTNET: u128 = 8_000_000;
+
 /// See [IC method `bitcoin_get_balance`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_balance).
 ///
-/// This call requires cycles payment. The required cycles varies according to the subnet size (number of nodes).
-/// Check [Gas and cycles cost](https://internetcomputer.org/docs/current/developer-docs/gas-cost) for more details.
-pub async fn bitcoin_get_balance(arg: GetBalanceRequest, cycles: u128) -> CallResult<(Satoshi,)> {
+/// This call requires cycles payment.
+/// This method handles the cycles cost under the hood.
+/// Check [API fees & Pricing](https://internetcomputer.org/docs/current/developer-docs/integrations/bitcoin/bitcoin-how-it-works/#api-fees--pricing) for more details.
+pub async fn bitcoin_get_balance(arg: GetBalanceRequest) -> CallResult<(Satoshi,)> {
+    let cycles = match arg.network {
+        BitcoinNetwork::Mainnet => GET_BALANCE_MAINNET,
+        BitcoinNetwork::Testnet => GET_BALANCE_TESTNET,
+        BitcoinNetwork::Regtest => 0,
+    };
     call_with_payment128(
         Principal::management_canister(),
         "bitcoin_get_balance",
@@ -22,12 +45,15 @@ pub async fn bitcoin_get_balance(arg: GetBalanceRequest, cycles: u128) -> CallRe
 
 /// See [IC method `bitcoin_get_utxos`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_utxos).
 ///
-/// This call requires cycles payment. The required cycles varies according to the subnet size (number of nodes).
-/// Check [Gas and cycles cost](https://internetcomputer.org/docs/current/developer-docs/gas-cost) for more details.
-pub async fn bitcoin_get_utxos(
-    arg: GetUtxosRequest,
-    cycles: u128,
-) -> CallResult<(GetUtxosResponse,)> {
+/// This call requires cycles payment.
+/// This method handles the cycles cost under the hood.
+/// Check [API fees & Pricing](https://internetcomputer.org/docs/current/developer-docs/integrations/bitcoin/bitcoin-how-it-works/#api-fees--pricing) for more details.
+pub async fn bitcoin_get_utxos(arg: GetUtxosRequest) -> CallResult<(GetUtxosResponse,)> {
+    let cycles = match arg.network {
+        BitcoinNetwork::Mainnet => GET_UTXO_MAINNET,
+        BitcoinNetwork::Testnet => GET_UTXO_TESTNET,
+        BitcoinNetwork::Regtest => 0,
+    };
     call_with_payment128(
         Principal::management_canister(),
         "bitcoin_get_utxos",
@@ -37,11 +63,28 @@ pub async fn bitcoin_get_utxos(
     .await
 }
 
+fn send_transaction_fee(arg: &SendTransactionRequest) -> u128 {
+    let (submission, payload) = match arg.network {
+        BitcoinNetwork::Mainnet => (
+            SEND_TRANSACTION_SUBMISSION_MAINNET,
+            SEND_TRANSACTION_PAYLOAD_MAINNET,
+        ),
+        BitcoinNetwork::Testnet => (
+            SEND_TRANSACTION_SUBMISSION_TESTNET,
+            SEND_TRANSACTION_PAYLOAD_TESTNET,
+        ),
+        BitcoinNetwork::Regtest => (0, 0),
+    };
+    submission + payload * arg.transaction.len() as u128
+}
+
 /// See [IC method `bitcoin_send_transaction`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_send_transaction).
 ///
-/// This call requires cycles payment. The required cycles varies according to the subnet size (number of nodes).
-/// Check [Gas and cycles cost](https://internetcomputer.org/docs/current/developer-docs/gas-cost) for more details.
-pub async fn bitcoin_send_transaction(arg: SendTransactionRequest, cycles: u128) -> CallResult<()> {
+/// This call requires cycles payment.
+/// This method handles the cycles cost under the hood.
+/// Check [API fees & Pricing](https://internetcomputer.org/docs/current/developer-docs/integrations/bitcoin/bitcoin-how-it-works/#api-fees--pricing) for more details.
+pub async fn bitcoin_send_transaction(arg: SendTransactionRequest) -> CallResult<()> {
+    let cycles = send_transaction_fee(&arg);
     call_with_payment128(
         Principal::management_canister(),
         "bitcoin_send_transaction",
@@ -53,12 +96,17 @@ pub async fn bitcoin_send_transaction(arg: SendTransactionRequest, cycles: u128)
 
 /// See [IC method `bitcoin_get_current_fee_percentiles`](https://internetcomputer.org/docs/current/references/ic-interface-spec/#ic-bitcoin_get_current_fee_percentiles).
 ///
-/// This call requires cycles payment. The required cycles varies according to the subnet size (number of nodes).
-/// Check [Gas and cycles cost](https://internetcomputer.org/docs/current/developer-docs/gas-cost) for more details.
+/// This call requires cycles payment.
+/// This method handles the cycles cost under the hood.
+/// Check [API fees & Pricing](https://internetcomputer.org/docs/current/developer-docs/integrations/bitcoin/bitcoin-how-it-works/#api-fees--pricing) for more details.
 pub async fn bitcoin_get_current_fee_percentiles(
     arg: GetCurrentFeePercentilesRequest,
-    cycles: u128,
 ) -> CallResult<(Vec<MillisatoshiPerByte>,)> {
+    let cycles = match arg.network {
+        BitcoinNetwork::Mainnet => GET_CURRENT_FEE_PERCENTILES_MAINNET,
+        BitcoinNetwork::Testnet => GET_CURRENT_FEE_PERCENTILES_TESTNET,
+        BitcoinNetwork::Regtest => 0,
+    };
     call_with_payment128(
         Principal::management_canister(),
         "bitcoin_get_current_fee_percentiles",
