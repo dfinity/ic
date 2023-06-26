a4404bf338b03061b68c4332c9466c5c57467589
Igor Novgorodov <igor.novgorodov@dfinity.org>
2023-06-26 11:50:45 +0000

    BOUN-723 BOUN-724: ic-boundary routing

--- rs/boundary_node/ic_boundary/src/snapshot.rs
+++ rs/boundary_node/ic_boundary/src/snapshot.rs
@@ -1,16 +1,49 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
-use anyhow::Error;
+use anyhow::{anyhow, Context, Error};
 use arc_swap::ArcSwapOption;
 use async_trait::async_trait;
-use ic_registry_client::client::RegistryClient;
+use candid::Principal;
+use ic_protobuf::registry::subnet::v1::SubnetType;
+
+use ic_registry_client::client::{RegistryClient, RegistryDataProvider};
+use ic_registry_client_helpers::{
+    crypto::CryptoRegistry,
+    node::NodeRegistry,
+    routing_table::RoutingTableRegistry,
+    subnet::{SubnetListRegistry, SubnetRegistry},
+};
 
 use crate::Run;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Node {
+    pub id: Principal,
+    pub addr: String,
+    pub port: u16,
+    pub tls_certificate_der: Vec<u8>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CanisterRange {
+    pub start: Principal,
+    pub end: Principal,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Subnet {
+    pub id: Principal,
+    pub subnet_type: SubnetType,
+    pub ranges: Vec<CanisterRange>,
+    pub nodes: Vec<Node>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RoutingTable {
-    // TODO(BOUN-722): Implement Routing table
-    // similar to rs/boundary_node/control_plane/src/registry.rs, but without encoding things as `String`s
+    pub registry_version: u64,
+    pub nns_subnet_id: Principal,
+    pub subnets: Vec<Subnet>,
 }
 
 pub struct Runner<'a> {
@@ -28,13 +61,119 @@ impl<'a> Runner<'a> {
             registry_client,
         }
     }
+
+    fn get_routing_table(&mut self) -> Result<RoutingTable, Error> {
+        let version = self.registry_client.get_latest_version();
+
+        let root_subnet_id = self
+            .registry_client
+            .get_root_subnet_id(version)
+            .context("failed to get root subnet id")? // Result
+            .context("root subnet id not available")?; // Option
+
+        let routing_table = self
+            .registry_client
+            .get_routing_table(version)
+            .context("failed to get routing table")? // Result
+            .context("routing table not available")?; // Option
+
+        // Generate a temporary hash table with subnet_id to canister ranges mapping for later reference
+        let mut ranges_by_subnet = HashMap::new();
+        for (range, subnet_id) in routing_table {
+            let range = CanisterRange {
+                start: range.start.get_ref().0,
+                end: range.end.get_ref().0,
+            };
+
+            ranges_by_subnet
+                .entry(subnet_id.as_ref().0)
+                .and_modify(|x: &mut Vec<CanisterRange>| x.push(range.clone())) // Make compiler happy
+                .or_insert_with(|| vec![range]);
+        }
+
+        let subnet_ids = self
+            .registry_client
+            .get_subnet_ids(version)
+            .context("failed to get subnet ids")? // Result
+            .context("subnet ids not available")?; // Option
+
+        let subnets = subnet_ids
+            .into_iter()
+            .map(|subnet_id| {
+                let subnet = self
+                    .registry_client
+                    .get_subnet_record(subnet_id, version)
+                    .context("failed to get subnet")?
+                    .context("subnet not available")?;
+
+                let node_ids = self
+                    .registry_client
+                    .get_node_ids_on_subnet(subnet_id, version)
+                    .context("failed to get node ids")? // Result
+                    .context("node ids not available")?; // Option
+
+                let nodes = node_ids
+                    .into_iter()
+                    .map(|node_id| {
+                        let transport_info = self
+                            .registry_client
+                            .get_transport_info(node_id, version)
+                            .context("failed to get transport info")? // Result
+                            .context("transport info not available")?; // Option
+
+                        let http_endpoint =
+                            transport_info.http.context("http endpoint not available")?;
+
+                        let cert = self
+                            .registry_client
+                            .get_tls_certificate(node_id, version)
+                            .context("failed to get tls certificate")? // Result
+                            .context("tls certificate not available")?; // Option
+
+                        let node_route = Node {
+                            id: node_id.as_ref().0,
+                            addr: http_endpoint.ip_addr,
+                            port: http_endpoint.port as u16, // Port is u16 anyway
+                            tls_certificate_der: cert.certificate_der,
+                        };
+
+                        let out: Result<Node, Error> = Ok(node_route);
+                        out
+                    })
+                    .collect::<Result<Vec<Node>, Error>>()
+                    .context("unable to get nodes")?;
+
+                let ranges = ranges_by_subnet
+                    .remove(&subnet_id.as_ref().0)
+                    .context("unable to find ranges")?;
+
+                let subnet_route = Subnet {
+                    id: subnet_id.as_ref().0,
+                    subnet_type: subnet.subnet_type(),
+                    ranges,
+                    nodes,
+                };
+
+                let out: Result<Subnet, Error> = Ok(subnet_route);
+                out
+            })
+            .collect::<Result<Vec<Subnet>, Error>>()
+            .context("unable to get subnets")?;
+
+        Ok(RoutingTable {
+            registry_version: version.get(),
+            nns_subnet_id: root_subnet_id.as_ref().0,
+            subnets,
+        })
+    }
 }
 
 #[async_trait]
 impl<'a> Run for Runner<'a> {
     async fn run(&mut self) -> Result<(), Error> {
-        // similar to rs/boundary_node/control_plane/src/registry.rs, but without encoding things as `String`s
-        // "TODO(BOUN-722): Implement Routing reading routing table"
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test;
---
+++ rs/boundary_node/ic_boundary/src/snapshot/test.rs
@@ -0,0 +1,177 @@
+use super::*;
+use ic_registry_client_fake::FakeRegistryClient;
+use ic_registry_proto_data_provider::ProtoRegistryDataProvider;
+use ic_types::{CanisterId, RegistryVersion, ReplicaVersion, SubnetId};
+
+use ic_protobuf::registry::{
+    node::v1::{ConnectionEndpoint, NodeRecord, Protocol},
+    routing_table::v1::RoutingTable as PbRoutingTable,
+    subnet::v1::{SubnetListRecord, SubnetRecord},
+};
+
+use ic_registry_routing_table::{CanisterIdRange, RoutingTable};
+
+use ic_crypto_test_utils_keys::public_keys::valid_tls_certificate;
+use ic_test_utilities::types::ids::{node_test_id, subnet_test_id};
+use ic_test_utilities_registry::test_subnet_record;
+
+use ic_registry_keys::{
+    make_crypto_tls_cert_key, make_node_record_key, make_routing_table_record_key,
+    make_subnet_list_record_key, make_subnet_record_key, ROOT_SUBNET_ID_KEY,
+};
+
+// Generate a fake registry client with some data
+fn create_fake_registry_client(subnet_count: u8) -> FakeRegistryClient {
+    let mut subnets: Vec<Vec<u8>> = vec![];
+    let data_provider = ProtoRegistryDataProvider::new();
+    let reg_ver = RegistryVersion::new(1);
+
+    // Add NNS subnet
+    data_provider
+        .add(
+            ROOT_SUBNET_ID_KEY,
+            reg_ver,
+            Some(ic_types::subnet_id_into_protobuf(subnet_test_id(0))),
+        )
+        .unwrap();
+
+    // Routing table
+    let mut routing_table = RoutingTable::default();
+
+    for i in 0..subnet_count {
+        let subnet_id = subnet_test_id(1 + i as u64);
+        let node_id = node_test_id(1001 + i as u64);
+        let node_ip = format!("192.168.0.{}", 1 + i);
+
+        subnets.push(subnet_id.get().into_vec());
+
+        let mut subnet_record = test_subnet_record();
+        subnet_record.membership = vec![node_id.get().into_vec()];
+
+        // Add subnet with node
+        data_provider
+            .add(
+                &make_subnet_record_key(subnet_id),
+                reg_ver,
+                Some(subnet_record),
+            )
+            .unwrap();
+
+        // Set connection information
+        let http_endpoint = ConnectionEndpoint {
+            ip_addr: node_ip,
+            port: 8080,
+            protocol: Protocol::Http1 as i32,
+        };
+
+        data_provider
+            .add(
+                &make_node_record_key(node_id),
+                reg_ver,
+                Some(NodeRecord {
+                    http: Some(http_endpoint),
+                    ..Default::default()
+                }),
+            )
+            .unwrap();
+
+        // Add some TLS certificate
+        data_provider
+            .add(
+                &make_crypto_tls_cert_key(node_id),
+                reg_ver,
+                Some(valid_tls_certificate()),
+            )
+            .expect("failed to add TLS certificate to registry");
+
+        // Add subnet to routing table
+        let canister_range = CanisterIdRange {
+            start: CanisterId::from((i as u64) * 1_000_000),
+            end: CanisterId::from((i as u64) * 1_000_000 + 999_999),
+        };
+
+        routing_table.insert(canister_range, subnet_id).unwrap();
+    }
+
+    // Add list of subnets
+    data_provider
+        .add(
+            make_subnet_list_record_key().as_str(),
+            reg_ver,
+            Some(SubnetListRecord { subnets }),
+        )
+        .expect("Coult not add subnet list record.");
+
+    // Add routing table
+    data_provider
+        .add(
+            &make_routing_table_record_key(),
+            reg_ver,
+            Some(PbRoutingTable::from(routing_table)),
+        )
+        .unwrap();
+
+    let registry_client = FakeRegistryClient::new(Arc::new(data_provider));
+    registry_client.update_to_latest_version();
+
+    registry_client
+}
+
+#[test]
+fn test_get_routing_table() -> Result<(), Error> {
+    let reg = Arc::new(create_fake_registry_client(4));
+    let rt_init = ArcSwapOption::const_empty();
+    let mut runner = Runner::new(&rt_init, reg);
+
+    let rt = runner.get_routing_table().unwrap();
+    assert_eq!(rt.registry_version, 1);
+    assert_eq!(rt.nns_subnet_id.to_string(), "fscpm-uiaaa-aaaaa-aaaap-yai");
+    assert_eq!(rt.subnets.len(), 4);
+
+    let subnets = vec![
+        (
+            "yndj2-3ybaa-aaaaa-aaaap-yai",
+            ("rwlgt-iiaaa-aaaaa-aaaaa-cai", "chwmy-2yaaa-aaaaa-pii7q-cai"),
+        ),
+        (
+            "fbysm-3acaa-aaaaa-aaaap-yai",
+            ("jza6g-bqaaa-aaaaa-pijaa-cai", "n7ie4-qyaaa-aaaaa-6qr7q-cai"),
+        ),
+        (
+            "y6zu2-uqdaa-aaaaa-aaaap-yai",
+            ("2fehv-lqaaa-aaaaa-6qsaa-cai", "fhryv-yyaaa-aaaab-ny27q-cai"),
+        ),
+        (
+            "evxvm-kyeaa-aaaaa-aaaap-yai",
+            ("ozhkl-dqaaa-aaaab-ny3aa-cai", "v4fqt-faaaa-aaaab-5bd7q-cai"),
+        ),
+    ];
+
+    let nodes = vec![
+        ("y7s52-3xjam-aaaaa-aaaap-2ai", "192.168.0.1", 8080),
+        ("ftjgm-3pkam-aaaaa-aaaap-2ai", "192.168.0.2", 8080),
+        ("ymia2-u7lam-aaaaa-aaaap-2ai", "192.168.0.3", 8080),
+        ("ehgbm-kxmam-aaaaa-aaaap-2ai", "192.168.0.4", 8080),
+    ];
+
+    for i in 0..rt.subnets.len() {
+        let sn = &rt.subnets[i];
+        assert_eq!(sn.id.to_string(), subnets[i].0);
+
+        assert_eq!(sn.ranges.len(), 1);
+        assert_eq!(sn.ranges[0].start.to_string(), subnets[i].1 .0);
+        assert_eq!(sn.ranges[0].end.to_string(), subnets[i].1 .1);
+
+        assert_eq!(sn.nodes.len(), 1);
+        assert_eq!(sn.nodes[0].id.to_string(), nodes[i].0);
+        assert_eq!(sn.nodes[0].addr, nodes[i].1);
+        assert_eq!(sn.nodes[0].port, nodes[i].2);
+
+        assert_eq!(
+            sn.nodes[0].tls_certificate_der,
+            valid_tls_certificate().certificate_der
+        );
+    }
+
+    Ok(())
+}
