f6815019691230accb8acfdcd0cf1cc22b4bdec5
Jason (Yeli) Zhu <jason.zhu@dfinity.org>
2023-06-28 18:34:02 +0000

    NNS1-2230 Switch SNS root to ic_cdk for canister interface and inter-canister calls

--- rs/nervous_system/runtime/src/lib.rs
+++ rs/nervous_system/runtime/src/lib.rs
@@ -1,6 +1,6 @@
 use async_trait::async_trait;
 use candid::utils::{ArgumentDecoder, ArgumentEncoder};
-use ic_base_types::CanisterId;
+use ic_base_types::{CanisterId, PrincipalId};
 use std::future::Future;
 
 // A trait to help parameterize the switch from dfn_core to ic_cdk. It should
@@ -93,5 +93,53 @@ impl Runtime for DfnRuntime {
 
     fn spawn_future<F: 'static + Future<Output = ()>>(future: F) {
         dfn_core::api::futures::spawn(future);
+    }
+}
+
+pub struct CdkRuntime;
+
+#[async_trait]
+impl Runtime for CdkRuntime {
+    // This method does not do clean up.
+    async fn call_without_cleanup<In, Out>(
+        _id: CanisterId,
+        _method: &str,
+        _args: In,
+    ) -> Result<Out, (i32, String)>
+    where
+        In: ArgumentEncoder + Send,
+        Out: for<'a> ArgumentDecoder<'a>,
+    {
+        unimplemented!("There is no implementation of CdkRuntime that does not cleanup");
+    }
+
+    async fn call_with_cleanup<In, Out>(
+        id: CanisterId,
+        method: &str,
+        args: In,
+    ) -> Result<Out, (i32, String)>
+    where
+        In: ArgumentEncoder + Send,
+        Out: for<'a> ArgumentDecoder<'a>,
+    {
+        let principal_id = PrincipalId::from(id);
+        ic_cdk::api::call::call(principal_id.into(), method, args)
+            .await
+            .map_err(|(code, msg)| (code as i32, msg))
+    }
+
+    async fn call_bytes_with_cleanup(
+        id: CanisterId,
+        method: &str,
+        args: &[u8],
+    ) -> Result<Vec<u8>, (i32, String)> {
+        let principal_id = PrincipalId::from(id);
+        ic_cdk::api::call::call_raw(principal_id.into(), method, args, 0)
+            .await
+            .map_err(|(code, msg)| (code as i32, msg))
+    }
+
+    fn spawn_future<F: 'static + Future<Output = ()>>(future: F) {
+        ic_cdk::spawn(future);
     }
 }
