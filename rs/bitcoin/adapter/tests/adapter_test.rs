7b9362c865aa7d9346227937c8eab6d1d1600bb2
Stefan Neamtu <stefan.neamtu@dfinity.org>
2023-07-11 12:42:51 +0000

    test[NET-1159]: bitcoin adapter transaction tests

--- rs/bitcoin/adapter/tests/adapter_test.rs
+++ rs/bitcoin/adapter/tests/adapter_test.rs
@@ -1,5 +1,5 @@
-use bitcoin::{consensus::encode::deserialize, Block};
-use bitcoincore_rpc::{Auth, Client, RpcApi};
+use bitcoin::{consensus::encode::deserialize, Address, Amount, Block};
+use bitcoincore_rpc::{bitcoincore_rpc_json::CreateRawTransactionInput, Auth, Client, RpcApi};
 use bitcoind::{BitcoinD, Conf, P2P};
 use ic_btc_adapter::{
     config::{Config, IncomingSource},
@@ -9,13 +9,18 @@ use ic_btc_adapter_client::setup_bitcoin_adapter_clients;
 use ic_btc_interface::Network;
 use ic_btc_types_internal::{
     BitcoinAdapterRequestWrapper, BitcoinAdapterResponseWrapper, GetSuccessorsRequestInitial,
+    SendTransactionRequest,
 };
 use ic_config::adapters::AdaptersConfig;
 use ic_interfaces_adapter_client::{Options, RpcAdapterClient, RpcError};
 use ic_logger::{replica_logger::no_op_logger, ReplicaLogger};
 use ic_metrics::MetricsRegistry;
-use std::net::{SocketAddr, SocketAddrV4};
-use std::path::Path;
+use std::{
+    collections::HashMap,
+    net::{SocketAddr, SocketAddrV4},
+    path::Path,
+    str::FromStr,
+};
 use tempfile::{Builder, TempPath};
 use tokio::runtime::Runtime;
 
@@ -45,6 +50,19 @@ impl AdapterClientWrapper {
                 network: Network::Regtest,
                 anchor,
                 processed_block_hashes: headers,
+            });
+
+        self.0.send_blocking(request, Options::default())
+    }
+
+    fn send_tx(
+        &self,
+        raw_tx: &[u8],
+    ) -> Result<BitcoinAdapterResponseWrapper, ic_interfaces_adapter_client::RpcError> {
+        let request =
+            BitcoinAdapterRequestWrapper::SendTransactionRequest(SendTransactionRequest {
+                network: Network::Regtest,
+                transaction: raw_tx.to_vec(),
             });
 
         self.0.send_blocking(request, Options::default())
@@ -197,8 +215,62 @@ fn start_adapter_and_client(
         .into_parts()
 }
 
+fn get_blackhole_address() -> Address {
+    Address::from_str("mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn").unwrap()
+}
+
+fn create_alice_and_bob_wallets(bitcoind: &BitcoinD) -> (Client, Client, Address, Address) {
+    let alice_client = Client::new(
+        format!("{}/wallet/{}", bitcoind.rpc_url(), "alice").as_str(),
+        Auth::CookieFile(bitcoind.params.cookie_file.clone()),
+    )
+    .unwrap();
+    alice_client
+        .create_wallet("alice", None, None, None, None)
+        .unwrap();
+
+    let bob_client = Client::new(
+        format!("{}/wallet/{}", bitcoind.rpc_url(), "bob").as_str(),
+        Auth::CookieFile(bitcoind.params.cookie_file.clone()),
+    )
+    .unwrap();
+    bob_client
+        .create_wallet("bob", None, None, None, None)
+        .unwrap();
+
+    let alice_address = alice_client.get_new_address(None, None).unwrap();
+    let bob_address = bob_client.get_new_address(None, None).unwrap();
+
+    (alice_client, bob_client, alice_address, bob_address)
+}
+
+fn fund_with_btc(to_fund_client: &Client, to_fund_address: &Address) {
+    let initial_amount = to_fund_client
+        .get_received_by_address(to_fund_address, Some(0))
+        .unwrap()
+        .as_btc();
+
+    to_fund_client
+        .generate_to_address(1, to_fund_address)
+        .unwrap();
+
+    // Generate 100 blocks for coinbase maturity
+    to_fund_client
+        .generate_to_address(100, &get_blackhole_address())
+        .unwrap();
+
+    // The reward for mining a block is 50 bitcoins
+    assert_eq!(
+        to_fund_client
+            .get_received_by_address(to_fund_address, Some(0))
+            .unwrap(),
+        Amount::from_btc(initial_amount + 50.0).unwrap()
+    );
+}
+
+/// Checks that the client (replica) receives the mined blocks using the gRPC service.
 #[test]
-fn test_adapter_receives_blocks() {
+fn test_receives_blocks() {
     let logger = no_op_logger();
     let bitcoind = get_default_bitcoind();
     let client = Client::new(
@@ -224,4 +296,130 @@ fn test_adapter_receives_blocks() {
     let blocks = sync_until_end_block(&adapter_client, &client, 0, &mut vec![]);
 
     assert_eq!(blocks.len(), 150);
+}
+
+/// The client (replica) receives newly created transactions by 3rd parties using the gRPC service.
+#[test]
+fn test_receives_new_3rd_party_txs() {
+    let logger = no_op_logger();
+    let bitcoind = get_default_bitcoind();
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let (adapter_client, _path) = start_adapter_and_client(
+        &rt,
+        vec![SocketAddr::V4(get_bitcoind_url(&bitcoind).unwrap())],
+        logger,
+    );
+
+    let (alice_client, bob_client, alice_address, bob_address) =
+        create_alice_and_bob_wallets(&bitcoind);
+
+    fund_with_btc(&alice_client, &alice_address);
+
+    assert_eq!(101, alice_client.get_blockchain_info().unwrap().blocks);
+    let txid = alice_client
+        .send_to_address(
+            &bob_address,
+            Amount::from_btc(1.0).unwrap(),
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        )
+        .expect("Failed to send to Bob");
+    assert_eq!(101, alice_client.get_blockchain_info().unwrap().blocks);
+    alice_client
+        .generate_to_address(1, &get_blackhole_address())
+        .unwrap();
+    assert_eq!(102, alice_client.get_blockchain_info().unwrap().blocks);
+
+    let alice_balance = alice_client.get_balance(None, None).unwrap();
+
+    // Take the tx fee into consideration
+    assert!(
+        alice_balance < Amount::from_btc(49.0).unwrap()
+            && alice_balance > Amount::from_btc(48.999).unwrap()
+    );
+    assert_eq!(
+        bob_client.get_balance(None, None).unwrap(),
+        Amount::from_btc(1.0).unwrap()
+    );
+
+    let blocks = sync_until_end_block(&adapter_client, &alice_client, 101, &mut vec![]);
+
+    assert_eq!(blocks.len(), 1);
+    assert!(blocks[0].txdata.iter().any(|tx| tx.txid() == txid));
+}
+
+/// Ensures the client (replica) can send a transaction (1 BTC from Alice to Bob) using the gRPC service.
+#[test]
+fn test_send_tx() {
+    let logger = no_op_logger();
+    let bitcoind = get_default_bitcoind();
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    let (adapter_client, _path) = start_adapter_and_client(
+        &rt,
+        vec![SocketAddr::V4(get_bitcoind_url(&bitcoind).unwrap())],
+        logger,
+    );
+
+    let (alice_client, bob_client, alice_address, bob_address) =
+        create_alice_and_bob_wallets(&bitcoind);
+
+    fund_with_btc(&alice_client, &alice_address);
+
+    let to_send = Amount::from_btc(1.0).unwrap();
+    let tx_fee = Amount::from_btc(0.001).unwrap();
+
+    let unspent = alice_client
+        .list_unspent(None, None, None, None, None)
+        .unwrap();
+    let utxo = unspent
+        .iter()
+        .find(|utxo| utxo.amount > to_send + tx_fee)
+        .expect("Not enough BTC in Alice's wallet");
+
+    let raw_tx_input = CreateRawTransactionInput {
+        txid: utxo.txid,
+        vout: utxo.vout,
+        sequence: None,
+    };
+
+    let mut outs = HashMap::new();
+    let change = utxo.amount - to_send - tx_fee;
+    outs.insert(bob_address.to_string(), to_send);
+    if change > Amount::from_btc(0.0).unwrap() {
+        outs.insert(alice_address.to_string(), change);
+    }
+
+    let raw_tx = alice_client
+        .create_raw_transaction(&[raw_tx_input], &outs, None, Some(true))
+        .expect("Failed to create raw transaction");
+
+    let signed_tx = alice_client
+        .sign_raw_transaction_with_wallet(&raw_tx, None, None)
+        .unwrap();
+
+    let res = adapter_client.send_tx(&signed_tx.hex);
+
+    let mut tries = 0;
+    while tries < 5
+        && bob_client.get_balances().unwrap().mine.untrusted_pending
+            == Amount::from_btc(0.0).unwrap()
+    {
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        tries += 1;
+    }
+
+    if let BitcoinAdapterResponseWrapper::SendTransactionResponse(_) = res.unwrap() {
+        assert_eq!(
+            bob_client.get_balances().unwrap().mine.untrusted_pending,
+            Amount::from_btc(1.0).unwrap()
+        );
+    } else {
+        panic!("Failed to send transaction");
+    }
 }
