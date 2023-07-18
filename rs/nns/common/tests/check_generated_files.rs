1ff338e53c1a1e44b1175c1c975a09fbf230878b
Elias Datler <elias.datler@dfinity.org>
2023-07-18 16:28:17 +0000

    chore(VER-2413): Update `check_generated_files` command

--- rs/nns/common/tests/check_generated_files.rs
+++ rs/nns/common/tests/check_generated_files.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 
 #[test]
 fn check_generated_files() {
-    let cmd = "cargo run --bin ic-nns-common-protobuf-generator";
+    let cmd = "bazel run //rs/nns/common/protobuf_generator:protobuf_generator";
 
     let manifest_dir = PathBuf::from(
         std::env::var("CARGO_MANIFEST_DIR")
