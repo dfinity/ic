4cb76599ba5715c51a52ed674591b0edf2a7bc8f
Eero Kelly <eero.kelly@dfinity.org>
2023-07-28 21:58:40 +0000

    Improve POC k8s testnet tools

--- testnet/tools/k8s/k8s_config.sh
+++ testnet/tools/k8s/k8s_config.sh
@@ -7,147 +7,128 @@ set -eEuo pipefail
 if (($# < 4)); then
     echo "Usage: k8s_config.sh <replica_version> <nns_ip> <app_ip> <out>"
     echo "  <replica_version>: The version deployed."
-    echo "  <nns_ip>: IP of the pod hosting the NNS node."
-    echo "  <app_ip>: IP of the pod hosting the app node."
+    echo "  <nns_ips>: IPs of pods hosting NNS nodes. (Separated by spaces)"
+    echo "  <app_ips>: IPs of pods hosting app nodes. (Separated by spaces)"
     echo "  <ip_prefix>: IP prefix to whitelist in the initial firewall."
     echo "  <out>: Folder to contain all script outputs."
     exit 1
 fi
 
 REPLICA_VERSION=$1
-NNS_IP=$2
-APP_IP=$3
+NNS_IPS=$2
+APP_IPS=$3
 IP_PREFIX=$4
 OUT=$5
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 TEMPDIR=$(mktemp -d /tmp/k8s_deploy.sh.XXXXXXXXXX)
 trap "rm -rf ${TEMPDIR}" exit
-
-##############################################################################
-echo "Building out folder structure..."
-##############################################################################
-rm -rf "${OUT}"
-mkdir -p "${OUT}"
-NNS_CONFIG_DIR="${TEMPDIR}/nns"
-APP_CONFIG_DIR="${TEMPDIR}/app"
 IC_PREP_DIR="${TEMPDIR}/prep"
 BIN_DIR="${TEMPDIR}/bin"
-INIT_DIR="${OUT}/init"
-NNS_OUT_DIR="${OUT}/nns"
-APP_OUT_DIR="${OUT}/app"
 
-mkdir ${NNS_CONFIG_DIR}
-mkdir ${APP_CONFIG_DIR}
-mkdir ${IC_PREP_DIR}
-mkdir ${BIN_DIR}
-mkdir ${INIT_DIR}
-mkdir ${NNS_OUT_DIR}
-mkdir ${APP_OUT_DIR}
+function create_directories() {
+    ##############################################################################
+    echo "Building out directory structure..."
+    ##############################################################################
+    rm -rf "${OUT}"
+    mkdir -p "${OUT}"
+    mkdir ${IC_PREP_DIR}
+    mkdir ${BIN_DIR}
+}
 
-##############################################################################
-echo "Downloading tools..."
-##############################################################################
-rclone --config="${REPO_ROOT}"/.rclone-anon.conf copy "public-s3:dfinity-download-public/ic/${REPLICA_VERSION}/release/ic-prep.gz" "${BIN_DIR}"
-rclone --config="${REPO_ROOT}"/.rclone-anon.conf copy "public-s3:dfinity-download-public/ic/${REPLICA_VERSION}/release/ic-nns-init.gz" "${BIN_DIR}"
+function download_tools() {
+    ##############################################################################
+    echo "Downloading tools..."
+    ##############################################################################
 
-find "${BIN_DIR}/" -name "*.gz" -print0 | xargs -P100 -0I{} bash -c "gunzip -f {} && basename {} .gz | xargs -I[] chmod +x ${BIN_DIR}/[]"
+    rclone --config="${REPO_ROOT}"/.rclone-anon.conf copy "public-s3:dfinity-download-public/ic/${REPLICA_VERSION}/release/ic-prep.gz" "${BIN_DIR}"
+    gunzip -f "${BIN_DIR}/ic-prep.gz"
+    chmod +x "${BIN_DIR}/ic-prep"
+}
 
-##############################################################################
-echo "Running ic-prep..."
-##############################################################################
-SSH="${REPO_ROOT}/testnet/tools/k8s/ssh_authorized_keys"
-WHITELIST_FILE="${REPO_ROOT}/testnet/tools/k8s/provisional_whitelist.json"
-# The principal id below is the one corresponding to the hardcoded key in
-# ic_test_utilities::identity::TEST_IDENTITY_KEYPAIR. We do not have access to it
-# in this script, so hardcode the corresponding principal instead.
-#
-# It is used for both the node operator and its corresponding provider.
-NODE_OPERATOR_ID="5o66h-77qch-43oup-7aaui-kz5ty-tww4j-t2wmx-e3lym-cbtct-l3gpw-wae"
-"${BIN_DIR}/ic-prep" \
-    "--working-dir" "${IC_PREP_DIR}" \
-    "--replica-version" "${REPLICA_VERSION}" \
-    "--nns-subnet-index" "0" \
-    "--p2p-flows" "1234-1" \
-    "--node" "idx:0,subnet_idx:0,p2p_addr:\"org.internetcomputer.p2p1://[${NNS_IP}]:4100\",xnet_api:\"http://[${NNS_IP}]:2497\",public_api:\"http://[${NNS_IP}]:8080\"" \
-    "--node" "idx:1,subnet_idx:1,p2p_addr:\"org.internetcomputer.p2p1://[${APP_IP}]:4100\",xnet_api:\"http://[${APP_IP}]:2497\",public_api:\"http://[${APP_IP}]:8080\"" \
-    "--provisional-whitelist" "${WHITELIST_FILE}" \
-    "--initial-node-operator" "${NODE_OPERATOR_ID}" \
-    "--initial-node-provider" "${NODE_OPERATOR_ID}" \
-    "--whitelisted-prefixes" "${IP_PREFIX}"
+function ic_prep() {
+    ##############################################################################
+    echo "Running ic-prep..."
+    ##############################################################################
 
-##############################################################################
-echo "Building config folders..."
-##############################################################################
-NNS_URL=("http://[${NNS_IP}]:8080")
+    local nns_ips=(${NNS_IPS})
+    local app_ips=(${APP_IPS})
 
-# Build NNS Node config
-# -------------------------------------------------------------------------
+    local node_definitions=()
+    local node_index=0
+    for ip in "${nns_ips[@]}"; do
+        node_definitions+=("--node")
+        node_definitions+=("idx:$((node_index++)),subnet_idx:0,p2p_addr:\"org.internetcomputer.p2p1://[${ip}]:4100\",xnet_api:\"http://[${ip}]:2497\",public_api:\"http://[${ip}]:8080\"")
+    done
 
-# Copy registry to NNS node
-cp -r "${IC_PREP_DIR}/ic_registry_local_store" "${NNS_CONFIG_DIR}"
+    for ip in "${app_ips[@]}"; do
+        node_definitions+=("--node")
+        node_definitions+=("idx:$((node_index++)),subnet_idx:1,p2p_addr:\"org.internetcomputer.p2p1://[${ip}]:4100\",xnet_api:\"http://[${ip}]:2497\",public_api:\"http://[${ip}]:8080\"")
+    done
 
-# Copy crypto material
-cp -r "${IC_PREP_DIR}/node-0/crypto/" "${NNS_CONFIG_DIR}/ic_crypto/"
-cp "${IC_PREP_DIR}/nns_public_key.pem" "${NNS_CONFIG_DIR}/nns_public_key.pem"
-echo "nns_url=${NNS_URL}" >"${NNS_CONFIG_DIR}/nns.conf"
+    # Allow any principal to create canisters on any subnet
+    local whitelist_file="${REPO_ROOT}/testnet/tools/k8s/provisional_whitelist.json"
 
-# Generate Network config
-echo "hostname=testnet-1" >"${NNS_CONFIG_DIR}/network.conf"
-echo "name_servers_fallback=2606:4700:4700::1111 2606:4700:4700::1001" >>"${NNS_CONFIG_DIR}/network.conf"
+    # The principal id below is the one corresponding to the hardcoded key in
+    # ic_test_utilities::identity::TEST_IDENTITY_KEYPAIR. We do not have access to it
+    # in this script, so hardcode the corresponding principal instead.
+    #
+    # It is used for both the node operator and its corresponding provider.
+    NODE_OPERATOR_ID="5o66h-77qch-43oup-7aaui-kz5ty-tww4j-t2wmx-e3lym-cbtct-l3gpw-wae"
+    "${BIN_DIR}/ic-prep" \
+        "--working-dir" "${IC_PREP_DIR}" \
+        "--replica-version" "${REPLICA_VERSION}" \
+        "${node_definitions[@]}" \
+        "--p2p-flows" "1234-1" \
+        "--provisional-whitelist" "${whitelist_file}" \
+        "--initial-node-operator" "${NODE_OPERATOR_ID}" \
+        "--initial-node-provider" "${NODE_OPERATOR_ID}" \
+        "--whitelisted-prefixes" "${IP_PREFIX}"
 
-# Generate Journalbeat config
-echo "journalbeat_hosts=elasticsearch-node-0.testnet.dfinity.systems:443 elasticsearch-node-1.testnet.dfinity.systems:443 elasticsearch-node-2.testnet.dfinity.systems:443" >"${NNS_CONFIG_DIR}/journalbeat.conf"
+    cp -r "${IC_PREP_DIR}/ic_registry_local_store" "${OUT}"
+}
 
-# Copy SSH Keys
-cp -Lr "${SSH}" "${NNS_CONFIG_DIR}/accounts_ssh_authorized_keys"
+function build_config() {
+    ##############################################################################
+    echo "Building config folders..."
+    ##############################################################################
 
-# Build App Node config
-# -------------------------------------------------------------------------
+    local nns_ips=(${NNS_IPS})
+    local app_ips=(${APP_IPS})
 
-# Copy crypto material
-cp -r "${IC_PREP_DIR}/node-1/crypto/" "${APP_CONFIG_DIR}/ic_crypto/"
-cp "${IC_PREP_DIR}/nns_public_key.pem" "${APP_CONFIG_DIR}/nns_public_key.pem"
-echo "nns_url=${NNS_URL}" >"${APP_CONFIG_DIR}/nns.conf"
+    local nns_urls=()
+    for ip in "${nns_ips[@]}"; do
+        nns_urls+=("http://[${ip}]:8080")
+    done
 
-# Generate Network config
-echo "hostname=testnet-2" >"${APP_CONFIG_DIR}/network.conf"
-echo "name_servers_fallback=2606:4700:4700::1111 2606:4700:4700::1001" >>"${APP_CONFIG_DIR}/network.conf"
+    local ssh="${REPO_ROOT}/testnet/tools/k8s/ssh_authorized_keys"
 
-# Generate Journalbeat config
-echo "journalbeat_hosts=elasticsearch-node-0.testnet.dfinity.systems:443 elasticsearch-node-1.testnet.dfinity.systems:443 elasticsearch-node-2.testnet.dfinity.systems:443" >"${APP_CONFIG_DIR}/journalbeat.conf"
+    local node_index=0
+    for ip in "${nns_ips[@]}"; do
+        "${REPO_ROOT}/ic-os/scripts/build-bootstrap-config-image.sh" \
+            "${OUT}/bootstrap-${node_index}.img" \
+            "--ic_registry_local_store" "${IC_PREP_DIR}/ic_registry_local_store" \
+            "--ic_crypto" "${IC_PREP_DIR}/node-${node_index}/crypto" \
+            "--nns_public_key" "${IC_PREP_DIR}/nns_public_key.pem" \
+            "--nns_url" "${nns_urls[*]}" \
+            "--hostname" "testnet-$((node_index++))" \
+            "--journalbeat_hosts" "elasticsearch-node-0.testnet.dfinity.systems:443 elasticsearch-node-1.testnet.dfinity.systems:443 elasticsearch-node-2.testnet.dfinity.systems:443" \
+            "--accounts_ssh_authorized_keys" "${ssh}"
+    done
 
-# Copy SSH Keys
-cp -Lr "${SSH}" "${APP_CONFIG_DIR}/accounts_ssh_authorized_keys"
+    for ip in "${app_ips[@]}"; do
+        "${REPO_ROOT}/ic-os/scripts/build-bootstrap-config-image.sh" \
+            "${OUT}/bootstrap-${node_index}.img" \
+            "--ic_crypto" "${IC_PREP_DIR}/node-${node_index}/crypto" \
+            "--nns_public_key" "${IC_PREP_DIR}/nns_public_key.pem" \
+            "--nns_url" "${nns_urls[*]}" \
+            "--hostname" "testnet-$((node_index++))" \
+            "--journalbeat_hosts" "elasticsearch-node-0.testnet.dfinity.systems:443 elasticsearch-node-1.testnet.dfinity.systems:443 elasticsearch-node-2.testnet.dfinity.systems:443" \
+            "--accounts_ssh_authorized_keys" "${ssh}"
+    done
+}
 
-##############################################################################
-echo "Assembling config images..."
-##############################################################################
-
-tar cf "${NNS_OUT_DIR}/ic-bootstrap.tar" -C "${NNS_CONFIG_DIR}" .
-tar cf "${APP_OUT_DIR}/ic-bootstrap.tar" -C "${APP_CONFIG_DIR}" .
-
-truncate --size 4M "${NNS_OUT_DIR}/bootstrap.img"
-mkfs.vfat -n CONFIG "${NNS_OUT_DIR}/bootstrap.img"
-mcopy -i "${NNS_OUT_DIR}/bootstrap.img" -o -s "${NNS_OUT_DIR}/ic-bootstrap.tar" ::
-truncate --size 4M "${APP_OUT_DIR}/bootstrap.img"
-mkfs.vfat -n CONFIG "${APP_OUT_DIR}/bootstrap.img"
-mcopy -i "${APP_OUT_DIR}/bootstrap.img" -o -s "${APP_OUT_DIR}/ic-bootstrap.tar" ::
-
-# NOTE: We produce "raw" config, as well as virtual disk. Which will be used
-# will depend on upload method.
-
-##############################################################################
-echo "Packing all other outputs..."
-##############################################################################
-# Set up canisters, nns-init, registry state
-rclone --config="${REPO_ROOT}"/.rclone-anon.conf --include '*' copyto "public-s3:dfinity-download-public/ic/${REPLICA_VERSION}/canisters" "${INIT_DIR}/canisters"
-
-find "${INIT_DIR}/canisters/" -name "*.gz" -print0 | xargs -P100 -0I{} bash -c "gunzip -f {}"
-
-cp "${BIN_DIR}/ic-nns-init" "${INIT_DIR}"
-cp -r "${IC_PREP_DIR}/ic_registry_local_store" "${INIT_DIR}"
-
-echo
-echo "To start the NNS, run:"
-echo "ic-nns-init --url 'http://[${NNS_IP}]:8080' --registry-local-store-dir ic_registry_local_store --wasm-dir canisters"
+create_directories
+download_tools
+ic_prep
+build_config
--- testnet/tools/k8s/k8s_testnet.sh
+++ testnet/tools/k8s/k8s_testnet.sh
@@ -8,12 +8,15 @@ if (($# < 2)); then
     echo "Usage: $0 <name> <version>"
     echo "  <name>:    TestNet name."
     echo "  <version>: TestNet version (git revision)."
+    echo "  <nns_size>: Number of NNS nodes to include in subnet."
+    echo "  <app_size>: Number of APP nodes to include in subnet."
     exit 1
 fi
 
 export NAME=$1
 export VERSION=$2
-export SIZE=2
+export NNS_SIZE=$3
+export APP_SIZE=$4
 
 if [ "${CIDR_RESERVATION:-}" == "" ]; then
     echo "CIDR_RESERVATION env var expected!" && exit 1
@@ -35,30 +38,42 @@ echo "Creating IPReservation"
 envsubst <template-ipreservation.yaml >"$OUTDIR/ipreservation.yaml"
 kubectl apply -f "$OUTDIR/ipreservation.yaml"
 
-IPLIST=(
-    "${CIDR_PREFIX}10"
-    "${CIDR_PREFIX}11"
-)
+NNS_IPS=()
+for ((i = 0; i < NNS_SIZE; i++)); do
+    NNS_IPS+=("${CIDR_PREFIX}0${i}")
+done
+
+APP_IPS=()
+for ((i = 0; i < APP_SIZE; i++)); do
+    APP_IPS+=("${CIDR_PREFIX}1${i}")
+done
 
 IP_PREFIX="fda6:8d22:43e1::/48"
 #########################################################################################
 echo "Creating K8s config volumes..."
 #########################################################################################
-#TODO: support k8s_config.sh with more then 2 nodes
-./k8s_config.sh "$VERSION" "${IPLIST[0]}" "${IPLIST[1]}" "${IP_PREFIX}" "$OUTDIR"
 
-for ((i = 0; i < SIZE; i++)); do
-    export NODE_NAME="$NAME-$i"
+./k8s_config.sh "$VERSION" "${NNS_IPS[@]}" "${APP_IPS[@]}" "${IP_PREFIX}" "$OUTDIR"
 
-    DIR="app"
-    if [ "$i" == "0" ]; then
-        DIR="nns"
-    fi
+NODE_INDEX=0
+for ((i = 0; i < NNS_SIZE; i++)); do
+    NODE_NAME="$NAME-${NODE_INDEX}"
 
     set -x
     virtctl -n "$NAMESPACE" image-upload dv "${NODE_NAME}-config" \
         --uploadproxy-url https://cdi-uploadproxy.sf1-idx1.dfinity.network \
-        --image-path "$OUTDIR/$DIR/bootstrap.img" \
+        --image-path "$OUTDIR/bootstrap-$((NODE_INDEX++)).img" \
+        --size=10Mi
+    set +x
+done
+
+for ((i = 0; i < APP_SIZE; i++)); do
+    NODE_NAME="$NAME-${NODE_INDEX}"
+
+    set -x
+    virtctl -n "$NAMESPACE" image-upload dv "${NODE_NAME}-config" \
+        --uploadproxy-url https://cdi-uploadproxy.sf1-idx1.dfinity.network \
+        --image-path "$OUTDIR/bootstrap-$((NODE_INDEX++)).img" \
         --size=10Mi
     set +x
 done
@@ -66,9 +81,20 @@ done
 #########################################################################################
 echo "Creating K8s VMs..."
 #########################################################################################
-for ((i = 0; i < SIZE; i++)); do
-    export NODE_NAME="$NAME-$i"
-    export IPV6="${IPLIST[$i]}"
+
+NODE_INDEX=0
+for ((i = 0; i < NNS_SIZE; i++)); do
+    export NODE_NAME="$NAME-$((NODE_INDEX++))"
+    export IPV6="${NNS_IPS[$i]}"
+
+    envsubst <template-vm.yaml >"$OUTDIR/$NODE_NAME.yaml"
+    kubectl apply -f "$OUTDIR/$NODE_NAME.yaml"
+done
+
+for ((i = 0; i < APP_SIZE; i++)); do
+    export NODE_NAME="$NAME-$((NODE_INDEX++))"
+    export IPV6="${APP_IPS[$i]}"
+
     envsubst <template-vm.yaml >"$OUTDIR/$NODE_NAME.yaml"
     kubectl apply -f "$OUTDIR/$NODE_NAME.yaml"
 done
@@ -76,8 +102,24 @@ done
 #########################################################################################
 echo "Doing ic-nns-init..."
 #########################################################################################
+
 pushd "$OUTDIR"
+
+# Pack up registry state, canisters, and init binary
+mkdir init
+
+cp -r ic_registry_local_store init/
+
+rclone --config="${REPO_ROOT}"/.rclone-anon.conf --include '*' copyto "public-s3:dfinity-download-public/ic/${VERSION}/canisters" "init/canisters"
+find "init/canisters/" -name "*.gz" -print0 | xargs -P100 -0I{} bash -c "gunzip -f {}"
+
+rclone --config="${REPO_ROOT}"/.rclone-anon.conf copy "public-s3:dfinity-download-public/ic/${VERSION}/release/ic-nns-init.gz" "init/"
+gunzip -f "init/ic-nns-init.gz"
+chmod +x "init/ic-nns-init"
+
 tar -cf init.tar init
+
+# Upload to init pod
 set -x
 virtctl -n "$NAMESPACE" image-upload dv "${NAME}-init" \
     --uploadproxy-url https://cdi-uploadproxy.sf1-idx1.dfinity.network \
@@ -85,9 +127,9 @@ virtctl -n "$NAMESPACE" image-upload dv "${NAME}-init" \
 set +x
 popd
 
-export NNS_IP="${IPLIST[0]}"
+export NNS_IP="${NNS_IPS[0]}"
 envsubst <template-init-job.yaml >"${OUTDIR}/${NAME}-init.yaml"
 kubectl apply -f "${OUTDIR}/${NAME}-init.yaml"
 
-echo "NNS Node IP: ${IPLIST[0]}"
-echo "APP Node IP: ${IPLIST[1]}"
+echo "NNS Node IPs: ${NNS_IPS[@]}"
+echo "APP Node IPs: ${APP_IPS[@]}"
