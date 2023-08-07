ca4fd8d1e89a23dec10ce3261a7625173fb87fcc
Jack Lloyd <jack.lloyd@dfinity.org>
2023-08-07 13:15:22 +0000

    fix(crypto): Remove duplicated Polynomial type

--- rs/crypto/internal/crypto_lib/bls12_381/vetkd/tests/tests.rs
+++ rs/crypto/internal/crypto_lib/bls12_381/vetkd/tests/tests.rs
@@ -1,79 +1,6 @@
+use ic_crypto_internal_bls12_381_type::Polynomial;
 use ic_crypto_internal_bls12_381_vetkd::*;
 use ic_crypto_test_utils_reproducible_rng::reproducible_rng;
-use rand::{CryptoRng, RngCore};
-
-/// A Polynomial whose coefficients are scalars in an elliptic curve group
-///
-/// The coefficients are stored in little-endian ordering, ie a_0 is
-/// self.coefficients\[0\]
-#[derive(Clone, Debug)]
-pub struct Polynomial {
-    coefficients: Vec<Scalar>,
-}
-
-impl Eq for Polynomial {}
-
-impl PartialEq for Polynomial {
-    fn eq(&self, other: &Self) -> bool {
-        // Accept leading zero elements
-        let max_coef = std::cmp::max(self.coefficients.len(), other.coefficients.len());
-
-        for i in 0..max_coef {
-            if self.coeff(i) != other.coeff(i) {
-                return false;
-            }
-        }
-
-        true
-    }
-}
-
-impl Polynomial {
-    pub fn new(coefficients: Vec<Scalar>) -> Self {
-        Self { coefficients }
-    }
-
-    /// Returns the polynomial with constant value `0`.
-    pub fn zero() -> Self {
-        Self::new(vec![])
-    }
-
-    /// Creates a random polynomial with the specified number of coefficients
-    fn random<R: CryptoRng + RngCore>(num_coefficients: usize, rng: &mut R) -> Self {
-        let mut coefficients = Vec::with_capacity(num_coefficients);
-
-        for _ in 0..num_coefficients {
-            coefficients.push(Scalar::random(rng))
-        }
-
-        Self { coefficients }
-    }
-
-    fn coeff(&self, idx: usize) -> Scalar {
-        match self.coefficients.get(idx) {
-            Some(s) => s.clone(),
-            None => Scalar::zero(),
-        }
-    }
-
-    fn evaluate_at(&self, x: &Scalar) -> Scalar {
-        if self.coefficients.is_empty() {
-            return Scalar::zero();
-        }
-
-        let mut coefficients = self.coefficients.iter().rev();
-        let mut ans = coefficients
-            .next()
-            .expect("Iterator was unexpectedly empty")
-            .clone();
-
-        for coeff in coefficients {
-            ans *= x;
-            ans += coeff;
-        }
-        ans
-    }
-}
 
 #[test]
 fn should_encrypted_key_share_be_functional() {
@@ -92,7 +19,7 @@ fn should_encrypted_key_share_be_functional() {
     //let (tpk, tsk) = transport_keygen(&mut rng);
 
     let master_sk = poly.coeff(0);
-    let master_pk = G2Affine::from(G2Affine::generator() * &master_sk);
+    let master_pk = G2Affine::from(G2Affine::generator() * master_sk);
 
     let dpk = DerivedPublicKey::compute_derived_key(&master_pk, &derivation_path);
 
