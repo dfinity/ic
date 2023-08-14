8c9ce9ef5fe1650d26151308f195a045821ca391
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2023-08-14 15:11:37 +0000

    chore(fuzzing): Include encoding roundtrip for candid fuzzers

--- rs/types/ic00_types/fuzz/fuzz_targets/decode_install_code_args.rs
+++ rs/types/ic00_types/fuzz/fuzz_targets/decode_install_code_args.rs
@@ -1,4 +1,4 @@
-use candid::Decode;
+use candid::{Decode, Encode};
 use ic_ic00_types::InstallCodeArgs;
 
 // This fuzz test feeds binary data to Candid's `Decode!` macro for InstallCodeArgs with the goal of exposing panics
@@ -6,8 +6,11 @@ use ic_ic00_types::InstallCodeArgs;
 
 pub fn do_fuzz_task(data: &[u8]) {
     let payload = data.to_vec();
-    let _decoded = match Decode!(payload.as_slice(), InstallCodeArgs) {
-        Ok(_v) => _v,
-        Err(_e) => return,
+    match Decode!(payload.as_slice(), InstallCodeArgs) {
+        Ok(install_code_args) => {
+            let encoded = Encode!(&install_code_args).unwrap();
+            assert_eq!(&encoded[..], data);
+        }
+        Err(_e) => (),
     };
 }
