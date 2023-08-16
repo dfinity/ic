ddc0d59ae6f331f9d8579b27e10594f6e7ecb2c0
Daniel Wong <daniel.wong@dfinity.org>
2023-08-16 20:14:33 +0000

    feat: NNS1-2488: Added update, delete, and upsert to ic_nns_governance::storage::neurons::Store.

--- rs/nns/governance/src/storage/neurons.rs
+++ rs/nns/governance/src/storage/neurons.rs
@@ -1,25 +1,27 @@
 #![allow(unused)] // TODO(NNS1-2443): Re-enable clippy once we start actually using this code.
 
-use crate::pb::v1::{governance_error::ErrorType, GovernanceError, Neuron};
+use crate::pb::v1::{
+    governance_error::ErrorType, neuron::Followees, BallotInfo, GovernanceError, KnownNeuronData,
+    Neuron, NeuronStakeTransfer,
+};
 use bytes::Buf;
+use ic_base_types::PrincipalId;
 use ic_nns_common::pb::v1::NeuronId;
 use ic_stable_structures::{BoundedStorable, StableBTreeMap, Storable, VectorMemory};
 use prost::Message;
-use std::borrow::Cow;
+use std::{borrow::Cow, collections::HashMap};
 
-// Because many arguments are needed to construct a Store, there is no natural
-// argument order that Store::new would be able to follow. Therefore,
-// constructing a Store is done like so:
+// Because many arguments are needed to construct a StableNeuronStore, there is
+// no natural argument order that StableNeuronStore::new would be able to
+// follow. Therefore, constructing a StableNeuronStore is done like so:
 //
-// ```
-// let neurons_store: neurons::Store = neurons::StoreBuilder {
-//     main: new_memory(...),
-//     hot_keys: etc,
-//     ...
-// }
-// .build()
-// ```
-pub(crate) struct StoreBuilder<Memory> {
+//     let stable_neurons_store = neurons::StableNeuronStoreBuilder {
+//         main: new_memory(...),
+//         hot_keys: etc,
+//         ...
+//     }
+//     .build()
+pub(crate) struct StableNeuronStoreBuilder<Memory> {
     pub main: Memory,
 
     // Collections
@@ -32,11 +34,11 @@ pub(crate) struct StoreBuilder<Memory> {
     pub transfer: Memory,
 }
 
-impl<Memory> StoreBuilder<Memory>
+impl<Memory> StableNeuronStoreBuilder<Memory>
 where
     Memory: ic_stable_structures::Memory,
 {
-    pub fn build(self) -> Store<Memory> {
+    pub fn build(self) -> StableNeuronStore<Memory> {
         let Self {
             main,
 
@@ -52,66 +54,113 @@ where
             transfer: _,
         } = self;
 
-        Store {
+        StableNeuronStore {
             main: StableBTreeMap::init(main),
         }
     }
 }
 
-pub(crate) struct Store<Memory>
+pub(crate) struct StableNeuronStore<Memory>
 where
     Memory: ic_stable_structures::Memory,
 {
     main: StableBTreeMap<u64, Neuron, Memory>,
+    // TODO(NNS1-2484): Implement.
+    // hot_keys: StableBTreeMap<, , Memory>,
+    // followees: StableBTreeMap<, , Memory>,
+    // recent_ballots: StableBTreeMap<, , Memory>,
+
+    // TODO(NNS1-2485): Implement
+    // known_neuron_data: StableBTreeMap<, , Memory>,
+    // transfer: StableBTreeMap<, , Memory>,
 }
 
-impl<Memory> Store<Memory>
+/// A collection of `Neuron`s, backed by some `ic_stable_structure::Memory`s.
+///
+/// Like any good collection/database, this supports the essential CRUD
+/// operations via the following methods:
+///
+///   create(Neuron)
+///   read(NeuronId)
+///   update(Neuron)
+///   delete(NeuronId)
+///
+/// Notice that when an element is passed (i.e. a Neuron) is passed (create and
+/// update), it is consumed, just like with Vec::push, or BTreeMap::insert.
+///
+/// Notice that all of these return Result<X, GovernanceError>, where X is ()
+/// for mutations, and Neuron for read.
+///
+/// Additionall, there is upsert, which updates or inserts, depending on whether
+/// an entry with the same ID already exists. You can think of this as insert,
+/// but clobbering is allowed.
+///
+/// Several `Memory`s are used instead of just oneñ, because the size of
+/// serialized Neurons varies significantly, which leads to inefficient use of
+/// space by ic_stable_structures. In other words, the point of breaking out
+/// into multiple `Memory`s is space efficiency. OTOH, this is probably less
+/// efficient in terms of time/processing. The caller need not be concerned with
+/// the details of how the `Memory`s are used; they simply need to make sure to
+/// pass them consistently from one canister upgrade to the next (i.e. do pass a
+/// `Memory` for main, when previously, it was passed for hot_keys). Currently,
+/// they are working on removing encoding size restrictions, but that might not
+/// land soon enough for us. That would probably obviate all of this.
+impl<Memory> StableNeuronStore<Memory>
 where
     Memory: ic_stable_structures::Memory,
 {
+    /// Adds a new entry.
+    ///
+    /// However, if the id is already in use, returns Err.
     pub fn create(&mut self, mut neuron: Neuron) -> Result<(), GovernanceError> {
-        // Key.
-        let neuron_id = match neuron.id.as_ref() {
-            None => {
-                return Err(GovernanceError::new_with_message(
-                    ErrorType::InvalidCommand,
-                    format!("Tried to store a Neuron, but it lacks an id: {:#?}", neuron),
-                ))
-            }
-            Some(neuron_id) => neuron_id.id,
-        };
+        let neuron_id = Self::id_or_err(&neuron)?;
 
-        // TODO(NNS1-2484): Use these.
-        let _hot_keys = std::mem::take(&mut neuron.hot_keys);
-        let _followees = std::mem::take(&mut neuron.followees);
-        let _recent_ballots = std::mem::take(&mut neuron.recent_ballots);
+        #[allow(unused)] // TODO(NNS1-2484, NNS1-2485): Re-enable clippy.
+        let DecomposedNeuron {
+            main: neuron,
 
-        // TODO(NNS1-2485): Use these.
-        let _known_neuron_data = std::mem::take(&mut neuron.known_neuron_data);
-        let _transfer = std::mem::take(&mut neuron.transfer);
+            // TODO(NNS1-2484): Use these.
+            hot_keys,
+            followees,
+            recent_ballots,
+
+            // TODO(NNS1-2485): Use these.
+            known_neuron_data,
+            transfer,
+        } = DecomposedNeuron::from(neuron);
 
         // Try to insert into main.
         let previous_neuron = self.main.insert(neuron_id, neuron);
-        let previous_neuron = match previous_neuron {
-            None => return Ok(()),
-            Some(previous_neuron) => previous_neuron,
-        };
 
-        // The previous step clobbered an existing Neuron. Here, we restore the
-        // original Neuron..
-        self.main.insert(neuron_id, previous_neuron);
+        // Make sure that we did not clobber an existing entry just now.
+        match previous_neuron {
+            // Phew! Now, we can safely proceed.
+            None => (),
 
-        // Return Err indicating that ID is already in use.
-        Err(GovernanceError::new_with_message(
-            ErrorType::PreconditionFailed,
-            format!("Neuron ID already in use: {}", neuron_id),
-        ))
+            // Yikes! We just clobbered an existing entry! Abort!
+            Some(previous_neuron) => {
+                // Restore the original entry.
+                self.main.insert(neuron_id, previous_neuron);
+
+                // Return Err indicating that ID is already in use.
+                return Err(GovernanceError::new_with_message(
+                    ErrorType::PreconditionFailed,
+                    format!("Neuron ID already in use: {}", neuron_id),
+                ));
+            }
+        }
+
+        // TODO(NNS1-2484, NNS1-2485): Implement.
+
+        Ok(())
     }
 
+    /// Retrieves an existing entry.
     pub fn read(&self, neuron_id: NeuronId) -> Result<Neuron, GovernanceError> {
         let neuron_id = neuron_id.id;
 
-        self.main
+        let abridged_neuron = self
+            .main
             .get(&neuron_id)
             // Deal with no entry by blaming it on the caller.
             .ok_or_else(|| {
@@ -119,19 +168,154 @@ where
                     ErrorType::NotFound,
                     format!("Unable to find neuron with ID {}", neuron_id),
                 )
-            })
+            })?;
+
+        // TODO(NNS1-2484, NNS1-2485): Implement.
+
+        Ok(abridged_neuron)
     }
 
-    // TODO(2488): pub fn update(neuron: Neuron)    -> Result<Neuron, GovernanceError> { todo!() }
-    // TODO(2488): pub fn delete(neuron_id: NeuronId)    -> Result<Neuron, GovernanceError> { todo!() }
+    /// Changes an existing entry.
+    ///
+    /// If the entry does not already exist, returns a NotFound Err.
+    pub fn update(&mut self, mut neuron: Neuron) -> Result<(), GovernanceError> {
+        let neuron_id = Self::id_or_err(&neuron)?;
 
-    // TODO(NNS1-2488): upsert: If not present, create ("insert"). If present,
-    // update. Use of this seems like an anti-pattern, but there may be some
-    // valid use cases...
+        #[allow(unused)] // TODO(NNS1-2484, NNS1-2485): Re-enable clippy.
+        let DecomposedNeuron {
+            main: neuron,
+
+            // TODO(NNS1-2484): Use these.
+            hot_keys,
+            followees,
+            recent_ballots,
+
+            // TODO(NNS1-2485): Use these.
+            known_neuron_data,
+            transfer,
+        } = DecomposedNeuron::from(neuron);
+
+        // Try to insert into main.
+        let previous_neuron = self.main.insert(
+            neuron_id,
+            // clone is done here, because we might later use neuron in an error
+            // message. This should be not a big performance hit, because this
+            // is an abridged neuron.
+            neuron.clone(),
+        );
+
+        // Make sure that we changed an existing entry, not created a new entry.
+        let _previous_neuron = previous_neuron.ok_or_else(|| {
+            // Yikes! There was no entry before. Abort!
+
+            // First, clean up.
+            self.main.remove(&neuron_id);
+
+            GovernanceError::new_with_message(
+                ErrorType::NotFound,
+                format!(
+                    "Tried to update an existing Neuron in stable memory, \
+                         but there was none: {:#?}",
+                    neuron,
+                ),
+            )
+        })?;
+
+        // TODO(NNS1-2484, NNS1-2485): Implement.
+
+        Ok(())
+    }
+
+    /// Removes an existing element.
+    ///
+    /// Returns Err if not found (and no changes are made, of course).
+    pub fn delete(&mut self, neuron_id: NeuronId) -> Result<(), GovernanceError> {
+        let neuron_id = neuron_id.id;
+
+        let deleted_neuron = self.main.remove(&neuron_id);
+
+        match deleted_neuron {
+            Some(_deleted_neuron) => (),
+            None => {
+                return Err(GovernanceError::new_with_message(
+                    ErrorType::NotFound,
+                    format!(
+                        "Tried to delete Neuron from stable storage by ID, \
+                         but ID not found: {}",
+                        neuron_id,
+                    ),
+                ));
+            }
+        }
+
+        // TODO(NNS1-2484, NNS1-2485): Implement.
+
+        Ok(())
+    }
+
+    /// Inserts or updates, depending on whether an entry (with the same ID) is
+    /// already present.
+    ///
+    /// Like insert, but if there is already an entry, it gets clobbered.
+    ///
+    /// This is useful when "writing through"/mirroring mutations on a heap
+    /// Neuron.
+    ///
+    /// Currently, the only way this can return Err is if there is an internal
+    /// bug, which the caller cannot possibly compensate for.
+    pub fn upsert(&mut self, neuron: Neuron) -> Result<(), GovernanceError> {
+        let neuron_id = Self::id_or_err(&neuron)?;
+
+        #[allow(unused)] // TODO(NNS1-2484, NNS1-2485): Re-enable clippy.
+        let DecomposedNeuron {
+            main: neuron,
+
+            // TODO(NNS1-2484): Use these.
+            hot_keys,
+            followees,
+            recent_ballots,
+
+            // TODO(NNS1-2485): Use these.
+            known_neuron_data,
+            transfer,
+        } = DecomposedNeuron::from(neuron);
+
+        let _previous_neuron = self.main.insert(
+            neuron_id,
+            // clone is done here, because we might later use neuron in an error
+            // message. This should be not a big performance hit, because this
+            // is an abridged neuron.
+            neuron.clone(),
+        );
+
+        // TODO(NNS1-2484, NNS1-2485): Implement.
+
+        Ok(())
+    }
+
+    // Misc Private Helper(s)
+    // ----------------------
+
+    /// Pulls out u64 id from a Neuron.
+    fn id_or_err(neuron: &Neuron) -> Result<u64, GovernanceError> {
+        neuron
+            .id
+            .as_ref()
+            // Unwrap id, converting it from NeuronId to simple u64, suitable
+            // for use in StableBTreeMap key.
+            .map(|neuron_id| neuron_id.id)
+            // Handle id field not set.
+            .ok_or_else(|| {
+                GovernanceError::new_with_message(
+                    ErrorType::InvalidCommand,
+                    format!("Tried to store a Neuron, but it lacks an id: {:#?}", neuron),
+                )
+            })
+    }
 }
 
-pub(crate) fn new_heap_based() -> Store<VectorMemory> {
-    StoreBuilder {
+pub(crate) fn new_heap_based() -> StableNeuronStore<VectorMemory> {
+    StableNeuronStoreBuilder {
         main: VectorMemory::default(),
 
         // Collections
@@ -149,7 +333,7 @@ pub(crate) fn new_heap_based() -> Store<VectorMemory> {
 // Make Neuron Suitable for ic_stable_structures
 // ---------------------------------------------
 
-// TODO(2486): AbridgedNeuron.
+// TODO(NNS1-2486): AbridgedNeuron.
 impl Storable for Neuron {
     fn to_bytes(&self) -> Cow<'_, [u8]> {
         Cow::from(self.encode_to_vec())
@@ -171,6 +355,73 @@ impl BoundedStorable for Neuron {
     // that, which seems to strike a good balance between comfortable room for
     // growth vs. excessive wasted space.
     const MAX_SIZE: u32 = 380;
+}
+
+// Private Helpers
+// ---------------
+
+/// Breaks out "fat" fields from a Neuron.
+///
+/// Used like so:
+///
+///     let DecomposedNeuron {
+///         main: abridged_neuron,
+///
+///         hot_keys,
+///         followees,
+///         recent_ballots,
+///
+///         known_neuron_data,
+///         transfer,
+///     } = DecomposedNeuron::from(full_neuron);
+///
+/// Of course, a similar effect can be achieved "manually" by calling
+/// std::mem::take on each of the auxiliary fields, but that is error prone,
+/// because it is very easy forget to take one of the auxilliary fields. By
+/// sticking to this, such mistakes can be avoided.
+///
+/// Notice that full_neuron in the above example gets consumed. It is "replaced"
+/// with abridged_neuron.
+struct DecomposedNeuron {
+    // TODO(2486): AbridgedNeuron.
+    main: Neuron,
+
+    // TODO(NNS1-2484): Use these.
+    // Collections
+    hot_keys: Vec<PrincipalId>,
+    followees: HashMap</* topic ID */ i32, Followees>,
+    recent_ballots: Vec<BallotInfo>,
+
+    // TODO(NNS1-2485): Use these.
+    // Singletons
+    known_neuron_data: Option<KnownNeuronData>,
+    transfer: Option<NeuronStakeTransfer>,
+}
+
+impl From<Neuron> for DecomposedNeuron {
+    fn from(mut source: Neuron) -> Self {
+        let hot_keys = std::mem::take(&mut source.hot_keys);
+        let followees = std::mem::take(&mut source.followees);
+        let recent_ballots = std::mem::take(&mut source.recent_ballots);
+
+        let known_neuron_data = std::mem::take(&mut source.known_neuron_data);
+        let transfer = std::mem::take(&mut source.transfer);
+
+        let main = source; // Just a local re-name.
+
+        Self {
+            main,
+
+            // Collections
+            hot_keys,
+            followees,
+            recent_ballots,
+
+            // Singletons
+            known_neuron_data,
+            transfer,
+        }
+    }
 }
 
 #[cfg(test)]
--- rs/nns/governance/src/storage/neurons/neurons_tests.rs
+++ rs/nns/governance/src/storage/neurons/neurons_tests.rs
@@ -1,61 +1,39 @@
 use super::*;
 
-// create followed by read.
+/// Summary:
+///
+///   1. create
+///   2. bad create
+///   3. read to verify create
+///   4. bad read
+///
+///   5. update
+///   6. read to verify the update
+///   7. bad update
+///   8. read to verify bad update
+///
+///   9. delete
+///   10. bad delete: repeat
+///   11. read to verify.
 #[test]
 fn test_store_simplest_nontrivial_case() {
     let mut store = new_heap_based();
 
-    // create a Neuron.
+    // 1. Create a Neuron.
     assert_eq!(
         store.create(Neuron {
             id: Some(NeuronId { id: 42 }),
-            cached_neuron_stake_e8s: 0xDEAD_BEEF,
+            cached_neuron_stake_e8s: 0xCAFE, // Yummy.
             ..Default::default()
         }),
         Ok(())
     );
 
-    // read it back.
-    assert_eq!(
-        store.read(NeuronId { id: 42 }),
-        Ok(Neuron {
-            id: Some(NeuronId { id: 42 }),
-            cached_neuron_stake_e8s: 0xDEAD_BEEF,
-            ..Default::default()
-        }),
-    );
-
-    // Bad read: Unknown NeuronId. This should result in Err.
-    let bad_read_result = store.read(NeuronId { id: 123 });
-    match &bad_read_result {
-        Err(err) => {
-            let GovernanceError {
-                error_type,
-                error_message,
-            } = err;
-
-            assert_eq!(
-                ErrorType::from_i32(*error_type),
-                Some(ErrorType::NotFound),
-                "{:?}",
-                err,
-            );
-
-            let error_message = error_message.to_lowercase();
-            assert!(error_message.contains("unable to find"), "{:?}", err,);
-            assert!(error_message.contains("123"), "{:?}", err,);
-        }
-
-        _ => panic!(
-            "read(0xDEAD) did not result in an Err: {:?}",
-            bad_read_result
-        ),
-    }
-
-    // Bad create: use an existing NeuronId.
+    // 2. Bad create: use an existing NeuronId. This should result in an
+    // InvalidCommand Err.
     let bad_create_result = store.create(Neuron {
         id: Some(NeuronId { id: 42 }),
-        cached_neuron_stake_e8s: 1,
+        cached_neuron_stake_e8s: 0xDEAD_BEEF,
         ..Default::default()
     });
     match &bad_create_result {
@@ -73,8 +51,8 @@ fn test_store_simplest_nontrivial_case() {
             );
 
             let error_message = error_message.to_lowercase();
-            assert!(error_message.contains("already in use"), "{:?}", err,);
-            assert!(error_message.contains("42"), "{:?}", err,);
+            assert!(error_message.contains("already in use"), "{:?}", err);
+            assert!(error_message.contains("42"), "{:?}", err);
         }
 
         _ => panic!(
@@ -82,4 +60,211 @@ fn test_store_simplest_nontrivial_case() {
             bad_create_result
         ),
     }
+
+    // 3. Read back the first neuron (the second one should have no effect).
+    assert_eq!(
+        store.read(NeuronId { id: 42 }),
+        Ok(Neuron {
+            id: Some(NeuronId { id: 42 }),
+            cached_neuron_stake_e8s: 0xCAFE,
+            ..Default::default()
+        }),
+    );
+
+    // 4. Bad read: Unknown NeuronId. This should result in a NotFound Err.
+    let bad_read_result = store.read(NeuronId { id: 0xDEAD_BEEF });
+    match &bad_read_result {
+        Err(err) => {
+            let GovernanceError {
+                error_type,
+                error_message,
+            } = err;
+
+            assert_eq!(
+                ErrorType::from_i32(*error_type),
+                Some(ErrorType::NotFound),
+                "{:?}",
+                err,
+            );
+
+            let error_message = error_message.to_lowercase();
+            assert!(error_message.contains("unable to find"), "{:?}", err);
+            assert!(error_message.contains("3735928559"), "{:?}", err); // 0xDEAD_BEEF
+        }
+
+        _ => panic!(
+            "read(0xDEAD) did not result in an Err: {:?}",
+            bad_read_result
+        ),
+    }
+
+    // 5. Update existing neuron.
+    let update_result = store.update(Neuron {
+        id: Some(NeuronId { id: 42 }),
+        cached_neuron_stake_e8s: 0xFEED, // After drink, we eat.
+        ..Default::default()
+    });
+    assert_eq!(update_result, Ok(()));
+
+    // 6. Read to verify update.
+    assert_eq!(
+        store.read(NeuronId { id: 42 }),
+        Ok(Neuron {
+            id: Some(NeuronId { id: 42 }),
+            // The most important part: entry actually changed.
+            cached_neuron_stake_e8s: 0xFEED,
+            ..Default::default()
+        }),
+    );
+
+    // 7. Bad update: Neuron not found (unknown ID).
+    let update_result = store.update(Neuron {
+        id: Some(NeuronId { id: 0xDEAD_BEEF }),
+        cached_neuron_stake_e8s: 0xBAD_F00D,
+        ..Default::default()
+    });
+    match &update_result {
+        // This is what we expected.
+        Err(err) => {
+            // Take a closer look at err.
+            let GovernanceError {
+                error_type,
+                error_message,
+            } = err;
+
+            // Inspect type.
+            let error_type = ErrorType::from_i32(*error_type);
+            assert_eq!(error_type, Some(ErrorType::NotFound), "{:?}", err);
+
+            // Next, turn to error_message.
+            let error_message = error_message.to_lowercase();
+            assert!(error_message.contains("update"), "{:?}", err);
+            assert!(error_message.contains("existing"), "{:?}", err);
+            assert!(error_message.contains("neuron"), "{:?}", err);
+            assert!(error_message.contains("there was none"), "{:?}", err);
+
+            assert!(error_message.contains("id"), "{:?}", err);
+            assert!(error_message.contains("3735928559"), "{:?}", err); // 0xDEAD_BEEF
+
+            assert!(
+                error_message.contains("cached_neuron_stake_e8s"),
+                "{:?}",
+                err,
+            );
+            assert!(error_message.contains("195948557"), "{:?}", err); // 0xBAD_F00D
+        }
+
+        // Any other result is bad.
+        _ => panic!("{:#?}", update_result),
+    }
+
+    // 8. Read to verify bad update.
+    let read_result = store.read(NeuronId { id: 0xDEAD_BEEF });
+    match &read_result {
+        // This is what we expected.
+        Err(err) => {
+            // Take a closer look at err.
+            let GovernanceError {
+                error_type,
+                error_message,
+            } = err;
+
+            // Inspect type.
+            let error_type = ErrorType::from_i32(*error_type);
+            assert_eq!(error_type, Some(ErrorType::NotFound), "{:?}", err);
+
+            // Next, turn to error_message.
+            let error_message = error_message.to_lowercase();
+            assert!(error_message.contains("unable to find"), "{:?}", err);
+            assert!(error_message.contains("3735928559"), "{:?}", err); // 0xDEAD_BEEF
+        }
+
+        _ => panic!("read did not return Err: {:?}", read_result),
+    }
+
+    // 9. Delete.
+    assert_eq!(store.delete(NeuronId { id: 42 }), Ok(()));
+
+    // 10. Bad delete: repeat.
+    let delete_result = store.delete(NeuronId { id: 42 });
+    match &delete_result {
+        // This is what we expected.
+        Err(err) => {
+            // Take a closer look at err.
+            let GovernanceError {
+                error_type,
+                error_message,
+            } = err;
+
+            // Inspect type.
+            let error_type = ErrorType::from_i32(*error_type);
+            assert_eq!(error_type, Some(ErrorType::NotFound), "{:?}", err);
+
+            // Next, turn to error_message.
+            let error_message = error_message.to_lowercase();
+            assert!(error_message.contains("not found"), "{:?}", err);
+            assert!(error_message.contains("42"), "{:?}", err);
+        }
+
+        _ => panic!("second delete did not return Err: {:?}", delete_result),
+    }
+
+    // 11. Read to verify delete.
+    let read_result = store.read(NeuronId { id: 42 });
+    match &read_result {
+        // This is what we expected.
+        Err(err) => {
+            // Take a closer look at err.
+            let GovernanceError {
+                error_type,
+                error_message,
+            } = err;
+
+            // Inspect type.
+            let error_type = ErrorType::from_i32(*error_type);
+            assert_eq!(error_type, Some(ErrorType::NotFound), "{:?}", err);
+
+            // Next, turn to error_message.
+            let error_message = error_message.to_lowercase();
+            assert!(error_message.contains("unable to find"), "{:?}", err);
+            assert!(error_message.contains("42"), "{:?}", err);
+        }
+
+        _ => panic!("read did not return Err: {:?}", read_result),
+    }
+}
+
+/// Summary:
+///
+///   1. upsert (effectively, an insert)
+///   2. read to verify
+///   3. upsert same ID (effectively, an update)
+///   4. read to verify
+#[test]
+fn test_store_upsert() {
+    let mut store = new_heap_based();
+
+    let neuron = Neuron {
+        id: Some(NeuronId { id: 0xF00D }),
+        cached_neuron_stake_e8s: 0xBEEF,
+        ..Default::default()
+    };
+
+    // 1. upsert (entry not already present)
+    assert_eq!(store.upsert(neuron.clone()), Ok(()));
+
+    // 2. read to verify
+    assert_eq!(store.read(NeuronId { id: 0xF00D }), Ok(neuron.clone()));
+
+    // Modify neuron.
+    let neuron = Neuron {
+        cached_neuron_stake_e8s: 0xCAFE,
+        ..neuron
+    };
+
+    // 3. upsert (change an existing entry)
+    assert_eq!(store.upsert(neuron.clone()), Ok(()));
+
+    // 4. read to verify
+    assert_eq!(store.read(NeuronId { id: 0xF00D }), Ok(neuron));
 }
