e9993111d44e4db0b520591e058bd2824a6f2f7d
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2023-08-23 17:43:28 +0000

    .

--- rs/p2p/quic_transport/benches/bench.rs
+++ rs/p2p/quic_transport/benches/bench.rs
@@ -6,7 +6,7 @@ use std::{
 
 use axum::{routing::any, Router};
 use bytes::Bytes;
-use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion, Throughput};
 use either::Either;
 use http::Request;
 use ic_icos_sev::Sev;
@@ -21,9 +21,9 @@ use ic_types::NodeId;
 use ic_types_test_utils::ids::node_test_id;
 use tokio::{runtime::Handle, sync::watch, task::JoinSet};
 
-const NUM_NODES: u64 = 2;
+const NUM_NODES: u64 = 40;
 const PARALLEL_REQUESTS: u64 = 100;
-const REQUEST_SIZE_BYTES: u64 = 512;
+const REQUEST_SIZE_BYTES: u64 = 512000;
 const RESPONSE_SIZE_BYTES: u64 = 512;
 
 async fn pong() -> String {
@@ -54,7 +54,7 @@ fn spawn_transport(
         node_id,
         watch_rx,
         Either::<_, DummyUdpSocket>::Left(node_addr),
-        Router::new().route("/", any(pong)),
+        Some(Router::new().route("/", any(pong))),
     ));
     (transport, node_id, node_addr)
 }
@@ -94,15 +94,14 @@ fn bench_transport(criterion: &mut Criterion) {
     let num_request_per_iter = (transports.len() as u64 - 1) * PARALLEL_REQUESTS;
     group.throughput(Throughput::Elements(num_request_per_iter));
     group.bench_with_input("Number of messages", &test_transport, |b, transport| {
-        b.to_async(&rt).iter(|| async {
-            let mut js_outer = JoinSet::new();
-            for _ in 0..PARALLEL_REQUESTS {
-                let transport_c = transport.clone();
-                js_outer.spawn(async move {
-                    let mut js_inner = JoinSet::new();
-                    for peer in transport_c.peers().await {
-                        let t = transport_c.clone();
-                        js_inner.spawn(async move {
+        b.to_async(&rt).iter_batched(
+            || {
+                let mut futs = Vec::new();
+                for _ in 0..PARALLEL_REQUESTS {
+                    let transport_c = transport.clone();
+                    futs.push(async move {
+                        for peer in transport_c.peers() {
+                            let t = transport_c.clone();
                             t.rpc(
                                 &peer,
                                 Request::builder()
@@ -111,17 +110,23 @@ fn bench_transport(criterion: &mut Criterion) {
                                     .unwrap(),
                             )
                             .await
-                        });
-                    }
-                    while let Some(a) = js_inner.join_next().await {
-                        a.unwrap().unwrap();
-                    }
-                });
-            }
-            while let Some(a) = js_outer.join_next().await {
-                a.unwrap();
-            }
-        })
+                            .unwrap();
+                        }
+                    })
+                }
+                futs
+            },
+            |futs| async {
+                let mut js = JoinSet::new();
+                for f in futs {
+                    js.spawn(f);
+                }
+                while let Some(a) = js.join_next().await {
+                    a.unwrap();
+                }
+            },
+            BatchSize::SmallInput,
+        );
     });
     group.finish();
 }
