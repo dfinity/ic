9be75b5d5b5ded227639abab1e0aaf3d5f5c2239
Jason (Yeli) Zhu <jason.zhu@dfinity.org>
2023-08-24 22:21:15 +0000

    feat: NNS1-2409 Implement StableNeuronIndexes and batch_add_heap_neurons_to_stable_indexes

--- rs/nns/governance/src/neuron_store.rs
+++ rs/nns/governance/src/neuron_store.rs
@@ -1,4 +1,4 @@
-use crate::pb::v1::Neuron;
+use crate::{pb::v1::Neuron, storage::NEURON_INDEXES};
 use ic_nns_common::pb::v1::NeuronId;
 use std::collections::BTreeMap;
 
@@ -57,4 +57,41 @@ impl NeuronStore {
     pub fn heap_neurons_mut(&mut self) -> &mut BTreeMap<u64, Neuron> {
         &mut self.heap_neurons
     }
+
+    /// For heap neurons starting from `last_neuron_id + 1` where `last_neuron_id` is the last neuron id that has been
+    /// migrated, and it adds at most `batch_size` of them into stable storage indexes. It is an undefined behavior if
+    /// `last_neuron_id` passed in was not returned by the one returned by the same function.
+    ///
+    /// Returns `Err(failure_reason)` if it failed; returns Ok(None) if the cursor reaches the end; returns
+    /// `Ok(last_neuron_id)` if the cursor has not reached the end.
+    ///
+    /// Note that a Neuron with id 0 will never be scan, and it's OK because it is not a valid id.
+    ///
+    #[allow(dead_code)] // TODO(NNS1-2409): Re-enable clippy.
+    pub(crate) fn batch_add_heap_neurons_to_stable_indexes(
+        &self,
+        last_neuron_id: u64,
+        batch_size: usize,
+    ) -> Result<Option<u64>, String> {
+        let mut new_last_neuron_id = None;
+        let mut count = 0;
+        for (neuron_id, neuron) in self
+            .heap_neurons
+            .range(last_neuron_id + 1..)
+            .take(batch_size)
+        {
+            NEURON_INDEXES.with(|indexes| indexes.borrow_mut().add_neuron(neuron))?;
+            count += 1;
+            new_last_neuron_id = Some(*neuron_id);
+        }
+
+        if count < batch_size {
+            // No more neurons to migrate
+            new_last_neuron_id = None
+        }
+        Ok(new_last_neuron_id)
+    }
 }
+
+#[cfg(test)]
+mod neuron_store_tests;
