1392362e9aec24792ff9f7552aa1839664aa2853
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2023-08-24 09:32:58 +0000

    .

--- rs/p2p/quic_transport/benches/bench.rs
+++ rs/p2p/quic_transport/benches/bench.rs
@@ -21,10 +21,10 @@ use ic_types::NodeId;
 use ic_types_test_utils::ids::node_test_id;
 use tokio::{runtime::Handle, sync::watch, task::JoinSet};
 
-const NUM_NODES: u64 = 40;
-const PARALLEL_REQUESTS: u64 = 100;
-const REQUEST_SIZE_BYTES: u64 = 512000;
-const RESPONSE_SIZE_BYTES: u64 = 512;
+const NUM_NODES: u64 = 2;
+const PARALLEL_REQUESTS: u64 = 1000;
+const REQUEST_SIZE_BYTES: u64 = 64;
+const RESPONSE_SIZE_BYTES: u64 = 32;
 
 async fn pong() -> String {
     "a".repeat(RESPONSE_SIZE_BYTES as usize)
@@ -59,9 +59,9 @@ fn spawn_transport(
     (transport, node_id, node_addr)
 }
 
-fn bench_transport(criterion: &mut Criterion) {
+fn bench_transport(criterion: &mut Criterion<criterion_time::ProcessTime>) {
     let log = no_op_logger();
-    let rt = tokio::runtime::Runtime::new().unwrap();
+    let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
     let mut group = criterion.benchmark_group("quic-transport-send-throughput");
 
     let (watch_tx, watch_rx) = watch::channel(SubnetTopology::default());
@@ -89,30 +89,33 @@ fn bench_transport(criterion: &mut Criterion) {
         .unwrap();
 
     let test_transport = transports.get(0).unwrap().0.clone();
+    let peers: Vec<NodeId> = transports
+        .iter()
+        .skip(1)
+        .map(|(_, node_id, _)| *node_id)
+        .collect();
 
     rt.block_on(async { tokio::time::sleep(Duration::from_secs(5)).await });
     let num_request_per_iter = (transports.len() as u64 - 1) * PARALLEL_REQUESTS;
     group.throughput(Throughput::Elements(num_request_per_iter));
-    group.bench_with_input("Number of messages", &test_transport, |b, transport| {
+    // group.throughput(Throughput::Bytes(num_request_per_iter * REQUEST_SIZE_BYTES));
+    group.bench_with_input("RPC", &test_transport, |b, transport| {
+        // Generate all the futures in advance including the request to not measure setup time.
         b.to_async(&rt).iter_batched(
             || {
                 let mut futs = Vec::new();
                 for _ in 0..PARALLEL_REQUESTS {
                     let transport_c = transport.clone();
-                    futs.push(async move {
-                        for peer in transport_c.peers().await {
-                            let t = transport_c.clone();
-                            t.rpc(
-                                &peer,
-                                Request::builder()
-                                    .uri("/")
-                                    .body(Bytes::from(vec![0; REQUEST_SIZE_BYTES as usize]))
-                                    .unwrap(),
-                            )
-                            .await
-                            .unwrap();
-                        }
-                    })
+                    for peer in peers.iter() {
+                        let bytes = Bytes::from(vec![0; REQUEST_SIZE_BYTES as usize]);
+                        let t = transport_c.clone();
+                        let p = *peer;
+                        futs.push(async move {
+                            t.rpc(&p, Request::builder().uri("/").body(bytes).unwrap())
+                                .await
+                                .unwrap();
+                        });
+                    }
                 }
                 futs
             },
@@ -125,11 +128,55 @@ fn bench_transport(criterion: &mut Criterion) {
                     a.unwrap();
                 }
             },
-            BatchSize::SmallInput,
+            BatchSize::PerIteration,
         );
+    });
+    group.bench_with_input("PUSH", &test_transport, |b, transport| {
+        b.to_async(&rt).iter_batched(
+            // Generate all the futures in advance including the request to not measure setup time.
+            || {
+                let mut futs = Vec::new();
+                for _ in 0..PARALLEL_REQUESTS {
+                    let transport_c = transport.clone();
+                    for peer in peers.iter() {
+                        let bytes = Bytes::from(vec![0; REQUEST_SIZE_BYTES as usize]);
+                        let t = transport_c.clone();
+                        let p = *peer;
+                        futs.push(async move {
+                            t.push(&p, Request::builder().uri("/").body(bytes).unwrap())
+                                .await
+                                .unwrap();
+                        });
+                    }
+                }
+                futs
+            },
+            |futs| async {
+                let mut js = JoinSet::new();
+                for f in futs {
+                    js.spawn(f);
+                }
+                while let Some(a) = js.join_next().await {
+                    a.unwrap();
+                }
+            },
+            BatchSize::PerIteration,
+        );
+    });
+    group.bench_with_input("PEERS", &test_transport, |b, transport| {
+        b.to_async(&rt).iter(|| async {
+            let _ = transport.peers();
+        });
     });
     group.finish();
 }
 
-criterion_group!(benches, bench_transport);
-criterion_main!(benches);
+
+fn main() {
+    let mut c = Criterion::default()
+        .with_measurement(criterion_time::ProcessTime::UserTime)
+        .warm_up_time(Duration::from_secs(10));
+
+    bench_transport(&mut c);
+    c.final_summary();
+}
