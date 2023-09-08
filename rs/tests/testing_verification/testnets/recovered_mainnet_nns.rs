086a8f3977559d991b638588e1077b66274e50d1
Bas van Dijk <bas@van.dijk.ch>
2023-09-08 23:30:19 +0000

    chore: run the nns_upgrade_test on the nightly CI pipeline

--- rs/tests/testing_verification/testnets/recovered_mainnet_nns.rs
+++ rs/tests/testing_verification/testnets/recovered_mainnet_nns.rs
@@ -4,7 +4,7 @@
 //
 // Create the testnet using:
 //
-//   rm -rf test_tmpdir; ict testnet create recovered_mainnet_nns --lifetime-mins 120 --set-required-host-features=dc=zh1 --verbose -- --test_env=SSH_AUTH_SOCK --test_tmpdir=test_tmpdir
+//   rm -rf test_tmpdir; ict testnet create recovered_mainnet_nns --lifetime-mins 120 --set-required-host-features=dc=zh1 --verbose -- --test_tmpdir=test_tmpdir
 //
 // Make sure to pick a DC in --set-required-host-features=dc=zh1 which is close to
 // where you are running the test from. Shipping state across the Atlantic can double the
