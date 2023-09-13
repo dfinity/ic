7bc8836a40853fe4a5a353d8cefee1cc365c0b57
Léo Kazmierczak <leo.kazmierczak@dfinity.org>
2023-09-13 08:16:23 +0000

    feat(cketh) Fix the logs endpoint

--- rs/ethereum/cketh/minter/src/main.rs
+++ rs/ethereum/cketh/minter/src/main.rs
@@ -609,8 +609,7 @@ fn http_request(req: HttpRequest) -> HttpResponse {
             .with_body_and_content_length(dashboard.render().unwrap())
             .build()
     } else if req.path() == "/logs" {
-        use ic_cketh_minter::logs::{Log, Priority};
-        use serde_json;
+        use ic_cketh_minter::logs::{Log, Priority, Sort};
         use std::str::FromStr;
 
         let max_skip_timestamp = match req.raw_query_param("time") {
@@ -625,26 +624,54 @@ fn http_request(req: HttpRequest) -> HttpResponse {
             None => 0,
         };
 
-        let mut entries: Log = Default::default();
+        let mut log: Log = Default::default();
 
         match req.raw_query_param("priority") {
             Some(priority_str) => match Priority::from_str(priority_str) {
                 Ok(priority) => match priority {
-                    Priority::Info => entries.push_logs(Priority::Info),
-                    Priority::TraceHttp => entries.push_logs(Priority::TraceHttp),
-                    Priority::Debug => entries.push_logs(Priority::Debug),
+                    Priority::Info => log.push_logs(Priority::Info),
+                    Priority::TraceHttp => log.push_logs(Priority::TraceHttp),
+                    Priority::Debug => log.push_logs(Priority::Debug),
                 },
-                Err(_) => entries.push_all(),
+                Err(_) => log.push_all(),
             },
-            None => entries.push_all(),
+            None => log.push_all(),
         }
 
-        entries
-            .entries
+        log.entries
             .retain(|entry| entry.timestamp >= max_skip_timestamp);
+
+        fn ordering_from_query_params(sort: Option<&str>, max_skip_timestamp: u64) -> Sort {
+            match sort {
+                Some(ord_str) => match Sort::from_str(ord_str) {
+                    Ok(order) => order,
+                    Err(_) => {
+                        if max_skip_timestamp == 0 {
+                            Sort::Ascending
+                        } else {
+                            Sort::Descending
+                        }
+                    }
+                },
+                None => {
+                    if max_skip_timestamp == 0 {
+                        Sort::Ascending
+                    } else {
+                        Sort::Descending
+                    }
+                }
+            }
+        }
+
+        log.sort_logs(ordering_from_query_params(
+            req.raw_query_param("sort"),
+            max_skip_timestamp,
+        ));
+
+        const MAX_BODY_SIZE: usize = 3_000_000;
         HttpResponseBuilder::ok()
             .header("Content-Type", "application/json; charset=utf-8")
-            .with_body_and_content_length(serde_json::to_string(&entries).unwrap_or_default())
+            .with_body_and_content_length(log.serialize_logs(MAX_BODY_SIZE))
             .build()
     } else {
         HttpResponseBuilder::not_found().build()
