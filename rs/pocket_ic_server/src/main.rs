ec5f21dbe668d7e9d977028230aad293cd468682
Elias Datler <elias.datler@dfinity.org>
2023-09-21 17:29:49 +0000

    chore(PocketIC): Less verbose logging by default

--- rs/pocket_ic_server/src/main.rs
+++ rs/pocket_ic_server/src/main.rs
@@ -236,7 +236,7 @@ fn setup_tracing() {
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
                 // axum logs rejections from built-in extractors with the `axum::rejection`
                 // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
-                "pocket_ic_server=debug,tower_http=debug,axum::rejection=trace".into()
+                "pocket_ic_server=info,tower_http=info,axum::rejection=trace".into()
             }),
         )
         .with(tracing_subscriber::fmt::layer())
