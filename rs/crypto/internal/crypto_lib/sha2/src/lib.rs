0913e39b7fd629b05ed6e0df70597bf4bd200eaa
Jack Lloyd <jack.lloyd@dfinity.org>
2023-09-26 19:27:41 +0000

    refactor(crypto): CRP-1914 Use RustCrypto for all SHA-2 hashing

--- rs/crypto/internal/crypto_lib/sha2/src/lib.rs
+++ rs/crypto/internal/crypto_lib/sha2/src/lib.rs
@@ -1,22 +1,10 @@
 //! Hashing utilities based on SHA-2 standard.
 //!
-//! The package works both on Wasm and non-Wasm architectures by using
-//! different SHA2-hasher implementations depending on the architecture:
-//!
-//! * When compiling to Wasm, we use pure Rust implementation (provided by sha2
-//!   package).
-//!
-//! * When compiling to native, we use openssl, which relies on OpenSSL.
-//!
-//! At the moment, the complexity introduced by architecture-dependent
-//! code is worth it:
-//!
-//! * OpenSSL is big, complicated and is hard to port to Wasm. So it's not
-//!   really a viable option to use it in canisters.
-//!
-//! * sha2 is pure Rust, but it's ~20% slower than OpenSSL on StateManager
-//!   benchmarks. Thus it makes sense to use a faster implementation in the
-//!   replica code.
+//! Currently this crate delegates the hashing to the RustCrypto sha2
+//! crate. That crate supports both a pure Rust implementation
+//! (necessary for Wasm) and additionally supports SHA-NI and ARMv8
+//! SHA hardware extensions which provide excellent performance on
+//! cores which support them (this includes all replica nodes).
 //!
 //! Clients should not use this crate directly, but rather the crate
 //! ic_crypto_sha256, which is a thin layer on top of this internal crate.
