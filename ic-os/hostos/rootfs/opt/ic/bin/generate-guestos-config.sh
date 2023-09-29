1b2f0cc8b390b3f528dde782623065fd404d3561
Khushboo Bindlish <khushboo@dfinity.org>
2023-09-29 16:42:09 +0000

    feat(NODE-1020): Bring up guestos as a SEV-SNP VM

--- ic-os/hostos/rootfs/opt/ic/bin/generate-guestos-config.sh
+++ ic-os/hostos/rootfs/opt/ic/bin/generate-guestos-config.sh
@@ -137,12 +137,85 @@ function generate_guestos_config() {
     fi
 }
 
+TMP_MOUNT_DIR="/tmp/sev-guest-mount"
+BOOT_COMPONENTS_DIR="/tmp/sev-guest-boot-components"
+LOOP_DEVICE="/dev/loop0"
+GUESTOS_LOCATION="/dev/mapper/hostlvm-guestos"
+SEV_SNP_FILE="/var/run/SEV"
+
+# Set up loop device and mount. Get the boot components
+mount_guestos_image_and_copy_files() {
+    mkdir -p "$TMP_MOUNT_DIR"
+    losetup -P "$LOOP_DEVICE" /dev/mapper/hostlvm-guestos
+    mount "${LOOP_DEVICE}p4" "$TMP_MOUNT_DIR"
+    mkdir -p "$BOOT_COMPONENTS_DIR"
+    cp "$TMP_MOUNT_DIR"/vmlinuz "$BOOT_COMPONENTS_DIR"
+    cp "$TMP_MOUNT_DIR"/initrd.img "$BOOT_COMPONENTS_DIR"
+    cp "$TMP_MOUNT_DIR"/extra_boot_args "$BOOT_COMPONENTS_DIR"
+}
+
+# Clean up loop device and mount
+unmount_guestos_image() {
+    umount -q "$TMP_MOUNT_DIR"
+    rmdir "$TMP_MOUNT_DIR"
+    losetup -d "$LOOP_DEVICE"
+}
+
+# Generate config for SEV GuestOS
+# Use the qemu script template and mount the guestos image.
+# Derive kernel, initrd and the cmdline and populate the qemu script.
+function generate_sev_guestos_config() {
+    INPUT="/opt/ic/share/sev_guestos.sh.template"
+    OUTPUT="/var/lib/sev_guestos.sh"
+    RESOURCES_MEMORY=$(/opt/ic/bin/fetch-property.sh --key=.resources.memory --metric=hostos_resources_memory --config=${DEPLOYMENT})
+    MAC_ADDRESS=$(/opt/ic/bin/generate-deterministic-mac.sh --index=1)
+    mount_guestos_image_and_copy_files
+    unmount_guestos_image
+
+    KERNEL="$BOOT_COMPONENTS_DIR/vmlinuz"
+    INITRD="$BOOT_COMPONENTS_DIR/initrd.img"
+    source "$BOOT_COMPONENTS_DIR"/extra_boot_args
+    if [ ! -f "${OUTPUT}" ]; then
+        sed -e "s@{{ resources_memory }}@${RESOURCES_MEMORY}@" \
+            -e "s@{{ mac_address }}@${MAC_ADDRESS}@" \
+            -e "s@{{ kernel }}@${KERNEL}@" \
+            -e "s@{{ initrd }}@${INITRD}@" \
+            -e "s@{{ extra_boot_args }}@${EXTRA_BOOT_ARGS}@" \
+            "${INPUT}" >"${OUTPUT}"
+        chmod ug+x "${OUTPUT}"
+        write_log "Generating SEV GuestOS configuration file: ${OUTPUT}"
+        write_metric "hostos_generate_sev_guestos_config" \
+            "1" \
+            "HostOS generate SEV GuestOS config" \
+            "gauge"
+    else
+        write_log "SEV GuestOS configuration file already exists: ${OUTPUT}"
+        write_metric "hostos_generate_sev_guestos_config" \
+            "0" \
+            "HostOS generate SEV GuestOS config" \
+            "gauge"
+    fi
+}
+
+# Check if SEV-SNP if enabled on host
+function is_sev_snp_enabled() {
+    if [ -f "$SEV_SNP_FILE" ]; then
+        return 0
+    fi
+
+    return 1
+}
+
 function main() {
     # Establish run order
     validate_arguments
     read_variables
     assemble_config_media
-    generate_guestos_config
+    if is_sev_snp_enabled; then
+        generate_sev_guestos_config
+    else
+        generate_guestos_config
+    fi
 }
 
 main
