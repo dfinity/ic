161803c5d5685d58049ffbbf972b5477924e8dbc
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2023-10-23 13:46:12 +0000

    test: remove the legacy chunking test

--- rs/p2p/tests/n_nodebasic.rs
+++ rs/p2p/tests/n_nodebasic.rs
@@ -11,7 +11,7 @@ const MAX_HEIGHT: u64 = 4;
 /// The test succeeds if the nodes manage to run up to the height `MAX_HEIGHT`.
 #[test]
 fn n_node_gossip_using_threads() {
-    framework::spawn_replicas_as_threads(true, NUM_TEST_INSTANCES, |p2p_test_context| {
+    framework::spawn_replicas_as_threads(NUM_TEST_INSTANCES, |p2p_test_context| {
         framework::replica_run_till_height(p2p_test_context, MAX_HEIGHT);
     });
 }
