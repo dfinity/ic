36d9617892a3ea23ec93dbf143bdf9bf1a8c7bf3
Roman Kashitsyn <roman.kashitsyn@dfinity.org>
2023-11-14 22:15:28 +0000

    feat(cketh): expose metrics for HTTP call retry counts

--- rs/ethereum/cketh/minter/src/main.rs
+++ rs/ethereum/cketh/minter/src/main.rs
@@ -514,6 +514,8 @@ fn http_request(req: HttpRequest) -> HttpResponse {
                     "The age of the oldest incomplete ETH withdrawal request in seconds.",
                 )?;
 
+                ic_cketh_minter::eth_rpc::encode_metrics(w)?;
+
                 Ok(())
             })
         }
