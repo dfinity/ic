029765df88faffbc76357c12aa0418ce40f0641b
Grégory Demay <gregory.demay@dfinity.org>
2023-11-20 09:56:19 +0000

    fix(cketh): fix typo in Solidity helper smart contract related to `event_counter`

--- rs/ethereum/cketh/minter/minter.sol
+++ rs/ethereum/cketh/minter/minter.sol
@@ -7,7 +7,8 @@ pragma solidity 0.8.18;
  * @notice This smart contract deposits incoming ETH to the ckETH minter account and emits deposit events.
  */
 contract CkEthDeposit {
-    address payable private cketh_minter_main_address;
+    uint256 private constant MAX_NUM_EVENTS_PER_BLOCK = 10;
+    address payable private immutable cketh_minter_main_address;
     uint256 private last_block_number;
     uint256 private event_counter;
 
@@ -23,8 +24,8 @@ contract CkEthDeposit {
     }
 
     /**
-     * @dev Return ckETH minter main address. 
-     * @return address of ckETH minter main address. 
+     * @dev Return ckETH minter main address.
+     * @return address of ckETH minter main address.
      */
     function getMinterAddress() public view returns (address) {
         return cketh_minter_main_address;
@@ -38,10 +39,10 @@ contract CkEthDeposit {
             last_block_number = block.number;
             event_counter = 0;
         }
-        require(event_counter < 0, "Maximum events per block reached");
+        require(event_counter < MAX_NUM_EVENTS_PER_BLOCK, "Maximum events per block reached");
 
         emit ReceivedEth(msg.sender, msg.value, _principal);
         cketh_minter_main_address.transfer(msg.value);
         event_counter++;
     }
-} 
+}
