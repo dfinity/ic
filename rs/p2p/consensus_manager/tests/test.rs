a5e76f26caa75d3e3c574d75678bfb41097d032f
Daniel Sharifi <daniel.sharifi@dfinity.org>
2023-11-27 15:39:36 +0000

    test(p2p-consensus): [NET-1595] Test node with broken connection receive adverts upon a reconnect

--- rs/p2p/consensus_manager/tests/test.rs
+++ rs/p2p/consensus_manager/tests/test.rs
@@ -123,10 +123,10 @@ fn test_artifact_in_validated_pool_is_sent_to_peer_joining_subnet() {
         registry_handle.registry_client.reload();
         registry_handle.registry_client.update_to_latest_version();
 
-        // Node 1 sends the advert
+        // Node_1 sends the advert
         processor_1.push_advert(1);
 
-        // Node 2 has received the advert
+        // Node_2 has received the advert
         wait_for(&mut sim, || processor_2.received_advert_once(1)).unwrap();
 
         // Node 3 joins the subnet
@@ -209,10 +209,10 @@ fn test_flapping_connection_does_not_cause_duplicate_artifact_downloads() {
         registry_handle.registry_client.reload();
         registry_handle.registry_client.update_to_latest_version();
 
-        // Node 1 sends the advert
+        // Node_1 sends the advert
         processor_1.push_advert(1);
 
-        // Node 2 has received the advert
+        // Node_2 has received the advert
         wait_for(&mut sim, || processor_2.received_advert_once(1)).unwrap();
 
         // Disconnect nodes, and run simulation for 5s.
@@ -235,7 +235,13 @@ fn test_flapping_connection_does_not_cause_duplicate_artifact_downloads() {
     });
 }
 
-/// Test that nodes retransmit adverts to a peer that reconnects.
+/// Test that nodes retransmits adverts tp peers that reconnect.
+/// Scenario
+/// 1. Node_1 connects to Node_2 and Node_3.
+/// 2. Node_1 produces an artifact.
+/// 3. Node_2 and Node_3 receive the artifact.
+/// 4. Node_2 disconnects and re-connects to Node_1.
+/// 5. Assert that Node_2 receives the artifact again from Node_1.
 #[test]
 fn test_adverts_are_retransmitted_on_reconnection() {
     with_test_replica_logger(|log| {
@@ -321,6 +327,108 @@ fn test_adverts_are_retransmitted_on_reconnection() {
 
         wait_for(&mut sim, || processor_2.received_advert_once(1))
             .expect("NODE_2 should receive `advert 1` from `NODE_1`.");
+
+        exit_notify.notify_waiters();
+        sim.run().unwrap();
+    });
+}
+
+/// Test that a node transmit adverts to its peers for adverts that were produced at a time
+/// its peer was disconnected, but that eventually reconnect.
+/// Scenario:
+/// 1. Node_1 connects to Node_2.
+/// 2. Node_1 and Node_2 disconnect.
+/// 3. Node_1's processor produces an artifact to be sent.
+/// 4. Node_1 and Node_2 reconnect.
+/// 5. Node_2 receives the artifact.
+#[test]
+fn test_new_adverts_are_transmitted_on_reconnection() {
+    with_test_replica_logger(|log| {
+        let mut sim = Builder::new()
+            .tick_duration(Duration::from_millis(100))
+            .simulation_duration(Duration::from_secs(20))
+            .build();
+
+        let exit_notify = Arc::new(Notify::new());
+        let (peer_manager_cmd_sender, topology_watcher, registry_handle) =
+            add_peer_manager_to_sim(&mut sim, exit_notify.clone(), log.clone());
+        let processor_1 = TestConsensus::new(log.clone(), NODE_1);
+        let processor_2 = TestConsensus::new(log.clone(), NODE_2);
+        let processor_3 = TestConsensus::new(log.clone(), NODE_3);
+
+        add_transport_to_sim(
+            &mut sim,
+            log.clone(),
+            NODE_1,
+            registry_handle.clone(),
+            topology_watcher.clone(),
+            None,
+            None,
+            None,
+            None,
+            Some(processor_1.clone()),
+            waiter_fut(),
+        );
+        add_transport_to_sim(
+            &mut sim,
+            log.clone(),
+            NODE_2,
+            registry_handle.clone(),
+            topology_watcher.clone(),
+            None,
+            None,
+            None,
+            None,
+            Some(processor_2.clone()),
+            waiter_fut(),
+        );
+        add_transport_to_sim(
+            &mut sim,
+            log.clone(),
+            NODE_3,
+            registry_handle.clone(),
+            topology_watcher.clone(),
+            None,
+            None,
+            None,
+            None,
+            Some(processor_3.clone()),
+            waiter_fut(),
+        );
+
+        peer_manager_cmd_sender
+            .send(PeerManagerAction::Add((NODE_1, RegistryVersion::from(2))))
+            .unwrap();
+        peer_manager_cmd_sender
+            .send(PeerManagerAction::Add((NODE_2, RegistryVersion::from(3))))
+            .unwrap();
+        peer_manager_cmd_sender
+            .send(PeerManagerAction::Add((NODE_3, RegistryVersion::from(4))))
+            .unwrap();
+        registry_handle.registry_client.reload();
+        registry_handle.registry_client.update_to_latest_version();
+
+        // ping pong advert
+        processor_1.push_advert(1);
+        wait_for(&mut sim, || processor_2.received_advert_once(1))
+            .expect("`NODE_2` should receive the advert from `NODE_1`.");
+
+        // Disconnect nodes, and run simulation for +5s to cause timeout in transport.
+        // TODO: Adjust this timeout duration to be injected with a config struct.
+        sim.partition(NODE_1.to_string(), NODE_2.to_string());
+        run_simulation_for(&mut sim, TIMEOUT_DURATION_TRIGGER).unwrap();
+
+        // Node_1 starts a send task while the link is broken.
+        processor_1.push_advert(2);
+
+        wait_for(&mut sim, || processor_3.received_advert_once(2)).expect(
+            "`NODE_3` is connected to `NODE_1` and should receive the advert from `NODE_1`.",
+        );
+
+        sim.repair(NODE_1.to_string(), NODE_2.to_string());
+
+        wait_for(&mut sim, || processor_2.received_advert_once(2))
+            .expect("`NODE_2` should receive the advert from `NODE_1` after reconnecting.");
 
         exit_notify.notify_waiters();
         sim.run().unwrap();
