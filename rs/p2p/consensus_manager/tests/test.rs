0a7e8aa3d1d7ea3d3d69bbf196871d72589840cb
Daniel Sharifi <daniel.sharifi@dfinity.org>
2023-11-27 13:27:51 +0000

    test(p2p-consensus): [NET-1576] Test adverts are retransmitted on reconnection.

--- rs/p2p/consensus_manager/tests/test.rs
+++ rs/p2p/consensus_manager/tests/test.rs
@@ -3,8 +3,8 @@ use std::{sync::Arc, time::Duration};
 use ic_p2p_test_utils::{
     consensus::TestConsensus,
     turmoil::{
-        add_peer_manager_to_sim, add_transport_to_sim, wait_for, wait_for_timeout, waiter_fut,
-        PeerManagerAction,
+        add_peer_manager_to_sim, add_transport_to_sim, run_simulation_for, wait_for,
+        wait_for_timeout, waiter_fut, PeerManagerAction,
     },
 };
 use ic_test_utilities_logger::with_test_replica_logger;
@@ -12,6 +12,8 @@ use ic_types::RegistryVersion;
 use ic_types_test_utils::ids::{NODE_1, NODE_2, NODE_3};
 use tokio::sync::Notify;
 use turmoil::Builder;
+
+const TIMEOUT_DURATION_TRIGGER: Duration = Duration::from_secs(5);
 
 #[test]
 fn test_artifact_sent_to_other_peer() {
@@ -198,7 +200,6 @@ fn test_flapping_connection_does_not_cause_duplicate_artifact_downloads() {
             Some(processor_2.clone()),
             waiter_fut(),
         );
-
         peer_manager_cmd_sender
             .send(PeerManagerAction::Add((NODE_1, RegistryVersion::from(2))))
             .unwrap();
@@ -228,6 +229,98 @@ fn test_flapping_connection_does_not_cause_duplicate_artifact_downloads() {
         wait_for(&mut sim, || processor_2.received_advert_once(2)).unwrap();
 
         assert!(processor_2.received_advert_once(1));
+
+        exit_notify.notify_waiters();
+        sim.run().unwrap();
+    });
+}
+
+/// Test that nodes retransmit adverts to a peer that reconnects.
+#[test]
+fn test_adverts_are_retransmitted_on_reconnection() {
+    with_test_replica_logger(|log| {
+        let mut sim = Builder::new()
+            .tick_duration(Duration::from_millis(100))
+            .simulation_duration(Duration::from_secs(20))
+            .build();
+
+        let exit_notify = Arc::new(Notify::new());
+        let (peer_manager_cmd_sender, topology_watcher, registry_handle) =
+            add_peer_manager_to_sim(&mut sim, exit_notify.clone(), log.clone());
+        let processor_1 = TestConsensus::new(log.clone(), NODE_1);
+        let processor_2 = TestConsensus::new(log.clone(), NODE_2);
+        let processor_3 = TestConsensus::new(log.clone(), NODE_3);
+
+        add_transport_to_sim(
+            &mut sim,
+            log.clone(),
+            NODE_1,
+            registry_handle.clone(),
+            topology_watcher.clone(),
+            None,
+            None,
+            None,
+            None,
+            Some(processor_1.clone()),
+            waiter_fut(),
+        );
+        add_transport_to_sim(
+            &mut sim,
+            log.clone(),
+            NODE_2,
+            registry_handle.clone(),
+            topology_watcher.clone(),
+            None,
+            None,
+            None,
+            None,
+            Some(processor_2.clone()),
+            waiter_fut(),
+        );
+        add_transport_to_sim(
+            &mut sim,
+            log.clone(),
+            NODE_3,
+            registry_handle.clone(),
+            topology_watcher.clone(),
+            None,
+            None,
+            None,
+            None,
+            Some(processor_3.clone()),
+            waiter_fut(),
+        );
+        peer_manager_cmd_sender
+            .send(PeerManagerAction::Add((NODE_1, RegistryVersion::from(2))))
+            .unwrap();
+        peer_manager_cmd_sender
+            .send(PeerManagerAction::Add((NODE_2, RegistryVersion::from(3))))
+            .unwrap();
+        peer_manager_cmd_sender
+            .send(PeerManagerAction::Add((NODE_3, RegistryVersion::from(4))))
+            .unwrap();
+
+        registry_handle.registry_client.reload();
+        registry_handle.registry_client.update_to_latest_version();
+
+        processor_1.push_advert(1);
+
+        wait_for(&mut sim, || processor_2.received_advert_once(1))
+            .expect("NODE_2 should receive `advert 1` from `NODE_1`.");
+
+        wait_for(&mut sim, || processor_3.received_advert_once(1))
+            .expect("NODE_3 should receive `advert 1` from `NODE_1`.");
+
+        // Crash `NODE_3` nodes, and run simulation for 5s for `NODE_1` to detect disconnection.
+        sim.crash(NODE_3.to_string());
+        run_simulation_for(&mut sim, TIMEOUT_DURATION_TRIGGER).unwrap();
+        sim.bounce(NODE_3.to_string());
+
+        wait_for(&mut sim, || processor_3.received_advert_count(1) == 2)
+            .expect("NODE_3 should receive `advert 1` twice from `NODE_1` since it reconnected.");
+
+        wait_for(&mut sim, || processor_2.received_advert_once(1))
+            .expect("NODE_2 should receive `advert 1` from `NODE_1`.");
 
         exit_notify.notify_waiters();
         sim.run().unwrap();
