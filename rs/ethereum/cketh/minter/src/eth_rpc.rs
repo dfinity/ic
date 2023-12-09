f0c84f5751bf19f78a7757945e3b761df2104fae
rvanasa <ryan.vandersmith@dfinity.org>
2023-12-08 18:51:09 -0700

    Add individual 'serde(rename)' attributes to 'LogEntry'

--- rs/ethereum/cketh/minter/src/eth_rpc.rs
+++ rs/ethereum/cketh/minter/src/eth_rpc.rs
@@ -291,7 +291,6 @@ pub struct GetLogsParam {
 //  }
 // ```
 #[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, CandidType)]
-#[serde(rename_all = "camelCase")]
 pub struct LogEntry {
     /// The address from which this log originated.
     pub address: Address,
@@ -303,18 +302,23 @@ pub struct LogEntry {
     pub data: Data,
     /// The block number in which this log appeared.
     /// None if the block is pending.
+    #[serde(rename = "blockNumber")]
     pub block_number: Option<BlockNumber>,
     // 32 Bytes - hash of the transactions from which this log was created.
     // None when its pending log.
+    #[serde(rename = "transactionHash")]
     pub transaction_hash: Option<Hash>,
     // Integer of the transactions position within the block the log was created from.
     // None if the log is pending.
+    #[serde(rename = "transactionIndex")]
     pub transaction_index: Option<CheckedAmountOf<()>>,
     /// 32 Bytes - hash of the block in which this log appeared.
     /// None if the block is pending.
+    #[serde(rename = "blockHash")]
     pub block_hash: Option<Hash>,
     /// Integer of the log index position in the block.
     /// None if the log is pending.
+    #[serde(rename = "logIndex")]
     pub log_index: Option<LogIndex>,
     /// "true" when the log was removed due to a chain reorganization.
     /// "false" if it's a valid log.
