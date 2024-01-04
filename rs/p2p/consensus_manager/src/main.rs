af248cfc019189f8b453c4ef5781813d50606869
Daniel Sharifi <daniel.sharifi@dfinity.org>
2024-01-04 15:05:45 +0000

    .

--- rs/p2p/consensus_manager/src/main.rs
+++ rs/p2p/consensus_manager/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     net::{IpAddr, SocketAddr},
     str::FromStr,
     sync::{Arc, RwLock},
-    time::Duration,
+    time::{Duration, Instant, SystemTime, UNIX_EPOCH},
 };
 
 use axum::headers::Location;
@@ -51,6 +51,11 @@ use tokio_util::time::DelayQueue;
 #[derive(PartialEq, Eq, Debug, Clone)]
 pub struct TestArtifact;
 
+type NodeIdAttribute = ();
+// 24 bytes
+// [message_id: [0..8], artifact_producer_node_id: [8..16], time_stamp: [16..24]]
+type MessageId = Vec<u8>;
+
 impl ArtifactKind for TestArtifact {
     // Does not matter
     const TAG: ArtifactTag = ArtifactTag::ConsensusArtifact;
@@ -59,21 +64,26 @@ impl ArtifactKind for TestArtifact {
     type PbMessageError = Infallible;
     type PbAttributeError = Infallible;
     type PbFilterError = Infallible;
-    type PbAttribute = ();
+
+    type PbAttribute = NodeIdAttribute;
+    type Attribute = NodeIdAttribute;
+
+    type PbId = MessageId;
+    type Id = MessageId;
 
     type Message = Vec<u8>;
-    type PbId = u64;
-    type Id = u64;
-    type Attribute = ();
     type PbFilter = ();
     type Filter = ();
 
     /// The function converts a TestArtifactMessage to an advert for a
     /// TestArtifact.
     fn message_to_advert(msg: &Self::Message) -> Advert<TestArtifact> {
-        let id = u64::from_le_bytes(msg[..8].try_into().unwrap());
+        // let id = u64::from_le_bytes(msg[..24].try_into().unwrap());
+        let id = msg[..24].into();
+        let attribute = ();
+
         Advert {
-            attribute: (),
+            attribute,
             size: msg.len(),
             id,
             integrity_hash: CryptoHash(vec![]),
@@ -89,8 +99,9 @@ impl ValidatedPoolReader<TestArtifact> for TestConsensus {
         &self,
         id: &<TestArtifact as ArtifactKind>::Id,
     ) -> Option<<TestArtifact as ArtifactKind>::Message> {
-        let mut id = id.to_le_bytes().to_vec();
+        let mut id = id.clone();
         id.resize(self.message_size, 0);
+
         Some(id)
     }
     fn get_all_validated_by_filter(
@@ -102,6 +113,7 @@ impl ValidatedPoolReader<TestArtifact> for TestConsensus {
 }
 
 impl PriorityFnAndFilterProducer<TestArtifact, TestConsensus> for TestConsensus {
+    /// Evaluates to drop iff this node produced it originally or it is older than `RELAY_LIFETIME`
     fn get_priority_function(
         &self,
         _pool: &TestConsensus,
@@ -109,7 +121,29 @@ impl PriorityFnAndFilterProducer<TestArtifact, TestConsensus> for TestConsensus 
         <TestArtifact as ArtifactKind>::Id,
         <TestArtifact as ArtifactKind>::Attribute,
     > {
-        Box::new(|_, _| Priority::Fetch)
+        let node_id = self.node_id;
+        Box::new(move |id, _attribute| {
+            let message_id = u64::from_le_bytes(id[..8].try_into().unwrap());
+            let artifact_producer_node_id = u64::from_le_bytes(id[8..16].try_into().unwrap());
+            let message_is_relayed_back = artifact_producer_node_id == node_id;
+
+            assert!(
+                !message_is_relayed_back,
+                "Message should not be relayed back to produced of an artifact."
+            );
+
+            let expiry_time_secs = u64::from_le_bytes(id[16..24].try_into().unwrap());
+
+            let expiry_time = UNIX_EPOCH
+                .checked_add(Duration::from_secs(expiry_time_secs))
+                .unwrap();
+
+            if expiry_time >= SystemTime::now() {
+                Priority::Drop
+            } else {
+                Priority::Fetch
+            }
+        })
     }
     fn get_filter(&self) -> <TestArtifact as ArtifactKind>::Filter {
         <TestArtifact as ArtifactKind>::Filter::default()
@@ -119,6 +153,7 @@ impl PriorityFnAndFilterProducer<TestArtifact, TestConsensus> for TestConsensus 
 #[derive(Clone)]
 pub struct TestConsensus {
     pub message_size: usize,
+    pub node_id: u64,
 }
 
 /// Simple program to greet a person
@@ -145,70 +180,93 @@ struct Args {
 }
 
 async fn load_generator(
+    node_id: u64,
     log: ReplicaLogger,
-    to_cm: tokio::sync::mpsc::Sender<ArtifactProcessorEvent<TestArtifact>>,
-    from_cm: crossbeam_channel::Receiver<UnvalidatedArtifactMutation<TestArtifact>>,
+    produced_artifacts: tokio::sync::mpsc::Sender<ArtifactProcessorEvent<TestArtifact>>,
+    received_artifacts: crossbeam_channel::Receiver<UnvalidatedArtifactMutation<TestArtifact>>,
     message_rate: usize,
     message_size: usize,
     relaying: bool,
 ) {
-    let (tx, mut rx) = tokio::sync::mpsc::channel(1000);
-    let jh = std::thread::spawn(move || loop {
-        let msg = from_cm.recv().unwrap();
-        tx.blocking_send(msg).unwrap();
+    let (tx, mut received_artifacts_rx) = tokio::sync::mpsc::channel(1000);
+    let _join_handle = std::thread::spawn(move || {
+        while let Ok(message) = received_artifacts.recv() {
+            tx.blocking_send(message).unwrap();
+        }
     });
 
-    let mut dq = DelayQueue::new();
+    let mut purge_queue: DelayQueue<Advert<TestArtifact>> = DelayQueue::new();
 
-    let mut interval = tokio::time::interval(
+    let mut produce_and_send_artifact = tokio::time::interval(
         Duration::from_secs(1)
             .checked_div(message_rate as u32)
             .unwrap(),
     );
 
-    // Calulated such that not exceed 20000 active adverts
-    let removal_delay = 20000 / message_rate as u64;
-    info!(log, "Using removal delay of {removal_delay}s");
-    info!(log, "Generatiing event evey {:?}", interval.period());
+    // Calculated such that not exceed 20000 active adverts
+    let removal_delay_secs = 20000 / message_rate as u64;
+    let removal_delay = Duration::from_secs(removal_delay_secs);
+    info!(log, "Using removal delay of {removal_delay_secs}s");
+
+    info!(
+        log,
+        "Generatiing event evey {:?}",
+        produce_and_send_artifact.period()
+    );
     let mut log_interval = tokio::time::interval(Duration::from_secs(10));
     let mut received_bytes = 0;
     let mut received_bytes_last = 0;
-    let mut received_msgs = 0;
+    let mut received_message_count = 0;
     let mut received_msgs_last = 0;
+
+    let mut current_messag_id: u64 = 0;
 
     loop {
         select! {
-            Some(r) = rx.recv()=> {
-                match r {
-                    UnvalidatedArtifactMutation::Insert((message,peer)) => {
-                        received_msgs += 1;
+            Some(artifact) = received_artifacts_rx.recv()=> {
+                match artifact {
+                    UnvalidatedArtifactMutation::<TestArtifact>::Insert((message, _peer)) => {
+                        received_message_count += 1;
                         received_bytes += message.len();
                         if relaying {
-                            dq.insert(TestArtifact::message_to_advert(&message), Duration::from_secs(removal_delay));
-                            to_cm.send(ArtifactProcessorEvent::Advert(TestArtifact::message_to_advert(&message))).await;
+                            let expiry_time_secs = u64::from_le_bytes(message[16..24].try_into().unwrap());
+
+                            let removal_delay = UNIX_EPOCH
+                                .checked_add(Duration::from_secs(expiry_time_secs))
+                                .unwrap()
+                                .elapsed();
+
+                            if let Ok(removal_delay) = removal_delay {
+                                purge_queue.insert(TestArtifact::message_to_advert(&message), removal_delay);
+                                produced_artifacts.send(ArtifactProcessorEvent::Advert(TestArtifact::message_to_advert(&message))).await;
+                            }
                         }
                     }
                     _ => {}
                 }
             }
-            _ = interval.tick() => {
-                let id = {
-                    let mut rng = thread_rng();
-                    rng.gen_range(0..u64::MAX)
-                };
-                let mut id = id.to_le_bytes().to_vec();
-                id.resize(message_size, 0);
-                dq.insert(TestArtifact::message_to_advert(&id), Duration::from_secs(removal_delay));
-                to_cm.send(ArtifactProcessorEvent::Advert(TestArtifact::message_to_advert(&id))).await;
+            _ = produce_and_send_artifact.tick() => {
+                let expiry_time = SystemTime::now().checked_add(removal_delay).unwrap().duration_since(UNIX_EPOCH).unwrap().as_secs();
+
+                let mut id = Vec::with_capacity(24);
+                id.extend_from_slice(&current_messag_id.to_le_bytes());
+                id.extend_from_slice(&node_id.to_le_bytes());
+                id.extend_from_slice(&expiry_time.to_le_bytes());
+
+                purge_queue.insert(TestArtifact::message_to_advert(&id), removal_delay);
+                produced_artifacts.send(ArtifactProcessorEvent::Advert(TestArtifact::message_to_advert(&id))).await;
+
+                current_messag_id += 1;
+
             }
             _ = log_interval.tick() => {
-                info!(log, "Rate of messages received {}", (received_msgs - received_msgs_last)/10);
+                info!(log, "Rate of messages received {}", (received_message_count - received_msgs_last)/10);
                 info!(log, "Rate of bytes received {}", (received_bytes - received_bytes_last)/10);
-                received_msgs_last = received_msgs;
+                received_msgs_last = received_message_count;
                 received_bytes_last = received_bytes;
             }
-            Some(n) = dq.next() => {
-                to_cm.send(ArtifactProcessorEvent::Purge(n.into_inner().id));
+            Some(n) = purge_queue.next() => {
+                produced_artifacts.send(ArtifactProcessorEvent::Purge(n.into_inner().id));
             }
         }
     }
@@ -219,7 +277,8 @@ fn main() {
 
     // generate deterministic crypto based on id
     let mut seeded_rng = ChaCha20Rng::seed_from_u64(args.id);
-    let node_id = node_test_id(seeded_rng.gen_range(0..u64::MAX));
+    let node_id_u64 = seeded_rng.gen_range(0..u64::MAX);
+    let node_id = node_test_id(node_id_u64);
     let cert = CertWithPrivateKey::builder()
         .cn(node_id.to_string())
         .build_ed25519(&mut seeded_rng);
@@ -236,15 +295,16 @@ fn main() {
         rt.handle().clone(),
         MetricsRegistry::default(),
     );
-    let (test_tx, test_rx) = tokio::sync::mpsc::channel(100000);
-    // let (relay_tx, relay_rx) = tokio::sync::mpsc::unbounded_channel();
+    let (incoming_artifact_tx, incoming_artifact_rx) = tokio::sync::mpsc::channel(100000);
     let (cb_tx, cb_rx) = crossbeam_channel::unbounded();
+
     let test_consensus = TestConsensus {
         message_size: args.message_size,
+        node_id: node_id_u64,
     };
 
     new_p2p_consensus.add_client(
-        test_rx,
+        incoming_artifact_rx,
         Arc::new(RwLock::new(test_consensus.clone())) as Arc<_>,
         Arc::new(test_consensus.clone()) as Arc<_>,
         cb_tx,
@@ -289,8 +349,9 @@ fn main() {
     new_p2p_consensus.run(quic_transport, watcher);
 
     rt.spawn(load_generator(
+        node_id_u64,
         log.clone(),
-        test_tx,
+        incoming_artifact_tx,
         cb_rx,
         args.message_rate as usize,
         args.message_size as usize,
