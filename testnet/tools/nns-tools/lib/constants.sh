fd3ac2b6658a9ca57c2edf346b30aea99cd0df2d
Jason (Yeli) Zhu <jason.zhu@dfinity.org>
2024-01-24 23:05:46 +0000

    refactor(sns-release): Refactor sns release testing scripts related to WASM hashes

---
+++ testnet/tools/nns-tools/lib/constants.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export NNS_CANISTERS=(
+    cycles-minting
+    governance
+    genesis-token
+    lifeline
+    registry
+    root
+    sns-wasm
+)
+
+export SNS_CANISTERS=(
+    swap
+    root
+    governance
+    ledger
+    index
+    archive
+)
