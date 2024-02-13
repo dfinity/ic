cc64e58ebdd48ce118344ab70515670ef66748c4
Andrew Battat <andrew.battat@dfinity.org>
2024-02-13 17:51:35 +0000

    fix: IPv4 connectivity check

--- ic-os/guestos/rootfs/opt/ic/bin/ipv4-connectivity-check.sh
+++ ic-os/guestos/rootfs/opt/ic/bin/ipv4-connectivity-check.sh
@@ -20,7 +20,7 @@ connectivity_status=0
 
 for endpoint in "${endpoints[@]}"; do
     # Using curl instead of ping as it requires less permissions
-    if curl --connect-timeout 10 "${endpoint}" &>/dev/null; then
+    if curl --ipv4 --connect-timeout 15 "${endpoint}" &>/dev/null; then
         connectivity_status=1
         break
     fi
