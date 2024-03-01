14fcdf9f21af52b39e1e6613463f4b4aee78658d
Leo Eichhorn <leo.eichhorn@dfinity.org>
2024-03-01 12:22:51 +0000

    feat(ecdsa): CON-1240 Allow multiple signing subnets for the same key

--- rs/tests/src/tecdsa/mod.rs
+++ rs/tests/src/tecdsa/mod.rs
@@ -30,6 +30,7 @@ pub mod tecdsa_add_nodes_test;
 pub mod tecdsa_complaint_test;
 pub mod tecdsa_remove_nodes_test;
 pub mod tecdsa_signature_test;
+pub mod tecdsa_two_signing_subnets_test;
 
 pub(crate) const KEY_ID1: &str = "secp256k1";
 pub(crate) const KEY_ID2: &str = "some_other_key";
