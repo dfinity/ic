635db6088f55763cf9baacaa9e3456cdb816358f
Jack Lloyd <jack.lloyd@dfinity.org>
2024-03-08 16:55:45 +0000

    chore(crypto): CRP-2423 Address Ed25519 crate review comments

--- rs/crypto/ed25519/src/lib.rs
+++ rs/crypto/ed25519/src/lib.rs
@@ -1,67 +1,50 @@
+#![warn(rust_2018_idioms)]
+#![warn(future_incompatible)]
+#![forbid(missing_docs)]
+#![forbid(unsafe_code)]
+
+//! A crate for creating and verifying Ed25519 signatures
+
 use ed25519_dalek::pkcs8::{DecodePrivateKey, DecodePublicKey, EncodePrivateKey, EncodePublicKey};
 use ed25519_dalek::{verify_batch, Signature, Signer, SigningKey, VerifyingKey};
 use rand::{CryptoRng, Rng};
 use zeroize::ZeroizeOnDrop;
 
-#[derive(Copy, Clone, Debug)]
-pub enum SecretKeyDecodingError {
-    IncorrectLength,
-    InvalidKeyEncoding,
+/// An error if a private key cannot be decoded
+#[derive(Clone, Debug)]
+pub enum PrivateKeyDecodingError {
+    /// The private key seems invalid in some way; the string contains details
+    InvalidKeyEncoding(String),
 }
 
 /// An Ed25519 secret key
 #[derive(Clone, ZeroizeOnDrop, Eq, PartialEq)]
-pub struct SecretKey {
+pub struct PrivateKey {
     sk: SigningKey,
 }
 
-impl std::fmt::Debug for SecretKey {
+impl std::fmt::Debug for PrivateKey {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("SecretKey")
+        f.debug_struct("PrivateKey")
             .field("public_key", &self.public_key().serialize_raw())
             .finish_non_exhaustive() // avoids printing secret information
     }
 }
 
-impl SecretKey {
+impl PrivateKey {
     /// The length in bytes of the raw private key
     pub const BYTES: usize = 32;
 
     /// Create a new random secret Ed25519 key
-    pub fn generate_key<R: CryptoRng + Rng>(rng: &mut R) -> Self {
+    pub fn generate() -> Self {
+        let mut rng = rand::thread_rng();
+        Self::generate_using_rng(&mut rng)
+    }
+
+    /// Create a new random secret Ed25519 key using specified RNG
+    pub fn generate_using_rng<R: CryptoRng + Rng>(rng: &mut R) -> Self {
         let sk = SigningKey::generate(rng);
         Self { sk }
-    }
-
-    /// Deserialize an Ed25519 key from raw format
-    ///
-    /// This is just the plain 32 byte random seed from which the
-    /// internal key material is derived
-    ///
-    /// This corresponds with the format used by SecretKey::serialize
-    pub fn deserialize_raw(bytes: &[u8]) -> Result<Self, SecretKeyDecodingError> {
-        let bytes = <[u8; Self::BYTES]>::try_from(bytes)
-            .map_err(|_| SecretKeyDecodingError::IncorrectLength)?;
-        let sk = SigningKey::from_bytes(&bytes);
-        Ok(Self { sk })
-    }
-
-    /// Deserialize an Ed25519 key from PKCS8 format
-    ///
-    /// This corresponds with the format used by SecretKey::serialize_pkcs8
-    pub fn deserialize_pkcs8(bytes: &[u8]) -> Result<Self, SecretKeyDecodingError> {
-        let sk = SigningKey::from_pkcs8_der(bytes)
-            .map_err(|_| SecretKeyDecodingError::InvalidKeyEncoding)?;
-        Ok(Self { sk })
-    }
-
-    /// Deserialize an Ed25519 key from PKCS8 PEM format
-    ///
-    /// This corresponds with the format used by SecretKey::serialize_pkcs8
-    pub fn deserialize_pkcs8_pem(pem: &str) -> Result<Self, SecretKeyDecodingError> {
-        let sk = SigningKey::from_pkcs8_pem(pem)
-            .map_err(|_| SecretKeyDecodingError::InvalidKeyEncoding)?;
-        Ok(Self { sk })
     }
 
     /// Sign a message and return a signature
@@ -85,58 +68,157 @@ impl SecretKey {
         self.sk.to_bytes()
     }
 
-    /// Serialize the Ed25519 secret key in PKCS8 format
+    /// Deserialize an Ed25519 private key from raw format
     ///
-    /// This is the "v2" PKCS8 format which includes the public key
+    /// This is just the plain 32 byte random seed from which the
+    /// internal key material is derived
+    ///
+    /// This corresponds with the format used by PrivateKey::serialize
+    pub fn deserialize_raw(bytes: &[u8]) -> Result<Self, PrivateKeyDecodingError> {
+        let bytes = <[u8; Self::BYTES]>::try_from(bytes).map_err(|_| {
+            PrivateKeyDecodingError::InvalidKeyEncoding(format!(
+                "Expected key of exactly {} bytes, got {}",
+                Self::BYTES,
+                bytes.len()
+            ))
+        })?;
+
+        let sk = SigningKey::from_bytes(&bytes);
+        Ok(Self { sk })
+    }
+
+    /// Serialize the Ed25519 secret key in PKCS8 v2 format
+    ///
+    /// This is the v2 PKCS8 format, which includes the public key
+    ///
+    /// # Warning
+    ///
+    /// Some software, notably OpenSSL, does not understand the v2 PKCS8 format.
     pub fn serialize_pkcs8(&self) -> Vec<u8> {
-        self.sk
-            .to_pkcs8_der()
+        let pkcs8 = self.sk.to_pkcs8_der();
+
+        // Key encoding with the pkcs8 crate can fail, largely to allow for
+        // falliable encoding on the part of the algorithm specific code.  But
+        // logically speaking, as long as the key is valid (which we've already
+        // checked) then there is no reason for encoding to ever fail outside of
+        // memory allocation errors. None of the error types that to_pkcs8_der
+        // can return have any relevance to encoding. And the dalek encoding
+        // functions themselves do not have any error cases.
+        pkcs8
             .expect("Failed to encode key as PKCS8")
             .to_bytes()
             .to_vec()
     }
 
-    /// Serialize the Ed25519 secret key in PKCS8 format with PEM encoding
+    /// Serialize the Ed25519 secret key in PKCS8 v1 format
+    ///
+    /// This is the v1 PKCS8 format, which omits the public key
+    ///
+    /// Use this only if required to interop with software which does
+    /// not understand the v2 PKCS8 format
+    pub fn serialize_pkcs8_v1(&self) -> Vec<u8> {
+        const DER_PREFIX: [u8; 16] = [
+            48, 46, // A sequence of 46 bytes follows.
+            2, 1, // An integer denoting version
+            0, // 0 if secret key only, 1 if public key is also present
+            48, 5, // An element of 5 bytes follows
+            6, 3, 43, 101, 112, // Object ID (6), length 3, value 1.3.101.112
+            4, 34, // An octet string of 34 bytes follows.
+            4, 32, // An octet string of 32 bytes follows.
+        ];
+
+        let mut pkcs8v1 = Vec::with_capacity(DER_PREFIX.len() + Self::BYTES);
+
+        pkcs8v1.extend_from_slice(&DER_PREFIX);
+        pkcs8v1.extend_from_slice(&self.serialize_raw());
+
+        pkcs8v1
+    }
+
+    /// Deserialize an Ed25519 private key from PKCS8 format
+    ///
+    /// Both v1 and v2 PKCS8 encodings are accepted. The only difference is
+    /// that v2 includes the public key as well.
+    ///
+    /// This corresponds with the format used by PrivateKey::serialize_pkcs8
+    pub fn deserialize_pkcs8(bytes: &[u8]) -> Result<Self, PrivateKeyDecodingError> {
+        let sk = SigningKey::from_pkcs8_der(bytes)
+            .map_err(|e| PrivateKeyDecodingError::InvalidKeyEncoding(format!("{:?}", e)))?;
+        Ok(Self { sk })
+    }
+
+    /// Serialize the Ed25519 secret key in PKCS8 v2 format with PEM encoding
+    ///
+    /// This is the v2 PKCS8 format which includes the public key
+    ///
+    /// # Warning
+    ///
+    /// Some software, notably OpenSSL, does not understand the v2 PKCS8 format.
     pub fn serialize_pkcs8_pem(&self) -> String {
-        self.sk
-            .to_pkcs8_pem(Default::default())
-            .expect("Failed to encode key as PKCS8")
-            .to_string()
+        let pkcs8 = self.sk.to_pkcs8_pem(Default::default());
+
+        // See comment in serialize_pkcs8 regarding this expect
+        pkcs8.expect("Failed to encode key as PKCS8").to_string()
+    }
+
+    /// Deserialize an Ed25519 private key from PKCS8 PEM format
+    ///
+    /// Both v1 and v2 PKCS8 encodings are accepted
+    ///
+    /// This corresponds with the format used by PrivateKey::serialize_pkcs8_pem
+    pub fn deserialize_pkcs8_pem(pem: &str) -> Result<Self, PrivateKeyDecodingError> {
+        let sk = SigningKey::from_pkcs8_pem(pem)
+            .map_err(|e| PrivateKeyDecodingError::InvalidKeyEncoding(format!("{:?}", e)))?;
+        Ok(Self { sk })
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+/// An invalid key was encountered
+#[derive(Clone, Debug)]
 pub enum PublicKeyDecodingError {
-    InvalidLength,
-    InvalidPublicKey,
+    /// The encoding of the public key is invalid, the string contains details
+    InvalidKeyEncoding(String),
+    /// The public key had a valid encoding, but contains elements of the torsion
+    /// subgroup. This should never happen with a non-malicious peer.
     KeyNotTorsionFree,
 }
 
+/// An Ed25519 public key
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 #[repr(transparent)]
 pub struct PublicKey {
     pk: VerifyingKey,
 }
 
+/// An error that occurs when verifying signatures or batches of signatures
 #[derive(Copy, Clone, Debug)]
 pub enum SignatureError {
+    /// The signature had an invalid length, and cannot possibly be valid
     InvalidLength,
+    /// The batch was invalid (eg due to length mismatch between number of
+    /// messages and number of signatures)
     InvalidBatch,
+    /// A signature was invalid
     InvalidSignature,
 }
 
 impl PublicKey {
+    /// The number of bytes in the raw public key
     pub const BYTES: usize = 32;
 
     /// Internal constructor
     ///
     /// Checks the point for validity before returning
     fn new(pk: VerifyingKey) -> Result<Self, PublicKeyDecodingError> {
-        // TODO(CRP-2412) This can be changed to `to_edwards().is_torsion_free()` once
+        // TODO(CRP-2412) This can be changed to `pk.to_edwards().is_torsion_free()` once
         // https://github.com/dalek-cryptography/curve25519-dalek/issues/624
         // makes it into a release
-        if !pk.to_montgomery().to_edwards(0).unwrap().is_torsion_free() {
-            return Err(PublicKeyDecodingError::KeyNotTorsionFree)?;
+        if !curve25519_dalek::edwards::CompressedEdwardsY(pk.to_bytes())
+            .decompress()
+            .unwrap()
+            .is_torsion_free()
+        {
+            return Err(PublicKeyDecodingError::KeyNotTorsionFree);
         }
 
         // We don't need to call is_weak here since that is subsumed by the
@@ -146,18 +228,6 @@ impl PublicKey {
         Ok(Self { pk })
     }
 
-    /// Deserialize a public key in raw format
-    ///
-    /// This is just the 32 byte encoding of the public point
-    pub fn deserialize_raw(bytes: &[u8]) -> Result<Self, PublicKeyDecodingError> {
-        let bytes = <[u8; Self::BYTES]>::try_from(bytes)
-            .map_err(|_| PublicKeyDecodingError::InvalidLength)?;
-        let pk = VerifyingKey::from_bytes(&bytes)
-            .map_err(|_| PublicKeyDecodingError::InvalidPublicKey)?;
-
-        Self::new(pk)
-    }
-
     /// Serialize this public key in raw format
     ///
     /// This is just the 32 byte encoding of the public point
@@ -165,23 +235,43 @@ impl PublicKey {
         *self.pk.as_bytes()
     }
 
-    /// Serialize this public key as a DER encoded structure
+    /// Deserialize a public key in raw format
     ///
-    /// See RFC 8410 for details on the format
-    pub fn serialize_rfc8410_der(&self) -> Vec<u8> {
-        self.pk
-            .to_public_key_der()
-            .expect("Encoding public key as DER failed")
-            .as_bytes()
-            .to_vec()
+    /// This is just the 32 byte encoding of the public point,
+    /// cooresponding to Self::serialize_raw
+    pub fn deserialize_raw(bytes: &[u8]) -> Result<Self, PublicKeyDecodingError> {
+        let bytes = <[u8; Self::BYTES]>::try_from(bytes).map_err(|_| {
+            PublicKeyDecodingError::InvalidKeyEncoding(format!(
+                "Expected key of exactly {} bytes, got {}",
+                Self::BYTES,
+                bytes.len()
+            ))
+        })?;
+        let pk = VerifyingKey::from_bytes(&bytes)
+            .map_err(|e| PublicKeyDecodingError::InvalidKeyEncoding(format!("{:?}", e)))?;
+
+        Self::new(pk)
     }
 
     /// Serialize this public key as a DER encoded structure
     ///
     /// See RFC 8410 for details on the format
+    pub fn serialize_rfc8410_der(&self) -> Vec<u8> {
+        let der = self.pk.to_public_key_der();
+
+        // See comment in serialize_pkcs8 regarding this expect
+        der.expect("Encoding public key as DER failed")
+            .as_bytes()
+            .to_vec()
+    }
+
+    /// Deserialize the DER encoded public key
+    ///
+    /// See RFC 8410 for details on the format. This cooresponds to
+    /// Self::serialize_rfc8410_der
     pub fn deserialize_rfc8410_der(bytes: &[u8]) -> Result<Self, PublicKeyDecodingError> {
         let pk = VerifyingKey::from_public_key_der(bytes)
-            .map_err(|_| PublicKeyDecodingError::InvalidPublicKey)?;
+            .map_err(|e| PublicKeyDecodingError::InvalidKeyEncoding(format!("{:?}", e)))?;
         Self::new(pk)
     }
 
@@ -221,13 +311,12 @@ impl PublicKey {
             .collect::<Result<Vec<_>, _>>()
             .map_err(|_| SignatureError::InvalidLength)?;
 
-        // This is safe because Self is a repr(transparent) wrapper around VerifyingKey
+        // We could use std::slice::from_raw_parts to avoid these copies, but
+        // we'd rather avoid unsafe unless strictly necessary.
         //
-        // Alternate approach, at the cost of copying all of the keys
-        // let keys = keys.iter().map(|k| k.pk).collect::<Vec<_>>();
-        let keys =
-            unsafe { std::slice::from_raw_parts(keys.as_ptr() as *const VerifyingKey, keys.len()) };
+        // unsafe { std::slice::from_raw_parts(keys.as_ptr() as *const VerifyingKey, keys.len()) };
+        let keys = keys.iter().map(|k| k.pk).collect::<Vec<_>>();
 
-        verify_batch(messages, &signatures, keys).map_err(|_| SignatureError::InvalidSignature)
+        verify_batch(messages, &signatures, &keys).map_err(|_| SignatureError::InvalidSignature)
     }
 }
