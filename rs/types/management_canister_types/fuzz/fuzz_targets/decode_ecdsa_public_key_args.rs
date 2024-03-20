d3963ed254569ac1ea0eafd9c0f14cb7affeeb7d
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2024-03-20 16:48:11 +0000

    chore(fuzzing): Update candid fuzzers to use the new skipping_quota

--- rs/types/management_canister_types/fuzz/fuzz_targets/decode_ecdsa_public_key_args.rs
+++ rs/types/management_canister_types/fuzz/fuzz_targets/decode_ecdsa_public_key_args.rs
@@ -1,15 +1,11 @@
 #![no_main]
-use candid::Decode;
 use ic_management_canister_types::ECDSAPublicKeyArgs;
+use ic_management_canister_types::Payload;
 use libfuzzer_sys::fuzz_target;
 
 // This fuzz test feeds binary data to Candid's `Decode!` macro for ECDSAPublicKeyArgs with the goal of exposing panics
 // e.g. caused by stack overflows during decoding.
 
 fuzz_target!(|data: &[u8]| {
-    let payload = data.to_vec();
-    let _decoded = match Decode!(payload.as_slice(), ECDSAPublicKeyArgs) {
-        Ok(_v) => _v,
-        Err(_e) => return,
-    };
+    let _ = ECDSAPublicKeyArgs::decode(data);
 });
