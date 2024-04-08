8d09e42d81f86f44a6c76d034a075aa9036d2697
Dimitris Sarlis <dimitrios.sarlis@dfinity.org>
2024-04-08 09:58:30 +0000

    chore: Remove old reject code field

--- rs/protobuf/src/gen/state/state.queues.v1.rs
+++ rs/protobuf/src/gen/state/state.queues.v1.rs
@@ -86,8 +86,6 @@ pub struct Request {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct RejectContext {
-    #[prost(uint64, tag = "1")]
-    pub reject_code_old: u64,
     #[prost(enumeration = "super::super::super::types::v1::RejectCode", tag = "3")]
     pub reject_code: i32,
     #[prost(string, tag = "2")]
