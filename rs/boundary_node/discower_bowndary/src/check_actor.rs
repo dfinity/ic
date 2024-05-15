02f207acfd46059edf83681bb44439bcf6edea9b
Nikolay Komarevskiy <nikolay.komarevskiy@dfinity.org>
2024-05-15 09:40:49 +0000

    (BOUN-759) add latency-based snapshot to API BN Discovery Library

--- rs/boundary_node/discower_bowndary/src/check_actor.rs
+++ rs/boundary_node/discower_bowndary/src/check_actor.rs
@@ -4,12 +4,7 @@ use tokio::time;
 use tokio_util::sync::CancellationToken;
 use tracing::{debug, error};
 
-use crate::{
-    check::{HealthCheck, HealthCheckResult},
-    messages::NodeHealthChanged,
-    node::Node,
-    types::SenderMpsc,
-};
+use crate::{check::HealthCheck, messages::NodeHealthUpdate, node::Node, types::SenderMpsc};
 
 const SERVICE_NAME: &str = "HealthCheckActor";
 
@@ -17,8 +12,7 @@ pub struct HealthCheckActor {
     checker: Arc<dyn HealthCheck>,
     period: Duration,
     node: Node,
-    health_state: Option<HealthCheckResult>,
-    health_manager_sender: SenderMpsc<NodeHealthChanged>,
+    health_manager_sender: SenderMpsc<NodeHealthUpdate>,
     token: CancellationToken,
 }
 
@@ -27,7 +21,7 @@ impl HealthCheckActor {
         checker: Arc<dyn HealthCheck>,
         period: Duration,
         node: Node,
-        health_manager_sender: SenderMpsc<NodeHealthChanged>,
+        health_manager_sender: SenderMpsc<NodeHealthUpdate>,
         token: CancellationToken,
     ) -> Self {
         Self {
@@ -36,27 +30,23 @@ impl HealthCheckActor {
             node,
             health_manager_sender,
             token,
-            health_state: None,
         }
     }
 
-    pub async fn run(mut self) {
+    pub async fn run(self) {
         let mut interval = time::interval(self.period);
         loop {
             tokio::select! {
                 _ = interval.tick() => {
-                    let new_health = self.checker.check(&self.node).await.unwrap_or(HealthCheckResult {is_healthy: false});
-                    if self.health_state.as_ref() != Some(&new_health) {
-                        self.health_state = Some(new_health.clone());
-                        let health_changed = NodeHealthChanged {
-                            node: self.node.clone(),
-                            health: new_health,
-                        };
-                        // Send results back to health manager for updating the snapshot.
-                        // It can never fail in our case
-                        if let Err(err) = self.health_manager_sender.send(health_changed).await {
-                            error!("{SERVICE_NAME}: failed to send results to HealthManagerActor: {err:?}");
-                        }
+                    let new_health = self.checker.check(&self.node).await.unwrap_or_default();
+                    let health_update = NodeHealthUpdate {
+                        node: self.node.clone(),
+                        health: new_health,
+                    };
+                    // Send results back to health manager for updating the snapshot.
+                    // It can never fail in our case
+                    if let Err(err) = self.health_manager_sender.send(health_update).await {
+                        error!("{SERVICE_NAME}: failed to send results to HealthManagerActor: {err:?}");
                     }
                 }
                 _ = self.token.cancelled() => {
