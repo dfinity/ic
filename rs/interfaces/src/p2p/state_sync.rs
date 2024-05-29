6742513a6d4838246c16d47e651c12f817ff4d95
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-05-29 11:33:38 +0000

    refactor: improve the documentation for the StateSync/P2P api and rename some methods

--- rs/interfaces/src/p2p/state_sync.rs
+++ rs/interfaces/src/p2p/state_sync.rs
@@ -42,8 +42,13 @@ pub enum AddChunkError {
 pub struct ChunkIdTag;
 pub type ChunkId = Id<ChunkIdTag, u32>;
 
+/// A 'Chunkable' object is used to assemble a single ongoing state sync.
 pub trait Chunkable<T> {
+    /// Returns the remaining chunks needed to complete the state sync.
+    /// The list is dynamic and may change over time based on addition of new chunks.
+    /// The function will return empty iff the corresponding state sync is completed.
     fn chunks_to_download(&self) -> Box<dyn Iterator<Item = ChunkId>>;
+    /// Delivers the corresponding chunk.
     fn add_chunk(&mut self, chunk_id: ChunkId, chunk: Chunk) -> Result<(), AddChunkError>;
 }
 
@@ -52,19 +57,31 @@ pub trait StateSyncClient: Send + Sync {
 
     /// Returns a list of all states available.
     fn available_states(&self) -> Vec<StateSyncArtifactId>;
-    /// Initiates new state sync for the specified Id. Returns None if the state should not be synced.
-    /// If `Some(..)` is returned a new state sync is initiated.
-    /// Callers of this interface need to uphold the following: `start_state_sync` is not called again
-    /// before the previously returned object is dropped.
-    /// TODO: (NET-1469) In the future the mentioned caller restriction should be lifted.
-    fn start_state_sync(
+
+    /// Initiates new state sync for the specified Id. If `Some(..)` is returned a new state sync is initiated.
+    /// Returns None if the state should not be synced.
+    ///
+    /// Requires: callers of this interface should not invoke `maybe_start_state_sync`
+    /// unless the previously returned (Chunkable) object is dropped. In otherwords,
+    /// the caller of this API must assume that there can be a single ongoing state sync.
+    ///
+    /// TODO: In the future the mentioned caller restriction should be lifted or the API should be adjusted to
+    /// capture the requirement.
+    fn maybe_start_state_sync(
         &self,
         id: &StateSyncArtifactId,
     ) -> Option<Box<dyn Chunkable<Self::Message> + Send>>;
-    /// Returns true if a state sync with the specified Id can be cancelled because a newer state is available.
-    /// The result of this function is only meaningful the Id refers to a active state sync started with `start_state_sync`.
-    /// TODO: (NET-1469) In the future this API should be made safer by only allowing the id of a previously initiated state sync.
-    fn should_cancel(&self, id: &StateSyncArtifactId) -> bool;
-    /// Get a specific chunk from the specified state.
+
+    /// Returns true if is safe to cancel a potentially ongoing state sync.
+    ///
+    /// Notes on the interface.
+    ///
+    /// The decision to cancel an ongoing state sync is not the inverse of starting a new state sync.
+    /// One can imagine that the implementer wants to hold on for an older state sync for longer period of time
+    /// instead of cancelling and starting the new one. This assumption is even more important given the requirement
+    /// that there can be a single active state sync.
+    fn cancel_if_running(&self, id: &StateSyncArtifactId) -> bool;
+
+    /// Returns a specific chunk from the specified state.
     fn chunk(&self, id: &StateSyncArtifactId, chunk_id: ChunkId) -> Option<Chunk>;
 }
