d5c8af3c52c4b30cb779a85a61631622cce1e56c
Shuo Wang <shuo.wang@dfinity.org>
2024-05-30 03:59:29 -0700

    fix

--- rs/state_manager/tests/common/mod.rs
+++ rs/state_manager/tests/common/mod.rs
@@ -624,7 +624,10 @@ pub fn state_manager_restart_test_with_state_sync<Test>(test: Test)
 
         let restart_fn = Box::new(move |state_manager, state_sync, starting_height| {
             drop(state_sync);
-            let state_manager = Arc::try_unwrap(state_manager).expect("Please make sure other strong references of state_manager have been dropped");
+            let state_manager = match Arc::try_unwrap(state_manager) {
+                Ok(sm) => sm,
+                Err(_) => panic!("Please make sure other strong references of state_manager have been dropped"),
+            };
             drop(state_manager);
             make_state_manager(starting_height)
         });
