3834707ad45316a5a7217122c4904c8c5554e622
Marko Kosmerl <marko.kosmerl@dfinity.org>
2024-06-11 15:19:47 +0000

    change

--- ic-os/bootloader/build-bootloader-tree.sh
+++ ic-os/bootloader/build-bootloader-tree.sh
@@ -25,7 +25,7 @@ sudo mount -t tmpfs tmpfs-podman "/tmp/tmpfs-podman"
 
 BASE_IMAGE="docker.io/dfinity/ic-build-bazel@sha256:1978886cfda51b09057bffd60f2e5edb588c6c0b74de87696cd4e964335dba87"
 
-sudo podman --root /tmp/tmpfs-podman build --network=host --cgroupns=host --iidfile ${TMPDIR}/iidfile - <<<"
+sudo podman --root /tmp/tmpfs-podman build --iidfile ${TMPDIR}/iidfile - <<<"
     FROM $BASE_IMAGE
     USER root:root
     RUN mkdir -p /build/boot/grub
@@ -43,7 +43,7 @@ sudo podman --root /tmp/tmpfs-podman build --network=host --cgroupns=host --iidf
 
 IMAGE=$(cat ${TMPDIR}/iidfile | cut -d':' -f2)
 
-CONTAINER=$(sudo podman --root /tmp/tmpfs-podman run -d "$IMAGE")
+CONTAINER=$(sudo podman --root /tmp/tmpfs-podman run --network=host --cgroupns=host -d "$IMAGE")
 
 sudo podman --root /tmp/tmpfs-podman export "$CONTAINER" | tar -C "$TMPDIR" -x build --strip-components=1
 tar cf "${OUT_FILE}" --sort=name --owner=root:0 --group=root:0 "--mtime=UTC 1970-01-01 00:00:00" -C "${TMPDIR}" boot
