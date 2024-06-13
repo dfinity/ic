079301d32c83f4d25a87e5154bdb2b08e564113b
Carly Gundy <47304080+cgundy@users.noreply.github.com>
2024-06-13 09:56:30 +0200

    format

--- gitlab-ci/src/ci-scripts/docker-login.sh
+++ gitlab-ci/src/ci-scripts/docker-login.sh
@@ -1,9 +1,9 @@
 set -eEuo pipefail
 
 if which docker 2>/dev/null; then
-  docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
+    docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
 fi
 # docker-bin used by container_pull in WORKSPACES.bazel
 if which docker-bin 2>/dev/null; then
-  docker-bin login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
+    docker-bin login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
 fi
