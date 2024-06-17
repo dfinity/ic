1ea4ecaff8eb0850e44c1d7dd4a7ab63a51b5631
Carly Gundy <carlene.gundy@dfinity.org>
2024-06-17 17:20:41 +0200

    test without push

--- gitlab-ci/src/ci-scripts/rosetta-api-docker-image.sh
+++ gitlab-ci/src/ci-scripts/rosetta-api-docker-image.sh
@@ -22,11 +22,11 @@ docker run --rm dfinity/rosetta-api:"$CI_COMMIT_SHA" --help
 ROSETTA_API_DATE=$(date +"%Y%m%d")
 ROSETTA_API_VERSION=$(grep -e '^version' "$CI_PROJECT_DIR"/rs/rosetta-api/Cargo.toml | sed -e 's|^version[ ]*=[ ]*"\([^"]*\)"|\1|g')
 
-# docker tag dfinity/rosetta-api:"$CI_COMMIT_SHA" dfinity/rosetta-api:"$ROSETTA_API_DATE"
-# docker tag dfinity/rosetta-api:"$CI_COMMIT_SHA" dfinity/rosetta-api:v"$ROSETTA_API_VERSION"
-# docker tag dfinity/rosetta-api:"$CI_COMMIT_SHA" dfinity/rosetta-api:latest
+docker tag dfinity/rosetta-api:"$CI_COMMIT_SHA" dfinity/rosetta-api:"$ROSETTA_API_DATE"
+docker tag dfinity/rosetta-api:"$CI_COMMIT_SHA" dfinity/rosetta-api:v"$ROSETTA_API_VERSION"
+docker tag dfinity/rosetta-api:"$CI_COMMIT_SHA" dfinity/rosetta-api:latest
 
-# docker push dfinity/rosetta-api:"$CI_COMMIT_SHA"
+docker push dfinity/rosetta-api:"$CI_COMMIT_SHA"
 # docker push dfinity/rosetta-api:"$ROSETTA_API_DATE"
 # docker push dfinity/rosetta-api:v"$ROSETTA_API_VERSION"
 # docker push dfinity/rosetta-api:latest
