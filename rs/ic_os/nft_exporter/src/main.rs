17484fe719ffc4442653a739a5958663195a74a6
Ruediger Birkner <ruediger.birkner@dfinity.org>
2024-06-24 14:10:30 +0000

    wip

--- rs/ic_os/nft_exporter/src/main.rs
+++ rs/ic_os/nft_exporter/src/main.rs
@@ -1,12 +1,13 @@
 use anyhow::{Context, Error};
 use clap::Parser;
 use nftables::{
-    helper::get_current_ruleset, schema::Counter as NftCounter, schema::NfListObject::Counter,
+    helper::get_current_ruleset, helper::get_current_ruleset_raw, schema::Counter as NftCounter, schema::NfListObject::Counter,
     schema::NfObject,
 };
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
+use std::process::{Command, Output};
 
 const SERVICE_NAME: &str = "nft-exporter";
 
@@ -32,12 +33,29 @@ fn nft_counter_to_metric(counter: NftCounter) -> String {
 fn main() -> Result<(), Error> {
     let cli = Cli::parse();
 
+    // Print the PATH environment variable
+    if let Ok(path) = std::env::var("PATH") {
+        println!("PATH: {}", path);
+    }
+
+    // Check which `nft` executable is being used
+    let which_output: Output = Command::new("which")
+        .arg("nft")
+        .output()?;
+    println!("which nft: {:?}", which_output);
+
+    let nft_ruleset_raw = get_current_ruleset_raw(Some("/usr/sbin/nft"), None).context("failed to get the current nft ruleset")?;
+    println!("{:?}\n\n\n\n\n", nft_ruleset_raw);
+
     let nft_ruleset =
         get_current_ruleset(None, None).context("failed to get the current nft ruleset")?;
+
+    println!("{:?}", nft_ruleset);
 
     let mut metrics = Vec::new();
     for nft_object in nft_ruleset.objects.iter() {
         if let NfObject::ListObject(Counter(counter)) = nft_object {
+            println!("Counter {}", counter.name);
             metrics.push(nft_counter_to_metric(counter.clone()));
         }
     }
