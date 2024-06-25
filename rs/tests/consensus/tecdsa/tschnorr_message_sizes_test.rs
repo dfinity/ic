b680134cd8f4948ea276e50499920905a9e0242c
Leo Eichhorn <leo.eichhorn@dfinity.org>
2024-06-25 14:25:12 +0000

    init

---
+++ rs/tests/consensus/tecdsa/tschnorr_message_sizes_test.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+
+use ic_tests::driver::group::SystemTestGroup;
+use ic_tests::systest;
+use ic_tests::tecdsa;
+
+fn main() -> Result<()> {
+    SystemTestGroup::new()
+        .with_setup(tecdsa::tschnorr_message_sizes_test::config)
+        .add_test(systest!(tecdsa::tschnorr_message_sizes_test::test))
+        .execute_from_args()?;
+    Ok(())
+}
