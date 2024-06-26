316b698a9a974426c68e59dd2a864d82199e654a
Dimitris Sarlis <dimitris@dfinity.org>
2024-06-26 09:48:10 +0000

    feat(bitcoin-query-api)!: Remove handling of bitcoin queries in boundary nodes

--- rs/boundary_node/ic_boundary/src/management.rs
+++ rs/boundary_node/ic_boundary/src/management.rs
@@ -1,21 +1,12 @@
 use std::{str::FromStr, sync::Arc, time::Duration};
 
-use anyhow::{anyhow, Context, Error};
 use axum::{body::Body, extract::State, middleware::Next, response::IntoResponse, Extension};
-use candid::{CandidType, Decode};
 use http::Request;
-use ic_btc_interface::{Network as BitcoinNetwork, NetworkInRequest};
-use ic_config::execution_environment::{BITCOIN_MAINNET_CANISTER_ID, BITCOIN_TESTNET_CANISTER_ID};
-use ic_management_canister_types::QueryMethod;
 use ic_types::CanisterId;
 use lazy_static::lazy_static;
 use ratelimit::Ratelimiter;
-use serde::Deserialize;
 
-use crate::{
-    core::{decoder_config, MANAGEMENT_CANISTER_ID_PRINCIPAL},
-    routes::{ApiError, ErrorCause, RateLimitCause, RequestContext, RequestType},
-};
+use crate::routes::{ApiError, ErrorCause, RateLimitCause, RequestContext, RequestType};
 
 const LEDGER_METHODS_TRANSFER: [&str; 4] = [
     "transfer",
@@ -25,73 +16,8 @@ const LEDGER_METHODS_TRANSFER: [&str; 4] = [
 ];
 
 lazy_static! {
-    static ref BITCOIN_MAINNET_CANISTER_ID_PRINCIPAL: CanisterId =
-        CanisterId::from_str(BITCOIN_MAINNET_CANISTER_ID).unwrap();
-    static ref BITCOIN_TESTNET_CANISTER_ID_PRINCIPAL: CanisterId =
-        CanisterId::from_str(BITCOIN_TESTNET_CANISTER_ID).unwrap();
-    static ref BITCOIN_METHODS: [String; 2] = [
-        QueryMethod::BitcoinGetBalanceQuery.to_string(),
-        QueryMethod::BitcoinGetUtxosQuery.to_string(),
-    ];
     static ref LEDGER_CANISTER_ID: CanisterId =
         CanisterId::from_str("ryjl3-tyaaa-aaaaa-aaaba-cai").unwrap();
-}
-
-pub async fn btc_mw(
-    Extension(ctx): Extension<Arc<RequestContext>>,
-    Extension(canister_id): Extension<CanisterId>,
-    mut request: Request<Body>,
-    next: Next<Body>,
-) -> Result<impl IntoResponse, ApiError> {
-    let is_match = |ctx: &RequestContext| {
-        let method = match &ctx.method_name {
-            Some(v) => v,
-            None => return false,
-        };
-
-        // Check canister ID
-        if canister_id != MANAGEMENT_CANISTER_ID_PRINCIPAL {
-            return false;
-        }
-
-        // Check canister method
-        (*BITCOIN_METHODS).contains(method)
-    };
-
-    if !is_match(&ctx) {
-        return Ok(next.run(request).await);
-    }
-
-    let canister_id = match extract_btc_network(&ctx) {
-        Ok(BitcoinNetwork::Mainnet) => Ok(*BITCOIN_MAINNET_CANISTER_ID_PRINCIPAL),
-        Ok(BitcoinNetwork::Testnet) => Ok(*BITCOIN_TESTNET_CANISTER_ID_PRINCIPAL),
-        Ok(n) => Err(ApiError::ProxyError(ErrorCause::MalformedRequest(format!(
-            "invalid network {n}"
-        )))),
-        Err(err) => Err(ApiError::ProxyError(ErrorCause::MalformedRequest(format!(
-            "failed to extract btc network: {err}"
-        )))),
-    }?;
-
-    request.extensions_mut().insert(canister_id);
-    let mut response = next.run(request).await;
-    // Override the canister_id in the response to properly log it
-    response.extensions_mut().insert(canister_id);
-
-    Ok(response)
-}
-
-#[derive(CandidType, Deserialize)]
-struct BitcoinNetworkRecord {
-    network: NetworkInRequest,
-}
-
-fn extract_btc_network(ctx: &RequestContext) -> Result<BitcoinNetwork, Error> {
-    let arg = ctx.arg.as_ref().ok_or_else(|| anyhow!("missing arg"))?;
-    let r =
-        Decode!([decoder_config()]; arg, BitcoinNetworkRecord).context("failed to decode arg")?;
-
-    Ok(r.network.into())
 }
 
 fn is_ledger_call(ctx: &RequestContext, canister_id: &CanisterId) -> bool {
--- rs/boundary_node/ic_boundary/src/management/test.rs
+++ rs/boundary_node/ic_boundary/src/management/test.rs
@@ -1,97 +1,16 @@
 use super::*;
 
+use anyhow::Error;
 use axum::{
     body::Body, http::Request, middleware, response::IntoResponse, routing::method_routing::post,
     Router,
 };
-use candid::Encode;
 use http::StatusCode;
-use std::str::FromStr;
 use tower::Service;
-
-fn gen_req(canister_id: CanisterId, network: BitcoinNetwork, method: String) -> Request<Body> {
-    let mut req = Request::post("/").body(Body::from("foobar")).unwrap();
-
-    let network = BitcoinNetworkRecord {
-        network: network.into(),
-    };
-
-    let ctx = RequestContext {
-        method_name: Some(method),
-        arg: Encode!(&network).ok(),
-        ..Default::default()
-    };
-    let ctx = Arc::new(ctx);
-
-    req.extensions_mut().insert(ctx);
-    req.extensions_mut().insert(canister_id);
-
-    req
-}
 
 // pass canister id from request to response
 async fn handler() -> impl IntoResponse {
     "foobaz"
-}
-
-#[tokio::test]
-async fn test_btc_mw() -> Result<(), Error> {
-    let mut app = Router::new()
-        .route("/", post(handler))
-        .layer(middleware::from_fn(btc_mw));
-
-    // Check mainnet/method1
-    let req = gen_req(
-        MANAGEMENT_CANISTER_ID_PRINCIPAL,
-        BitcoinNetwork::Mainnet,
-        QueryMethod::BitcoinGetBalanceQuery.to_string(),
-    );
-    let res = app.call(req).await.unwrap();
-    let canister_id = res.extensions().get::<CanisterId>().cloned().unwrap();
-    assert_eq!(canister_id, *BITCOIN_MAINNET_CANISTER_ID_PRINCIPAL);
-
-    // Check testnet/method2
-    let req = gen_req(
-        MANAGEMENT_CANISTER_ID_PRINCIPAL,
-        BitcoinNetwork::Testnet,
-        QueryMethod::BitcoinGetUtxosQuery.to_string(),
-    );
-    let res = app.call(req).await.unwrap();
-    let canister_id = res.extensions().get::<CanisterId>().cloned().unwrap();
-    assert_eq!(canister_id, *BITCOIN_TESTNET_CANISTER_ID_PRINCIPAL);
-
-    // Check invalid network
-    let req = gen_req(
-        MANAGEMENT_CANISTER_ID_PRINCIPAL,
-        BitcoinNetwork::Regtest,
-        QueryMethod::BitcoinGetUtxosQuery.to_string(),
-    );
-    let res = app.call(req).await.unwrap();
-    let error_cause = res.extensions().get::<ErrorCause>().cloned().unwrap();
-    assert!(matches!(error_cause, ErrorCause::MalformedRequest(_)));
-
-    // Check some other method, shouldn't change canister id
-    let req = gen_req(
-        MANAGEMENT_CANISTER_ID_PRINCIPAL,
-        BitcoinNetwork::Regtest,
-        "foobar".to_string(),
-    );
-    let res = app.call(req).await.unwrap();
-    let canister_id = res.extensions().get::<CanisterId>();
-    assert!(canister_id.is_none());
-
-    // Check non-management canister, shouldn't change it
-    let canister_id = CanisterId::from_str("sqjm4-qahae-aq").unwrap();
-    let req = gen_req(
-        canister_id,
-        BitcoinNetwork::Testnet,
-        QueryMethod::BitcoinGetUtxosQuery.to_string(),
-    );
-    let res = app.call(req).await.unwrap();
-    let canister_id_out = res.extensions().get::<CanisterId>();
-    assert!(canister_id_out.is_none());
-
-    Ok(())
 }
 
 fn gen_req_ledger(method: String) -> Request<Body> {
