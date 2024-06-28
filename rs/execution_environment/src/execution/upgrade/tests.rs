fb4e5bdfede06ff81ce913bf1a23b9ea016f8930
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2024-06-28 09:09:49 +0000

    chore(RUN-931): Add doc links to HypervisorErrors

--- rs/execution_environment/src/execution/upgrade/tests.rs
+++ rs/execution_environment/src/execution/upgrade/tests.rs
@@ -974,12 +974,12 @@ fn dts_uninstall_with_aborted_upgrade() {
     }
 
     let err = check_ingress_status(test.ingress_status(&message_id)).unwrap_err();
-    assert_eq!(err.code(), ErrorCode::CanisterWasmModuleNotFound);
-    assert_eq!(
-        err.description(),
-        format!(
-            "Error from Canister {canister_id}: Attempted to execute a message, but the canister contains no Wasm module.",
-        )
+    err.assert_contains(
+        ErrorCode::CanisterWasmModuleNotFound,
+        &format!(
+            "Error from Canister {canister_id}: Attempted to execute a message, \
+            but the canister contains no Wasm module.",
+        ),
     );
 }
 
