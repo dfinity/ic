0806424249cfbddc955d45db5fbcbf355a0fec68
push-from-github <project_31166135_bot_fe945cd3addd4d5b141baa69e47ee65e@noreply.gitlab.com>
2024-07-01 08:38:41 +0000

    chore(github-sync): PR#294 / chore: Improve Upgrade with Alternative URLs test

--- rs/tests/src/orchestrator/upgrade_with_alternative_urls.rs
+++ rs/tests/src/orchestrator/upgrade_with_alternative_urls.rs
@@ -30,8 +30,8 @@ use crate::{
         test_env_api::*,
     },
     orchestrator::utils::upgrade::{
-        bless_replica_version_with_urls, deploy_guestos_to_all_subnet_nodes,
-        get_assigned_replica_version, UpdateImageType,
+        assert_assigned_replica_version, bless_replica_version_with_urls,
+        deploy_guestos_to_all_subnet_nodes, get_assigned_replica_version, UpdateImageType,
     },
     util::{block_on, get_nns_node},
 };
@@ -92,12 +92,12 @@ pub fn test(env: TestEnv) {
     let test_version = format!("{}-test", original_version);
     block_on(deploy_guestos_to_all_subnet_nodes(
         &nns_node,
-        &ReplicaVersion::try_from(test_version).unwrap(),
+        &ReplicaVersion::try_from(test_version.clone()).unwrap(),
         subnet_id,
     ));
 
-    info!(logger, "Waiting until the replica process is killed");
+    info!(logger, "Waiting until the subnet is upgraded");
 
-    // Wait until the replica process is killed.
-    nns_node.await_status_is_unavailable().unwrap();
+    // Wait until the subnet is upgraded.
+    assert_assigned_replica_version(&nns_node, &test_version, logger);
 }
