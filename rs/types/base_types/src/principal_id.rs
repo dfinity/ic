ec427b54f6022ec1e028f23ab4fcc57d96ff4a7d
Alin Sinpalean <alin.sinpalean@dfinity.org>
2024-07-04 17:57:34 +0000

    refactor: Have `PrincipalId` derive `PartialEq`

--- rs/types/base_types/src/principal_id.rs
+++ rs/types/base_types/src/principal_id.rs
@@ -22,18 +22,14 @@ use std::{
 /// Principals have variable length, bounded by 29 bytes. Since we
 /// want [`PrincipalId`] to implement the Copy trait, we encode them as
 /// a fixed-size array and a length.
-#[derive(Clone, Copy, Eq, PartialOrd, Ord, Serialize, Deserialize, comparable::Comparable)]
+#[derive(
+    Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, comparable::Comparable,
+)]
 #[describe_type(String)]
 #[describe_body(self.to_string())]
 #[repr(transparent)]
 #[serde(transparent)]
 pub struct PrincipalId(#[comparable_ignore] pub Principal);
-
-impl PartialEq for PrincipalId {
-    fn eq(&self, other: &PrincipalId) -> bool {
-        self.0 == other.0
-    }
-}
 
 impl Hash for PrincipalId {
     fn hash<H: Hasher>(&self, state: &mut H) {
