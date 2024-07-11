acfc60c138ca8a1d7944a1f165e1b37560d39f55
Maciej Modelski <maciej.modelski@dfinity.org>
2024-07-11 19:57:21 +0000

    enable u256 test for approvals upgrade

--- rs/rosetta-api/icrc1/ledger/tests/tests.rs
+++ rs/rosetta-api/icrc1/ledger/tests/tests.rs
@@ -328,7 +328,6 @@ fn test_block_transformation() {
     );
 }
 
-#[cfg_attr(feature = "u256-tokens", ignore)]
 #[test]
 fn icrc1_test_approval_upgrade() {
     ic_icrc1_ledger_sm_tests::icrc1_test_approval_upgrade(
