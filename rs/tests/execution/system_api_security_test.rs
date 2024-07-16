d11fe644b9a45390825664877b574de9119b2a31
Dragoljub Duric <dragoljub.duric@dfinity.org>
2024-07-16 14:06:36 +0000

    .

--- rs/tests/execution/system_api_security_test.rs
+++ rs/tests/execution/system_api_security_test.rs
@@ -9,10 +9,10 @@ use ic_tests::execution::system_api_security_test;
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_setup(system_api_security_test::config)
+        .add_test(systest!(system_api_security_test::malicious_inputs))
         .add_test(systest!(
             system_api_security_test::malicious_intercanister_calls
         ))
-        .add_test(systest!(system_api_security_test::malicious_inputs))
         .execute_from_args()?;
     Ok(())
 }
