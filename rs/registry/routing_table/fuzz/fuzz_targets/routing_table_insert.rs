8037fc503280317bb8ff94f9fbddb2d2f1fb5a82
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2024-07-18 08:01:42 +0000

    canister functions as x86

--- rs/registry/routing_table/fuzz/fuzz_targets/routing_table_insert.rs
+++ rs/registry/routing_table/fuzz/fuzz_targets/routing_table_insert.rs
@@ -1,12 +1,14 @@
 #![no_main]
 use arbitrary::Arbitrary;
-use ic_registry_routing_table::{CanisterIdRange, RoutingTable};
 use ic_base_types::{CanisterId, PrincipalId, SubnetId};
+use ic_registry_routing_table::{CanisterIdRange, CanisterIdRanges, RoutingTable};
 use libfuzzer_sys::fuzz_target;
 use std::cell::RefCell;
+use std::collections::HashSet;
 
 thread_local! {
     static ROUTING_TABLE: RefCell<RoutingTable> = RefCell::new(RoutingTable::new());
+    static KNOWN_SUBNET:  RefCell<HashSet<SubnetId>> = RefCell::new(HashSet::new());
 }
 
 #[derive(Debug, Arbitrary)]
@@ -14,27 +16,40 @@ struct RoutingTableInsertData {
     x: u64,
     y: u64,
     z: u64,
+    migration: bool,
 }
 
-
 fuzz_target!(|table_data: RoutingTableInsertData| {
-    let (start, end, subnet_id) = (CanisterId::from_u64(table_data.x), CanisterId::from_u64(table_data.y), table_data.z);
+    let (start, end, subnet_id, migration) = (
+        CanisterId::from_u64(table_data.x),
+        CanisterId::from_u64(table_data.y),
+        table_data.z,
+        table_data.migration,
+    );
 
-    let canister_ranges: CanisterIdRange;
+    let canister_range: CanisterIdRange;
     if start > end {
-        canister_ranges = CanisterIdRange {
-            start: end, 
+        canister_range = CanisterIdRange {
+            start: end,
             end: start,
         };
     } else {
-        canister_ranges = CanisterIdRange {
-            start: start, 
+        canister_range = CanisterIdRange {
+            start: start,
             end: end,
         };
     }
 
     let subnet = SubnetId::from(PrincipalId::new_subnet_test_id(subnet_id));
+    let _ = ROUTING_TABLE.with_borrow_mut(|table| {
+        let known_subnet = KNOWN_SUBNET.with_borrow(|subnets| subnets.contains(&subnet));
 
-    let _ = ROUTING_TABLE.with_borrow_mut(|table| {table.insert(canister_ranges, subnet) });
-
+        if known_subnet && migration {
+            let canister_ranges = CanisterIdRanges::try_from(vec![canister_range]).unwrap();
+            table.assign_ranges(canister_ranges, subnet);
+        } else {
+            table.insert(canister_range, subnet);
+            KNOWN_SUBNET.with_borrow_mut(|subnets| subnets.insert(subnet));
+        }
+    });
 });
