a8e51d2ee0dc50a37210340f1558d8f6fcc2817c
Marko Kosmerl <marko.kosmerl@dfinity.org>
2024-07-19 14:37:01 +0200

    change

--- gitlab-ci/src/ci-scripts/before-script.sh
+++ gitlab-ci/src/ci-scripts/before-script.sh
@@ -3,15 +3,17 @@
 set -eEuo pipefail
 
 # login to docker hub to avoid rate limit disruptions
-if which docker 2>/dev/null; then
-    docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
-fi
-# docker-bin used by container_pull in WORKSPACES.bazel
-if which docker-bin 2>/dev/null; then
-    # save auth to user's .docker/config.json
-    docker-bin login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
-    # save auth to root's .docker/config.json
-    sudo docker-bin login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
+if [ -n "${DOCKER_HUB_USER:-}" ] && [ -n "${DOCKER_HUB_PASSWORD_RO:-}" ]; then
+  if which docker 2>/dev/null; then
+      docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
+  fi
+  # docker-bin used by container_pull in WORKSPACES.bazel
+  if which docker-bin 2>/dev/null; then
+      # save auth to user's .docker/config.json
+      docker-bin login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
+      # save auth to root's .docker/config.json
+      sudo docker-bin login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASSWORD_RO"
+  fi
 fi
 
 # print node name for easier debugging
