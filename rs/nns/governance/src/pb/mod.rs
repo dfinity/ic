80908abca70e98e69a395397d11f0b3eb7808b22
Max Summe <maximilian.summe@dfinity.org>
2024-07-24 09:56:42 -0700

    Make module available'

--- rs/nns/governance/src/pb/mod.rs
+++ rs/nns/governance/src/pb/mod.rs
@@ -2,5 +2,9 @@
 #[path = "../gen/ic_nns_governance.pb.v1.rs"]
 pub mod v1;
 
+#[allow(clippy::all)]
+#[path = "../gen/ic_nns_governance.pb.storage.rs"]
+pub mod storage;
+
 mod conversions;
 mod convert_struct_to_enum;
