0c3ff523bad0ffc3889c4e56eae85e72272990e0
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2024-07-25 13:10:31 +0000

    fix test

--- gitlab-ci/src/dependencies/scanner/manager/bazel_rust_dependency_manager_test.py
+++ gitlab-ci/src/dependencies/scanner/manager/bazel_rust_dependency_manager_test.py
@@ -309,7 +309,7 @@ def test_get_findings_for_bazel_repo():
         executor = MockBazelCargoExecutor(expected_cargo_audit_output=json.load(audit), expected_bazel_queries=expected_queries, expected_bazel_responses=expected_responses)
         bazel_test = BazelRustDependencyManager(executor=executor)
 
-        findings = bazel_test.get_findings("ic", None, None)
+        findings = bazel_test.get_findings("ic", Project("ic", "ic"), None)
 
         assert findings is not None
         assert len(findings) == 3
