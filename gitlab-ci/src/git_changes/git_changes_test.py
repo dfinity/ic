d5d847b909be0884bb8d88ed42ac8c9e0496c888
Carly Gundy <47304080+cgundy@users.noreply.github.com>
2024-07-25 14:31:41 +0200

    Update git_changes_test.py

--- gitlab-ci/src/git_changes/git_changes_test.py
+++ gitlab-ci/src/git_changes/git_changes_test.py
@@ -121,7 +121,7 @@ def test_target_branch_CI_MERGE_REQUEST_TARGET_BRANCH_NAME_is_unset(tmpdir):
     os.environ["CI_DEFAULT_BRANCH"] = "default-branch"
     os.environ["CI_MERGE_REQUEST_TARGET_BRANCH_NAME"] = ""
 
-    assert git_changes.target_branch() == "master"
+    assert git_changes.target_branch() == "default-branch"
 
 def test_target_branch_CI_MERGE_REQUEST_TARGET_BRANCH_NAME_is_set(tmpdir):
     """Tests that the target branch is empty."""
