1fb3f99861e796f8b15d9fd3ab76e4ea9eeff5c4
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-07-29 14:32:58 +0000

    .

--- rs/replicated_state/src/canister_state/tests.rs
+++ rs/replicated_state/src/canister_state/tests.rs
@@ -625,7 +625,12 @@ fn canister_state_log_visibility_round_trip() {
 fn compatibility_for_log_visibility() {
     // If this fails, you are making a potentially incompatible change to `LogVisibility`.
     // See note [Handling changes to Enums in Replicated State] for how to proceed.
-    assert_eq!(LogVisibility::iter().count(), 3);
+    assert_eq!(
+        LogVisibility::iter()
+            .map(|x| x as i32)
+            .collect::<Vec<i32>>(),
+        [1, 2]
+    );
 }
 
 #[test]
