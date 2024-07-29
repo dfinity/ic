c304d6817be026a74e2e14f9664948c195569e9d
Maciej Kot <maciej.kot@dfinity.org>
2024-07-29 09:31:31 +0000

    Add test

--- rs/system_api/src/sandbox_safe_system_state.rs
+++ rs/system_api/src/sandbox_safe_system_state.rs
@@ -60,7 +60,7 @@ pub enum CallbackUpdate {
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct SystemStateChanges {
     pub(super) new_certified_data: Option<Vec<u8>>,
-    pub(super) callback_updates: Vec<CallbackUpdate>,
+    pub callback_updates: Vec<CallbackUpdate>,
     cycles_balance_change: CyclesBalanceChange,
     // The cycles that move from the main balance to the reserved balance.
     // Invariant: `cycles_balance_change` contains
