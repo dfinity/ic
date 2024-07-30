a5d55bd6bcd7bf9718a1fe3276a582d9ae557e85
Ruediger Birkner <ruediger.birkner@dfinity.org>
2024-07-30 19:49:29 +0000

    adapt test

--- rs/tests/src/boundary_nodes/boundary_nodes_integration.rs
+++ rs/tests/src/boundary_nodes/boundary_nodes_integration.rs
@@ -2238,21 +2238,21 @@ pub fn direct_to_replica_options_test(env: TestEnv) {
             method: Method::POST,
             expect: StatusCode::BAD_REQUEST,
             path: format!("/api/v2/canister/{cid}/query"),
-            allowed_methods: "POST".into(),
+            allowed_methods: "HEAD, POST".into(),
         },
         TestCase {
             name: "call OPTIONS".into(),
             method: Method::POST,
             expect: StatusCode::BAD_REQUEST,
             path: format!("/api/v2/canister/{cid}/call"),
-            allowed_methods: "POST".into(),
+            allowed_methods: "HEAD, POST".into(),
         },
         TestCase {
-            name: "read_status OPTIONS".into(),
+            name: "read_state OPTIONS".into(),
             method: Method::POST,
             expect: StatusCode::BAD_REQUEST,
             path: format!("/api/v2/canister/{cid}/read_state"),
-            allowed_methods: "POST".into(),
+            allowed_methods: "HEAD, POST".into(),
         },
     ];
 
