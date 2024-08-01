393610a21d66d8d469463fca01e36761646d2cdb
Daniel Sharifi <daniel.sharifi@dfinity.org>
2024-08-01 15:37:23 +0000

    .

--- rs/http_endpoints/public/src/call/call_v3.rs
+++ rs/http_endpoints/public/src/call/call_v3.rs
@@ -43,6 +43,10 @@ use std::{
 use tokio_util::time::FutureExt;
 use tower::{util::BoxCloneService, ServiceBuilder};
 
+/// The timeout duration used when creating a subscriber for the ingres message,
+/// by calling [`IngressWatcherHandle::subscribe_for_certification`].
+const SUBSCRIPTION_TIMEOUT: Duration = Duration::from_secs(1);
+
 enum CallV3Response {
     Certificate(Certificate),
     UserError(UserError),
@@ -191,11 +195,9 @@ async fn call_sync_v3(
 
     let message_id = ingress_submitter.message_id();
 
-    let timeout = Duration::from_secs(ingress_message_certificate_timeout_seconds);
-
     let certification_subscriber = match ingress_watcher_handle
         .subscribe_for_certification(message_id.clone())
-        .timeout(timeout)
+        .timeout(SUBSCRIPTION_TIMEOUT)
         .await
     {
         Ok(Ok(message_subscriber)) => Ok(message_subscriber),
@@ -250,7 +252,9 @@ async fn call_sync_v3(
 
     match certification_subscriber
         .wait_for_certification()
-        .timeout(timeout)
+        .timeout(Duration::from_secs(
+            ingress_message_certificate_timeout_seconds,
+        ))
         .await
     {
         Ok(()) => (),
