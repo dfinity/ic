85b5ba92be5d9848a4fb71e53ceb6efb286e6d74
Roel Storms <rstorms@zh1-spm22.zh1.dfinity.network>
2024-08-09 13:15:56 +0000

    Fixed errors and switched to AFL

--- rs/protobuf/fuzz/fuzz_targets/decode_block.rs
+++ rs/protobuf/fuzz/fuzz_targets/decode_block.rs
@@ -1,7 +1,10 @@
 #![no_main]
-use libfuzzer_sys::fuzz_target;
+use ic_protobuf::proxy::ProtoProxy;
+use ic_protobuf::proxy::ProxyDecodeError;
 use ic_protobuf::types::v1 as pb;
+use libfuzzer_sys::fuzz_target;
+use pb::Block;
 
 fuzz_target!(|data: &[u8]| {
-    let _decoded = pb::Block::proxy_decode(&data);
-}); 
+    let _decoded: Result<Block, ProxyDecodeError> = pb::Block::proxy_decode(&data);
+});
