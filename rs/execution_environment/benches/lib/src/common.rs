85f58e976a642a20879f4100983e1eb1a1feea89
AlexandraZapuc <61285418+AlexandraZapuc@users.noreply.github.com>
2024-08-13 00:33:04 +0200

    feat: EXC-1567: Add charging for take and load canister snapshot (#811)
    This PR updates the canister manager to charge canisters that are
    executing `take_canister_snapshot` or `load_canister_snapshot`.
    
    Closes EXC-1567
    
    ---------
    
    Co-authored-by: Andriy Berestovskyy <andriy.berestovskyy@dfinity.org>

--- rs/execution_environment/benches/lib/src/common.rs
+++ rs/execution_environment/benches/lib/src/common.rs
@@ -306,6 +306,9 @@ where
         subnet_configs
             .scheduler_config
             .upload_wasm_chunk_instructions,
+        subnet_configs
+            .scheduler_config
+            .canister_snapshot_baseline_instructions,
     );
     for Benchmark(id, wat, expected_ops) in benchmarks {
         run_benchmark(
