7713f654b36e91240fe995a8a8f184fcc0c78b5a
Andrew Battat <andrew.battat@dfinity.org>
2024-08-14 22:49:42 +0000

    Rename Component struct

--- rs/ic_os/manifest_tool/src/components_parser.rs
+++ rs/ic_os/manifest_tool/src/components_parser.rs
@@ -3,21 +3,21 @@ use regex::Regex;
 use std::fs;
 
 #[derive(Debug)]
-pub struct Components {
+pub struct Component {
     pub source: String,
     pub destination: String,
 }
 
-impl Components {
+impl Component {
     pub fn new(name: String, path: String) -> Self {
-        Components {
+        Component {
             source: name,
             destination: path,
         }
     }
 }
 
-pub fn get_components_from_bzl(file_path: &str) -> Result<Vec<Components>> {
+pub fn get_components_from_bzl(file_path: &str) -> Result<Vec<Component>> {
     let content = fs::read_to_string(file_path)
         .with_context(|| format!("Failed to read file: {}", file_path))?;
 
@@ -29,13 +29,13 @@ pub fn get_components_from_bzl(file_path: &str) -> Result<Vec<Components>> {
     for cap in re.captures_iter(&content) {
         let name = cap[1].to_string();
         let path = cap[2].to_string();
-        components.push(Components::new(name, path));
+        components.push(Component::new(name, path));
     }
 
     Ok(components)
 }
 
-pub fn get_all_components() -> Result<Vec<Components>> {
+pub fn get_all_components() -> Result<Vec<Component>> {
     //TODO: help: fix file paths
     let guestos_components = get_components_from_bzl("../../../ic-os/components/guestos.bzl")?;
     let hostos_components = get_components_from_bzl("../../../ic-os/components/hostos.bzl")?;
