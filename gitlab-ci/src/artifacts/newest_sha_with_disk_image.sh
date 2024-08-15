75bd28db41cc2e0c42e4c3daf32feaf24248ba0a
Saša Tomić <sasa.tomic@dfinity.org>
2024-08-15 17:06:57 +0200

    fix(ci): Use .zst instead of .gz disk images in more places

--- gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh
+++ gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh
@@ -11,7 +11,6 @@ fi
 function disk_image_exists() {
     local -r GIT_SHA=$1
     local -r URLS=(
-        "https://download.dfinity.systems/ic/${GIT_SHA}/guest-os/disk-img/disk-img.tar.gz"
         "https://download.dfinity.systems/ic/${GIT_SHA}/guest-os/disk-img/disk-img.tar.zst"
     )
 
