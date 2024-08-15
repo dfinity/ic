6965408a897ca4c0061b0d7276feb662f93b8179
Andrew Battat <andrew.battat@dfinity.org>
2024-08-15 21:19:30 +0000

    Refactor check_unused_components

--- rs/ic_os/manifest_tool/src/check_unused_components.rs
+++ rs/ic_os/manifest_tool/src/check_unused_components.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use std::collections::HashSet;
 use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
 
 use crate::components_parser::{get_icos_manifest, IcosManifest, COMPONENTS_PATH};
 
@@ -9,52 +10,61 @@ pub fn check_unused_components(repo_root: &PathBuf) -> Result<()> {
 
     let components_path: PathBuf = repo_root.join(COMPONENTS_PATH);
 
-    let component_repo_files = collect_component_repo_files(&components_path)?;
+    let repo_files = collect_repo_files(&components_path)?;
+    let manifest_files = collect_manifest_files(&icos_manifest);
 
-    let mut manifest_sources = HashSet::new();
-    collect_manifest_sources(&icos_manifest, &mut manifest_sources);
+    let unused_files: Vec<&PathBuf> = repo_files.difference(&manifest_files).collect();
 
-    let unused_components: Vec<&PathBuf> =
-        component_repo_files.difference(&manifest_sources).collect();
-
-    if !unused_components.is_empty() {
-        println!("Unused components:");
-        for component in unused_components {
+    if !unused_files.is_empty() {
+        println!("Unused files:");
+        for component in unused_files {
             println!("{}", component.display());
         }
     } else {
-        println!("No unused components found.");
+        println!("No unused file found.");
     }
 
     Ok(())
 }
 
-fn collect_component_repo_files(dir: &Path) -> Result<HashSet<PathBuf>> {
+fn collect_repo_files(dir: &Path) -> Result<HashSet<PathBuf>, std::io::Error> {
     let mut files = HashSet::new();
-    for entry in std::fs::read_dir(dir)? {
-        let entry = entry?;
-        let path = entry.path();
 
-        if path.is_dir() {
-            files.extend(collect_component_repo_files(&path)?);
-        } else {
+    for entry in WalkDir::new(dir) {
+        let entry = entry?;
+        let path = entry.path().to_path_buf();
+
+        if path.is_file() {
+            if let Some(file_name) = path.file_name().and_then(|s| s.to_str()) {
+                if file_name.ends_with(".bzl")
+                    || file_name.ends_with(".bazel")
+                    || file_name.to_lowercase().starts_with("readme.")
+                {
+                    continue;
+                }
+            }
             files.insert(path);
         }
     }
+
     Ok(files)
 }
 
-fn collect_manifest_sources(icos_manifest: &IcosManifest, manifest_sources: &mut HashSet<PathBuf>) {
+fn collect_manifest_files(icos_manifest: &IcosManifest) -> HashSet<PathBuf> {
+    let mut manifest_files: HashSet<PathBuf> = HashSet::new();
+
     for entry in &icos_manifest.guestos.manifest {
-        manifest_sources.insert(entry.source.clone());
+        manifest_files.insert(entry.source.clone());
     }
     for entry in &icos_manifest.hostos.manifest {
-        manifest_sources.insert(entry.source.clone());
+        manifest_files.insert(entry.source.clone());
     }
     for entry in &icos_manifest.setupos.manifest {
-        manifest_sources.insert(entry.source.clone());
+        manifest_files.insert(entry.source.clone());
     }
     for entry in &icos_manifest.boundary_guestos.manifest {
-        manifest_sources.insert(entry.source.clone());
+        manifest_files.insert(entry.source.clone());
     }
+
+    manifest_files
 }
