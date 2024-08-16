b7c7a1f48be1d789b098c55680772cf14fc43ff4
Andrew Battat <andrew.battat@dfinity.org>
2024-08-16 21:16:29 +0000

    Remove unnecessary merge branch code

--- pre-commit/check-unused-components.sh
+++ pre-commit/check-unused-components.sh
@@ -7,9 +7,5 @@ if [[ -n "$(git rev-parse -q --verify MERGE_HEAD)" ]]; then
     exit 0
 fi
 
-echo "Fetch the $MERGE_BRANCH branch"
-git fetch origin $MERGE_BRANCH:$MERGE_BRANCH
-MERGE_BASE=$(git merge-base HEAD $MERGE_BRANCH)
-
 
 bazel run //rs/ic_os/manifest_tool -- --repo-root $(git rev-parse --show-toplevel) check-unused-components
