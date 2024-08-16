38a6d96664caeef124c89507486110cfd2e5a5b3
Jan Wendling <jan.wendling@dfinity.org>
2024-08-16 06:37:05 +0000

    feat: add ic-gateway to dependency scanning

--- gitlab-ci/src/dependencies/job/bazel_rust_ic_scanner_periodic_job.py
+++ gitlab-ci/src/dependencies/job/bazel_rust_ic_scanner_periodic_job.py
@@ -17,6 +17,7 @@ REPOS_TO_SCAN = [
     Repository("response-verification", "https://github.com/dfinity/response-verification", [Project(name="response-verification", path="response-verification", owner=Team.TRUST_TEAM)]),
     Repository("agent-rs", "https://github.com/dfinity/agent-rs", [Project(name="agent-rs", path="agent-rs", owner=Team.SDK_TEAM)]),
     Repository("ic-canister-sig-creation", "https://github.com/dfinity/ic-canister-sig-creation", [Project(name="ic-canister-sig-creation", path="ic-canister-sig-creation", owner=Team.GIX_TEAM)]),
+    Repository("ic-gateway", "https://github.com/dfinity/ic-gateway",[Project(name="ic-gateway", path="ic-gateway", owner=Team.BOUNDARY_NODE_TEAM)]),
 ]
 
 if __name__ == "__main__":
