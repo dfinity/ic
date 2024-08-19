86bdf3205bd767fd551e09353daf25f133e5fd39
Stefan Neamtu <stefan.neamtu@gmail.com>
2024-08-19 17:07:16 +0000

    blocked by tower-test 0.4 which does not use tower 0.5

--- rs/pocket_ic_server/src/pocket_ic.rs
+++ rs/pocket_ic_server/src/pocket_ic.rs
@@ -969,7 +969,7 @@ async fn setup_adapter_mock(
 
             async move {
                 if let Some(client) = client {
-                    Ok(client)
+                    Ok(hyper_util::rt::TokioIo::new(client))
                 } else {
                     Err(std::io::Error::new(
                         std::io::ErrorKind::Other,
