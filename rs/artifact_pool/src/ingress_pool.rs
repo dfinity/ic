c890f067f5bd535458615c166e06d32351cb96bb
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-08-23 15:52:36 +0200

    refactor: clarify the priority fn semantics (#1042)
    The MR renames the priority function types to a name that better matches
    the semantics.
    
    The MR also clarifies the semantics of the priority/filter function.
    
    ---------
    
    Co-authored-by: IDX GitHub Automation <infra+github-automation@dfinity.org>

--- rs/artifact_pool/src/ingress_pool.rs
+++ rs/artifact_pool/src/ingress_pool.rs
@@ -13,8 +13,8 @@ use ic_interfaces::{
         UnvalidatedIngressArtifact, ValidatedIngressArtifact,
     },
     p2p::consensus::{
-        ArtifactMutation, ArtifactWithOpt, ChangeResult, MutablePool, Priority, PriorityFn,
-        PriorityFnFactory, UnvalidatedArtifact, ValidatedPoolReader,
+        ArtifactMutation, ArtifactWithOpt, Bouncer, BouncerFactory, BouncerValue, ChangeResult,
+        MutablePool, UnvalidatedArtifact, ValidatedPoolReader,
     },
     time_source::TimeSource,
 };
@@ -372,8 +372,8 @@ impl IngressPrioritizer {
     }
 }
 
-impl PriorityFnFactory<SignedIngress, IngressPoolImpl> for IngressPrioritizer {
-    fn get_priority_function(&self, pool: &IngressPoolImpl) -> PriorityFn<IngressMessageId> {
+impl BouncerFactory<SignedIngress, IngressPoolImpl> for IngressPrioritizer {
+    fn new_bouncer(&self, pool: &IngressPoolImpl) -> Bouncer<IngressMessageId> {
         // EXPLANATION: Because ingress messages are included in blocks, consensus
         // does not rely on ingress gossip for correctness. Ingress gossip exists to
         // reduce latency in cases where replicas don't have enough ingress messages
@@ -382,16 +382,16 @@ impl PriorityFnFactory<SignedIngress, IngressPoolImpl> for IngressPrioritizer {
         // Please note that all P2P ingress messages will be dropped if 'exceeds_threshold'
         // returns true until the next invocation of 'get_priority_function'.
         if pool.exceeds_threshold() {
-            return Box::new(move |_| Priority::Drop);
+            return Box::new(move |_| BouncerValue::Unwanted);
         }
         let time_source = self.time_source.clone();
         Box::new(move |ingress_id| {
             let start = time_source.get_relative_time();
             let range = start..=start + MAX_INGRESS_TTL;
             if range.contains(&ingress_id.expiry()) {
-                Priority::FetchNow
+                BouncerValue::Wants
             } else {
-                Priority::Drop
+                BouncerValue::Unwanted
             }
         })
     }
