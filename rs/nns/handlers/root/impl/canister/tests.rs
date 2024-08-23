b1cba80aea35b1b814d6ec2c95a60f782ade7e8f
Daniel Wong <daniel.wong@dfinity.org>
2024-08-23 17:33:31 +0200

    Replace a bunch of long copy & pasted comments with a "pointer".

--- rs/nns/handlers/root/impl/canister/tests.rs
+++ rs/nns/handlers/root/impl/canister/tests.rs
@@ -1,25 +1,7 @@
 use super::*;
 use candid_parser::utils::{service_equal, CandidSource};
 
-/// This is NOT affected by
-///
-///   1. comments (in ./registry.did)
-///   2. whitespace
-///   3. order of type definitions
-///   4. names of types
-///   5. etc.
-///
-/// Whereas, this test fails in the following cases
-///
-///   1. extra (or missing) fields
-///   2. differences in field names
-///   3. etc.
-///
-/// If this test passes, that does NOT mean that the API has evolved safely;
-/// there is a different test for that (namely,
-/// candid_changes_are_backwards_compatible). This test does not compare the
-/// current working copy against master. Rather, it only compares ./canister.rs
-/// to registry.did.
+/// See analogous comments in registry/canister/canister/tests.rs (yes, canister occurs twice).
 #[test]
 fn test_implemented_interface_matches_declared_interface_exactly() {
     let declared_interface = CandidSource::Text(include_str!("root.did"));
