fe9934703ba47e2639f0686860f7cdbd470d3587
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2024-08-23 14:57:19 +0000

    add system test

--- rs/rosetta-api/tests/system_tests/common/system_test_environment.rs
+++ rs/rosetta-api/tests/system_tests/common/system_test_environment.rs
@@ -1,15 +1,44 @@
-pub struct RosettaTestingEnviornment {
+use crate::common::{
+    constants::{DEFAULT_INITIAL_BALANCE, STARTING_CYCLES_PER_CANISTER},
+    utils::test_identity,
+};
+use candid::{Encode, Principal};
+use ic_agent::Identity;
+use ic_icp_rosetta_runner::{start_rosetta, RosettaContext, RosettaOptionsBuilder};
+use ic_icrc1_test_utils::minter_identity;
+use ic_ledger_test_utils::build_ledger_wasm;
+use ic_ledger_test_utils::pocket_ic_helpers::ledger::LEDGER_CANISTER_ID;
+use ic_rosetta_test_utils::path_from_env;
+use ic_types::PrincipalId;
+use icp_ledger::{AccountIdentifier, LedgerCanisterInitPayload};
+use pocket_ic::{nonblocking::PocketIc, PocketIcBuilder};
+use std::collections::HashMap;
+use tempfile::TempDir;
+
+pub struct RosettaTestingEnvironment {
     pub pocket_ic: PocketIc,
     pub rosetta_context: RosettaContext,
 }
 
+impl RosettaTestingEnvironment {
+    pub fn builder() -> RosettaTestingEnviornmentBuilder {
+        RosettaTestingEnviornmentBuilder::new()
+    }
+}
+
 pub struct RosettaTestingEnviornmentBuilder {
     pub persistent_storage: bool,
+    pub icp_ledger_init_args: Option<LedgerCanisterInitPayload>,
+    pub controller_id: Option<PrincipalId>,
 }
 
 impl RosettaTestingEnviornmentBuilder {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            persistent_storage: false,
+            icp_ledger_init_args: None,
+            controller_id: None,
+        }
     }
 
     pub fn with_persistent_storage(mut self) -> Self {
@@ -17,22 +46,46 @@ impl RosettaTestingEnviornmentBuilder {
         self
     }
 
-    pub fn build(self) -> RosettaTestingEnviornment {
+    pub fn with_icp_ledger_init_args(mut self, args: LedgerCanisterInitPayload) -> Self {
+        self.icp_ledger_init_args = Some(args);
+        self
+    }
+
+    pub fn with_icp_ledger_controller_id(mut self, controller_id: PrincipalId) -> Self {
+        self.controller_id = Some(controller_id);
+        self
+    }
+
+    pub async fn build(self) -> RosettaTestingEnvironment {
         let mut pocket_ic = PocketIcBuilder::new().with_nns_subnet().build_async().await;
+        pocket_ic.make_live(None).await;
 
         let ledger_canister_id = Principal::from(LEDGER_CANISTER_ID);
         let canister_id = pocket_ic
             .create_canister_with_id(None, None, ledger_canister_id)
             .await
             .expect("Unable to create the canister in which the Ledger would be installed");
+
+        let init_args = self.icp_ledger_init_args.unwrap_or_else(|| {
+            LedgerCanisterInitPayload::builder()
+                .minting_account(minter_identity().sender().unwrap().into())
+                .initial_values(HashMap::from([(
+                    AccountIdentifier::new(PrincipalId(test_identity().sender().unwrap()), None),
+                    icp_ledger::Tokens::from_tokens(DEFAULT_INITIAL_BALANCE).unwrap(),
+                )]))
+                .build()
+                .unwrap()
+        });
+
         pocket_ic
             .install_canister(
                 canister_id,
-                icp_ledger_wasm_bytes(),
-                icp_ledger_init(sender_canister_id),
-                None,
+                build_ledger_wasm().bytes().to_vec(),
+                Encode!(&init_args).unwrap(),
+                self.controller_id.map(|id| id.into()),
             )
             .await;
+
         assert_eq!(
             ledger_canister_id, canister_id,
             "Canister IDs do not match: expected {}, got {}",
@@ -49,13 +102,20 @@ impl RosettaTestingEnviornmentBuilder {
         );
 
         let replica_url = pocket_ic.make_live(None).await;
+        let rosetta_bin = path_from_env("ROSETTA_BIN_PATH");
 
         let rosetta_state_directory =
             TempDir::new().expect("failed to create a temporary directory");
+        let rosetta_context = start_rosetta(
+            &rosetta_bin,
+            Some(rosetta_state_directory.path().to_owned()),
+            RosettaOptionsBuilder::new(replica_url.to_string()).build(),
+        )
+        .await;
 
-        RosettaTestingEnviornment {
+        RosettaTestingEnvironment {
             pocket_ic,
-            rosetta_context: RosettaContext::new(self.persistent_storage),
+            rosetta_context,
         }
     }
 }
--- rs/rosetta-api/tests/system_tests/system_tests.rs
+++ rs/rosetta-api/tests/system_tests/system_tests.rs
@@ -1,1 +1,8 @@
+use common::system_test_environment::RosettaTestingEnvironment;
 
+mod common;
+
+#[test]
+fn smoke_test() {
+    let rosetta_testing_environment = RosettaTestingEnvironment::builder().build();
+}
