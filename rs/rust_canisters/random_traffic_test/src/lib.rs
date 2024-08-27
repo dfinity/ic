eef770050fb60fccf3fef1723f0d06e8483d02b8
Christian Stieger <christian.stieger@dfinity.org>
2024-08-27 16:34:47 +0000

    .

--- rs/rust_canisters/random_traffic_test/src/lib.rs
+++ rs/rust_canisters/random_traffic_test/src/lib.rs
@@ -1,32 +1,35 @@
 use candid::CandidType;
-use ic_base_types::{CanisterId, NumBytes};
-use ic_types::NumInstructions;
+use ic_base_types::CanisterId;
 use serde::{Deserialize, Serialize};
-use std::collections::{BTreeMap, VecDeque};
 
-/// A full config for sending messages. This is sent to the `start` method of the canister.
-#[derive(Serialize, Deserialize, Debug, CandidType)]
+/// A full config for generating random calls and replies.
+#[derive(Serialize, Deserialize, Clone, Default, Debug, CandidType)]
 pub struct Config {
-    pub request_payloads: VecDeque<(NumBytes, CanisterId)>,
-    pub response_payloads: BTreeMap<CanisterId, VecDeque<(NumBytes, NumInstructions)>>,
+    pub call_bytes_min: u32,
+    pub call_bytes_max: u32,
+    pub receivers: Vec<CanisterId>,
+    pub reply_bytes_min: u32,
+    pub reply_bytes_max: u32,
+    pub instructions_count_min: u32,
+    pub instructions_count_max: u32,
+    pub downstream_call_weight: u32,
+    pub reply_weight: u32,
 }
 
 /// Indicate whether a request was sent successfully including the size of the payload; or rejected
 /// synchronously by the IC including the error code.
 #[derive(Serialize, Deserialize, Clone, Debug, CandidType)]
 pub enum Request {
-    Data(NumBytes),
+    Data(u32),
     Rejected(i32),
-    EncodeFailed,
 }
 
 /// Indicates whether a data response was received including the size of the payload; or rejected
 /// including the message in the reject response.
 #[derive(Serialize, Deserialize, Clone, Debug, CandidType)]
 pub enum Response {
-    Data(NumBytes),
+    Data(u32),
     Rejected(String),
-    DecodeFailed,
 }
 
 /// Record for one message cycle. Records whether and how many bytes were sent out; and received..
---
+++ rs/rust_canisters/random_traffic_test/src/main.rs
@@ -0,0 +1,187 @@
+use candid::{Decode, Encode};
+use dfn_core::api;
+use ic_base_types::CanisterId;
+use rand::{distributions::Distribution, rngs::StdRng, seq::SliceRandom, Rng, SeedableRng};
+use random_traffic_test::*;
+use std::cell::{Cell, RefCell};
+use std::collections::BTreeMap;
+use std::ops::RangeInclusive;
+
+thread_local! {
+    static RNG: RefCell<StdRng> = RefCell::new(StdRng::seed_from_u64(13));
+    static CONFIG: RefCell<Config> = RefCell::default();
+    static CALLS_PER_ROUND: Cell<u32> = Cell::default();
+    static CALL_ID: Cell<u32> = Cell::default();
+    static RECORDS: RefCell<BTreeMap<u32, Record>> = RefCell::default();
+}
+
+/// Sets the canister state according to `f` by parsing `arg_data` of type `T`.
+fn set_state<T, F>(f: F) -> ()
+where
+    T: candid::CandidType + for<'a> serde::Deserialize<'a>,
+    F: FnOnce(T) -> (),
+{
+    let msg = match candid::Decode!(&api::arg_data()[..], T) {
+        Ok(item) => {
+            f(item);
+            "accepted"
+        }
+        Err(_) => "rejected",
+    };
+
+    let msg = candid::Encode!(&msg.to_string()).unwrap();
+    api::reply(&msg[..]);
+}
+
+/// Sets the test config.
+#[export_name = "canister_update set_config"]
+fn set_config() {
+    set_state(|config: Config| CONFIG.set(config));
+}
+
+/// Sets the requests per round to be sent each heart beat.
+#[export_name = "canister_update set_calls_per_round"]
+fn set_calls_per_round() {
+    set_state(|calls_per_round: u32| CALLS_PER_ROUND.set(calls_per_round));
+}
+
+/// Seeds `RNG`.
+#[export_name = "canister_update seed_rng"]
+fn seed_rng() {
+    set_state(|seed: u64| RNG.set(StdRng::seed_from_u64(seed)));
+}
+
+fn insert_sent_record(call_id: u32, record: Request) {
+    RECORDS.with_borrow_mut(|records| {
+        records.insert(
+            call_id,
+            Record {
+                sent: record,
+                received: None,
+            },
+        );
+    })
+}
+
+fn set_received_record(call_id: u32, record: Response) {
+    RECORDS.with_borrow_mut(|records| {
+        records.get_mut(&call_id).unwrap().received = Some(record);
+    });
+}
+
+/// Returns the canister records.
+#[export_name = "canister_query records"]
+fn records() {
+    let records = RECORDS.with_borrow(|records| records.values().cloned().collect::<Vec<_>>());
+    let msg = candid::Encode!(&records).unwrap();
+    api::reply(&msg[..]);
+}
+
+/// Returns a random receiver if any.
+fn choose_receiver() -> Option<CanisterId> {
+    CONFIG.with_borrow(|config| {
+        RNG.with_borrow_mut(|rng| config.receivers.as_slice().choose(rng).cloned())
+    })
+}
+
+/// Wrapper to generate a random `u32` from one of the ranges in `Config`.
+fn gen_range<F>(f: F) -> u32
+where
+    F: FnOnce(&Config) -> RangeInclusive<u32>,
+{
+    CONFIG.with_borrow(|config| RNG.with_borrow_mut(|rng| rng.gen_range(f(&config))))
+}
+
+/// Determines whether a downstream call should be attempted or if a reply should be sent back.
+fn probe_make_call() -> bool {
+    CONFIG.with_borrow(|config| {
+        let dist = rand::distributions::WeightedIndex::new([
+            config.downstream_call_weight,
+            config.reply_weight,
+        ])
+        .unwrap();
+        let choices = [true, false];
+        RNG.with_borrow_mut(|rng| choices[dist.sample(rng)])
+    })
+}
+
+/// Returns a message id for use in keeping records.
+fn next_call_id() -> u32 {
+    let id = CALL_ID.take();
+    CALL_ID.set(id + 1);
+    id
+}
+
+/// Attemps to call a randomly chosen `receiver` with a random payload size.
+fn try_call() -> Result<(), ()> {
+    let receiver = choose_receiver().ok_or(())?;
+    let payload_bytes = gen_range(|config| config.call_bytes_min..=config.call_bytes_max);
+
+    let call_id = next_call_id();
+    let on_reply = move || {
+        let reply = candid::Decode!(&api::arg_data()[..], Vec<u8>).unwrap();
+        set_received_record(call_id, Response::Data(reply.len() as u32));
+    };
+    let on_reject = move || {
+        set_received_record(call_id, Response::Rejected(api::reject_message()));
+    };
+
+    let msg = candid::Encode!(&vec![0_u8; payload_bytes as usize]).unwrap();
+    match api::call_with_callbacks(
+        api::CanisterId::try_from(receiver).unwrap(),
+        "handle_call",
+        &msg[..],
+        on_reply,
+        on_reject,
+    ) {
+        0 => {
+            insert_sent_record(call_id, Request::Data(msg.len() as u32));
+            Ok(())
+        }
+        error_code => {
+            insert_sent_record(call_id, Request::Rejected(error_code));
+            Err(())
+        }
+    }
+}
+
+/// Replies with a random payload size after a random count of instructions have passed.
+fn reply() {
+    let payload_bytes = gen_range(|config| config.reply_bytes_min..=config.reply_bytes_max);
+    let instructions_count =
+        gen_range(|config| config.instructions_count_min..=config.instructions_count_max);
+
+    // Do some thinking.
+    let counts = api::performance_counter(0) + instructions_count as u64;
+    while counts > api::performance_counter(0) {}
+
+    let msg = candid::Encode!(&vec![0_u8; payload_bytes as usize]).unwrap();
+    api::reply(&msg[..]);
+}
+
+/// Randomly determines whether to make a downstream call; reply if not or if the downstream call fails.
+#[export_name = "canister_update handle_call"]
+fn handle_call() {
+    if probe_make_call() {
+        if let Err(()) = try_call() {
+            reply();
+        }
+    } else {
+        reply();
+    }
+}
+
+/// Attempts to call random canisters; stops once
+/// - the maximum number of calls per round is reached.
+/// - calling fails synchronously.
+#[export_name = "canister_heartbeat"]
+fn heartbeat() {
+    for _ in 0..CALLS_PER_ROUND.get() {
+        if let Err(()) = try_call() {
+            return;
+        }
+    }
+}
+
+#[export_name = "canister_init"]
+fn main() {}
