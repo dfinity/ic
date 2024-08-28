5311b6519160a4df6e746d60472a0a1d38ffa44a
Andrew Battat <andrew.battat@dfinity.org>
2024-08-28 10:13:29 +0000

    Create constructors

--- rs/ic_os/config/src/types.rs
+++ rs/ic_os/config/src/types.rs
@@ -1,17 +1,17 @@
 use ic_types::malicious_behaviour::MaliciousBehaviour;
 
-struct SetuposConfig {
+pub struct SetuposConfig {
     hostos_config: HostOSConfig,
 }
 
-struct HostOSConfig {
+pub struct HostOSConfig {
     vm_memory: u32,
     vm_cpu: String,
     ic_config: IcConfig,
 }
 
 // todo: fix types and separate dev/prod
-struct IcConfig {
+pub struct IcConfig {
     networking: Networking,
     nns_public_key_path: String,
     nns_url: String,
@@ -20,23 +20,165 @@ struct IcConfig {
     hostname: String,
     node_operator_private_key_path: Option<String>,
 
-    ic_crypto_path: String,
-    ic_state_path: String,
-    ic_registry_local_store_path: String,
-    accounts_ssh_authorized_keys_path: String,
-    backup_retention_time_seconds: String,
-    backup_puging_interval_seconds: String,
-    malicious_behavior: MaliciousBehaviour,
-    query_stats_epoch_length: String,
-    bitcoind_addr: String,
-    jaeger_addr: String,
-    socks_proxy: String,
+    ic_crypto_path: Option<String>,
+    ic_state_path: Option<String>,
+    ic_registry_local_store_path: Option<String>,
+    accounts_ssh_authorized_keys_path: Option<String>,
+    backup_retention_time_seconds: Option<String>,
+    backup_purging_interval_seconds: Option<String>,
+    malicious_behavior: Option<MaliciousBehaviour>,
+    query_stats_epoch_length: Option<String>,
+    bitcoind_addr: Option<String>,
+    jaeger_addr: Option<String>,
+    socks_proxy: Option<String>,
 }
 
-struct Networking {
+pub struct Networking {
     ipv6_address: String,
     ipv6_gateway: String,
     ipv4_address: Option<String>,
     ipv4_gateway: Option<String>,
     domain: Option<String>,
+}
+
+impl SetuposConfig {
+    pub fn new(
+        vm_memory: u32,
+        vm_cpu: String,
+        nns_public_key_path: String,
+        nns_url: String,
+        elasticsearch_hosts: String,
+        elasticsearch_tags: Option<String>,
+        hostname: String,
+        node_operator_private_key_path: Option<String>,
+        ic_crypto_path: Option<String>,
+        ic_state_path: Option<String>,
+        ic_registry_local_store_path: Option<String>,
+        accounts_ssh_authorized_keys_path: Option<String>,
+        backup_retention_time_seconds: Option<String>,
+        backup_purging_interval_seconds: Option<String>,
+        malicious_behavior: Option<MaliciousBehaviour>,
+        query_stats_epoch_length: Option<String>,
+        bitcoind_addr: Option<String>,
+        jaeger_addr: Option<String>,
+        socks_proxy: Option<String>,
+        ipv6_address: String,
+        ipv6_gateway: String,
+        ipv4_address: Option<String>,
+        ipv4_gateway: Option<String>,
+        domain: Option<String>,
+    ) -> Self {
+        let ic_config = IcConfig::new(
+            nns_public_key_path,
+            nns_url,
+            elasticsearch_hosts,
+            elasticsearch_tags,
+            hostname,
+            node_operator_private_key_path,
+            ic_crypto_path,
+            ic_state_path,
+            ic_registry_local_store_path,
+            accounts_ssh_authorized_keys_path,
+            backup_retention_time_seconds,
+            backup_purging_interval_seconds,
+            malicious_behavior,
+            query_stats_epoch_length,
+            bitcoind_addr,
+            jaeger_addr,
+            socks_proxy,
+            ipv6_address,
+            ipv6_gateway,
+            ipv4_address,
+            ipv4_gateway,
+            domain,
+        );
+
+        let hostos_config = HostOSConfig::new(vm_memory, vm_cpu, ic_config);
+
+        SetuposConfig { hostos_config }
+    }
+}
+
+impl HostOSConfig {
+    pub fn new(vm_memory: u32, vm_cpu: String, ic_config: IcConfig) -> Self {
+        HostOSConfig {
+            vm_memory,
+            vm_cpu,
+            ic_config,
+        }
+    }
+}
+
+impl IcConfig {
+    pub fn new(
+        nns_public_key_path: String,
+        nns_url: String,
+        elasticsearch_hosts: String,
+        elasticsearch_tags: Option<String>,
+        hostname: String,
+        node_operator_private_key_path: Option<String>,
+        ic_crypto_path: Option<String>,
+        ic_state_path: Option<String>,
+        ic_registry_local_store_path: Option<String>,
+        accounts_ssh_authorized_keys_path: Option<String>,
+        backup_retention_time_seconds: Option<String>,
+        backup_purging_interval_seconds: Option<String>,
+        malicious_behavior: Option<MaliciousBehaviour>,
+        query_stats_epoch_length: Option<String>,
+        bitcoind_addr: Option<String>,
+        jaeger_addr: Option<String>,
+        socks_proxy: Option<String>,
+        ipv6_address: String,
+        ipv6_gateway: String,
+        ipv4_address: Option<String>,
+        ipv4_gateway: Option<String>,
+        domain: Option<String>,
+    ) -> Self {
+        let networking = Networking::new(
+            ipv6_address,
+            ipv6_gateway,
+            ipv4_address,
+            ipv4_gateway,
+            domain,
+        );
+
+        IcConfig {
+            networking,
+            nns_public_key_path,
+            nns_url,
+            elasticsearch_hosts,
+            elasticsearch_tags,
+            hostname,
+            node_operator_private_key_path,
+            ic_crypto_path,
+            ic_state_path,
+            ic_registry_local_store_path,
+            accounts_ssh_authorized_keys_path,
+            backup_retention_time_seconds,
+            backup_purging_interval_seconds,
+            malicious_behavior,
+            query_stats_epoch_length,
+            bitcoind_addr,
+            jaeger_addr,
+            socks_proxy,
+        }
+    }
+}
+
+impl Networking {
+    fn new(
+        ipv6_address: String,
+        ipv6_gateway: String,
+        ipv4_address: Option<String>,
+        ipv4_gateway: Option<String>,
+        domain: Option<String>,
+    ) -> Self {
+        Networking {
+            ipv6_address,
+            ipv6_gateway,
+            ipv4_address,
+            ipv4_gateway,
+            domain,
+        }
+    }
 }
