5ab5f30e2c7f784a957668117bb1da7c6968b8be
Daniel Wong <daniel.wong@dfinity.org>
2024-08-28 18:26:02 +0200

    Test that opaque principal gets rejected by registry when get_changes_since is called.

---
+++ rs/nns/integration_tests/src/registry_get_changes_since.rs
@@ -0,0 +1,43 @@
+use ic_base_types::{PrincipalId, PrincipalIdClass};
+use ic_nns_test_utils::{
+    common::NnsInitPayloadsBuilder,
+    state_test_helpers::{
+        registry_get_changes_since, setup_nns_canisters, state_machine_builder_for_nns_tests,
+    },
+};
+use ic_registry_transport::pb::v1::{RegistryGetChangesSinceResponse, RegistryError, registry_error};
+
+#[test]
+fn test_disallow_opauqe_callers() {
+    let state_machine = state_machine_builder_for_nns_tests().build();
+    let nns_init_payloads = NnsInitPayloadsBuilder::new().build();
+    setup_nns_canisters(&state_machine, nns_init_payloads);
+
+    let sender = PrincipalId::new_user_test_id(42);
+    assert_eq!(sender.class(), Ok(PrincipalIdClass::Opaque));
+    let response = registry_get_changes_since(&state_machine, sender, 1);
+
+    let RegistryGetChangesSinceResponse {
+        error,
+        version,
+        deltas,
+    } = response;
+
+    assert_eq!(version, 0);
+    assert_eq!(deltas, vec![]);
+
+    let error = error.unwrap();
+    let RegistryError {
+        code,
+        reason,
+        key,
+    } = error;
+
+    assert_eq!(key, Vec::<u8>::new());
+
+    assert_eq!(registry_error::Code::try_from(code), Ok(registry_error::Code::Authorization));
+    let reason = reason.to_lowercase();
+    for key_word in ["caller", "opaque"] {
+        assert!(reason.contains(key_word), "{} not in {:?}", key_word, reason);
+    }
+}
