e802a27818ba054e49643d78e2169752910d8cc5
Daniel Wong <daniel.wong@dfinity.org>
2024-08-29 13:16:04 +0200

    Added test that shows that a non-opaque principals are (still) allowed to call the get_changes_since method of the registry canister (per review feedback).

--- rs/nns/integration_tests/src/registry_get_changes_since.rs
+++ rs/nns/integration_tests/src/registry_get_changes_since.rs
@@ -8,6 +8,7 @@ use ic_nns_test_utils::{
 use ic_registry_transport::pb::v1::{
     registry_error, RegistryError, RegistryGetChangesSinceResponse,
 };
+use std::str::FromStr;
 
 #[test]
 fn test_disallow_opaque_callers() {
@@ -49,4 +50,29 @@ fn test_disallow_opaque_callers() {
             reason
         );
     }
+}
+
+#[test]
+fn test_allow_non_opaque_callers() {
+    // Step 1: Prepare the world. (Same as previous test.)
+    let state_machine = state_machine_builder_for_nns_tests().build();
+    let nns_init_payloads = NnsInitPayloadsBuilder::new().build();
+    setup_nns_canisters(&state_machine, nns_init_payloads);
+
+    // Step 2: Call the code under test. Unlike the previous test, the sender is a
+    // self-authenticatying principal, not an opaque principal.
+    let sender =
+        PrincipalId::from_str("ubktz-haghv-fqsdh-23fhi-3urex-bykoz-pvpfd-5rs6w-qpo3t-nf2dv-oae")
+            .unwrap();
+    assert_eq!(sender.class(), Ok(PrincipalIdClass::SelfAuthenticating));
+    let response = registry_get_changes_since(&state_machine, sender, 1);
+
+    // Step 3: Inspect results.
+    let RegistryGetChangesSinceResponse {
+        error,
+        version: _,
+        deltas: _,
+    } = response;
+
+    assert_eq!(error, None);
 }
