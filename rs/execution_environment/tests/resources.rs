11ab2f9a50fd924101b43a28782597d68979ffb3
Andriy Berestovskyy <andriy.berestovskyy@dfinity.org>
2024-08-30 11:12:50 +0200

    macOS fix

--- rs/execution_environment/tests/resources.rs
+++ rs/execution_environment/tests/resources.rs
@@ -5,24 +5,24 @@ use std::process::{Command, Stdio};
 fn poc() {
     let pid = std::process::id().to_string();
     println!("before creating SM");
-    Command::new("ps")
-        .args(["-o", "nlwp", &pid])
+    Command::new("sh")
+        .args(["-c", format!("ps -M {pid} | wc -l").as_str()])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status()
         .expect("failed to execute process");
     let env = StateMachine::new();
     println!("after creating SM");
-    Command::new("ps")
-        .args(["-o", "nlwp", &pid])
+    Command::new("sh")
+        .args(["-c", format!("ps -M {pid} | wc -l").as_str()])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status()
         .expect("failed to execute process");
     drop(env);
     println!("after dropping SM");
-    Command::new("ps")
-        .args(["-o", "nlwp", &pid])
+    Command::new("sh")
+        .args(["-c", format!("ps -M {pid} | wc -l").as_str()])
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .status()
