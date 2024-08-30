df8438ace2490e4766bae383945452c330383bb9
Eero Kelly <eero.kelly@dfinity.org>
2024-08-30 13:45:19 +0000

    Add tools to benchmark bare metal nodes

---
+++ ic-os/dev-tools/bare_metal_deployment/benchmark_driver.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -eEuo pipefail
+
+benchmark_runner="${1}"
+ssh_key="${2}"
+ip_address="${3}"
+tools="${@:4}"
+
+key_component=()
+if [ "${ssh_key}" != "None" ]; then
+    key_component=("-i", "${ssh_key}")
+fi
+
+TMPDIR=$(mktemp -d)
+trap "rm -rf ${TMPDIR}" exit
+
+# Send over the runner
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${key_component[@]}" "${benchmark_runner}" "admin@[${ip_address}]:benchmark_runner.sh"
+
+# Send over the tools
+if [ "${tools}" != "" ]; then
+    scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${key_component[@]}" ${tools[@]} "admin@[${ip_address}]:"
+fi
+
+# Run benchmarking
+>&2 echo "Benchmarking node..."
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${key_component[@]}" "admin@${ip_address}" "sudo ./benchmark_runner.sh"
+
+# Collect results
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "${key_component[@]}" -r "admin@[${ip_address}]:results" "${TMPDIR}"
+
+# Output results
+echo "-------------------- Benchmark Results --------------------"
+cat ${TMPDIR}/results/*
+echo "-----------------------------------------------------------"
