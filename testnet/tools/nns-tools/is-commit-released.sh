660c721e85000cf9f866b270ed41666738b3dfbe
Daniel Wong <daniel.wong@dfinity.org>
2024-09-02 17:50:49 +0200

    Added is-commit-released.sh to nns-tools.

---
+++ testnet/tools/nns-tools/is-commit-released.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+NNS_TOOLS_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+source "$NNS_TOOLS_DIR/lib/include.sh"
+
+CANISTER_NAME=$1
+QUERY_COMMIT_ID=$2
+
+CANISTER_ID=$(nns_canister_id "$CANISTER_NAME")
+echo "Fetching git_commit_id from $CANISTER_NAME ($CANISTER_ID)..."
+
+CURRENT_COMMIT_ID=$(dfx canister --ic metadata "$CANISTER_ID" 'git_commit_id')
+echo "$CANISTER_NAME reports itself to be on commit $CURRENT_COMMIT_ID."
+
+echo
+if git merge-base --is-ancestor "$QUERY_COMMIT_ID" "$CURRENT_COMMIT_ID"; then
+    echo "🎉 ${QUERY_COMMIT_ID} is included in the code that $CANISTER_NAME is currently running."
+    exit 0
+else
+    echo "🙅 ${QUERY_COMMIT_ID} is not (yet?) being run by $CANISTER_NAME."
+    exit 1
+fi
