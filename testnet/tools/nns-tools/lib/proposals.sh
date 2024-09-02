363e1f6b253f0da3df6bbbbecc7b35c196ee4521
Daniel Wong <daniel.wong@dfinity.org>
2024-09-02 18:05:09 +0200

    Small corrections in the UI of a couple of scripts in nns-tools.

--- testnet/tools/nns-tools/lib/proposals.sh
+++ testnet/tools/nns-tools/lib/proposals.sh
@@ -157,11 +157,11 @@ This should match \`wasm_module\` field of this proposal.$(if [ ! -z "$CANDID_AR
 [latest-didc]: https://github.com/dfinity/candid/releases/latest
 
 \`\`\`
-didc encode '$CANDID_ARGS' | xxd -r -p | sha256sum
+didc encode '$CANDID_ARGS'
 
 \`\`\`
 
-This should match the \`arg_hash\` field of this proposal.
+This should match the \`arg_hex\` field of this proposal.
 "
         fi)
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
