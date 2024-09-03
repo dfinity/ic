dbf6b74c0cd073cf96d5549d3826836823c3a11c
Ognjen Maric <ognjen.maric@dfinity.org>
2024-09-03 13:23:08 +0000

    Vendor LocalKey in from tokio
    Our Bazel infrastructure lacks support for the same crate with different
    features, so vendoring LocalKey in.

--- rs/tla_instrumentation/tla_instrumentation/tests/structs.rs
+++ rs/tla_instrumentation/tla_instrumentation/tests/structs.rs
@@ -25,12 +25,12 @@ mod tla_stuff {
     pub const PID: &str = "Counter";
     pub const CAN_NAME: &str = "mycan";
 
+    use local_key::task_local;
     use std::{collections::BTreeMap, sync::RwLock};
     use tla_instrumentation::{
         GlobalState, InstrumentationState, Label, TlaConstantAssignment, TlaValue, ToTla, Update,
         UpdateTrace, VarAssignment,
     };
-    use tokio::task_local;
 
     task_local! {
         pub static TLA_INSTRUMENTATION_STATE: InstrumentationState;
