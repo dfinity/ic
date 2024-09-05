2f6d99284ff5cbf920743a2c0ae9de36dc78e79c
Andrew Battat <andrew.battat@dfinity.org>
2024-09-05 17:26:07 +0000

    Cargo fmt

--- rs/ic_os/hostos_tool/src/main.rs
+++ rs/ic_os/hostos_tool/src/main.rs
@@ -73,8 +73,9 @@ pub fn main() -> Result<()> {
                 }
             };
 
-            let deployment_name = deployment_name
-            .context("Error: Deployment name not found when attempting to generate mac address")?;
+            let deployment_name = deployment_name.context(
+                "Error: Deployment name not found when attempting to generate mac address",
+            )?;
 
             generate_network_config(
                 &network_info,
