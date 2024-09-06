514553b1b9db15afc272c3bbb8f2aa615f68ed49
Kamil Popielarz <kamil.popielarz@dfinity.org>
2024-09-06 09:03:03 +0000

    subnet_splitting_test.rs

--- rs/tests/src/orchestrator/mod.rs
+++ rs/tests/src/orchestrator/mod.rs
@@ -1,7 +1,6 @@
 pub mod node_assign_test;
 pub mod subnet_recovery_app_subnet;
 pub mod subnet_recovery_nns_subnet;
-pub mod subnet_splitting;
 pub mod unassigned_node_upgrade_test;
 pub mod unstuck_subnet_test;
 pub mod upgrade_downgrade;
