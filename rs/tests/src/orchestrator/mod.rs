791363a268663fabc5d26deaaebe90bde51e14de
Kamil Popielarz <kamil.popielarz@dfinity.org>
2024-09-06 09:11:35 +0000

    unstuck_subnet_test.rs

--- rs/tests/src/orchestrator/mod.rs
+++ rs/tests/src/orchestrator/mod.rs
@@ -2,6 +2,5 @@ pub mod node_assign_test;
 pub mod subnet_recovery_app_subnet;
 pub mod subnet_recovery_nns_subnet;
 pub mod unassigned_node_upgrade_test;
-pub mod unstuck_subnet_test;
 pub mod upgrade_downgrade;
 pub mod upgrade_with_alternative_urls;
