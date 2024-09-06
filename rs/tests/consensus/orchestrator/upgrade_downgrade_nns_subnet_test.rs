942c18bb891d24f93dc133a870949c4a674da66a
Kamil Popielarz <kamil.popielarz@dfinity.org>
2024-09-06 12:08:46 +0000

    upgrade_downgrade_nns_subnet_test.rs

--- rs/tests/consensus/orchestrator/upgrade_downgrade_nns_subnet_test.rs
+++ rs/tests/consensus/orchestrator/upgrade_downgrade_nns_subnet_test.rs
@@ -1,18 +1,41 @@
+use std::time::Duration;
+
 use anyhow::Result;
 
+use ic_consensus_system_test_upgrade_common::upgrade_downgrade_nns_subnet;
+use ic_consensus_system_test_utils::rw_message::install_nns_and_check_progress;
 use ic_registry_subnet_type::SubnetType;
 use ic_system_test_driver::driver::group::SystemTestGroup;
+use ic_system_test_driver::driver::ic::{InternetComputer, Subnet};
+use ic_system_test_driver::driver::test_env::TestEnv;
+use ic_system_test_driver::driver::test_env_api::HasTopologySnapshot;
 use ic_system_test_driver::systest;
-use ic_tests::orchestrator::upgrade_downgrade::{
-    config, upgrade_downgrade_nns_subnet, UP_DOWNGRADE_OVERALL_TIMEOUT,
-    UP_DOWNGRADE_PER_TEST_TIMEOUT,
-};
+use ic_types::Height;
 
+const DKG_INTERVAL: u64 = 9;
+const ALLOWED_FAILURES: usize = 1;
+const SUBNET_SIZE: usize = 3 * ALLOWED_FAILURES + 1; // 4 nodes
+const UP_DOWNGRADE_OVERALL_TIMEOUT: Duration = Duration::from_secs(25 * 60);
+const UP_DOWNGRADE_PER_TEST_TIMEOUT: Duration = Duration::from_secs(20 * 60);
+
+fn setup(env: TestEnv) {
+    let subnet_under_test = Subnet::new(SubnetType::System)
+        .add_nodes(SUBNET_SIZE)
+        .with_dkg_interval_length(Height::from(DKG_INTERVAL));
+
+    InternetComputer::new()
+        .with_mainnet_config()
+        .add_subnet(subnet_under_test)
+        .setup_and_start(&env)
+        .expect("failed to setup IC under test");
+
+    install_nns_and_check_progress(env.topology_snapshot());
+}
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_overall_timeout(UP_DOWNGRADE_OVERALL_TIMEOUT)
         .with_timeout_per_test(UP_DOWNGRADE_PER_TEST_TIMEOUT)
-        .with_setup(|env| config(env, SubnetType::System, true))
+        .with_setup(setup)
         .add_test(systest!(upgrade_downgrade_nns_subnet))
         .execute_from_args()?;
 
