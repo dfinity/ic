498c8aae9c5eaeb3675ed9a8cd2e18a639216c9a
Max Summe <maximilian.summe@dfinity.org>
2024-09-06 12:49:49 -0700

    Remove change unintended for this PR

--- rs/nns/governance/tests/fixtures/mod.rs
+++ rs/nns/governance/tests/fixtures/mod.rs
@@ -717,9 +717,7 @@ impl NNS {
                 .ledger
                 .accounts
                 .clone(),
-            governance_proto: GovernanceProto {
-                ..Default::default()
-            },
+            governance_proto: self.governance.clone_proto(),
             latest_gc_num_proposals: self.governance.latest_gc_num_proposals,
         }
     }
