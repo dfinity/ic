dfinity/ic

commit 3c1524ed2b702da8d9d2354597eab5b1abf8414b

feat(ckerc20): Add already installed canisters to ledger suite orchestrator (#1312)
XC-189: Add the ability to register via upgrade arguments ledger suites
that were initially not installed by the ledger suite orchestrator to be
able to be managed by the orchestrator, e.g. to top-up cycles or to
upgrade the underlying canisters. This is required to be able to add,
via a future NNS proposal, the ckETH ledger suite under control of the
ledger suite orchestrator.

Since the design of the orchestrator currently assumes that all managed
ledger suites corresponds to ckERC20 tokens, which would not be the case
if the ckETH ledger suite was to be managed by the orchestrator, an
important refactoring was necessary:

1. Clear separation between ckERC20 ledger suites and the other ones. In
particular, `OrchestratorInfo::managed_canisters` contains only ckERC20
ledger suites managed by the orchestrator, while the other ones are
visible via a new dedicated field
`OrchestratorInfo::managed_other_canisters`.
2. Under the hood, to avoid a migration of stable memory, a new field
was added to `ManagedCanisters` to contain the managed ledger suites
which are not ckERC20 tokens. Those two fields are kept as an
implementation detail, by exposing a `TokenId` that wraps both notions
(a ledger suite for ckERC20 token or a ledger suite for some thing
else), such that managed canisters are only accessed by `TokenId`.
3. All current tasks (top-up, archive discovery, upgrades) of the
orchestrator should work as is for all managed canisters (ckERC20 ledger
suites and the other ones).

---------

Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>

rs/ethereum/ledger-suite-orchestrator/src/state/mod.rs
@@ -4,8 +4,9 @@ pub mod test_fixtures;
 mod tests;
 
 use crate::candid::{CyclesManagement, InitArg};
-use crate::scheduler::{Erc20Token, Task};
+use crate::scheduler::{Erc20Token, InvalidManageInstalledCanistersError, Task};
 use crate::storage::memory::{state_memory, StableMemory};
+use crate::storage::WasmHashError;
 use candid::Principal;
 use ic_cdk::trap;
 use ic_stable_structures::{storable::Bound, Cell, Storable};
@@ -15,6 +16,7 @@ use std::borrow::Cow;
 use std::cell::RefCell;
 use std::collections::{BTreeMap, BTreeSet};
 use std::fmt::{Debug, Display, Formatter};
+use std::iter::once;
 use std::marker::PhantomData;
 use std::str::FromStr;
 
@@ -213,7 +215,100 @@ pub type GitCommitHash = Hash<GIT_COMMIT_HASH_LENGTH>;
 
 #[derive(Clone, PartialEq, Debug, Default, Deserialize, Serialize)]
 pub struct ManagedCanisters {
+    /// Canisters for an ERC-20 token
     canisters: BTreeMap<Erc20Token, Canisters>,
+    /// Canisters for a non-ERC-20 token, identified by its token symbol
+    #[serde(default)]
+    other_canisters: BTreeMap<TokenSymbol, Canisters>,
+}
+
+impl ManagedCanisters {
+    pub fn find_by_id(&self, token_id: &TokenId) -> Option<&Canisters> {
+        match token_id {
+            TokenId::Erc20(contract) => self.canisters.get(contract),
+            TokenId::Other(symbol) => self.other_canisters.get(symbol),
+        }
+    }
+
+    pub fn get_mut(&mut self, token_id: &TokenId) -> Option<&mut Canisters> {
+        match token_id {
+            TokenId::Erc20(contract) => self.canisters.get_mut(contract),
+            TokenId::Other(symbol) => self.other_canisters.get_mut(symbol),
+        }
+    }
+
+    pub fn insert_once(&mut self, token_id: TokenId, canisters: Canisters) {
+        assert_eq!(
+            self.find_by_id(&token_id),
+            None,
+            "BUG: token {:?} is already managed",
+            token_id
+        );
+        let previous_element = match token_id {
+            TokenId::Erc20(contract) => self.canisters.insert(contract, canisters),
+            TokenId::Other(token_symbol) => self.other_canisters.insert(token_symbol, canisters),
+        };
+        assert_eq!(previous_element, None);
+    }
+
+    pub fn all_canisters_iter(&self) -> impl Iterator<Item = (TokenId, &Canisters)> {
+        self.canisters
+            .iter()
+            .map(|(key, value)| (TokenId::from(key.clone()), value))
+            .chain(
+                self.other_canisters
+                    .iter()
+                    .map(|(key, value)| (TokenId::from(key.clone()), value)),
+            )
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Deserialize, Serialize)]
+pub struct TokenSymbol(String);
+
+impl Display for TokenSymbol {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<String> for TokenSymbol {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Deserialize, Serialize)]
+pub enum TokenId {
+    Erc20(Erc20Token),
+    Other(TokenSymbol),
+}
+impl TokenId {
+    pub fn into_erc20_unchecked(self) -> Erc20Token {
+        match self {
+            TokenId::Erc20(token) => token,
+            TokenId::Other(symbol) => panic!("BUG: token id {:?} is not an ERC-20 token", symbol),
+        }
+    }
+
+    pub fn into_other_unchecked(self) -> TokenSymbol {
+        match self {
+            TokenId::Erc20(token) => panic!("BUG: token id {:?} is an ERC-20 token", token),
+            TokenId::Other(symbol) => symbol,
+        }
+    }
+}
+
+impl From<Erc20Token> for TokenId {
+    fn from(value: Erc20Token) -> Self {
+        TokenId::Erc20(value)
+    }
+}
+
+impl From<TokenSymbol> for TokenId {
+    fn from(value: TokenSymbol) -> Self {
+        TokenId::Other(value)
+    }
 }
 
 #[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
@@ -224,9 +319,9 @@ pub struct Canisters {
     pub metadata: CanistersMetadata,
 }
 
-#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Deserialize, Serialize)]
 pub struct CanistersMetadata {
-    pub ckerc20_token_symbol: String,
+    pub token_symbol: String,
 }
 
 impl Canisters {
@@ -251,16 +346,11 @@ impl Canisters {
         &self.archives
     }
 
-    pub fn collect_principals(&self) -> Vec<Principal> {
-        let mut result: Vec<Principal> = vec![];
-        if let Some(ledger_principal) = self.ledger_canister_id() {
-            result.push(*ledger_principal);
-        }
-        if let Some(index_principal) = self.index_canister_id() {
-            result.push(*index_principal);
-        }
-        result.extend(self.archives.clone());
-        result
+    pub fn principals_iter(&self) -> impl Iterator<Item = &Principal> {
+        self.ledger_canister_id()
+            .into_iter()
+            .chain(self.index_canister_id())
+            .chain(self.archive_canister_ids().iter())
     }
 }
 
@@ -445,16 +535,21 @@ impl State {
         &mut self.cycles_management
     }
 
-    pub fn managed_canisters_iter(&self) -> impl Iterator<Item = (&Erc20Token, &Canisters)> {
-        self.managed_canisters.canisters.iter()
+    pub fn all_managed_canisters_iter(&self) -> impl Iterator<Item = (TokenId, &Canisters)> {
+        self.managed_canisters.all_canisters_iter()
     }
 
-    pub fn managed_erc20_tokens_iter(&self) -> impl Iterator<Item = &Erc20Token> {
-        self.managed_canisters.canisters.keys()
+    pub fn all_managed_principals(&self) -> impl Iterator<Item = &Principal> {
+        self.all_managed_canisters_iter()
+            .flat_map(|(_, canisters)| canisters.principals_iter())
     }
 
-    pub fn managed_canisters(&self, contract: &Erc20Token) -> Option<&Canisters> {
-        self.managed_canisters.canisters.get(contract)
+    pub fn all_managed_tokens_ids_iter(&self) -> impl Iterator<Item = TokenId> + '_ {
+        self.all_managed_canisters_iter().map(|(id, _)| id)
+    }
+
+    pub fn managed_canisters(&self, token_id: &TokenId) -> Option<&Canisters> {
+        self.managed_canisters.find_by_id(token_id)
     }
 
     pub fn ledger_suite_version(&self) -> Option<&LedgerSuiteVersion> {
@@ -473,40 +568,38 @@ impl State {
         self.ledger_suite_version = Some(new_version);
     }
 
-    fn managed_canisters_mut(&mut self, contract: &Erc20Token) -> Option<&mut Canisters> {
-        self.managed_canisters.canisters.get_mut(contract)
+    fn managed_canisters_mut(&mut self, token_id: &TokenId) -> Option<&mut Canisters> {
+        self.managed_canisters.get_mut(token_id)
     }
 
     pub fn managed_status<'a, T: 'a>(
         &'a self,
-        contract: &Erc20Token,
+        token_id: &TokenId,
     ) -> Option<&'a ManagedCanisterStatus>
     where
         Canisters: ManageSingleCanister<T>,
     {
-        self.managed_canisters(contract)
+        self.managed_canisters(token_id)
             .and_then(|c| c.get().map(|c| &c.status))
     }
 
-    pub fn record_new_erc20_token(&mut self, contract: Erc20Token, metadata: CanistersMetadata) {
-        assert_eq!(
-            self.managed_canisters(&contract),
-            None,
-            "BUG: ERC-20 token {:?} is already managed",
-            contract
-        );
-        assert_eq!(
-            self.managed_canisters
-                .canisters
-                .insert(contract, Canisters::new(metadata)),
-            None
-        );
+    /// Record other canisters managed by the orchestrator.
+    pub fn record_manage_other_canisters(&mut self, other_canisters: InstalledLedgerSuite) {
+        let token_id = TokenId::from(other_canisters.token_symbol.clone());
+        self.managed_canisters
+            .insert_once(token_id, Canisters::from(other_canisters));
     }
 
-    pub fn record_archives(&mut self, contract: &Erc20Token, archives: Vec<Principal>) {
+    pub fn record_new_erc20_token(&mut self, contract: Erc20Token, metadata: CanistersMetadata) {
+        let token_id = TokenId::from(contract);
+        self.managed_canisters
+            .insert_once(token_id, Canisters::new(metadata));
+    }
+
+    pub fn record_archives(&mut self, token_id: &TokenId, archives: Vec<Principal>) {
         let canisters = self
-            .managed_canisters_mut(contract)
-            .unwrap_or_else(|| panic!("BUG: token {:?} is not managed", contract));
+            .managed_canisters_mut(token_id)
+            .unwrap_or_else(|| panic!("BUG: token {:?} is not managed", token_id));
         canisters.archives = archives;
     }
 
@@ -517,9 +610,10 @@ impl State {
     ) where
         Canisters: ManageSingleCanister<T>,
     {
+        let token_id = TokenId::from(contract.clone());
         let canisters = self
-            .managed_canisters_mut(contract)
-            .unwrap_or_else(|| panic!("BUG: token {:?} is not managed", contract));
+            .managed_canisters_mut(&token_id)
+            .unwrap_or_else(|| panic!("BUG: token {:?} is not managed", token_id));
         canisters
             .try_insert(Canister::<T>::new(ManagedCanisterStatus::Created {
                 canister_id,
@@ -537,14 +631,15 @@ impl State {
     where
         Canisters: ManageSingleCanister<T>,
     {
+        let token_id = TokenId::from(contract.clone());
         let managed_canister = self
-            .managed_canisters_mut(contract)
+            .managed_canisters_mut(&token_id)
             .and_then(Canisters::get_mut)
             .unwrap_or_else(|| {
                 panic!(
                     "BUG: no managed canisters or no {} canister for {:?}",
                     Canisters::display_name(),
-                    contract
+                    token_id
                 )
             });
         let canister_id = *managed_canister.canister_id();
@@ -697,4 +792,90 @@ pub fn init_state(state: State) {
             .set(ConfigState::Initialized(state))
             .expect("failed to initialize state in stable cell")
     });
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct InstalledLedgerSuite {
+    token_symbol: TokenSymbol,
+    ledger: Principal,
+    ledger_wasm_hash: WasmHash,
+    index: Principal,
+    index_wasm_hash: WasmHash,
+    archives: Vec<Principal>,
+}
+
+impl From<InstalledLedgerSuite> for Canisters {
+    fn from(value: InstalledLedgerSuite) -> Self {
+        Self {
+            ledger: Some(LedgerCanister::new(ManagedCanisterStatus::Installed {
+                canister_id: value.ledger,
+                installed_wasm_hash: value.ledger_wasm_hash,
+            })),
+            index: Some(IndexCanister::new(ManagedCanisterStatus::Installed {
+                canister_id: value.index,
+                installed_wasm_hash: value.index_wasm_hash,
+            })),
+            archives: value.archives,
+            metadata: CanistersMetadata {
+                token_symbol: value.token_symbol.to_string(),
+            },
+        }
+    }
+}
+
+impl InstalledLedgerSuite {
+    pub fn validate(
+        state: &State,
+        args: crate::candid::InstalledLedgerSuite,
+    ) -> Result<InstalledLedgerSuite, InvalidManageInstalledCanistersError> {
+        let token_symbol = TokenSymbol(args.token_symbol);
+        let token_id = TokenId::Other(token_symbol.clone());
+        if state.managed_canisters(&token_id).is_some() {
+            return Err(InvalidManageInstalledCanistersError::TokenAlreadyManaged(
+                token_symbol,
+            ));
+        }
+        let ledger = args.ledger.canister_id;
+        let ledger_wasm_hash = args.ledger.installed_wasm_hash.parse().map_err(|e| {
+            InvalidManageInstalledCanistersError::WasmHashError(WasmHashError::Invalid(e))
+        })?;
+        let index = args.index.canister_id;
+        let index_wasm_hash = args.index.installed_wasm_hash.parse().map_err(|e| {
+            InvalidManageInstalledCanistersError::WasmHashError(WasmHashError::Invalid(e))
+        })?;
+        if ledger_wasm_hash == index_wasm_hash {
+            return Err(InvalidManageInstalledCanistersError::WasmHashError(
+                WasmHashError::Invalid("ledger and index wasm hashes are the same".to_string()),
+            ));
+        }
+        let archives = args.archives.unwrap_or_default();
+
+        let installed_principals: BTreeSet<_> = once(&ledger)
+            .chain(once(&index))
+            .chain(archives.iter())
+            .collect();
+        let managed_principals: BTreeSet<_> = state.all_managed_principals().collect();
+        let overlapping_principals: BTreeSet<_> = managed_principals
+            .intersection(&installed_principals)
+            .collect();
+        if !overlapping_principals.is_empty() {
+            return Err(
+                InvalidManageInstalledCanistersError::AlreadyManagedPrincipals(
+                    overlapping_principals
+                        .into_iter()
+                        .cloned()
+                        .cloned()
+                        .collect(),
+                ),
+            );
+        }
+        Ok(InstalledLedgerSuite {
+            token_symbol,
+            ledger,
+            ledger_wasm_hash,
+            index,
+            index_wasm_hash,
+            archives,
+        })
+    }
 }

commit 48f24e7e48590122130186e894d8096b12e9b772

fix(ckerc20): bug in renaming field (#1429)
(XC-189): Fix a bug introduced in #1312 where a field stored in stable
memory was renamed. The rename is kept transparent using
`#[serde(rename)]` and test that ensures that changes in stable memory
are backwards-compatible were fixed as well.

rs/ethereum/ledger-suite-orchestrator/src/state/mod.rs
@@ -321,6 +321,7 @@ pub struct Canisters {
 
 #[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Deserialize, Serialize)]
 pub struct CanistersMetadata {
+    #[serde(rename = "ckerc20_token_symbol")]
     pub token_symbol: String,
 }
 

rs/ethereum/ledger-suite-orchestrator/src/state/tests.rs
@@ -401,10 +401,14 @@ mod validate_config {
 
 mod schema_upgrades {
     use crate::candid::CyclesManagement;
+    use crate::scheduler::test_fixtures::{
+        cketh_ledger_suite, cketh_token_symbol, usdc, usdc_ledger_suite,
+    };
     use crate::scheduler::Task;
     use crate::state::test_fixtures::arb_state;
     use crate::state::{
-        decode, encode, Canisters, Erc20Token, LedgerSuiteVersion, ManagedCanisters, State,
+        decode, encode, Canisters, CanistersMetadata, Erc20Token, IndexCanister, LedgerCanister,
+        LedgerSuiteVersion, ManagedCanisters, State,
     };
     use candid::{Deserialize, Principal};
     use proptest::proptest;
@@ -413,13 +417,49 @@ mod schema_upgrades {
 
     #[derive(Clone, PartialEq, Debug, Default, Deserialize, Serialize)]
     pub struct ManagedCanistersPreviousVersion {
-        canisters: BTreeMap<Erc20Token, Canisters>,
+        canisters: BTreeMap<Erc20Token, CanistersPreviousVersion>,
     }
 
     impl From<ManagedCanisters> for ManagedCanistersPreviousVersion {
         fn from(value: ManagedCanisters) -> Self {
             ManagedCanistersPreviousVersion {
-                canisters: value.canisters,
+                canisters: value
+                    .canisters
+                    .into_iter()
+                    .map(|(k, v)| (k, v.into()))
+                    .collect(),
+            }
+        }
+    }
+
+    #[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+    pub struct CanistersPreviousVersion {
+        pub ledger: Option<LedgerCanister>,
+        pub index: Option<IndexCanister>,
+        pub archives: Vec<Principal>,
+        pub metadata: CanistersMetadataPreviousVersion,
+    }
+
+    impl From<Canisters> for CanistersPreviousVersion {
+        fn from(value: Canisters) -> Self {
+            CanistersPreviousVersion {
+                ledger: value.ledger,
+                index: value.index,
+                archives: value.archives,
+                metadata: value.metadata.into(),
+            }
+        }
+    }
+
+    #[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+    pub struct CanistersMetadataPreviousVersion {
+        pub ckerc20_token_symbol: String,
+    }
+
+    impl From<CanistersMetadata> for CanistersMetadataPreviousVersion {
+        fn from(value: CanistersMetadata) -> Self {
+            CanistersMetadataPreviousVersion {
+                ckerc20_token_symbol: value.token_symbol,
             }
         }
     }
@@ -460,7 +500,9 @@ mod schema_upgrades {
 
     proptest! {
         #[test]
-        fn should_be_able_to_upgrade_state(state in arb_state()) {
+        fn should_be_able_to_upgrade_state(mut state in arb_state()) {
+            state.managed_canisters.canisters.insert(usdc(), usdc_ledger_suite());
+            state.managed_canisters.other_canisters.insert(cketh_token_symbol(), cketh_ledger_suite());
             let state_before_upgrade: StatePreviousVersion = state.into();
 
             let serialized_state_before_upgrade = encode(&state_before_upgrade);

rs/ethereum/ledger-suite-orchestrator/src/scheduler/test_fixtures.rs
@@ -1,5 +1,8 @@
 use crate::scheduler::Erc20Token;
-use crate::state::{CanistersMetadata, TokenId};
+use crate::state::{
+    Canisters, CanistersMetadata, IndexCanister, LedgerCanister, ManagedCanisterStatus, TokenId,
+    TokenSymbol,
+};
 
 pub const DAI_ADDRESS: &str = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 pub const USDC_ADDRESS: &str = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
@@ -39,6 +42,25 @@ pub fn usdc_metadata() -> CanistersMetadata {
     }
 }
 
+pub fn usdc_ledger_suite() -> Canisters {
+    Canisters {
+        ledger: Some(LedgerCanister::new(ManagedCanisterStatus::Installed {
+            canister_id: "xevnm-gaaaa-aaaar-qafnq-cai".parse().unwrap(),
+            installed_wasm_hash: "8457289d3b3179aa83977ea21bfa2fc85e402e1f64101ecb56a4b963ed33a1e6"
+                .parse()
+                .unwrap(),
+        })),
+        index: Some(IndexCanister::new(ManagedCanisterStatus::Installed {
+            canister_id: "xrs4b-hiaaa-aaaar-qafoa-cai".parse().unwrap(),
+            installed_wasm_hash: "eb3096906bf9a43996d2ca9ca9bfec333a402612f132876c8ed1b01b9844112a"
+                .parse()
+                .unwrap(),
+        })),
+        archives: vec!["t4dy3-uiaaa-aaaar-qafua-cai".parse().unwrap()],
+        metadata: usdc_metadata(),
+    }
+}
+
 pub fn usdt() -> Erc20Token {
     crate::candid::Erc20Contract {
         chain_id: 1_u8.into(),
@@ -55,5 +77,29 @@ pub fn usdt_token_id() -> TokenId {
 pub fn usdt_metadata() -> CanistersMetadata {
     CanistersMetadata {
         token_symbol: "ckUSDT".to_string(),
+    }
+}
+pub fn cketh_token_symbol() -> TokenSymbol {
+    TokenSymbol::from("ckETH".to_string())
+}
+
+pub fn cketh_ledger_suite() -> Canisters {
+    Canisters {
+        ledger: Some(LedgerCanister::new(ManagedCanisterStatus::Installed {
+            canister_id: "ss2fx-dyaaa-aaaar-qacoq-cai".parse().unwrap(),
+            installed_wasm_hash: "8457289d3b3179aa83977ea21bfa2fc85e402e1f64101ecb56a4b963ed33a1e6"
+                .parse()
+                .unwrap(),
+        })),
+        index: Some(IndexCanister::new(ManagedCanisterStatus::Installed {
+            canister_id: "s3zol-vqaaa-aaaar-qacpa-cai".parse().unwrap(),
+            installed_wasm_hash: "eb3096906bf9a43996d2ca9ca9bfec333a402612f132876c8ed1b01b9844112a"
+                .parse()
+                .unwrap(),
+        })),
+        archives: vec!["xob7s-iqaaa-aaaar-qacra-cai".parse().unwrap()],
+        metadata: CanistersMetadata {
+            token_symbol: cketh_token_symbol().to_string(),
+        },
     }
 }