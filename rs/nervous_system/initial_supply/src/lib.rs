e762633f54a5accaa47299ad4ac4fb165c3eedd9
Daniel Wong <daniel.wong@dfinity.org>
2024-09-13 21:13:38 +0200

    Library that determines the initial token supply by sending a bunch of queries to ledger.

---
+++ rs/nervous_system/initial_supply/src/lib.rs
@@ -0,0 +1,292 @@
+use candid::Nat;
+use ic_base_types::{CanisterId, PrincipalId};
+use ic_nervous_system_runtime::Runtime;
+use icrc_ledger_types::icrc3::{
+    archive::{ArchivedRange, QueryArchiveFn},
+    transactions::{
+        GetTransactionsRequest, GetTransactionsResponse, Transaction, TransactionRange,
+    },
+};
+use num_bigint::BigUint;
+use std::ops::AddAssign;
+
+pub async fn initial_supply_e8s<MyRuntime: Runtime>(
+    ledger_canister_id: CanisterId,
+    options: InitialSupplyOptions,
+) -> Result<u64, String> {
+    let InitialSupplyOptions {
+        batch_size,
+        max_transactions,
+    } = options;
+    let ledger = ThickLedgerClient::new(ledger_canister_id);
+
+    let mut result = Nat(BigUint::from(0_u64));
+    let mut transaction_count: u64 = 0;
+    let mut first_timestamp = None;
+    'outer: loop {
+        let transactions = ledger
+            .get_transactions::<MyRuntime>(transaction_count, batch_size)
+            .await?;
+
+        if transactions.is_empty() {
+            // No more transactions -> Done!
+            break;
+        }
+
+        for transaction in transactions {
+            // Look at timestamp. If != first_timestamp, we are done.
+            match first_timestamp {
+                None => {
+                    first_timestamp = Some(transaction.timestamp);
+                }
+                Some(first_timestamp) => {
+                    if transaction.timestamp != first_timestamp {
+                        // Found a non-initial transaction -> Done!
+                        break 'outer;
+                    }
+                }
+            }
+            debug_assert_eq!(Some(transaction.timestamp), first_timestamp);
+
+            // Bail if this scan seems to go on forever.
+            if transaction_count >= max_transactions {
+                return Err(format!(
+                    "Unable to find the last initial transaction after scanning {} transactions.",
+                    transaction_count,
+                ));
+            }
+
+            // Unpack transaction; it should be a mint.
+            let mint = match transaction.mint {
+                Some(ok) => ok,
+                None => {
+                    return Err(format!(
+                        "Transaction {} was not a mint, even though it was among the initial transactions: {:#?}",
+                        transaction_count, transaction,
+                    ));
+                }
+            };
+
+            // Update running totals.
+            result.add_assign(mint.amount);
+            transaction_count = transaction_count
+                .checked_add(1)
+                .ok_or_else(|| format!("Transaction count overflowed u64."))?;
+        }
+    }
+
+    // Convert to return type.
+    let result = u64::try_from(result.0).map_err(|err| {
+        format!(
+            "Failed to convert initial supply in e8s does to u64. Reason: {:?}",
+            err,
+        )
+    })?;
+
+    Ok(result)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct InitialSupplyOptions {
+    /// Give up if more than this many transactions need to be scanned to come up with a result.
+    pub max_transactions: u64,
+
+    /// How many transactions to fetch at a time.
+    pub batch_size: u64,
+}
+
+impl InitialSupplyOptions {
+    /// Sensible values.
+    pub fn new() -> Self {
+        Self {
+            max_transactions: 100_000,
+            batch_size: 250,
+        }
+    }
+
+    /*
+    pub fn max_transactions(&self) -> u64 {
+        self.max_transactions
+    }
+
+    pub fn set_max_transactions(&mut self, new_value: u64) -> &mut Self {
+        self.max_transactions = new_value;
+        self
+    }
+
+    pub fn with_max_transactions(mut self, new_value: u64) -> Self {
+        self.max_transactions = new_value;
+        self
+    }
+
+    pub fn batch_size(&self) -> u64 {
+        self.batch_size
+    }
+
+    pub fn set_batch_size(&mut self, new_value: u64) -> &mut Self {
+        self.batch_size = new_value;
+        self
+    }
+
+    pub fn with_batch_size(mut self, new_value: u64) -> Self {
+        self.batch_size = new_value;
+        self
+    }
+    */
+}
+
+impl Default for InitialSupplyOptions {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+// TODO: Might probably make sense to spin this in its own library.
+/// A ledger canister does not hold very old transactions. Instead, it passes
+/// old transactions to archive canister(s). Therefore, it is not enough to
+/// simply call a ledger canister's get_transactions method. The response can
+/// indicate that older records must be fetched from an archive canister.
+/// Following those redirects is our main job here.
+#[derive(Debug)]
+struct ThickLedgerClient {
+    ledger_canister_id: CanisterId,
+}
+
+impl ThickLedgerClient {
+    pub fn new(ledger_canister_id: CanisterId) -> Self {
+        Self { ledger_canister_id }
+    }
+
+    pub async fn get_transactions<MyRuntime: Runtime>(
+        &self,
+        start: u64,
+        length: u64,
+    ) -> Result<Vec<Transaction>, String> {
+        let start = Nat::from(start);
+        let length = Nat::from(length);
+        let request = GetTransactionsRequest {
+            start: start.clone(),
+            length: length.clone(),
+        };
+
+        let (mut response,): (GetTransactionsResponse,) =
+            MyRuntime::call_with_cleanup(self.ledger_canister_id, "get_transactions", (request,))
+                .await
+                .map_err(|err| format!("Failed to call ledger: {:?}", err))?;
+
+        response
+            .archived_transactions
+            .sort_by_key(|archived_range| archived_range.start.clone());
+        normalize_get_transactions_response(start, &mut response)?;
+
+        let mut result = vec![];
+        // Fetch transactions from archive.
+        // TODO: Make these calls concurrently. We only need to do this if
+        // serial is too slow.
+        for archived_range in response.archived_transactions {
+            let mut transactions = self
+                .follow_get_transactions_redirect::<MyRuntime>(archived_range)
+                .await
+                .map_err(|err| {
+                    format!(
+                        "Ledger {} (partially) forwarded us (presumably to archive), \
+                         but that failed: {}",
+                        self.ledger_canister_id, err,
+                    )
+                })?;
+            result.append(&mut transactions);
+        }
+
+        result.append(&mut response.transactions);
+
+        Ok(result)
+    }
+
+    async fn follow_get_transactions_redirect<MyRuntime: Runtime>(
+        &self,
+        archived_range: ArchivedRange<QueryArchiveFn<GetTransactionsRequest, TransactionRange>>,
+    ) -> Result<Vec<Transaction>, String> {
+        type Request = GetTransactionsRequest;
+        type Response = TransactionRange;
+        type F = QueryArchiveFn<Request, Response>;
+
+        let ArchivedRange::<F> {
+            start,
+            length,
+            callback,
+        } = archived_range;
+
+        let F {
+            canister_id,
+            method,
+            _marker: _,
+        } = callback;
+
+        let request = Request { start, length };
+        let (response,): (Response,) = MyRuntime::call_with_cleanup(
+            CanisterId::unchecked_from_principal(PrincipalId::from(canister_id)),
+            &method,
+            (request,),
+        )
+        .await
+        .map_err(|err| {
+            format!(
+                "Redirected to the {} method of {}, but calling that method failed: {:?}",
+                method, canister_id, err,
+            )
+        })?;
+
+        Ok(response.transactions)
+    }
+}
+
+/// Returns Err if the response seems to have gaps.
+///
+/// Here, normalizing just means that response.archived_transactions is sorted.
+fn normalize_get_transactions_response(
+    mut start: Nat,
+    response: &mut GetTransactionsResponse,
+) -> Result<(), String> {
+    if start >= response.log_length {
+        // These should already be empty, but since we are normalizing right
+        // now, let's clear just to be super sure.
+        response.transactions.clear();
+        response.archived_transactions.clear();
+
+        return Ok(());
+    }
+
+    // This is in case archived_transactions is not already sorted. That might
+    // be guaranteed already, but I have not seen any such promises in writing.
+    // This is really all the normalization going on here. The rest of this
+    // function just makes sure that the response is not missing any requested
+    // transactions.
+    response
+        .archived_transactions
+        .sort_by_key(|archived_range| archived_range.start.clone());
+
+    for archived_range in &response.archived_transactions {
+        if archived_range.start != start {
+            return Err(format!(
+                "GetTransactionsResponse seems to be missing requested transactions \
+                 from {} to ({} - 1). (This is might not be a retry-able failure.)",
+                start, archived_range.start,
+            ));
+        }
+
+        start.add_assign(archived_range.length.clone());
+    }
+
+    if response.first_index != start {
+        return Err(format!(
+            "GetTransactionsResponse seems to be missing requested transactions \
+             from {} to ({} - 1). (This is might not be a retry-able failure.)",
+            start, response.first_index,
+        ));
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests;
---
+++ rs/nervous_system/initial_supply/src/tests.rs
@@ -0,0 +1,232 @@
+use super::*;
+use async_trait::async_trait;
+use candid::{
+    decode_args, encode_args,
+    utils::{ArgumentDecoder, ArgumentEncoder},
+};
+use icrc_ledger_types::{icrc1::account::Account, icrc3::transactions::Mint};
+use lazy_static::lazy_static;
+use std::{cell::RefCell, collections::VecDeque, future::Future};
+
+fn i2nat(i: u64) -> Nat {
+    Nat(BigUint::from(i))
+}
+
+#[tokio::test]
+async fn test_initial_supply() {
+    // Step 1: Prepare the world.
+
+    const BATCH_SIZE: u64 = 4;
+
+    lazy_static! {
+        static ref LEDGER_CANISTER_ID: CanisterId = CanisterId::from_u64(42);
+        static ref ARCHIVE_CANISTER_ID: CanisterId = CanisterId::from_u64(43);
+        static ref FIRST_TRANSACTION_TIMESTAMP_NANOSECONDS: u64 = 1_669_073_904_187_044_208;
+        static ref LEDGER_REQUEST_1: Vec<u8> = encode_args((GetTransactionsRequest {
+            start: i2nat(0),
+            length: i2nat(BATCH_SIZE),
+        },))
+        .unwrap();
+        static ref ARCHIVE_REQUEST_1: Vec<u8> = encode_args((GetTransactionsRequest {
+            start: i2nat(2),
+            length: i2nat(2),
+        },))
+        .unwrap();
+        static ref LEDGER_REQUEST_2: Vec<u8> = encode_args((GetTransactionsRequest {
+            start: i2nat(4),
+            length: i2nat(BATCH_SIZE),
+        },))
+        .unwrap();
+    }
+
+    println!("\n");
+    println!("LEDGER: {}", *LEDGER_CANISTER_ID);
+    println!("ARCHIVE: {}", *ARCHIVE_CANISTER_ID);
+    println!("\n");
+
+    type Call = (
+        (CanisterId, &'static str, &'static [u8]), // Call Arguments
+        Result<Vec<u8>, (i32, String)>,            // Return Value
+    );
+    thread_local! {
+        static EXPECTED_CALLS: RefCell<VecDeque<Call>> = {
+            let ledger_request_1: &[u8] = &*LEDGER_REQUEST_1;
+            let archive_request_1: &[u8] = &*ARCHIVE_REQUEST_1;
+            let ledger_request_2: &[u8] = &*LEDGER_REQUEST_2;
+
+            let mut garbage = 531_157_996;
+            let mut new_mint = |amount: u64| {
+                garbage += 1;
+
+                Transaction {
+                    timestamp: *FIRST_TRANSACTION_TIMESTAMP_NANOSECONDS,
+                    mint: Some(Mint {
+                        amount: i2nat(amount),
+                        created_at_time: Some(*FIRST_TRANSACTION_TIMESTAMP_NANOSECONDS),
+                        to: Account {
+                            owner: PrincipalId::new_user_test_id(garbage).0,
+                            subaccount: None,
+                        },
+                        memo: None,
+                    }),
+
+                    kind: "mint".to_string(),
+
+                    burn: None,
+                    approve: None,
+                    transfer: None,
+                }
+            };
+
+            fn increase_timestamp(mut transaction: Transaction, nanoseconds: u64) -> Transaction {
+                transaction.timestamp += nanoseconds;
+
+                transaction
+                    .mint
+                    .as_mut()
+                    .unwrap()
+                    .created_at_time
+                    .as_mut()
+                    .map(|created_at_time| *created_at_time += nanoseconds);
+
+                transaction
+            }
+
+            type Redirect = QueryArchiveFn<GetTransactionsRequest, TransactionRange>;
+            type Range = ArchivedRange<Redirect>;
+
+            let ledger_response_1 = encode_args((GetTransactionsResponse {
+                transactions: vec![
+                    new_mint(1),
+                    new_mint(20),
+                ],
+                archived_transactions: vec![
+                    Range {
+                        start: i2nat(0),
+                        length: i2nat(2),
+                        callback: Redirect {
+                            canister_id: PrincipalId::from(*ARCHIVE_CANISTER_ID).0,
+                            method: "foo".to_string(),
+                            _marker: Default::default(),
+                        },
+                    }
+                ],
+                first_index: i2nat(2),
+                log_length: i2nat(7),
+            },)).unwrap();
+
+            let archive_response_1 = encode_args((TransactionRange {
+                transactions: vec![
+                    new_mint(300),
+                    new_mint(4000),
+                ],
+            },)).unwrap();
+
+            let ledger_response_2 = encode_args((GetTransactionsResponse {
+                transactions: vec![
+                    new_mint(50_000),
+                    new_mint(600_000),
+                    increase_timestamp(new_mint(7_000_000), 7),
+                ],
+                archived_transactions: vec![],
+                first_index: i2nat(4),
+                log_length: i2nat(7),
+            },)).unwrap();
+
+            RefCell::new(VecDeque::from([
+                // 1
+                (
+                    (LEDGER_CANISTER_ID.clone(), "get_transactions", ledger_request_1),
+                    Ok(ledger_response_1),
+                ),
+
+                // 2
+                (
+                    (ARCHIVE_CANISTER_ID.clone(), "foo", archive_request_1),
+                    Ok(archive_response_1)
+                ),
+
+                // 3
+                (
+                    (LEDGER_CANISTER_ID.clone(), "get_transactions", ledger_request_2),
+                    Ok(ledger_response_2),
+                ),
+            ]))
+        };
+    }
+
+    struct MockRuntime {}
+
+    #[async_trait]
+    impl Runtime for MockRuntime {
+        async fn call_without_cleanup<In, Out>(
+            _callee: CanisterId,
+            _method: &str,
+            _args: In,
+        ) -> Result<Out, (i32, String)>
+        where
+            In: ArgumentEncoder + Send,
+            Out: for<'a> ArgumentDecoder<'a>,
+        {
+            unimplemented!()
+        }
+
+        async fn call_with_cleanup<In, Out>(
+            callee: CanisterId,
+            method: &str,
+            args: In,
+        ) -> Result<Out, (i32, String)>
+        where
+            In: ArgumentEncoder + Send,
+            Out: for<'a> ArgumentDecoder<'a>,
+        {
+            let response =
+                Self::call_bytes_with_cleanup(callee, method, &encode_args(args).unwrap()).await?;
+
+            Ok(decode_args(&response).unwrap())
+        }
+
+        async fn call_bytes_with_cleanup(
+            callee: CanisterId,
+            method: &str,
+            args: &[u8],
+        ) -> Result<Vec<u8>, (i32, String)> {
+            let (call_arguments, return_value) = EXPECTED_CALLS.with(|expected_calls| {
+                expected_calls.borrow_mut().pop_front().unwrap_or_else(|| {
+                    panic!("Unexpected call: {:?}", (callee, method, args));
+                })
+            });
+
+            let args: &[u8] = &call_arguments.2;
+            let call_arguments = (call_arguments.0, call_arguments.1, args);
+            assert_eq!((callee, method, args), call_arguments,);
+
+            return_value
+        }
+
+        // Spawns a future.
+        fn spawn_future<F: 'static + Future<Output = ()>>(_future: F) {
+            unimplemented!()
+        }
+    }
+
+    // Step 2: Call the code under test.
+
+    let total_e8s: u64 = initial_supply_e8s::<MockRuntime>(
+        LEDGER_CANISTER_ID.clone(),
+        InitialSupplyOptions {
+            batch_size: BATCH_SIZE,
+            ..Default::default()
+        },
+    )
+    .await
+    .unwrap();
+
+    // Step 3: Inspect results.
+
+    assert_eq!(total_e8s, 654_321);
+
+    EXPECTED_CALLS.with(|expected_calls| {
+        assert_eq!(*expected_calls.borrow(), VecDeque::new());
+    });
+}
