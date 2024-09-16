16b840a65c4d14354ab46ac32e46caacde1c420d
Christian Stieger <christian.stieger@dfinity.org>
2024-09-16 20:52:10 +0000

    .

--- rs/state_manager/src/manifest/split/tests.rs
+++ rs/state_manager/src/manifest/split/tests.rs
@@ -1,11 +1,10 @@
 use assert_matches::assert_matches;
-//use ic_base_types::{CanisterId, SnapshotId};
-use ic_base_types::CanisterId;
-//use ic_registry_routing_table::{CanisterIdRange, CanisterIdRanges};
+use ic_base_types::{CanisterId, SnapshotId};
+use ic_registry_routing_table::{CanisterIdRange, CanisterIdRanges};
 use ic_state_layout::{CheckpointLayout, ReadOnly};
 use ic_test_utilities_types::ids::{SUBNET_0, SUBNET_1};
 use ic_types::{state_sync::CURRENT_STATE_SYNC_VERSION, Height};
-
+//use pretty_assertions::assert_eq;
 use super::*;
 
 /// Expected hash of a zero length file.
@@ -203,7 +202,7 @@ fn split_manifest_split_marker_last() {
         )
     );
 }
-/*
+
 #[test]
 fn split_manifest_3_canisters() {
     const CANISTER_1: CanisterId = CanisterId::from_u64(1);
@@ -308,7 +307,7 @@ fn split_manifest_3_canisters() {
         )
     );
 }
-*/
+
 /// Returns a `FileInfo` for a zero length file with the given relative path.
 fn empty_file_info(path: &str) -> FileInfo {
     FileInfo {
@@ -317,7 +316,7 @@ fn empty_file_info(path: &str) -> FileInfo {
         hash: EMPTY_FILE_HASH,
     }
 }
-/*
+
 /// Generates a valid `(FileInfo, ChunkInfo)` pair for a non-empty file with
 /// the given relative path..
 fn non_empty_file_and_chunk_infos(path: &str) -> (FileInfo, ChunkInfo) {
@@ -338,7 +337,7 @@ fn non_empty_file_and_chunk_infos(path: &str) -> (FileInfo, ChunkInfo) {
         },
     )
 }
-*/
+
 /// Returns the relative path to the `canister.pbuf` for the given canister.
 fn canister_pbuf_path(canister_id: CanisterId) -> String {
     // Empty root so that all paths are relative like in the manifest.
@@ -354,7 +353,7 @@ fn canister_pbuf_path(canister_id: CanisterId) -> String {
         .unwrap()
         .to_string()
 }
-/*
+
 /// Returns the relative path to the `snapshot.pbuf` for the given snapshot.
 fn snapshot_pbuf_path(snapshot_id: SnapshotId) -> String {
     // Empty root so that all paths are relative like in the manifest.
@@ -370,7 +369,7 @@ fn snapshot_pbuf_path(snapshot_id: SnapshotId) -> String {
         .unwrap()
         .to_string()
 }
-*/
+
 /// Returns the expected `FileInfo` and `ChunkInfo` for the split marker from
 /// `SUBNET_0`.
 fn expected_split_marker() -> (FileInfo, ChunkInfo) {
@@ -394,28 +393,27 @@ fn expected_split_marker() -> (FileInfo, ChunkInfo) {
         },
     )
 }
-/*
+
 /// Returns the expected `FileInfo` and `ChunkInfo` for the system metadata of
 /// `SUBNET_1`.
 fn expected_subnet_1_system_metadata() -> (FileInfo, ChunkInfo) {
     (
         FileInfo {
             relative_path: PathBuf::from(SYSTEM_METADATA_FILE),
-            size_bytes: 63,
+            size_bytes: 65,
             hash: [
-                122, 238, 38, 137, 170, 83, 240, 133, 62, 48, 18, 112, 233, 148, 191, 115, 239,
-                115, 135, 234, 25, 157, 24, 45, 161, 179, 219, 112, 242, 95, 10, 217,
+                135, 141, 106, 99, 2, 246, 244, 239, 255, 254, 121, 198, 217, 0, 168, 217, 254,
+                105, 171, 56, 159, 169, 160, 159, 87, 16, 106, 9, 168, 21, 6, 246,
             ],
         },
         ChunkInfo {
             file_index: 13,
-            size_bytes: 63,
+            size_bytes: 65,
             offset: 0,
             hash: [
-                167, 162, 133, 251, 148, 255, 80, 204, 229, 63, 140, 219, 43, 228, 234, 250, 69,
-                49, 7, 200, 173, 216, 136, 186, 183, 255, 101, 117, 229, 161, 238, 156,
+                48, 206, 112, 95, 239, 131, 62, 43, 197, 160, 224, 171, 236, 107, 12, 29, 127, 75,
+                227, 118, 27, 199, 106, 203, 192, 91, 53, 67, 219, 53, 31, 173,
             ],
         },
     )
 }
-*/
