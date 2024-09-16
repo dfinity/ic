fdccf7a5f81b0d49ad311914882eed3ec04a3a72
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-09-16 09:59:13 +0000

    calculate expiry duration

--- rs/execution_environment/src/execution_environment.rs
+++ rs/execution_environment/src/execution_environment.rs
@@ -29,9 +29,7 @@ use ic_error_types::{ErrorCode, RejectCode, UserError};
 use ic_interfaces::execution_environment::{
     ExecutionMode, IngressHistoryWriter, RegistryExecutionSettings, SubnetAvailableMemory,
 };
-use ic_limits::{
-    LOG_CANISTER_OPERATION_CYCLES_THRESHOLD, MAX_INGRESS_TTL, SMALL_APP_SUBNET_MAX_SIZE,
-};
+use ic_limits::{LOG_CANISTER_OPERATION_CYCLES_THRESHOLD, SMALL_APP_SUBNET_MAX_SIZE};
 use ic_logger::{error, info, warn, ReplicaLogger};
 use ic_management_canister_types::{
     CanisterChangeOrigin, CanisterHttpRequestArgs, CanisterIdRecord, CanisterInfoRequest,
@@ -74,6 +72,7 @@ use ic_types::{
     },
     methods::SystemMethod,
     nominal_cycles::NominalCycles,
+    time::expiry_time_from_now,
     CanisterId, Cycles, LongExecutionMode, NumBytes, NumInstructions, SubnetId, Time,
 };
 use ic_types::{messages::MessageId, methods::WasmMethod};
@@ -1627,8 +1626,10 @@ impl ExecutionEnvironment {
                 CanisterCall::Request(request)
             }
             CanisterMessageOrTask::Message(CanisterMessage::Ingress(ingress)) => {
-                let start = ingress.expiry_time.saturating_sub(MAX_INGRESS_TTL);
-                let latency = time.saturating_duration_since(start).as_secs_f64();
+                let now = time;
+                let expiry_duration = expiry_time_from_now().saturating_duration_since(now);
+                let start = ingress.expiry_time.saturating_sub(expiry_duration);
+                let latency = now.saturating_duration_since(start).as_secs_f64();
                 self.metrics.canister_ingress_queue_latency.observe(latency);
                 CanisterCall::Ingress(ingress)
             }
