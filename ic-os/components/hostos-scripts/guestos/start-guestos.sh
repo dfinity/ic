08137fe03c0efa1c57a23fddd79eed968fa426ca
Eero Kelly <eero.kelly@dfinity.org>
2024-09-19 19:05:06 +0000

    Change monitoring strategy for GuestOS VM

--- ic-os/components/hostos-scripts/guestos/start-guestos.sh
+++ ic-os/components/hostos-scripts/guestos/start-guestos.sh
@@ -67,28 +67,10 @@ function start_guestos() {
     fi
 }
 
-function enable_guestos() {
-    if [ "$(virsh list --autostart | grep 'guestos')" ]; then
-        write_log "GuestOS virtual machine is already enabled."
-        write_metric "hostos_guestos_service_enable" \
-            "0" \
-            "GuestOS virtual machine enable state" \
-            "gauge"
-    else
-        virsh autostart guestos
-        write_log "Enabling GuestOS virtual machine."
-        write_metric "hostos_guestos_service_enable" \
-            "1" \
-            "GuestOS virtual machine enable state" \
-            "gauge"
-    fi
-}
-
 function main() {
     # Establish run order
     define_guestos
     start_guestos
-    enable_guestos
 }
 
 main
