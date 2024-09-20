ee5a50001ddf598f152f035b72bee7456a776905
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-09-20 16:10:50 +0200

    fix: start the BTC adapter only by accepting a config (#1584)
    To enable the addition of the BTC adapter to pocket ic

--- rs/bitcoin/adapter/src/main.rs
+++ rs/bitcoin/adapter/src/main.rs
@@ -1,9 +1,7 @@
 use clap::Parser;
-use ic_adapter_metrics_server::start_metrics_grpc;
-use ic_async_utils::{abort_on_panic, incoming_from_nth_systemd_socket, shutdown_signal};
-use ic_btc_adapter::{
-    cli::Cli, config::IncomingSource, start_grpc_server_and_router, AdapterState,
-};
+use ic_async_utils::abort_on_panic;
+use ic_async_utils::shutdown_signal;
+use ic_btc_adapter::{cli::Cli, start_server};
 use ic_logger::{info, new_replica_logger_from_config};
 use ic_metrics::MetricsRegistry;
 use serde_json::to_string_pretty;
@@ -22,29 +20,19 @@ pub async fn main() {
             panic!("An error occurred while getting the config: {}", err);
         }
     };
-    let (logger, _async_log_guard) = new_replica_logger_from_config(&config.logger);
+    let (log, _async_log_guard) = new_replica_logger_from_config(&config.logger);
 
     info!(
-        logger,
+        log,
         "Starting the adapter with config: {}",
         to_string_pretty(&config).unwrap()
     );
-
     let metrics_registry = MetricsRegistry::global();
-
-    // Metrics server should only be started if we are managed by systemd and receive the
-    // metrics socket as FD(4).
-    // SAFETY: The process is managed by systemd and is configured to start with at metrics socket.
-    // Additionally this function is only called once here.
-    // Systemd Socket config: ic-https-outcalls-adapter.socket
-    // Systemd Service config: ic-https-outcalls-adapter.service
-    if config.incoming_source == IncomingSource::Systemd {
-        let stream = unsafe { incoming_from_nth_systemd_socket(2) };
-        start_metrics_grpc(metrics_registry.clone(), logger.clone(), stream);
-    }
-
-    let adapter_state = AdapterState::new(config.idle_seconds);
-    start_grpc_server_and_router(&config, &metrics_registry, logger.clone(), adapter_state);
-
-    shutdown_signal(logger.inner_logger.root.clone()).await;
+    start_server(
+        &log,
+        &metrics_registry,
+        &tokio::runtime::Handle::current(),
+        config,
+    );
+    shutdown_signal(log.inner_logger.root.clone()).await;
 }
