1956b968c600e5eaffa079e6080dd57050cfe1fa
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-09-23 12:18:19 +0000

    .

--- rs/https_outcalls/adapter/src/lib.rs
+++ rs/https_outcalls/adapter/src/lib.rs
@@ -16,8 +16,9 @@ pub use config::{Config, IncomingSource};
 pub use rpc_server::CanisterHttp;
 
 use futures::{Future, Stream};
+use ic_async_utils::{incoming_from_first_systemd_socket, incoming_from_path};
 use ic_https_outcalls_service::https_outcalls_service_server::HttpsOutcallsServiceServer;
-use ic_logger::ReplicaLogger;
+use ic_logger::{error, ReplicaLogger};
 use ic_metrics::MetricsRegistry;
 use std::time::Duration;
 use tokio::io::{AsyncRead, AsyncWrite};
@@ -26,6 +27,35 @@ use tonic::transport::{
     Server,
 };
 use tower::layer::util::Identity;
+
+pub fn start_server(
+    log: &ReplicaLogger,
+    metrics_registry: &MetricsRegistry,
+    rt_handle: &tokio::runtime::Handle,
+    config: config::Config,
+) {
+    let log = log.clone();
+    let metrics_registry = metrics_registry.clone();
+    rt_handle.spawn(async move {
+        // Create server with https enforcement.
+        let server = AdapterServer::new(config.clone(), log.clone(), &metrics_registry);
+        match config.incoming_source {
+            IncomingSource::Path(uds_path) => server
+                .serve(incoming_from_path(uds_path))
+                .await
+                .map_err(|e| error!(log, "Canister Http adapter crashed: {}", e))
+                .expect("gRPC server crashed"),
+            IncomingSource::Systemd => server
+                // SAFETY: We are manged by systemd that is configured to pass socket as FD(3).
+                // Additionally, this is the only call to connect with the systemd socket and
+                // therefore we are sole owner.
+                .serve(unsafe { incoming_from_first_systemd_socket() })
+                .await
+                .map_err(|e| error!(log, "Canister Http adapter crashed: {}", e))
+                .expect("gRPC server crashed"),
+        };
+    });
+}
 
 /// Start the HttpsOutcallsService server.
 pub struct AdapterServer(Router<Identity>);
