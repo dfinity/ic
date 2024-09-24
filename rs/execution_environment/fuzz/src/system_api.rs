acdb6a2ccb06e24d622231e767a41f9737367449
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-09-24 13:02:23 +0000

    disable inner code

--- rs/execution_environment/fuzz/src/system_api.rs
+++ rs/execution_environment/fuzz/src/system_api.rs
@@ -13,33 +13,27 @@ use ic_state_machine_tests::{
 use ic_types::{CanisterId, Cycles, NumInstructions};
 use wasm_fuzzers::ic_wasm::ICWasmModule;
 
-// Change limits in order not to duplicate prod values.
-const B: u64 = 1_000_000_000;
-const MAX_INSTRUCTIONS_PER_ROUND: NumInstructions = NumInstructions::new(5 * B);
-const MAX_INSTRUCTIONS_PER_MESSAGE: NumInstructions = NumInstructions::new(20 * B);
-const MAX_INSTRUCTIONS_PER_SLICE: NumInstructions = NumInstructions::new(B);
-
 #[inline(always)]
-pub fn run_fuzzer(module: ICWasmModule) {
-    let wasm = module.module.to_bytes();
-    let (_env, _canister_id) =
-        setup_and_install_wasm(CanisterSettingsArgsBuilder::new().build(), wasm);
+pub fn run_fuzzer(_module: ICWasmModule) {
+    // let wasm = module.module.to_bytes();
+    // let (_env, _canister_id) =
+    //     setup_and_install_wasm(CanisterSettingsArgsBuilder::new().build(), wasm);
 
     //let _ = env.execute_ingress(canister_id, "update", vec![]);
 }
 
-fn setup_and_install_wasm(
-    settings: CanisterSettingsArgs,
-    wasm: Vec<u8>,
-) -> (StateMachine, CanisterId) {
-    let env = StateMachineBuilder::new()
-        .with_subnet_type(SubnetType::Application)
-        .with_checkpoints_enabled(false)
-        .build();
-    let canister_id =
-        env.create_canister_with_cycles(None, Cycles::from(100_000_000_000_u128), Some(settings));
-    env.install_wasm_in_mode(canister_id, CanisterInstallMode::Install, wasm, vec![])
-        .unwrap();
+// fn setup_and_install_wasm(
+//     settings: CanisterSettingsArgs,
+//     wasm: Vec<u8>,
+// ) -> (StateMachine, CanisterId) {
+//     let env = StateMachineBuilder::new()
+//         .with_subnet_type(SubnetType::Application)
+//         .with_checkpoints_enabled(false)
+//         .build();
+//     let canister_id =
+//         env.create_canister_with_cycles(None, Cycles::from(100_000_000_000_u128), Some(settings));
+//     env.install_wasm_in_mode(canister_id, CanisterInstallMode::Install, wasm, vec![])
+//         .unwrap();
 
-    (env, canister_id)
-}
+//     (env, canister_id)
+// }
