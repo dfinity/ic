c94ba36496fbeed00c6537bd68e8af36532c5c7b
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-09-25 16:15:17 +0000

    cleanup

--- rs/execution_environment/fuzz/fuzz_targets/execute_system_api_call.rs
+++ rs/execution_environment/fuzz/fuzz_targets/execute_system_api_call.rs
@@ -1,7 +1,10 @@
 #![no_main]
+use ic_management_canister_types::{CanisterInstallMode, CanisterSettingsArgsBuilder};
+use ic_registry_subnet_type::SubnetType;
+use ic_state_machine_tests::StateMachineBuilder;
+use ic_types::Cycles;
 use libfuzzer_sys::fuzz_target;
 use wasm_fuzzers::ic_wasm::ICWasmModule;
-use execution_fuzzers::system_api::run_fuzzer;
 
 // This fuzz tries to execute system API call.
 //
@@ -11,5 +14,19 @@ use execution_fuzzers::system_api::run_fuzzer;
 // bazel run --config=fuzzing //rs/execution_environment/fuzz:execute_system_api_call
 
 fuzz_target!(|module: ICWasmModule| {
-    run_fuzzer(module);
+    let wasm = module.module.to_bytes();
+    let env = StateMachineBuilder::new()
+        .with_subnet_type(SubnetType::Application)
+        .no_dts()
+        .with_checkpoints_enabled(false)
+        .build();
+    let canister_id = env.create_canister_with_cycles(
+        None,
+        Cycles::from(100_000_000_000_u128),
+        Some(CanisterSettingsArgsBuilder::new().build()),
+    );
+    env.install_wasm_in_mode(canister_id, CanisterInstallMode::Install, wasm, vec![])
+        .unwrap();
+
+    let _ = env.execute_ingress(canister_id, "update", vec![]);
 });
--- rs/execution_environment/fuzz/src/lib.rs
+++
@@ -1,1 +0,0 @@
-pub mod system_api;
