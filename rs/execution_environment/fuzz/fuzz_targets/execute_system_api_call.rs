83fcec7611bc636f09395abafd7a17be065aa447
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-09-26 12:47:56 +0000

    optimize setup_env call

--- rs/execution_environment/fuzz/fuzz_targets/execute_system_api_call.rs
+++ rs/execution_environment/fuzz/fuzz_targets/execute_system_api_call.rs
@@ -1,20 +1,33 @@
 #![no_main]
 use ic_management_canister_types::{CanisterInstallMode, CanisterSettingsArgsBuilder};
 use ic_registry_subnet_type::SubnetType;
-use ic_state_machine_tests::StateMachineBuilder;
+use ic_state_machine_tests::{CanisterId, StateMachine, StateMachineBuilder};
 use ic_types::Cycles;
 use libfuzzer_sys::fuzz_target;
+use std::cell::RefCell;
 use wasm_fuzzers::ic_wasm::ICWasmModule;
 
-// This fuzz tries to execute system API call.
-//
-// The fuzz test is only compiled but not executed by CI.
-//
-// To execute the fuzzer run
-// bazel run --config=fuzzing //rs/execution_environment/fuzz:execute_system_api_call
+thread_local! {
+    static ENV: RefCell<(StateMachine, CanisterId)> = RefCell::new(setup_env());
+}
 
-fuzz_target!(|module: ICWasmModule| {
-    let wasm = module.module.to_bytes();
+fn with_env<F, R>(f: F) -> R
+where
+    F: FnOnce(&StateMachine, &CanisterId) -> R,
+{
+    ENV.with(|env| {
+        let env_ref = env.borrow();
+        f(&env_ref.0, &env_ref.1) // Pass references to the closure
+    })
+}
+
+const HELLO_WORLD_WAT: &str = r#"
+(module
+    (func $hi)
+    (export "canister_query hi" (func $hi))
+)"#;
+
+fn setup_env() -> (StateMachine, CanisterId) {
     let env = StateMachineBuilder::new()
         .with_subnet_type(SubnetType::Application)
         .no_dts() // Disable DTS to avoid sandbox_launcher binary dependency (which does not work well with fuzz tests).
@@ -25,11 +38,29 @@ fuzz_target!(|module: ICWasmModule| {
         Cycles::from(100_000_000_000_u128),
         Some(CanisterSettingsArgsBuilder::new().build()),
     );
+    let wasm = wat::parse_str(HELLO_WORLD_WAT).unwrap();
     env.install_wasm_in_mode(canister_id, CanisterInstallMode::Install, wasm, vec![])
         .unwrap();
+    (env, canister_id)
+}
 
-    // For determinism, all methods are executed
-    for wasm_method in module.exported_functions.iter() {
-        let _ = env.execute_ingress(canister_id, wasm_method.name(), vec![]);
-    }
+// This fuzz tries to execute system API call.
+//
+// The fuzz test is only compiled but not executed by CI.
+//
+// To execute the fuzzer run
+// bazel run --config=fuzzing //rs/execution_environment/fuzz:execute_system_api_call
+
+fuzz_target!(|module: ICWasmModule| {
+    let wasm = module.module.to_bytes();
+
+    with_env(|env, canister_id| {
+        env.install_wasm_in_mode(*canister_id, CanisterInstallMode::Reinstall, wasm, vec![])
+            .unwrap();
+
+        // For determinism, all methods are executed
+        for wasm_method in module.exported_functions.iter() {
+            let _ = env.execute_ingress(*canister_id, wasm_method.name(), vec![]);
+        }
+    });
 });
