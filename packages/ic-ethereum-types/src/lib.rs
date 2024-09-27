d7c6f18032e0019222a671df12ab9e718bd01577
gregorydemay <gregory.demay@dfinity.org>
2024-09-27 14:54:53 +0200

    XC-200: added missing Rust docs

--- packages/ic-ethereum-types/src/lib.rs
+++ packages/ic-ethereum-types/src/lib.rs
@@ -1,3 +1,10 @@
+//! Ethereum types used by canisters on the Internet Computer.
+
+#![forbid(unsafe_code)]
+#![warn(rust_2018_idioms)]
+#![forbid(missing_docs)]
+#![warn(future_incompatible)]
+
 mod address;
 pub mod serde_data;
 
