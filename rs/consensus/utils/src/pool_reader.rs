41058117e016707c7e709fc3aa64d879d157fa94
Leo Eichhorn <leo.eichhorn@dfinity.org>
2024-09-27 12:27:58 +0000

    fix

--- rs/consensus/utils/src/pool_reader.rs
+++ rs/consensus/utils/src/pool_reader.rs
@@ -586,6 +586,7 @@ where
 pub mod test {
     use super::*;
     use ic_consensus_mocks::{dependencies, dependencies_with_subnet_params, Dependencies};
+    use ic_interfaces_registry::RegistryClient;
     use ic_test_utilities_registry::{add_subnet_record, SubnetRecordBuilder};
     use ic_test_utilities_types::ids::{node_test_id, subnet_test_id};
 
