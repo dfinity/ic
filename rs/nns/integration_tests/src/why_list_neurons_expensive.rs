f7e13006dbda9a369624b0fc5fda528a321635df
Daniel Wong <daniel.wong@dfinity.org>
2024-09-30 22:02:02 +0200

    Getting flame graphs for the first time. This required a few orthogonal changes:
    1. start_address is a byte offset, not page.
    2. Read "name" metadata from ic-wasm instrumented WASM. This is later used to visualize.
    3. Visualize. Output is at list_neurons.svg. This involved copying fn render_profiling from the ic-repl repo.
    
    (I will upload an example list_neurons.svg to GitHub shortly.)

--- rs/nns/integration_tests/src/why_list_neurons_expensive.rs
+++ rs/nns/integration_tests/src/why_list_neurons_expensive.rs
@@ -1,17 +1,48 @@
+use anyhow::anyhow;
 use candid::{Decode, Encode};
 use flate2::read::GzDecoder;
 use ic_base_types::PrincipalId;
+use ic_cdk::api::stable::WASM_PAGE_SIZE_IN_BYTES;
+use ic_nervous_system_string::clamp_debug_len;
 use ic_nns_constants::GOVERNANCE_CANISTER_ID;
 use ic_nns_governance_api::pb::v1::ListNeurons;
 use ic_nns_test_utils::state_test_helpers::{get_profiling, list_neurons, unwrap_wasm_result};
 use ic_nns_test_utils_golden_nns_state::new_state_machine_with_golden_nns_state_or_panic;
-use std::io::Read; // For flate2.
+use std::{
+    collections::BTreeMap,
+    io::Read, // For flate2.
+    path::PathBuf,
+};
+// TODO: Figure out how to implement read_custom_sections using the walrus
+// library so that we are not using two WASM libraries.
+use wasmparser::{Parser, Payload};
 
 fn decompress_gz(buffer: &[u8]) -> Vec<u8> {
     let mut result = vec![];
     let mut decoder = GzDecoder::new(buffer);
     decoder.read_to_end(&mut result).unwrap();
     result
+}
+
+fn read_custom_section(wasm_bytes: &[u8], query_name: &str) -> Vec<u8> {
+    let parser = Parser::new(0);
+    let mut custom_section_data = None;
+
+    for payload in parser.parse_all(wasm_bytes) {
+        match payload.unwrap() {
+            Payload::CustomSection(custom_section) => {
+                if custom_section.name() == query_name {
+                    custom_section_data = Some(custom_section.data().to_vec());
+                    // This assumes that there are no more custom sections with
+                    // the same name.
+                    break;
+                }
+            }
+            _ => continue, // Ignore other payloads
+        }
+    }
+
+    custom_section_data.unwrap()
 }
 
 #[test]
@@ -37,7 +68,7 @@ fn test_why_list_neurons_expensive() {
             vec![], // args
         )
         .unwrap();
-    let (start_address, page_limit) = Decode!(
+    let (start_page, page_limit) = Decode!(
         &unwrap_wasm_result(state_machine.query(
             GOVERNANCE_CANISTER_ID,
             "where_ic_wasm_instrument_memory",
@@ -47,9 +78,13 @@ fn test_why_list_neurons_expensive() {
         u64
     )
     .unwrap();
+    let start_address = start_page * WASM_PAGE_SIZE_IN_BYTES as u64;
     println!("");
     println!("Result from ic_wasm_instrument_memory:");
-    println!("  start_address = {}", start_address);
+    println!(
+        "  start_page = {} (start_address = {})",
+        start_page, start_address
+    );
     println!("  page_limit = {}", page_limit);
     println!("");
 
@@ -57,6 +92,8 @@ fn test_why_list_neurons_expensive() {
     let page_limit = page_limit / 2;
 
     // Step 1.2.2: Enable ic-wasm profiling.
+
+    // Step 1.2.2.1: Modify governance WASM.
     let mut instrumented_governance_wasm =
         walrus::Module::from_buffer(&decompress_gz(&governance_wasm_gz))
             .expect("walrus cannot cope with our WASM.");
@@ -70,7 +107,25 @@ fn test_why_list_neurons_expensive() {
     )
     .unwrap();
     let instrumented_governance_wasm = instrumented_governance_wasm.emit_wasm();
-    println!("\nInstalling instrumented governance WASM...\n");
+
+    // Step 1.2.2.2: Read some metadata from modified governance WASM that will later be used
+    // visualize. This is based on
+    // https://sourcegraph.com/github.com/dfinity/ic-repl@746bea25ddd4cc98709f6b9eaa283f32a21ac30d/-/blob/src/helper.rs?L504
+    let name_custom_section_payload = Decode!(
+        &read_custom_section(&instrumented_governance_wasm, "icp:public name"),
+        BTreeMap<u16, String>
+    )
+    .unwrap();
+    println!("");
+    println!(
+        "custom section 'name' payload:\n{:#?}",
+        name_custom_section_payload
+    );
+
+    // Step 1.2.2.3: Install modified governance WASM.
+    println!("");
+    println!("Installing governance WITH ic-wasm profiling ENABLED...");
+    println!("");
     state_machine
         .upgrade_canister(
             GOVERNANCE_CANISTER_ID,
@@ -78,9 +133,12 @@ fn test_why_list_neurons_expensive() {
             vec![], // args
         )
         .unwrap();
-    println!("\nDone installing instrumented governance WASM. Ready for fine-grained performance measurement 👍\n");
+    println!("");
+    println!("Done installing governance WITH ic-wasm profiling ENABLED.");
+    println!("Ready for fine-grained performance measurement 👍");
+    println!("");
 
-    // Step 2: Run the code under test.
+    // Step 2: Run the code under test (while profiling is enabled).
 
     let caller = PrincipalId::new_user_test_id(42); // DO NOT MERGE
     list_neurons(
@@ -96,8 +154,100 @@ fn test_why_list_neurons_expensive() {
 
     // Step 3: Inspect results. In particular, generate flame graph.
 
+    // Step 3.1: Fetch measurement.
     let profiling = get_profiling(&state_machine, GOVERNANCE_CANISTER_ID);
-    println!("\n\nprofiling:\n{:#?}", profiling);
 
-    panic!("\n\nHELLO WHY list_neurons EXPENSIVE\n\n");
+    // Step 3.2: Visualize. Output is at list_neurons.svg.
+    let lol_idk = render_profiling(
+        profiling,
+        &name_custom_section_payload,
+        "list_neurons", // title
+        PathBuf::from("list_neurons.svg"),
+    )
+    .unwrap();
+}
+
+enum CostValue {
+    Complete(u64),
+    StartCost(u64),
+}
+
+// Copied from https://github.com/dfinity/ic-repl/blob/746bea25ddd4cc98709f6b9eaa283f32a21ac30d/src/profiling.rs#L85C1-L162C2
+fn render_profiling(
+    input: Vec<(i32, i64)>,
+    names: &BTreeMap<u16, String>,
+    title: &str,
+    filename: PathBuf,
+) -> anyhow::Result<CostValue> {
+    use inferno::flamegraph::{from_reader, Options};
+    let mut stack = Vec::new();
+    let mut prefix = Vec::new();
+    let mut result = Vec::new();
+    let mut total = 0;
+    let mut prev = None;
+    let start_cost = input.first().map(|(_, count)| *count);
+    for (id, count) in input.into_iter() {
+        if id >= 0 {
+            stack.push((id, count, 0));
+            let name = match names.get(&(id as u16)) {
+                Some(name) => name.clone(),
+                None => "func_".to_string() + &id.to_string(),
+            };
+            prefix.push(name);
+        } else {
+            match stack.pop() {
+                None => return Err(anyhow!("pop empty stack")),
+                Some((start_id, start, children)) => {
+                    if start_id != -id {
+                        return Err(anyhow!("func id mismatch"));
+                    }
+                    let cost = count - start;
+                    let frame = prefix.join(";");
+                    prefix.pop().unwrap();
+                    if let Some((parent, parent_cost, children_cost)) = stack.pop() {
+                        stack.push((parent, parent_cost, children_cost + cost));
+                    } else {
+                        total += cost as u64;
+                    }
+                    match prev {
+                        Some(prev) if prev == frame => {
+                            // Add an empty spacer to avoid collapsing adjacent same-named calls
+                            // See https://github.com/jonhoo/inferno/issues/185#issuecomment-671393504
+                            result.push(format!("{};spacer 0", prefix.join(";")));
+                        }
+                        _ => (),
+                    }
+                    result.push(format!("{} {}", frame, cost - children));
+                    prev = Some(frame);
+                }
+            }
+        }
+    }
+    let cost = if !stack.is_empty() {
+        eprintln!("A trap occured or trace is too large");
+        CostValue::StartCost(start_cost.unwrap() as u64)
+    } else {
+        CostValue::Complete(total)
+    };
+    //println!("Cost: {} Wasm instructions", total);
+    let mut opt = Options::default();
+    opt.count_name = "instructions".to_string();
+    let title = if matches!(cost, CostValue::StartCost(_)) {
+        title.to_string() + " (incomplete)"
+    } else {
+        title.to_string()
+    };
+    opt.title = title;
+    opt.image_width = Some(1024);
+    opt.flame_chart = true;
+    opt.no_sort = true;
+    // Reserve result order to make flamegraph from left to right.
+    // See https://github.com/jonhoo/inferno/issues/236
+    result.reverse();
+    let logs = result.join("\n");
+    let reader = std::io::Cursor::new(logs);
+    println!("Flamegraph written to {}", filename.display());
+    let mut writer = std::fs::File::create(&filename)?;
+    from_reader(&mut opt, reader, &mut writer)?;
+    Ok(cost)
 }
