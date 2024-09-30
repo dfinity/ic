6d2e312423e2f21c8da178f69936812832fb05b9
Carly Gundy <carlene.gundy@dfinity.org>
2024-09-30 10:57:29 +0200

    fix(IDX): update diff logic to work outside of PRs

--- ci/bazel-scripts/diff.sh
+++ ci/bazel-scripts/diff.sh
@@ -13,7 +13,8 @@ set -euo pipefail
 set -x
 cd "$(git rev-parse --show-toplevel)"
 
-MERGE_BASE="${MERGE_BASE_SHA:-HEAD}"
+TARGET_BRANCH="${CI_PULL_REQUEST_TARGET_BRANCH_NAME:-$DEFAULT_BRANCH}"
+MERGE_BASE=$(git merge-base HEAD "$TARGET_BRANCH")
 COMMIT_RANGE=${COMMIT_RANGE:-$MERGE_BASE".."}
 DIFF_FILES=$(git diff --name-only "${COMMIT_RANGE}")
 
