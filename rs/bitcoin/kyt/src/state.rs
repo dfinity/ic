a5f354d038dfcc5b33bed8052369e5028703df0c
Paul H. Liu <paul@dfinity.org>
2024-09-30 05:59:52 -0700

    Minor

--- rs/bitcoin/kyt/src/state.rs
+++ rs/bitcoin/kyt/src/state.rs
@@ -55,8 +55,8 @@ const MAX_CONCURRENT: u32 = 50;
 // Default expiry of cached fetch transaction entries is 7 days (in nano seconds).
 const DEFAULT_FETCH_TX_EXPIRY: u64 = 7 * 24 * 3600 * 1000000;
 
-// Max number entries in the cache is set to 10_000. Since the average transaction size
-// is about 400 bytes, the estimated storage space for the cache to in the order of 10s of MB.
+// Max number of entries in the cache is set to 10_000. Since the average transaction size
+// is about 400 bytes, the estimated memory usage of the cache is in the order of 10s of MBs.
 const MAX_FETCH_TX_ENTRIES: usize = 10_000;
 
 // The internal KYT state includes:
@@ -72,10 +72,10 @@ thread_local! {
 }
 
 // Time in nanoseconds since the epoch (1970-01-01).
-pub type Timestamp = u64;
+type Timestamp = u64;
 
 // Time duration in nanoseconds.
-pub type Duration = u64;
+type Duration = u64;
 
 struct FetchTxCache<T> {
     expiry: Duration,
@@ -98,14 +98,14 @@ impl<T> FetchTxCache<T> {
         self.status.get(&txid)
     }
 
-    /// Set the status of a txid with a timestamp. If the txid does not
-    /// already have a status, a new status entry is created and associated
-    /// with the given the timestamp.
-    ///
-    /// When called with non-decreasing timestamps, this function will remove
-    /// at most one past (i.e. the oldest) entry based on the `expiry` and
-    /// `max_entries` settings, when either condition satisfies.
-    pub fn set_status_with(&mut self, txid: Txid, status: T, now: Timestamp) {
+    // Set the status of a txid with a timestamp. If the txid does not
+    // already have a status, a new status entry is created and associated
+    // with the given the timestamp.
+    //
+    // When called with non-decreasing timestamps, this function will remove
+    // at most one past (i.e. the oldest) entry based on the `expiry` and
+    // `max_entries` settings, when either condition satisfies.
+    fn set_status_with(&mut self, txid: Txid, status: T, now: Timestamp) {
         if self.status.insert(txid, status).is_none() {
             // This is a new entry, record its created time.
             let _ = self.created.push_back((txid, now));
@@ -119,7 +119,7 @@ impl<T> FetchTxCache<T> {
         }
     }
 
-    pub fn clear_status(&mut self, txid: Txid) {
+    fn clear_status(&mut self, txid: Txid) {
         let _ = self.status.remove(&txid);
         self.created.retain(|(id, _)| *id != txid);
     }
@@ -129,15 +129,15 @@ pub fn get_fetch_status(txid: Txid) -> Option<FetchTxStatus> {
     FETCH_TX_CACHE.with(|cache| cache.borrow().get_status(txid).cloned())
 }
 
-#[cfg(not(test))]
-fn time() -> u64 {
-    ic_cdk::api::time()
-}
-
-// Mock the time syscall when running tests.
+// Mock the time when running tests.
 #[cfg(test)]
 fn time() -> u64 {
     0
+}
+
+#[cfg(not(test))]
+fn time() -> u64 {
+    ic_cdk::api::time()
 }
 
 pub fn set_fetch_status(txid: Txid, status: FetchTxStatus) {
