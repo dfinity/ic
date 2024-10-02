fdb0e722c4efd2dc9c9855c982dd91923f18f5ae
Andrew Battat <andrew.battat@dfinity.org>
2024-10-02 14:38:00 +0000

    Remove remuneration.conf from boundary-node bootstrap

--- ic-os/components/boundary-guestos/opt/ic/bin/bootstrap-ic-node.sh
+++ ic-os/components/boundary-guestos/opt/ic/bin/bootstrap-ic-node.sh
@@ -68,7 +68,6 @@ function process_bootstrap() {
         ic_boundary.conf
         canister-ratelimit.yml
         network.conf
-        remuneration.conf
         nns_public_key.pem
         nns.conf
         pre_isolation_canisters.txt
