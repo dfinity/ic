04f661ea24f1acd4d9b2ec2fb245e59493c773da
Andre Popovitch <andre@popovit.ch>
2024-10-07 05:40:23 -0500

    feat(sns): Add periodic task for caching the upgrade steps (#1788)
    This PR implements part of periodic task type A, which fetches the
    remaining upgrade steps from SNS-W.
    
    This PR does not implement the part of periodic task type A that
    refreshes deployed_version when an upgrade triggered by a change to
    target_version is in progress.
    
    This PR also implements a SnsGov.get_upgrade_journal endpoint, which
    will be used in the future to get all the info needed to audit SNS
    Upgrades, but for now will just be used to see the cached_upgrade_steps
    
    Remaining work: Add a pocketic test
    
    | [Next PR](https://github.com/dfinity/ic/pull/1854) >
    
    ---------
    
    Co-authored-by: Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>

--- rs/sns/governance/canister/canister.rs
+++ rs/sns/governance/canister/canister.rs
@@ -47,9 +47,10 @@ use ic_sns_governance::{
         GetModeResponse, GetNeuron, GetNeuronResponse, GetProposal, GetProposalResponse,
         GetRunningSnsVersionRequest, GetRunningSnsVersionResponse,
         GetSnsInitializationParametersRequest, GetSnsInitializationParametersResponse,
-        Governance as GovernanceProto, ListNervousSystemFunctionsResponse, ListNeurons,
-        ListNeuronsResponse, ListProposals, ListProposalsResponse, ManageNeuron,
-        ManageNeuronResponse, NervousSystemParameters, RewardEvent, SetMode, SetModeResponse,
+        GetUpgradeJournalRequest, GetUpgradeJournalResponse, Governance as GovernanceProto,
+        ListNervousSystemFunctionsResponse, ListNeurons, ListNeuronsResponse, ListProposals,
+        ListProposalsResponse, ManageNeuron, ManageNeuronResponse, NervousSystemParameters,
+        RewardEvent, SetMode, SetModeResponse,
     },
     types::{Environment, HeapGrowthPotential},
 };
@@ -724,6 +725,17 @@ fn add_maturity() {
 #[candid_method(update, rename = "add_maturity")]
 fn add_maturity_(request: AddMaturityRequest) -> AddMaturityResponse {
     governance_mut().add_maturity(request)
+}
+
+#[export_name = "canister_query get_upgrade_journal"]
+fn get_upgrade_journal() {
+    over(candid_one, get_upgrade_journal_)
+}
+
+#[candid_method(query, rename = "get_upgrade_journal")]
+fn get_upgrade_journal_(arg: GetUpgradeJournalRequest) -> GetUpgradeJournalResponse {
+    let GetUpgradeJournalRequest {} = arg;
+    governance().get_upgrade_journal()
 }
 
 /// Mints tokens for testing
