f9d0a42438e65fc0aa22181b46bcee618bf6bd47
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2024-10-07 09:39:41 +0000

    save

--- rs/nervous_system/integration_tests/tests/advance_target_version.rs
+++ rs/nervous_system/integration_tests/tests/advance_target_version.rs
@@ -50,15 +50,7 @@ fn test_get_upgrade_journal() {
 
     // Deploy an SNS instance via proposal.
     let sns = {
-        let create_service_nervous_system = CreateServiceNervousSystemBuilder::default()
-            .with_governance_parameters_neuron_minimum_dissolve_delay_to_vote(ONE_MONTH_SECONDS * 6)
-            .with_one_developer_neuron(
-                PrincipalId::new_user_test_id(830947),
-                ONE_MONTH_SECONDS * 6,
-                756575,
-                0,
-            )
-            .build();
+        let create_service_nervous_system = CreateServiceNervousSystemBuilder::default().build();
         let swap_parameters = create_service_nervous_system
             .swap_parameters
             .clone()
@@ -97,57 +89,21 @@ fn test_get_upgrade_journal() {
 
     // State B: after the first periodic task's completion. No changes expected yet.
     {
-        let sns_pb::GetUpgradeJournalResponse { upgrade_steps, .. } =
+        let sns_pb::GetUpgradeJournalResponse {
+            upgrade_steps,
+            response_timestamp_seconds,
+        } =
             sns::governance::get_upgrade_journal(&pocket_ic, sns.governance.canister_id);
         let upgrade_steps = upgrade_steps
             .expect("upgrade_steps should be Some")
             .versions;
         assert_eq!(upgrade_steps, vec![initial_sns_version.clone()]);
-    }
 
-    // Publish a new SNS version.
-    let (new_sns_version_1, new_sns_version_2) = {
-        let (_, original_ledger_wasm) = deployed_sns_starting_info
-            .get(&SnsCanisterType::Ledger)
-            .unwrap();
-
-        let new_sns_version_1 = {
-            let ledger_wasm = create_modified_sns_wasm(original_ledger_wasm, Some(1));
-            add_wasm_via_nns_proposal(&pocket_ic, ledger_wasm.clone()).unwrap();
-            let ledger_wasm_hash = ledger_wasm.sha256_hash().to_vec();
-            sns_pb::governance::Version {
-                ledger_wasm_hash,
-                ..initial_sns_version.clone()
-            }
-        };
-
-        let new_sns_version_2 = {
-            let ledger_wasm = create_modified_sns_wasm(original_ledger_wasm, Some(2));
-            add_wasm_via_nns_proposal(&pocket_ic, ledger_wasm.clone()).unwrap();
-            let ledger_wasm_hash = ledger_wasm.sha256_hash().to_vec();
-            sns_pb::governance::Version {
-                ledger_wasm_hash,
-                ..new_sns_version_1.clone()
-            }
-        };
-
-        let sns_version = nns::sns_wasm::get_lastest_sns_version(&pocket_ic);
-        assert_ne!(sns_version, initial_sns_version);
-
-        (new_sns_version_1, new_sns_version_2)
-    };
-
-    wait_for_next_periodic_task(UPGRADE_STEPS_INTERVAL_REFRESH_BACKOFF_SECONDS);
-
-    // State C: after the second periodic task's completion.
-    {
-        let sns_pb::GetUpgradeJournalResponse { upgrade_steps, .. } =
-            sns::governance::get_upgrade_journal(&pocket_ic, sns.governance.canister_id);
-        let upgrade_steps = upgrade_steps.expect("cached_upgrade_steps should be Some");
-
-        assert_eq!(
-            upgrade_steps.versions,
-            vec![initial_sns_version, new_sns_version_1, new_sns_version_2,]
-        );
+        // bazel test //rs/nervous_system/integration_tests/... --test_arg=test_get_upgrade_journal
+        // ...
+        // assertion `left == right` failed
+        // left: Some(1620505063)
+        // right: Some(1620505059)
+        assert_eq!(response_timestamp_seconds, Some(1620501459 + UPGRADE_STEPS_INTERVAL_REFRESH_BACKOFF_SECONDS));
     }
 }
