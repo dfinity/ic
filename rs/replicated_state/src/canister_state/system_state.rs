401dee79ae38ad8e1ffa1b192749e5ff346fafb8
Dragoljub Djuric <dragoljub.duric@dfinity.org>
2024-10-08 10:46:15 +0200

    Update rs/replicated_state/src/canister_state/system_state.rs
    Co-authored-by: Dimitris Sarlis <dimitrios.sarlis@dfinity.org>

--- rs/replicated_state/src/canister_state/system_state.rs
+++ rs/replicated_state/src/canister_state/system_state.rs
@@ -517,7 +517,7 @@ impl TaskQueue {
                     self.paused_or_aborted_task,
                     Some(ExecutionTask::PausedExecution { .. })
                 ),
-                "Received aborted task {:?} is not compatible with paused task {:?}.",
+                "Received aborted task {:?} which is not compatible with paused task {:?}.",
                 aborted_task,
                 self.paused_or_aborted_task
             ),
