441c8dea16c5e502b79123757663c5e20835f024
Max Summe <maximilian.summe@dfinity.org>
2024-10-08 12:48:12 -0700

    Fix broken edge case in tests

--- rs/nervous_system/integration_tests/tests/sns_release_qualification.rs
+++ rs/nervous_system/integration_tests/tests/sns_release_qualification.rs
@@ -249,14 +249,18 @@ fn test_sns_upgrade(sns_canisters_to_upgrade: Vec<SnsCanisterType>) {
     );
 
     // Every canister we are testing has two upgrades.  We are just making sure the counts match
-    for _ in sns_canisters_to_upgrade {
-        sns::governance::propose_to_upgrade_sns_to_next_version_and_wait(
+    for canister_type in &sns_canisters_to_upgrade {
+        sns::upgrade_sns_to_next_version_and_assert_change(
             &pocket_ic,
-            sns.governance.canister_id,
+            sns.root.canister_id,
+            *canister_type,
         );
-        sns::governance::propose_to_upgrade_sns_to_next_version_and_wait(
+    }
+    for canister_type in sns_canisters_to_upgrade {
+        sns::upgrade_sns_to_next_version_and_assert_change(
             &pocket_ic,
-            sns.governance.canister_id,
+            sns.root.canister_id,
+            canister_type,
         );
     }
 }
