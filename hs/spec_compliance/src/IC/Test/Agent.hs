83dfdd95f51ad061fbf1f0ad6a1b1b4c075b4687
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-10-09 09:41:56 +0000

    .

--- hs/spec_compliance/src/IC/Test/Agent.hs
+++ hs/spec_compliance/src/IC/Test/Agent.hs
@@ -781,7 +781,9 @@ code2xx, code202, code4xx, code202_or_4xx :: (HasCallStack) => Response BS.ByteS
 code2xx = codePred "2xx" $ \c -> 200 <= c && c < 300
 code202 = codePred "202" $ \c -> c == 202
 code4xx = codePred "4xx" $ \c -> 400 <= c && c < 500
+
 code400 = codePred "400" $ \c -> c == 400
+
 code403 = codePred "403" $ \c -> c == 403
 
 code202_or_4xx = codePred "202 or 4xx" $ \c -> c == 202 || 400 <= c && c < 500
