e9fb82f8117f969c07ff49ab7bca1f6ccffddcc1
Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>
2024-10-10 10:41:58 +0200

    Move transaction generation function to sm-tests and use it for both ICP and ICRC golden state tests

--- rs/ledger_suite/icp/tests/golden_nns_state.rs
+++ rs/ledger_suite/icp/tests/golden_nns_state.rs
@@ -1,6 +1,7 @@
-use candid::{Decode, Encode, Nat};
+use candid::{Decode, Encode};
 use canister_test::Wasm;
-use ic_base_types::{CanisterId, PrincipalId};
+use ic_base_types::CanisterId;
+use ic_icrc1_ledger_sm_tests::{generate_transactions, TransactionGenerationParameters};
 use ic_ledger_core::block::BlockType;
 use ic_ledger_test_utils::state_machine_helpers::index::{
     get_all_blocks, wait_until_sync_is_completed,
@@ -11,26 +12,22 @@ use ic_ledger_test_utils::{
     build_mainnet_ledger_archive_wasm, build_mainnet_ledger_index_wasm, build_mainnet_ledger_wasm,
 };
 use ic_nns_constants::{
-    GOVERNANCE_CANISTER_INDEX_IN_NNS_SUBNET, LEDGER_CANISTER_INDEX_IN_NNS_SUBNET,
-    LEDGER_INDEX_CANISTER_INDEX_IN_NNS_SUBNET,
+    LEDGER_CANISTER_INDEX_IN_NNS_SUBNET, LEDGER_INDEX_CANISTER_INDEX_IN_NNS_SUBNET,
 };
 use ic_nns_test_utils_golden_nns_state::new_state_machine_with_golden_nns_state_or_panic;
 use ic_state_machine_tests::StateMachine;
 use icp_ledger::{Archives, FeatureFlags, LedgerCanisterPayload, UpgradeArgs};
-use icrc_ledger_types::icrc1::account::{Account, Subaccount};
-use icrc_ledger_types::icrc2::approve::ApproveArgs;
-use icrc_ledger_types::icrc2::transfer_from::TransferFromArgs;
 use std::time::Instant;
 
 const INDEX_CANISTER_ID: CanisterId =
     CanisterId::from_u64(LEDGER_INDEX_CANISTER_INDEX_IN_NNS_SUBNET);
 const LEDGER_CANISTER_ID: CanisterId = CanisterId::from_u64(LEDGER_CANISTER_INDEX_IN_NNS_SUBNET);
-const MINTER_ID: CanisterId = CanisterId::from_u64(GOVERNANCE_CANISTER_INDEX_IN_NNS_SUBNET);
-const NUM_REPETITIONS_PER_TRANSACTION: usize = 200;
-const USER1_PRINCIPAL_ID: PrincipalId = PrincipalId::new_user_test_id(101);
-const USER1_SUBACCOUNT: Subaccount = [0x77u8; 32];
-const USER2_PRINCIPAL_ID: PrincipalId = PrincipalId::new_user_test_id(102);
-const USER2_SUBACCOUNT: Subaccount = [0xBBu8; 32];
+const NUM_TRANSACTIONS_PER_TYPE: usize = 20;
+const MINT_MULTIPLIER: u64 = 10_000;
+const TRANSFER_MULTIPLIER: u64 = 1000;
+const APPROVE_MULTIPLIER: u64 = 100;
+const TRANSFER_FROM_MULTIPLIER: u64 = 10;
+const BURN_MULTIPLIER: u64 = 1;
 
 /// Create a state machine with the golden NNS state, then upgrade and downgrade the ICP
 /// ledger canister suite.
@@ -110,104 +107,17 @@ impl Setup {
     }
 
     pub fn perform_transactions(&self) {
-        let start = Instant::now();
-        // Create a bunch of transactions - 5 types of transactions, 200 of each means that each
-        // run of `perform_transactions` triggers an archival.
-        for _ in 0..NUM_REPETITIONS_PER_TRANSACTION {
-            // Mint
-            let _block_index = ic_icrc1_ledger_sm_tests::transfer(
-                &self.state_machine,
-                LEDGER_CANISTER_ID,
-                MINTER_ID.get().0,
-                Account {
-                    owner: USER1_PRINCIPAL_ID.0,
-                    subaccount: Some(USER1_SUBACCOUNT),
-                },
-                1_000_000_000u64,
-            )
-            .expect("should successfully mint to user1 using icrc1_transfer");
-
-            // Transfer
-            let _block_index = ic_icrc1_ledger_sm_tests::transfer(
-                &self.state_machine,
-                LEDGER_CANISTER_ID,
-                Account {
-                    owner: USER1_PRINCIPAL_ID.0,
-                    subaccount: Some(USER1_SUBACCOUNT),
-                },
-                Account {
-                    owner: USER2_PRINCIPAL_ID.0,
-                    subaccount: Some(USER2_SUBACCOUNT),
-                },
-                1u64,
-            )
-            .expect("should successfully perform icrc1_transfer from user1 to user2");
-
-            // Burn
-            let _block_index = ic_icrc1_ledger_sm_tests::transfer(
-                &self.state_machine,
-                LEDGER_CANISTER_ID,
-                Account {
-                    owner: USER1_PRINCIPAL_ID.0,
-                    subaccount: Some(USER1_SUBACCOUNT),
-                },
-                MINTER_ID.get().0,
-                10_000u64,
-            )
-            .expect("should successfully perform icrc1_transfer from user1 to burn");
-
-            // Approve
-            let approve_args = ApproveArgs {
-                from_subaccount: Some(USER1_SUBACCOUNT),
-                spender: Account {
-                    owner: USER2_PRINCIPAL_ID.0,
-                    subaccount: Some(USER2_SUBACCOUNT),
-                },
-                amount: Nat::from(100_000u64),
-                expected_allowance: None,
-                expires_at: None,
-                fee: None,
-                memo: None,
-                created_at_time: None,
-            };
-            let _block_index = ic_icrc1_ledger_sm_tests::send_approval(
-                &self.state_machine,
-                LEDGER_CANISTER_ID,
-                USER1_PRINCIPAL_ID.0,
-                &approve_args,
-            )
-            .expect("should successfully perform icrc2_approve from user1 to user2");
-
-            // Transfer From
-            let transfer_from_args = TransferFromArgs {
-                spender_subaccount: Some(USER2_SUBACCOUNT),
-                from: Account {
-                    owner: USER1_PRINCIPAL_ID.0,
-                    subaccount: Some(USER1_SUBACCOUNT),
-                },
-                to: Account {
-                    owner: USER2_PRINCIPAL_ID.0,
-                    subaccount: Some(USER2_SUBACCOUNT),
-                },
-                amount: Nat::from(10_000u64),
-                fee: None,
-                memo: None,
-                created_at_time: None,
-            };
-            let _block_index = ic_icrc1_ledger_sm_tests::send_transfer_from(
-                &self.state_machine,
-                LEDGER_CANISTER_ID,
-                USER2_PRINCIPAL_ID.0,
-                &transfer_from_args,
-            )
-                .expect(
-                    "should successfully perform icrc2_transfer_from from user1 to user2 with user2 as spender",
-                );
-        }
-        println!(
-            "Time taken for {} transactions: {:?}",
-            5 * NUM_REPETITIONS_PER_TRANSACTION,
-            start.elapsed()
+        generate_transactions(
+            &self.state_machine,
+            LEDGER_CANISTER_ID,
+            TransactionGenerationParameters {
+                mint_multiplier: MINT_MULTIPLIER,
+                transfer_multiplier: TRANSFER_MULTIPLIER,
+                approve_multiplier: APPROVE_MULTIPLIER,
+                transfer_from_multiplier: TRANSFER_FROM_MULTIPLIER,
+                burn_multiplier: BURN_MULTIPLIER,
+                num_transactions_per_type: NUM_TRANSACTIONS_PER_TYPE,
+            },
         );
         let start = Instant::now();
         wait_until_sync_is_completed(&self.state_machine, INDEX_CANISTER_ID, LEDGER_CANISTER_ID);
