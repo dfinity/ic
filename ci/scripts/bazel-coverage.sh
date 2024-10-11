c01215645868066007b9493f910902c153b02243
Marko Kosmerl <marko.kosmerl@dfinity.org>
2024-10-11 13:59:36 +0000

    test

--- ci/scripts/bazel-coverage.sh
+++ ci/scripts/bazel-coverage.sh
@@ -10,3 +10,12 @@ bazel --output_base=/var/tmp/bazel-output/ coverage --config=ci --combined_repor
     --test_timeout=3000 --combined_report=lcov $(<cov_targets.txt) || true
 cp bazel-out/_coverage/_coverage_report.dat cov_report.dat
 genhtml --output cov_html cov_report.dat
+
+# upload report to s3
+rclone sync cov_html public-s3://dfinity-download-public/coverage/${CI_COMMIT_SHA}/ \
+    --config .rclone.conf
+
+# rint url as log and annotation
+URL="https://download.dfinity.systems/coverage/$CI_COMMIT_SHA/index.html"
+echo "Code Coverage Report [$CI_COMMIT_SHA]($URL)" >>$GITHUB_STEP_SUMMARY
+echo "Code Coverage Report: $URL"
