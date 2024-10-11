670a23c511852ff36019445493f7a96647ba014e
IDX GitLab Automation <idx@dfinity.org>
2024-10-11 13:38:56 +0000

    chore: use candid_method

--- rs/boundary_node/rate_limits/canister/canister.rs
+++ rs/boundary_node/rate_limits/canister/canister.rs
@@ -1,17 +1,21 @@
+use candid::candid_method;
+use ic_cdk_macros::{init, update};
 use rate_limits_api::{GetConfigResponse, InitArg, Version};
 use storage::{get_stable_version, set_stable_version};
 use types::{ConfigResponse, OutputConfig};
 mod storage;
 mod types;
 
-#[ic_cdk::init]
+#[init]
+#[candid_method(init)]
 fn init(_init_arg: InitArg) {
     // Initialize version to 1
     set_stable_version(1);
     // TODO: init periodic timer for fetching API BNs principals.
 }
 
-#[ic_cdk_macros::update]
+#[update(name = "get_config")]
+#[candid_method(update, rename = "get_config")]
 fn get_config(_version: Option<Version>) -> GetConfigResponse {
     let version = get_stable_version();
 
