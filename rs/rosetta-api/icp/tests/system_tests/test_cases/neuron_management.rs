b2e648968ad4c9c6ff3b4d268ea2a2eac83e0065
Nikolas Haimerl <nikolas.haimerl@dfinity.org>
2024-10-15 11:29:43 +0000

    add setter for dissolve delay

--- rs/rosetta-api/icp/tests/system_tests/test_cases/neuron_management.rs
+++ rs/rosetta-api/icp/tests/system_tests/test_cases/neuron_management.rs
@@ -3,6 +3,8 @@ use crate::common::utils::{get_test_agent, list_neurons, test_identity};
 use ic_agent::identity::BasicIdentity;
 use ic_agent::Identity;
 use ic_icp_rosetta_client::RosettaCreateNeuronArgs;
+use ic_icp_rosetta_client::RosettaSetNeuronDissolveDelayArgs;
+use ic_nns_governance::pb::v1::neuron::DissolveState;
 use ic_types::PrincipalId;
 use icp_ledger::AccountIdentifier;
 use lazy_static::lazy_static;
@@ -39,7 +41,7 @@ fn test_create_neuron() {
         env.rosetta_client
             .create_neuron(
                 env.network_identifier.clone(),
-                (*TEST_IDENTITY).clone(),
+                &(*TEST_IDENTITY).clone(),
                 RosettaCreateNeuronArgs::builder(staked_amount.into())
                     .with_from_subaccount(from_subaccount)
                     .with_neuron_index(neuron_index)
@@ -57,5 +59,82 @@ fn test_create_neuron() {
             n.controller == Some(PrincipalId::from(TEST_IDENTITY.sender().unwrap()))
                 && n.cached_neuron_stake_e8s == staked_amount
         }));
+    });
+}
+
+#[test]
+fn test_set_neuron_dissolve_delay_timestamp() {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        let env = RosettaTestingEnvironment::builder()
+            .with_initial_balances(
+                vec![(
+                    AccountIdentifier::from(TEST_IDENTITY.sender().unwrap()),
+                    // A hundred million ICP should be enough
+                    icp_ledger::Tokens::from_tokens(100_000_000).unwrap(),
+                )]
+                .into_iter()
+                .collect(),
+            )
+            .with_governance_canister()
+            .build()
+            .await;
+
+        // Stake the minimum amount 100 million e8s
+        let staked_amount = 100_000_000u64;
+        let neuron_index = 0;
+        let from_subaccount = [0; 32];
+
+        env.rosetta_client
+            .create_neuron(
+                env.network_identifier.clone(),
+                &(*TEST_IDENTITY).clone(),
+                RosettaCreateNeuronArgs::builder(staked_amount.into())
+                    .with_from_subaccount(from_subaccount)
+                    .with_neuron_index(neuron_index)
+                    .build(),
+            )
+            .await
+            .unwrap();
+
+        // See if the neuron was created successfully
+        let agent = get_test_agent(env.pocket_ic.url().unwrap().port().unwrap()).await;
+        let neuron = list_neurons(&agent).await.full_neurons[0].to_owned();
+
+        let dissolve_delay_timestamp = match neuron.dissolve_state.unwrap() {
+            DissolveState::WhenDissolvedTimestampSeconds(dissolve_delay_timestamp) => {
+                dissolve_delay_timestamp
+            }
+            k => panic!(
+                "Neuron should be in WhenDissolvedTimestampSeconds state, but is instead: {:?}",
+                k
+            ),
+        };
+        let new_dissolve_delay_timestamp = dissolve_delay_timestamp + 1000;
+
+        env.rosetta_client
+            .set_neuron_dissolve_delay(
+                env.network_identifier.clone(),
+                &(*TEST_IDENTITY).clone(),
+                RosettaSetNeuronDissolveDelayArgs::builder(new_dissolve_delay_timestamp)
+                    .with_neuron_index(neuron_index)
+                    .build(),
+            )
+            .await
+            .unwrap();
+
+        let neuron = list_neurons(&agent).await.full_neurons[0].to_owned();
+        // The State of the Neuron should still be in NOT_DISSOLVING
+        let dissolve_delay_timestamp = match neuron.dissolve_state.unwrap() {
+            DissolveState::WhenDissolvedTimestampSeconds(dissolve_delay_timestamp) => {
+                dissolve_delay_timestamp
+            }
+            k => panic!(
+                "Neuron should be in WhenDissolvedTimestampSeconds state, but is instead: {:?}",
+                k
+            ),
+        };
+        // The Dissolve Delay Timestamp should be updated
+        assert_eq!(dissolve_delay_timestamp, new_dissolve_delay_timestamp);
     });
 }
