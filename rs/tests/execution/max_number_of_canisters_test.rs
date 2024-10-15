e7f26563a6298c3411bacff162218eef4fe86c39
Bas van Dijk <bas@dfinity.org>
2024-10-15 18:20:49 +0000

    inline max_number_of_canisters_test

--- rs/tests/execution/max_number_of_canisters_test.rs
+++ rs/tests/execution/max_number_of_canisters_test.rs
@@ -1,31 +1,14 @@
 use anyhow::Result;
-use candid::{Decode, Encode};
-use futures::future::join_all;
-use ic_agent::{agent::RejectCode, export::Principal, identity::Identity, AgentError};
-use ic_management_canister_types::{
-    CanisterSettingsArgs, CanisterSettingsArgsBuilder, CanisterStatusResultV2, CreateCanisterArgs,
-    EmptyBlob, Payload,
-};
-use ic_registry_subnet_type::SubnetType;
+use execution_system_tests_common::config_max_number_of_canisters;
+use ic_agent::agent::RejectCode;
 use ic_system_test_driver::driver::group::SystemTestGroup;
-use ic_system_test_driver::driver::ic::{InternetComputer, Subnet};
 use ic_system_test_driver::driver::test_env::TestEnv;
 use ic_system_test_driver::driver::test_env_api::{GetFirstHealthyNodeSnapshot, HasPublicApiUrl};
 use ic_system_test_driver::systest;
-use ic_system_test_driver::types::*;
 use ic_system_test_driver::util::*;
-use ic_tests::execution::config_max_number_of_canisters;
-use ic_types::{Cycles, PrincipalId};
-use ic_universal_canister::{call_args, management, wasm, CallInterface, UNIVERSAL_CANISTER_WASM};
-use ic_utils::call::AsyncCall;
-use ic_utils::interfaces::{
-    management_canister::{
-        builders::{CanisterUpgradeOptions, InstallMode},
-        UpdateCanisterBuilder,
-    },
-    ManagementCanister,
-};
-use slog::info;
+use ic_types::Cycles;
+use ic_universal_canister::{management, wasm};
+use ic_utils::interfaces::ManagementCanister;
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
