9fc5ab4e9e51ac94615fdc61da85d01647a4b34a
max-dfinity <100170574+max-dfinity@users.noreply.github.com>
2024-10-15 14:46:52 -0700

    feat(registry): Add node_reward_type to NodeRecord (#1608)
    This adds support for node_reward_type to NodeRecord and to
    AddNodePayload.
    
    It adds it as a string to AddNodePayload, but as an enum with validation
    to NodeRecord.

--- rs/orchestrator/src/firewall.rs
+++ rs/orchestrator/src/firewall.rs
@@ -1036,6 +1036,7 @@ mod tests {
                     hostos_version_id: None,
                     public_ipv4_config: None,
                     domain: None,
+                    node_reward_type: None,
                 }),
             )
             .expect("Failed to add node record.");
