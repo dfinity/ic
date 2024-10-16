83ae7715a636e782d0bb7d8bd0e800404c8f7d52
Manuel Amador <manuel.amador@dfinity.org>
2024-10-16 16:19:33 +0200

    When dealing with testing scenarios, use boot-time knobs to alter system behavior.
    This speeds up the test setup by up to a minute per test, and economizes gigabytes of disk writes as well.
    
    On a system with a 500 MB/s SSD, here are some performance figures (with caches cleared).
    
    Before:
    
    ```
    Opening root partition
    Clearing SetupOS-age checks
    Clearing hardware checks
    Clearing network checks
    Closing root partition
    
    real	0m11.770s
    user	0m3.065s
    sys	0m13.193s
    ```
    
    After:
    
    ```
    Opening boot file system /home/manuel/src/ic/bazel-bin/disk-img.tar.zst
    Clearing age, hardware and network checks in SetupOS
    
    real	0m0.575s
    user	0m0.156s
    sys	0m0.559s
    ```

--- rs/ic_os/dev_test_tools/setupos-disable-checks/src/main.rs
+++ rs/ic_os/dev_test_tools/setupos-disable-checks/src/main.rs
@@ -4,91 +4,66 @@ use std::path::{Path, PathBuf};
 
 use anyhow::{Context, Error};
 use clap::Parser;
+use regex::Regex;
 use tempfile::NamedTempFile;
 use tokio::fs;
 
 use partition_tools::{ext::ExtPartition, Partition};
 
+const CHECK_DISABLER_CMDLINE_ARGS: &str =
+    "ic.setupos.check_hardware=0 ic_setupos.check_network=0 ic.setupos.check_age=0";
 const SERVICE_NAME: &str = "setupos-disable-checks";
 
 #[derive(Parser)]
 #[command(name = SERVICE_NAME)]
 struct Cli {
     #[arg(long, default_value = "disk.img")]
+    /// Path to SetupOS disk image; its GRUB boot partition will be modified.
     image_path: PathBuf,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let cli = Cli::parse();
+    let extra_boot_args_path = Path::new("/extra_boot_args");
+    let extra_boot_args_re = Regex::new(r"EXTRA_BOOT_ARGS=(.*)").unwrap();
 
-    // Open rootfs partition
-    println!("Opening root partition");
-    let mut rootfs = ExtPartition::open(cli.image_path, Some(6)).await?;
+    // Open boot file system.
+    eprintln!("Opening boot file system {}", cli.image_path.display());
+    let mut bootfs = ExtPartition::open(cli.image_path, Some(5)).await?;
 
     // Overwrite age checks
-    println!("Clearing SetupOS-age checks");
-    let check_setupos_age = NamedTempFile::new()?;
+    println!("Clearing age, hardware and network checks in SetupOS");
+    let new_args =
+        match extra_boot_args_re.captures(bootfs.read_file(extra_boot_args_path).await?.as_str()) {
+            Some(captures) => {
+                let existing_args = captures.get(1).unwrap().as_str().trim_matches('"');
+                let to_append = " ".to_owned() + CHECK_DISABLER_CMDLINE_ARGS;
+                let existing_args_idempotent = existing_args.replace(to_append.as_str(), "");
+                existing_args_idempotent.to_owned() + to_append.as_str()
+            }
+            None => CHECK_DISABLER_CMDLINE_ARGS.to_string(),
+        };
+
+    let temp_extra_boot_args = NamedTempFile::new()?;
     fs::write(
-        check_setupos_age.path(),
-        indoc::formatdoc!(
-            r#"
-                #!/usr/bin/env bash
-                echo "Skipping SetupOS-age checks."
-            "#
+        temp_extra_boot_args.path(),
+        format!(
+            "# This file has been modified by setupos-disable-checks.
+EXTRA_BOOT_ARGS=\"{}\"
+",
+            new_args
         ),
     )
     .await
-    .context("failed to write file")?;
-    fs::set_permissions(check_setupos_age.path(), Permissions::from_mode(0o755)).await?;
-    rootfs
-        .write_file(
-            check_setupos_age.path(),
-            Path::new("/opt/ic/bin/check-setupos-age.sh"),
-        )
+    .context("failed to write temporary extra boot args")?;
+    fs::set_permissions(temp_extra_boot_args.path(), Permissions::from_mode(0o755)).await?;
+
+    bootfs
+        .write_file(temp_extra_boot_args.path(), extra_boot_args_path)
         .await?;
 
-    // Overwrite hardware checks
-    println!("Clearing hardware checks");
-    let hardware = NamedTempFile::new()?;
-    fs::write(
-        hardware.path(),
-        indoc::formatdoc!(
-            r#"
-                #!/usr/bin/env bash
-                echo "Skipping hardware checks."
-            "#
-        ),
-    )
-    .await
-    .context("failed to write file")?;
-    fs::set_permissions(hardware.path(), Permissions::from_mode(0o755)).await?;
-    rootfs
-        .write_file(hardware.path(), Path::new("/opt/ic/bin/check-hardware.sh"))
-        .await?;
-
-    // Overwrite network checks
-    println!("Clearing network checks");
-    let network = NamedTempFile::new()?;
-    fs::write(
-        network.path(),
-        indoc::formatdoc!(
-            r#"
-                #!/usr/bin/env bash
-                echo "Skipping network checks."
-            "#
-        ),
-    )
-    .await
-    .context("failed to write file")?;
-    fs::set_permissions(network.path(), Permissions::from_mode(0o755)).await?;
-    rootfs
-        .write_file(network.path(), Path::new("/opt/ic/bin/check-network.sh"))
-        .await?;
-
-    // Close rootfs partition
-    println!("Closing root partition");
-    rootfs.close().await?;
+    bootfs.close().await?;
 
     Ok(())
 }
