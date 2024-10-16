72737bd49fad026c017ab9a589de19cced347a9a
Oleksandr Tkachenko <oleksandr.tkachenko@dfinity.org>
2024-10-16 08:14:19 +0000

    mv rpc_csp_vault_reconnection_test

--- rs/tests/src/crypto/mod.rs
+++ rs/tests/src/crypto/mod.rs
@@ -3,7 +3,6 @@ pub mod ic_crypto_csp_socket_test;
 pub mod ic_crypto_csp_umask_test;
 pub mod ic_crypto_fstrim_tool_test;
 pub mod request_signature_test;
-pub mod rpc_csp_vault_reconnection_test;
 
 use ic_registry_subnet_type::SubnetType;
 use ic_system_test_driver::driver::{
