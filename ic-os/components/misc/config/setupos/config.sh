f237d01cec34ccc3d8f5a1edd0bc54b2c1b354c2
Andrew Battat <andrew.battat@dfinity.org>
2024-10-16 23:21:45 +0000

    Fix config.sh comment

--- ic-os/components/misc/config/setupos/config.sh
+++ ic-os/components/misc/config/setupos/config.sh
@@ -5,9 +5,7 @@
 # Retrieves a value from the config.json file using a JSON path.
 # Arguments:
 #   $1 - JSON path to the desired value (e.g., '.icos_settings.node_operator_private_key_path')
-# Note:
-#   - If the key is not found, this function will produce an empty string.
-#   - If the value at the key is `null` (e.g., if the Rust type is an `Option` with value `None`), it will output 'null' as a string.
+# Note: If the key is not found, this function will return null.
 function get_config_value() {
     local CONFIG_FILE="/var/ic/config/config.json"
     local key=$1
