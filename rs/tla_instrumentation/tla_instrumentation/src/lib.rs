daaf94a17c4a0b22c2631872d519b160a91d7aa3
Ognjen Maric <ognjen.maric@dfinity.org>
2024-10-16 15:07:01 +0200

    Yet more error reporting improvements

--- rs/tla_instrumentation/tla_instrumentation/src/lib.rs
+++ rs/tla_instrumentation/tla_instrumentation/src/lib.rs
@@ -2,11 +2,24 @@ pub mod checker;
 pub mod tla_state;
 pub mod tla_value;
 use std::cell::RefCell;
+use std::fmt::Formatter;
 use std::mem;
 use std::rc::Rc;
 
 pub use tla_state::*;
 pub use tla_value::*;
+
+#[derive(Clone, Debug)]
+pub struct SourceLocation {
+    pub file: String,
+    pub line: String,
+}
+
+impl std::fmt::Display for SourceLocation {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&format!("{}: Line {}", self.file, self.line))
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct Update {
@@ -122,7 +135,7 @@ pub struct MessageHandlerState {
 }
 
 impl MessageHandlerState {
-    pub fn new(update: Update, global: GlobalState) -> Self {
+    pub fn new(update: Update, global: GlobalState, source_location: SourceLocation) -> Self {
         let locals = update.default_start_locals.clone();
         let label = update.start_label.clone();
         Self {
@@ -131,6 +144,7 @@ impl MessageHandlerState {
                 global,
                 local: LocalState { locals, label },
                 responses: Vec::new(),
+                source_location,
             }),
         }
     }
@@ -148,8 +162,9 @@ impl InstrumentationState {
         update: Update,
         global: GlobalState,
         globals_snapshotter: Rc<dyn Fn() -> GlobalState>,
+        source_location: SourceLocation,
     ) -> Self {
-        let state = MessageHandlerState::new(update, global);
+        let state = MessageHandlerState::new(update, global, source_location);
         Self {
             handler_state: Rc::new(RefCell::new(state)),
             state_pairs: Rc::new(RefCell::new(Vec::new())),
@@ -177,8 +192,8 @@ pub fn log_request(
     method: &str,
     args: TlaValue,
     global: GlobalState,
+    source_location: SourceLocation,
 ) -> ResolvedStatePair {
-    // TODO: do we want to push the label to the location stack here, or just replace it?
     *state
         .context
         .location
@@ -201,6 +216,7 @@ pub fn log_request(
                 method: method.to_string(),
                 args,
             }],
+            source_location,
         },
     };
     ResolvedStatePair::resolve(
@@ -215,6 +231,7 @@ pub fn log_response(
     from: Destination,
     message: TlaValue,
     global: GlobalState,
+    source_location: SourceLocation
 ) {
     let local = state.context.get_state();
     let stage = &mut state.stage;
@@ -228,6 +245,7 @@ pub fn log_response(
         global,
         local,
         responses: vec![ResponseBuffer { from, message }],
+        source_location,
     });
     state.context.global = GlobalState::new();
     state.context.locals = VarAssignment::new();
@@ -241,14 +259,15 @@ pub fn log_fn_return(state: &mut MessageHandlerState) {
     state.context.return_from_function()
 }
 
-// TODO: Does this work for modeling arguments as non-deterministically chosen locals?
-pub fn log_method_call(function: Update, global: GlobalState) -> MessageHandlerState {
-    MessageHandlerState::new(function, global)
-}
+// // TODO: Does this work for modeling arguments as non-deterministically chosen locals?
+// pub fn log_method_call(function: Update, global: GlobalState, s) -> MessageHandlerState {
+//     MessageHandlerState::new(function, global)
+// }
 
 pub fn log_method_return(
     state: &mut MessageHandlerState,
     global: GlobalState,
+    source_location: SourceLocation,
 ) -> ResolvedStatePair {
     let local = state.context.end_update();
 
@@ -262,6 +281,7 @@ pub fn log_method_return(
             global,
             local,
             requests: Vec::new(),
+            source_location,
         },
     };
     ResolvedStatePair::resolve(
@@ -353,7 +373,8 @@ macro_rules! tla_log_request {
         let res = TLA_INSTRUMENTATION_STATE.try_with(|state| {
             let mut handler_state = state.handler_state.borrow_mut();
             let globals = (*state.globals_snapshotter)();
-            let new_state_pair = $crate::log_request(&mut handler_state, $label, $to, $method, message.clone(), globals);
+            let location = $crate::SourceLocation { file: file!().to_string(), line: line!().to_string() };
+            let new_state_pair = $crate::log_request(&mut handler_state, $label, $to, $method, message.clone(), globals, location);
             let mut state_pairs = state.state_pairs.borrow_mut();
             state_pairs.push(new_state_pair);
         });
@@ -375,10 +396,11 @@ macro_rules! tla_log_request {
 macro_rules! tla_log_response {
     ($from:expr, $message:expr) => {{
         let message = $message.to_tla_value();
+        let location = $crate::SourceLocation { file: file!().to_string(), line: line!().to_string() };
         let res = TLA_INSTRUMENTATION_STATE.try_with(|state| {
             let mut handler_state = state.handler_state.borrow_mut();
             let globals = (*state.globals_snapshotter)();
-            $crate::log_response(&mut handler_state, $from, message.clone(), globals);
+            $crate::log_response(&mut handler_state, $from, message.clone(), globals, location);
         });
         match res {
             Ok(_) => (),
