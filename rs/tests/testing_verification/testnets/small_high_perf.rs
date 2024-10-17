562032d215602598a5d8a51d1fd9c8123ab24537
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-10-17 10:02:49 +0000

    modify small_high_perf.rs

--- rs/tests/testing_verification/testnets/small_high_perf.rs
+++ rs/tests/testing_verification/testnets/small_high_perf.rs
@@ -1,17 +1,17 @@
 // Set up a testnet containing:
 //   one 1-node System and one 1-node Application subnets, one unassigned node, single boundary node, and a p8s (with grafana) VM.
-// All replica nodes use the following resources: 6 vCPUs, 24GiB of RAM, and 50 GiB disk.
+// All replica nodes use the following resources: 64 vCPUs, 480 GiB of RAM, and 2'000 GiB disk.
 //
 // You can setup this testnet with a lifetime of 180 mins by executing the following commands:
 //
 //   $ ./ci/tools/docker-run
-//   $ ict testnet create small --lifetime-mins=180 --output-dir=./small -- --test_tmpdir=./small
+//   $ ict testnet create small_high_perf --lifetime-mins=180 --output-dir=./small_high_perf -- --test_tmpdir=./small_high_perf
 //
-// The --output-dir=./small will store the debug output of the test driver in the specified directory.
-// The --test_tmpdir=./small will store the remaining test output in the specified directory.
+// The --output-dir=./small_high_perf will store the debug output of the test driver in the specified directory.
+// The --test_tmpdir=./small_high_perf will store the remaining test output in the specified directory.
 // This is useful to have access to in case you need to SSH into an IC node for example like:
 //
-//   $ ssh -i small/_tmp/*/setup/ssh/authorized_priv_keys/admin admin@
+//   $ ssh -i small_high_perf/_tmp/*/setup/ssh/authorized_priv_keys/admin admin@
 //
 // Note that you can get the  address of the IC node from the ict console output:
 //
@@ -28,9 +28,9 @@
 //
 // To get access to P8s and Grafana look for the following lines in the ict console output:
 //
-//     prometheus: Prometheus Web UI at http://prometheus.small--1692597750709.testnet.farm.dfinity.systems,
-//     grafana: Grafana at http://grafana.small--1692597750709.testnet.farm.dfinity.systems,
-//     progress_clock: IC Progress Clock at http://grafana.small--1692597750709.testnet.farm.dfinity.systems/d/ic-progress-clock/ic-progress-clock?refresh=10su0026from=now-5mu0026to=now,
+//     prometheus: Prometheus Web UI at http://prometheus.small_high_perf--1692597750709.testnet.farm.dfinity.systems,
+//     grafana: Grafana at http://grafana.small_high_perf--1692597750709.testnet.farm.dfinity.systems,
+//     progress_clock: IC Progress Clock at http://grafana.small_high_perf--1692597750709.testnet.farm.dfinity.systems/d/ic-progress-clock/ic-progress-clock?refresh=10su0026from=now-5mu0026to=now,
 //
 // Happy testing!
 
@@ -41,7 +41,7 @@ use ic_registry_subnet_type::SubnetType;
 use ic_system_test_driver::driver::{
     boundary_node::BoundaryNode,
     group::SystemTestGroup,
-    ic::{InternetComputer, Subnet},
+    ic::{AmountOfMemoryKiB, ImageSizeGiB, InternetComputer, NrOfVCPUs, Subnet, VmResources},
     prometheus_vm::{HasPrometheus, PrometheusVm},
     test_env::TestEnv,
     test_env_api::{await_boundary_node_healthy, HasTopologySnapshot, NnsCustomizations},
@@ -61,6 +61,11 @@ pub fn setup(env: TestEnv) {
         .start(&env)
         .expect("Failed to start prometheus VM");
     InternetComputer::new()
+        .with_default_vm_resources(VmResources {
+            vcpus: Some(NrOfVCPUs::new(64)),
+            memory_kibibytes: Some(AmountOfMemoryKiB::new(480 << 20)),
+            boot_image_minimal_size_gibibytes: Some(ImageSizeGiB::new(2_000)),
+        })
         .add_subnet(Subnet::new(SubnetType::System).add_nodes(1))
         .add_subnet(Subnet::new(SubnetType::Application).add_nodes(1))
         .with_unassigned_nodes(1)
