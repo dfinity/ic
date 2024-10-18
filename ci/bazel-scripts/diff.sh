04208dd9d9aac3c02837a3e471589b7499ee0674
Bas van Dijk <bas@dfinity.org>
2024-10-18 07:35:18 +0000

    wip

--- ci/bazel-scripts/diff.sh
+++ ci/bazel-scripts/diff.sh
@@ -57,8 +57,7 @@ if [ "${BAZEL_COMMAND:-}" == "build" ]; then
     TARGETS=$(bazel query "rdeps(//..., set(${files[*]}))")
 elif [ "${BAZEL_COMMAND:-}" == "test" ]; then
     EXCLUDED_TAGS=(manual $EXCLUDED_TEST_TAGS)
-    EXCLUDED_TAGS_EXPRESSION="$(IFS='|'; echo "${EXCLUDED_TAGS[*]}")"
-    TARGETS=$(bazel query "kind(test, rdeps(//..., set(${files[*]}))) except attr('tags', '$EXCLUDED_TAGS_EXPRESSION', //...)")
+    TARGETS=$(bazel query "kind(test, rdeps(//..., set(${files[*]}))) except attr('tags', '$(IFS='|'; echo "${EXCLUDED_TAGS[*]}")', //...)")
 else
     echo "Unknown BAZEL_COMMAND: ${BAZEL_COMMAND:-}" >&2
     exit 1
