4c6b66c0c4773e686d77adc9cdc1739c6e355a78
Ognjen Maric <ognjen.maric@dfinity.org>
2024-10-22 17:23:40 +0200

    Skip TLA checks for large states

--- rs/tla_instrumentation/tla_instrumentation/src/tla_value.rs
+++ rs/tla_instrumentation/tla_instrumentation/src/tla_value.rs
@@ -20,10 +20,12 @@ pub enum TlaValue {
 }
 
 impl TlaValue {
+    /// An approximation of the size of the TLA value, in terms of the number of atoms.
+    /// Ignores string lengths or number sizes.
     pub fn size(&self) -> u64 {
         match self {
             TlaValue::Set(set) => set.iter().map(|x| x.size()).sum(),
-            TlaValue::Record(map) => map.iter().map(|(k, v)| k.len() as u64 + v.size()).sum(),
+            TlaValue::Record(map) => map.iter().map(|(_k, v)| 1 + v.size()).sum(),
             TlaValue::Function(map) => map.iter().map(|(k, v)| k.size() + v.size()).sum(),
             TlaValue::Seq(vec) => vec.iter().map(|x| x.size()).sum(),
             TlaValue::Literal(_s) => 1 as u64,
