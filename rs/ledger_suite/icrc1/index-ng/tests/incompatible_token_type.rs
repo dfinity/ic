04de5fbbc0b762a6e8869ec027a679272cecbf83
Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>
2024-10-24 18:17:58 +0200

    Refactoring

--- rs/ledger_suite/icrc1/index-ng/tests/incompatible_token_type.rs
+++ rs/ledger_suite/icrc1/index-ng/tests/incompatible_token_type.rs
@@ -9,26 +9,15 @@ use ic_state_machine_tests::StateMachine;
 
 mod common;
 
-fn index_wasm_u64() -> Vec<u8> {
-    let index_wasm_path = std::env::var("IC_ICRC1_INDEX_WASM_U64_PATH").expect(
-        "The Ledger wasm path must be set using the env variable IC_ICRC1_INDEX_WASM_U64_PATH",
-    );
+fn index_wasm(env_var: &str) -> Vec<u8> {
+    let index_wasm_path = std::env::var(env_var).expect(&format!(
+        "The Index wasm path must be set using the env variable {}",
+        env_var
+    ));
     std::fs::read(&index_wasm_path).unwrap_or_else(|e| {
         panic!(
-            "failed to load Wasm file from path {} (env var IC_ICRC1_INDEX_WASM_U64_PATH): {}",
-            index_wasm_path, e
-        )
-    })
-}
-
-fn index_wasm_u256() -> Vec<u8> {
-    let index_wasm_path = std::env::var("IC_ICRC1_INDEX_WASM_U256_PATH").expect(
-        "The Ledger wasm path must be set using the env variable IC_ICRC1_INDEX_WASM_U256_PATH",
-    );
-    std::fs::read(&index_wasm_path).unwrap_or_else(|e| {
-        panic!(
-            "failed to load Wasm file from path {} (env var IC_ICRC1_INDEX_WASM_U256_PATH): {}",
-            index_wasm_path, e
+            "failed to load index Wasm file from path {} (env var {}): {}",
+            index_wasm_path, env_var, e
         )
     })
 }
@@ -36,13 +25,19 @@ fn index_wasm_u256() -> Vec<u8> {
 #[test]
 #[should_panic(expected = "assertion `left == right` failed: u256 representation is 32-bytes long")]
 fn should_fail_to_upgrade_index_ng_from_u64_to_u256_wasm() {
-    index_install_and_upgrade_test(index_wasm_u64(), index_wasm_u256());
+    index_install_and_upgrade_test(
+        index_wasm("IC_ICRC1_INDEX_WASM_U64_PATH"),
+        index_wasm("IC_ICRC1_INDEX_WASM_U256_PATH"),
+    );
 }
 
 #[test]
 #[should_panic(expected = "called `Result::unwrap()` on an `Err` value: TryFromSliceError(())")]
 fn should_fail_to_upgrade_index_ng_from_u256_to_u64_wasm() {
-    index_install_and_upgrade_test(index_wasm_u256(), index_wasm_u64());
+    index_install_and_upgrade_test(
+        index_wasm("IC_ICRC1_INDEX_WASM_U256_PATH"),
+        index_wasm("IC_ICRC1_INDEX_WASM_U64_PATH"),
+    );
 }
 
 fn index_install_and_upgrade_test(index_install_wasm: Vec<u8>, index_upgrade_wasm: Vec<u8>) {
