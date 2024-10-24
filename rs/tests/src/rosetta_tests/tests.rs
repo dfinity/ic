c705212d5b08b808262cf314d161d658527792b3
NikolasHai <113891786+NikolasHai@users.noreply.github.com>
2024-10-24 17:07:16 +0200

    feat(ICP-Rosetta): FI-1540: add disburse of neuron functionality (#2182)
    This MR proposes the following changes:
    
    1. Add the functionality of disbursing a neuron to the ICP Rosetta
    client
    
    ---------
    
    Co-authored-by: Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>

--- rs/tests/src/rosetta_tests/tests.rs
+++ rs/tests/src/rosetta_tests/tests.rs
@@ -3,7 +3,6 @@ pub mod list_known_neurons;
 pub mod list_neurons;
 pub mod make_transaction;
 pub mod network;
-pub mod neuron_disburse;
 pub mod neuron_dissolve;
 pub mod neuron_follow;
 pub mod neuron_hotkey;
