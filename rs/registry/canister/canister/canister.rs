68fa8a2bd15672cc9eacf8ed141b9abcb49e5732
Pietro <pietro.di.marco@dfinity.org>
2024-10-25 09:44:42 +0000

    Remove caller restrictions get_changes_since endpoint

--- rs/registry/canister/canister/canister.rs
+++ rs/registry/canister/canister/canister.rs
@@ -188,22 +188,6 @@ ic_nervous_system_common_build_metadata::define_get_build_metadata_candid_method
 #[export_name = "canister_query get_changes_since"]
 fn get_changes_since() {
     fn main() -> Result<RegistryGetChangesSinceResponse, (Code, String)> {
-        // Authorize: Only self-authenticating and anonymous principals are allowed to call us.
-        const ALLOWED_CALLER_CLASSES: [Result<PrincipalIdClass, String>; 2] = [
-            Ok(PrincipalIdClass::SelfAuthenticating),
-            Ok(PrincipalIdClass::Anonymous),
-        ];
-        let class = caller().class();
-        if !ALLOWED_CALLER_CLASSES.contains(&class) {
-            return Err((
-                Code::Authorization,
-                format!(
-                    "Caller must be self-authenticating, or anonymous (but was {:?}).",
-                    class,
-                ),
-            ));
-        }
-
         // Parse request.
         let request = deserialize_get_changes_since_request(arg_data())
             .map_err(|err| (Code::MalformedMessage, err.to_string()))?;
