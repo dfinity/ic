2294217dda2d3d751bd7a422cf86ad8d6e28e312
Andrew Battat <andrew.battat@dfinity.org>
2024-10-25 23:10:26 +0000

    Update config.sh config references

--- ic-os/components/misc/config/setupos/config.sh
+++ ic-os/components/misc/config/setupos/config.sh
@@ -4,7 +4,7 @@
 
 # Retrieves a value from the config.json file using a JSON path.
 # Arguments:
-#   $1 - JSON path to the desired value (e.g., '.icos_settings.node_operator_private_key_path')
+#   $1 - JSON path to the desired value (e.g., '.icos_settings.nns_urls')
 # Note: If the key is not found, this function will return null.
 function get_config_value() {
     local CONFIG_FILE="/var/ic/config/config.json"
