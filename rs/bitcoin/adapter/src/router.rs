c206fa272f4f4c8fb83b6c72ab12cc8ddc1a4c25
Mihail Jianu <mihail.jianu@dfinity.org>
2024-10-28 11:28:23 +0000

    fix

--- rs/bitcoin/adapter/src/router.rs
+++ rs/bitcoin/adapter/src/router.rs
@@ -56,11 +56,6 @@ pub fn start_main_event_loop(
                 blockchain_manager.make_idle();
                 adapter_state.active().await;
             }
-            if adapter_state.is_idle() {
-                connection_manager.make_idle();
-                blockchain_manager.make_idle();
-                adapter_state.active().await;
-            }
 
             // We do a select over tokio::sync::mpsc::Receiver::recv, tokio::sync::mpsc::UnboundedReceiver::recv,
             // tokio::time::Interval::tick which are all cancellation safe.
