5c654e133fc0d1a78a52070dbe0ca8a257c9515e
Mihail Jianu <mihail.jianu@dfinity.org>
2024-10-28 10:01:26 +0000

    make sure make_idle is aclled every time

--- rs/bitcoin/adapter/src/router.rs
+++ rs/bitcoin/adapter/src/router.rs
@@ -49,12 +49,12 @@ pub fn start_main_event_loop(
 
     tokio::task::spawn(async move {
         let mut tick_interval = interval(Duration::from_millis(100));
+        if adapter_state.is_idle() {
+            connection_manager.make_idle();
+            blockchain_manager.make_idle();
+        }
 
         loop {
-            if adapter_state.is_idle() {
-                connection_manager.make_idle();
-                blockchain_manager.make_idle();
-            }
             adapter_state.active().await;
 
             // We do a select over tokio::sync::mpsc::Receiver::recv, tokio::sync::mpsc::UnboundedReceiver::recv,
@@ -62,6 +62,8 @@ pub fn start_main_event_loop(
             loop {
                 tokio::select! {
                     _ = adapter_state.idle() => {
+                        connection_manager.make_idle();
+                        blockchain_manager.make_idle();
                         break;
                     },
                     event = connection_manager.receive_stream_event() => {
