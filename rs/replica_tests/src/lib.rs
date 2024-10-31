80a8a2b05aada5f8f05d40c4649abdc56846f24d
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-10-31 08:55:48 +0000

    .

--- rs/replica_tests/src/lib.rs
+++ rs/replica_tests/src/lib.rs
@@ -1,6 +1,6 @@
 use core::future::Future;
 use ic_base_types::{PrincipalId, SubnetId};
-use ic_canister_client_sender::{Sender as CanisterSender};
+use ic_canister_client_sender::Sender as CanisterSender;
 use ic_config::{crypto::CryptoConfig, transport::TransportConfig, Config};
 use ic_error_types::{ErrorCode, RejectCode, UserError};
 use ic_execution_environment::IngressHistoryReaderImpl;
