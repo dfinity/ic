5d5097bf87504a68634bf8bffd17d17fc7493996
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2024-11-01 14:40:51 +0000

    third

--- rs/nns/governance/init/src/lib.rs
+++ rs/nns/governance/init/src/lib.rs
@@ -251,6 +251,7 @@ impl GovernanceCanisterInitPayloadBuilder {
                     "neuron_id",
                     "owner_id",
                     "created_ts_ns",
+                    "dissolve_delay_s",
                     "staked_icpt",
                     "follows",
                     "not_for_profit",
@@ -275,10 +276,13 @@ impl GovernanceCanisterInitPayloadBuilder {
             let creation_ts_ns = record[2]
                 .parse::<u64>()
                 .expect("couldn't read the neuron's creation time");
-            let staked_icpt = record[3]
+            let dissolve_delay_seconds = record[3]
+                .parse::<u64>()
+                .expect("couldn't read the neuron's dissolve delay");
+            let staked_icpt = record[4]
                 .parse::<u64>()
                 .expect("couldn't read the neuron's staked icpt amount");
-            let followees: Vec<NeuronIdProto> = record[4]
+            let followees: Vec<NeuronIdProto> = record[5]
                 .split_terminator(',')
                 .map(|x| NeuronIdProto {
                     id: x.parse::<u64>().expect("could not parse followee"),
@@ -290,19 +294,15 @@ impl GovernanceCanisterInitPayloadBuilder {
 
             let neuron_id = NeuronIdProto::from(neuron_id);
 
-            let not_for_profit = record[5]
+            let not_for_profit = record[6]
                 .parse::<bool>()
                 .expect("couldn't read the neuron's not-for-profit flag");
 
             let memo = self.rng.next_u64();
 
-            let maturity_e8s_equivalent = if record.len() < 7 {
-                0
-            } else {
-                record[6]
-                    .parse::<u64>()
-                    .expect("could not parse maturity_e8s_equivalent")
-            };
+            let maturity_e8s_equivalent = record[7]
+                .parse::<u64>()
+                .expect("could not parse maturity_e8s_equivalent");
 
             let kyc_verified = false;
 
@@ -317,7 +317,7 @@ impl GovernanceCanisterInitPayloadBuilder {
                 kyc_verified,
                 maturity_e8s_equivalent,
                 not_for_profit,
-                dissolve_state: Some(DissolveState::DissolveDelaySeconds(1)),
+                dissolve_state: Some(DissolveState::DissolveDelaySeconds(dissolve_delay_seconds)),
                 followees: [(Topic::Unspecified as i32, Followees { followees })]
                     .iter()
                     .cloned()
