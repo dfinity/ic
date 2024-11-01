266d3afb97f12a3eeb91bbe76c0e27d85edb5be6
michael-weigelt <122277901+michael-weigelt@users.noreply.github.com>
2024-11-01 14:41:01 +0100

    Update rs/execution_environment/src/execution_environment.rs
    Co-authored-by: Dimitris Sarlis <dimitrios.sarlis@dfinity.org>

--- rs/execution_environment/src/execution_environment.rs
+++ rs/execution_environment/src/execution_environment.rs
@@ -1381,7 +1381,7 @@ impl ExecutionEnvironment {
 
             Ok(Ic00Method::SubnetInfo) => match &msg {
                 CanisterCall::Ingress(_) => {
-                    self.reject_unexpected_ingress(Ic00Method::CreateCanister)
+                    self.reject_unexpected_ingress(Ic00Method::SubnetInfo)
                 }
                 CanisterCall::Request(_) => {
                     let res = SubnetInfoArgs::decode(payload)
