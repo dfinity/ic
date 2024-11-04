51a4193cb6ceabb8485e3b4146c3ca97a9e18c2c
Bas van Dijk <bas@dfinity.org>
2024-11-04 11:47:20 +0000

    clippy

--- rs/universal_canister/lib/build.rs
+++ rs/universal_canister/lib/build.rs
@@ -1,5 +1,8 @@
 fn main() {
     // lib.rs depends on the environment variable UNIVERSAL_CANISTER_WASM_PATH which is set to the right value in BUILD.bazel.
     // To make cargo clippy / build work we set the variable to a fake value.
-    println!("cargo:rustc-env=UNIVERSAL_CANISTER_WASM_PATH={}", std::env::var("UNIVERSAL_CANISTER_WASM_PATH").unwrap_or("lib.rs".to_string()));
+    println!(
+        "cargo:rustc-env=UNIVERSAL_CANISTER_WASM_PATH={}",
+        std::env::var("UNIVERSAL_CANISTER_WASM_PATH").unwrap_or("lib.rs".to_string())
+    );
 }
