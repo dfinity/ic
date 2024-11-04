2f9ddc12cd7ccd6ff7b2f893dd07179f80c598d4
Martin Raszyk <martin.raszyk@dfinity.org>
2024-11-04 18:27:56 +0100

    fmt

--- rs/rust_canisters/stable_memory_integrity/tests/stable_memory_integrity.rs
+++ rs/rust_canisters/stable_memory_integrity/tests/stable_memory_integrity.rs
@@ -3,8 +3,8 @@ use candid::{Decode, Encode};
 use proptest::prelude::*;
 
 use ic_base_types::CanisterId;
-use ic_stable_memory_integrity::StableOperationResult;
 use ic_pocket_ic_tests::StateMachine;
+use ic_stable_memory_integrity::StableOperationResult;
 use ic_types::{ingress::WasmResult, Cycles, MAX_STABLE_MEMORY_IN_BYTES};
 
 const KB: u64 = 1024;
