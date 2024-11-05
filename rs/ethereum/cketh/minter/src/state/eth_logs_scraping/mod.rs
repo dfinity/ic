2b99f394a29f9f52f7c3e825898504335393eb50
gregorydemay <gregory.demay@dfinity.org>
2024-11-05 14:42:31 +0100

    XC-222: LogScrapings container

--- rs/ethereum/cketh/minter/src/state/eth_logs_scraping/mod.rs
+++ rs/ethereum/cketh/minter/src/state/eth_logs_scraping/mod.rs
@@ -1,6 +1,64 @@
 use crate::numeric::BlockNumber;
 use ic_ethereum_types::Address;
+use std::collections::BTreeMap;
 use std::fmt::Debug;
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct LogScrapings {
+    scrapings: BTreeMap<LogScrapingId, LogScrapingState>,
+}
+
+impl LogScrapings {
+    pub fn new(last_scraped_block_number: BlockNumber) -> Self {
+        let mut scrapings = BTreeMap::new();
+        for id in LogScrapingId::iter() {
+            scrapings.insert(id, LogScrapingState::new(last_scraped_block_number));
+        }
+        Self { scrapings }
+    }
+
+    pub fn set_contract_address(
+        &mut self,
+        id: &LogScrapingId,
+        contract_address: Address,
+    ) -> Result<(), LogScrapingStateError> {
+        self.get_mut(id).set_contract_address(contract_address)
+    }
+
+    pub fn set_last_scraped_block_number(&mut self, id: &LogScrapingId, block_number: BlockNumber) {
+        self.get_mut(id).set_last_scraped_block_number(block_number)
+    }
+
+    fn get_mut(&mut self, id: &LogScrapingId) -> &mut LogScrapingState {
+        self.scrapings
+            .get_mut(id)
+            .expect("BUG: LogScrapings should contain all LogScrapingId")
+    }
+
+    pub fn last_scraped_block_number(&self, id: &LogScrapingId) -> BlockNumber {
+        self.get(id).last_scraped_block_number()
+    }
+
+    pub fn contract_address(&self, id: &LogScrapingId) -> Option<&Address> {
+        self.get(id).contract_address()
+    }
+
+    fn get(&self, id: &LogScrapingId) -> &LogScrapingState {
+        self.scrapings
+            .get(id)
+            .expect("BUG: LogScrapings should contain all LogScrapingId")
+    }
+}
+
+#[derive(Clone, PartialEq, Copy, Debug, PartialOrd, Ord, Eq, EnumIter)]
+#[repr(u8)]
+pub enum LogScrapingId {
+    EthDepositWithoutSubaccount,
+    Erc20DepositWithoutSubaccount,
+    EthOrErc20DepositWithSubaccount,
+}
 
 #[derive(Clone, PartialEq, Debug)]
 pub enum LogScrapingStateError {
