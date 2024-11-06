1380e49da7b4826fcc573ba985574e693e3a1441
Dimitris Sarlis <dimitrios.sarlis@dfinity.org>
2024-11-06 16:37:44 +0100

    feat: Update canister creation fee (#2353)
    This changes the canister creation fee according to the proposal listed
    in the
    [forum](https://forum.dfinity.org/t/evaluating-compute-pricing-in.-response-to-increased-demand-on-the-internet-computer-protocol/36565).

--- rs/config/src/subnet_config.rs
+++ rs/config/src/subnet_config.rs
@@ -434,7 +434,7 @@ impl CyclesAccountManagerConfig {
         let ten_update_instructions_execution_fee_in_cycles = 10;
         Self {
             reference_subnet_size: DEFAULT_REFERENCE_SUBNET_SIZE,
-            canister_creation_fee: Cycles::new(100_000_000_000),
+            canister_creation_fee: Cycles::new(500_000_000_000),
             compute_percent_allocated_per_second_fee: Cycles::new(10_000_000),
 
             // The following fields are set based on a thought experiment where
