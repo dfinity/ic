d42e2b8ad14b015fa56c788cc6e6988f3ad5556e
Alin Sinpalean <alin.sinpalean@dfinity.org>
2024-11-11 13:04:51 +0000

    Document that the default in case of no CallContextManager is unreachable.

--- rs/execution_environment/src/hypervisor.rs
+++ rs/execution_environment/src/hypervisor.rs
@@ -444,6 +444,8 @@ impl Hypervisor {
         let caller = api_type.caller();
         let subnet_available_callbacks = round_limits.subnet_available_callbacks.max(0) as u64;
         let canister_available_callbacks = system_state.call_context_manager().map_or(
+            // The default is never used (since we would never end up here with no
+            // `CallContextManager`) but preferrable to an `unwrap()`.
             self.canister_guaranteed_callback_quota,
             |ccm| {
                 self.canister_guaranteed_callback_quota
