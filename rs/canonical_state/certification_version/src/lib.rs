e60c25f8861e3fcc25362a961072f31544d45717
Christian Stieger <christian.stieger@dfinity.org>
2024-11-12 22:20:05 +0000

    .

--- rs/canonical_state/certification_version/src/lib.rs
+++ rs/canonical_state/certification_version/src/lib.rs
@@ -102,9 +102,7 @@ pub fn all_supported_versions() -> impl std::iter::Iterator<Item = Certification
 
 /// Returns `true` iff `certification_version` is supported.
 pub fn is_supported(certification_version: CertificationVersion) -> bool {
-    all_supported_versions()
-        .find(|v| *v == certification_version)
-        .is_some()
+    all_supported_versions().any(|v| v == certification_version)
 }
 
 #[test]
