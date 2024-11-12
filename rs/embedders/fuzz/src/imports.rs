0582d4e7d4dc5c98966ba2db9d02bd88076bd0aa
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2024-11-12 17:43:53 +0000

    Single config

--- rs/embedders/fuzz/src/imports.rs
+++ rs/embedders/fuzz/src/imports.rs
@@ -1,93 +1,53 @@
 use std::rc::Rc;
 
+use crate::wasm_executor::{
+    get_execution_parameters, get_system_state, MAX_SUBNET_AVAILABLE_MEMORY,
+};
+use ic_config::embedders::Config as EmbeddersConfig;
 use ic_config::flag_status::FlagStatus;
-use ic_config::{embedders::Config as EmbeddersConfig, subnet_config::SchedulerConfig};
-use ic_cycles_account_manager::ResourceSaturation;
+use ic_embedders::wasm_utils::instrumentation::WasmMemoryType;
 use ic_embedders::wasmtime_embedder::{system_api, StoreData};
 use ic_embedders::WasmtimeEmbedder;
-use ic_interfaces::execution_environment::{ExecutionMode, SubnetAvailableMemory};
 use ic_logger::replica_logger::no_op_logger;
-use ic_registry_subnet_type::SubnetType;
+use ic_replicated_state::Memory;
 use ic_replicated_state::NumWasmPages;
-use ic_replicated_state::{Memory, NetworkTopology};
-use ic_system_api::{
-    sandbox_safe_system_state::SandboxSafeSystemState, ApiType, DefaultOutOfInstructionsHandler,
-    ExecutionParameters, InstructionLimits, SystemApiImpl,
-};
-use ic_test_utilities::cycles_account_manager::CyclesAccountManagerBuilder;
-use ic_test_utilities_state::SystemStateBuilder;
+use ic_system_api::{ApiType, DefaultOutOfInstructionsHandler, SystemApiImpl};
 use ic_test_utilities_types::ids::user_test_id;
-use ic_types::{
-    messages::RequestMetadata, time::UNIX_EPOCH, ComputeAllocation, MemoryAllocation, NumBytes,
-    NumInstructions,
-};
+use ic_types::{time::UNIX_EPOCH, NumBytes};
 use std::collections::HashMap;
 use wasm_encoder::{
     EntityType, FuncType, ImportSection, Module, TypeSection, ValType as EncodedValType,
 };
 use wasmtime::{Engine, Extern, Store, StoreLimits, ValType};
 
-const SUBNET_MEMORY_CAPACITY: i64 = i64::MAX / 2;
-const MAX_NUM_INSTRUCTIONS: NumInstructions = NumInstructions::new(1_000_000_000);
-
 pub(crate) fn system_api_imports() -> Vec<u8> {
-    let config = EmbeddersConfig::default();
+    let system_api_type = WasmMemoryType::Wasm64;
+    let mut config = EmbeddersConfig::default();
+    config.feature_flags.write_barrier = FlagStatus::Enabled;
+    config.feature_flags.wasm64 = FlagStatus::Enabled;
+
     let engine = Engine::new(&WasmtimeEmbedder::wasmtime_execution_config(&config))
         .expect("Failed to initialize Wasmtime engine");
     let api_type = ApiType::init(UNIX_EPOCH, vec![], user_test_id(24).get());
 
-    let system_state = SystemStateBuilder::default().build();
-    let cycles_account_manager = CyclesAccountManagerBuilder::new().build();
-    let dirty_page_overhead = SchedulerConfig::application_subnet().dirty_page_overhead;
-    let network_topology = NetworkTopology::default();
-
-    let sandbox_safe_system_state = SandboxSafeSystemState::new_for_testing(
-        &system_state,
-        cycles_account_manager,
-        &network_topology,
-        dirty_page_overhead,
-        ComputeAllocation::default(),
-        RequestMetadata::new(0, UNIX_EPOCH),
-        api_type.caller(),
-        api_type.call_context_id(),
-    );
-    let canister_memory_limit = NumBytes::from(4 << 30);
     let canister_current_memory_usage = NumBytes::from(0);
     let canister_current_message_memory_usage = NumBytes::from(0);
     let system_api = SystemApiImpl::new(
-        api_type,
-        sandbox_safe_system_state,
+        api_type.clone(),
+        get_system_state(api_type),
         canister_current_memory_usage,
         canister_current_message_memory_usage,
-        ExecutionParameters {
-            instruction_limits: InstructionLimits::new(
-                FlagStatus::Disabled,
-                MAX_NUM_INSTRUCTIONS,
-                MAX_NUM_INSTRUCTIONS,
-            ),
-            canister_memory_limit,
-            wasm_memory_limit: None,
-            memory_allocation: MemoryAllocation::default(),
-            compute_allocation: ComputeAllocation::default(),
-            subnet_type: SubnetType::Application,
-            execution_mode: ExecutionMode::Replicated,
-            subnet_memory_saturation: ResourceSaturation::default(),
-        },
-        SubnetAvailableMemory::new(
-            SUBNET_MEMORY_CAPACITY,
-            SUBNET_MEMORY_CAPACITY,
-            SUBNET_MEMORY_CAPACITY,
-        ),
-        EmbeddersConfig::default()
-            .feature_flags
-            .wasm_native_stable_memory,
-        EmbeddersConfig::default().feature_flags.canister_backtrace,
-        EmbeddersConfig::default().max_sum_exported_function_name_lengths,
+        get_execution_parameters(),
+        *MAX_SUBNET_AVAILABLE_MEMORY,
+        config.feature_flags.wasm_native_stable_memory,
+        config.feature_flags.canister_backtrace,
+        config.max_sum_exported_function_name_lengths,
         Memory::new_for_testing(),
         NumWasmPages::from(0),
         Rc::new(DefaultOutOfInstructionsHandler::default()),
         no_op_logger(),
     );
+
     let mut store = Store::new(
         &engine,
         StoreData {
@@ -105,7 +65,7 @@ pub(crate) fn system_api_imports() -> Vec<u8> {
         config.feature_flags,
         config.stable_memory_dirty_page_limit,
         config.stable_memory_accessed_page_limit,
-        ic_embedders::wasm_utils::instrumentation::WasmMemoryType::Wasm64,
+        system_api_type,
     );
 
     // to avoid store move
