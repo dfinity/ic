d0f4f8d68bb1f73003d0ed9b67261ff15aae52c6
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-11-12 09:35:02 +0000

    .

--- rs/p2p/quic_transport/src/request_handler.rs
+++ rs/p2p/quic_transport/src/request_handler.rs
@@ -30,7 +30,7 @@ use crate::{
         QuicTransportMetrics, ERROR_TYPE_ACCEPT, ERROR_TYPE_APP, ERROR_TYPE_FINISH,
         ERROR_TYPE_READ, ERROR_TYPE_STOPPED, ERROR_TYPE_WRITE, STREAM_TYPE_BIDI,
     },
-    ConnId, MAX_MESSAGE_SIZE_BYTES,
+    ConnId, ResetStreamOnDrop, MAX_MESSAGE_SIZE_BYTES,
 };
 
 const QUIC_METRIC_SCRAPE_INTERVAL: Duration = Duration::from_secs(5);
@@ -106,14 +106,17 @@ pub async fn run_stream_acceptor(
 }
 
 #[instrument(skip(metrics, router, bi_tx, bi_rx))]
+/// Note: The method is cancel-safe.
 async fn handle_bi_stream(
     peer_id: NodeId,
     conn_id: ConnId,
     metrics: QuicTransportMetrics,
     router: Router,
-    mut bi_tx: SendStream,
+    bi_tx: SendStream,
     bi_rx: RecvStream,
 ) -> Result<(), anyhow::Error> {
+    let mut send_stream_guard = ResetStreamOnDrop::new(bi_tx);
+    let bi_tx = &mut send_stream_guard.send_stream;
     let mut request = read_request(bi_rx).await.inspect_err(|_| {
         metrics
             .request_handle_errors_total
@@ -143,14 +146,13 @@ async fn handle_bi_stream(
     // We can ignore the errors because if both peers follow the protocol an errors will only occur
     // if the other peer has closed the connection. In this case `accept_bi` in the peer event
     // loop will close this connection.
-    write_response(&mut bi_tx, response)
-        .await
-        .inspect_err(|_| {
-            metrics
-                .request_handle_errors_total
-                .with_label_values(&[STREAM_TYPE_BIDI, ERROR_TYPE_WRITE])
-                .inc();
-        })?;
+    let response_bytes = to_response_bytes(response).await?;
+    bi_tx.write_all(&response_bytes).await.inspect_err(|_| {
+        metrics
+            .request_handle_errors_total
+            .with_label_values(&[STREAM_TYPE_BIDI, ERROR_TYPE_WRITE])
+            .inc();
+    })?;
     bi_tx.finish().inspect_err(|_| {
         metrics
             .request_handle_errors_total
@@ -207,10 +209,7 @@ async fn read_request(mut recv_stream: RecvStream) -> Result<Request<Body>, anyh
         .with_context(|| "Failed to build request.")
 }
 
-async fn write_response(
-    send_stream: &mut SendStream,
-    response: Response<Body>,
-) -> Result<(), anyhow::Error> {
+async fn to_response_bytes(response: Response<Body>) -> Result<Vec<u8>, anyhow::Error> {
     let (parts, body) = response.into_parts();
     // Check for axum error in body
     // TODO: Think about this. What is the error that can happen here?
@@ -231,10 +230,5 @@ async fn write_response(
             .collect(),
         body: body.into(),
     };
-
-    let response_bytes = response_proto.encode_to_vec();
-    send_stream
-        .write_all(&response_bytes)
-        .await
-        .with_context(|| "Failed to write response to stream.")
+    Ok(response_proto.encode_to_vec())
 }
