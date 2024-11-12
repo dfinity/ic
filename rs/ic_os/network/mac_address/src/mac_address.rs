f0f5047ad7d7c604c0e7aeff3af439220a873714
DFINITYManu <123385598+DFINITYManu@users.noreply.github.com>
2024-11-12 17:28:30 +0100

    Update rs/ic_os/network/mac_address/src/mac_address.rs
    Co-authored-by: Andrew Battat <113942931+andrewbattat@users.noreply.github.com>

--- rs/ic_os/network/mac_address/src/mac_address.rs
+++ rs/ic_os/network/mac_address/src/mac_address.rs
@@ -145,17 +145,21 @@ fn get_ipmi_mac() -> Result<FormattedMacAddress> {
 /// Retrieves the MAC address from IPMI if available, else falls back to
 /// a known hard-coded string that is hexadecimally-encoded "invirt".
 pub fn get_mgmt_mac() -> Result<FormattedMacAddress> {
-    let stdout = Command::new("systemd-detect-virt")
+    let output = Command::new("systemd-detect-virt")
         .output()?
-        .stdout
-        .into_iter()
-        .collect::<Vec<u8>>();
-    let output = std::str::from_utf8(&stdout).unwrap_or("[INVALID UTF8]");
-    match output {
-        "none\n" => get_ipmi_mac(),
-        // The following string is hex for "invirt".
-        _ => FormattedMacAddress::try_from("69:6e:76:69:72:74"),
-    }
+        .stdout;
+
+    let stdout = std::str::from_utf8(&output)?.trim();
+    
+    match stdout {
+        "none" => {
+             println!("Running on bare metal. Retrieving IPMI MAC address.");
+             get_ipmi_mac()
+        },
+        _ => {
+             println!("Virtualization detected. Using hard-coded mac address.");
+             FormattedMacAddress::try_from("69:6e:76:69:72:74")
+        },
 }
 
 #[cfg(test)]
