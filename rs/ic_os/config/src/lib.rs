4a354a3111909ea9f4f5d05cae8c02d7d4da6048
Andrew Battat <andrew.battat@dfinity.org>
2024-11-15 21:37:30 +0000

    Add node_reward_type to config tool

--- rs/ic_os/config/src/lib.rs
+++ rs/ic_os/config/src/lib.rs
@@ -74,6 +74,7 @@ mod tests {
         };
         let icos_dev_settings = ICOSDevSettings::default();
         let icos_settings = ICOSSettings {
+            node_reward_type: "type3.1".to_string(),
             mgmt_mac: FormattedMacAddress::try_from("ec:2a:72:31:a2:0c")?,
             deployment_environment: "Mainnet".to_string(),
             logging,
@@ -162,6 +163,7 @@ mod tests {
             "domain_name": "example.com"
         },
         "icos_settings": {
+            "node_reward_type": "type3.1",
             "mgmt_mac": "ec:2a:72:31:a2:0c",
             "deployment_environment": "Mainnet",
             "logging": {
@@ -221,6 +223,7 @@ mod tests {
             "domain_name": "example.com"
         },
         "icos_settings": {
+            "node_reward_type": "type3.1",
             "mgmt_mac": "ec:2a:72:31:a2:0c",
             "deployment_environment": "Mainnet",
             "logging": {
--- rs/ic_os/config/src/main.rs
+++ rs/ic_os/config/src/main.rs
@@ -103,6 +103,8 @@ pub struct GenerateTestnetConfigClapArgs {
 
     // ICOSSettings arguments
     #[arg(long)]
+    pub node_reward_type: Option<String>,
+    #[arg(long)]
     pub mgmt_mac: Option<String>,
     #[arg(long)]
     pub deployment_environment: Option<String>,
@@ -174,6 +176,7 @@ pub fn main() -> Result<()> {
                 ipv4_prefix_length,
                 domain_name,
                 verbose,
+                node_reward_type,
             } = get_config_ini_settings(&config_ini_path)?;
 
             // create NetworkSettings
@@ -222,7 +225,10 @@ pub fn main() -> Result<()> {
                 None => get_ipmi_mac()?,
             };
 
+            let node_reward_type = node_reward_type.expect("Node reward type is required.");
+
             let icos_settings = ICOSSettings {
+                node_reward_type,
                 mgmt_mac,
                 deployment_environment: deployment_json_settings.deployment.name,
                 logging,
@@ -352,6 +358,7 @@ pub fn main() -> Result<()> {
                 ipv4_gateway: clap_args.ipv4_gateway,
                 ipv4_prefix_length: clap_args.ipv4_prefix_length,
                 domain_name: clap_args.domain_name,
+                node_reward_type: clap_args.node_reward_type,
                 mgmt_mac: clap_args.mgmt_mac,
                 deployment_environment: clap_args.deployment_environment,
                 elasticsearch_hosts: clap_args.elasticsearch_hosts,
