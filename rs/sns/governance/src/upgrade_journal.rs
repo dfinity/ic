355528591cd126736d75ca78320f079a899e9a2f
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2024-11-18 15:45:52 +0000

    fourth

--- rs/sns/governance/src/upgrade_journal.rs
+++ rs/sns/governance/src/upgrade_journal.rs
@@ -160,3 +160,31 @@ impl From<upgrade_journal_entry::TargetVersionReset> for upgrade_journal_entry::
         upgrade_journal_entry::Event::TargetVersionReset(event)
     }
 }
+
+impl upgrade_journal_entry::Event {
+    /// Useful for specifying expected states of the SNS upgrade journal in a way that isn't
+    /// overly fragile.
+    pub fn redact_human_readable(self: Self) -> Self {
+        match self {
+            Self::UpgradeOutcome(upgrade_outcome) => {
+                Self::UpgradeOutcome(upgrade_journal_entry::UpgradeOutcome {
+                    human_readable: None,
+                    ..upgrade_outcome
+                })
+            }
+            Self::UpgradeStepsReset(upgrade_steps_reset) => {
+                Self::UpgradeStepsReset(upgrade_journal_entry::UpgradeStepsReset {
+                    human_readable: None,
+                    ..upgrade_steps_reset
+                })
+            }
+            Self::TargetVersionReset(target_version_reset) => {
+                Self::TargetVersionReset(upgrade_journal_entry::TargetVersionReset {
+                    human_readable: None,
+                    ..target_version_reset
+                })
+            }
+            event => event,
+        }
+    }
+}
