516327ab313b50d215b4493d91587d7af46cdcf2
Andre Popovitch <andre@popovit.ch>
2024-11-19 14:40:29 -0600

    Address PR feedback

--- rs/nervous_system/agent/src/sns/root.rs
+++ rs/nervous_system/agent/src/sns/root.rs
@@ -15,6 +15,15 @@ pub struct RootCanister {
     pub canister_id: PrincipalId,
 }
 
+#[allow(clippy::large_enum_variant)]
+#[derive(Debug, thiserror::Error)]
+pub enum ListSnsCanistersError<E> {
+    #[error("SNS root canister did not return canister IDs for all canisters - this should never happen")]
+    SnsRootDidNotReturnAllCanisterIds(ListSnsCanistersResponse),
+    #[error("Failed to call SNS root canister")]
+    CallFailed(#[from] E),
+}
+
 impl RootCanister {
     pub fn new(canister_id: impl Into<PrincipalId>) -> Self {
         let canister_id = canister_id.into();
@@ -35,24 +44,30 @@ impl RootCanister {
             .await
     }
 
-    pub async fn list_sns_canisters<C: CallCanisters>(&self, agent: &C) -> Result<Sns, C::Error> {
+    pub async fn list_sns_canisters<C: CallCanisters>(
+        &self,
+        agent: &C,
+    ) -> Result<Sns, ListSnsCanistersError<C::Error>> {
+        let response = agent
+            .call(self.canister_id, ListSnsCanistersRequest {})
+            .await?;
         let ListSnsCanistersResponse {
-            root: Some(sns_root_canister_id_1),
+            root: Some(sns_root_canister_id),
             governance: Some(sns_governance_canister_id),
             ledger: Some(sns_ledger_canister_id),
             swap: Some(swap_canister_id),
             index: Some(index_canister_id),
             archives,
             dapps: _,
-        } = agent
-            .call(self.canister_id, ListSnsCanistersRequest {})
-            .await?
+        } = response
         else {
-            panic!("SNS root canister did not return canister IDs for all canisters - this should never happen");
+            return Err(ListSnsCanistersError::SnsRootDidNotReturnAllCanisterIds(
+                response,
+            ));
         };
 
         Ok(Sns {
-            root: RootCanister::new(sns_root_canister_id_1),
+            root: RootCanister::new(sns_root_canister_id),
             governance: GovernanceCanister::new(sns_governance_canister_id),
             ledger: LedgerCanister::new(sns_ledger_canister_id),
             swap: SwapCanister::new(swap_canister_id),
