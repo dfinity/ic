7af6596c49a095f01e61bea415ac775fea929fa2
Christian Stieger <christian.stieger@dfinity.org>
2024-11-19 12:29:50 +0000

    .

--- rs/embedders/fuzz/src/wasm_executor.rs
+++ rs/embedders/fuzz/src/wasm_executor.rs
@@ -141,7 +141,7 @@ pub(crate) fn get_system_state(api_type: ApiType) -> SandboxSafeSystemState {
         dirty_page_overhead,
         ComputeAllocation::default(),
         HypervisorConfig::default().subnet_callback_soft_limit as u64,
-        RequestMetadata::new(0, UNIX_EPOCH),
+        Default::default(),
         api_type.caller(),
         api_type.call_context_id(),
     )
