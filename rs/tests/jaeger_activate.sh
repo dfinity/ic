d8026bb9bc78fa54478df61bb2b23be565015792
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-11-19 11:57:22 +0000

    .

--- rs/tests/jaeger_activate.sh
+++ rs/tests/jaeger_activate.sh
@@ -4,13 +4,12 @@
 
 set -euo pipefail
 
-docker run --rm --name jaeger \
-  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
-  -p 16686:16686 \
-  -p 4317:4317 \
-  -p 4318:4318 \
-  -p 14250:14250 \
-  -p 14268:14268 \
-  -p 14269:14269 \
-  -p 9411:9411 \
-  jaegertracing/all-in-one:1.63.0
+docker load -i /config/jaeger.tar
+docker run -d --name jaeger \
+    -e COLLECTOR_OTLP_ENABLED=true \
+    -e SPAN_STORAGE_TYPE=badger \
+    -e BADGER_DIRECTORY_VALUE=/badger/data \
+    -e BADGER_DIRECTORY_KEY=/badger/key \
+    -p 4317:4317 \
+    -p 16686:16686 \
+    jaegertracing/all-in-one:1.58
