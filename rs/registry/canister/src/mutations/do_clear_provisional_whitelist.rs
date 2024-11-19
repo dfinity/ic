d77b057ccd38358f379770efe05fb9d43151d2f1
Pietro <pietro.di.marco@dfinity.org>
2024-11-19 11:46:19 +0100

    Add ts in registry value

--- rs/registry/canister/src/mutations/do_clear_provisional_whitelist.rs
+++ rs/registry/canister/src/mutations/do_clear_provisional_whitelist.rs
@@ -20,6 +20,7 @@ impl Registry {
                 value,
                 version: _,
                 deletion_marker: _,
+                timestamp: _,
             }) => ProvisionalWhitelist::decode(value.as_slice()).unwrap(),
             None => panic!("Provisional whitelist not found in the registry"),
         };
