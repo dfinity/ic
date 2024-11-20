f7ad21608d3edafb74a586e4b81e781bf5a74979
Martin Raszyk <martin.raszyk@dfinity.org>
2024-11-20 16:45:41 +0100

    feat: create PocketIC handle for existing PocketIC instace

--- packages/pocket-ic/src/nonblocking.rs
+++ packages/pocket-ic/src/nonblocking.rs
@@ -78,6 +78,8 @@ pub struct PocketIc {
     http_gateway: Option<HttpGatewayInfo>,
     server_url: Url,
     reqwest_client: reqwest::Client,
+    // do not delete the instance when dropping this handle if this handle was created for an existing instance
+    keep: bool,
     _log_guard: Option<WorkerGuard>,
 }
 
@@ -89,6 +91,29 @@ impl PocketIc {
             .with_application_subnet()
             .build_async()
             .await
+    }
+
+    /// Creates a PocketIC handle to an existing instance on a running server.
+    pub async fn new_from_existing_instance(
+        server_url: Url,
+        instance_id: InstanceId,
+        max_request_time_ms: Option<u64>,
+    ) -> Self {
+        let test_driver_pid = std::process::id();
+        let log_guard = setup_tracing(test_driver_pid);
+
+        let reqwest_client = reqwest::Client::new();
+        debug!("instance_id={} Reusing existing instance.", instance_id);
+
+        Self {
+            instance_id,
+            max_request_time_ms,
+            http_gateway: None,
+            server_url,
+            reqwest_client,
+            keep: true,
+            _log_guard: log_guard,
+        }
     }
 
     pub(crate) async fn from_components(
@@ -139,8 +164,14 @@ impl PocketIc {
             http_gateway: None,
             server_url,
             reqwest_client,
+            keep: false,
             _log_guard: log_guard,
         }
+    }
+
+    /// Returns the URL of the PocketIC server on which this PocketIC instance is running.
+    pub fn get_server_url(&self) -> Url {
+        self.server_url.clone()
     }
 
     /// Returns the topology of the different subnets of this PocketIC instance.
@@ -1266,11 +1297,13 @@ impl PocketIc {
 
     pub(crate) async fn do_drop(&mut self) {
         self.stop_http_gateway().await;
-        self.reqwest_client
-            .delete(self.instance_url())
-            .send()
-            .await
-            .expect("Failed to send delete request");
+        if !self.keep {
+            self.reqwest_client
+                .delete(self.instance_url())
+                .send()
+                .await
+                .expect("Failed to send delete request");
+        }
     }
 
     async fn get<T: DeserializeOwned>(&self, endpoint: &str) -> T {
