ad967b926aa5e97f98c679f6746093bb54e6d25b
Andrew Battat <andrew.battat@dfinity.org>
2024-11-20 20:18:53 +0000

    Rename use_nns_public_key and use_node_operator_private_key

--- rs/ic_os/config/src/lib.rs
+++ rs/ic_os/config/src/lib.rs
@@ -75,9 +75,9 @@ mod tests {
             mgmt_mac: FormattedMacAddress::try_from("ec:2a:72:31:a2:0c")?,
             deployment_environment: "Mainnet".to_string(),
             logging,
-            nns_public_key_exists: true,
+            use_nns_public_key: true,
             nns_urls: vec!["http://localhost".parse().unwrap()],
-            node_operator_private_key_exists: true,
+            use_node_operator_private_key: true,
             use_ssh_authorized_keys: false,
             icos_dev_settings,
         };
@@ -167,11 +167,11 @@ mod tests {
                 "elasticsearch_hosts": "elasticsearch-node-0.mercury.dfinity.systems:443 elasticsearch-node-1.mercury.dfinity.systems:443",
                 "elasticsearch_tags": "tag1 tag2"
             },
-            "nns_public_key_exists": true,
+            "use_nns_public_key": true,
             "nns_urls": [
                 "http://localhost"
             ],
-            "node_operator_private_key_exists": true,
+            "use_node_operator_private_key": true,
             "use_ssh_authorized_keys": false,
             "icos_dev_settings": {}
         },
@@ -227,11 +227,11 @@ mod tests {
                 "elasticsearch_hosts": "elasticsearch-node-0.mercury.dfinity.systems:443 elasticsearch-node-1.mercury.dfinity.systems:443",
                 "elasticsearch_tags": "tag1 tag2"
             },
-            "nns_public_key_exists": true,
+            "use_nns_public_key": true,
             "nns_urls": [
                 "http://localhost"
             ],
-            "node_operator_private_key_exists": true,
+            "use_node_operator_private_key": true,
             "use_ssh_authorized_keys": false,
             "icos_dev_settings": {}
         },
