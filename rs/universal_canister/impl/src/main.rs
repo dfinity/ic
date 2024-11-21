9c5d9acb04cec5844a2f7188f63b5dbc478049b3
Michael Weigelt <michael.weigelt@dfinity.com>
2024-11-21 14:02:58 +0000

    uni canister public endpoint

--- rs/universal_canister/impl/src/main.rs
+++ rs/universal_canister/impl/src/main.rs
@@ -380,8 +380,8 @@ fn eval(ops_bytes: OpsBytes) {
                 stack.push_int64(api::mint_cycles(amount));
             }
             Ops::MintCycles128 => {
-                let amount_high = stack.pop_int64();
                 let amount_low = stack.pop_int64();
+                let amount_high = stack.pop_int64();
                 stack.push_blob(api::mint_cycles128(amount_high, amount_low))
             }
             Ops::OneWayCallNew => {
