f160aa2b6b3c93e5fd3dda2a7bcb3fe5d7da2b2b
Dimitris Sarlis <dimitrios.sarlis@dfinity.org>
2024-11-23 10:32:02 +0000

    Fix more tests

--- rs/scenario_tests/src/tests/testcase_4_3_xnet_slo.rs
+++ rs/scenario_tests/src/tests/testcase_4_3_xnet_slo.rs
@@ -324,7 +324,7 @@ pub async fn test_impl(
         };
 
     for (i, m) in aggregated_metrics.iter().enumerate() {
-        let attempted_calls = m.requests_sent + m.call_errors;
+        let attempted_calls = m.calls_attempted;
         if attempted_calls != 0 {
             let failed_calls = m.call_errors + m.reject_responses;
             let error_ratio = 100. * failed_calls as f64 / attempted_calls as f64;
@@ -363,17 +363,23 @@ pub async fn test_impl(
             m.latency_distribution.buckets().last().unwrap().1 + m.reject_responses;
         // All messages sent more than `targeted_latency_seconds` before the end of the
         // test should have gotten a response.
-        let responses_expected = (m.requests_sent as f64
+        let responses_expected = ((m.calls_attempted - m.call_errors) as f64
             * (runtime - targeted_latency_seconds) as f64
             / runtime as f64) as usize;
         // Account for requests enqueued this round (in case canister messages were
         // executed before ingress messages, i.e. the heartbeat was executed before
         // metrics collection) or uncounted responses (if ingress executed first).
         let responses_expected = responses_expected - subnet_to_subnet_rate;
-        let actual = format!("{}/{}", responses_received, m.requests_sent);
+        let actual = format!(
+            "{}/{}",
+            responses_received,
+            m.calls_attempted - m.call_errors
+        );
         let msg = format!(
             "Expected requests sent more than {}s ago ({}/{}) to receive responses",
-            targeted_latency_seconds, responses_expected, m.requests_sent
+            targeted_latency_seconds,
+            responses_expected,
+            m.calls_attempted - m.call_errors
         );
         expect(
             responses_received >= responses_expected,
