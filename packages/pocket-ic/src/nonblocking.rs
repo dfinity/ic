7fe7e089e7368784b64f7c827bbda84576a1c8be
Martin Raszyk <martin.raszyk@dfinity.org>
2024-11-25 14:06:46 +0100

    feat: PocketIC library functions for generic calls

--- packages/pocket-ic/src/nonblocking.rs
+++ packages/pocket-ic/src/nonblocking.rs
@@ -630,7 +630,11 @@ impl PocketIc {
         .await
     }
 
-    pub(crate) async fn query_call_with_effective_principal(
+    /// Execute a query call on a canister explicitly specifying an effective principal to route the request:
+    /// this API is useful for making generic calls (including management canister calls) without using dedicated functions from this library
+    /// (e.g., making generic calls in dfx to a PocketIC instance).
+    #[instrument(skip(self, payload), fields(instance_id=self.instance_id, canister_id = %canister_id.to_string(), effective_principal = %effective_principal.to_string(), sender = %sender.to_string(), method = %method, payload_len = %payload.len()))]
+    pub async fn query_call_with_effective_principal(
         &self,
         canister_id: CanisterId,
         effective_principal: RawEffectivePrincipal,
@@ -1425,7 +1429,11 @@ impl PocketIc {
         }
     }
 
-    pub(crate) async fn update_call_with_effective_principal(
+    /// Execute an update call on a canister explicitly specifying an effective principal to route the request:
+    /// this API is useful for making generic calls (including management canister calls) without using dedicated functions from this library
+    /// (e.g., making generic calls in dfx to a PocketIC instance).
+    #[instrument(skip(self, payload), fields(instance_id=self.instance_id, canister_id = %canister_id.to_string(), effective_principal = %effective_principal.to_string(), sender = %sender.to_string(), method = %method, payload_len = %payload.len()))]
+    pub async fn update_call_with_effective_principal(
         &self,
         canister_id: CanisterId,
         effective_principal: RawEffectivePrincipal,
