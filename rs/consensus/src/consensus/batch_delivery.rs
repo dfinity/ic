c3d430dca49894c6593ab5b2927f75b6e296a2e1
Leon Tan <leon.tan@dfinity.org>
2024-11-27 10:30:28 +0000

    Deliver remote dkg transcripts from non-summary blocks as well

--- rs/consensus/src/consensus/batch_delivery.rs
+++ rs/consensus/src/consensus/batch_delivery.rs
@@ -275,6 +275,12 @@ pub fn generate_responses_to_subnet_calls(
         ))
     } else {
         let block_payload = block_payload.as_ref().as_data();
+
+        consensus_responses.append(&mut generate_responses_to_setup_initial_dkg_calls(
+            &block_payload.dkg.transcripts_for_remote_subnets,
+            log,
+        ));
+
         if let Some(payload) = &block_payload.idkg {
             consensus_responses.append(&mut generate_responses_to_signature_request_contexts(
                 payload,
