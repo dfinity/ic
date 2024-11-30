dfinity/ic

commit 2181ddf2a690ca0262d2d9d0511b093bfa350ece

refactor(cketh/ckerc20): mark older helper smart contracts as deprecated (#2747)
The helper smart contract supporting subaccount is the new preferred way
to do deposit, both of ETH and ERC-20. The 2 previous helper smart
contracts are therefore deprecated. Concretely,

1. The status (active or deprecated) of a helper smart contract is added
to the minter dashboard.
2. In case the address of the helper smart contract supporting
subaccount is set, the minter endpoints to retrieve the state of a
scraping (contract address and last scraped block number) for deposit of
ETH and ERC-20 will be that of the new helper smart contract supporting
subaccount.
3. The field `last_deposit_with_subaccount_scraped_block_number` was
removed. This is not a breaking change since it was never deployed in
production.

rs/ethereum/cketh/minter/cketh_minter.did
@@ -201,9 +201,6 @@ type MinterInfo = record {
     // Last scraped block number for logs of the ERC20 helper contract.
     last_erc20_scraped_block_number: opt nat;
 
-    // Last scraped block number for logs of the deposit with subaccount helper contract.
-    last_deposit_with_subaccount_scraped_block_number: opt nat;
-
     // Canister ID of the ckETH ledger.
     cketh_ledger_id: opt principal;
 

rs/ethereum/cketh/minter/src/endpoints.rs
@@ -76,7 +76,6 @@ pub struct MinterInfo {
     pub erc20_balances: Option<Vec<Erc20Balance>>,
     pub last_eth_scraped_block_number: Option<Nat>,
     pub last_erc20_scraped_block_number: Option<Nat>,
-    pub last_deposit_with_subaccount_scraped_block_number: Option<Nat>,
     pub cketh_ledger_id: Option<Principal>,
     pub evm_rpc_id: Option<Principal>,
 }

rs/ethereum/cketh/minter/src/main.rs
@@ -26,7 +26,7 @@ use ic_cketh_minter::logs::INFO;
 use ic_cketh_minter::memo::BurnMemo;
 use ic_cketh_minter::numeric::{Erc20Value, LedgerBurnIndex, Wei};
 use ic_cketh_minter::state::audit::{process_event, Event, EventType};
-use ic_cketh_minter::state::eth_logs_scraping::LogScrapingId;
+use ic_cketh_minter::state::eth_logs_scraping::{LogScrapingId, LogScrapingInfo};
 use ic_cketh_minter::state::transactions::{
     Erc20WithdrawalRequest, EthWithdrawalRequest, Reimbursed, ReimbursementIndex,
     ReimbursementRequest,
@@ -222,18 +222,18 @@ async fn get_minter_info() -> MinterInfo {
             (None, None)
         };
 
-        let eth_helper_contract_address = s
-            .log_scrapings
-            .contract_address(LogScrapingId::EthDepositWithoutSubaccount)
-            .map(|a| a.to_string());
+        let LogScrapingInfo {
+            eth_helper_contract_address,
+            last_eth_scraped_block_number,
+            erc20_helper_contract_address,
+            last_erc20_scraped_block_number,
+        } = s.log_scrapings.info();
+
         MinterInfo {
             minter_address: s.minter_address().map(|a| a.to_string()),
             smart_contract_address: eth_helper_contract_address.clone(),
             eth_helper_contract_address,
-            erc20_helper_contract_address: s
-                .log_scrapings
-                .contract_address(LogScrapingId::Erc20DepositWithoutSubaccount)
-                .map(|a| a.to_string()),
+            erc20_helper_contract_address,
             supported_ckerc20_tokens,
             minimum_withdrawal_amount: Some(s.cketh_minimum_withdrawal_amount.into()),
             ethereum_block_height: Some(s.ethereum_block_height.into()),
@@ -247,21 +247,8 @@ async fn get_minter_info() -> MinterInfo {
                 },
             ),
             erc20_balances,
-            last_eth_scraped_block_number: Some(
-                s.log_scrapings
-                    .last_scraped_block_number(LogScrapingId::EthDepositWithoutSubaccount)
-                    .into(),
-            ),
-            last_erc20_scraped_block_number: Some(
-                s.log_scrapings
-                    .last_scraped_block_number(LogScrapingId::Erc20DepositWithoutSubaccount)
-                    .into(),
-            ),
-            last_deposit_with_subaccount_scraped_block_number: Some(
-                s.log_scrapings
-                    .last_scraped_block_number(LogScrapingId::EthOrErc20DepositWithSubaccount)
-                    .into(),
-            ),
+            last_eth_scraped_block_number,
+            last_erc20_scraped_block_number,
             cketh_ledger_id: Some(s.cketh_ledger_id),
             evm_rpc_id: s.evm_rpc_id,
         }

rs/ethereum/cketh/minter/src/state/eth_logs_scraping/mod.rs
@@ -1,4 +1,8 @@
+#[cfg(test)]
+mod tests;
+
 use crate::numeric::BlockNumber;
+use candid::Nat;
 use ic_ethereum_types::Address;
 use std::collections::BTreeMap;
 use std::fmt::{Debug, Display, Formatter};
@@ -15,7 +19,10 @@ impl LogScrapings {
     pub fn new(last_scraped_block_number: BlockNumber) -> Self {
         let mut scrapings = BTreeMap::new();
         for id in LogScrapingId::iter() {
-            scrapings.insert(id, LogScrapingState::new(last_scraped_block_number));
+            scrapings.insert(
+                id,
+                LogScrapingState::new(last_scraped_block_number, id.status()),
+            );
         }
         Self { scrapings }
     }
@@ -55,6 +62,37 @@ impl LogScrapings {
             .get(&id)
             .expect("BUG: LogScrapings should contain all LogScrapingId")
     }
+
+    pub fn info(&self) -> LogScrapingInfo {
+        let to_info = |state: &LogScrapingState| {
+            let contract_address = state.contract_address().map(|a| a.to_string());
+            let last_scraped_block_number = Some(Nat::from(state.last_scraped_block_number()));
+            (contract_address, last_scraped_block_number)
+        };
+        let (
+            deposit_with_subaccount_contract_address,
+            deposit_with_subaccount_last_scraped_block_number,
+        ) = to_info(self.get(LogScrapingId::EthOrErc20DepositWithSubaccount));
+        if deposit_with_subaccount_contract_address.is_some() {
+            return LogScrapingInfo {
+                eth_helper_contract_address: deposit_with_subaccount_contract_address.clone(),
+                last_eth_scraped_block_number: deposit_with_subaccount_last_scraped_block_number
+                    .clone(),
+                erc20_helper_contract_address: deposit_with_subaccount_contract_address,
+                last_erc20_scraped_block_number: deposit_with_subaccount_last_scraped_block_number,
+            };
+        }
+        let (eth_helper_contract_address, last_eth_scraped_block_number) =
+            to_info(self.get(LogScrapingId::EthDepositWithoutSubaccount));
+        let (erc20_helper_contract_address, last_erc20_scraped_block_number) =
+            to_info(self.get(LogScrapingId::Erc20DepositWithoutSubaccount));
+        LogScrapingInfo {
+            eth_helper_contract_address,
+            last_eth_scraped_block_number,
+            erc20_helper_contract_address,
+            last_erc20_scraped_block_number,
+        }
+    }
 }
 
 #[derive(Clone, PartialEq, Copy, Debug, PartialOrd, Ord, Eq, EnumIter)]
@@ -63,6 +101,16 @@ pub enum LogScrapingId {
     EthDepositWithoutSubaccount,
     Erc20DepositWithoutSubaccount,
     EthOrErc20DepositWithSubaccount,
+}
+
+impl LogScrapingId {
+    fn status(&self) -> LogScrapingStatus {
+        match self {
+            LogScrapingId::EthDepositWithoutSubaccount => LogScrapingStatus::Deprecated,
+            LogScrapingId::Erc20DepositWithoutSubaccount => LogScrapingStatus::Deprecated,
+            LogScrapingId::EthOrErc20DepositWithSubaccount => LogScrapingStatus::Active,
+        }
+    }
 }
 
 impl Display for LogScrapingId {
@@ -82,17 +130,39 @@ pub enum LogScrapingStateError {
     InvalidContractAddress(String),
 }
 
+#[derive(Clone, Copy, PartialEq, Debug)]
+#[repr(u8)]
+pub enum LogScrapingStatus {
+    Active,
+    Deprecated,
+}
+
+impl Display for LogScrapingStatus {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LogScrapingStatus::Active => {
+                write!(f, "active 🟢")
+            }
+            LogScrapingStatus::Deprecated => {
+                write!(f, "deprecated 🟠")
+            }
+        }
+    }
+}
+
 #[derive(Clone, PartialEq, Debug)]
 pub struct LogScrapingState {
     contract_address: Option<Address>,
     last_scraped_block_number: BlockNumber,
+    status: LogScrapingStatus,
 }
 
 impl LogScrapingState {
-    pub fn new(last_scraped_block_number: BlockNumber) -> Self {
+    pub fn new(last_scraped_block_number: BlockNumber, status: LogScrapingStatus) -> Self {
         Self {
             contract_address: None,
             last_scraped_block_number,
+            status,
         }
     }
 
@@ -120,4 +190,16 @@ impl LogScrapingState {
     pub fn contract_address(&self) -> Option<&Address> {
         self.contract_address.as_ref()
     }
+
+    pub fn status(&self) -> LogScrapingStatus {
+        self.status
+    }
+}
+
+#[derive(Clone, PartialEq, Debug, Default)]
+pub struct LogScrapingInfo {
+    pub eth_helper_contract_address: Option<String>,
+    pub last_eth_scraped_block_number: Option<Nat>,
+    pub erc20_helper_contract_address: Option<String>,
+    pub last_erc20_scraped_block_number: Option<Nat>,
 }

rs/ethereum/cketh/minter/src/state/eth_logs_scraping/tests.rs
@@ -1,0 +1,71 @@
+use crate::state::eth_logs_scraping::{LogScrapingId, LogScrapingInfo, LogScrapings};
+
+#[test]
+fn should_use_info_from_deposit_with_subaccount_when_contract_address_present() {
+    const LAST_SCRAPED_BLOCK_NUMBER: u32 = 21_235_426_u32;
+    const ETH_HELPER_SMART_CONTRACT: &str = "0x7574eB42cA208A4f6960ECCAfDF186D627dCC175";
+    const ERC20_HELPER_SMART_CONTRACT: &str = "0x6abDA0438307733FC299e9C229FD3cc074bD8cC0";
+    const DEPOSIT_WITH_SUBACCOUNT_HELPER_SMART_CONTRACT: &str =
+        "0x2D39863d30716aaf2B7fFFd85Dd03Dda2BFC2E38";
+
+    let mut scrapings = LogScrapings::new(LAST_SCRAPED_BLOCK_NUMBER.into());
+
+    assert_eq!(
+        scrapings.info(),
+        LogScrapingInfo {
+            last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
+            last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
+            ..Default::default()
+        }
+    );
+
+    scrapings
+        .set_contract_address(
+            LogScrapingId::EthDepositWithoutSubaccount,
+            ETH_HELPER_SMART_CONTRACT.parse().unwrap(),
+        )
+        .unwrap();
+    scrapings
+        .set_contract_address(
+            LogScrapingId::Erc20DepositWithoutSubaccount,
+            ERC20_HELPER_SMART_CONTRACT.parse().unwrap(),
+        )
+        .unwrap();
+
+    assert_eq!(
+        scrapings.info(),
+        LogScrapingInfo {
+            eth_helper_contract_address: Some(ETH_HELPER_SMART_CONTRACT.to_string()),
+            last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
+            erc20_helper_contract_address: Some(ERC20_HELPER_SMART_CONTRACT.to_string()),
+            last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
+        }
+    );
+
+    scrapings
+        .set_contract_address(
+            LogScrapingId::EthOrErc20DepositWithSubaccount,
+            DEPOSIT_WITH_SUBACCOUNT_HELPER_SMART_CONTRACT
+                .parse()
+                .unwrap(),
+        )
+        .unwrap();
+    scrapings.set_last_scraped_block_number(
+        LogScrapingId::EthOrErc20DepositWithSubaccount,
+        (LAST_SCRAPED_BLOCK_NUMBER + 1).into(),
+    );
+
+    assert_eq!(
+        scrapings.info(),
+        LogScrapingInfo {
+            eth_helper_contract_address: Some(
+                DEPOSIT_WITH_SUBACCOUNT_HELPER_SMART_CONTRACT.to_string()
+            ),
+            last_eth_scraped_block_number: Some((LAST_SCRAPED_BLOCK_NUMBER + 1).into()),
+            erc20_helper_contract_address: Some(
+                DEPOSIT_WITH_SUBACCOUNT_HELPER_SMART_CONTRACT.to_string()
+            ),
+            last_erc20_scraped_block_number: Some((LAST_SCRAPED_BLOCK_NUMBER + 1).into()),
+        }
+    );
+}

rs/ethereum/cketh/minter/tests/ckerc20.rs
@@ -1690,9 +1690,6 @@ fn should_retrieve_minter_info() {
             erc20_balances: Some(erc20_balances),
             last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
             last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
-            last_deposit_with_subaccount_scraped_block_number: Some(
-                LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()
-            ),
             cketh_ledger_id: Some(ckerc20.cketh_ledger_id()),
             evm_rpc_id: ckerc20.cketh.evm_rpc_id.map(Principal::from),
         }

rs/ethereum/cketh/minter/tests/cketh.rs
@@ -1096,9 +1096,6 @@ fn should_retrieve_minter_info() {
             erc20_balances: None,
             last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
             last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
-            last_deposit_with_subaccount_scraped_block_number: Some(
-                LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()
-            ),
             cketh_ledger_id: Some(cketh.ledger_id.into()),
             evm_rpc_id: cketh.evm_rpc_id.map(Principal::from),
         }

rs/ethereum/cketh/minter/src/dashboard/tests.rs
@@ -1293,7 +1293,7 @@ mod assertions {
         pub fn has_last_synced_block_href(&self, id: LogScrapingId, expected_href: &str) -> &Self {
             self.has_href_value(
                 &format!(
-                    "#helper-smart-contract-{} > td:nth-child(3) > code > a",
+                    "#helper-smart-contract-{} > td:nth-child(4) > code > a",
                     lower_alphanumeric(id).unwrap()
                 ),
                 expected_href,

commit 5ce01d0a871d5162d0b2ff0b585d71ef2e644ac9

fix(cketh): Undo breaking change in `get_minter_info` (#2907)
Fix an undesired breaking changed introduced by #2747 :

1. The fields `eth_helper_contract_address` and
`erc20_helper_contract_address` in `get_minter_info` were wrongly reused
to point to the new helper smart contract
[0x18901044688D3756C35Ed2b36D93e6a5B8e00E68](https://etherscan.io/address/0x18901044688D3756C35Ed2b36D93e6a5B8e00E68)
that supports deposit with subaccounts and that was added as part of
proposal
[134264](https://dashboard.internetcomputer.org/proposal/134264).
2. This broke clients that relied on that information to make deposit of
ETH or ERC-20 because the new helper smart contract has a different ABI.
This is visible by such a
[transaction](https://etherscan.io/tx/0x0968b25814221719bf966cf4bbd2de8290ed2ab42c049d451d64e46812d1574e),
where the transaction tried to call the method `deposit` (`0xb214faa5`)
that does exist on the [deprecated ETH helper smart
contract](https://etherscan.io/address/0x7574eB42cA208A4f6960ECCAfDF186D627dCC175)
but doesn't on the new contract (it should have been `depositEth`
(`0x17c819c4`)).
3. The fix simply consists in reverting the changes regarding the values
of the fields `eth_helper_contract_address` and
`erc20_helper_contract_address` in `get_minter_info` (so that they point
back to
[0x7574eB42cA208A4f6960ECCAfDF186D627dCC175](https://etherscan.io/address/0x7574eB42cA208A4f6960ECCAfDF186D627dCC175)
and
[0x6abDA0438307733FC299e9C229FD3cc074bD8cC0](https://etherscan.io/address/0x6abDA0438307733FC299e9C229FD3cc074bD8cC0),
respectively) and adding new fields to contain the state of the log
scraping (address and last scraped block number) for the new helper
smart contract.

---------

Co-authored-by: Thomas Locher <thomas.locher@dfinity.org>

rs/ethereum/cketh/minter/cketh_minter.did
@@ -172,6 +172,9 @@ type MinterInfo = record {
     // Address of the ERC20 helper smart contract
     erc20_helper_contract_address: opt text;
 
+    // Address of the ETH or ERC20 deposit with subaccount helper smart contract.
+    deposit_with_subaccount_helper_contract_address: opt text;
+
     // Information of supported ERC20 tokens.
     supported_ckerc20_tokens: opt vec CkErc20Token;
 
@@ -200,6 +203,9 @@ type MinterInfo = record {
 
     // Last scraped block number for logs of the ERC20 helper contract.
     last_erc20_scraped_block_number: opt nat;
+
+    // Last scraped block number for logs of the deposit with subaccount helper contract.
+    last_deposit_with_subaccount_scraped_block_number: opt nat;
 
     // Canister ID of the ckETH ledger.
     cketh_ledger_id: opt principal;

rs/ethereum/cketh/minter/src/endpoints.rs
@@ -67,6 +67,7 @@ pub struct MinterInfo {
     pub smart_contract_address: Option<String>,
     pub eth_helper_contract_address: Option<String>,
     pub erc20_helper_contract_address: Option<String>,
+    pub deposit_with_subaccount_helper_contract_address: Option<String>,
     pub supported_ckerc20_tokens: Option<Vec<CkErc20Token>>,
     pub minimum_withdrawal_amount: Option<Nat>,
     pub ethereum_block_height: Option<CandidBlockTag>,
@@ -76,6 +77,7 @@ pub struct MinterInfo {
     pub erc20_balances: Option<Vec<Erc20Balance>>,
     pub last_eth_scraped_block_number: Option<Nat>,
     pub last_erc20_scraped_block_number: Option<Nat>,
+    pub last_deposit_with_subaccount_scraped_block_number: Option<Nat>,
     pub cketh_ledger_id: Option<Principal>,
     pub evm_rpc_id: Option<Principal>,
 }

rs/ethereum/cketh/minter/src/main.rs
@@ -227,6 +227,8 @@ async fn get_minter_info() -> MinterInfo {
             last_eth_scraped_block_number,
             erc20_helper_contract_address,
             last_erc20_scraped_block_number,
+            deposit_with_subaccount_helper_contract_address,
+            last_deposit_with_subaccount_scraped_block_number,
         } = s.log_scrapings.info();
 
         MinterInfo {
@@ -234,6 +236,7 @@ async fn get_minter_info() -> MinterInfo {
             smart_contract_address: eth_helper_contract_address.clone(),
             eth_helper_contract_address,
             erc20_helper_contract_address,
+            deposit_with_subaccount_helper_contract_address,
             supported_ckerc20_tokens,
             minimum_withdrawal_amount: Some(s.cketh_minimum_withdrawal_amount.into()),
             ethereum_block_height: Some(s.ethereum_block_height.into()),
@@ -249,6 +252,7 @@ async fn get_minter_info() -> MinterInfo {
             erc20_balances,
             last_eth_scraped_block_number,
             last_erc20_scraped_block_number,
+            last_deposit_with_subaccount_scraped_block_number,
             cketh_ledger_id: Some(s.cketh_ledger_id),
             evm_rpc_id: s.evm_rpc_id,
         }

rs/ethereum/cketh/minter/src/state/eth_logs_scraping/mod.rs
@@ -69,28 +69,21 @@ impl LogScrapings {
             let last_scraped_block_number = Some(Nat::from(state.last_scraped_block_number()));
             (contract_address, last_scraped_block_number)
         };
-        let (
-            deposit_with_subaccount_contract_address,
-            deposit_with_subaccount_last_scraped_block_number,
-        ) = to_info(self.get(LogScrapingId::EthOrErc20DepositWithSubaccount));
-        if deposit_with_subaccount_contract_address.is_some() {
-            return LogScrapingInfo {
-                eth_helper_contract_address: deposit_with_subaccount_contract_address.clone(),
-                last_eth_scraped_block_number: deposit_with_subaccount_last_scraped_block_number
-                    .clone(),
-                erc20_helper_contract_address: deposit_with_subaccount_contract_address,
-                last_erc20_scraped_block_number: deposit_with_subaccount_last_scraped_block_number,
-            };
-        }
         let (eth_helper_contract_address, last_eth_scraped_block_number) =
             to_info(self.get(LogScrapingId::EthDepositWithoutSubaccount));
         let (erc20_helper_contract_address, last_erc20_scraped_block_number) =
             to_info(self.get(LogScrapingId::Erc20DepositWithoutSubaccount));
+        let (
+            deposit_with_subaccount_helper_contract_address,
+            last_deposit_with_subaccount_scraped_block_number,
+        ) = to_info(self.get(LogScrapingId::EthOrErc20DepositWithSubaccount));
         LogScrapingInfo {
             eth_helper_contract_address,
             last_eth_scraped_block_number,
             erc20_helper_contract_address,
             last_erc20_scraped_block_number,
+            deposit_with_subaccount_helper_contract_address,
+            last_deposit_with_subaccount_scraped_block_number,
         }
     }
 }
@@ -202,4 +195,6 @@ pub struct LogScrapingInfo {
     pub last_eth_scraped_block_number: Option<Nat>,
     pub erc20_helper_contract_address: Option<String>,
     pub last_erc20_scraped_block_number: Option<Nat>,
+    pub deposit_with_subaccount_helper_contract_address: Option<String>,
+    pub last_deposit_with_subaccount_scraped_block_number: Option<Nat>,
 }

rs/ethereum/cketh/minter/src/state/eth_logs_scraping/tests.rs
@@ -1,7 +1,7 @@
 use crate::state::eth_logs_scraping::{LogScrapingId, LogScrapingInfo, LogScrapings};
 
 #[test]
-fn should_use_info_from_deposit_with_subaccount_when_contract_address_present() {
+fn should_not_change_other_field_when_deposit_with_subaccount_present() {
     const LAST_SCRAPED_BLOCK_NUMBER: u32 = 21_235_426_u32;
     const ETH_HELPER_SMART_CONTRACT: &str = "0x7574eB42cA208A4f6960ECCAfDF186D627dCC175";
     const ERC20_HELPER_SMART_CONTRACT: &str = "0x6abDA0438307733FC299e9C229FD3cc074bD8cC0";
@@ -15,6 +15,9 @@ fn should_use_info_from_deposit_with_subaccount_when_contract_address_present() 
         LogScrapingInfo {
             last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
             last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
+            last_deposit_with_subaccount_scraped_block_number: Some(
+                LAST_SCRAPED_BLOCK_NUMBER.into()
+            ),
             ..Default::default()
         }
     );
@@ -32,13 +35,18 @@ fn should_use_info_from_deposit_with_subaccount_when_contract_address_present() 
         )
         .unwrap();
 
+    let info_before = scrapings.info();
     assert_eq!(
-        scrapings.info(),
+        info_before,
         LogScrapingInfo {
             eth_helper_contract_address: Some(ETH_HELPER_SMART_CONTRACT.to_string()),
             last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
             erc20_helper_contract_address: Some(ERC20_HELPER_SMART_CONTRACT.to_string()),
             last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER.into()),
+            deposit_with_subaccount_helper_contract_address: None,
+            last_deposit_with_subaccount_scraped_block_number: Some(
+                LAST_SCRAPED_BLOCK_NUMBER.into()
+            ),
         }
     );
 
@@ -58,14 +66,13 @@ fn should_use_info_from_deposit_with_subaccount_when_contract_address_present() 
     assert_eq!(
         scrapings.info(),
         LogScrapingInfo {
-            eth_helper_contract_address: Some(
+            deposit_with_subaccount_helper_contract_address: Some(
                 DEPOSIT_WITH_SUBACCOUNT_HELPER_SMART_CONTRACT.to_string()
             ),
-            last_eth_scraped_block_number: Some((LAST_SCRAPED_BLOCK_NUMBER + 1).into()),
-            erc20_helper_contract_address: Some(
-                DEPOSIT_WITH_SUBACCOUNT_HELPER_SMART_CONTRACT.to_string()
+            last_deposit_with_subaccount_scraped_block_number: Some(
+                (LAST_SCRAPED_BLOCK_NUMBER + 1).into()
             ),
-            last_erc20_scraped_block_number: Some((LAST_SCRAPED_BLOCK_NUMBER + 1).into()),
+            ..info_before
         }
     );
 }

rs/ethereum/cketh/minter/tests/ckerc20.rs
@@ -25,7 +25,8 @@ use ic_cketh_test_utils::{
     format_ethereum_address_to_eip_55, CkEthSetup, CKETH_MINIMUM_WITHDRAWAL_AMOUNT,
     DEFAULT_DEPOSIT_BLOCK_NUMBER, DEFAULT_DEPOSIT_FROM_ADDRESS, DEFAULT_DEPOSIT_LOG_INDEX,
     DEFAULT_DEPOSIT_TRANSACTION_HASH, DEFAULT_ERC20_DEPOSIT_LOG_INDEX,
-    DEFAULT_ERC20_DEPOSIT_TRANSACTION_HASH, DEFAULT_USER_SUBACCOUNT, EFFECTIVE_GAS_PRICE,
+    DEFAULT_ERC20_DEPOSIT_TRANSACTION_HASH, DEFAULT_USER_SUBACCOUNT,
+    DEPOSIT_WITH_SUBACCOUNT_HELPER_CONTRACT_ADDRESS, EFFECTIVE_GAS_PRICE,
     ERC20_HELPER_CONTRACT_ADDRESS, ETH_HELPER_CONTRACT_ADDRESS, GAS_USED,
     LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL, MINTER_ADDRESS,
 };
@@ -1681,6 +1682,7 @@ fn should_retrieve_minter_info() {
             erc20_helper_contract_address: Some(format_ethereum_address_to_eip_55(
                 ERC20_HELPER_CONTRACT_ADDRESS
             )),
+            deposit_with_subaccount_helper_contract_address: None,
             supported_ckerc20_tokens: Some(supported_ckerc20_tokens),
             minimum_withdrawal_amount: Some(Nat::from(CKETH_MINIMUM_WITHDRAWAL_AMOUNT)),
             ethereum_block_height: Some(Finalized),
@@ -1690,10 +1692,52 @@ fn should_retrieve_minter_info() {
             erc20_balances: Some(erc20_balances),
             last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
             last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
+            last_deposit_with_subaccount_scraped_block_number: Some(
+                LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()
+            ),
             cketh_ledger_id: Some(ckerc20.cketh_ledger_id()),
             evm_rpc_id: ckerc20.cketh.evm_rpc_id.map(Principal::from),
         }
     );
+}
+
+// If a contract needs to be changed, the contract ABI must be preserved,
+// otherwise this will be a breaking change for clients (e.g. front-ends).
+#[test]
+#[allow(deprecated)]
+fn should_not_change_address_of_other_contracts_when_adding_new_contract() {
+    let ckerc20 = CkErc20Setup::default();
+    let info_at_start = ckerc20.cketh.get_minter_info();
+    assert_eq!(
+        info_at_start,
+        MinterInfo {
+            smart_contract_address: Some(format_ethereum_address_to_eip_55(
+                ETH_HELPER_CONTRACT_ADDRESS
+            )),
+            eth_helper_contract_address: Some(format_ethereum_address_to_eip_55(
+                ETH_HELPER_CONTRACT_ADDRESS
+            )),
+            erc20_helper_contract_address: Some(format_ethereum_address_to_eip_55(
+                ERC20_HELPER_CONTRACT_ADDRESS
+            )),
+            ..info_at_start.clone()
+        }
+    );
+
+    let ckerc20 = ckerc20.add_support_for_subaccount();
+
+    assert_eq!(
+        ckerc20.get_minter_info(),
+        MinterInfo {
+            deposit_with_subaccount_helper_contract_address: Some(
+                format_ethereum_address_to_eip_55(DEPOSIT_WITH_SUBACCOUNT_HELPER_CONTRACT_ADDRESS)
+            ),
+            last_deposit_with_subaccount_scraped_block_number: Some(
+                LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()
+            ),
+            ..info_at_start
+        }
+    )
 }
 
 #[test]

rs/ethereum/cketh/minter/tests/cketh.rs
@@ -1087,6 +1087,7 @@ fn should_retrieve_minter_info() {
                 ETH_HELPER_CONTRACT_ADDRESS
             )),
             erc20_helper_contract_address: None,
+            deposit_with_subaccount_helper_contract_address: None,
             supported_ckerc20_tokens: None,
             minimum_withdrawal_amount: Some(Nat::from(CKETH_MINIMUM_WITHDRAWAL_AMOUNT)),
             ethereum_block_height: Some(Finalized),
@@ -1096,6 +1097,9 @@ fn should_retrieve_minter_info() {
             erc20_balances: None,
             last_eth_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
             last_erc20_scraped_block_number: Some(LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()),
+            last_deposit_with_subaccount_scraped_block_number: Some(
+                LAST_SCRAPED_BLOCK_NUMBER_AT_INSTALL.into()
+            ),
             cketh_ledger_id: Some(cketh.ledger_id.into()),
             evm_rpc_id: cketh.evm_rpc_id.map(Principal::from),
         }