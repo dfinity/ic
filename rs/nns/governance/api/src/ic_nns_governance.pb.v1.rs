b2225577e114908f7b22a74509c16d63c713e43d
Dragoljub Duric <dragoljub.duric@dfinity.org>
2024-12-02 15:41:11 +0000

    Fix unit tests.

--- rs/nns/governance/api/src/ic_nns_governance.pb.v1.rs
+++ rs/nns/governance/api/src/ic_nns_governance.pb.v1.rs
@@ -2571,7 +2571,7 @@ pub mod update_canister_settings {
         #[prost(uint64, optional, tag = "6")]
         pub wasm_memory_limit: Option<u64>,
         #[prost(uint64, optional, tag = "7")]
-        pub wasm_memory_threshold: ::core::option::Option<u64>,
+        pub wasm_memory_threshold: Option<u64>,
     }
     /// Log visibility of a canister.
     #[derive(
