dbcc53bd36f88f6bb09fb83fd72a0baa871b5f10
David Frank <david.frank@dfinity.org>
2024-12-02 22:18:03 +0000

    SEV benchmark

--- rs/ic_os/verification/src/main.rs
+++ rs/ic_os/verification/src/main.rs
@@ -1,7 +1,24 @@
 use sev::firmware::guest::Firmware;
+use std::time::Instant;
 
 fn main() {
-    let mut firmware = Firmware::open().unwrap();
-    let report = firmware.get_report(None, Some([1; 64]), None).unwrap();
-    println!("{report:?}");
+    let start = Instant::now();
+    std::thread::scope(|scope| {
+        for _ in 0..64 {
+            scope.spawn(|| {
+                let mut firmware = Firmware::open().unwrap();
+                let mut v = Vec::with_capacity(1000);
+                for _ in 0..100 {
+                    v.push(
+                        firmware
+                            .get_report(Some(1), Some([1; 64]), Some(1))
+                            .unwrap(),
+                    );
+                }
+                println!("{:?} finished", std::thread::current().id());
+                std::hint::black_box(v);
+            });
+        }
+    });
+    println!("{:?}", start.elapsed());
 }
