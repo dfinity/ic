f27312a4f6c5c37b4e8a81d9439c0164bdebe180
Andrew Battat <andrew.battat@dfinity.org>
2024-12-03 21:32:55 +0000

    Replace HwAddr with MacAddr6 in farm

--- rs/tests/driver/src/driver/nested.rs
+++ rs/tests/driver/src/driver/nested.rs
@@ -16,7 +16,8 @@ use anyhow::{bail, Result};
 use async_trait::async_trait;
 use config_types::DeploymentEnvironment;
 use deterministic_ips::node_type::NodeType;
-use deterministic_ips::{calculate_deterministic_mac, HwAddr, IpVariant, MacAddr6Ext};
+use deterministic_ips::{calculate_deterministic_mac, IpVariant, MacAddr6Ext};
+use macaddr::MacAddr6;
 use serde::{Deserialize, Serialize};
 use slog::info;
 use ssh2::Session;
@@ -126,7 +127,7 @@ impl NestedVms for TestEnv {
 
     fn write_nested_vm(&self, name: &str, vm: &AllocatedVm) -> Result<()> {
         // Remap the IPv6 addresses based on their deterministic IPs
-        let seed_mac = vm.mac6.parse::<HwAddr>().unwrap();
+        let seed_mac = vm.mac6.parse::<MacAddr6>().unwrap();
         let old_ip = vm.ipv6;
 
         // TODO: We transform the IPv6 to get this information, but it could be
