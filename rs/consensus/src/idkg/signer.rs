e2405a55a8cac556dbf5a9e2fbfd5e09d3976d29
Leo Eichhorn <leo.eichhorn@dfinity.org>
2024-12-03 11:02:13 +0000

    clippy

--- rs/consensus/src/idkg/signer.rs
+++ rs/consensus/src/idkg/signer.rs
@@ -47,6 +47,7 @@ use super::utils::{build_signature_inputs, update_purge_height};
 
 #[derive(Clone, Debug)]
 #[allow(dead_code)]
+#[allow(clippy::large_enum_variant)]
 enum CreateSigShareError {
     Ecdsa(ThresholdEcdsaCreateSigShareError),
     Schnorr(ThresholdSchnorrCreateSigShareError),
