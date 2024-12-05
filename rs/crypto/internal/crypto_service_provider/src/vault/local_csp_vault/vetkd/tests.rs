07239e28607d5c5cb226609e7cbb0d8eb747d20f
Franz-Stefan Preiss <franzstefan@dfinity.org>
2024-12-05 11:24:28 +0000

    feat(crypto): CRP-2649 add vault method to create vetKD key share

---
+++ rs/crypto/internal/crypto_service_provider/src/vault/local_csp_vault/vetkd/tests.rs
@@ -0,0 +1,27 @@
+#[test]
+fn should_correctly_create_encrypted_vetkd_key_share() {
+    // Here we only test with a single test vector: an extensive test with the
+    // entire test vector suite is done at the crypto lib level.
+
+    todo!()
+}
+
+#[test]
+fn should_fail_to_create_key_share_with_invalid_master_public_key() {
+    todo!()
+}
+
+#[test]
+fn should_fail_to_create_key_share_with_invalid_encryption_public_key() {
+    todo!()
+}
+
+#[test]
+fn should_fail_to_create_key_share_if_key_is_missing_in_secret_key_store() {
+    todo!()
+}
+
+#[test]
+fn should_fail_to_create_key_share_if_key_in_secret_key_store_has_wrong_type() {
+    todo!()
+}
--- rs/types/types/src/crypto/hash/domain_separator.rs
+++ rs/types/types/src/crypto/hash/domain_separator.rs
@@ -54,6 +54,7 @@ pub enum DomainSeparator {
     IDkgTranscript,
     EcdsaSigShare,
     SchnorrSigShare,
+    VetKdEncryptedKeyShareContent,
     IDkgComplaintContent,
     SignedIDkgComplaint,
     IDkgOpeningContent,
@@ -113,6 +114,9 @@ impl DomainSeparator {
             DomainSeparator::IDkgTranscript => "ic-idkg-transcript-domain",
             DomainSeparator::EcdsaSigShare => "ic-threshold-ecdsa-sig-share-domain",
             DomainSeparator::SchnorrSigShare => "ic-threshold-schnorr-sig-share-domain",
+            DomainSeparator::VetKdEncryptedKeyShareContent => {
+                "ic-vetkd-encrypted-key-share-content-domain"
+            }
             DomainSeparator::IDkgComplaintContent => "ic-threshold-ecdsa-complaint-content-domain",
             DomainSeparator::SignedIDkgComplaint => "ic-threshold-ecdsa-complaint-domain",
             DomainSeparator::IDkgOpeningContent => "ic-threshold-ecdsa-opening-content-domain",
