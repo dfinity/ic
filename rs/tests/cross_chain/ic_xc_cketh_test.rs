767a768a4ddc29a95059243811b254939308bd95
Grégory Demay <gregory.demay@dfinity.org>
2024-12-09 17:16:30 +0000

    XC-256 curl to anvil poc

--- rs/tests/cross_chain/ic_xc_cketh_test.rs
+++ rs/tests/cross_chain/ic_xc_cketh_test.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, bail, Context, Result};
 use ic_registry_subnet_type::SubnetType;
 use ic_system_test_driver::{
     driver::{
@@ -12,8 +12,12 @@ use ic_system_test_driver::{
         universal_vm::{UniversalVm, UniversalVms},
     },
     systest,
+    util::block_on,
 };
+use reqwest::Client;
 use std::env;
+
+const UNIVERSAL_VM_NAME: &str = "foundry";
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
@@ -39,11 +43,28 @@ fn setup_with_system_and_application_subnets(env: TestEnv) {
 fn ic_xc_cketh_test(env: TestEnv) {
     let logger = env.logger();
     let topology_snapshot = env.topology_snapshot();
+    let docker_host = env.get_deployed_universal_vm(UNIVERSAL_VM_NAME).unwrap();
+    let docker_host_ip = docker_host.get_vm().unwrap().ipv6;
+    let client = Client::new();
+
+    // create the zone if it doesn't exist yet
+    let url = format!("http://[{:?}]:{:?}", docker_host_ip, 8545);
+
+    block_on(async {
+        let response = client
+            .get(&url)
+            .send()
+            .await
+            .expect("failed to get the zone");
+
+        let response_json: serde_json::Value = response
+            .json()
+            .await
+            .expect("failed to decode the response");
+    });
 }
 
 fn foundry_config(env: TestEnv) {
-    const UNIVERSAL_VM_NAME: &str = "foundry";
-
     UniversalVm::new(String::from(UNIVERSAL_VM_NAME))
         .with_config_img(get_dependency_path(
             env::var("CKETH_UVM_CONFIG_PATH").expect("CKETH_UVM_CONFIG_PATH not set"),
@@ -62,6 +83,8 @@ fn foundry_config(env: TestEnv) {
                 r#"
 # Run nginx auto proxy
 docker load -i /config/foundry.tar
+docker run --detach --rm --name foundry -p 8545:8545 foundry:latest "anvil --host 0.0.0.0"
+docker logs foundry
 "#
             ))
             .unwrap()
