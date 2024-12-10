cff6e6cd6a64401e8e95be21d88330fd3a077bac
Rostislav Rumenov <rostislav.rumenov@dfinity.org>
2024-12-10 12:11:14 +0000

    .

--- rs/ingress_manager/benches/build_payload.rs
+++ rs/ingress_manager/benches/build_payload.rs
@@ -30,7 +30,7 @@ use ic_registry_client::client::RegistryClientImpl;
 use ic_registry_keys::make_subnet_record_key;
 use ic_registry_proto_data_provider::ProtoRegistryDataProvider;
 use ic_test_utilities::{
-    artifact_pool_config::with_test_pool_config, crypto::temp_crypto_component_with_fake_registry,
+    artifact_pool_config::with_test_pool_config,
     cycles_account_manager::CyclesAccountManagerBuilder,
 };
 use ic_test_utilities_registry::test_subnet_record;
