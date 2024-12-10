05306bf2ee99cfe4890659d5458b6c7c59271d15
Grégory Demay <gregory.demay@dfinity.org>
2024-12-10 14:41:12 +0000

    XC-256: deploy and call smart contract

--- rs/tests/cross_chain/ic_xc_cketh_test.rs
+++ rs/tests/cross_chain/ic_xc_cketh_test.rs
@@ -49,6 +49,7 @@ fn setup_anvil(env: &TestEnv) {
         .with_config_img(get_dependency_path(
             env::var("CKETH_UVM_CONFIG_PATH").expect("CKETH_UVM_CONFIG_PATH not set"),
         ))
+        .enable_ipv4() //forge needs to download the version of the solidity compiler indicated in the smart contracts that are being deployed
         .start(&env)
         .expect("failed to setup universal VM");
 
@@ -97,14 +98,22 @@ fn ic_xc_cketh_test(env: TestEnv) {
             json!({"jsonrpc":"2.0","id":1,"result":"0x0"})
         )
     });
-    println!(
-        "{}",
-        deploy_smart_contract(
+
+    let minter_address = "0xb25eA1D493B49a1DeD42aC5B1208cC618f9A9B80";
+
+    let eth_deposit_helper_contract_address = deploy_smart_contract(
+        &docker_host,
+        "EthDepositHelper.sol",
+        "CkEthDeposit",
+        minter_address,
+    );
+    assert_eq!(
+        call_smart_contract(
             &docker_host,
-            "EthDepositHelper.sol",
-            "CkEthDeposit",
-            "0xb25eA1D493B49a1DeD42aC5B1208cC618f9A9B80"
-        )
+            &eth_deposit_helper_contract_address,
+            "getMinterAddress()(address)"
+        ),
+        minter_address
     );
 }
 
@@ -114,5 +123,19 @@ fn deploy_smart_contract(
     contract_name: &str,
     constructor_args: &str,
 ) -> String {
-    foundry.block_on_bash_script(&format!(r#"docker run --net {DOCKER_NETWORK_NAME} -it --rm -v /config/{filename}:/contracts/{filename} foundry "forge create --rpc-url http://anvil:{FOUNDRY_PORT} --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 /contracts/{filename}:{contract_name} --constructor-args {constructor_args}""#)).unwrap()
+    let json_output = foundry.block_on_bash_script(&format!(r#"docker run --net {DOCKER_NETWORK_NAME} --rm -v /config/{filename}:/contracts/{filename} foundry "forge create --json --rpc-url http://anvil:{FOUNDRY_PORT} --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 /contracts/{filename}:{contract_name} --constructor-args {constructor_args}""#)).unwrap();
+    println!(
+        "Deployed {filename} with constructor args {constructor_args}: {}",
+        json_output
+    );
+    let parsed_output: serde_json::Value = serde_json::from_str(&json_output).unwrap();
+    parsed_output["deployedTo"].as_str().unwrap().to_string()
+}
+
+fn call_smart_contract(
+    foundry: &DeployedUniversalVm,
+    contract_address: &str,
+    method: &str,
+) -> String {
+    foundry.block_on_bash_script(&format!(r#"docker run --net {DOCKER_NETWORK_NAME} --rm foundry "cast call {contract_address} '{method}' --rpc-url http://anvil:{FOUNDRY_PORT}""#)).unwrap().trim().to_string()
 }
