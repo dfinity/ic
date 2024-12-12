03f2513b73183282cb25e8767ef9e87d39f9fe47
Christian Stieger <christian.stieger@dfinity.org>
2024-12-12 15:39:33 +0000

    .

--- rs/messaging/src/routing/stream_handler.rs
+++ rs/messaging/src/routing/stream_handler.rs
@@ -25,7 +25,7 @@ use ic_replicated_state::{
 use ic_types::{
     messages::{
         Payload, RejectContext, Request, RequestOrResponse, Response,
-        MAX_INTER_CANISTER_PAYLOAD_IN_BYTES_U64, MAX_RESPONSE_COUNT_BYTES,
+        MAX_INTER_CANISTER_PAYLOAD_IN_BYTES_U64, MAX_RESPONSE_COUNT_BYTES, NO_DEADLINE,
     },
     xnet::{RejectReason, RejectSignal, StreamIndex, StreamIndexedQueue, StreamSlice},
     CanisterId, SubnetId,
@@ -842,8 +842,19 @@ impl StreamHandlerImpl {
                                 );
                                 return Some((reason, msg));
                             }
+                            RequestOrResponse::Response(response)
+                                if response.deadline != NO_DEADLINE
+                                    && matches!(
+                                        err,
+                                        StateError::CanisterNotFound(_) | StateError::CanisterStopped(_)
+                                    ) =>
+                            {
+                                // These are expected edge cases for best effort responses;
+                                // silently drop them.
+                            }
                             RequestOrResponse::Response(response) => {
-                                // Critical error, responses should always be inducted successfully.
+                                // Critical error, guaranteed responses (and best-effort responses passing
+                                // the exceptions above) should always be inducted successfully.
                                 error!(
                                     self.log,
                                     "{}: Inducting response failed: {}\n{:?}",
