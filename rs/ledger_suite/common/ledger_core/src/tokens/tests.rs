e6acc169f6eaa5b97e19401d768b42e4a321a76f
Maciej Modelski <maciej.modelski@dfinity.org>
2024-12-12 10:54:01 +0000

    small refactor

--- rs/ledger_suite/common/ledger_core/src/tokens/tests.rs
+++ rs/ledger_suite/common/ledger_core/src/tokens/tests.rs
@@ -5,9 +5,7 @@ use proptest::prelude::{any, prop_assert_eq, proptest};
 #[test]
 fn timestamp_serialization() {
     proptest!(|(e8s in any::<u64>())| {
-        let tokens = Tokens {
-            e8s,
-        };
+        let tokens = Tokens { e8s };
         let new_tokens = Tokens::from_bytes(tokens.to_bytes());
         prop_assert_eq!(new_tokens, tokens);
     })
