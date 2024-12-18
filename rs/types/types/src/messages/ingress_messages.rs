6350684695c4739160e342fd6c2832ba6c5cba21
Jack Lloyd <jack.lloyd@dfinity.org>
2024-12-18 14:22:36 -0500

    missing add

--- rs/types/types/src/messages/ingress_messages.rs
+++ rs/types/types/src/messages/ingress_messages.rs
@@ -282,6 +282,10 @@ impl SignedIngress {
         self.signed.content()
     }
 
+    pub fn signed(&self) -> &HttpRequest<SignedIngressContent> {
+        &self.signed
+    }
+
     pub fn authentication(&self) -> &Authentication {
         self.signed.authentication()
     }
