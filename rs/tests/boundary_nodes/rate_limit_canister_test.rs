39d7e4d2c63e4999de3d47d9d1a1d66fd0645517
IDX GitLab Automation <idx@dfinity.org>
2024-12-18 14:13:45 +0000

    chore: installed rate-limit canister at id

--- rs/tests/boundary_nodes/rate_limit_canister_test.rs
+++ rs/tests/boundary_nodes/rate_limit_canister_test.rs
@@ -14,19 +14,24 @@ Coverage:: The rate-limit canister interface works as expected.
 
 end::catalog[] */
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 use candid::{Decode, Encode, Principal};
+use ic_base_types::PrincipalId;
+use ic_nns_test_utils::itest_helpers::install_rust_canister_from_path;
+use ic_system_test_driver::{
+    driver::test_env_api::NnsInstallationBuilder,
+    util::{assert_create_agent, create_agent_mapping},
+};
 use k256::elliptic_curve::SecretKey;
 use rand::{rngs::OsRng, SeedableRng};
 use rand_chacha::ChaChaRng;
 use slog::{info, Logger};
-use std::env;
+use std::{env, net::{SocketAddr, SocketAddrV6}};
 use std::time::Duration;
-use tokio::runtime::Runtime;
+use tokio::{runtime::Runtime, time::sleep};
 
 use ic_agent::{
-    identity::{AnonymousIdentity, Secp256k1Identity},
-    Agent, Identity,
+    agent::http_transport::{reqwest_transport::reqwest::Client, ReqwestTransport}, identity::{AnonymousIdentity, Secp256k1Identity}, Agent, Identity
 };
 use ic_registry_subnet_type::SubnetType;
 use ic_system_test_driver::{
@@ -35,12 +40,12 @@ use ic_system_test_driver::{
         ic::InternetComputer,
         test_env::TestEnv,
         test_env_api::{
-            GetFirstHealthyNodeSnapshot, HasPublicApiUrl, HasTopologySnapshot, IcNodeContainer,
-            READY_WAIT_TIMEOUT, RETRY_BACKOFF,
+            get_dependency_path, GetFirstHealthyNodeSnapshot, HasPublicApiUrl, HasTopologySnapshot,
+            IcNodeContainer,
         },
     },
-    retry_with_msg_async, systest,
-    util::agent_observes_canister_module,
+    systest,
+    util::runtime_from_url,
 };
 use regex::Regex;
 
@@ -54,9 +59,21 @@ use rate_limits_api::{
 pub fn setup(env: TestEnv) {
     InternetComputer::new()
         .add_fast_single_node_subnet(SubnetType::System)
-        .add_fast_single_node_subnet(SubnetType::Application)
+        .use_specified_ids_allocation_range()
+        .with_api_boundary_nodes(1)
         .setup_and_start(&env)
         .expect("failed to setup IC under test");
+    let nns_node = env
+        .topology_snapshot()
+        .root_subnet()
+        .nodes()
+        .next()
+        .unwrap();
+    NnsInstallationBuilder::new()
+        .at_ids()
+        .install(&nns_node, &env)
+        .expect("could not install NNS canisters");
+    info!(&env.logger(), "Checking readiness of all replica nodes ...");
     env.topology_snapshot().subnets().for_each(|subnet| {
         subnet
             .nodes()
@@ -67,11 +84,51 @@ pub fn setup(env: TestEnv) {
 pub fn complete_flow_test(env: TestEnv) {
     let logger = env.logger();
 
-    info!(&logger, "installing canister");
+    let rt = Runtime::new().expect("Could not create tokio runtime.");
 
     let mut rng = ChaChaRng::from_rng(OsRng).unwrap();
     let full_access_identity = Secp256k1Identity::from_private_key(SecretKey::random(&mut rng));
     let full_access_principal = full_access_identity.sender().unwrap();
+    let api_bn = env.topology_snapshot().api_boundary_nodes().next().unwrap();
+
+    let api_bn_url = api_bn.get_public_url();
+    let api_bn_ipv6 = api_bn.get_ip_addr();
+    let ipv6 = SocketAddr::new(api_bn_ipv6, 0).into();
+    let api_bn_domain = api_bn.get_domain().unwrap();
+    info!(
+        &logger,
+        "API BN url {api_bn_url}, ipv6 {api_bn_ipv6}, domain {api_bn_domain:?}"
+    );
+
+    let client = Client::builder()
+        .danger_accept_invalid_certs(true)
+        .resolve(&api_bn_domain, ipv6)
+        .build()
+        .expect("Could not create HTTP client.");
+
+    let transport = ReqwestTransport::create_with_client(format!("https://{api_bn_domain}"), client).unwrap();
+    let mut api_bn_agent = Agent::builder()
+        .with_transport(transport)
+        .with_identity(full_access_identity.clone())
+        .build()
+        .unwrap();
+    let_ = rt.block_on(api_bn_agent.fetch_root_key());
+
+    info!(&logger, "installing rate-limit canister ...");
+
+    let canister_id = Principal::from_text("u637p-5aaaa-aaaaq-qaaca-cai").unwrap();
+
+    let nns_node = env.get_first_healthy_system_node_snapshot();
+    let nns = runtime_from_url(nns_node.get_public_url(), nns_node.effective_canister_id());
+
+    info!(
+        &logger,
+        "creating rate-limit canister at specific id={canister_id} ..."
+    );
+
+    let mut rate_limit_canister = rt
+        .block_on(nns.create_canister_at_id(PrincipalId(canister_id)))
+        .unwrap();
 
     let args = Encode!(&InitArg {
         registry_polling_period_secs: 60,
@@ -79,77 +136,33 @@ pub fn complete_flow_test(env: TestEnv) {
     })
     .unwrap();
 
-    let app_node = env.get_first_healthy_application_node_snapshot();
-    let canister_id = app_node.create_and_install_canister_with_arg(
-        &env::var("RATE_LIMIT_CANISTER_WASM_PATH").expect("RATE_LIMIT_CANISTER_WASM_PATH not set"),
+    info!(&logger, "installing rate-limit canister wasm");
+
+    rt.block_on(install_rust_canister_from_path(
+        &mut rate_limit_canister,
+        get_dependency_path(
+            env::var("RATE_LIMIT_CANISTER_WASM_PATH")
+                .expect("RATE_LIMIT_CANISTER_WASM_PATH not set"),
+        ),
         Some(args),
+    ));
+
+    info!(
+        &logger,
+        "rate-limit canister with id={canister_id} installed successfully"
     );
 
-    let rt = Runtime::new().expect("Could not create tokio runtime.");
-    let agent_node = app_node.build_default_agent();
-
     rt.block_on(async move {
-        // wait for canister to finish installing
-        retry_with_msg_async!(
-            format!(
-                "agent of {} observes canister module {}",
-                app_node.get_public_url().to_string(),
-                canister_id.to_string()
-            ),
-            &logger,
-            READY_WAIT_TIMEOUT,
-            RETRY_BACKOFF,
-            || async {
-                match agent_observes_canister_module(&agent_node, &canister_id).await {
-                    true => Ok(()),
-                    false => bail!("Canister module not available yet"),
-                }
-            }
-        )
-        .await
-        .unwrap();
-        info!(&logger, "installed rate-limit canister ({canister_id})");
-
-        info!(&logger, "creating two agents with different access rights");
-        let app_node_url = app_node.get_public_url().to_string();
-        let agent_full_access = create_agent(full_access_identity, app_node_url.clone()).await;
-        let agent_restricted_read = create_agent(AnonymousIdentity {}, app_node_url).await;
-
-        info!(&logger, "Call 1. Add a new config (version = 2) containing some rules (FullAccess level of the caller is required)");
-        add_config_1(logger.clone(), &agent_full_access, canister_id).await;
-
-        info!(&logger, "Call 2. Read config by privileged user (FullAccess or FullRead caller level). Response will expose rules/descriptions in their full form");
-        let version = 2;
-        read_config(logger.clone(), &agent_full_access, version, canister_id).await;
-
-        info!(&logger, "Call 3. Read config by non-privileged user (RestrictedRead level). Rules and descriptions are hidden in the response");
-        let rule_ids = read_config(logger.clone(), &agent_restricted_read, version, canister_id).await;
-
-        info!(&logger, "Call 4. Inspect the metadata of a rule before its disclosure. Some metadata fields should be hidden");
-        let rule_metadata = read_rule(logger.clone(), &agent_restricted_read, &rule_ids[2], canister_id).await;
-        assert!(rule_metadata.rule_raw.is_none());
-        assert!(rule_metadata.description.is_none());
-
-        info!(&logger, "Call 5. Disclose two rules linked to one incident");
-        let incident_id = "b97730ac-4879-47f2-9fea-daf20b8d4b64".to_string();
-        disclose_incident(logger.clone(), &agent_full_access, incident_id, canister_id).await;
-
-        info!(&logger, "Call 6. Read config by non-privileged user again. Now rules related to the disclosed incident are fully visible");
-        let _ = read_config(logger.clone(), &agent_restricted_read, version, canister_id).await;
-
-        info!(&logger, "Call 7. Add another config (version = 3) with one newly added rule, and one remove rule");
-        add_config_2(logger.clone(), &agent_full_access, canister_id).await;
-
-        info!(&logger, "Call 8. Read config by privileged user (FullAccess or FullRead caller level). Response will expose rules/descriptions in their full form");
-        let version = 3;
-        let _ = read_config(logger.clone(), &agent_full_access, version, canister_id).await;
-
-        info!(&logger, "Call 9. Inspect the metadata of the removed rule. All metadata fields should be visible, including versions when the rule was added/removed");
-        let rule_metadata = read_rule(logger.clone(), &agent_restricted_read, &rule_ids[2], canister_id).await;
-        assert!(rule_metadata.rule_raw.is_some());
-        assert!(rule_metadata.description.is_some());
-        assert_eq!(rule_metadata.added_in_version, 2);
-        assert_eq!(rule_metadata.removed_in_version, Some(3));
+        info!(&logger, "Reading a config from rate-limit canister");
+        let config = read_config(logger.clone(), &api_bn_agent, 1, canister_id).await;
+        info!(&logger, "Add a new config (version = 2) containing some rules (FullAccess level of the caller is required)");
+        add_config_1(logger.clone(), &api_bn_agent, canister_id).await;
+        let config = read_config(logger.clone(), &api_bn_agent, 2, canister_id).await;
+        loop {
+            info!(&logger, "Trying to read config ...");
+            let config = read_config(logger.clone(), &api_bn_agent, 2, canister_id).await;
+            sleep(Duration::from_secs(5)).await;
+        }
     });
 }
 
@@ -164,68 +177,23 @@ async fn create_agent<I: Identity + 'static>(identity: I, domain: String) -> Age
 }
 
 async fn add_config_1(logger: Logger, agent: &Agent, canister_id: Principal) {
-    // Note two rules (indices = [0, 2]) are linked to the same incident_id_1
-    // RuleIds are generated randomly on the canister side
-    let rule_1 = RateLimitRule {
+    let rule = RateLimitRule {
         canister_id: Some(canister_id),
         subnet_id: None,
-        methods_regex: Some(Regex::new(r"^(method_1)$").unwrap()),
-        request_types: Some(vec![RequestType::Call]),
-        limit: Action::Limit(1, Duration::from_secs(60)),
-    };
-
-    let rule_2 = RateLimitRule {
-        canister_id: Some(canister_id),
-        subnet_id: None,
-        methods_regex: Some(Regex::new(r"^(method_2)$").unwrap()),
-        request_types: Some(vec![RequestType::Query]),
-        limit: Action::Limit(2, Duration::from_secs(60)),
-    };
-
-    let rule_3 = RateLimitRule {
-        canister_id: Some(canister_id),
-        subnet_id: None,
-        methods_regex: Some(Regex::new(r"^(method_3)$").unwrap()),
+        methods_regex: None,
         request_types: None,
-        limit: Action::Limit(3, Duration::from_secs(60)),
-    };
-
-    let rule_4 = RateLimitRule {
-        canister_id: Some(canister_id),
-        subnet_id: None,
-        methods_regex: Some(Regex::new(r"^(method_4)$").unwrap()),
-        request_types: Some(vec![RequestType::ReadState]),
-        limit: Action::Limit(4, Duration::from_secs(60)),
+        limit: Action::Block,
     };
 
     let args = Encode!(&InputConfig {
         schema_version: 1,
-        rules: vec![
-            InputRule {
-                incident_id: "b97730ac-4879-47f2-9fea-daf20b8d4b64".to_string(),
-                rule_raw: rule_1.to_bytes_json().unwrap(),
-                description:
-                    "Some vulnerability #1 discovered, temporarily rate-limiting the canister calls"
-                        .to_string(),
-            },
-            InputRule {
-                incident_id: "f63c821c-9320-476a-bc89-94cb99d04639".to_string(),
-                rule_raw: rule_2.to_bytes_json().unwrap(),
-                description: "Some vulnerability #2 discovered".to_string(),
-            },
-            InputRule {
-                incident_id: "b97730ac-4879-47f2-9fea-daf20b8d4b64".to_string(),
-                rule_raw: rule_3.to_bytes_json().unwrap(),
-                description:
-                    "Some vulnerability #1 discovered, temporarily rate-limiting the canister calls"
-                        .to_string(),
-            },
-            InputRule {
-                incident_id: "389bbff8-bffa-4430-bb70-8ce1ea399c07".to_string(),
-                rule_raw: rule_4.to_bytes_json().unwrap(),
-                description: "Some vulnerability #3 discovered".to_string(),
-            },
-        ],
+        rules: vec![InputRule {
+            incident_id: "b97730ac-4879-47f2-9fea-daf20b8d4b64".to_string(),
+            rule_raw: rule.to_bytes_json().unwrap(),
+            description:
+                "Some vulnerability #1 discovered, temporarily rate-limiting the canister calls"
+                    .to_string(),
+        },],
     })
     .unwrap();
 
@@ -332,7 +300,7 @@ async fn read_config(
         .with_arg(args)
         .call()
         .await
-        .expect("update call failed");
+        .expect("query call failed");
 
     let decoded = Decode!(&response, GetConfigResponse)
         .expect("failed to decode candid response")
