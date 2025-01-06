307ccd49c40457de86d4ef6d5681237e800f05e6
Dragoljub Duric <dragoljub.duric@dfinity.org>
2025-01-06 13:15:05 +0000

    .

--- rs/execution_environment/src/execution_environment/tests.rs
+++ rs/execution_environment/src/execution_environment/tests.rs
@@ -707,14 +707,13 @@ fn get_running_canister_status_from_another_canister() {
     );
 }
 
-//#[ignore]
 #[test]
 fn get_canister_status_from_another_canister_when_memory_low() {
     let mut test = ExecutionTestBuilder::new().build();
     let controller = test.universal_canister().unwrap();
     let binary = wat::parse_str("(module)").unwrap();
     let canister = test.create_canister(Cycles::new(1_000_000_000_000));
-    let memory_allocation = NumBytes::from(158);
+    let memory_allocation = NumBytes::from(300);
     test.install_canister_with_allocation(canister, binary, None, Some(memory_allocation.get()))
         .unwrap();
     let canister_status_args = Encode!(&CanisterIdRecord::from(canister)).unwrap();
