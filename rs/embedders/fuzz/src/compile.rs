07c415f387b02d869584aa329a18d91be044e9a5
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2025-01-06 17:30:50 +0000

    compare compilation results

--- rs/embedders/fuzz/src/compile.rs
+++ rs/embedders/fuzz/src/compile.rs
@@ -4,7 +4,9 @@ use ic_config::flag_status::FlagStatus;
 use ic_embedders::{wasm_utils::compile, WasmtimeEmbedder};
 use ic_logger::replica_logger::no_op_logger;
 use ic_wasm_types::BinaryEncodedWasm;
-use wasm_smith::{Config, Module};
+use std::time::Duration;
+use tokio::runtime::Runtime;
+use wasm_smith::{Config, MemoryOffsetChoices, Module};
 
 #[derive(Debug)]
 pub struct MaybeInvalidModule(pub Module);
@@ -13,6 +15,7 @@ impl<'a> Arbitrary<'a> for MaybeInvalidModule {
     fn arbitrary(u: &mut Unstructured<'a>) -> Result<Self> {
         let mut config = Config::arbitrary(u)?;
         config.allow_invalid_funcs = true;
+        config.memory_offset_choices = MemoryOffsetChoices(40, 20, 40);
         Ok(MaybeInvalidModule(Module::new(config, u)?))
     }
 }
@@ -22,10 +25,52 @@ pub fn run_fuzzer(module: MaybeInvalidModule) {
     let mut config = EmbeddersConfig::default();
     config.feature_flags.wasm64 = FlagStatus::Enabled;
     let wasm = module.0.to_bytes();
-    let binary_wasm = BinaryEncodedWasm::new(wasm);
-    let embedder = WasmtimeEmbedder::new(config, no_op_logger());
 
-    let (_, _) = compile(&embedder, &binary_wasm);
+    let rt: Runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(3)
+        .max_blocking_threads(1)
+        .enable_all()
+        .build()
+        .unwrap_or_else(|err| panic!("Could not create tokio runtime: {}", err));
+
+    let first_execution = rt.spawn({
+        let wasm = wasm.clone();
+        let binary_wasm = BinaryEncodedWasm::new(wasm);
+        let embedder = WasmtimeEmbedder::new(config.clone(), no_op_logger());
+
+        async move { compile(&embedder, &binary_wasm) }
+    });
+
+    let second_execution = rt.spawn({
+        let binary_wasm = BinaryEncodedWasm::new(wasm);
+        let embedder = WasmtimeEmbedder::new(config, no_op_logger());
+
+        async move { compile(&embedder, &binary_wasm) }
+    });
+
+    rt.block_on(async move {
+        // The omitted field is Result<InstancePre<StoreData>, HypervisorError>
+        // 1. InstancePre<StoreData> doesn't implement PartialEq
+        // 2. HypervisorError is the same in compilation_result which is checked for equality
+
+        let (_, compilation_result_1) = first_execution.await.unwrap();
+        let (_, compilation_result_2) = second_execution.await.unwrap();
+
+        let time_removed_compilation_result_1 = compilation_result_1.map(|mut r| {
+            r.0.compilation_time = Duration::from_millis(1);
+            r
+        });
+
+        let time_removed_compilation_result_2 = compilation_result_2.map(|mut r| {
+            r.0.compilation_time = Duration::from_millis(1);
+            r
+        });
+
+        assert_eq!(
+            time_removed_compilation_result_1,
+            time_removed_compilation_result_2
+        );
+    });
 }
 
 #[cfg(test)]
