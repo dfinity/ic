4b0d6568ee502034dd700f6099201547b1bb91f1
Leon Tan <leon.tan@dfinity.org>
2025-01-08 10:18:05 +0000

    WIP building local vetkeys dkg

--- rs/consensus/dkg/src/utils.rs
+++ rs/consensus/dkg/src/utils.rs
@@ -1,8 +1,11 @@
 use ic_consensus_utils::pool_reader::PoolReader;
+use ic_interfaces_registry::RegistryClient;
+use ic_registry_client_helpers::subnet::SubnetRegistry;
 use ic_types::{
     consensus::Block,
-    crypto::threshold_sig::ni_dkg::{NiDkgDealing, NiDkgId},
-    NodeId,
+    crypto::threshold_sig::ni_dkg::{NiDkgDealing, NiDkgId, NiDkgMasterPublicKeyId},
+    registry::RegistryClientError,
+    NodeId, RegistryVersion, SubnetId,
 };
 use std::collections::{BTreeMap, HashSet};
 
@@ -49,4 +52,26 @@ pub(super) fn get_dkg_dealings(
                 });
             acc
         })
+}
+
+/// Fetch all key ids for which the subnet should hold a key
+fn get_enabled_vet_keys(
+    subnet_id: SubnetId,
+    registry_client: &dyn RegistryClient,
+    registry_version: RegistryVersion,
+) -> Result<Vec<NiDkgMasterPublicKeyId>, RegistryClientError> {
+    // TODO: Handle None case
+    let subnet_record = registry_client
+        .get_subnet_record(subnet_id, registry_version)?
+        .unwrap();
+
+    let keys = subnet_record
+        .chain_key_config
+        .into_iter()
+        .flat_map(|config| config.key_configs)
+        .filter_map(|config| config.key_id)
+        .filter_map(|key_id| NiDkgMasterPublicKeyId::try_from(key_id).ok())
+        .collect::<Vec<_>>();
+
+    Ok(keys)
 }
