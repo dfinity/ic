c6e4e123f52de24ccabf73f2f362232a6047bbcd
Thomas Locher <thomas.locher@dfinity.org>
2025-01-10 14:54:51 +0100

    Updated the unified ckBTC/ckETH blocklist generation script.

--- rs/cross-chain/scripts/generate_blocklist.py
+++ rs/cross-chain/scripts/generate_blocklist.py
@@ -28,6 +28,9 @@ DIGITAL_CURRENCY_TYPE_PREFIX = 'Digital Currency Address - '
 # The blocked addresses are stored in this Rust file.
 BLOCKLIST_FILENAME = 'blocklist.rs'
 
+# Invalid addresses in the OFAC SDN list to be commented out.
+INVALID_ADDRESSES = ['TUCsTq7TofTCJRRoHk6RvhMoS2mJLm5Yzq']
+
 # This prefix is needed for each element in the XML tree.
 PREFIX = '{https://sanctionslistservice.ofac.treas.gov/api/PublicationPreview/exports/XML}'
 
@@ -59,23 +62,66 @@ def extract_addresses(currency, xml_file_path):
     addresses.sort()
     return addresses
 
-def store_blocklist(currency, addresses):
-    blocklist_file = open(BLOCKLIST_FILENAME, 'w')
-    blocklist_file.write('/// '+ currency + ' is not accepted from nor sent to addresses on this list.\n')
+def write_btc_preamble(blocklist_file):
+    blocklist_file.write('#[cfg(test)]\nmod tests;\n\nuse bitcoin::Address;\n\n')
     blocklist_file.write('/// The script to generate this file, including information about the source data, can be found here:\n')
     blocklist_file.write('/// /rs/cross-chain/scripts/generate_blocklist.py\n\n')
+    blocklist_file.write('/// BTC is not accepted from nor sent to addresses on this list.\n')
     blocklist_file.write('/// NOTE: Keep it sorted!\n')
-    blocklist_file.write('pub const ' + currency + '_ADDRESS_BLOCKLIST: &[&str] = &[\n')
-    for address in addresses:
-        blocklist_file.write('\t"' + address + '",\n')
-        print(address)
-    blocklist_file.write(']+\n\n')
-    blocklist_file.write('pub fn is_blocked(from_address: &Address) -> bool {\n')
-    blocklist_file.write('    ' + currency + '_ADDRESS_BLOCKLIST.binary_search(from_address).is_ok()\n')
+    blocklist_file.write('pub const BTC_ADDRESS_BLOCKLIST: &[&str] = &[\n')
+
+def write_eth_preamble(blocklist_file):
+    blocklist_file.write('#[cfg(test)]\nmod tests;\n\nuse ic_ethereum_types::Address;\n\n')
+    blocklist_file.write('macro_rules! ethereum_address {\n    ($address:expr) => {\n        Address::new(hex_literal::hex!($address))\n    };\n}\n\n')
+    blocklist_file.write('/// The script to generate this file, including information about the source data, can be found here:\n')
+    blocklist_file.write('/// /rs/cross-chain/scripts/generate_blocklist.py\n\n')
+    blocklist_file.write('/// ETH is not accepted from nor sent to addresses on this list.\n')
+    blocklist_file.write('/// NOTE: Keep it sorted!\n')
+    blocklist_file.write('const ETH_ADDRESS_BLOCKLIST: &[Address] = &[\n')
+
+def write_btc_postamble(blocklist_file):
+    blocklist_file.write('pub fn is_blocked(address: &Address) -> bool {\n')
+    blocklist_file.write('    ' + 'BTC_ADDRESS_BLOCKLIST\n        .binary_search(&address.to_string().as_ref())\n        .is_ok()\n')
     blocklist_file.write('}')
+    
+def write_eth_postamble(blocklist_file):
+    blocklist_file.write('pub fn is_blocked(address: &Address) -> bool {\n')
+    blocklist_file.write('    ' + 'ETH_ADDRESS_BLOCKLIST.binary_search(address).is_ok()\n')
+    blocklist_file.write('}')
+
+def store_blocklist(currency, addresses):
+    blocklist_file = open(BLOCKLIST_FILENAME, 'w')
+    
+    if currency == 'BTC':
+        write_btc_preamble(blocklist_file)
+        address_prefix = ''
+        address_suffix = ''
+        offset = 0
+    else:   # currency == 'ETH'
+        write_eth_preamble(blocklist_file)
+        address_prefix = 'ethereum_address!('
+        address_suffix = ')'
+        offset = 2
+    for address in addresses:
+        # Ethereum addresses are case-insensitive. By contrast, only Bech32 Bitcoin addresses are case-insensitive.
+        if currency == 'ETH':
+            address = address.lower()
+        if address in INVALID_ADDRESSES:
+            blocklist_file.write('    // ' + address_prefix + '"' + address[offset:] + '"' + address_suffix + ' (Invalid address prefix)\n')
+            print('Invalid address:', address)
+        else:
+            blocklist_file.write('    ' + address_prefix + '"' + address[offset:] + '"' + address_suffix + ',\n')
+            print(address)
+    blocklist_file.write('];\n\n')
+    
+    if currency == 'BTC':
+        write_btc_postamble(blocklist_file)
+    else:
+        write_eth_postamble(blocklist_file)
+    
     blocklist_file.close()
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     if len(sys.argv) < 3:
         print('Usage: ' + sys.argv[0] + ' [currency (BTC or ETH)] [path to SDN.XML file]')
     else:
