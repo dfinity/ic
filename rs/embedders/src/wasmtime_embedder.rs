d3e4d0ee506892b8e64b551796bb7908e2c2a7db
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2025-01-13 10:54:58 +0000

    feat(EXC-1800): Use Wasmtime `deserialize_open_file`.
    EXC-1800
    
    When using the on-disk compilation cache, switch to Wasmtime's new
    `deserialize_open_file` API which allows sandboxes to mmap the existing
    file so that canister code doesn't take up resident memory in the
    sandbox.

--- rs/embedders/src/wasmtime_embedder.rs
+++ rs/embedders/src/wasmtime_embedder.rs
@@ -336,28 +336,18 @@ impl WasmtimeEmbedder {
         self.pre_instantiate(&module)
     }
 
-    /// TODO(EXC-1800): Replace this with `wasmtime::Module::deserialize_open_file`.
-    fn deserialize_from_file(&self, serialized_module: &File) -> HypervisorResult<Module> {
-        let mmap_size = serialized_module.metadata().unwrap().size() as usize;
-        let mmap_ptr = unsafe {
-            mmap(
-                std::ptr::null_mut(),
-                mmap_size,
-                ProtFlags::PROT_READ,
-                MapFlags::MAP_PRIVATE,
-                serialized_module.as_raw_fd(),
-                0,
-            )
-        }
-        .unwrap_or_else(|err| panic!("Module deserialization failed: {:?}", err))
-            as *mut u8;
-        let bytes = unsafe { std::slice::from_raw_parts(mmap_ptr, mmap_size) };
+    fn deserialize_from_file(&self, serialized_module: File) -> HypervisorResult<Module> {
+        // SAFETY: The compilation cache setup guarantees that this file is a
+        // valid serialized module and will not be modified after initial
+        // creation.
         unsafe {
-            Module::deserialize(&self.create_engine()?, bytes).map_err(|err| {
-                HypervisorError::WasmEngineError(WasmEngineError::FailedToDeserializeModule(
-                    format!("{:?}", err),
-                ))
-            })
+            Module::deserialize_open_file(&self.create_engine()?, serialized_module).map_err(
+                |err| {
+                    HypervisorError::WasmEngineError(WasmEngineError::FailedToDeserializeModule(
+                        format!("{:?}", err),
+                    ))
+                },
+            )
         }
     }
 
@@ -365,10 +355,7 @@ impl WasmtimeEmbedder {
         &self,
         serialized_module: File,
     ) -> HypervisorResult<InstancePre<StoreData>> {
-        // TODO(EXC-1800): Switch to new wasmtime API and remove leaking the
-        // file.
-        let module = self.deserialize_from_file(&serialized_module)?;
-        let _ = serialized_module.into_raw_fd();
+        let module = self.deserialize_from_file(serialized_module)?;
         self.pre_instantiate(&module)
     }
 
