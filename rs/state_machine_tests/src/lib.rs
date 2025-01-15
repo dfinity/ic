f491f848c4da05a852186f0c2279e5bcb7ce60f8
kpop-dfinity <125868903+kpop-dfinity@users.noreply.github.com>
2025-01-15 16:26:04 +0100

    refactor(consensus): simplify `IngressPayload` implementation (#3444)
    Currently the payload is one big bytes buffer which contains all ingress
    messages in a packed representation. This makes it a big hard to work
    with.
    In this PR we simplify the structure by replacing the buffer with a map
    from `IngressMessageId`s to a _serialized_ ingress messages.
    The following two important properties are preserved:
    1. The individual ingress messages deserialization is delayed until it's
    actually needed
    2. We preserve the original byte representation of the ingress messages
    
    I've ran multiple benchmarks and didn't notice a _big_ change:
    1. `consensus-performance` system test showed the same throughput /
    block rates with both implementations
    2. the [serialization/deserialization
    ](https://github.com/dfinity/ic/blob/master/rs/consensus/benches/validate_payload.rs#L374-L404)
    of ingress payload gets about 2x slower, but it's still in sub 1ms
    territory
    
    ---------
    
    Co-authored-by: Leon Tan <leon.tan@dfinity.org>

--- rs/state_machine_tests/src/lib.rs
+++ rs/state_machine_tests/src/lib.rs
@@ -1370,9 +1370,7 @@ impl StateMachine {
         // used by the function `Self::execute_payload` of the `StateMachine`.
         let xnet_payload = batch_payload.xnet.clone();
         let ingress = &batch_payload.ingress;
-        let ingress_messages = (0..ingress.message_count())
-            .map(|i| ingress.get(i).unwrap().1)
-            .collect();
+        let ingress_messages = ingress.clone().try_into().unwrap();
         let (http_responses, _) =
             CanisterHttpPayloadBuilderImpl::into_messages(&batch_payload.canister_http);
         let inducted: Vec<_> = http_responses
