a9cb37d63c394e55bcad946b088294790bbc2039
Andrew Battat <andrew.battat@dfinity.org>
2025-01-17 16:02:27 +0000

    Normalize config.sh to return empty string

--- ic-os/components/misc/config/setupos/config.sh
+++ ic-os/components/misc/config/setupos/config.sh
@@ -5,9 +5,10 @@
 # Retrieves a value from the config.json file using a JSON path.
 # Arguments:
 #   $1 - JSON path to the desired value (e.g., '.icos_settings.nns_urls')
-# Note: If the key is not found, this function will return null.
+# Returns:
+#   The value if found, otherwise an empty string
 function get_config_value() {
     local CONFIG_FILE="/var/ic/config/config.json"
     local key=$1
-    jq -r "${key}" "${CONFIG_FILE}"
+    jq -re "${key}" "${CONFIG_FILE}" 2>/dev/null || echo ""
 }
