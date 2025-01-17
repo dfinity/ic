c945410434b302dcd70dbee83b175749baf32d04
Andrew Battat <andrew.battat@dfinity.org>
2025-01-17 16:07:46 +0000

    Fix typo

--- ic-os/components/misc/config/setupos/config.sh
+++ ic-os/components/misc/config/setupos/config.sh
@@ -6,7 +6,7 @@
 # Arguments:
 #   $1 - JSON path to the desired value (e.g., '.icos_settings.nns_urls')
 # Returns:
-    # If key not found or value is "null", retuns empty string. Otherwise, returns value
+    # If key not found or value is "null", returns empty string. Otherwise, returns value
 function get_config_value() {
     local CONFIG_FILE="/var/ic/config/config.json"
     local key=$1
