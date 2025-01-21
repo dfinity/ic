e0e0f10a8d6b7d871842198630974a5ed4d72899
Alin Sinpalean <alin.sinpalean@dfinity.org>
2025-01-21 06:29:31 +0000

    Fix formatting.

--- rs/tests/message_routing/xnet/xnet_malicious_slices.rs
+++ rs/tests/message_routing/xnet/xnet_malicious_slices.rs
@@ -153,8 +153,7 @@ pub async fn test_async(env: TestEnv, config: Config) {
     // Start all canisters (via update `start` call).
     info!(logger, "Calling start() on all canisters...");
     start_all_canisters(
-        &canisters,
-        1024, // send messages with 1024 byte payloads
+        &canisters, 1024, // send messages with 1024 byte payloads
         0,    // guaranteed response calls
         1024, // same response size
         10,   // each canister sends 10 RPS
