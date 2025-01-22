4035180be7e86d60e586d75644caef3987a61c31
Mihail Jianu <mihail.jianu@dfinity.org>
2025-01-22 16:50:04 +0000

    reverse fuzzer changes

--- rs/fuzzers/bitcoin/fuzz_targets/deserialize_bitcoin_raw_network_message.rs
+++ rs/fuzzers/bitcoin/fuzz_targets/deserialize_bitcoin_raw_network_message.rs
@@ -1,6 +1,6 @@
 #![no_main]
 use bitcoin::consensus::encode::deserialize;
-use bitcoin::p2p::message::RawNetworkMessage;
+use bitcoin::network::message::RawNetworkMessage;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
