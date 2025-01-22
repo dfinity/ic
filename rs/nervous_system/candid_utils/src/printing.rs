da728fa70b094a79fb56a39250d33d09c7a68ed5
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2025-01-22 20:03:38 +0000

    second

--- rs/nervous_system/candid_utils/src/printing.rs
+++ rs/nervous_system/candid_utils/src/printing.rs
@@ -1,11 +1,9 @@
 use candid::{CandidType, IDLValue};
 
 pub fn pretty<T: CandidType>(value: T) -> Result<String, String> {
-    let value_pretty: String = IDLValue::try_from_candid_type(&value)
-        .map_err(|err| err.to_string())?
-        .to_string();
+    let value = IDLValue::try_from_candid_type(&value).map_err(|err| err.to_string())?;
 
-    Ok(value_pretty.to_string())
+    Ok(value.to_string())
 }
 
 #[cfg(test)]
