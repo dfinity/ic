b5951b1a53ed7654441b6ae2c2775e1d6f87e58f
Andrew Battat <andrew.battat@dfinity.org>
2025-01-23 18:35:07 +0000

    Add GuestOS launching log

--- ic-os/components/hostos-scripts/guestos/start-guestos.sh
+++ ic-os/components/hostos-scripts/guestos/start-guestos.sh
@@ -68,6 +68,7 @@ function start_guestos() {
 
 function main() {
     # Establish run order
+    echo "GUESTOS LAUNCHING. If onboarding a node, please wait up to 10 minutes for a 'Join request successful!' confirmation message.\n\n\n" > /dev/tty1
     define_guestos
     start_guestos
 }
