fc35269222efdc338d9dd4101f0a834700ba1548
Andrew Battat <andrew.battat@dfinity.org>
2025-01-28 00:10:31 +0000

    Log guestos.service console logs to tty1

--- ic-os/components/hostos-scripts/guestos/start-guestos.sh
+++ ic-os/components/hostos-scripts/guestos/start-guestos.sh
@@ -32,6 +32,14 @@ done
 # Set arguments if undefined
 CONFIG="${CONFIG:=/var/lib/libvirt/guestos.xml}"
 
+write_tty1_log() {
+    local message=$1
+
+    echo "${SCRIPT} ${message}" > /dev/tty1
+
+    logger -t "${SCRIPT}" "${message}"
+}
+
 function define_guestos() {
     if [ "$(virsh list --all | grep 'guestos')" ]; then
         write_log "GuestOS virtual machine is already defined."
@@ -57,7 +65,44 @@ function start_guestos() {
             "GuestOS virtual machine start state" \
             "gauge"
     else
-        virsh start guestos
+        # Attempt to start; if it fails, dump logs.
+        if ! virsh start guestos; then
+            # The sleep below gives QEMU time to clear/reinitialize its console 
+            # so that error messages won't be immediately overwritten. This helps
+            # ensure the logs and troubleshooting info remain visible on the host console.
+            sleep 10
+
+            write_tty1_log "ERROR: Failed to start GuestOS virtual machine.\n\n\n"
+
+            write_tty1_log "#################################################"
+            write_tty1_log "###      LOGGING GUESTOS.SERVICE LOGS...      ###"
+            write_tty1_log "#################################################"
+            journalctl -u guestos.service > /dev/tty1
+
+            write_tty1_log "#################################################"
+            write_tty1_log "###          TROUBLESHOOTING INFO...          ###"
+            write_tty1_log "#################################################"
+            host_ipv6_address="$(/opt/ic/bin/hostos_tool generate-ipv6-address --node-type GuestOS 2>/dev/null)"
+            write_tty1_log "Host ipv6 address: $host_ipv6_address"
+
+            if [ -f /var/log/libvirt/qemu/guestos-serial.log ]; then
+                write_tty1_log "#################################################"
+                write_tty1_log "###  LOGGING GUESTOS CONSOLE LOGS, IF ANY...  ###"
+                write_tty1_log "#################################################"
+                tail -n 30 /var/log/libvirt/qemu/guestos-serial.log | while IFS= read -r line; do
+                    write_tty1_log "$line"
+                done
+            else
+                write_tty1_log "No /var/log/libvirt/qemu/guestos-serial.log file found."
+            fi
+
+            write_tty1_log "Exiting start-guestos.sh so that systemd can restart guestos.service in 5 minutes."
+            exit 1
+        fi
+
+        sleep 10
+        write_tty1_log "GuestOS virtual machine successfully launched.\nIf onboarding, please wait for up to 10 minutes for a 'Join request successful!' message\n\n\n"
+
         write_log "Starting GuestOS virtual machine."
         write_metric "hostos_guestos_service_start" \
             "1" \
