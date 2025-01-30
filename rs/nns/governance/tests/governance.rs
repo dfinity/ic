a9a31de587d813a5ed4211be496efe406fa761ed
Daniel Wong <daniel.wong@dfinity.org>
2025-01-30 14:49:56 +0100

    Delete tests that verify legacy behavior (i.e. before private neurons).

--- rs/nns/governance/tests/governance.rs
+++ rs/nns/governance/tests/governance.rs
@@ -57,7 +57,6 @@ use ic_nns_governance::{
         WAIT_FOR_QUIET_DEADLINE_INCREASE_SECONDS,
     },
     governance_proto_builder::GovernanceProtoBuilder,
-    is_private_neuron_enforcement_enabled,
     pb::v1::{
         add_or_remove_node_provider::Change,
         governance::{GovernanceCachedMetrics, GovernanceCachedMetricsChange, MigrationsDesc},
@@ -94,8 +93,6 @@ use ic_nns_governance::{
         Tally, TallyChange, Topic, UpdateNodeProvider, Visibility, Vote, VotingPowerEconomics,
         WaitForQuietState, WaitForQuietStateDesc,
     },
-    temporarily_disable_private_neuron_enforcement, temporarily_disable_set_visibility_proposals,
-    temporarily_enable_private_neuron_enforcement, temporarily_enable_set_visibility_proposals,
     DEFAULT_VOTING_POWER_REFRESHED_TIMESTAMP_SECONDS,
 };
 use ic_nns_governance_api::{
@@ -2290,69 +2287,9 @@ fn fixture_for_manage_neuron() -> GovernanceProto {
         .build()
 }
 
-/// Legacy behavior.
 #[test]
-fn test_get_neuron_when_private_neuron_enforcement_disabled() {
+fn test_enforce_private_neuron() {
     // Step 1: Prepare the world.
-
-    // This is the only difference (in step 1) compared to the previous test.
-    let _restore_on_drop = temporarily_disable_private_neuron_enforcement();
-
-    let driver = fake::FakeDriver::default();
-    let governance = Governance::new(
-        fixture_for_manage_neuron(),
-        driver.get_fake_env(),
-        driver.get_fake_ledger(),
-        driver.get_fake_cmc(),
-    );
-
-    // Step 2: Call code under test.
-    let neuron_id = NeuronId { id: 1 };
-    let key = NeuronIdOrSubaccount::NeuronId(neuron_id);
-    let controller = principal(1);
-
-    let neuron_info = governance
-        .get_neuron_info_by_id_or_subaccount(&key, *RANDOM_PRINCIPAL_ID)
-        .unwrap();
-
-    let full_neuron = governance
-        .get_full_neuron_by_id_or_subaccount(
-            &NeuronIdOrSubaccount::NeuronId(neuron_id),
-            &controller,
-        )
-        .unwrap();
-
-    let list_neurons_response = governance.list_neurons(
-        &ListNeurons {
-            neuron_ids: vec![1],
-            ..Default::default()
-        },
-        controller,
-    );
-
-    // Step 3: Inspect results.
-    assert_eq!(neuron_info.visibility, None);
-    assert_eq!(full_neuron.visibility, None);
-
-    assert_eq!(
-        list_neurons_response,
-        ListNeuronsResponse {
-            neuron_infos: hashmap! {
-                1 => neuron_info,
-            },
-            full_neurons: vec![api::Neuron::from(full_neuron)],
-            total_pages_available: Some(1),
-        },
-    );
-}
-
-/// Behavior of glorious future.
-#[test]
-fn test_get_neuron_when_private_neuron_enforcement_enabled() {
-    // Step 1: Prepare the world.
-
-    // This is the only difference (in step 1) compared to the previous test.
-    let _restore_on_drop = temporarily_enable_private_neuron_enforcement();
 
     let driver = fake::FakeDriver::default();
     let governance = Governance::new(
@@ -4621,11 +4558,6 @@ fn create_mature_neuron(dissolved: bool) -> (fake::FakeDriver, Governance, Neuro
     );
 
     // Make sure the neuron was created with the right details.
-    let visibility = if is_private_neuron_enforcement_enabled() {
-        Some(Visibility::Private as i32)
-    } else {
-        None
-    };
     let expected_voting_power = neuron_stake_e8s
         // Age bonus.
         * 17
@@ -4641,7 +4573,7 @@ fn create_mature_neuron(dissolved: bool) -> (fake::FakeDriver, Governance, Neuro
             aging_since_timestamp_seconds: driver.now(),
             dissolve_state: Some(DissolveState::DissolveDelaySeconds(dissolve_delay_seconds)),
             kyc_verified: true,
-            visibility,
+            visibility: Some(Visibility::Private as i32),
             voting_power_refreshed_timestamp_seconds: Some(START_TIMESTAMP_SECONDS),
             deciding_voting_power: Some(expected_voting_power),
             potential_voting_power: Some(expected_voting_power),
@@ -7140,141 +7072,6 @@ fn test_add_and_remove_hot_key() {
         .neuron_store
         .get_neuron_ids_readable_by_caller(new_controller)
         .contains(neuron.id.as_ref().unwrap()));
-}
-
-// TODO(NNS1-3228): Delete this.
-#[test]
-fn test_are_set_visibility_proposals_enabled_flag() {
-    // Step 1: Prepare the world.
-
-    let mut random = StdRng::seed_from_u64(485_539_390);
-
-    let mut new_neuron = || {
-        let id = random.gen();
-        let controller = PrincipalId::new_user_test_id(id);
-        let account = compute_neuron_staking_subaccount_bytes(controller, random.gen()).to_vec();
-
-        Neuron {
-            id: Some(NeuronId { id }),
-            account,
-            controller: Some(controller),
-
-            dissolve_state: NOTDISSOLVING_MIN_DISSOLVE_DELAY_TO_VOTE,
-            // Enough to make a proposal, but not a whale.
-            cached_neuron_stake_e8s: random.gen_range(100..=200) * E8,
-
-            ..Default::default()
-        }
-    };
-
-    // This can make and vote on ManageNeuron proposals that target the puppet
-    // Neuron.
-    let leader = new_neuron();
-
-    let puppet = Neuron {
-        // Follows leader on the NeuronManagement topic.
-        followees: hashmap! {
-            Topic::NeuronManagement as i32 => Followees {
-                followees: vec![leader.id.unwrap()],
-            },
-        },
-
-        ..new_neuron()
-    };
-
-    let governance_proto = GovernanceProtoBuilder::new()
-        .with_neurons(vec![leader.clone(), puppet.clone()])
-        .build();
-
-    let fake_driver = fake::FakeDriver::default();
-    let mut governance = Governance::new(
-        governance_proto,
-        fake_driver.get_fake_env(),
-        fake_driver.get_fake_ledger(),
-        fake_driver.get_fake_cmc(),
-    );
-
-    let mut make_set_visibility_proposal = || -> Result<ProposalId, GovernanceError> {
-        governance
-            .make_proposal(
-                leader.id.as_ref().unwrap(),
-                leader.controller.as_ref().unwrap(),
-                &Proposal {
-                    title: Some("SetVisibility of puppet to Public".to_string()),
-                    summary: "SetVisibility of puppet to Public".to_string(),
-                    url: "https://forum.dfinity.org/set_visibility".to_string(),
-                    action: Some(proposal::Action::ManageNeuron(Box::new(ManageNeuron {
-                        neuron_id_or_subaccount: Some(NeuronIdOrSubaccount::NeuronId(
-                            puppet.id.unwrap(),
-                        )),
-                        command: Some(manage_neuron::Command::Configure(
-                            manage_neuron::Configure {
-                                operation: Some(
-                                    manage_neuron::configure::Operation::SetVisibility(
-                                        SetVisibility {
-                                            visibility: Some(Visibility::Public as i32),
-                                        },
-                                    ),
-                                ),
-                            },
-                        )),
-                        id: None,
-                    }))),
-                },
-            )
-            .now_or_never()
-            .unwrap()
-    };
-
-    // Case A: SetVisibility (ManageNeuron) proposals are disabled. When we
-    // start rolling out neuron visibility, this will be the case. This is so
-    // that clients (esp nns-dapp) will not suddenly see these kinds of
-    // proposals when they might not know how to handle them.
-    {
-        let _restore_on_drop = temporarily_disable_set_visibility_proposals();
-
-        // Step 2: Call the code under test.
-        let result = make_set_visibility_proposal();
-
-        // Step 3: Inspect result(s).
-
-        let error = result.unwrap_err();
-
-        // Decompose the error.
-        let GovernanceError {
-            error_type,
-            error_message,
-        } = &error;
-
-        // Inspect the error type.
-        assert_eq!(
-            ErrorType::try_from(*error_type),
-            Ok(ErrorType::Unavailable),
-            "{:?}",
-            error,
-        );
-
-        // Make sure the error message looks right.
-        let message = error_message.to_lowercase();
-        for key_word in ["visibility", "allowed", "yet"] {
-            assert!(message.contains(key_word), "{:?}", error);
-        }
-    }
-
-    // Case B: SetVisibility proposals are enabled. Eventually, we'll want to
-    // allow these, after clients (esp nns-dapp) are ready for them.
-    {
-        let _restore_on_drop = temporarily_enable_set_visibility_proposals();
-
-        // Step 2: Call the code under test.
-        let result = make_set_visibility_proposal();
-
-        // Step 3: Inspect result(s).
-
-        // After unwrapping, no further inspection is needed. Also, it is
-        // unclear what ID to expect. The main thing is that we get an ID back.
-        let _proposal_id: ProposalId = result.unwrap();
-    }
 }
 
 #[test]
@@ -10827,15 +10624,13 @@ fn test_include_public_neurons_in_full_neurons() {
         caller_controlled_neuron,
     ];
 
-    if is_private_neuron_enforcement_enabled() {
-        for neuron in &mut expected_full_neurons {
-            if neuron.known_neuron_data.is_some() {
-                neuron.visibility = Some(Visibility::Public as i32);
-            }
-            let visibility = &mut neuron.visibility;
-            if visibility.is_none() {
-                *visibility = Some(Visibility::Private as i32);
-            }
+    for neuron in &mut expected_full_neurons {
+        if neuron.known_neuron_data.is_some() {
+            neuron.visibility = Some(Visibility::Public as i32);
+        }
+        let visibility = &mut neuron.visibility;
+        if visibility.is_none() {
+            *visibility = Some(Visibility::Private as i32);
         }
     }
 
@@ -14960,74 +14755,33 @@ fn test_neuron_info_private_enforcement() {
         }
     };
 
-    // Case A: Private neurons are enforced.
-    {
-        let _restore_on_drop = temporarily_enable_private_neuron_enforcement();
-        assert!(is_private_neuron_enforcement_enabled());
+    let neuron_id_to_expect_redact = vec![
+        (no_explicit_visibility_neuron.id.unwrap(), true),
+        (private_neuron.id.unwrap(), true),
+        // Unlike the previous two lines, expect_redact is false here,
+        // because these neurons are public (either explicitly, or by being
+        // a known neuron).
+        (public_neuron.id.unwrap(), false),
+        (known_neuron.id.unwrap(), false),
+    ];
+    main(neuron_id_to_expect_redact);
 
-        let neuron_id_to_expect_redact = vec![
-            (no_explicit_visibility_neuron.id.unwrap(), true),
-            (private_neuron.id.unwrap(), true),
-            // Unlike the previous two lines, expect_redact is false here,
-            // because these neurons are public (either explicitly, or by being
-            // a known neuron).
-            (public_neuron.id.unwrap(), false),
-            (known_neuron.id.unwrap(), false),
-        ];
-        main(neuron_id_to_expect_redact);
-
-        for (neuron_id, expected_visibility) in [
-            (
-                no_explicit_visibility_neuron.id.unwrap(),
-                Visibility::Private,
-            ),
-            (private_neuron.id.unwrap(), Visibility::Private),
-            (public_neuron.id.unwrap(), Visibility::Public),
-            (known_neuron.id.unwrap(), Visibility::Public),
-        ] {
-            let neuron_info = governance.get_neuron_info(&neuron_id, controller).unwrap();
-            assert_eq!(
-                neuron_info.visibility,
-                Some(expected_visibility as i32),
-                "{:?}",
-                neuron_info,
-            );
-        }
-    }
-
-    // Case B: Private neurons are NOT enforced.
-    {
-        // Here, private neuron enforcement is DISABLED, unlike the previous
-        // block/case.
-        let _restore_on_drop = temporarily_disable_private_neuron_enforcement();
-        assert!(!is_private_neuron_enforcement_enabled());
-
-        let neuron_id_to_expect_redact = vec![
-            // Here, expect_redact is false, unlike the previous block/case)
-            // where expect_redact is true for these neurons.
-            (no_explicit_visibility_neuron.id.unwrap(), false),
-            (private_neuron.id.unwrap(), false),
-            // Same as in the previous block/case.
-            (public_neuron.id.unwrap(), false),
-            (known_neuron.id.unwrap(), false),
-        ];
-        main(neuron_id_to_expect_redact);
-
-        // Insepct visibility. This time, the no explicit visibility neuron is shown as such.
-        for (neuron_id, expected_visibility) in [
-            (no_explicit_visibility_neuron.id.unwrap(), None),
-            (private_neuron.id.unwrap(), Some(Visibility::Private)),
-            (public_neuron.id.unwrap(), Some(Visibility::Public)),
-            (known_neuron.id.unwrap(), Some(Visibility::Public)),
-        ] {
-            let neuron_info = governance.get_neuron_info(&neuron_id, controller).unwrap();
-            assert_eq!(
-                neuron_info.visibility,
-                expected_visibility.map(|visibility| visibility as i32),
-                "{:?}",
-                neuron_info,
-            );
-        }
+    for (neuron_id, expected_visibility) in [
+        (
+            no_explicit_visibility_neuron.id.unwrap(),
+            Visibility::Private,
+        ),
+        (private_neuron.id.unwrap(), Visibility::Private),
+        (public_neuron.id.unwrap(), Visibility::Public),
+        (known_neuron.id.unwrap(), Visibility::Public),
+    ] {
+        let neuron_info = governance.get_neuron_info(&neuron_id, controller).unwrap();
+        assert_eq!(
+            neuron_info.visibility,
+            Some(expected_visibility as i32),
+            "{:?}",
+            neuron_info,
+        );
     }
 }
 
