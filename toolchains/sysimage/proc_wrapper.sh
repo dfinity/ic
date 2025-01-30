dca239ef78603313c22f34db4e2aec0a7f99c4ae
David Frank <david.frank@dfinity.org>
2025-01-30 16:07:19 +0000

    wip

--- toolchains/sysimage/proc_wrapper.sh
+++ toolchains/sysimage/proc_wrapper.sh
@@ -10,15 +10,10 @@ tmpdir=$(mktemp -d --tmpdir "icosbuildXXXX")
 trap 'sudo rm -rf "$tmpdir"' INT TERM EXIT
 ICOS_TMPDIR="$tmpdir" "$@"
 
-start_time=$(date +%s.%N)
 # Calculate the checksum for every output created by Bazel. For the calculation, we use icsum which is much
 # faster than Bazel's built-in checksum for sparse files (e.g. disk images).
-for arg in $@; do
-    if [[ -w "$arg" ]] && ! getfattr -n user.icsum "$arg" > /dev/null 2>&1; then
+for arg in ${@:2}; do
+    if [[ -f "$arg" ]] && [[ -w "$arg" ]] && ! getfattr -n user.icsum "$arg" >/dev/null 2>&1; then
         setfattr -n user.icsum -v $(icsum "$arg") "$arg"
     fi
 done
-
-end_time=$(date +%s.%N)
-elapsed_time=$(echo "$end_time - $start_time" | bc)
-echo "x Elapsed time: $elapsed_time seconds"
