88054f0845b547442b997e105e82c96265222216
Daniel Sharifi <daniel.sharifi@dfinity.org>
2025-01-30 15:19:13 +0000

    use performance nodes and increase resources

--- rs/tests/consensus/liveness_with_equivocation_test.rs
+++ rs/tests/consensus/liveness_with_equivocation_test.rs
@@ -1,7 +1,11 @@
 use ic_consensus_system_test_liveness_test_common::test;
 use ic_registry_subnet_type::SubnetType;
+use ic_system_test_driver::driver::farm::HostFeature;
 use ic_system_test_driver::driver::group::SystemTestGroup;
-use ic_system_test_driver::driver::ic::{InternetComputer, Subnet};
+use ic_system_test_driver::driver::ic::{
+    AmountOfMemoryKiB, ImageSizeGiB, InternetComputer, NrOfVCPUs, Subnet, VmAllocationStrategy,
+    VmResources,
+};
 use ic_system_test_driver::driver::prometheus_vm::{HasPrometheus, PrometheusVm};
 use ic_system_test_driver::driver::test_env::TestEnv;
 use ic_system_test_driver::systest;
@@ -10,6 +14,12 @@ use ic_types::malicious_behaviour::MaliciousBehaviour;
 use anyhow::Result;
 
 fn setup(env: TestEnv) {
+    let resources = VmResources {
+        vcpus: Some(NrOfVCPUs::new(64)),
+        memory_kibibytes: Some(AmountOfMemoryKiB::new(33560000)), // 32GiB
+        boot_image_minimal_size_gibibytes: Some(ImageSizeGiB::new(500)),
+    };
+
     let malicious_behaviour =
         MaliciousBehaviour::new(true).set_maliciously_propose_equivocating_blocks();
     PrometheusVm::default()
@@ -21,6 +31,8 @@ fn setup(env: TestEnv) {
                 .add_nodes(11)
                 .add_malicious_nodes(2, malicious_behaviour),
         )
+        .with_required_host_features(vec![HostFeature::Performance])
+        .with_default_vm_resources(resources)
         .setup_and_start(&env)
         .expect("failed to setup IC under test");
     env.sync_with_prometheus();
