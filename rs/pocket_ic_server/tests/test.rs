dafc18f388879f000d6341264cac15fb167353a9
Pietro <pietro.di.marco@dfinity.org>
2025-01-31 15:35:37 +0100

    Add tests

--- rs/pocket_ic_server/tests/test.rs
+++ rs/pocket_ic_server/tests/test.rs
@@ -12,6 +12,7 @@ use ic_registry_proto_data_provider::ProtoRegistryDataProvider;
 use ic_registry_transport::pb::v1::{
     registry_mutation::Type, RegistryAtomicMutateRequest, RegistryMutation,
 };
+use ic_types::PrincipalId;
 use ic_utils::interfaces::ManagementCanister;
 use nix::sys::signal::Signal;
 use pocket_ic::common::rest::{
@@ -1480,4 +1481,75 @@ fn test_gateway_address_in_use() {
         "Failed to bind to address 127.0.0.1:{}: Address already in use",
         port
     )));
+}
+
+fn test_canister_wasm() -> Vec<u8> {
+    let wasm_path = std::env::var_os("TEST_WASM").expect("Missing test canister wasm file");
+    std::fs::read(wasm_path).unwrap()
+}
+
+#[test]
+fn test_custom_blockmaker() {
+    const INIT_CYCLES: u128 = 2_000_000_000_000;
+
+    let pocket_ic = PocketIcBuilder::new().with_application_subnet().build();
+    let topology = pocket_ic.topology();
+    let application_subnet = topology.get_app_subnets()[0];
+
+    // Create and install test canister.
+    let canister = pocket_ic.create_canister_on_subnet(None, None, application_subnet);
+    pocket_ic.add_cycles(canister, INIT_CYCLES);
+    pocket_ic.install_canister(canister, test_canister_wasm(), vec![], None);
+
+    let blockmaker_node = topology
+        .subnet_configs
+        .get(&application_subnet)
+        .unwrap()
+        .node_ids[0]
+        .clone();
+
+    let mut subnets_blockmakers = vec![RawSubnetBlockmakerMetrics {
+        subnet: application_subnet.into(),
+        blockmaker: blockmaker_node.clone(),
+        failed_blockmakers: vec![],
+    }];
+    let mut tick_configs = TickConfigs {
+        blockmakers: Some(BlockMakerConfigs {
+            blockmakers_per_subnet: subnets_blockmakers,
+        }),
+    };
+
+    for _ in 0..5 {
+        pocket_ic.tick_with_configs(tick_configs.clone());
+    }
+
+    //advance time until next day so that management canister can record blockmaker metrics
+    pocket_ic.advance_time(std::time::Duration::from_secs(60 * 60 * 24));
+
+    pocket_ic.tick();
+    pocket_ic.tick();
+
+    let response = pocket_ic
+        .update_call(
+            canister,
+            Principal::anonymous(),
+            "node_metrics_history_proxy",
+            Encode!(&NodeMetricsHistoryArgs {
+                subnet_id: PrincipalId::from(application_subnet),
+                start_at_timestamp_nanos: 0,
+            })
+            .unwrap(),
+        )
+        .unwrap();
+
+    let metrics = Decode!(&response, Vec<NodeMetricsHistoryResponse>).unwrap();
+    let blockmaker_node_metrics = metrics[0]
+        .node_metrics
+        .clone()
+        .into_iter()
+        .find(|x| x.node_id.0 == Principal::from(blockmaker_node.clone()))
+        .unwrap();
+
+    assert_eq!(blockmaker_node_metrics.num_blocks_proposed_total, 5);
+    assert_eq!(blockmaker_node_metrics.num_block_failures_total, 0);
 }
