994ab12a51997c0cab3db549cc95b452680a252b
Paul H. Liu <paul@dfinity.org>
2025-01-31 00:51:27 -0800

    Add a test that dumps stable memory after replaying all events

--- rs/bitcoin/ckbtc/minter/src/storage.rs
+++ rs/bitcoin/ckbtc/minter/src/storage.rs
@@ -173,3 +173,50 @@ pub fn record_event<R: CanisterRuntime>(payload: EventType, runtime: &R) {
             .expect("failed to append an entry to the event log");
     })
 }
+
+pub fn record_event_v0<R: CanisterRuntime>(payload: EventType, runtime: &R) {
+    let bytes = encode_event(&Event {
+        timestamp: Some(runtime.time()),
+        payload,
+    });
+    V0_EVENTS.with(|events| {
+        events
+            .borrow()
+            .append(&bytes)
+            .expect("failed to append an entry to the event log");
+    })
+}
+
+#[cfg(feature = "canbench-rs")]
+mod benches {
+    use super::*;
+    use canbench_rs::bench;
+
+    #[bench(raw)]
+    fn migrate_events_bench() -> canbench_rs::BenchResult {
+        // These thread local state must be re-initialized after
+        // canbench loads the stable memory from a file.
+        MEMORY_MANAGER
+            .with(|x| *x.borrow_mut() = MemoryManager::init(DefaultMemoryImpl::default()));
+        V0_EVENTS.with(|x| {
+            *x.borrow_mut() = MEMORY_MANAGER.with(|m| {
+                StableLog::init(
+                    m.borrow().get(V0_LOG_INDEX_MEMORY_ID),
+                    m.borrow().get(V0_LOG_DATA_MEMORY_ID),
+                )
+                .expect("failed to initialize stable log")
+            })
+        });
+        V1_EVENTS.with(|x| {
+            *x.borrow_mut() = MEMORY_MANAGER.with(|m| {
+                StableLog::init(
+                    m.borrow().get(V1_LOG_INDEX_MEMORY_ID),
+                    m.borrow().get(V1_LOG_DATA_MEMORY_ID),
+                )
+                .expect("failed to initialize stable log")
+            })
+        });
+
+        canbench_rs::bench_fn(migrate_old_events_if_not_empty)
+    }
+}
