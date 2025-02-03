d3a56dc1f8a96b56800c2431f462083d88962477
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-02-03 17:14:07 +0100

    changing the model

--- rs/nns/handlers/recovery/impl/src/update_nns_status_proposal.rs
+++ rs/nns/handlers/recovery/impl/src/update_nns_status_proposal.rs
@@ -1,40 +1,45 @@
+use std::cell::RefCell;
+
+use candid::CandidType;
 use ic_base_types::{NodeId, PrincipalId};
+use serde::Deserialize;
 
-pub enum NnsHealthStatus {
-    Healthy,
-    Unhealthy,
-}
-
+#[derive(Clone, Debug, CandidType, Deserialize, PartialEq, Eq)]
 pub enum Ballot {
     Yes,
     No,
     Undecided,
 }
 
+#[derive(Clone, Debug, CandidType, Deserialize)]
 pub struct NodeOperatorBallot {
     pub principal: PrincipalId,
-    pub node_tied_to_ballot: NodeId,
+    pub nodes_tied_to_ballot: Vec<NodeId>,
     pub ballot: Ballot,
     pub signature: Vec<u8>,
 }
 
-pub struct RecoveryProposalDetails {
-    /// Should be equal to recovery proposal
-    pub payload: String,
+#[derive(Clone, Debug, CandidType, Deserialize)]
+pub enum RecoveryPayload {
+    Halt,
+    DoRecovery { height: u64, state_hash: String },
+    Unhalt,
+}
 
+#[derive(Clone, Debug, CandidType, Deserialize)]
+pub struct RecoveryProposal {
     /// The principal id of the proposer (must be one of the node
     /// operators of the NNS subnet according to the registry at
     /// time of submission).
     pub proposer: PrincipalId,
-
     /// The timestamp, in seconds, at which the proposal was submitted.
     pub submission_timestamp_seconds: u64,
-
-    /// List containing the
+    /// The ballots cast by node operators.
     pub node_operator_ballots: Vec<NodeOperatorBallot>,
+    /// Payload for the proposal
+    pub payload: RecoveryPayload,
 }
 
-pub struct UpdateNnsHealthStatus {
-    pub status: NnsHealthStatus,
-    pub recovery_proposal: Option<RecoveryProposalDetails>,
+thread_local! {
+  static PROPOSALS: RefCell<Vec<RecoveryProposal>> = const { RefCell::new(Vec::new()) };
 }
