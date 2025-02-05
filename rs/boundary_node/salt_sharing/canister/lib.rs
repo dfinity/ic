28c8a072a5941ac5e01d681aa897a0781a3460e3
Nikolay Komarevskiy <90605504+nikolay-komarevskiy@users.noreply.github.com>
2025-02-05 16:14:48 +0100

    chore(boundary): add metrics and access control to `salt_sharing` canister (#3762)
    **Changes**:
    - added metrics
    - bazelification
    - access control, only API boundary nodes can call the `get_salt()`
    - `inspect_message` for early rejections of replicated query calls
    - added polling of API boundary nodes
    - improved structure by moving implementation of `/logs`
    
    **How to test**:
    ```
    // deploy with dfx
    $ dfx deploy salt_sharing -m reinstall --playground --argument \          
        '(record {   
            regenerate_now = true;
            salt_generation_strategy = variant { StartOfMonth };
            registry_polling_interval_secs = 60;
        })'
    ```
    **Check metrics**:
    ```
    https://{canister_id}.raw.icp0.io/metrics
    ```
    **Check logs**:
    ```
    https://{canister_id}.raw.icp0.io/logs
    ```
    
    ---------
    
    Co-authored-by: IDX GitLab Automation <idx@dfinity.org>
    Co-authored-by: r-birkner <103420898+r-birkner@users.noreply.github.com>

--- rs/boundary_node/salt_sharing/canister/lib.rs
+++ rs/boundary_node/salt_sharing/canister/lib.rs
@@ -1,6 +1,12 @@
 #[cfg(any(target_family = "wasm", test))]
 mod canister;
+#[allow(dead_code)]
+#[cfg(any(target_family = "wasm", test))]
+mod helpers;
+#[allow(dead_code)]
 mod logs;
+#[allow(dead_code)]
+mod metrics;
 #[allow(dead_code)]
 mod storage;
 #[allow(dead_code)]
