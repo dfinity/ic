dd83602db52243f70681459ac7c45f2d9bc2191e
Daniel Wong <daniel.wong@dfinity.org>
2025-02-05 21:09:08 +0100

    Make tests build.

--- rs/nns/governance/src/governance/tests/mod.rs
+++ rs/nns/governance/src/governance/tests/mod.rs
@@ -22,7 +22,10 @@ use ic_sns_init::pb::v1::SnsInitPayload;
 use ic_sns_init::pb::v1::{self as sns_init_pb};
 use lazy_static::lazy_static;
 use maplit::{btreemap, hashmap};
-use std::convert::TryFrom;
+use std::{
+    convert::TryFrom,
+    time::Duration,
+};
 
 mod list_neurons;
 mod neurons_fund;
