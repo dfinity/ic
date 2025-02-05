bae1dd5a291bcad344e1922a8ac2da0912d64f25
Mihail Jianu <mihail.jianu@dfinity.org>
2025-02-05 14:13:59 +0000

    break -> continue

--- rs/bitcoin/adapter/src/blockchainmanager.rs
+++ rs/bitcoin/adapter/src/blockchainmanager.rs
@@ -595,7 +595,12 @@ impl BlockchainManager {
             }
 
             if selected_inventory.is_empty() {
-                break;
+                // Technically peers are ordered on the number of availble requests they have, so 
+                // if for example the first peer has an empty inventory, that means that all subsequent
+                // peers will have an empty inventory as well.
+                // However, breaking here is a bit hard to read and an "optimization hack", which doesn't 
+                // bring a lot of value. Explicitly trying all peers instead is clearer.
+                continue;
             }
 
             trace!(
