5c871d43d4a9e3277eb6d987689d4dd932e30771
Mihail Jianu <mihail.jianu@dfinity.org>
2025-02-05 11:53:32 +0000

    prune blocks if response empty

--- rs/bitcoin/adapter/src/blockchainstate.rs
+++ rs/bitcoin/adapter/src/blockchainstate.rs
@@ -137,6 +137,11 @@ impl BlockchainState {
         self.header_cache.get(hash)
     }
 
+    /// Returns the hashes of all cached blocks.
+    pub fn get_cached_blocks(&self) -> Vec<BlockHash> {
+        self.block_cache.keys().cloned().collect()
+    }
+
     /// Processes the `headers` message received from Bitcoin nodes by adding them to the state.
     /// Headers are expected to be sorted. If they are not, the headers will be likely be rejected
     /// with a [AddHeaderError::PrevHeaderNotCached](AddHeaderError::PrevHeaderNotCached) error.
