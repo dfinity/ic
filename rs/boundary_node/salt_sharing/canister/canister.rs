9a2f72dd9d568d00a878001e7cd2de159b0708af
IDX GitLab Automation <idx@dfinity.org>
2025-02-05 10:48:34 +0000

    chore: add candid compatibility test

--- rs/boundary_node/salt_sharing/canister/canister.rs
+++ rs/boundary_node/salt_sharing/canister/canister.rs
@@ -72,3 +72,61 @@ fn http_request(request: HttpRequest) -> HttpResponse {
         _ => HttpResponseBuilder::not_found().build(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_candid_interface_compatibility() {
+        use candid_parser::utils::{service_equal, CandidSource};
+
+        fn source_to_str(source: &CandidSource) -> String {
+            match source {
+                CandidSource::File(f) => {
+                    std::fs::read_to_string(f).unwrap_or_else(|_| "".to_string())
+                }
+                CandidSource::Text(t) => t.to_string(),
+            }
+        }
+
+        fn check_service_equal(
+            new_name: &str,
+            new: CandidSource,
+            old_name: &str,
+            old: CandidSource,
+        ) {
+            let new_str = source_to_str(&new);
+            let old_str = source_to_str(&old);
+            match service_equal(new, old) {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!(
+                        "{} is not compatible with {}!\n\n\
+                {}:\n\
+                {}\n\n\
+                {}:\n\
+                {}\n",
+                        new_name, old_name, new_name, new_str, old_name, old_str
+                    );
+                    panic!("{:?}", e);
+                }
+            }
+        }
+
+        candid::export_service!();
+
+        let new_interface = __export_service();
+
+        // check the public interface against the actual one
+        let old_interface = std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
+            .join("canister/salt_sharing_canister.did");
+
+        check_service_equal(
+            "actual rate-limit candid interface",
+            candid_parser::utils::CandidSource::Text(&new_interface),
+            "declared candid interface in interface.did file",
+            candid_parser::utils::CandidSource::File(old_interface.as_path()),
+        );
+    }
+}
