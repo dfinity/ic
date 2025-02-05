e6495b1d37c55669edf62ac6a1a1b57ddc62d5ba
Maciej Modelski <maciej.modelski@dfinity.org>
2025-02-05 15:40:47 +0000

    add tests from mainnet

--- rs/ledger_suite/icrc1/ledger/tests/tests.rs
+++ rs/ledger_suite/icrc1/ledger/tests/tests.rs
@@ -647,6 +647,15 @@ fn test_stable_migration_endpoints_disabled(ledger_wasm_mainnet: Vec<u8>) {
 }
 
 #[test]
+fn icrc1_test_incomplete_migration_from_mainnet() {
+    ic_ledger_suite_state_machine_tests::test_incomplete_migration(
+        ledger_mainnet_wasm(),
+        ledger_wasm_lowupgradeinstructionlimits(),
+        encode_init_args_with_large_archive_trigger_threshold,
+    );
+}
+
+#[test]
 fn icrc1_test_incomplete_migration_from_v3() {
     ic_ledger_suite_state_machine_tests::test_incomplete_migration(
         ledger_mainnet_v3_wasm(),
@@ -670,6 +679,15 @@ fn icrc1_test_incomplete_migration_from_v2_noledgerversion() {
         ledger_mainnet_v2_noledgerversion_wasm(),
         ledger_wasm_lowupgradeinstructionlimits(),
         encode_init_args,
+    );
+}
+
+#[test]
+fn icrc1_test_incomplete_migration_to_current_from_mainnet() {
+    ic_ledger_suite_state_machine_tests::test_incomplete_migration_to_current(
+        ledger_mainnet_wasm(),
+        ledger_wasm_lowupgradeinstructionlimits(),
+        encode_init_args_with_large_archive_trigger_threshold,
     );
 }
 
@@ -701,6 +719,15 @@ fn icrc1_test_incomplete_migration_to_current_from_v2_noledgerversion() {
 }
 
 #[test]
+fn icrc1_test_migration_resumes_from_frozen_from_mainnet() {
+    ic_ledger_suite_state_machine_tests::test_migration_resumes_from_frozen(
+        ledger_mainnet_wasm(),
+        ledger_wasm_lowupgradeinstructionlimits(),
+        encode_init_args,
+    );
+}
+
+#[test]
 fn icrc1_test_migration_resumes_from_frozen_from_v3() {
     ic_ledger_suite_state_machine_tests::test_migration_resumes_from_frozen(
         ledger_mainnet_v3_wasm(),
@@ -715,6 +742,15 @@ fn icrc1_test_migration_resumes_from_frozen_from_v2() {
         ledger_mainnet_v2_wasm(),
         ledger_wasm_lowupgradeinstructionlimits(),
         encode_init_args,
+    );
+}
+
+#[test]
+fn icrc1_test_metrics_while_migrating_from_mainnet() {
+    ic_ledger_suite_state_machine_tests::test_metrics_while_migrating(
+        ledger_mainnet_wasm(),
+        ledger_wasm_lowupgradeinstructionlimits(),
+        encode_init_args_with_large_archive_trigger_threshold,
     );
 }
 
