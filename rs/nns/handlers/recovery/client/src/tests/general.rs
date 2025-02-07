3e1b83c0e8d49b847224e74a9b5956cddd0c5f8b
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-02-07 03:00:39 +0100

    adding proposal placement tests

--- rs/nns/handlers/recovery/client/src/tests/general.rs
+++ rs/nns/handlers/recovery/client/src/tests/general.rs
@@ -1,9 +1,12 @@
 use std::sync::Arc;
 
 use ed25519_dalek::SigningKey as EdSigningKey;
-use ic_agent::identity::BasicIdentity;
-use ic_nns_handler_recovery_interface::signing::{ed25519::EdwardsCurve, Verifier};
-use p256::elliptic_curve::rand_core::OsRng;
+use ic_agent::identity::{BasicIdentity, Prime256v1Identity};
+use ic_nns_handler_recovery_interface::{
+    recovery::{NewRecoveryProposal, RecoveryPayload},
+    signing::{ed25519::EdwardsCurve, p256::Prime256, Verifier},
+};
+use p256::{elliptic_curve::rand_core::OsRng, SecretKey};
 
 use crate::{
     implementation::RecoveryCanisterImpl,
@@ -38,27 +41,61 @@ async fn can_get_node_operators() {
     assert!(current_operators.len().eq(&node_operators_with_keys.len()))
 }
 
-// #[tokio::test]
-// async fn can_place_proposals() {
-//     let node_operators_with_keys = generate_node_operators();
-//     let (mut pic, canister) =
-//         init_pocket_ic(preconfigured_recovery_init_args(&node_operators_with_keys)).await;
+#[tokio::test]
+async fn can_place_proposals_edwards() {
+    let key = EdSigningKey::generate(&mut OsRng);
+    let signer = EdwardsCurve::new(key.clone());
 
-//     let mut node_operator_iter = node_operators_with_keys.iter();
-//     let first = node_operator_iter.next().unwrap();
-//     let first_client = first
-//         .into_recovery_canister_client(&mut pic, canister)
-//         .await;
+    let node_operators_with_keys =
+        generate_node_operators(vec![signer.to_public_key_der().unwrap()]);
+    let (pic, canister) =
+        init_pocket_ic(preconfigured_recovery_init_args(&node_operators_with_keys)).await;
 
-//     let response = first_client
-//         .submit_new_recovery_proposal(NewRecoveryProposal {
-//             payload: RecoveryPayload::Halt,
-//         })
-//         .await;
+    let identity = BasicIdentity::from_signing_key((key.to_bytes()).into());
 
-//     println!("{:?}", response);
-//     assert!(response.is_ok());
-// }
+    let client = RecoveryCanisterImpl::new(
+        get_ic_agent(Box::new(identity), pic.url().unwrap().as_str()).await,
+        canister,
+        Arc::new(signer),
+    );
+
+    let response = client
+        .submit_new_recovery_proposal(NewRecoveryProposal {
+            payload: RecoveryPayload::Halt,
+        })
+        .await;
+
+    assert!(response.is_ok());
+}
+
+#[tokio::test]
+async fn can_place_proposals_prime256() {
+    let secret_key = SecretKey::random(&mut OsRng);
+    let signing_key = secret_key.clone().into();
+
+    let signer = Prime256::new(signing_key);
+
+    let node_operators_with_keys =
+        generate_node_operators(vec![signer.to_public_key_der().unwrap()]);
+    let (pic, canister) =
+        init_pocket_ic(preconfigured_recovery_init_args(&node_operators_with_keys)).await;
+
+    let identity = Prime256v1Identity::from_private_key(secret_key);
+
+    let client = RecoveryCanisterImpl::new(
+        get_ic_agent(Box::new(identity), pic.url().unwrap().as_str()).await,
+        canister,
+        Arc::new(signer),
+    );
+
+    let response = client
+        .submit_new_recovery_proposal(NewRecoveryProposal {
+            payload: RecoveryPayload::Halt,
+        })
+        .await;
+
+    assert!(response.is_ok());
+}
 
 // #[tokio::test]
 // async fn can_vote_on_proposals() {
