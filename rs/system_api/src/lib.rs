23d6bcbdc0b11d0ffd88855bb7dc5165bb40f22c
Michael Weigelt <michael.weigelt@dfinity.org>
2025-02-10 12:12:29 +0000

    Revert "implement replication factor"
    This reverts commit ba7f1000666b8532319028543ddc45ca37d97630.

--- rs/system_api/src/lib.rs
+++ rs/system_api/src/lib.rs
@@ -3523,17 +3523,7 @@ impl SystemApi for SystemApiImpl {
         size: usize,
         heap: &[u8],
     ) -> HypervisorResult<u32> {
-        let msg_bytes = valid_subslice("ic0_replication_factor", src, size, heap)?;
-        let subnet_id = PrincipalId::try_from(msg_bytes)
-            .map_err(|e| HypervisorError::InvalidPrincipalId(PrincipalIdBlobParseError(e.0)))?;
-        let result = self
-            .sandbox_safe_system_state
-            .network_topology
-            .get_subnet_size(&subnet_id.into())
-            .map(|x| x as u32)
-            .ok_or(HypervisorError::SubnetNotFound);
-        trace_syscall!(self, ReplicationFactor, result);
-        result
+        todo!()
     }
 
     fn ic0_cost_call(
