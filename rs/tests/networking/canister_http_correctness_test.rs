6242659c93c59f08e4db12a29b4cb286bc1e96de
Daniel Sharifi <daniel.sharifi@dfinity.org>
2025-02-10 14:44:28 +0000

    .

--- rs/tests/networking/canister_http_correctness_test.rs
+++ rs/tests/networking/canister_http_correctness_test.rs
@@ -96,54 +96,53 @@ fn main() -> Result<()> {
         .with_setup(canister_http::setup)
         .add_parallel(
             SystemTestSubGroup::new()
-                .add_test(systest!(test_enforce_https))
-                .add_test(systest!(test_transform_function_is_executed))
-                .add_test(systest!(test_composite_transform_function_is_executed))
-                .add_test(systest!(test_no_cycles_attached))
-                .add_test(systest!(test_2mb_response_cycle_for_rejection_path))
-                .add_test(systest!(test_4096_max_response_cycle_case_1))
-                .add_test(systest!(test_4096_max_response_cycle_case_2))
-                .add_test(systest!(test_max_response_limit_too_large))
-                .add_test(systest!(
-                    test_transform_that_bloats_response_above_2mb_limit
-                ))
-                .add_test(systest!(test_non_existing_transform_function))
-                .add_test(systest!(test_post_request))
-                .add_test(systest!(test_http_endpoint_response_is_too_large))
-                .add_test(systest!(
-                    test_http_endpoint_with_delayed_response_is_rejected
-                ))
-                .add_test(systest!(test_that_redirects_are_not_followed))
-                .add_test(systest!(test_http_calls_to_ic_fails))
-                .add_test(systest!(test_invalid_domain_name))
-                .add_test(systest!(test_invalid_ip))
-                .add_test(systest!(test_get_hello_world_call))
-                .add_test(systest!(test_post_call))
-                .add_test(systest!(test_head_call))
-                .add_test(systest!(test_max_possible_request_size))
-                .add_test(systest!(test_max_possible_request_size_exceeded))
-                .add_test(systest!(test_non_ascii_url_is_rejected))
-                .add_test(systest!(test_max_url_length))
-                .add_test(systest!(test_max_url_length_exceeded))
-                .add_test(systest!(
-                    test_small_maximum_possible_response_size_only_headers
-                ))
-                .add_test(systest!(
-                    test_small_maximum_possible_response_size_exceeded_only_headers
-                ))
-                .add_test(systest!(test_large_maximum_response_size))
-                .add_test(systest!(test_maximum_possible_value_of_max_response_bytes))
-                .add_test(systest!(
-                    test_maximum_possible_value_of_max_response_bytes_exceeded
-                ))
-                .add_test(systest!(check_caller_id_on_transform_function))
-                .add_test(systest!(
-                    reference_transform_function_exposed_by_different_canister
-                ))
-                .add_test(systest!(test_max_number_of_request_headers))
-                .add_test(systest!(test_max_number_of_request_headers_exceeded))
-                .add_test(systest!(test_max_number_of_response_headers))
-                .add_test(systest!(test_max_number_of_response_headers_exceeded)),
+                // .add_test(systest!(test_enforce_https))
+                // .add_test(systest!(test_transform_function_is_executed))
+                // .add_test(systest!(test_composite_transform_function_is_executed))
+                // .add_test(systest!(test_no_cycles_attached))
+                // .add_test(systest!(test_2mb_response_cycle_for_rejection_path))
+                // .add_test(systest!(test_4096_max_response_cycle_case_1))
+                // .add_test(systest!(test_4096_max_response_cycle_case_2))
+                // .add_test(systest!(test_max_response_limit_too_large))
+                // .add_test(systest!(
+                //     test_transform_that_bloats_response_above_2mb_limit
+                // ))
+                // .add_test(systest!(test_non_existing_transform_function))
+                // .add_test(systest!(test_post_request))
+                // .add_test(systest!(test_http_endpoint_response_is_too_large))
+                // .add_test(systest!(
+                //     test_http_endpoint_with_delayed_response_is_rejected
+                // ))
+                // .add_test(systest!(test_that_redirects_are_not_followed))
+                // .add_test(systest!(test_http_calls_to_ic_fails))
+                // .add_test(systest!(test_invalid_domain_name))
+                // .add_test(systest!(test_invalid_ip))
+                // .add_test(systest!(test_get_hello_world_call))
+                // .add_test(systest!(test_post_call))
+                // .add_test(systest!(test_head_call))
+                // .add_test(systest!(test_max_possible_request_size))
+                // .add_test(systest!(test_max_possible_request_size_exceeded))
+                // .add_test(systest!(test_non_ascii_url_is_rejected))
+                // .add_test(systest!(test_max_url_length))
+                // .add_test(systest!(test_max_url_length_exceeded))
+                // .add_test(systest!(
+                //     test_small_maximum_possible_response_size_only_headers
+                // ))
+                // .add_test(systest!(
+                //     test_small_maximum_possible_response_size_exceeded_only_headers
+                // ))
+                // .add_test(systest!(test_large_maximum_response_size))
+                // .add_test(systest!(test_maximum_possible_value_of_max_response_bytes))
+                // .add_test(systest!(
+                //     test_maximum_possible_value_of_max_response_bytes_exceeded
+                // ))
+                // .add_test(systest!(check_caller_id_on_transform_function))
+                // .add_test(systest!(
+                //     reference_transform_function_exposed_by_different_canister
+                // ))
+                // .add_test(systest!(test_max_number_of_request_headers))
+                .add_test(systest!(test_max_number_of_request_headers_exceeded)), // .add_test(systest!(test_max_number_of_response_headers))
+                                                                                  // .add_test(systest!(test_max_number_of_response_headers_exceeded)),
         )
         .execute_from_args()?;
 
@@ -1522,7 +1521,7 @@ fn test_max_number_of_request_headers_exceeded(env: TestEnv) {
     assert_matches!(
         response,
         Err(RejectResponse {
-            reject_code: RejectCode::CanisterError,
+            reject_code: RejectCode::CanisterReject,
             ..
         })
     );
