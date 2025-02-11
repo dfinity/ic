5c191e567bee81998922e3feb03d028bbed40820
Roman Melnikov <roman.melnikov@serokell.io>
2025-02-11 13:05:06 +0100

    sns-testing: use nervous_agent_helpers
    We want to be able SNS-specific parts of sns-testing scenario on the
    network provided either via PocketIC or via 'ic-agent' clients.
    
    The following parts of sns-testing scenario were reimplemented to use
    nervous-agent-based implementation:
    * NNS proposal creation and adoption to create the new SNS.
    * SNS swap completion.
    * SNS proposal creation and adoption to upgrade the test canister.

--- rs/sns/testing/src/main.rs
+++ rs/sns/testing/src/main.rs
@@ -1,7 +1,12 @@
+use std::path::PathBuf;
+
 use clap::Parser;
+use ic_base_types::PrincipalId;
+use ic_nervous_system_integration_tests::pocket_ic_helpers::load_registry_mutations;
 use ic_sns_testing::nns_dapp::bootstrap_nns;
 use ic_sns_testing::sns::{
-    create_sns, install_test_canister, upgrade_sns_controlled_test_canister, TestCanisterInitArgs,
+    create_sns_pocket_ic, install_test_canister, upgrade_sns_controlled_test_canister_pocket_ic,
+    TestCanisterInitArgs,
 };
 use pocket_ic::PocketIcBuilder;
 use reqwest::Url;
@@ -10,13 +15,17 @@ use reqwest::Url;
 struct SnsTestingOpts {
     #[arg(long)]
     server_url: Url,
+    #[arg(long)]
+    state_dir: PathBuf,
 }
 
 #[tokio::main]
 async fn main() {
     let opts = SnsTestingOpts::parse();
+
     let mut pocket_ic = PocketIcBuilder::new()
         .with_server_url(opts.server_url)
+        .with_state_dir(opts.state_dir.clone())
         .with_nns_subnet()
         .with_sns_subnet()
         .with_ii_subnet()
@@ -25,7 +34,19 @@ async fn main() {
         .await;
     let endpoint = pocket_ic.make_live(Some(8080)).await;
     println!("PocketIC endpoint: {}", endpoint);
-    bootstrap_nns(&pocket_ic).await;
+
+    let registry_proto_path = opts.state_dir.join("registry.proto");
+    let initial_mutations = load_registry_mutations(registry_proto_path);
+    let dev_participant_id = PrincipalId::new_user_test_id(1000);
+
+    bootstrap_nns(
+        &pocket_ic,
+        vec![initial_mutations],
+        vec![],
+        vec![dev_participant_id],
+    )
+    .await;
+
     let greeting = "Hello there".to_string();
     let test_canister_id = install_test_canister(
         &pocket_ic,
@@ -36,11 +57,12 @@ async fn main() {
     .await;
     println!("Test canister ID: {}", test_canister_id);
     println!("Creating SNS...");
-    let (sns, _nns_proposal_id) = create_sns(&pocket_ic, vec![test_canister_id]).await;
+    let sns = create_sns_pocket_ic(&pocket_ic, dev_participant_id, vec![test_canister_id]).await;
     println!("SNS created");
     println!("Upgrading SNS-controlled test canister...");
-    upgrade_sns_controlled_test_canister(
+    upgrade_sns_controlled_test_canister_pocket_ic(
         &pocket_ic,
+        dev_participant_id,
         sns,
         test_canister_id,
         TestCanisterInitArgs {
--- rs/sns/testing/tests/sns_testing_ci.rs
+++ rs/sns/testing/tests/sns_testing_ci.rs
@@ -1,20 +1,38 @@
 use candid::{Decode, Encode, Principal};
+use ic_base_types::PrincipalId;
+use ic_nervous_system_integration_tests::pocket_ic_helpers::load_registry_mutations;
 use ic_sns_testing::nns_dapp::bootstrap_nns;
 use ic_sns_testing::sns::{
-    create_sns, install_test_canister, upgrade_sns_controlled_test_canister, TestCanisterInitArgs,
+    create_sns_pocket_ic, install_test_canister, upgrade_sns_controlled_test_canister_pocket_ic,
+    TestCanisterInitArgs,
 };
 use pocket_ic::PocketIcBuilder;
+use tempfile::TempDir;
 
 #[tokio::test]
 async fn test_sns_testing_pocket_ic() {
+    let state_dir = TempDir::new().unwrap();
+    let state_dir = state_dir.path().to_path_buf();
+
     let pocket_ic = PocketIcBuilder::new()
+        .with_state_dir(state_dir.clone())
         .with_nns_subnet()
         .with_sns_subnet()
         .with_ii_subnet()
         .with_application_subnet()
         .build_async()
         .await;
-    bootstrap_nns(&pocket_ic).await;
+    let registry_proto_path = state_dir.join("registry.proto");
+    let initial_mutations = load_registry_mutations(registry_proto_path);
+    let dev_participant_id = PrincipalId::new_user_test_id(1000);
+
+    bootstrap_nns(
+        &pocket_ic,
+        vec![initial_mutations],
+        vec![],
+        vec![dev_participant_id],
+    )
+    .await;
     let greeting = "Hello there".to_string();
     let test_canister_id = install_test_canister(
         &pocket_ic,
@@ -37,10 +55,11 @@ async fn test_sns_testing_pocket_ic() {
         Decode!(&test_canister_response, String).expect("Failed to decode test canister response"),
         format!("{}, {}!", greeting, test_call_arg.clone()),
     );
-    let (sns, _nns_proposal_id) = create_sns(&pocket_ic, vec![test_canister_id]).await;
+    let sns = create_sns_pocket_ic(&pocket_ic, dev_participant_id, vec![test_canister_id]).await;
     let new_greeting = "Hi".to_string();
-    upgrade_sns_controlled_test_canister(
+    upgrade_sns_controlled_test_canister_pocket_ic(
         &pocket_ic,
+        dev_participant_id,
         sns,
         test_canister_id,
         TestCanisterInitArgs {
