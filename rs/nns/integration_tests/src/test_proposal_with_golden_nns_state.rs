89a695c0573c72637516b658c6702de776741fdd
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2025-02-12 13:33:00 +0000

    simplify

--- rs/nns/integration_tests/src/test_proposal_with_golden_nns_state.rs
+++ rs/nns/integration_tests/src/test_proposal_with_golden_nns_state.rs
@@ -24,49 +24,38 @@ fn test_proposal_with_golden_nns_state() {
     // Step 1.1: Load golden nns state into a StateMachine.
     let state_machine = new_state_machine_with_golden_nns_state_or_panic();
 
-    // Save initial state for the ultimate assertion of this test.
-    let network_economics = nns_get_network_economics_parameters(&state_machine);
-
     // Phase I. Upgrade NNS Governance to a test-only version (using a super powerful neuron) that
     // allows adopting any proposal by calling adopt_proposal.
-    {
-        let neuron_controller = PrincipalId::new_self_authenticating(&[1, 2, 3, 4]);
-        let proposer_neuron_id =
-            nns_create_super_powerful_neuron(&state_machine, neuron_controller);
 
-        println!("Proposing to upgrade NNS Governance ...");
+    let neuron_controller = PrincipalId::new_self_authenticating(&[1, 2, 3, 4]);
+    let proposer_neuron_id =
+        nns_create_super_powerful_neuron(&state_machine, neuron_controller);
 
-        let _proposal_id = nns_propose_upgrade_nns_canister(
-            &state_machine,
-            neuron_controller,
-            proposer_neuron_id,
-            nns_canister_upgrade.canister_id,
-            nns_canister_upgrade.wasm_content.clone(),
-            nns_canister_upgrade.module_arg.clone(),
-            true,
-        );
+    println!("Proposing to upgrade NNS Governance ...");
 
-        // Step 3: Verify result(s): In a short while, the canister should be running the new code.
-        wait_for_canister_upgrade_to_succeed(
-            &state_machine,
-            nns_canister_upgrade.canister_id,
-            &nns_canister_upgrade.wasm_hash,
-            nns_canister_upgrade.controller_principal_id(),
-        );
+    let _proposal_id = nns_propose_upgrade_nns_canister(
+        &state_machine,
+        neuron_controller,
+        proposer_neuron_id,
+        nns_canister_upgrade.canister_id,
+        nns_canister_upgrade.wasm_content.clone(),
+        nns_canister_upgrade.module_arg.clone(),
+        true,
+    );
 
-        println!("Upgrading NNS Governance to a test version succeeded!");
-    }
+    // Step 3: Verify result(s): In a short while, the canister should be running the new code.
+    wait_for_canister_upgrade_to_succeed(
+        &state_machine,
+        nns_canister_upgrade.canister_id,
+        &nns_canister_upgrade.wasm_hash,
+        nns_canister_upgrade.controller_principal_id(),
+    );
+
+    println!("Upgrading NNS Governance to a test version succeeded!");
 
     // Phase II. Test that adopt_proposal now works for another proposal (so we no longer need the
     // super powerful neuron).
     {
-        let neuron_controller = PrincipalId::new_self_authenticating(&[5, 6, 7, 8]);
-        let proposer_neuron_id = nns_create_neuron_with_stake(
-            &state_machine,
-            neuron_controller,
-            Tokens::from_e8s(network_economics.neuron_minimum_stake_e8s),
-        );
-
         /*
         let proposal_id = if let Ok(proposal_id) = env::var("NNS_PROPOSAL_ID_TO_ADOPT") {
             let id = proposal_id
