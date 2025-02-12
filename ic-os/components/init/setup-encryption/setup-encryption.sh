034279e47e0acdcffdd44702c724f68d1c2c077b
Alex Uta <alexandru.uta@gmail.com>
2025-02-12 11:47:26 +0000

    Fix script.

--- ic-os/components/init/setup-encryption/setup-encryption.sh
+++ ic-os/components/init/setup-encryption/setup-encryption.sh
@@ -6,45 +6,29 @@ if [ -e /dev/vda10 ]; then
     exit 0
 fi
 
-# this command creates a partition that is not aligned to 4k sectors.
-# however we need alignment to 4k sectors for the encryption to work.
-# so we need to create a partition that is aligned to 4k sectors.
+DISK="/dev/vda"
+PARTITION="/dev/vda10"
 
-# -n will not commit the changes to the disk. It will only print the changes.
-echo "- - L" | sfdisk -n --force --no-reread -a /dev/vda
-# check the start and end sectors of the partition.
-# the output looks like this:
-# Device          Start         End     Sectors  Size Type
-# /dev/vda1        2048      206847      204800  100M EFI System
-# /dev/vda2      206848      411647      204800  100M Linux filesystem
-# /dev/vda3      411648      616447      204800  100M Linux filesystem
-# /dev/vda4      616448     2713599     2097152    1G Linux filesystem
-# /dev/vda5     2713600    23685119    20971520   10G Linux filesystem
-# /dev/vda6    23685120    44656639    20971520   10G Linux filesystem
-# /dev/vda7    44656640    46753791     2097152    1G Linux filesystem
-# /dev/vda8    46753792    67725311    20971520   10G Linux filesystem
-# /dev/vda9    67725312    88696831    20971520   10G Linux filesystem
-# /dev/vda10   88696832  2236180479  2147483648    1T Linux filesystem
-# The partition table is unchanged (--no-act).
-# get the line for /dev/vda10 and get the start sector.
-start_sector=$(echo "- - L" | sfdisk -n --force --no-reread -a /dev/vda | grep /dev/vda10 | awk '{print $2}')
-# get the end sector.
-end_sector=$(echo "- - L" | sfdisk -n --force --no-reread -a /dev/vda | grep /dev/vda10 | awk '{print $3}')
-# check if the start sector is aligned to 4kb sectors.
-# if the start sector is not aligned to 4kb sectors, we need to align it.
-if [ $((start_sector * 512 % 4096)) -ne 0 ]; then
-    # align the start sector to 4kb sectors.
-    start_sector=$((start_sector + ((start_sector * 512) % 4096) / 512))
-fi
-# check if the end sector is aligned to 4kb sectors.
-# if the end sector is not aligned to 4kb sectors, we need to align it.
-if [ $((end_sector * 512 % 4096)) -ne 0 ]; then
-    # align the end sector to 4kb sectors.
-    end_sector=$((end_sector + ((end_sector * 512) % 4096) / 512))
+start_sector=$(echo "- - L" | sfdisk -n --force --no-reread -a $DISK | grep $PARTITION  | grep -v "Created" | awk '{print $2}')
+end_sector=$(echo "- - L" | sfdisk -n --force --no-reread -a $DISK | grep $PARTITION  | grep -v "Created" | awk '{print $3}')
+
+# Ensure the start sector is a multiple of 8 (aligned to 4KiB)
+if [ $((start_sector % 8)) -ne 0 ]; then
+    start_sector=$(( (start_sector + 7) / 8 * 8 ))  # Round up to the next multiple of 8
 fi
 
-# run the sfdisk command to create the partition.
-echo "$start_sector $end_sector L" | sfdisk --force --no-reread -a /dev/vda 
+# Ensure the end sector is a multiple of 8 (aligned to 4KiB), rounding down
+if [ $((end_sector % 8)) -ne 0 ]; then
+    end_sector=$((end_sector / 8 * 8))
+fi
+
+partition_size=$((end_sector - start_sector + 1))
+# Ensure that the partition size is a multiple of 8 (aligned to 4KiB), rounding down
+if [ $((partition_size % 8)) -ne 0 ]; then
+    partition_size=$((partition_size / 8 * 8))
+fi
+
+echo "$start_sector, $partition_size, L" | sfdisk --force --no-reread -a /dev/vda
 
 
 # Generate a key and initialize encrypted store with it.
