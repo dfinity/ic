b1073d6c6200bdda8cd29097be1120d37ffb5d49
Nicolas Mattia <nicolas.mattia@dfinity.org>
2025-02-13 12:39:13 +0100

    chore(IDX): use github.ref_protected where applicable
    This means we don't have to keep track of the protected branches and
    means we can use unified logic for protected ref detection.

--- ci/bazel-scripts/main.sh
+++ ci/bazel-scripts/main.sh
@@ -11,16 +11,6 @@ ic_version_rc_only="0000000000000000000000000000000000000000"
 release_build="false"
 s3_upload="False"
 
-protected_branches=("master" "rc--*" "hotfix-*" "master-private")
-
-# if we are on a protected branch or targeting a rc branch we set ic_version to the commit_sha and upload to s3
-for pattern in "${protected_branches[@]}"; do
-    if [[ "$BRANCH_NAME" == $pattern ]]; then
-        IS_PROTECTED_BRANCH="true"
-        break
-    fi
-done
-
 # if we are on a protected branch or targeting a rc branch we set release build, ic_version to the commit_sha and
 # upload to s3
 if [[ "${IS_PROTECTED_BRANCH:-}" == "true" ]] || [[ "${CI_PULL_REQUEST_TARGET_BRANCH_NAME:-}" == "rc--"* ]]; then
--- ci/scripts/run-build-ic.sh
+++ ci/scripts/run-build-ic.sh
@@ -5,16 +5,6 @@ VERSION=$(git rev-parse HEAD)
 
 cd "$CI_PROJECT_DIR"
 
-protected_branches=("master" "rc--*" "hotfix-*" "master-private")
-
-# if we are on a protected branch or targeting a rc branch we set ic_version to the commit_sha and upload to s3
-for pattern in "${protected_branches[@]}"; do
-    if [[ "$BRANCH_NAME" == $pattern ]]; then
-        IS_PROTECTED_BRANCH="true"
-        break
-    fi
-done
-
 # run build with release on protected branches or if a pull_request is targeting an rc branch
 if [ "${IS_PROTECTED_BRANCH:-}" == "true" ] || [[ "${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-}" == "rc--"* ]]; then
     ci/container/build-ic.sh -i -c -b
