d8674286229837a1b8b218d2434d0286ed13f6cb
Michael Weigelt <michael.weigelt@dfinity.org>
2025-02-17 15:05:16 +0000

    extend pocket ic mgmt types

--- packages/pocket-ic/src/management_canister.rs
+++ packages/pocket-ic/src/management_canister.rs
@@ -577,3 +577,143 @@ pub struct BitcoinGetBlockHeadersResult {
     pub tip_height: BitcoinBlockHeight,
     pub block_headers: Vec<BitcoinBlockHeader>,
 }
+
+// ========================================================================= //
+// Types associated with the IC management canister that are not specified,
+// but nevertheless useful for PocketIC.
+//
+// TODO: These should be moved to a separate module in the public crate
+// ic-management-canister-types
+
+use ic_management_canister_types::{EcdsaKeyId, SchnorrKeyId};
+use serde::Serialize;
+use strum_macros::{Display, EnumCount, EnumIter, EnumString};
+
+/// Methods exported by ic:00.
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Display, EnumIter, EnumString)]
+#[strum(serialize_all = "snake_case")]
+pub enum Ic00Method {
+    CanisterStatus,
+    CanisterInfo,
+    CreateCanister,
+    DeleteCanister,
+    DepositCycles,
+    HttpRequest,
+    ECDSAPublicKey,
+    InstallCode,
+    InstallChunkedCode,
+    RawRand,
+    SetupInitialDKG,
+    SignWithECDSA,
+    StartCanister,
+    StopCanister,
+    UninstallCode,
+    UpdateSettings,
+    ComputeInitialIDkgDealings,
+    ReshareChainKey,
+
+    // Schnorr interface.
+    SchnorrPublicKey,
+    SignWithSchnorr,
+
+    // VetKd interface.
+    #[strum(serialize = "vetkd_public_key")]
+    VetKdPublicKey,
+    #[strum(serialize = "vetkd_derive_encrypted_key")]
+    VetKdDeriveEncryptedKey,
+
+    // Bitcoin Interface.
+    BitcoinGetBalance,
+    BitcoinGetUtxos,
+    BitcoinGetBlockHeaders,
+    BitcoinSendTransaction,
+    BitcoinGetCurrentFeePercentiles,
+    // Private APIs used exclusively by the bitcoin canisters.
+    BitcoinSendTransactionInternal, // API for sending transactions to the network.
+    BitcoinGetSuccessors,           // API for fetching blocks from the network.
+
+    // Subnet information
+    NodeMetricsHistory,
+    SubnetInfo,
+
+    FetchCanisterLogs,
+
+    // These methods are only available on test IC instances where there is a
+    // need to fabricate cycles without burning ICP first.
+    ProvisionalCreateCanisterWithCycles,
+    ProvisionalTopUpCanister,
+
+    // Support for chunked uploading of Wasm modules.
+    UploadChunk,
+    StoredChunks,
+    ClearChunkStore,
+
+    // Support for canister snapshots.
+    TakeCanisterSnapshot,
+    LoadCanisterSnapshot,
+    ListCanisterSnapshots,
+    DeleteCanisterSnapshot,
+}
+
+/// Types of curves that can be used for threshold key derivation (vetKD).
+/// ```text
+/// (variant { bls12_381_g2; })
+/// ```
+#[derive(
+    Copy,
+    Clone,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    Hash,
+    Debug,
+    CandidType,
+    Deserialize,
+    EnumIter,
+    Serialize,
+)]
+pub enum VetKdCurve {
+    #[serde(rename = "bls12_381_g2")]
+    #[allow(non_camel_case_types)]
+    Bls12_381_G2,
+}
+
+/// Unique identifier for a key that can be used for threshold key derivation
+/// (vetKD). The name is just an identifier, but it may be used to convey
+/// some information about the key (e.g. that the key is meant to be used for
+/// testing purposes).
+/// ```text
+/// (record { curve: vetkd_curve; name: text})
+/// ```
+#[derive(
+    Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, CandidType, Deserialize, Serialize,
+)]
+pub struct VetKdKeyId {
+    pub curve: VetKdCurve,
+    pub name: String,
+}
+
+/// Unique identifier for a key that can be used for one of the signature schemes
+/// supported on the IC.
+/// ```text
+/// (variant { EcdsaKeyId; SchnorrKeyId })
+/// ```
+#[derive(
+    Clone,
+    Eq,
+    PartialEq,
+    Ord,
+    PartialOrd,
+    Hash,
+    Debug,
+    CandidType,
+    Deserialize,
+    EnumCount,
+    Serialize,
+)]
+pub enum MasterPublicKeyId {
+    Ecdsa(EcdsaKeyId),
+    Schnorr(SchnorrKeyId),
+    VetKd(VetKdKeyId),
+}
