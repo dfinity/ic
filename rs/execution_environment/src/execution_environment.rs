5dad14ab8219a35819f6994e638375c3b40cae10
Dragoljub Duric <dragoljub.duric@dfinity.org>
2025-02-19 16:21:55 +0000

    .

--- rs/execution_environment/src/execution_environment.rs
+++ rs/execution_environment/src/execution_environment.rs
@@ -1636,9 +1636,9 @@ impl ExecutionEnvironment {
                 let res = match response {
                     Ok((res, canister_id)) => {
                         if let Some(canister_id) = canister_id {
-                            state.canister_state_mut(canister_id).map(|canister_state| {
+                            if let Some(canister_state) = state.canister_state_mut(canister_id) {
                                 canister_state.update_on_low_wasm_memory_hook_condition()
-                            });
+                            }
                         }
                         Ok(res)
                     }
