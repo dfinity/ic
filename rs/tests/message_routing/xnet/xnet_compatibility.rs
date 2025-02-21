318075263d9ca9451c33da7a2d6079a71f020bbe
Andrew Battat <andrew.battat@dfinity.org>
2025-02-21 19:28:04 +0000

    xnet_compatibility MAINNET_NNS_SUBNET_REVISION_ENV EV

--- rs/tests/message_routing/xnet/xnet_compatibility.rs
+++ rs/tests/message_routing/xnet/xnet_compatibility.rs
@@ -170,7 +170,8 @@ pub async fn test_async(env: TestEnv) {
     )
     .with_call_timeouts(&call_timeouts);
 
-    let mainnet_version = read_dependency_to_string("mainnet_nns_subnet_revision.txt").unwrap();
+    let mainnet_version = std::env::var("MAINNET_NNS_SUBNET_REVISION_ENV")
+        .expect("could not read mainnet version from environment");
 
     let original_branch_version = read_dependency_from_env_to_string("ENV_DEPS__IC_VERSION_FILE")
         .expect("tip-of-branch IC version");
