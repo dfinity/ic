587ad42ae294ef4c0193befca1f64fc9ef0a637b
Franz-Stefan Preiss <franzstefan@dfinity.org>
2025-02-21 10:43:59 +0000

    Fix consensus

--- rs/consensus/vetkd/src/lib.rs
+++ rs/consensus/vetkd/src/lib.rs
@@ -32,10 +32,7 @@ use ic_types::{
         bytes_to_vetkd_payload, vetkd_payload_to_bytes, ConsensusResponse, ValidationContext,
         VetKdAgreement, VetKdErrorCode, VetKdPayload,
     },
-    crypto::{
-        vetkd::{VetKdArgs, VetKdEncryptedKey},
-        ExtendedDerivationPath,
-    },
+    crypto::vetkd::{VetKdArgs, VetKdDerivationDomain, VetKdEncryptedKey},
     messages::{CallbackId, Payload as ResponsePayload, RejectContext},
     CountBytes, Height, NumBytes, SubnetId, Time,
 };
@@ -198,9 +195,9 @@ impl VetKdPayloadBuilderImpl {
                     continue;
                 };
                 let _args = VetKdArgs {
-                    derivation_path: ExtendedDerivationPath {
+                    derivation_domain: VetKdDerivationDomain {
                         caller: context.request.sender.into(),
-                        derivation_path: context.derivation_path.clone(),
+                        domain: context.derivation_path[0].clone(),
                     },
                     ni_dkg_id: ctxt_args.ni_dkg_id.clone(),
                     derivation_id: ctxt_args.derivation_id.clone(),
@@ -289,9 +286,9 @@ impl VetKdPayloadBuilderImpl {
             return invalid_artifact_err(InvalidVetKdPayloadReason::UnexpectedIDkgContext(id));
         };
         let _args = VetKdArgs {
-            derivation_path: ExtendedDerivationPath {
+            derivation_domain: VetKdDerivationDomain {
                 caller: context.request.sender.into(),
-                derivation_path: context.derivation_path.clone(),
+                domain: context.derivation_path[0].clone(),
             },
             ni_dkg_id: ctxt_args.ni_dkg_id.clone(),
             derivation_id: ctxt_args.derivation_id.clone(),
