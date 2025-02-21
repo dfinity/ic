a6c197a795bcd6140b111e6fc1d9942f88d91cb1
Franz-Stefan Preiss <franzstefan@dfinity.org>
2025-02-21 13:01:02 +0000

    Fix crypto test

--- rs/crypto/internal/crypto_service_provider/src/vault/local_csp_vault/vetkd/tests.rs
+++ rs/crypto/internal/crypto_service_provider/src/vault/local_csp_vault/vetkd/tests.rs
@@ -35,9 +35,9 @@ fn should_correctly_create_encrypted_vetkd_key_share_for_smoke_test_vector() {
                 f3e68e13f62604d027660883213c90ea72810bcecee58b883fb62118e538243\
                 03718e6876ea400d083beb0439d3934122a4c4b2e58e3f145305b9a0c0a00e3\
                 2dd808574dec2605dbc7f122fe593ca0c07ca92720d0f17b7d53c9c68dbb93d\
-                489078859e5e5fe2b6612ac9536fe7f8b463cafcc003efe88f1cddd26499322\
-                9229edb5ec96ea3fd2d083b815776b6cc7e03586d864374ba54d44c09201db8\
-                d33dd44",
+                489078859e5e5fe2b6612ac9536fe7f8b463c8cc50fa2c7933696b9c7b07f66\
+                b1f7de29d33b2520293dfbe70ff1e243d68d70a0aa189baa33c8d997ebab28a\
+                50adbc1",
             )
             .expect("invalid test vector")
         ))
