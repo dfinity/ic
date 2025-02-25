05583f7991776f385bcc5a3f3c3b13b461768c37
Maciej Modelski <maciej.modelski@dfinity.org>
2025-02-25 15:13:47 +0000

    instr counter

--- rs/ledger_suite/icp/ledger/src/main.rs
+++ rs/ledger_suite/icp/ledger/src/main.rs
@@ -3,14 +3,15 @@ use dfn_candid::{candid, candid_one, CandidOne};
 #[allow(unused_imports)]
 use dfn_core::BytesS;
 use dfn_core::{
-    api::set_certified_data,
     endpoint::reject_on_decode_error::{over, over_async, over_async_may_reject},
     over_init, printer, setup,
 };
 use dfn_protobuf::protobuf;
 use ic_base_types::{CanisterId, PrincipalId};
 use ic_canister_log::{LogEntry, Sink};
-use ic_cdk::api::{caller, data_certificate, instruction_counter, print, time, trap};
+use ic_cdk::api::{
+    caller, data_certificate, instruction_counter, print, set_certified_data, time, trap,
+};
 use ic_icrc1::endpoints::{convert_transfer_error, StandardRecord};
 use ic_ledger_canister_core::ledger::LedgerContext;
 use ic_ledger_canister_core::runtime::heap_memory_size_bytes;
@@ -122,7 +123,7 @@ fn init(
         initial_values,
         minting_account,
         icrc1_minting_account,
-        TimeStamp::from(dfn_core::api::now()),
+        TimeStamp::from_nanos_since_unix_epoch(time()),
         transaction_window,
         send_whitelist,
         transfer_fee,
@@ -762,11 +763,8 @@ const MAX_INSTRUCTIONS_PER_UPGRADE: u64 = 5_000_000;
 const MAX_INSTRUCTIONS_PER_TIMER_CALL: u64 = 500_000;
 
 fn post_upgrade(args: Option<LedgerCanisterPayload>) {
-    let start = dfn_core::api::performance_counter(0);
+    let start = instruction_counter();
 
-    // In order to read the first bytes we need to use ic_cdk.
-    // dfn_core assumes the first 4 bytes store stable memory length
-    // and return bytes starting from the 5th byte.
     let mut magic_bytes_reader = ic_cdk::api::stable::StableReader::default();
     const MAGIC_BYTES: &[u8; 3] = b"MGR";
     let mut first_bytes = [0u8; 3];
@@ -851,7 +849,7 @@ fn post_upgrade(args: Option<LedgerCanisterPayload>) {
         );
     }
 
-    let end = dfn_core::api::performance_counter(0);
+    let end = instruction_counter();
     let post_upgrade_instructions_consumed = end - start;
     POST_UPGRADE_INSTRUCTIONS_CONSUMED
         .with(|n| *n.borrow_mut() = post_upgrade_instructions_consumed);
@@ -918,7 +916,7 @@ fn post_upgrade_() {
 
 #[export_name = "canister_pre_upgrade"]
 fn pre_upgrade() {
-    let start = dfn_core::api::performance_counter(0);
+    let start = instruction_counter();
     setup::START.call_once(|| {
         printer::hook();
     });
@@ -938,7 +936,7 @@ fn pre_upgrade() {
         let mut buffered_writer = BufferedWriter::new(BUFFER_SIZE, writer);
         ciborium::ser::into_writer(&*ledger, &mut buffered_writer)
             .expect("Failed to write the Ledger state to memory manager managed stable memory");
-        let end = dfn_core::api::performance_counter(0);
+        let end = instruction_counter();
         let instructions_consumed = end - start;
         let counter_bytes: [u8; 8] = instructions_consumed.to_le_bytes();
         buffered_writer
@@ -1469,12 +1467,12 @@ fn encode_metrics(w: &mut ic_metrics_encoder::MetricsEncoder<Vec<u8>>) -> std::i
     )?;
     w.encode_gauge(
         "ledger_stable_memory_pages",
-        dfn_core::api::stable_memory_size_in_pages() as f64,
+        ic_cdk::api::stable::stable_size() as f64,
         "Size of the stable memory allocated by this canister measured in 64K Wasm pages.",
     )?;
     w.encode_gauge(
         "stable_memory_bytes",
-        (dfn_core::api::stable_memory_size_in_pages() * 64 * 1024) as f64,
+        (ic_cdk::api::stable::stable_size() * 64 * 1024) as f64,
         "Size of the stable memory allocated by this canister measured in bytes.",
     )?;
     w.encode_gauge(
