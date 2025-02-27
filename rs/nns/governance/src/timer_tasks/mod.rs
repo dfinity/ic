d9b0eeeeefe6fc2c6a43ead39994f7ad1f1528f4
Jason Zhu <jason.zhu@dfinity.org>
2025-02-27 18:19:44 +0000

    Add metrics collection to timer tasks

--- rs/nns/governance/src/timer_tasks/mod.rs
+++ rs/nns/governance/src/timer_tasks/mod.rs
@@ -1,10 +1,15 @@
-use ic_nervous_system_timer_task::RecurringAsyncTask;
+use ic_nervous_system_timer_task::{RecurringAsyncTask, TimerTaskMetricsRegistry};
 use seeding::SeedingTask;
+use std::cell::RefCell;
 
 use crate::canister_state::GOVERNANCE;
 
 mod seeding;
 
+thread_local! {
+    static METRICS_REGISTRY: RefCell<TimerTaskMetricsRegistry> = RefCell::new(TimerTaskMetricsRegistry::default());
+}
+
 pub fn schedule_tasks() {
-    SeedingTask::new(&GOVERNANCE).schedule();
+    SeedingTask::new(&GOVERNANCE).schedule(&METRICS_REGISTRY);
 }
