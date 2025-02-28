85184bf1284b2535763b72f7ffb9650528ade9af
mraszyk <31483726+mraszyk@users.noreply.github.com>
2025-02-28 12:50:28 +0100

    fix(PocketIC): avoid overly long sleep in auto progress mode (#4144)
    This PR avoids an overly long sleep in the auto progress mode of
    PocketIC by not forcing a sleep of the same duration as the last round
    duration: there are cases when the round duration could be high and the
    additional sleep increases PocketIC latency unnecessarily.

--- rs/pocket_ic_server/src/state_api/state.rs
+++ rs/pocket_ic_server/src/state_api/state.rs
@@ -54,7 +54,7 @@ use tokio::{
     sync::mpsc::Receiver,
     sync::{mpsc, Mutex, RwLock},
     task::{spawn, spawn_blocking, JoinHandle, JoinSet},
-    time::{self, sleep, Instant},
+    time::{self, sleep},
 };
 use tower::ServiceExt;
 use tower_http::cors::{Any, CorsLayer};
@@ -1019,7 +1019,6 @@ impl ApiState {
                 debug!("Starting auto progress for instance {}.", instance_id);
                 let mut now = SystemTime::now();
                 loop {
-                    let start = Instant::now();
                     let old = std::mem::replace(&mut now, SystemTime::now());
                     let op = AdvanceTimeAndTick(now.duration_since(old).unwrap_or_default());
                     if Self::execute_operation(
@@ -1047,12 +1046,8 @@ impl ApiState {
                     {
                         break;
                     }
-                    let duration = start.elapsed();
-                    sleep(std::cmp::max(
-                        duration,
-                        std::cmp::max(artificial_delay, MIN_OPERATION_DELAY),
-                    ))
-                    .await;
+                    let sleep_duration = std::cmp::max(artificial_delay, MIN_OPERATION_DELAY);
+                    sleep(sleep_duration).await;
                     if received_stop_signal(&mut rx) {
                         break;
                     }
