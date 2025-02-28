18964cafa65088302df596635ac3c16971a1dbb5
David Frank <david.frank@dfinity.org>
2025-02-28 14:57:56 +0000

    working

---
+++ deploy-guest.sh
@@ -0,0 +1,13 @@
+input="bazel-bin/ic-os/guestos/envs/local-base-dev/disk-img.tar.zst"
+hostos="2a00:fb01:400:44:6800:95ff:fed7:d475"
+
+chmod +w ${input}
+scp -o StrictHostKeyChecking=no -6 ${input} admin@[${hostos}]:/tmp
+
+ssh -o StrictHostKeyChecking=no -6 admin@${hostos} << EOF
+   sudo systemctl stop monitor-guestos
+   sudo systemctl stop guestos
+   tar xaf /tmp/disk-img.tar.zst -C /tmp disk.img
+   sudo dd if=/tmp/disk.img of=/dev/hostlvm/guestos bs=8M conv=sparse
+   sudo systemctl start guestos
+EOF
