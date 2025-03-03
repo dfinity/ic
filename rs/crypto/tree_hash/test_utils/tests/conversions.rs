c0d94cca48a521bda666fce83508e57eb5a5ee2a
Christian Stieger <christian.stieger@dfinity.org>
2025-03-03 15:41:05 +0000

    .

--- rs/crypto/tree_hash/test_utils/tests/conversions.rs
+++ rs/crypto/tree_hash/test_utils/tests/conversions.rs
@@ -5,18 +5,18 @@ use ic_crypto_tree_hash_test_utils::{
 };
 use proptest::prelude::*;
 
-proptest! {
-    #[test]
-    fn hash_tree_builder_from_labeled_tree_works_correctly(tree in arbitrary_labeled_tree()){
-        let builder = hash_tree_builder_from_labeled_tree(&tree);
-        // check that the witness is correct by pruning it completely
-        let wg = builder
-            .witness_generator()
-            .expect("Failed to retrieve a witness constructor");
-        let witness = wg
-            .witness(&tree)
-            .expect("Failed to build a witness for the whole tree");
-        let witness = prune_witness(&witness, &tree).expect("failed to prune witness");
-        assert_matches!(witness, Witness::Pruned { digest: _ });
-    }
+#[test_strategy::proptest]
+fn hash_tree_builder_from_labeled_tree_works_correctly(
+    #[strategy(arbitrary_labeled_tree())] tree: LabeledTree<Vec<u8>>,
+) {
+    let builder = hash_tree_builder_from_labeled_tree(&tree);
+    // check that the witness is correct by pruning it completely
+    let wg = builder
+        .witness_generator()
+        .expect("Failed to retrieve a witness constructor");
+    let witness = wg
+        .witness(&tree)
+        .expect("Failed to build a witness for the whole tree");
+    let witness = prune_witness(&witness, &tree).expect("failed to prune witness");
+    assert_matches!(witness, Witness::Pruned { digest: _ });
 }
