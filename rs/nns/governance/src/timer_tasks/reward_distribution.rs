875697dbcf7b5bc6ee2d8ee5bdc0fed1724e8f83
Max Summe <maximilian.summe@dfinity.org>
2025-03-03 09:04:18 -0800

    Add comment

--- rs/nns/governance/src/timer_tasks/reward_distribution.rs
+++ rs/nns/governance/src/timer_tasks/reward_distribution.rs
@@ -35,9 +35,13 @@ fn delay_until_next_run(
         * REWARD_DISTRIBUTION_PERIOD_SECONDS
         + genesis_timestamp_seconds;
 
+    // We add 1 to the end of the period to make sure we always run after the period is over, to
+    // avoid missing any proposals that would be ready to settle right on the edge of the period.
     let next =
         latest_distribution_nominal_end_timestamp_seconds + REWARD_DISTRIBUTION_PERIOD_SECONDS + 1;
 
+    // We want the difference between next and now.  If it's in the past, we want to run
+    // immediately
     Duration::from_secs(next.saturating_sub(now))
 }
 
