ead45744c22d0a54762630007b8388310cd96932
Max Summe <maximilian.summe@dfinity.org>
2025-03-06 16:52:29 -0800

    wip

---
+++ rs/node_rewards/canister/src/storage.rs
@@ -0,0 +1,20 @@
+use crate::registry::RegistryClient;
+use crate::registry_store::CanisterRegistryStore;
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
+use ic_stable_structures::{DefaultMemoryImpl, StableVec};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+/// MemoryId for upgrades, if any.  This is reserved, but probably not going to be used.
+const UPGRADES_MEMORY_ID: MemoryId = MemoryId::new(0);
+
+const REGISTRY_STORE_MEMORY_ID: MemoryId = MemoryId::new(1);
+
+type VM = VirtualMemory<DefaultMemoryImpl>;
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static REGISTRY_STORE: RefCell<VM> = RefCell::new(MEMORY_MANAGER.with_borrow(|mm| mm.get(REGISTRY_STORE_MEMORY_ID)));
+}
