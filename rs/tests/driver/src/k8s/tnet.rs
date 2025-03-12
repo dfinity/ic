4e9869c3eb9a62c6fb47f85271b3ef11c8b00d21
Marko Kosmerl <marko.kosmerl@dfinity.org>
2025-03-12 14:49:10 -0300

    Update rs/tests/driver/src/k8s/tnet.rs
    Co-authored-by: Bas van Dijk <bas@dfinity.org>

--- rs/tests/driver/src/k8s/tnet.rs
+++ rs/tests/driver/src/k8s/tnet.rs
@@ -363,11 +363,7 @@ impl TNet {
         // We make reservation for 42 vcpus only if VM uses 64 vcpus because there are
         // already other k8s resources having resource requests that prevents reservation to succeeds.
         // Note that VM still gets 64 vcpus.
-        let vcpus = if vm_req.vcpus.to_string() == "64" {
-            "42".to_string()
-        } else {
-            vm_req.vcpus.to_string()
-        };
+        let vcpus = min(42, vm_req.vcpus).to_string()
         // Same as above, we make reservation for 312142680 memory only if VM uses 512142680 memory because there are
         // already other k8s resources having resource requests that prevents reservation to succeeds.
         let mem = if vm_req.memory_kibibytes.to_string() == "512142680" {
