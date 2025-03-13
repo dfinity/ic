2ef202b4de13c726e2ea96502f8940027087b24d
Ruediger Birkner <ruediger.birkner@dfinity.org>
2025-03-13 11:03:09 +0000

    added more logs and moved canister installation

--- rs/tests/dre/utils/steps/workload.rs
+++ rs/tests/dre/utils/steps/workload.rs
@@ -3,6 +3,7 @@ use super::Step;
 use anyhow::bail;
 use ic_agent::{
     agent::http_transport::reqwest_transport::reqwest::{redirect::Policy, Client, ClientBuilder},
+    export::Principal,
     identity::AnonymousIdentity,
     Agent,
 };
@@ -62,7 +63,10 @@ impl Step for ApiBoundaryNodeWorkload {
         let api_boundary_nodes: Vec<IcNodeSnapshot> =
             env.topology_snapshot().api_boundary_nodes().collect();
 
-        // create HTTP client to talk to the API boundary nodes
+        info!(
+            logger,
+            "Create HTTP client to talk to the API boundary nodes"
+        );
         let http_client = {
             let mut client_builder = ClientBuilder::new()
                 .redirect(Policy::none())
@@ -81,17 +85,22 @@ impl Step for ApiBoundaryNodeWorkload {
             client_builder.build().expect("failed to build http client")
         };
 
+        info!(logger, "Install the counter canister");
+        let counter_canister_id: Principal = rt.block_on(install_counter_canister(env.clone()))?;
+
         let domains: Vec<String> = api_boundary_nodes
             .iter()
             .map(|node| node.get_domain().expect("API BN has no domain"))
             .collect();
 
-        for domain in domains {
+        for (index, domain) in domains.into_iter().enumerate() {
+            info!(logger, "Test API BN with domain {domain}");
             rt.block_on(test_api_boundary_node(
+                index.try_into().unwrap(),
                 domain,
+                counter_canister_id,
                 http_client.clone(),
                 logger.clone(),
-                env.clone(),
             ))?;
         }
 
@@ -99,13 +108,42 @@ impl Step for ApiBoundaryNodeWorkload {
     }
 }
 
+async fn install_counter_canister(
+    env: ic_system_test_driver::driver::test_env::TestEnv,
+) -> anyhow::Result<Principal> {
+    // install a counter canister on an app subnet
+    let node = env.get_first_healthy_application_node_snapshot();
+
+    let agent = node.build_default_agent_async().await;
+    let effective_canister_id = node.effective_canister_id();
+    let mgr = ManagementCanister::create(&agent);
+    let (canister_id,) = mgr
+        .create_canister()
+        .as_provisional_create_with_amount(None)
+        .with_effective_canister_id(effective_canister_id)
+        .call_and_wait()
+        .await
+        .map_err(|err| format!("Couldn't create canister with provisional API: {}", err))
+        .unwrap();
+    let canister_code = wat::parse_str(COUNTER_CANISTER_WAT).unwrap();
+    let install_code = mgr.install_code(&canister_id, canister_code.as_slice());
+    install_code
+        .call_and_wait()
+        .await
+        .map_err(|err| format!("Couldn't install canister: {}", err))
+        .unwrap();
+
+    Ok(canister_id)
+}
+
 async fn test_api_boundary_node(
+    index: u32,
     domain: String,
+    counter_canister_id: Principal,
     http_client: Client,
     logger: Logger,
-    env: ic_system_test_driver::driver::test_env::TestEnv,
 ) -> anyhow::Result<()> {
-    // health check the API BN -- it should be healthy
+    info!(logger, "Health check the API BN");
     retry_with_msg_async!(
         "check_api_bns_health",
         &logger,
@@ -136,51 +174,35 @@ async fn test_api_boundary_node(
         .build()
         .unwrap();
 
-    // implicit status call to fetch the root key
+    info!(
+        logger,
+        "api/v2/status - implicit status call to fetch the root key"
+    );
     let _ = bn_agent.fetch_root_key().await;
 
-    // install a counter canister on an app subnet
-    let node = env.get_first_healthy_application_node_snapshot();
-
-    let agent = node.build_default_agent_async().await;
-    let effective_canister_id = node.effective_canister_id();
-    let mgr = ManagementCanister::create(&agent);
-    let (canister_id,) = mgr
-        .create_canister()
-        .as_provisional_create_with_amount(None)
-        .with_effective_canister_id(effective_canister_id)
-        .call_and_wait()
-        .await
-        .map_err(|err| format!("Couldn't create canister with provisional API: {}", err))
-        .unwrap();
-    let canister_code = wat::parse_str(COUNTER_CANISTER_WAT).unwrap();
-    let install_code = mgr.install_code(&canister_id, canister_code.as_slice());
-    install_code
-        .call_and_wait()
-        .await
-        .map_err(|err| format!("Couldn't install canister: {}", err))
-        .unwrap();
-
-    // issue a query call (read the current counter value)
+    info!(
+        logger,
+        "api/v2/query - issue a query call (read the current counter value)"
+    );
     let read_result = retry_with_msg_async!(
-        format!("query call on canister={canister_id}"),
+        format!("query call on canister={counter_canister_id}"),
         &logger,
         CANISTER_RETRY_TIMEOUT,
         CANISTER_RETRY_BACKOFF,
         || async {
-            let read_result = bn_agent.query(&canister_id, "read").call().await;
+            let read_result = bn_agent.query(&counter_canister_id, "read").call().await;
             if let Ok(bytes) = read_result {
                 Ok(bytes)
             } else {
                 bail!(
-                    "querying the counter canister ({canister_id}) failed, err: {:?}",
+                    "querying the counter canister ({counter_canister_id}) failed, err: {:?}",
                     read_result.unwrap_err()
                 )
             }
         }
     )
     .await
-    .expect("querying the counter canister ({canister_id}) failed after {max_attempts} attempts");
+    .expect("querying the counter canister ({counter_canister_id}) failed after {max_attempts} attempts");
 
     let counter = u32::from_le_bytes(
         read_result
@@ -189,30 +211,37 @@ async fn test_api_boundary_node(
             .expect("slice with incorrect length"),
     );
 
-    assert_eq!(counter, 0);
+    assert_eq!(counter, index);
 
-    // issue an update call (increase the counter value)
+    info!(
+        logger,
+        "api/v3/call - issue an update call (increase the counter value)"
+    );
     retry_with_msg_async!(
-        format!("update call on canister={canister_id}"),
+        format!("update call on canister={counter_canister_id}"),
         &logger,
         CANISTER_RETRY_TIMEOUT,
         CANISTER_RETRY_BACKOFF,
         || async {
-            let result = bn_agent.update(&canister_id, "write").call_and_wait().await;
+            let result = bn_agent.update(&counter_canister_id, "write").call_and_wait().await;
             if let Ok(bytes) = result {
                 Ok(bytes)
             } else {
                 bail!(
-                    "updating the counter canister ({canister_id}) failed, err: {:?}",
+                    "updating the counter canister ({counter_canister_id}) failed, err: {:?}",
                     result.unwrap_err()
                 )
             }
         }
     )
     .await
-    .expect("querying the counter canister ({canister_id}) failed after {max_attempts} attempts");
+    .expect("querying the counter canister ({counter_canister_id}) failed after {max_attempts} attempts");
 
-    // issue a read state to fetch the API BNs -- the canister ID just needs to exist as it is only used to route the request to a subnet
+    info!(
+        logger,
+        "api/v3/read_state - issue a read state to fetch the API BNs"
+    );
+    //  the canister ID is just needed for the API BN to route it to a subnet
     let _ = bn_agent
         .fetch_api_boundary_nodes_by_canister_id(REGISTRY_CANISTER_ID.into())
         .await
