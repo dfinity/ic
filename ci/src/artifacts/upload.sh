0ff7319a08c2c0c21c70686dbb59fe08a2cb5b57
Nicolas Mattia <nicolas.mattia@dfinity.org>
2025-03-17 11:59:34 +0100

    chore(IDX): simplify S3 upload
    This makes the S3 upload of artifacts easier to follow:
    
    * Remove `rclone` files and inline config as `rclone` CLI args to make
      config more transparent
    * Remove config related to "proxy" upload (not being used and not
      working) as well as related `--s3_endpoint` arg
    * Remove unused `rclone` wrapper

--- ci/src/artifacts/upload.sh
+++ ci/src/artifacts/upload.sh
@@ -13,7 +13,7 @@ done <"$VERSION_FILE"
 
 VERSION="$(cat $VERSION_TXT)"
 
-# rclone reads the $(dirname $f) to get file attribuates.
+# rclone reads the $(dirname $f) to get file attributes.
 # Therefore symlink should be resolved.
 f="$1"
 if [ -L "$f" ]; then
@@ -25,31 +25,39 @@ if [ "$(basename $f)" == "SHA256SUMS" ]; then
     cat "$f" >&2
 fi
 
+# Multipart upload does not work trough Cloudflare for some reason.
+# Just disabling it with `--s3-upload-cutoff` for now.
+rclone_common_flags=(
+    --stats-one-line
+    --checksum
+    --immutable
+    --s3-upload-cutoff=5G
+)
+
 if [ "${UPLOAD_BUILD_ARTIFACTS:-}" == "1" ]; then
-    echo "uploading $f"
-    # Multipart upload does not work trough the proxy or through Cloudflare for some
-    # reason. Just disabling it with `--s3-upload-cutoff` for now.
-    "$RCLONE" \
-        --config="$RCLONE_CONFIG" \
-        --stats-one-line \
-        --checksum \
-        --immutable \
-        --s3-upload-cutoff=5G \
+    echo "uploading $f to AWS" >&2
+    AWS_PROFILE=default "$RCLONE" \
+        "${rclone_common_flags[@]}" \
+        --s3-provider=AWS \
+        --s3-region=eu-central-1 \
+        --s3-env-auth \
         copy \
         "$f" \
-        "public-s3:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
+        ":s3:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
+    echo "done uploading to AWS" >&2
 
     # Upload to Cloudflare's R2 (S3)
-    unset RCLONE_S3_ENDPOINT
-    AWS_PROFILE=cf "$RCLONE" \
-        --config="$RCLONE_CONFIG" \
-        --stats-one-line \
-        --checksum \
-        --immutable \
-        --s3-upload-cutoff=5G \
+    # using profile 'cf' to look up the right creds in ~/.aws/credentials
+    echo "uploading $f to Cloudflare" >&2
+    echo AWS_PROFILE=cf "$RCLONE" -v \
+        "${rclone_common_flags[@]}" \
+        --s3-provider=Cloudflare \
+        --s3-endpoint=https://64059940cc95339fc7e5888f431876ee.r2.cloudflarestorage.com \
+        --s3-env-auth \
         copy \
         "$f" \
-        "public-s3-cf:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
+        ":s3:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
+    echo "done uploading to Cloudflare" >&2
 else
     echo "dry run for $f"
 fi
