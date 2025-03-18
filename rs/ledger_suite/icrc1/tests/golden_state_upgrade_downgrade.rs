2fd7b7c6ced2f310c2f1ea249846bdd9d8c098ee
Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>
2025-03-18 14:28:25 +0100

    Clippy

--- rs/ledger_suite/icrc1/tests/golden_state_upgrade_downgrade.rs
+++ rs/ledger_suite/icrc1/tests/golden_state_upgrade_downgrade.rs
@@ -35,6 +35,7 @@ const BURN_MULTIPLIER: u64 = 1;
 // Corresponds to ic_icrc1_ledger::LEDGER_VERSION where allowances and balances are
 // migrated to stable structures
 const LEDGER_VERSION_2: u64 = 2;
+#[cfg(not(feature = "u256-tokens"))]
 // Corresponds to ic_icrc1_ledger::LEDGER_VERSION where blocks are migrated to stable structures
 const LEDGER_VERSION_3: u64 = 3;
 
