ecce574b4e03ce4ddd274383371132e0de459f3d
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-03-20 14:03:24 +0100

    extracting a long test

--- rs/http_utils/src/file_downloader.rs
+++ rs/http_utils/src/file_downloader.rs
@@ -361,8 +361,6 @@ impl Error for FileDownloadError {}
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
-
     use assert_matches::assert_matches;
     use flate2::write::GzEncoder;
     use flate2::Compression;
@@ -686,70 +684,6 @@ mod tests {
                 );
             }
             _ => panic!("Expected FileDownloadError::IoError"),
-        }
-    }
-
-    async fn get_hash(downloader: FileDownloader, version: &str) -> String {
-        let url = format!(
-            "https://download.dfinity.systems/ic/{}/guest-os/update-img/SHA256SUMS",
-            version
-        );
-
-        let file_path = std::path::Path::new("/tmp/shasums");
-        downloader
-            .download_file(&url, file_path, None)
-            .await
-            .unwrap();
-
-        std::fs::read_to_string(file_path)
-            .unwrap()
-            .lines()
-            .find(|line| line.ends_with("update-img.tar.zst"))
-            .map(|line| line.split_once(' ').unwrap().0)
-            .unwrap()
-            .to_string()
-    }
-
-    #[test]
-    async fn test_large_file_download() {
-        let version = std::env::var("MAINNET_NNS_SUBNET_REVISION_ENV").unwrap();
-
-        // Use a normal downloader since hash is required for further testing.
-        let downloader = FileDownloader::new_with_timeout(None, std::time::Duration::from_secs(30));
-        let hash = get_hash(downloader, version.as_ref()).await;
-
-        let url = format!(
-            "https://download.dfinity.systems/ic/{}/guest-os/update-img/update-img.tar.zst",
-            version
-        );
-        let downloader = FileDownloader::new_with_timeout(None, std::time::Duration::from_secs(2));
-
-        let output = PathBuf::from_str("/tmp/replica").unwrap();
-        let mut last_iteration_size = 0;
-        loop {
-            let response = downloader
-                .download_file(url.as_str(), output.as_path(), Some(hash.clone()))
-                .await;
-            if response.is_ok() {
-                break;
-            }
-
-            if let Err(FileDownloadError::ReqwestError(e)) = response {
-                if !e.is_timeout() {
-                    panic!("Unexpected error: {:?}", e);
-                }
-            } else {
-                panic!("Unexpected error: {:?}", response);
-            }
-
-            let metadata = std::fs::metadata(&output).unwrap();
-            assert_ne!(
-                metadata.len(),
-                last_iteration_size,
-                "Sizes are the same in two iterations meaning there is a bug in the implementation"
-            );
-            last_iteration_size = metadata.len();
-            assert!(last_iteration_size > 0);
         }
     }
 }
