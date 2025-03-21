dfinity/ic

commit 363520e0432194f562b0ed57c128ec53001d38d0

chore(IDX): flatten upload.sh (#4396)
This moves the "skip" logic out of `upload.sh` and into `upload.bzl`.
This allows us to reuse `upload.sh` wherever necessary without the
script needed to figure out if it needs to run or not.

This'll allow us to move the upload out of the build and into a new CI
step.

ci/src/artifacts/upload.sh
@@ -2,20 +2,23 @@
 
 set -eEuo pipefail
 
-while read -r k v; do
-    case "$k" in
-        HOME)
-            # Required by rclone to get credentials from $HOME/.aws/credentials
-            export HOME="$v"
-            ;;
-    esac
-done <"$VERSION_FILE"
+# ~/.aws/credentials is needed by rclone. If home is not set, expect
+# VERSION_FILE to contain the $HOME.
+if [ -z "${HOME:-}" ]; then
+    while read -r k v; do
+        case "$k" in
+            HOME)
+                export HOME="$v"
+                ;;
+        esac
+    done <"$VERSION_FILE"
+fi
 
 VERSION="$(cat $VERSION_TXT)"
 
 # rclone reads the $(dirname $f) to get file attributes.
 # Therefore symlink should be resolved.
-f="$1"
+f="${1:?No file to upload}"
 if [ -L "$f" ]; then
     f=$(readlink "$f")
 fi
@@ -34,33 +37,34 @@ rclone_common_flags=(
     --s3-upload-cutoff=5G
 )
 
-if [ "${UPLOAD_BUILD_ARTIFACTS:-}" == "1" ]; then
-    echo "uploading $f to AWS" >&2
-    AWS_PROFILE=default "$RCLONE" \
-        "${rclone_common_flags[@]}" \
-        --s3-provider=AWS \
-        --s3-region=eu-central-1 \
-        --s3-env-auth \
-        copy \
-        "$f" \
-        ":s3:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
-    echo "done uploading to AWS" >&2
+REMOTE_SUBDIR="${REMOTE_SUBDIR:?Remote subdirectory not set}"
 
-    # Upload to Cloudflare's R2 (S3)
-    # using profile 'cf' to look up the right creds in ~/.aws/credentials
-    echo "uploading $f to Cloudflare" >&2
-    echo AWS_PROFILE=cf "$RCLONE" -v \
-        "${rclone_common_flags[@]}" \
-        --s3-provider=Cloudflare \
-        --s3-endpoint=https://64059940cc95339fc7e5888f431876ee.r2.cloudflarestorage.com \
-        --s3-env-auth \
-        copy \
-        "$f" \
-        ":s3:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
-    echo "done uploading to Cloudflare" >&2
-else
-    echo "dry run for $f"
-fi
+echo "uploading $f to AWS" >&2
+AWS_PROFILE=default "$RCLONE" \
+    "${rclone_common_flags[@]}" \
+    --s3-provider=AWS \
+    --s3-region=eu-central-1 \
+    --s3-env-auth \
+    copy \
+    "$f" \
+    ":s3:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
+echo "done uploading to AWS" >&2
+
+# Upload to Cloudflare's R2 (S3)
+# using profile 'cf' to look up the right creds in ~/.aws/credentials
+echo "uploading $f to Cloudflare" >&2
+echo AWS_PROFILE=cf "$RCLONE" -v \
+    "${rclone_common_flags[@]}" \
+    --s3-provider=Cloudflare \
+    --s3-endpoint=https://64059940cc95339fc7e5888f431876ee.r2.cloudflarestorage.com \
+    --s3-env-auth \
+    copy \
+    "$f" \
+    ":s3:dfinity-download-public/ic/${VERSION}/$REMOTE_SUBDIR/"
+echo "done uploading to Cloudflare" >&2
 
 URL_PATH="ic/${VERSION}/$REMOTE_SUBDIR/$(basename $f)"
-echo "https://download.dfinity.systems/${URL_PATH}" >"$2"
+echo "https://download.dfinity.systems/${URL_PATH}" >&2
+if [ -n "${2:-}" ]; then
+    echo "https://download.dfinity.systems/${URL_PATH}" >"$2"
+fi

ci/src/artifacts/upload.bzl
@@ -29,19 +29,26 @@ def _upload_artifact_impl(ctx):
         outputs = [checksum],
     )
 
+    uploader = ctx.file._artifacts_uploader
+
+    # If s3 upload is not enabled, then use a noop uploader.
+    if not s3_upload:
+        uploader = ctx.actions.declare_file("dummy_upload.sh")
+        ctx.actions.write(uploader, "#!/usr/bin/env bash\necho dummy upload for $1\ntouch $2", is_executable = True)
+
     allinputs = ctx.files.inputs + [checksum]
     for f in allinputs:
         filename = ctx.label.name + "_" + f.basename
         url = ctx.actions.declare_file(filename + ".url")
         ctx.actions.run(
-            executable = ctx.file._artifacts_uploader,
+            executable = uploader,
             arguments = [f.path, url.path],
             env = {
                 "RCLONE": ctx.file._rclone.path,
                 "REMOTE_SUBDIR": ctx.attr.remote_subdir,
                 "VERSION_FILE": ctx.version_file.path,
                 "VERSION_TXT": ctx.file._version_txt.path,
-            } | ({"UPLOAD_BUILD_ARTIFACTS": "1"} if s3_upload else {}),
+            },
             inputs = [f, ctx.version_file, ctx.file._version_txt],
             outputs = [url],
             tools = [ctx.file._rclone],

commit 75e7a2020b0af0d08db92bfcfa242f799e0f5ab1

fix(IDX): upload artifacts to Cloudflare
An issue was introduced in
https://github.com/dfinity/ic/commit/363520e0432194f562b0ed57c128ec53001d38d0
where the Cloudflare upload was effectively commented out.

ci/src/artifacts/upload.sh
@@ -53,7 +53,7 @@ echo "done uploading to AWS" >&2
 # Upload to Cloudflare's R2 (S3)
 # using profile 'cf' to look up the right creds in ~/.aws/credentials
 echo "uploading $f to Cloudflare" >&2
-echo AWS_PROFILE=cf "$RCLONE" -v \
+AWS_PROFILE=cf "$RCLONE" -v \
     "${rclone_common_flags[@]}" \
     --s3-provider=Cloudflare \
     --s3-endpoint=https://64059940cc95339fc7e5888f431876ee.r2.cloudflarestorage.com \