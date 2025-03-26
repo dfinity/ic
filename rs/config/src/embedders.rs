1b113dc3e83418408fff01abbbf8656a6e3db0f5
Adam Bratschi-Kaye <adam.bratschikaye@dfinity.org>
2025-03-26 09:55:59 +0000

    reduce default sandbox rss

--- rs/config/src/embedders.rs
+++ rs/config/src/embedders.rs
@@ -55,8 +55,8 @@ pub(crate) const DEFAULT_MAX_SANDBOX_COUNT: usize = 10_000;
 /// duration and sandbox process eviction is activated.
 pub(crate) const DEFAULT_MAX_SANDBOX_IDLE_TIME: Duration = Duration::from_secs(30 * 60);
 
-/// Sandbox processes may be evicted if their total RSS exceeds 70 GiB.
-pub(crate) const DEFAULT_MAX_SANDBOXES_RSS: NumBytes = NumBytes::new(70 * 1024 * 1024 * 1024);
+/// Sandbox processes may be evicted if their total RSS exceeds 50 GiB.
+pub(crate) const DEFAULT_MAX_SANDBOXES_RSS: NumBytes = NumBytes::new(50 * 1024 * 1024 * 1024);
 
 /// The maximum number of pages that a message dirties without optimizing dirty
 /// page copying by triggering a new execution slice for copying pages.
