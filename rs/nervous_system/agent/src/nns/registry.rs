ad802bda0fe03626acf859e8bf460f3661068235
Daniel Wong <daniel.wong@dfinity.org>
2025-03-27 21:08:05 +0100

    Added get_monolithic_blob. No tests yet though!

--- rs/nervous_system/agent/src/nns/registry.rs
+++ rs/nervous_system/agent/src/nns/registry.rs
@@ -1,7 +1,9 @@
 use crate::CallCanisters;
 use ic_base_types::{CanisterId, SubnetId};
 use ic_nns_constants::REGISTRY_CANISTER_ID;
+use ic_registry_canister_api::{Chunk, GetChunkRequest};
 use registry_canister::pb::v1::GetSubnetForCanisterRequest;
+use std::fmt::Display;
 
 pub mod requests;
 
@@ -27,4 +29,35 @@ pub async fn get_subnet_for_canister<C: CallCanisters>(
         .expect("SubnetForCanister.subnet_id was not specified.");
 
     Ok(SubnetId::from(subnet_id))
+}
+
+/// Returns the concatenation of a bunch of chunks from Registry.
+///
+/// Each chunk is fetched via Registry's get_chunk method.
+pub async fn get_monolithic_blob<C: CallCanisters>(
+    agent: &C,
+    canister_id: CanisterId,
+    chunk_content_sha256s: &[Vec<u8>],
+) -> Result<Vec<u8>, String> {
+    fn new_err(cause: impl Display) -> String {
+        format!("Failed to get monolithic registry blob: {}", cause)
+    }
+
+    let mut result = vec![];
+
+    // TODO: This could be done in parallel, but this is simpler.
+    for content_sha256 in chunk_content_sha256s {
+        let content_sha256 = Some(content_sha256.to_vec());
+        let Chunk { content } = agent
+            .call(canister_id, GetChunkRequest { content_sha256 })
+            .await
+            .map_err(new_err)?
+            .map_err(new_err)?;
+
+        let mut content = content.ok_or_else(|| new_err("get_chunk response has no content"))?;
+
+        result.append(&mut content);
+    }
+
+    Ok(result)
 }
