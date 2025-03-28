af87efc66bae81c116ee3e6da3ede46fd142cc7d
Jason Zhu <jason.zhu@dfinity.org>
2025-03-28 20:20:52 +0000

    Remove topic_followee_index

--- rs/nns/governance/src/governance/benches.rs
+++ rs/nns/governance/src/governance/benches.rs
@@ -17,10 +17,8 @@ use crate::{
     },
     temporarily_disable_allow_active_neurons_in_stable_memory,
     temporarily_disable_migrate_active_neurons_to_stable_memory,
-    temporarily_disable_stable_memory_following_index,
     temporarily_enable_allow_active_neurons_in_stable_memory,
     temporarily_enable_migrate_active_neurons_to_stable_memory,
-    temporarily_enable_stable_memory_following_index,
     test_utils::{MockEnvironment, StubCMC, StubIcpLedger},
 };
 use canbench_rs::{bench, bench_fn, BenchResult};
@@ -413,24 +411,8 @@ fn make_neuron(
 }
 
 #[bench(raw)]
-fn cascading_vote_stable_neurons_with_heap_index() -> BenchResult {
-    let _a = temporarily_enable_allow_active_neurons_in_stable_memory();
-    let _b = temporarily_disable_stable_memory_following_index();
-    let _c = temporarily_enable_migrate_active_neurons_to_stable_memory();
-
-    cast_vote_cascade_helper(
-        SetUpStrategy::Chain {
-            num_neurons: 151,
-            num_followees: 15,
-        },
-        Topic::NetworkEconomics,
-    )
-}
-
-#[bench(raw)]
 fn cascading_vote_stable_everything() -> BenchResult {
     let _a = temporarily_enable_allow_active_neurons_in_stable_memory();
-    let _b = temporarily_enable_stable_memory_following_index();
     let _c = temporarily_enable_migrate_active_neurons_to_stable_memory();
 
     cast_vote_cascade_helper(
@@ -445,7 +427,6 @@ fn cascading_vote_stable_everything() -> BenchResult {
 #[bench(raw)]
 fn cascading_vote_all_heap() -> BenchResult {
     let _a = temporarily_disable_allow_active_neurons_in_stable_memory();
-    let _b = temporarily_disable_stable_memory_following_index();
     let _c = temporarily_disable_migrate_active_neurons_to_stable_memory();
 
     cast_vote_cascade_helper(
@@ -460,7 +441,6 @@ fn cascading_vote_all_heap() -> BenchResult {
 #[bench(raw)]
 fn cascading_vote_heap_neurons_stable_index() -> BenchResult {
     let _a = temporarily_disable_allow_active_neurons_in_stable_memory();
-    let _b = temporarily_enable_stable_memory_following_index();
     let _c = temporarily_disable_migrate_active_neurons_to_stable_memory();
 
     cast_vote_cascade_helper(
@@ -475,7 +455,6 @@ fn cascading_vote_heap_neurons_stable_index() -> BenchResult {
 #[bench(raw)]
 fn single_vote_all_stable() -> BenchResult {
     let _a = temporarily_enable_allow_active_neurons_in_stable_memory();
-    let _b = temporarily_enable_stable_memory_following_index();
     let _c = temporarily_enable_migrate_active_neurons_to_stable_memory();
 
     cast_vote_cascade_helper(
@@ -487,7 +466,6 @@ fn single_vote_all_stable() -> BenchResult {
 #[bench(raw)]
 fn centralized_following_all_stable() -> BenchResult {
     let _a = temporarily_enable_allow_active_neurons_in_stable_memory();
-    let _b = temporarily_enable_stable_memory_following_index();
     let _c = temporarily_enable_migrate_active_neurons_to_stable_memory();
 
     cast_vote_cascade_helper(
--- rs/nns/governance/unreleased_changelog.md
+++ rs/nns/governance/unreleased_changelog.md
@@ -20,6 +20,9 @@ on the process that this file is part of, see
 
 ## Removed
 
+* The `topic_followee_index` in the heap is removed, along with the flag
+  `USE_STABLE_MEMORY_FOLLOWING_INDEX` that was set to true in the proposal 135063.
+
 ## Fixed
 
 ## Security
