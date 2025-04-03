a55c7924b17e91d807ee0e15fe921cce3bc25901
Shuo Wang <shuo.wang94@gmail.com>
2025-04-03 16:22:31 -0700

    fix Send

--- rs/state_layout/src/state_layout.rs
+++ rs/state_layout/src/state_layout.rs
@@ -2519,7 +2519,7 @@ pub fn validate_wasm(wasm_file_layout: &dyn WasmFileLayout) -> Result<(), Layout
 
 impl<T> WasmFile<T>
 where
-    T: ReadPolicy,
+    T: ReadPolicy + Send,
 {
     pub fn deserialize(self, module_hash: Option<WasmHash>) -> Result<CanisterModule, LayoutError> {
         CanisterModule::new_from_file(Box::new(self), module_hash).map_err(|err| {
