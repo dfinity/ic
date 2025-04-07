1378b24060b68794a3193aac2ce493b3a5327ffb
Jack Lloyd <jack.lloyd@dfinity.org>
2025-04-07 14:31:31 -0400

    chore(crypto): CRP-2645 Use ic-vetkd-utils for VetKD key derivation

--- packages/ic-vetkd-utils/src/lib.rs
+++ packages/ic-vetkd-utils/src/lib.rs
@@ -106,6 +106,17 @@ impl TransportSecretKey {
     }
 }
 
+/// Return true iff the argument is a valid encoding of a transport public key
+pub fn is_valid_transport_public_key_encoding(bytes: &[u8]) -> bool {
+    if bytes.len() != 48 {
+        return false;
+    }
+
+    let bytes : [u8; 48] = bytes.try_into().expect("Size already checked");
+
+    option_from_ctoption(G1Affine::from_compressed(&bytes)).is_some()
+}
+
 #[cfg_attr(feature = "js", wasm_bindgen)]
 #[derive(Clone, Debug, Eq, PartialEq)]
 /// A derived public key
