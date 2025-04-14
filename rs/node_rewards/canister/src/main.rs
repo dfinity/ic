58966dcc5dd7cf27b697b4a00a87c0fb8535f663
Max Summe <maximilian.summe@dfinity.org>
2025-04-14 15:57:13 -0700

    wip

--- rs/node_rewards/canister/src/main.rs
+++ rs/node_rewards/canister/src/main.rs
@@ -12,6 +12,7 @@ use ic_registry_canister_client::StableCanisterRegistryClient;
 use std::cell::RefCell;
 use std::sync::Arc;
 use std::time::Duration;
+use ic_nns_constants::GOVERNANCE_CANISTER_ID;
 
 fn main() {}
 
@@ -64,6 +65,12 @@ fn schedule_registry_sync() {
     });
 }
 
+fn panic_if_not_governance() {
+    if ic_cdk::caller() != GOVERNANCE_CANISTER_ID.get().0 {
+        panic!("Only the governance canister can call this method");
+    }
+}
+
 #[cfg(any(feature = "test", test))]
 #[query(hidden = true)]
 fn get_registry_value(key: String) -> Result<Option<Vec<u8>>, String> {
@@ -74,6 +81,7 @@ fn get_registry_value(key: String) -> Result<Option<Vec<u8>>, String> {
 async fn get_node_providers_monthly_xdr_rewards(
     request: GetNodeProvidersMonthlyXdrRewardsRequest,
 ) -> GetNodeProvidersMonthlyXdrRewardsResponse {
+    panic_if_not_governance();
     NodeRewardsCanister::get_node_providers_monthly_xdr_rewards(&CANISTER, request).await
 }
 
@@ -95,4 +103,11 @@ mod tests {
         let result = service_equal(declared_interface, implemented_interface);
         assert!(result.is_ok(), "{:?}\n\n", result.unwrap_err());
     }
+   // Test only governance can call get_monthly_xdr_rewards
+   #[test]
+   #[should_panic(expected = "Only the governance canister can call this method")]
+   fn test_get_monthly_xdr_rewards_only_governance_can_call() {
+      let request = GetNodeProvidersMonthlyXdrRewardsRequest {registry_version: None};
+      get_node_providers_monthly_xdr_rewards(request);
+   }
 }
