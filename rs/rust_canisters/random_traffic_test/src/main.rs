113f01ef3caedb6ee6e293777c944a02c3fc5e4a
Christian Stieger <christian.stieger@dfinity.org>
2025-04-14 13:59:48 +0000

    .

--- rs/rust_canisters/random_traffic_test/src/main.rs
+++ rs/rust_canisters/random_traffic_test/src/main.rs
@@ -339,7 +339,8 @@ async fn pulse(calls_count: u32) -> u32 {
 /// for heartbeats that try to make at least 1 call.
 #[heartbeat]
 async fn heartbeat() {
-    if let calls_count @ 1.. = CONFIG.with_borrow(|config| config.calls_per_heartbeat) {
+    let calls_count = CONFIG.with_borrow(|config| config.calls_per_heartbeat);
+    if calls_count > 0 {
         pulse(calls_count).await;
         SUCCESSFUL_HEARTBEAT_INVOCATIONS
             .replace(SUCCESSFUL_HEARTBEAT_INVOCATIONS.get() + calls_count);
