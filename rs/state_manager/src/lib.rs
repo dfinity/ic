d131485e1c8d96fc99fc83735c2bd72742f04bff
Shuo Wang <shuo.wang94@gmail.com>
2025-04-16 14:45:48 -0700

    add metrics for loaded wasm

--- rs/state_manager/src/lib.rs
+++ rs/state_manager/src/lib.rs
@@ -61,7 +61,7 @@ use ic_types::{
 use ic_utils_thread::{deallocator_thread::DeallocatorThread, JoinOnDrop};
 use prometheus::{Histogram, HistogramVec, IntCounter, IntCounterVec, IntGauge, IntGaugeVec};
 use prost::Message;
-use std::convert::{From, TryFrom};
+use std::convert::{identity, From, TryFrom};
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::ops::Deref;
@@ -191,6 +191,7 @@ pub struct CheckpointMetrics {
     replicated_state_altered_after_checkpoint: IntCounter,
     tip_handler_request_duration: HistogramVec,
     num_page_maps_by_load_status: IntGaugeVec,
+    num_loaded_wasm_files_by_source: IntGaugeVec,
     log: ReplicaLogger,
 }
 
@@ -238,6 +239,12 @@ impl CheckpointMetrics {
             "How many PageMaps are loaded or not at the end of checkpoint interval.",
             &["status"],
         );
+
+        let num_loaded_wasm_files_by_source = metrics_registry.int_gauge_vec(
+            "state_manager_num_loaded_wasm_files_by_source",
+            "How many WasmFiles of canisters or snapshots are loaded at the end of checkpoint interval.",
+            &["source"],
+        );
         Self {
             make_checkpoint_step_duration,
             load_checkpoint_step_duration,
@@ -246,6 +253,7 @@ impl CheckpointMetrics {
             replicated_state_altered_after_checkpoint,
             tip_handler_request_duration,
             num_page_maps_by_load_status,
+            num_loaded_wasm_files_by_source,
             log: replica_logger,
         }
     }
@@ -1591,6 +1599,46 @@ impl StateManagerImpl {
             .num_page_maps_by_load_status
             .with_label_values(&["not_loaded"])
             .set(not_loaded);
+    }
+
+    /// Populate `num_loaded_wasm_files_by_source` in the metrics with their actual
+    /// values in provided state.
+    fn observe_num_loaded_wasm_files(&self, state: &ReplicatedState) {
+        let num_loaded_canister_wasm = state
+            .canister_states
+            .iter()
+            .filter_map(|(_, canister)| canister.execution_state.as_ref())
+            .filter(|execution_state| {
+                execution_state
+                    .wasm_binary
+                    .binary
+                    .file_loading_status()
+                    .is_some_and(identity)
+            })
+            .count();
+
+        let num_loaded_snapshot_wasm = state
+            .canister_snapshots
+            .iter()
+            .filter(|(_, snapshot)| {
+                snapshot
+                    .execution_snapshot()
+                    .wasm_binary
+                    .file_loading_status()
+                    .is_some_and(identity)
+            })
+            .count();
+
+        self.metrics
+            .checkpoint_metrics
+            .num_loaded_wasm_files_by_source
+            .with_label_values(&["canister"])
+            .set(num_loaded_canister_wasm as i64);
+        self.metrics
+            .checkpoint_metrics
+            .num_loaded_wasm_files_by_source
+            .with_label_values(&["snapshot"])
+            .set(num_loaded_snapshot_wasm as i64);
     }
 
     /// Reads states metadata file, returning an empty one if any errors occurs.
