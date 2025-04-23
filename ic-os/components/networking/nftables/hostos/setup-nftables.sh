79f85398c538fc9af4911aaa97f69b84bed5e3b1
Andrew Battat <andrew.battat@dfinity.org>
2025-04-23 19:51:09 +0000

    Rename get_ipv6_prefix

--- ic-os/components/networking/nftables/hostos/setup-nftables.sh
+++ ic-os/components/networking/nftables/hostos/setup-nftables.sh
@@ -16,7 +16,7 @@ Usage:
 EOF
 }
 
-function read_config_variables() {
+function get_ipv6_prefix() {
     ipv6_prefix=$(get_config_value '.network_settings.ipv6_config.Deterministic.prefix')
     IPV6_PREFIX="${ipv6_prefix:+${ipv6_prefix}::/64}" # Add suffix to prefix if found
     IPV6_PREFIX="${IPV6_PREFIX:-::1/128}"             # Default to loopback for easy templating
@@ -42,7 +42,7 @@ if [ "${IN_FILE}" == "" -o "${OUT_FILE}" == "" ]; then
     exit 1
 fi
 
-read_config_variables
+get_ipv6_prefix
 
 mkdir -p /run/ic-node/nftables-ruleset/
 sed -e "s@{{ ipv6_prefix }}@${IPV6_PREFIX}@" \
