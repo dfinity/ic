2dfbf68b91735749f43699f90563e6d10d002b0d
Daniel Wong <daniel.wong@dfinity.org>
2025-04-24 16:02:02 +0200

    lint

--- rs/registry/canister/src/registry.rs
+++ rs/registry/canister/src/registry.rs
@@ -453,9 +453,7 @@ mod tests {
     /// This should bring back the registry in a state indistinguishable
     /// from the one before calling this method.
     fn serialize_then_deserialize(registry: Registry) {
-        let serialized = registry
-            .serializable_form()
-            .encode_to_vec();
+        let serialized = registry.serializable_form().encode_to_vec();
 
         let mut restored = Registry::new();
         restored.from_serializable_form(
