79cdfea8ca9cab4774acf9774fa62f4636477748
Kamil Popielarz <kamil.popielarz@dfinity.org>
2025-04-25 11:45:25 +0000

    progress

--- rs/tests/consensus/max_ingress_payload_size_test.rs
+++ rs/tests/consensus/max_ingress_payload_size_test.rs
@@ -17,161 +17,267 @@ Coverage::
 
 end::catalog[] */
 
-use ic_agent::{Agent, AgentError};
-use ic_base_types::PrincipalId;
+use anyhow::Result;
+use assert_matches::assert_matches;
+use ic_agent::agent_error::HttpErrorPayload;
+use ic_agent::AgentError;
+use ic_consensus_system_test_utils::rw_message::install_nns_and_check_progress;
 use ic_registry_subnet_type::SubnetType;
-use ic_system_test_driver::driver::group::SystemTestGroup;
-use ic_system_test_driver::systest;
 use ic_system_test_driver::{
     driver::{
-        ic::{InternetComputer, Subnet},
+        boundary_node::{BoundaryNode, BoundaryNodeVm},
+        group::SystemTestGroup,
+        ic::InternetComputer,
         test_env::TestEnv,
-        test_env_api::{HasPublicApiUrl, HasTopologySnapshot, IcNodeContainer, TopologySnapshot},
+        test_env_api::{HasPublicApiUrl, HasTopologySnapshot, IcNodeContainer, IcNodeSnapshot},
     },
+    systest,
     util::UniversalCanister,
 };
 use ic_universal_canister::wasm;
 
-use anyhow::Result;
-use futures::join;
-use slog::{info, Logger};
-use std::sync::Arc;
+const BOUNDARY_NODE_NAME: &str = "boundary-node-1";
+const MAX_QUERY_MESSAGE_SIZE_BYTES: u64 = 4 * 1024 * 1024;
+const REQUEST_HEADERS_OVERHEAD: u64 = 360;
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/413
+const CONTENT_TOO_LARGE_STATUS: u16 = 413;
 
-const INGRESS_MAX_SIZE: usize = 4 * 1024 * 1024;
-
-// NOTE: Unfortunately, the maximum sizes can not be used in the test.
-// In both cases, we have to account for the extra command bytes of the unican,
-// that precede the message. In the case of the ingress messages, we also have
-// to account for the ingress message header.
-const INGRESS_MSG_SIZE: usize = 4 * 1024 * 1024 - 360;
-
-/// Configuration that is used for the maximum payload size tests.
-/// In this test, the payload size is set to 2MiB and the ingress size to 4MiB.
-/// This allows us to test, that in a misconfigured setting, the specified
-/// max_block_payload_size still fits through.
-/// It also allows us to test the limit in the XNet setting properly.
+/// Sets up a testnet with
+/// 1. System subnet with a single node
+/// 2. Application subnet with a single node
+/// 3. Boundary node
+/// and installs [`UniversalCanister`]s on the subnets.
 fn setup(env: TestEnv) {
     InternetComputer::new()
-        .add_subnet(
-            Subnet::new(SubnetType::System)
-                .add_nodes(1)
-                .with_max_ingress_message_size(INGRESS_MAX_SIZE as u64),
-        )
-        .add_subnet(
-            Subnet::new(SubnetType::Application)
-                .add_nodes(1)
-                .with_max_ingress_message_size(INGRESS_MAX_SIZE as u64),
-        )
+        .add_fast_single_node_subnet(SubnetType::System)
+        .add_fast_single_node_subnet(SubnetType::System)
+        .add_fast_single_node_subnet(SubnetType::Application)
         .setup_and_start(&env)
         .expect("failed to setup IC under test");
+
+    install_nns_and_check_progress(env.topology_snapshot());
+
+    BoundaryNode::new(String::from(BOUNDARY_NODE_NAME))
+        .allocate_vm(&env)
+        .expect("Allocation of BoundaryNode failed.")
+        .for_ic(&env, "")
+        .use_real_certs_and_dns()
+        .start(&env)
+        .expect("failed to setup BoundaryNode VM");
+
+    let rt = tokio::runtime::Runtime::new().expect("Could not create tokio runtime.");
+
+    for subnet in env.topology_snapshot().subnets() {
+        for node in subnet.nodes() {
+            node.await_status_is_healthy().unwrap();
+            let logger = env.logger();
+            rt.block_on(async move {
+                let agent = node.build_default_agent_async().await;
+                let canister = UniversalCanister::new_with_retries(
+                    &agent,
+                    node.effective_canister_id(),
+                    &logger,
+                )
+                .await;
+
+                canister
+                    .update(wasm().stable_grow(100).reply())
+                    .await
+                    .unwrap();
+            });
+        }
+    }
+
+    let boundary_node_vm = env
+        .get_deployed_boundary_node(BOUNDARY_NODE_NAME)
+        .unwrap()
+        .get_snapshot()
+        .unwrap();
+
+    boundary_node_vm
+        .await_status_is_healthy()
+        .expect("Boundary node did not come up healthy.");
 }
 
-/// Tests, that an ingress message that is close to the maximum size is accepted
-/// by the block maker, whereas a message this is exactly the maximum size is
-/// not accepted.
-fn test(env: TestEnv) {
-    let log = env.logger();
-    let topology = env.topology_snapshot();
+fn get_first_node(env: &TestEnv, subnet_type: SubnetType) -> IcNodeSnapshot {
+    let root_subnet_id = env.topology_snapshot().root_subnet_id();
 
-    info!(log, "Checking readiness of all nodes after the IC setup...");
-    topology.subnets().for_each(|subnet| {
-        subnet
-            .nodes()
-            .for_each(|node| node.await_status_is_healthy().unwrap())
-    });
+    env.topology_snapshot()
+        .subnets()
+        .filter(|subnet| subnet.subnet_id != root_subnet_id)
+        .find(|subnet| subnet.subnet_type() == subnet_type)
+        .expect("There is a subnet for each subnet type")
+        .nodes()
+        .next()
+        .expect("Each subnet has at least one node")
+}
 
-    info!(log, "All nodes are ready, IC setup succeeded.");
-    let (
-        (assist_agent, assist_effective_canister_id),
-        (target_agent, target_effective_canister_id),
-    ) = setup_agents(topology);
-    let rt = tokio::runtime::Runtime::new().expect("Could not create tokio runtime.");
-    rt.block_on(async move {
-        let (_, target_unican) = setup_unicans(
-            &log,
-            &assist_agent,
-            assist_effective_canister_id,
-            &target_agent,
-            target_effective_canister_id,
-        )
-        .await;
-
-        // Send a message that is supposed to fit.
-        make_ingress_call(&target_unican, INGRESS_MSG_SIZE)
-            .await
-            .unwrap();
-    })
+enum RequestType {
+    Query,
+    Update,
 }
 
 /// Makes an ingress call to the specified canister with a message of the
 /// specified size.
 async fn make_ingress_call(
-    dst: &UniversalCanister<'_>,
+    canister: &UniversalCanister<'_>,
     size: usize,
+    request: RequestType,
 ) -> Result<Vec<u8>, AgentError> {
-    // NOTE: We use reply here before stable write, since we don't actually
-    // care about the write, we just want to send a large message.
-    dst.update(wasm().reply().stable_write(0, &vec![0; size]))
-        .await
+    match request {
+        RequestType::Query => {
+            canister
+                .query(wasm().reply().stable_write(0, &vec![0; size]))
+                .await
+        }
+        RequestType::Update => {
+            canister
+                .update(wasm().reply().stable_write(0, &vec![0; size]))
+                .await
+        }
+    }
 }
 
-async fn setup_unicans<'a>(
-    logger: &Logger,
-    assist_agent: &'a Agent,
-    assist_effective_canister_id: PrincipalId,
-    target_agent: &'a Agent,
-    target_effective_canister_id: PrincipalId,
-) -> (Arc<UniversalCanister<'a>>, Arc<UniversalCanister<'a>>) {
-    // Install a `UniversalCanister` on each
-    let (assist_unican, target_unican) = join!(
-        UniversalCanister::new_with_retries(assist_agent, assist_effective_canister_id, logger),
-        UniversalCanister::new_with_retries(target_agent, target_effective_canister_id, logger)
+fn send_request(
+    env: &TestEnv,
+    subnet_type: SubnetType,
+    message_size: u64,
+    call: RequestType,
+) -> Result<(), AgentError> {
+    let node = get_first_node(&env, subnet_type);
+    let deployed_boundary_node = env.get_deployed_boundary_node(BOUNDARY_NODE_NAME).unwrap();
+    let boundary_node_vm = deployed_boundary_node.get_snapshot().unwrap();
+    let agent = boundary_node_vm.build_default_agent();
+    let universal_canister =
+        UniversalCanister::from_canister_id(&agent, node.effective_canister_id().into());
+
+    let rt = tokio::runtime::Runtime::new().expect("Could not create tokio runtime.");
+    rt.block_on(async move {
+        make_ingress_call(&universal_canister, message_size as usize, call)
+            .await
+            .map(|_| ())
+    })
+}
+
+fn test_app_subnet_update_within_limits(env: TestEnv) {
+    assert_eq!(
+        send_request(
+            &env,
+            SubnetType::Application,
+            ic_limits::MAX_INGRESS_BYTES_PER_MESSAGE_APP_SUBNET - REQUEST_HEADERS_OVERHEAD,
+            RequestType::Update,
+        ),
+        Ok(())
     );
+}
 
-    // NOTE: Since we will be making calls to these canisters in parallel, we have
-    // to make it `Send`.
-    let (assist_unican, target_unican) = (Arc::new(assist_unican), Arc::new(target_unican));
-
-    // Grow the stable memory so it can actually store the amount of data
-    join!(
-        stable_grow(&assist_unican, 100),
-        stable_grow(&target_unican, 100)
+fn test_app_subnet_query_within_limits(env: TestEnv) {
+    assert_eq!(
+        send_request(
+            &env,
+            SubnetType::Application,
+            MAX_QUERY_MESSAGE_SIZE_BYTES - REQUEST_HEADERS_OVERHEAD,
+            RequestType::Query,
+        ),
+        Ok(())
     );
-
-    (assist_unican, target_unican)
 }
 
-fn setup_agents(
-    topology_snapshot: TopologySnapshot,
-) -> ((Agent, PrincipalId), (Agent, PrincipalId)) {
-    let target_node_nns = topology_snapshot.root_subnet().nodes().next().unwrap();
-    let assist_node_app = topology_snapshot
-        .subnets()
-        .find(|s| s.subnet_type() == SubnetType::Application)
-        .unwrap()
-        .nodes()
-        .next()
-        .unwrap();
-    let assist_agent_app = assist_node_app.with_default_agent(|agent| async move { agent });
-    let target_agent_nns = target_node_nns.with_default_agent(|agent| async move { agent });
-    (
-        (assist_agent_app, assist_node_app.effective_canister_id()),
-        (target_agent_nns, target_node_nns.effective_canister_id()),
-    )
+fn test_app_subnet_update_exceeds_limits(env: TestEnv) {
+    assert_matches!(
+        send_request(
+            &env,
+            SubnetType::Application,
+            ic_limits::MAX_INGRESS_BYTES_PER_MESSAGE_APP_SUBNET + 1,
+            RequestType::Update,
+        ),
+        Err(AgentError::HttpError(HttpErrorPayload {
+            status: CONTENT_TOO_LARGE_STATUS,
+            ..
+        }))
+    );
 }
 
-/// Grow the canisters stable memory by the given number of pages
-async fn stable_grow(unican: &UniversalCanister<'_>, num_pages: u32) {
-    unican
-        .update(wasm().stable_grow(num_pages).reply())
-        .await
-        .unwrap();
+fn test_app_subnet_query_exceeds_limits(env: TestEnv) {
+    assert_matches!(
+        send_request(
+            &env,
+            SubnetType::Application,
+            MAX_QUERY_MESSAGE_SIZE_BYTES + 1,
+            RequestType::Query,
+        ),
+        Err(AgentError::HttpError(HttpErrorPayload {
+            status: CONTENT_TOO_LARGE_STATUS,
+            ..
+        }))
+    );
+}
+
+fn test_nns_subnet_update_within_limits(env: TestEnv) {
+    assert_eq!(
+        send_request(
+            &env,
+            SubnetType::System,
+            ic_limits::MAX_INGRESS_BYTES_PER_MESSAGE_NNS_SUBNET - REQUEST_HEADERS_OVERHEAD,
+            RequestType::Update,
+        ),
+        Ok(())
+    );
+}
+
+fn test_nns_subnet_query_within_limits(env: TestEnv) {
+    assert_eq!(
+        send_request(
+            &env,
+            SubnetType::System,
+            MAX_QUERY_MESSAGE_SIZE_BYTES - REQUEST_HEADERS_OVERHEAD,
+            RequestType::Query,
+        ),
+        Ok(())
+    );
+}
+
+fn test_nns_subnet_update_exceeds_limits(env: TestEnv) {
+    assert_matches!(
+        send_request(
+            &env,
+            SubnetType::System,
+            ic_limits::MAX_INGRESS_BYTES_PER_MESSAGE_NNS_SUBNET + 1,
+            RequestType::Update,
+        ),
+        Err(AgentError::HttpError(HttpErrorPayload {
+            status: CONTENT_TOO_LARGE_STATUS,
+            ..
+        }))
+    );
+}
+
+fn test_nns_subnet_query_exceeds_limits(env: TestEnv) {
+    assert_matches!(
+        send_request(
+            &env,
+            SubnetType::System,
+            MAX_QUERY_MESSAGE_SIZE_BYTES + 1,
+            RequestType::Query,
+        ),
+        Err(AgentError::HttpError(HttpErrorPayload {
+            status: CONTENT_TOO_LARGE_STATUS,
+            ..
+        }))
+    );
 }
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_setup(setup)
-        .add_test(systest!(test))
+        .add_test(systest!(test_app_subnet_update_within_limits))
+        .add_test(systest!(test_app_subnet_update_exceeds_limits))
+        .add_test(systest!(test_app_subnet_query_within_limits))
+        .add_test(systest!(test_app_subnet_query_exceeds_limits))
+        .add_test(systest!(test_nns_subnet_query_within_limits))
+        .add_test(systest!(test_nns_subnet_query_exceeds_limits))
+        .add_test(systest!(test_nns_subnet_update_within_limits))
+        .add_test(systest!(test_nns_subnet_update_exceeds_limits))
         .execute_from_args()?;
     Ok(())
 }
