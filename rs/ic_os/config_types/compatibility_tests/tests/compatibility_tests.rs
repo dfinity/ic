b709168ce79947ca1fe5fbd2c242de1f17dcc69b
Andrew Battat <andrew.battat@dfinity.org>
2025-05-01 11:53:06 +0000

    Refactor test_backwards_compatibility

--- rs/ic_os/config_types/compatibility_tests/tests/compatibility_tests.rs
+++ rs/ic_os/config_types/compatibility_tests/tests/compatibility_tests.rs
@@ -3,32 +3,23 @@ use std::path::PathBuf;
 
 #[test]
 fn test_backwards_compatibility() {
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let test_data_dir = PathBuf::from(manifest_dir).join("fixtures");
+    let test_data_dir =
+        PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("fixtures");
 
-    // Test each historical config version
     for entry in std::fs::read_dir(&test_data_dir).unwrap() {
         let path = entry.unwrap().path();
-        if let Some(ext) = path.extension() {
-            if ext == "json" {
-                let config_json = std::fs::read_to_string(&path).unwrap();
-                let filename = path.file_name().unwrap().to_str().unwrap();
 
-                // Try to deserialize into each config type
-                if filename.starts_with("hostos") {
-                    match serde_json::from_str::<HostOSConfig>(&config_json) {
-                        Ok(_) => println!("Successfully deserialized {}", filename),
-                        Err(e) => {
-                            println!("Failed to deserialize {}: {}", filename, e);
-                            panic!(
-                                "Failed to deserialize historical HostOSConfig from {}",
-                                filename
-                            );
-                        }
-                    }
-                }
-            }
-        }
+        let config_json = std::fs::read_to_string(&path).unwrap();
+        let filename = path.file_name().unwrap().to_str().unwrap();
+
+        serde_json::from_str::<HostOSConfig>(&config_json).unwrap_or_else(|e| {
+            panic!(
+                "Failed to deserialize historical HostOSConfig from {}: {}",
+                filename, e
+            )
+        });
+
+        println!("Successfully deserialized {}", filename);
     }
 }
 
