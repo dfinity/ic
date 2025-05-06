7e0a166f2769674fd48fdddec35307e18d040437
Leo Eichhorn <leo.eichhorn@dfinity.org>
2025-05-06 15:55:12 +0000

    init

--- rs/registry/subnet_features/src/lib.rs
+++ rs/registry/subnet_features/src/lib.rs
@@ -194,7 +194,7 @@ impl TryFrom<pb::ChainKeyConfig> for ChainKeyConfig {
 
 #[cfg(test)]
 mod tests {
-    use ic_management_canister_types_private::EcdsaCurve;
+    use ic_management_canister_types_private::{EcdsaCurve, VetKdCurve, VetKdKeyId};
 
     use super::*;
     use std::str::FromStr;
@@ -221,14 +221,24 @@ mod tests {
     fn test_chain_key_config_round_trip() {
         // Run code under test.
         let chain_key_config = ChainKeyConfig {
-            key_configs: vec![KeyConfig {
-                key_id: MasterPublicKeyId::Ecdsa(EcdsaKeyId {
-                    curve: EcdsaCurve::Secp256k1,
-                    name: "test_curve".to_string(),
-                }),
-                pre_signatures_to_create_in_advance: 77,
-                max_queue_size: 30,
-            }],
+            key_configs: vec![
+                KeyConfig {
+                    key_id: MasterPublicKeyId::Ecdsa(EcdsaKeyId {
+                        curve: EcdsaCurve::Secp256k1,
+                        name: "test_key1".to_string(),
+                    }),
+                    pre_signatures_to_create_in_advance: 77,
+                    max_queue_size: 30,
+                },
+                KeyConfig {
+                    key_id: MasterPublicKeyId::VetKd(VetKdKeyId {
+                        curve: VetKdCurve::Bls12_381_G2,
+                        name: "test_key2".to_string(),
+                    }),
+                    pre_signatures_to_create_in_advance: 0,
+                    max_queue_size: 30,
+                },
+            ],
             signature_request_timeout_ns: Some(123_456),
             idkg_key_rotation_period_ms: Some(321_654),
         };
@@ -237,18 +247,32 @@ mod tests {
 
         // Assert expected result value.
         let expected_chain_key_config_pb = pb::ChainKeyConfig {
-            key_configs: vec![pb::KeyConfig {
-                key_id: Some(pb_types::MasterPublicKeyId {
-                    key_id: Some(pb_types::master_public_key_id::KeyId::Ecdsa(
-                        pb_types::EcdsaKeyId {
-                            curve: 1,
-                            name: "test_curve".to_string(),
-                        },
-                    )),
-                }),
-                pre_signatures_to_create_in_advance: Some(77),
-                max_queue_size: Some(30),
-            }],
+            key_configs: vec![
+                pb::KeyConfig {
+                    key_id: Some(pb_types::MasterPublicKeyId {
+                        key_id: Some(pb_types::master_public_key_id::KeyId::Ecdsa(
+                            pb_types::EcdsaKeyId {
+                                curve: 1,
+                                name: "test_key1".to_string(),
+                            },
+                        )),
+                    }),
+                    pre_signatures_to_create_in_advance: Some(77),
+                    max_queue_size: Some(30),
+                },
+                pb::KeyConfig {
+                    key_id: Some(pb_types::MasterPublicKeyId {
+                        key_id: Some(pb_types::master_public_key_id::KeyId::Vetkd(
+                            pb_types::VetKdKeyId {
+                                curve: 1,
+                                name: "test_key2".to_string(),
+                            },
+                        )),
+                    }),
+                    pre_signatures_to_create_in_advance: Some(0),
+                    max_queue_size: Some(30),
+                },
+            ],
             signature_request_timeout_ns: Some(123_456),
             idkg_key_rotation_period_ms: Some(321_654),
         };
