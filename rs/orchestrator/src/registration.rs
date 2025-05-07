dc4dff1e802451b71101a40892943ceb2cee5746
Pierugo Pace <pierugo.pace@dfinity.org>
2025-05-07 12:38:47 +0000

    chore: remove dependency to canister_client

--- rs/orchestrator/src/registration.rs
+++ rs/orchestrator/src/registration.rs
@@ -2,11 +2,11 @@
 use crate::{
     error::{OrchestratorError, OrchestratorResult},
     metrics::{KeyRotationStatus, OrchestratorMetrics},
-    signer::{Hsm, NodeProviderSigner, Signer},
+    signer::{Hsm, NodeIdentity, NodeProviderSigner, Signer},
     utils::http_endpoint_to_url,
 };
 use candid::Encode;
-use ic_canister_client::{Agent, Sender};
+use ic_agent::{agent::AgentBuilder, NonceGenerator};
 use ic_config::{
     http_handler::Config as HttpConfig,
     initial_ipv4_config::IPv4Config as InitialIPv4Config,
@@ -149,16 +149,18 @@ impl NodeRegistration {
                     let nns_url = self
                         .get_random_nns_url_from_config()
                         .expect("no NNS urls available");
-                    let agent = Agent::new(nns_url, signer);
+                    let agent = Agent::builder()
+                        .with_url(nns_url)
+                        .with_identity(signer)
+                        .build()
+                        .expect("Failed to create IC agent");
+                    let add_node_encoded = Encode!(&add_node_payload)
+                        .expect("Could not encode payload for the registration request");
+
                     if let Err(e) = agent
-                        .execute_update(
-                            &REGISTRY_CANISTER_ID,
-                            &REGISTRY_CANISTER_ID,
-                            "add_node",
-                            Encode!(&add_node_payload)
-                                .expect("Could not encode payload for the registration request"),
-                            generate_nonce(),
-                        )
+                        .update(&REGISTRY_CANISTER_ID.get_ref().0, "add_node")
+                        .with_arg(add_node_encoded)
+                        .call_and_wait()
                         .await
                     {
                         warn!(self.log, "Registration request failed: {}", e);
@@ -440,26 +442,22 @@ impl NodeRegistration {
             })
         };
 
-        let sender = Sender::Node {
-            pub_key: node_pub_key.key_value,
-            sign: Arc::new(sign_cmd),
-        };
-
-        let agent = Agent::new(nns_url.clone(), sender);
+        let signer = NodeIdentity::new(node_pub_key.key_value, Arc::new(sign_cmd));
+        let agent = Agent::builder()
+            .with_url(nns_url)
+            .with_identity(signer)
+            .build()
+            .expect("Failed to create IC agent");
         let update_node_payload = UpdateNodeDirectlyPayload {
             idkg_dealing_encryption_pk: Some(protobuf_to_vec(idkg_pk)),
         };
-
-        let arguments =
+        let update_node_encoded =
             Encode!(&update_node_payload).expect("Could not encode payload for update_node-call.");
+
         agent
-            .execute_update(
-                &REGISTRY_CANISTER_ID,
-                &REGISTRY_CANISTER_ID,
-                "update_node_directly",
-                arguments,
-                generate_nonce(),
-            )
+            .update(&REGISTRY_CANISTER_ID.get_ref().0, "update_node_directly")
+            .with_arg(update_node_encoded)
+            .call_and_wait()
             .await
             .map_err(|e| format!("Error when sending register additional key request: {e}"))?;
 
@@ -689,13 +687,29 @@ fn process_domain_name(log: &ReplicaLogger, domain: &str) -> OrchestratorResult<
 
 /// Create a nonce to be included with the ingress message sent to the node
 /// handler.
-fn generate_nonce() -> Vec<u8> {
-    SystemTime::now()
-        .duration_since(SystemTime::UNIX_EPOCH)
-        .unwrap()
-        .as_nanos()
-        .to_le_bytes()
-        .to_vec()
+struct NonceGeneratorImpl;
+impl NonceGenerator for NonceGeneratorImpl {
+    fn generate(&self) -> Option<Vec<u8>> {
+        Some(
+            SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_nanos()
+                .to_le_bytes()
+                .to_vec(),
+        )
+    }
+}
+
+/// Simple wrapper around ic_agent::agent::AgentBuilder to include a custom
+/// ingress expiry and nonce generator.
+struct Agent;
+impl Agent {
+    fn builder() -> AgentBuilder {
+        <AgentBuilder as Default>::default()
+            .with_ingress_expiry(Duration::from_secs(6 * 60))
+            .with_nonce_generator(NonceGeneratorImpl)
+    }
 }
 
 fn protobuf_to_vec<M: Message>(entry: M) -> Vec<u8> {
--- rs/orchestrator/src/signer.rs
+++ rs/orchestrator/src/signer.rs
@@ -1,19 +1,42 @@
-use ic_canister_client::Sender;
-use ic_canister_client_sender::{Secp256k1KeyPair, SigKeys};
+use ic_agent::{
+    agent::EnvelopeContent, export::Principal, identity::Secp256k1Identity, Identity, Signature,
+};
 use ic_sys::utility_command::{UtilityCommand, UtilityCommandResult};
+use ic_types::messages::MessageId;
 use std::{path::Path, sync::Arc};
 
 /// An abstract message signer interface.
 pub trait Signer: Send + Sync {
     /// Returns the message signer bundle containing the public key and a signing command. This
     /// object is intended to be used with an agent to send messages to IC canisters.
-    fn get(&self) -> UtilityCommandResult<Sender>;
+    fn get(&self) -> UtilityCommandResult<Box<dyn Identity>>;
+}
+
+type SignBytes = Arc<dyn Fn(&[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> + Send + Sync>;
+type SignMessageId =
+    Arc<dyn Fn(&MessageId) -> Result<Vec<u8>, Box<dyn std::error::Error>> + Send + Sync>;
+
+pub struct NodeProviderSigner {
+    identity: Secp256k1Identity,
+}
+
+impl NodeProviderSigner {
+    pub fn new(path: &Path) -> Option<Self> {
+        let identity = Secp256k1Identity::from_pem_file(path).ok()?;
+        Some(Self { identity })
+    }
+}
+
+impl Signer for NodeProviderSigner {
+    fn get(&self) -> UtilityCommandResult<Box<dyn Identity>> {
+        Ok(Box::new(self.identity.clone()))
+    }
 }
 
 pub struct Hsm;
 
 impl Signer for Hsm {
-    fn get(&self) -> UtilityCommandResult<Sender> {
+    fn get(&self) -> UtilityCommandResult<Box<dyn Identity>> {
         UtilityCommand::notify_host("Starting node registration.", 1);
         UtilityCommand::try_to_attach_hsm();
         let pub_key = UtilityCommand::read_public_key(None, None).execute()?;
@@ -27,29 +50,86 @@ impl Signer for Hsm {
             UtilityCommand::try_to_detach_hsm();
             res
         }
-        Ok(Sender::ExternalHsm {
+        Ok(Box::new(HsmIdentity {
             pub_key,
             sign: Arc::new(get_sign_command),
+        }))
+    }
+}
+
+pub struct HsmIdentity {
+    pub_key: Vec<u8>,
+    sign: SignBytes,
+}
+
+impl Identity for HsmIdentity {
+    fn sender(&self) -> Result<Principal, String> {
+        Ok(Principal::self_authenticating(self.pub_key.clone()))
+    }
+
+    fn public_key(&self) -> Option<Vec<u8>> {
+        Some(self.pub_key.clone())
+    }
+
+    fn sign(&self, content: &EnvelopeContent) -> Result<Signature, String> {
+        let msg = content.to_request_id().signable();
+        let signature =
+            Some((self.sign)(&msg).map_err(|err| format!("Cannot create hsm signature: {err}"))?);
+        let public_key = self.public_key();
+        Ok(Signature {
+            public_key,
+            signature,
+            delegations: None,
         })
     }
 }
 
-pub struct NodeProviderSigner {
-    keypair: Secp256k1KeyPair,
+pub struct NodeIdentity {
+    pub_key: Vec<u8>,
+    sign: SignMessageId,
 }
 
-impl NodeProviderSigner {
-    pub fn new(path: &Path) -> Option<Self> {
-        let contents = std::fs::read_to_string(path).ok()?;
-        let keypair = Secp256k1KeyPair::from_pem(&contents).ok()?;
-        Some(Self { keypair })
+impl NodeIdentity {
+    pub fn new(mut pub_key: Vec<u8>, sign: SignMessageId) -> Self {
+        // The constant is the prefix of the DER encoding of the ASN.1
+        // SubjectPublicKeyInfo data structure. It can be read as follows:
+        // 0x30 0x2A: Sequence of length 42 bytes
+        //   0x30 0x05: Sequence of length 5 bytes
+        //     0x06 0x03 0x2B 0x65 0x70: OID of length 3 bytes, 1.3.101.112 (where 43 =
+        //              1 * 40 + 3)
+        //   0x03 0x21: Bit string of length 33 bytes
+        //     0x00 [raw key]: No padding [raw key]
+        let mut der_encoded: Vec<u8> = vec![
+            0x30, 0x2A, 0x30, 0x05, 0x06, 0x03, 0x2B, 0x65, 0x70, 0x03, 0x21, 0x00,
+        ];
+        der_encoded.append(&mut pub_key);
+
+        Self {
+            pub_key: der_encoded,
+            sign,
+        }
     }
 }
 
-impl Signer for NodeProviderSigner {
-    fn get(&self) -> UtilityCommandResult<Sender> {
-        Ok(Sender::SigKeys(SigKeys::EcdsaSecp256k1(
-            self.keypair.clone(),
-        )))
+impl Identity for NodeIdentity {
+    fn sender(&self) -> Result<Principal, String> {
+        Ok(Principal::self_authenticating(self.pub_key.clone()))
+    }
+
+    fn public_key(&self) -> Option<Vec<u8>> {
+        Some(self.pub_key.clone())
+    }
+
+    fn sign(&self, content: &EnvelopeContent) -> Result<Signature, String> {
+        let msg = MessageId::try_from(content.to_request_id().as_ref())
+            .expect("Cannot fail, RequestId is a MessageId");
+        let signature =
+            Some((self.sign)(&msg).map_err(|err| format!("Cannot create node signature: {err}"))?);
+        let public_key = self.public_key();
+        Ok(Signature {
+            public_key,
+            signature,
+            delegations: None,
+        })
     }
 }
