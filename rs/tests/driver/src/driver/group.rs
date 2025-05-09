40c71a16be444b1ae62ab0ff47619fc813b26838
Bas van Dijk <bas@dfinity.org>
2025-05-09 18:48:42 +0000

    Removed some more unncessary `#[rustfmt::skip]` attributes

--- rs/tests/driver/src/driver/group.rs
+++ rs/tests/driver/src/driver/group.rs
@@ -1,6 +1,4 @@
 #![allow(dead_code)]
-#[rustfmt::skip]
-use walkdir::WalkDir;
 use crate::driver::constants;
 use crate::driver::{
     farm::{Farm, HostFeature},
@@ -27,6 +25,7 @@ use crate::driver::{
 };
 use crate::k8s::tnet::TNet;
 use crate::util::block_on;
+use walkdir::WalkDir;
 
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
