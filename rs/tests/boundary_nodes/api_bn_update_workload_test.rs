0e35c5cbac3f75083f84d873062499a7fe6242ad
r-birkner <103420898+r-birkner@users.noreply.github.com>
2025-05-09 16:59:56 +0200

    chore: moving farm tests from BN to API BN (#5025)
    This PR updates all farm tests that used to rely on boundary nodes for
    API calls to use the API boundary nodes.
    
    In addition, it removes a couple of boundary node farm tests that are
    now obsolete:
    * `bn_integration_test` has been turned into the
    `api_bn_integration_test` and the HTTP gateway specific tests are moved
    to `dfinity/ic-gateway`.
    * `bn_reboot_test` is not needed anymore.
    * `custom_domains_integration_test` has been manual for over a year and
    hasn't been used in a long time. It is not needed anymore.
    
    ---------
    
    Co-authored-by: IDX GitHub Automation <infra+github-automation@dfinity.org>

--- rs/tests/boundary_nodes/api_bn_update_workload_test.rs
+++ rs/tests/boundary_nodes/api_bn_update_workload_test.rs
@@ -11,7 +11,7 @@ use ic_system_test_driver::systest;
 const APP_SUBNET_SIZE: usize = 4;
 const RPS: usize = 100;
 const PAYLOAD_SIZE_BYTES: usize = 1000;
-const USE_BOUNDARY_NODE: bool = true;
+const USE_API_BOUNDARY_NODE: bool = true;
 const WORKLOAD_RUNTIME: Duration = Duration::from_secs(5 * 60);
 // Timeout parameters
 const TASK_TIMEOUT_DELTA: Duration = Duration::from_secs(10 * 60);
@@ -20,14 +20,14 @@ const OVERALL_TIMEOUT_DELTA: Duration = Duration::from_secs(5 * 60);
 fn main() -> Result<()> {
     let per_task_timeout: Duration = WORKLOAD_RUNTIME + TASK_TIMEOUT_DELTA; // This should be a bit larger than the workload execution time.
     let overall_timeout: Duration = per_task_timeout + OVERALL_TIMEOUT_DELTA; // This should be a bit larger than the per_task_timeout.
-    let setup = |env| setup(env, APP_SUBNET_SIZE, USE_BOUNDARY_NODE, None);
+    let setup = |env| setup(env, APP_SUBNET_SIZE, None);
     let test = |env| {
         test(
             env,
             RPS,
             PAYLOAD_SIZE_BYTES,
             WORKLOAD_RUNTIME,
-            USE_BOUNDARY_NODE,
+            USE_API_BOUNDARY_NODE,
         )
     };
     SystemTestGroup::new()
