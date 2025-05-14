828f4a313a3cb454528aa1f6d981fcbf971b7a68
Franz-Stefan Preiss <franzstefan@dfinity.org>
2025-05-14 12:21:20 +0000

    Temporarily disable vetKD canister public key equality check in system tests

--- rs/tests/consensus/tecdsa/utils/src/lib.rs
+++ rs/tests/consensus/tecdsa/utils/src/lib.rs
@@ -249,7 +249,12 @@ pub fn run_chain_key_signature_test(
         let public_key = get_public_key_with_retries(key_id, canister, logger, 100)
             .await
             .unwrap();
-        assert_eq!(existing_key, public_key);
+        // TODO(CRP-2789): Re-enable vetKD public key equality check
+        if let MasterPublicKeyId::VetKd(vetkd_key_id) = key_id {
+            // skip canister public key equality check because of https://github.com/dfinity/ic/pull/5088
+        } else {
+            assert_eq!(existing_key, public_key);
+        }
         let signature = get_signature_with_logger(
             message_hash.clone(),
             ECDSA_SIGNATURE_FEE,
