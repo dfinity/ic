1f2c0237b7d2aca2e00dae651bdd72e53fa4da40
Andriy Berestovskyy <andriy.berestovskyy@dfinity.org>
2025-05-15 11:11:13 +0000

    Set PAGES_TO_MAP=4

--- rs/memory_tracker/src/lib.rs
+++ rs/memory_tracker/src/lib.rs
@@ -20,7 +20,7 @@ use std::{
 // checkpoint file per signal handler call. Higher value gives higher
 // throughput in memory intensive workloads, but may regress performance
 // in other workloads because it increases work per signal handler call.
-const PAGES_TO_MAP: u64 = 2;
+const PAGES_TO_MAP: u64 = 4;
 
 // The new signal handler requires `AccessKind` which currently available only
 // on Linux without WSL.
