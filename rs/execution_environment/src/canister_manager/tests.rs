1ee7cae71083be90094d4b0e2bbd3e72f518062a
Dimitris Sarlis <dimitrios.sarlis@dfinity.org>
2025-05-16 09:51:39 +0000

    test: Remove some usages of install_canister_with_allocation

--- rs/execution_environment/src/canister_manager/tests.rs
+++ rs/execution_environment/src/canister_manager/tests.rs
@@ -4383,9 +4383,7 @@ fn cycles_correct_if_install_succeeds() {
 
 #[test]
 fn cycles_correct_if_install_fails_at_validation() {
-    let mut test = ExecutionTestBuilder::new()
-        .with_allocatable_compute_capacity_in_percent(50)
-        .build();
+    let mut test = ExecutionTestBuilder::new().build();
 
     let wat = r#"
         (module
@@ -4406,19 +4404,37 @@ fn cycles_correct_if_install_fails_at_validation() {
     let initial_cycles = Cycles::new(1_000_000_000_000_000);
     let id = test.create_canister(initial_cycles);
 
-    test.install_canister_with_allocation(id, wasm, Some(100), None)
-        .unwrap_err();
+    test.install_canister(id, wasm.clone()).unwrap();
     assert_eq!(
         test.canister_state(id).system_state.balance(),
         initial_cycles - test.canister_execution_cost(id),
     );
-    assert_eq!(
+    let execution_cost_of_successful_installation = test.cycles_account_manager().execution_cost(
+        wasm_compilation_cost(&wasm) + NumInstructions::from(5 * *DROP_MEMORY_GROW_CONST_COST),
+        test.subnet_size(),
+        test.canister_wasm_execution_mode(id),
+    );
+    assert_delta!(
         test.canister_execution_cost(id),
-        test.cycles_account_manager().execution_cost(
-            NumInstructions::from(0),
-            test.subnet_size(),
-            test.canister_wasm_execution_mode(id),
-        )
+        execution_cost_of_successful_installation,
+        Cycles::new(10)
+    );
+
+    // Attempt to install the same canister again, should fail because it's non-empty.
+    test.install_canister(id, wasm).unwrap_err();
+    assert_eq!(
+        test.canister_state(id).system_state.balance(),
+        initial_cycles - test.canister_execution_cost(id),
+    );
+    let execution_cost_of_failed_installation = test.cycles_account_manager().execution_cost(
+        NumInstructions::from(0),
+        test.subnet_size(),
+        test.canister_wasm_execution_mode(id),
+    );
+    assert_delta!(
+        test.canister_execution_cost(id) - execution_cost_of_successful_installation,
+        execution_cost_of_failed_installation,
+        Cycles::new(10)
     );
 }
 
