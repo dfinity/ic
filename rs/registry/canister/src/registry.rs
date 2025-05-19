68c96a18c987de845725277a881a8c3251ba92e3
Daniel Wong <daniel.wong@dfinity.org>
2025-05-19 11:09:22 +0200

    Added a bigger test, to wit, test_apply_mutations_near_max_chunkable_len_when_chunking_is_enabled.

--- rs/registry/canister/src/registry.rs
+++ rs/registry/canister/src/registry.rs
@@ -1237,6 +1237,21 @@ mod tests {
         apply_mutations_skip_invariant_checks(&mut registry, mutations);
     }
 
+    // This is like the previous test, except that the mutation is approx 7.6x
+    // larger (closer to the 10 MiB limit, as opposed to 1.3 MiB).
+    #[test]
+    fn test_apply_mutations_delta_near_max_chunkable_len_when_chunking_is_enabled() {
+        let _restore_on_drop = temporarily_enable_chunkifying_large_values();
+
+        let mut registry = Registry::new();
+
+        let key = b"key";
+        let too_large_value = vec![0; MAX_CHUNKABLE_ATOMIC_MUTATION_LEN - 100];
+        let mutations = vec![upsert(key, too_large_value)];
+
+        apply_mutations_skip_invariant_checks(&mut registry, mutations);
+    }
+
     #[test]
     #[should_panic(expected = "Mutation too large. First key =")]
     fn test_apply_mutations_too_large_even_when_chunking_is_enabled() {
