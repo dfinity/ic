1bd398bb639cd3cb69514cb71977134c95339174
Michael Weigelt <michael.weigelt@dfinity.org>
2025-05-20 12:55:24 +0000

    dts test

--- rs/execution_environment/tests/dts.rs
+++ rs/execution_environment/tests/dts.rs
@@ -11,11 +11,13 @@ use ic_config::{
 };
 use ic_management_canister_types_private::{
     CanisterIdRecord, CanisterInfoRequest, CanisterInstallMode, CanisterInstallModeV2,
-    CanisterSettingsArgsBuilder, CanisterSnapshotDataKind, ClearChunkStoreArgs,
-    DeleteCanisterSnapshotArgs, EmptyBlob, InstallChunkedCodeArgs, InstallCodeArgs,
-    ListCanisterSnapshotArgs, LoadCanisterSnapshotArgs, Method, Payload,
-    ReadCanisterSnapshotDataArgs, ReadCanisterSnapshotMetadataArgs, StoredChunksArgs,
-    TakeCanisterSnapshotArgs, UninstallCodeArgs, UpdateSettingsArgs, UploadChunkArgs, IC_00,
+    CanisterSettingsArgsBuilder, CanisterSnapshotDataKind, CanisterSnapshotDataOffset,
+    ClearChunkStoreArgs, DeleteCanisterSnapshotArgs, EmptyBlob, GlobalTimer,
+    InstallChunkedCodeArgs, InstallCodeArgs, ListCanisterSnapshotArgs, LoadCanisterSnapshotArgs,
+    Method, OnLowWasmMemoryHookStatus, Payload, ReadCanisterSnapshotDataArgs,
+    ReadCanisterSnapshotMetadataArgs, StoredChunksArgs, TakeCanisterSnapshotArgs,
+    UninstallCodeArgs, UpdateSettingsArgs, UploadCanisterSnapshotDataArgs,
+    UploadCanisterSnapshotMetadataArgs, UploadChunkArgs, IC_00,
 };
 use ic_registry_subnet_type::SubnetType;
 use ic_replicated_state::canister_state::{execution_state::NextScheduledMethod, NextExecution};
@@ -1122,8 +1124,6 @@ fn dts_aborted_execution_does_not_block_subnet_messages() {
             | Method::NodeMetricsHistory
             | Method::SubnetInfo
             | Method::ProvisionalCreateCanisterWithCycles
-            | Method::UploadCanisterSnapshotMetadata
-            | Method::UploadCanisterSnapshotData
             | Method::ProvisionalTopUpCanister => {}
             // Unsupported methods accepting just one argument.
             // Deleting an aborted canister requires to stop it first.
@@ -1238,6 +1238,31 @@ fn dts_aborted_execution_does_not_block_subnet_messages() {
                     aborted_canister_id,
                     (aborted_canister_id, 0).into(),
                     CanisterSnapshotDataKind::WasmModule { size: 0, offset: 0 },
+                )
+                .encode();
+                (method, call_args().other_side(args))
+            }),
+            Method::UploadCanisterSnapshotMetadata => test_supported(|aborted_canister_id| {
+                let args = UploadCanisterSnapshotMetadataArgs::new(
+                    aborted_canister_id,
+                    None,
+                    0,
+                    vec![],
+                    0,
+                    0,
+                    vec![],
+                    Some(GlobalTimer::Inactive),
+                    Some(OnLowWasmMemoryHookStatus::ConditionNotSatisfied),
+                )
+                .encode();
+                (method, call_args().other_side(args))
+            }),
+            Method::UploadCanisterSnapshotData => test_supported(|aborted_canister_id| {
+                let args = UploadCanisterSnapshotDataArgs::new(
+                    aborted_canister_id,
+                    (aborted_canister_id, 0).into(),
+                    CanisterSnapshotDataOffset::WasmChunk,
+                    vec![],
                 )
                 .encode();
                 (method, call_args().other_side(args))
