383b37858aa5410609ccd345264c650231407dc3
Mathieu Ducroux <mathieu.ducroux@dfinity.org>
2025-05-21 14:29:00 +0200

    test(blocklist): re-add blocked address checks for case sensitivity

--- rs/ethereum/cketh/minter/src/blocklist/tests.rs
+++ rs/ethereum/cketh/minter/src/blocklist/tests.rs
@@ -12,11 +12,16 @@ fn check_blocklist_is_sorted() {
 
 #[test]
 fn should_find_blocked_address() {
-    let blocked_address = SAMPLE_BLOCKED_ADDRESS;
+    let blocked_address = Address::from_str(&format!("{:x}", SAMPLE_BLOCKED_ADDRESS)).unwrap(); // Lowercase
+    assert!(is_blocked(&blocked_address));
+    let blocked_address = Address::from_str(&format!("{:X}", SAMPLE_BLOCKED_ADDRESS)).unwrap(); // Uppercase
     assert!(is_blocked(&blocked_address));
 
     let not_blocked_address =
         Address::from_str("0x4838B106FCe9647Bdf1E7877BF73cE8B0BAD5f97").unwrap();
+    assert!(!is_blocked(&not_blocked_address));
+    let not_blocked_address =
+        Address::from_str("0x4838B106FCe9647Bdf1E7877bF73cE8B0BAD5f97").unwrap(); // Same address as above, different case at the 25th char
     assert!(!is_blocked(&not_blocked_address));
 }
 
