a3ea999a304da56d72706e85acbbef79282e179f
Leo Eichhorn <leo.eichhorn@dfinity.org>
2025-05-23 16:06:28 +0000

    .

--- rs/tests/consensus/subnet_recovery/common.rs
+++ rs/tests/consensus/subnet_recovery/common.rs
@@ -262,7 +262,7 @@ fn app_subnet_recovery_test(env: TestEnv, cfg: Config) {
     info!(logger, "IC_VERSION_ID: {master_version:?}");
     let topology_snapshot = env.topology_snapshot();
 
-    // choose a node from the nns subnet
+    // Choose a node from the nns subnet
     let nns_node = get_nns_node(&topology_snapshot);
     info!(
         logger,
