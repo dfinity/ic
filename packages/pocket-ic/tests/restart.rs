d27bd9e3293c5290e749c94d1c8cc80ae591c749
Martin Raszyk <martin.raszyk@dfinity.org>
2025-05-27 15:52:56 +0200

    debug

--- packages/pocket-ic/tests/restart.rs
+++ packages/pocket-ic/tests/restart.rs
@@ -1,18 +1,9 @@
 use pocket_ic::PocketIcBuilder;
-use reqwest::blocking::Client;
-use std::{thread, time::Duration};
+use std::time::Duration;
 
-// This test must be in a separate test suite so that the PocketIC server is not reused
-// by a different test.
 #[test]
-fn create_instance_twice() {
+fn hyper_issue() {
     let mut pic = PocketIcBuilder::new().with_nns_subnet().build();
     std::thread::sleep(Duration::from_secs(5));
-    let url = pic.make_live(None);
-    let status_endpoint = url.join("api/v2/status").unwrap();
-
-    // Check that the server is running.
-    let client = Client::new();
-    let response = client.get(status_endpoint.clone()).send().unwrap();
-    assert_eq!(response.status(), 200);
+    let _url = pic.make_live(None);
 }
