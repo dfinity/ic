4be3d05c6c3b4024ae95777887677eef91899344
Max Summe <maximilian.summe@dfinity.org>
2025-06-03 12:31:29 -0700

    Remove subnet_id from the canister range keys

--- rs/registry/keys/src/lib.rs
+++ rs/registry/keys/src/lib.rs
@@ -371,7 +371,7 @@ pub fn make_nns_canister_records_key() -> String {
 }
 
 /// Returns a key for the CanisterRange registry entry
-pub fn make_canister_range_key(range_start: CanisterId, subnet_id: SubnetId) -> String {
+pub fn make_canister_range_key(range_start: CanisterId) -> String {
     if CanisterId::try_from_principal_id(range_start.get()).is_err() {
         // try_from_principal_id ensures the CanisterId is plausibly represeting a u64
         // which is currently an implied requirement for our routing table.
@@ -383,10 +383,7 @@ pub fn make_canister_range_key(range_start: CanisterId, subnet_id: SubnetId) -> 
     // If at some point we stop having the same length CanisterIds, we will need to prepend a length
     // byte into this encoding to have the same properties apply, and that will also require a data migration.
     let encoded_range_start = hex::encode(range_start.get().to_vec());
-    format!(
-        "{}{}_{}",
-        CANISTER_RANGE_PREFIX, encoded_range_start, subnet_id
-    )
+    format!("{}{}", CANISTER_RANGE_PREFIX, encoded_range_start)
 }
 
 #[cfg(test)]
