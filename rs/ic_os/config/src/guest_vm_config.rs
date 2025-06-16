eefb7e372a1c73c1a9d83d975e2cd5d5f192d5a1
David Frank <david.frank@dfinity.org>
2025-06-16 13:25:31 +0200

    Testing some new changes

--- rs/ic_os/config/src/guest_vm_config.rs
+++ rs/ic_os/config/src/guest_vm_config.rs
@@ -153,7 +153,7 @@ fn make_bootstrap_options(
 
     TEST
 
-    ANOTHER LINE
+    ANOTHER LINE FIX
 
     bootstrap_options.ipv6_address = Some(guestos_ipv6_config.address.clone());
     bootstrap_options.ipv6_gateway = Some(guestos_ipv6_config.gateway.to_string());
