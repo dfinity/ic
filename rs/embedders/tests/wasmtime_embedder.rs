a64b8a2c16a7d77aa3835851a0d4e2bbe5f0bd1a
Alexandra Zapuc <alexandra.zapuc@dfinity.org>
2025-06-18 08:51:48 +0000

    fix linux clippy

--- rs/embedders/tests/wasmtime_embedder.rs
+++ rs/embedders/tests/wasmtime_embedder.rs
@@ -31,7 +31,6 @@ use ic_types::{
 const WASM_PAGE_SIZE: u32 = wasmtime_environ::Memory::DEFAULT_PAGE_SIZE;
 
 #[cfg(target_os = "linux")]
-
 /// Ensures that attempts to execute messages on wasm modules that do not
 /// define memory fails.
 #[test]
