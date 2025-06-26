e89267d2b0c58d039602607441c49a555b705a12
Andrew Battat <113942931+andrewbattat@users.noreply.github.com>
2025-06-26 13:37:26 +0100

    refactor(node): Organize config tool into hostos and setupos directories (#5702)
    Organize config tool into hostos and setupos directories making the
    config pipeline easier to understand

--- rs/ic_os/os_tools/guest_vm_runner/src/guest_vm_config.rs
+++ rs/ic_os/os_tools/guest_vm_runner/src/guest_vm_config.rs
@@ -1,7 +1,7 @@
 use anyhow::{Context, Result};
 use askama::Template;
-use config::guestos_bootstrap_image::BootstrapOptions;
-use config::guestos_config::generate_guestos_config;
+use config::hostos::guestos_bootstrap_image::BootstrapOptions;
+use config::hostos::guestos_config::generate_guestos_config;
 use config_types::{GuestOSConfig, HostOSConfig};
 use deterministic_ips::node_type::NodeType;
 use deterministic_ips::{calculate_deterministic_mac, IpVariant};
