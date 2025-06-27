620fdcc7edb51578a950350a94b185f75e7b057f
Pietro <pietro.di.marco@dfinity.org>
2025-06-27 11:04:24 +0200

    feat: Add timestamp to registry versions mapping in stable memory

--- rs/node_rewards/canister/src/storage.rs
+++ rs/node_rewards/canister/src/storage.rs
@@ -1,11 +1,13 @@
 use ic_registry_canister_client::{
-    RegistryDataStableMemory, StorableRegistryKey, StorableRegistryValue,
+    RegistryDataStableMemory, RegistryVersionsValue, StorableRegistryKey, StorableRegistryValue,
+    TimestampKey,
 };
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
 use std::cell::RefCell;
 
 const REGISTRY_STORE_MEMORY_ID: MemoryId = MemoryId::new(0);
+const TIMESTAMP_TO_REGISTRY_VERSIONS_MAP_MEMORY_ID: MemoryId = MemoryId::new(1);
 
 type VM = VirtualMemory<DefaultMemoryImpl>;
 
@@ -16,6 +18,11 @@ thread_local! {
     static REGISTRY_DATA_STORE_BTREE_MAP: RefCell<StableBTreeMap<StorableRegistryKey, StorableRegistryValue, VM>>
         = RefCell::new(MEMORY_MANAGER.with_borrow(|mm|
             StableBTreeMap::init(mm.get(REGISTRY_STORE_MEMORY_ID))
+        ));
+
+    static TIMESTAMP_TO_REGISTRY_VERSIONS_MAP: RefCell<StableBTreeMap<TimestampKey, RegistryVersionsValue, VM>>
+        = RefCell::new(MEMORY_MANAGER.with_borrow(|mm|
+            StableBTreeMap::init(mm.get(TIMESTAMP_TO_REGISTRY_VERSIONS_MAP_MEMORY_ID))
         ));
 }
 
@@ -31,5 +38,15 @@ impl RegistryDataStableMemory for RegistryStoreStableMemoryBorrower {
         f: impl FnOnce(&mut StableBTreeMap<StorableRegistryKey, StorableRegistryValue, VM>) -> R,
     ) -> R {
         REGISTRY_DATA_STORE_BTREE_MAP.with_borrow_mut(f)
+    }
+    fn with_timestamp_to_registry_versions_map<R>(
+        f: impl FnOnce(&StableBTreeMap<TimestampKey, RegistryVersionsValue, VM>) -> R,
+    ) -> R {
+        TIMESTAMP_TO_REGISTRY_VERSIONS_MAP.with_borrow_mut(f)
+    }
+    fn with_timestamp_to_registry_versions_map_mut<R>(
+        f: impl FnOnce(&StableBTreeMap<TimestampKey, RegistryVersionsValue, VM>) -> R,
+    ) -> R {
+        TIMESTAMP_TO_REGISTRY_VERSIONS_MAP.with_borrow_mut(f)
     }
 }
--- rs/registry/canister-client/src/lib.rs
+++ rs/registry/canister-client/src/lib.rs
@@ -11,7 +11,10 @@ mod stable_canister_client;
 mod stable_memory;
 
 pub use stable_canister_client::StableCanisterRegistryClient;
-pub use stable_memory::{RegistryDataStableMemory, StorableRegistryKey, StorableRegistryValue};
+pub use stable_memory::{
+    RegistryDataStableMemory, RegistryVersionsValue, StorableRegistryKey, StorableRegistryValue,
+    TimestampKey,
+};
 
 /// The CanisterRegistryClient provides methods to maintain and read a local cache of Registry data
 /// This is similar to the RegistryClient interface use in the protocol, but without the
