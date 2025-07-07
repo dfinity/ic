b1a5f24badc712c53f2c881a52a45a34610f50c8
Pierugo Pace <pierugo.pace@dfinity.org>
2025-07-07 12:23:44 +0000

    fix: propagate signature request reject

--- rs/tests/test_canisters/signer/src/main.rs
+++ rs/tests/test_canisters/signer/src/main.rs
@@ -1,7 +1,7 @@
 use candid::Principal;
 use ic_cdk::{
     api::{
-        call::{call_with_payment128, CallResult},
+        call::{call_with_payment128, CallResult, ManualReply},
         management_canister::{
             ecdsa::{sign_with_ecdsa, SignWithEcdsaArgument, SignWithEcdsaResponse},
             schnorr::{sign_with_schnorr, SignWithSchnorrArgument, SignWithSchnorrResponse},
@@ -14,29 +14,33 @@ use ic_signer::{GenEcdsaParams, GenSchnorrParams, GenVetkdParams};
 
 /// Generates a dummy ECDSA signature of given size parameters.
 /// The call does not verify the signature, it only generates it.
-#[update]
+#[update(manual_reply = true)]
 pub async fn gen_ecdsa_sig(
     GenEcdsaParams {
         derivation_path_length,
         derivation_path_element_size,
         key_id,
     }: GenEcdsaParams,
-) -> Result<SignWithEcdsaResponse, String> {
+) -> ManualReply<SignWithEcdsaResponse> {
     let signature_request = SignWithEcdsaArgument {
         message_hash: vec![1; 32],
         derivation_path: vec![vec![2; derivation_path_element_size]; derivation_path_length],
         key_id,
     };
 
-    sign_with_ecdsa(signature_request)
+    match sign_with_ecdsa(signature_request)
         .await
         .map(|(res,)| res)
         .map_err(|err| err.1)
+    {
+        Ok(sig) => ManualReply::one(sig),
+        Err(err) => ManualReply::reject(err),
+    }
 }
 
 /// Generates a dummy Schnorr signature of given size parameters.
 /// The call does not verify the signature, it only generates it.
-#[update]
+#[update(manual_reply = true)]
 pub async fn gen_schnorr_sig(
     GenSchnorrParams {
         message_size,
@@ -44,29 +48,33 @@ pub async fn gen_schnorr_sig(
         derivation_path_element_size,
         key_id,
     }: GenSchnorrParams,
-) -> Result<SignWithSchnorrResponse, String> {
+) -> ManualReply<SignWithSchnorrResponse> {
     let signature_request = SignWithSchnorrArgument {
         message: vec![1; message_size],
         derivation_path: vec![vec![2; derivation_path_element_size]; derivation_path_length],
         key_id,
     };
 
-    sign_with_schnorr(signature_request)
+    match sign_with_schnorr(signature_request)
         .await
         .map(|(res,)| res)
         .map_err(|err| err.1)
+    {
+        Ok(sig) => ManualReply::one(sig),
+        Err(err) => ManualReply::reject(err),
+    }
 }
 
 /// Generates a dummy VetKD key of given size parameters.
 /// The call does not verify the encrypted key, it only generates it.
-#[update]
+#[update(manual_reply = true)]
 pub async fn gen_vetkd_key(
     GenVetkdParams {
         context_size,
         input_size,
         key_id,
     }: GenVetkdParams,
-) -> Result<VetKDDeriveKeyResult, String> {
+) -> ManualReply<VetKDDeriveKeyResult> {
     let key_request = VetKDDeriveKeyArgs {
         context: vec![1; context_size],
         input: vec![2; input_size],
@@ -74,7 +82,10 @@ pub async fn gen_vetkd_key(
         transport_public_key: ic_bls12_381::G1Affine::generator().to_compressed().to_vec(),
     };
 
-    vetkd_derive_key(&key_request).await.map_err(|err| err.1)
+    match vetkd_derive_key(&key_request).await.map_err(|err| err.1) {
+        Ok(key) => ManualReply::one(key),
+        Err(err) => ManualReply::reject(err),
+    }
 }
 
 pub async fn vetkd_derive_key(arg: &VetKDDeriveKeyArgs) -> CallResult<VetKDDeriveKeyResult> {
