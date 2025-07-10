ca0ed9787001878ee9ed077f4be1ca4169ee2038
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2025-07-10 09:10:38 +0200

    feat(sns): SNS treasury manager API (#5788)
    This PR adds the Rust API types for the **SNS Treasury Manager**
    extensions.
    
    # Why
    
    For the [upcoming SNS Liquidity Pools
    feature](https://forum.dfinity.org/t/sns-liquidity-pools-design/50439),
    the design involves implementing SNS extensions for managing assets on
    behalf of the DAO; concretely, by allocating some of the treasury tokens
    into DEX liquidity pools.
    
    The first such extension is planned to be implemented as a separate
    canister, as it needs to have DEX-specific code that should not be part
    of the ICP mono repo. This is why the SNS and the SNS extension
    implementation need an API to agree on. This is that API, although we
    make it more general and not tailor specifically to managing DEX assets,
    so that it suites potential future use cases.
    
    # What
    
    The main addition is the Candid file which should be seen as the source
    of truth for the API.
    
    In addition, there are Rust types to aid implementors and a mock
    canister implementation that is used to test that those Rust types
    exactly match the Candid service definitions.
    
    ---------
    
    Co-authored-by: ShahriarJavidi <shahriar.javidi@dfinity.org>
    Co-authored-by: ShahriarJavidi <33692111+ShahriarJavidi@users.noreply.github.com>

---
+++ rs/sns/treasury_manager/src/lib.rs
@@ -0,0 +1,309 @@
+use candid::{CandidType, Nat, Principal};
+use serde::{Deserialize, Serialize, Serializer};
+use std::{collections::BTreeMap, fmt::Display};
+
+#[derive(CandidType, Clone, Debug, Deserialize)]
+pub struct TreasuryManagerInit {
+    pub allowances: Vec<Allowance>,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize)]
+pub struct TreasuryManagerUpgrade {}
+
+#[derive(CandidType, Clone, Debug, Deserialize)]
+pub enum TreasuryManagerArg {
+    Init(TreasuryManagerInit),
+    Upgrade(TreasuryManagerUpgrade),
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
+pub struct Balance {
+    #[serde(serialize_with = "serialize_nat_as_u64")]
+    pub amount_decimals: Nat,
+    pub account: Option<Account>,
+    pub name: Option<String>,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
+pub struct BalanceBook {
+    pub treasury_owner: Option<Balance>,
+    pub treasury_manager: Option<Balance>,
+    pub external_custodian: Option<Balance>,
+    pub fee_collector: Option<Balance>,
+    pub payees: Option<Balance>,
+    pub payers: Option<Balance>,
+}
+
+#[derive(CandidType, Clone, Debug, Default, Deserialize, PartialEq)]
+pub struct Balances {
+    pub timestamp_ns: u64,
+    pub asset_to_balances: Option<BTreeMap<Asset, BalanceBook>>,
+}
+
+pub type TreasuryManagerResult = Result<Balances, Vec<Error>>;
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Error {
+    pub code: u64,
+    pub message: String,
+    pub kind: ErrorKind,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub enum ErrorKind {
+    /// Prevents the call from being attempted.
+    Precondition {},
+
+    /// Prevents the response from being interpreted.
+    Postcondition {},
+
+    /// An error that occurred while calling a canister.
+    Call {
+        method: String,
+        canister_id: Principal,
+    },
+
+    /// Backend refers to, e.g., the DEX canister that this asset manager talks to.
+    Backend {},
+
+    /// The service is currently not available; please call back later.
+    TemporarilyUnavailable {},
+
+    /// An exotic error that cannot be categorized using the tags above.
+    Generic { generic_error_name: String },
+}
+
+pub trait TreasuryManager {
+    /// Implements the `deposit` API function.
+    fn deposit(
+        &mut self,
+        request: DepositRequest,
+    ) -> impl std::future::Future<Output = TreasuryManagerResult> + Send;
+
+    /// Implements the `withdraw` API function.
+    fn withdraw(
+        &mut self,
+        request: WithdrawRequest,
+    ) -> impl std::future::Future<Output = TreasuryManagerResult> + Send;
+
+    /// Implements the `audit_trail` API query function.
+    fn audit_trail(&self, request: AuditTrailRequest) -> AuditTrail;
+
+    /// Implements the `balances` API query function.
+    fn balances(&self, request: BalancesRequest) -> TreasuryManagerResult;
+
+    // While the following methods go beyond just the Treasury Manager API agreement, they guide
+    // the implementers to organize the code in a reasonable and predictable way.
+
+    /// Context: the source of truth for balances are some remote canisters (e.g., the ledgers).
+    /// The Treasury Manager needs to have a local cache of these balances to be able to make
+    /// important decisions, e.g., how much can be refunded / withdrawn. That cache should be
+    /// regularly updated, and this is the function that should do that.
+    ///
+    /// Should not be exposed as an API function, but rather called periodically by the canister.
+    fn refresh_balances(&mut self) -> impl std::future::Future<Output = ()> + Send;
+
+    /// Should not be exposed as an API function, but rather called periodically by the canister.
+    fn issue_rewards(&mut self) -> impl std::future::Future<Output = ()> + Send;
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
+pub struct DepositRequest {
+    pub allowances: Vec<Allowance>,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
+pub struct BalancesRequest {}
+
+pub type Subaccount = [u8; 32];
+
+#[derive(CandidType, Clone, Copy, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
+pub struct Account {
+    pub owner: Principal,
+    pub subaccount: Option<Subaccount>,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
+pub struct WithdrawRequest {
+    /// If not set, accounts specified at the time of deposit will be used for the withdrawal.
+    pub withdraw_accounts: Option<BTreeMap<Principal, Account>>,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
+pub struct AuditTrailRequest {}
+
+#[derive(CandidType, Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Step {
+    index: usize,
+    is_final: bool,
+}
+
+impl Display for Step {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.is_final {
+            write!(f, "{}-fin", self.index)
+        } else {
+            write!(f, "{}", self.index)
+        }
+    }
+}
+
+#[derive(CandidType, Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
+pub enum Operation {
+    Deposit,
+    Balances,
+    IssueReward,
+    Withdraw,
+}
+
+impl Operation {
+    pub fn name(&self) -> &'static str {
+        match self {
+            Self::Deposit => "Deposit",
+            Self::Balances => "Balances",
+            Self::IssueReward => "IssueReward",
+            Self::Withdraw => "Withdraw",
+        }
+    }
+}
+
+#[derive(CandidType, Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
+pub struct TreasuryManagerOperation {
+    operation: Operation,
+    step: Step,
+}
+
+impl TreasuryManagerOperation {
+    pub fn new(operation: Operation) -> Self {
+        Self {
+            operation,
+            step: Step {
+                index: 0,
+                is_final: false,
+            },
+        }
+    }
+
+    pub fn new_final(operation: Operation) -> Self {
+        Self {
+            operation,
+            step: Step {
+                index: 0,
+                is_final: false,
+            },
+        }
+    }
+
+    pub fn next(&self) -> Self {
+        let index = self.step.index.saturating_add(1);
+        Self {
+            operation: self.operation,
+            step: Step {
+                index,
+                is_final: false,
+            },
+        }
+    }
+
+    pub fn next_final(&self) -> Self {
+        let index = self.step.index.saturating_add(1);
+        Self {
+            operation: self.operation,
+            step: Step {
+                index,
+                is_final: true,
+            },
+        }
+    }
+}
+
+/// To be used for ledger transaction memos.
+impl From<TreasuryManagerOperation> for Vec<u8> {
+    fn from(operation: TreasuryManagerOperation) -> Self {
+        const PREFIX: &str = "TreasuryManager";
+
+        format!(
+            "{}.{}-{}",
+            PREFIX,
+            operation.operation.name(),
+            operation.step,
+        )
+        .as_bytes()
+        .to_vec()
+    }
+}
+
+/// Most operations that a Treasury Manager performs are (direct or indirect) ledger transactions.
+/// However, for generality, any call from the Treasury Manager can be recorded in the audit trail,
+/// even if it is not related to any literal ledger transaction, e.g., adding a token to a DEX
+/// for the first time, or checking the latest ledger metadata.
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Transaction {
+    pub timestamp_ns: u64,
+    pub canister_id: Principal,
+
+    pub result: Result<TransactionWitness, Error>,
+    pub purpose: String,
+
+    pub treasury_manager_operation: TreasuryManagerOperation,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq)]
+pub struct AuditTrail {
+    pub transactions: Vec<Transaction>,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum Asset {
+    Token {
+        symbol: String,
+        ledger_canister_id: Principal,
+        #[serde(serialize_with = "serialize_nat_as_u64")]
+        ledger_fee_decimals: Nat,
+    },
+}
+
+#[derive(CandidType, Clone, Debug, PartialEq, Eq, Hash, Deserialize)]
+pub struct Allowance {
+    pub asset: Asset,
+
+    /// Total amount that may be consumed, including the fees.
+    #[serde(serialize_with = "serialize_nat_as_u64")]
+    pub amount_decimals: Nat,
+
+    /// The owner account is used to return the leftover assets and issue rewards.
+    pub owner_account: Account,
+}
+
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub struct Transfer {
+    pub ledger_canister_id: String,
+    #[serde(serialize_with = "serialize_nat_as_u64")]
+    pub amount_decimals: Nat,
+    #[serde(serialize_with = "serialize_nat_as_u64")]
+    pub block_index: Nat,
+
+    pub sender: Option<Account>,
+    pub receiver: Option<Account>,
+}
+
+/// Most of the time, this just points to the Ledger block index. But for generality, once can
+/// also use this structure for representing witnesses of non-ledger transactions, e.g., from adding
+/// a token to a DEX for the first time.
+#[derive(CandidType, Clone, Debug, Deserialize, PartialEq, Serialize)]
+pub enum TransactionWitness {
+    Ledger(Vec<Transfer>),
+
+    /// Represents a transaction that is not related to the ledger, e.g., DEX operations.
+    /// The argument is a (best-effort) JSON encoding of the response (for human inspection).
+    NonLedger(String),
+}
+
+fn serialize_nat_as_u64<S>(nat: &Nat, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    // Convert Nat to u64 for JSON serialization
+    let value: String = nat.to_string();
+    serializer.serialize_str(&value)
+}
