c5953377239d2775a91925013ca6cba5e4c9ac13
Mihail Jianu <mihail.jianu@dfinity.org>
2025-07-10 13:15:04 +0000

    ff

--- rs/tests/networking/canister_http_soak_test.rs
+++ rs/tests/networking/canister_http_soak_test.rs
@@ -55,7 +55,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-pub fn test(env: TestEnv) {
+pub fn test(env: TestEnv)  {
     let logger = env.logger();
 
     let app_subnets = get_all_application_subnets(&env);
