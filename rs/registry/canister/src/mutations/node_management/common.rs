85b2dafcbde39b11880dbe86f784d5f03d512d5a
max-dfinity <100170574+max-dfinity@users.noreply.github.com>
2025-07-09 17:22:55 -0700

    refactor(registry): Use shards for get_subnet_for_canister (#5867)
    This change allows get_subnet_for_canister to operate more efficiently,
    only loading the portion of the routing table needed to return a
    response, instead of all of the shards

--- rs/registry/canister/src/mutations/node_management/common.rs
+++ rs/registry/canister/src/mutations/node_management/common.rs
@@ -258,6 +258,9 @@ pub fn node_exists_with_ipv4(registry: &Registry, ipv4_addr: &str) -> bool {
 /// Similar to `get_key_family` on the `RegistryClient`, return a list of
 /// tuples, (ID, value).  This strips the prefix from the key and returns the
 /// value as a decoded struct.
+///
+/// This function must return keys in order of their bytes, which should
+/// also be the same order as the string representations.
 pub(crate) fn get_key_family<T: prost::Message + Default>(
     registry: &Registry,
     prefix: &str,
@@ -265,6 +268,8 @@ pub(crate) fn get_key_family<T: prost::Message + Default>(
     get_key_family_iter(registry, prefix).collect()
 }
 
+/// This function must return keys in order of their bytes, which should
+/// also be the same order as the string representations.
 pub(crate) fn get_key_family_iter<'a, T: prost::Message + Default>(
     registry: &'a Registry,
     prefix: &'a str,
@@ -272,27 +277,45 @@ pub(crate) fn get_key_family_iter<'a, T: prost::Message + Default>(
     get_key_family_iter_at_version(registry, prefix, registry.latest_version())
 }
 
+/// This function must return keys in order of their bytes, which should
+/// also be the same order as the string representations.
 pub(crate) fn get_key_family_iter_at_version<'a, T: prost::Message + Default>(
     registry: &'a Registry,
     prefix: &'a str,
     version: u64,
 ) -> impl Iterator<Item = (String, T)> + 'a {
+    get_key_family_raw_iter_at_version(registry, prefix, version).filter_map(|(id, value)| {
+        let latest_value: Option<T> =
+            with_chunks(|chunks| decode_high_capacity_registry_value::<T, _>(value, chunks));
+
+        let latest_value = latest_value?;
+
+        Some((id, latest_value))
+    })
+}
+
+/// This function must return keys in order of their bytes, which should
+/// also be the same order as the string representations.
+pub(crate) fn get_key_family_raw_iter_at_version<'a>(
+    registry: &'a Registry,
+    prefix: &'a str,
+    version: u64,
+) -> impl Iterator<Item = (String, &'a HighCapacityRegistryValue)> + 'a {
     let prefix_bytes = prefix.as_bytes();
     let start = prefix_bytes.to_vec();
 
+    // Note, using the 'store' which is a BTreeMap is what guarantees the order of keys.
     registry
         .store
         .range(start..)
         .take_while(|(k, _)| k.starts_with(prefix_bytes))
         .filter_map(move |(key, values)| {
-            let value: &HighCapacityRegistryValue =
+            let latest_value: &HighCapacityRegistryValue =
                 values.iter().rev().find(|value| value.version <= version)?;
 
-            let latest_value: Option<T> =
-                with_chunks(|chunks| decode_high_capacity_registry_value::<T, _>(value, chunks));
-
-            // Skip deleted values.
-            let latest_value = latest_value?;
+            if !latest_value.is_present() {
+                return None; // Deleted or otherwise empty value.
+            }
 
             let id = key
                 .strip_prefix(prefix_bytes)
