5c089a4efa2bd7d45d4697e12e849b9b869ae5b9
Leo Eichhorn <leo.eichhorn@dfinity.org>
2025-07-11 09:08:56 +0000

    clippy

--- rs/registry/subnet_features/src/lib.rs
+++ rs/registry/subnet_features/src/lib.rs
@@ -1,5 +1,5 @@
 use candid::CandidType;
-use ic_management_canister_types_private::{EcdsaKeyId, MasterPublicKeyId};
+use ic_management_canister_types_private::MasterPublicKeyId;
 use ic_protobuf::types::v1 as pb_types;
 use ic_protobuf::{
     proxy::{try_from_option_field, ProxyDecodeError},
@@ -185,7 +185,7 @@ impl TryFrom<pb::ChainKeyConfig> for ChainKeyConfig {
 
 #[cfg(test)]
 mod tests {
-    use ic_management_canister_types_private::{EcdsaCurve, VetKdCurve, VetKdKeyId};
+    use ic_management_canister_types_private::{EcdsaCurve, EcdsaKeyId, VetKdCurve, VetKdKeyId};
 
     use super::*;
     use std::str::FromStr;
