af258f1aa450b41b55a246a3339fb651a4dddb60
Roel Storms <roel.storms@dfinity.org>
2025-07-15 17:38:52 +0200

    Added conversion to consensus::Block

--- rs/protobuf/fuzz/fuzz_targets/decode_block.rs
+++ rs/protobuf/fuzz/fuzz_targets/decode_block.rs
@@ -3,8 +3,13 @@ use ic_protobuf::proxy::ProtoProxy;
 use ic_protobuf::proxy::ProxyDecodeError;
 use ic_protobuf::types::v1 as pb;
 use libfuzzer_sys::fuzz_target;
-use pb::Block;
 
 fuzz_target!(|data: &[u8]| {
-    let _decoded: Result<Block, ProxyDecodeError> = pb::Block::proxy_decode(data);
+    let decoded: Result<pb::Block, ProxyDecodeError> = pb::Block::proxy_decode(data);
+    match decoded {
+        Ok(b) => {
+            let _d = ic_types::consensus::Block::try_from(b);
+        }
+        Err(_e) => (),
+    }
 });
