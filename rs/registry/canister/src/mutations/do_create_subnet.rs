91b5fbcfe5bdfc578be0cedee33cc70b1ff8aeed
Leo Eichhorn <leo.eichhorn@dfinity.org>
2025-07-16 08:55:25 +0000

    clippy

--- rs/registry/canister/src/mutations/do_create_subnet.rs
+++ rs/registry/canister/src/mutations/do_create_subnet.rs
@@ -6,7 +6,7 @@ use dfn_core::api::{call, CanisterId};
 use dfn_core::println;
 use ic_base_types::{NodeId, PrincipalId, RegistryVersion, SubnetId};
 use ic_management_canister_types_private::{
-    EcdsaKeyId, MasterPublicKeyId, SetupInitialDKGArgs, SetupInitialDKGResponse,
+    MasterPublicKeyId, SetupInitialDKGArgs, SetupInitialDKGResponse,
 };
 use ic_protobuf::registry::{
     node::v1::NodeRecord,
@@ -559,7 +559,7 @@ mod test {
         add_fake_subnet, get_invariant_compliant_subnet_record, invariant_compliant_registry,
         prepare_registry_with_nodes,
     };
-    use ic_management_canister_types_private::EcdsaCurve;
+    use ic_management_canister_types_private::{EcdsaCurve, EcdsaKeyId};
     use ic_nervous_system_common_test_keys::{TEST_USER1_PRINCIPAL, TEST_USER2_PRINCIPAL};
     use ic_registry_subnet_features::{ChainKeyConfig, DEFAULT_ECDSA_MAX_QUEUE_SIZE};
     use ic_types::ReplicaVersion;
