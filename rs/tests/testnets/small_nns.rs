e7db6bab2eaeb9495a5f66519bb1e51600bdaf53
Nikola Milosavljevic <73236646+NikolaMilosa@users.noreply.github.com>
2025-07-17 19:38:28 +0200

    chore: adding vector vm to all testnets (#5963)
    This PR builds on top of #5948 and adds vector VMs to all our testnets
    and a `guest_os_qualification` test

--- rs/tests/testnets/small_nns.rs
+++ rs/tests/testnets/small_nns.rs
@@ -45,6 +45,7 @@ use ic_system_test_driver::driver::{
     prometheus_vm::{HasPrometheus, PrometheusVm},
     test_env::TestEnv,
     test_env_api::HasTopologySnapshot,
+    vector_vm::VectorVm,
 };
 use nns_dapp::{
     install_ii_nns_dapp_and_subnet_rental, nns_dapp_customizations, set_authorized_subnets,
@@ -61,6 +62,9 @@ pub fn setup(env: TestEnv) {
     PrometheusVm::default()
         .start(&env)
         .expect("Failed to start prometheus VM");
+    let mut vector_vm = VectorVm::new();
+    vector_vm.start(&env).expect("Failed to start Vector VM");
+
     InternetComputer::new()
         .add_subnet(Subnet::new(SubnetType::System).add_nodes(1))
         .add_subnet(Subnet::new(SubnetType::Application).add_nodes(1))
@@ -79,6 +83,10 @@ pub fn setup(env: TestEnv) {
     let ic_gateway_url = ic_gateway.get_public_url();
     let ic_gateway_domain = ic_gateway_url.domain().unwrap();
     env.sync_with_prometheus_by_name("", Some(ic_gateway_domain.to_string()));
+    vector_vm
+        .sync_targets(&env)
+        .expect("Failed to sync Vector targets");
+
     install_ii_nns_dapp_and_subnet_rental(&env, &ic_gateway_url, None);
     set_authorized_subnets(&env);
 }
