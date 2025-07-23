06621806fa434ea0becb5b17720774126d739d35
AlexandraZapuc <61285418+AlexandraZapuc@users.noreply.github.com>
2025-07-23 13:21:39 +0200

    feat: EXC-2058: Add environment variables validation (#5987)
    This PR adds validation for environment variable constraints to ensure
    input correctness and prevent misconfiguration.
    
    Validations included:
    	•	Rejects settings when environment variables names are too long.
    • Rejects settings when environment variables values exceed the maximum
    allowed length.
    • Detects and rejects when duplicate environment variable names are
    found.
    	•	Enforces a maximum number of environment variables

--- rs/execution_environment/src/execution_environment.rs
+++ rs/execution_environment/src/execution_environment.rs
@@ -406,6 +406,9 @@ impl ExecutionEnvironment {
             canister_snapshot_data_baseline_instructions,
             config.default_wasm_memory_limit,
             config.max_number_of_snapshots_per_canister,
+            config.max_environment_variables,
+            config.max_environment_variable_name_length,
+            config.max_environment_variable_value_length,
         );
         let metrics = ExecutionEnvironmentMetrics::new(metrics_registry);
         let canister_manager = CanisterManager::new(
