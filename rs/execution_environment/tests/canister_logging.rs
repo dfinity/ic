00e84737205a5f396774e05eddd7150c99e1a083
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-07-23 09:35:06 +0000

    clippy

--- rs/execution_environment/tests/canister_logging.rs
+++ rs/execution_environment/tests/canister_logging.rs
@@ -1,6 +1,5 @@
 use ic_base_types::PrincipalId;
 use ic_config::execution_environment::Config as ExecutionConfig;
-use ic_config::flag_status::FlagStatus;
 use ic_config::subnet_config::SubnetConfig;
 use ic_management_canister_types_private::{
     self as ic00, BoundedAllowedViewers, CanisterIdRecord, CanisterInstallMode, CanisterLogRecord,
