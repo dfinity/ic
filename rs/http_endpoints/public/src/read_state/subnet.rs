7f5ee9c824fb9eb64c3b1293236d5df6f39d7494
Stefan Schneider <stefan.schneider@dfinity.org>
2025-08-05 08:43:14 +0000

    feat: [MR-670] Bump certification version
    This commit bumps the current certification version to V21. V21
    introduces a new format to represent the routing table in the state tree
    and was previously implemented in commit 7a809c0.
    
    In addition, this PR also whitelists the new paths for calls to
    `api/v2/canister/*/read_state` and `api/v2/subnet/*/read_state`.
    
    This commit is part of the larger canister migration feature and
    particularly the effort to expose the routing table in delegations in a
    more scalable way.

--- rs/http_endpoints/public/src/read_state/subnet.rs
+++ rs/http_endpoints/public/src/read_state/subnet.rs
@@ -193,6 +193,7 @@ fn verify_paths(paths: &[Path], effective_principal_id: PrincipalId) -> Result<(
             | [b"subnet", _subnet_id, b"public_key" | b"canister_ranges" | b"node"] => {}
             [b"subnet", _subnet_id, b"node", _node_id]
             | [b"subnet", _subnet_id, b"node", _node_id, b"public_key"] => {}
+            [b"canister_ranges", _subnet_id] => {}
             [b"subnet", subnet_id, b"metrics"] => {
                 let principal_id = parse_principal_id(subnet_id)?;
                 verify_principal_ids(&principal_id, &effective_principal_id)?;
@@ -249,6 +250,10 @@ mod test {
                         ByteBuf::from(subnet_test_id(1).get().to_vec()).into(),
                         Label::from("metrics")
                     ]),
+                    Path::new(vec![
+                        Label::from("canister_ranges"),
+                        ByteBuf::from(subnet_test_id(1).get().to_vec()).into(),
+                    ]),
                 ],
                 subnet_test_id(1).get(),
             ),
@@ -284,6 +289,16 @@ mod test {
                     ByteBuf::from(canister_test_id(1).get().to_vec()).into(),
                     Label::from("module_hash")
                 ])
+            ],
+            subnet_test_id(1).get(),
+        )
+        .is_err());
+
+        assert!(verify_paths(
+            &[
+                Path::new(vec![
+                    Label::from("canister_ranges"),
+                ]),
             ],
             subnet_test_id(1).get(),
         )
