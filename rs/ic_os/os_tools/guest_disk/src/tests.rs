5472dffb36d43476257dc7065a5d207e49764d00
David Frank <david.frank@dfinity.org>
2025-08-07 17:27:26 +0200

    clippy

--- rs/ic_os/os_tools/guest_disk/src/tests.rs
+++ rs/ic_os/os_tools/guest_disk/src/tests.rs
@@ -389,7 +389,7 @@ fn test_open_store_multiple_times_with_different_keys() {
 
         fixture
             .open(Partition::Store)
-            .expect(&format!("Failed to open store partition on iteration {i}"));
+            .unwrap_or_else(|_| panic!("Failed to open store partition on iteration {i}"));
         assert!(Path::new("/dev/mapper/vda10-crypt").exists());
         deactive_crypt_device_with_check("vda10-crypt");
     }
