0d8c934bd8bb1be1123d9ffda9d75e8ac9f057a9
Stefan Schneider <31004026+schneiderstefan@users.noreply.github.com>
2025-08-11 12:32:09 +0200

    Update rs/crypto/tree_hash/src/flat_map.rs
    Co-authored-by: Alin Sinpalean <58422065+alin-at-dfinity@users.noreply.github.com>

--- rs/crypto/tree_hash/src/flat_map.rs
+++ rs/crypto/tree_hash/src/flat_map.rs
@@ -118,7 +118,7 @@ impl<K: Ord, V> FlatMap<K, V> {
 
     /// Returns references to the key/value pair with the largest key that is at most `key`.
     ///
-    /// Complexity(O(log(N)))
+    /// Complexity: O(log(N))
     pub fn lower_bound(&self, key: &K) -> Option<(&K, &V)> {
         match self.keys.binary_search(key) {
             Ok(idx) => Some((&self.keys[idx], &self.values[idx])),
