f0df0587879ce30e4c8c54c1948d1ab2e30ab041
Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>
2025-08-13 09:50:32 +0200

    Clean up BlockBuilder and use it in tests

--- rs/ledger_suite/icrc1/test_utils/src/icrc3.rs
+++ rs/ledger_suite/icrc1/test_utils/src/icrc3.rs
@@ -1,19 +1,29 @@
 use candid::Nat;
 use ic_ledger_core::tokens::TokensType;
 use icrc_ledger_types::icrc::generic_value::ICRC3Value;
+use icrc_ledger_types::icrc1::account::Account;
 use serde_bytes::ByteBuf;
 use std::collections::BTreeMap;
 
-pub struct BlockBuilder<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> {
+/// Helper function to convert Account to ICRC3Value array format
+fn account_to_icrc3_value(account: &Account) -> ICRC3Value {
+    let mut account_array = vec![ICRC3Value::Blob(ByteBuf::from(account.owner.as_slice()))];
+    if let Some(subaccount) = account.subaccount {
+        account_array.push(ICRC3Value::Blob(ByteBuf::from(subaccount)));
+    }
+    ICRC3Value::Array(account_array)
+}
+
+pub struct BlockBuilder<Tokens: TokensType> {
     block_id: u64,
     timestamp: u64,
-    fee_collector: Option<FeeCollector>,
+    fee_collector: Option<Account>,
     fee_collector_block: Option<u64>,
     fee: Option<Tokens>,
     parent_hash: Option<Vec<u8>>,
 }
 
-impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCollector> {
+impl<Tokens: TokensType> BlockBuilder<Tokens> {
     /// Create a new BlockBuilder with the specified block ID and timestamp
     pub fn new(block_id: u64, timestamp: u64) -> Self {
         Self {
@@ -27,7 +37,7 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCo
     }
 
     /// Set the fee collector
-    pub fn with_fee_collector(mut self, fee_collector: FeeCollector) -> Self {
+    pub fn with_fee_collector(mut self, fee_collector: Account) -> Self {
         self.fee_collector = Some(fee_collector);
         self
     }
@@ -51,43 +61,30 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCo
     }
 
     /// Create a transfer operation
-    pub fn transfer(
-        self,
-        from: impl Into<Vec<u8>>,
-        to: impl Into<Vec<u8>>,
-        amount: Tokens,
-    ) -> TransferBuilder<Tokens, FeeCollector> {
+    pub fn transfer(self, from: Account, to: Account, amount: Tokens) -> TransferBuilder<Tokens> {
         TransferBuilder {
             builder: self,
-            from: from.into(),
-            to: to.into(),
+            from,
+            to,
             amount,
             spender: None,
         }
     }
 
     /// Create a mint operation
-    pub fn mint(
-        self,
-        to: impl Into<Vec<u8>>,
-        amount: Tokens,
-    ) -> MintBuilder<Tokens, FeeCollector> {
+    pub fn mint(self, to: Account, amount: Tokens) -> MintBuilder<Tokens> {
         MintBuilder {
             builder: self,
-            to: to.into(),
+            to,
             amount,
         }
     }
 
     /// Create a burn operation
-    pub fn burn(
-        self,
-        from: impl Into<Vec<u8>>,
-        amount: Tokens,
-    ) -> BurnBuilder<Tokens, FeeCollector> {
+    pub fn burn(self, from: Account, amount: Tokens) -> BurnBuilder<Tokens> {
         BurnBuilder {
             builder: self,
-            from: from.into(),
+            from,
             amount,
             spender: None,
         }
@@ -96,14 +93,14 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCo
     /// Create an approve operation
     pub fn approve(
         self,
-        from: impl Into<Vec<u8>>,
-        spender: impl Into<Vec<u8>>,
+        from: Account,
+        spender: Account,
         allowance: Tokens,
-    ) -> ApproveBuilder<Tokens, FeeCollector> {
+    ) -> ApproveBuilder<Tokens> {
         ApproveBuilder {
             builder: self,
-            from: from.into(),
-            spender: spender.into(),
+            from,
+            spender,
             allowance,
             expected_allowance: None,
             expires_at: None,
@@ -111,7 +108,11 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCo
     }
 
     /// Build the final ICRC3Value block
-    fn build_with_operation(self, op_name: &str, tx_fields: BTreeMap<String, ICRC3Value>) -> ICRC3Value {
+    fn build_with_operation(
+        self,
+        op_name: &str,
+        tx_fields: BTreeMap<String, ICRC3Value>,
+    ) -> ICRC3Value {
         let mut block_map = BTreeMap::new();
 
         // Add timestamp
@@ -120,7 +121,7 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCo
         // Create transaction
         let mut tx_map = BTreeMap::new();
         tx_map.insert("op".to_string(), ICRC3Value::Text(op_name.to_string()));
-        
+
         // Add operation-specific fields
         for (key, value) in tx_fields {
             tx_map.insert(key, value);
@@ -134,11 +135,8 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCo
         }
 
         // Add fee collector if specified
-        if let Some(fee_collector) = self.fee_collector {
-            block_map.insert(
-                "fee_col".to_string(),
-                ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(fee_collector))]),
-            );
+        if let Some(fee_collector) = &self.fee_collector {
+            block_map.insert("fee_col".to_string(), account_to_icrc3_value(fee_collector));
         }
 
         // Add fee collector block if specified
@@ -163,39 +161,30 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BlockBuilder<Tokens, FeeCo
 }
 
 /// Builder for transfer operations
-pub struct TransferBuilder<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> {
-    builder: BlockBuilder<Tokens, FeeCollector>,
-    from: Vec<u8>,
-    to: Vec<u8>,
+pub struct TransferBuilder<Tokens: TokensType> {
+    builder: BlockBuilder<Tokens>,
+    from: Account,
+    to: Account,
     amount: Tokens,
-    spender: Option<Vec<u8>>,
+    spender: Option<Account>,
 }
 
-impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> TransferBuilder<Tokens, FeeCollector> {
+impl<Tokens: TokensType> TransferBuilder<Tokens> {
     /// Set the spender for the transfer
-    pub fn with_spender(mut self, spender: impl Into<Vec<u8>>) -> Self {
-        self.spender = Some(spender.into());
+    pub fn with_spender(mut self, spender: Account) -> Self {
+        self.spender = Some(spender);
         self
     }
 
     /// Build the transfer block
     pub fn build(self) -> ICRC3Value {
         let mut tx_fields = BTreeMap::new();
-        tx_fields.insert(
-            "from".to_string(),
-            ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(self.from))]),
-        );
-        tx_fields.insert(
-            "to".to_string(),
-            ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(self.to))]),
-        );
+        tx_fields.insert("from".to_string(), account_to_icrc3_value(&self.from));
+        tx_fields.insert("to".to_string(), account_to_icrc3_value(&self.to));
         tx_fields.insert("amt".to_string(), ICRC3Value::Nat(self.amount.into()));
 
-        if let Some(spender) = self.spender {
-            tx_fields.insert(
-                "spender".to_string(),
-                ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(spender))]),
-            );
+        if let Some(spender) = &self.spender {
+            tx_fields.insert("spender".to_string(), account_to_icrc3_value(spender));
         }
 
         self.builder.build_with_operation("xfer", tx_fields)
@@ -203,20 +192,17 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> TransferBuilder<Tokens, Fe
 }
 
 /// Builder for mint operations
-pub struct MintBuilder<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> {
-    builder: BlockBuilder<Tokens, FeeCollector>,
-    to: Vec<u8>,
+pub struct MintBuilder<Tokens: TokensType> {
+    builder: BlockBuilder<Tokens>,
+    to: Account,
     amount: Tokens,
 }
 
-impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> MintBuilder<Tokens, FeeCollector> {
+impl<Tokens: TokensType> MintBuilder<Tokens> {
     /// Build the mint block
     pub fn build(self) -> ICRC3Value {
         let mut tx_fields = BTreeMap::new();
-        tx_fields.insert(
-            "to".to_string(),
-            ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(self.to))]),
-        );
+        tx_fields.insert("to".to_string(), account_to_icrc3_value(&self.to));
         tx_fields.insert("amt".to_string(), ICRC3Value::Nat(self.amount.into()));
 
         self.builder.build_with_operation("mint", tx_fields)
@@ -224,34 +210,28 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> MintBuilder<Tokens, FeeCol
 }
 
 /// Builder for burn operations
-pub struct BurnBuilder<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> {
-    builder: BlockBuilder<Tokens, FeeCollector>,
-    from: Vec<u8>,
+pub struct BurnBuilder<Tokens: TokensType> {
+    builder: BlockBuilder<Tokens>,
+    from: Account,
     amount: Tokens,
-    spender: Option<Vec<u8>>,
+    spender: Option<Account>,
 }
 
-impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BurnBuilder<Tokens, FeeCollector> {
+impl<Tokens: TokensType> BurnBuilder<Tokens> {
     /// Set the spender for the burn
-    pub fn with_spender(mut self, spender: impl Into<Vec<u8>>) -> Self {
-        self.spender = Some(spender.into());
+    pub fn with_spender(mut self, spender: Account) -> Self {
+        self.spender = Some(spender);
         self
     }
 
     /// Build the burn block
     pub fn build(self) -> ICRC3Value {
         let mut tx_fields = BTreeMap::new();
-        tx_fields.insert(
-            "from".to_string(),
-            ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(self.from))]),
-        );
+        tx_fields.insert("from".to_string(), account_to_icrc3_value(&self.from));
         tx_fields.insert("amt".to_string(), ICRC3Value::Nat(self.amount.into()));
 
-        if let Some(spender) = self.spender {
-            tx_fields.insert(
-                "spender".to_string(),
-                ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(spender))]),
-            );
+        if let Some(spender) = &self.spender {
+            tx_fields.insert("spender".to_string(), account_to_icrc3_value(spender));
         }
 
         self.builder.build_with_operation("burn", tx_fields)
@@ -259,16 +239,16 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> BurnBuilder<Tokens, FeeCol
 }
 
 /// Builder for approve operations
-pub struct ApproveBuilder<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> {
-    builder: BlockBuilder<Tokens, FeeCollector>,
-    from: Vec<u8>,
-    spender: Vec<u8>,
+pub struct ApproveBuilder<Tokens: TokensType> {
+    builder: BlockBuilder<Tokens>,
+    from: Account,
+    spender: Account,
     allowance: Tokens,
     expected_allowance: Option<Tokens>,
     expires_at: Option<u64>,
 }
 
-impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> ApproveBuilder<Tokens, FeeCollector> {
+impl<Tokens: TokensType> ApproveBuilder<Tokens> {
     /// Set the expected allowance
     pub fn with_expected_allowance(mut self, expected_allowance: Tokens) -> Self {
         self.expected_allowance = Some(expected_allowance);
@@ -284,174 +264,52 @@ impl<Tokens: TokensType, FeeCollector: Into<Vec<u8>>> ApproveBuilder<Tokens, Fee
     /// Build the approve block
     pub fn build(self) -> ICRC3Value {
         let mut tx_fields = BTreeMap::new();
+        tx_fields.insert("from".to_string(), account_to_icrc3_value(&self.from));
+        tx_fields.insert("spender".to_string(), account_to_icrc3_value(&self.spender));
         tx_fields.insert(
-            "from".to_string(),
-            ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(self.from))]),
+            "allowance".to_string(),
+            ICRC3Value::Nat(self.allowance.into()),
         );
-        tx_fields.insert(
-            "spender".to_string(),
-            ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(self.spender))]),
-        );
-        tx_fields.insert("allowance".to_string(), ICRC3Value::Nat(self.allowance.into()));
 
         if let Some(expected_allowance) = self.expected_allowance {
-            tx_fields.insert("expected_allowance".to_string(), ICRC3Value::Nat(expected_allowance.into()));
+            tx_fields.insert(
+                "expected_allowance".to_string(),
+                ICRC3Value::Nat(expected_allowance.into()),
+            );
         }
 
         if let Some(expires_at) = self.expires_at {
-            tx_fields.insert("expires_at".to_string(), ICRC3Value::Nat(Nat::from(expires_at)));
+            tx_fields.insert(
+                "expires_at".to_string(),
+                ICRC3Value::Nat(Nat::from(expires_at)),
+            );
         }
 
         self.builder.build_with_operation("approve", tx_fields)
     }
 }
 
-pub fn transfer_block<F: Into<Vec<u8>>, T: Into<Vec<u8>>>(
-    block_id: u64,
-    from: F,
-    to: T,
-    amount: u64,
-    timestamp: u64,
-) -> ICRC3Value {
-    let mut block_map = BTreeMap::new();
-
-    // Add timestamp
-    block_map.insert("ts".to_string(), ICRC3Value::Nat(Nat::from(timestamp)));
-
-    // Create transaction
-    let mut tx_map = BTreeMap::new();
-    tx_map.insert("op".to_string(), ICRC3Value::Text("xfer".to_string()));
-
-    tx_map.insert(
-        "from".to_string(),
-        ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(from))]),
-    );
-    tx_map.insert(
-        "to".to_string(),
-        ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(to))]),
-    );
-    tx_map.insert("amt".to_string(), ICRC3Value::Nat(Nat::from(amount)));
-
-    block_map.insert("tx".to_string(), ICRC3Value::Map(tx_map));
-
-    // Add parent hash for blocks after the first
-    if block_id > 0 {
-        let parent_hash = vec![0u8; 32]; // Simplified parent hash for testing
-        block_map.insert(
-            "phash".to_string(),
-            ICRC3Value::Blob(ByteBuf::from(parent_hash)),
-        );
-    }
-
-    ICRC3Value::Map(block_map)
-}
-
-pub fn mint_block<T: Into<Vec<u8>>, F: Into<Vec<u8>>>(
-    block_id: u64,
-    to: T,
-    amount: u64,
-    timestamp: u64,
-    fee_collector: Option<F>,
-) -> ICRC3Value {
-    let mut block_map = BTreeMap::new();
-
-    // Add timestamp
-    block_map.insert("ts".to_string(), ICRC3Value::Nat(Nat::from(timestamp)));
-
-    // Create transaction
-    let mut tx_map = BTreeMap::new();
-    tx_map.insert("op".to_string(), ICRC3Value::Text("mint".to_string()));
-
-    tx_map.insert(
-        "to".to_string(),
-        ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(to))]),
-    );
-    tx_map.insert("amt".to_string(), ICRC3Value::Nat(Nat::from(amount)));
-
-    block_map.insert("tx".to_string(), ICRC3Value::Map(tx_map));
-
-    if let Some(fee_collector) = fee_collector {
-        block_map.insert(
-            "fee_col".to_string(),
-            ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(fee_collector))]),
-        );
-    }
-
-    // Add parent hash for blocks after the first
-    if block_id > 0 {
-        let parent_hash = vec![0u8; 32]; // Simplified parent hash for testing
-        block_map.insert(
-            "phash".to_string(),
-            ICRC3Value::Blob(ByteBuf::from(parent_hash)),
-        );
-    }
-
-    ICRC3Value::Map(block_map)
-}
-
-pub fn burn_block<F: Into<Vec<u8>>>(
-    block_id: u64,
-    from: F,
-    amount: u64,
-    timestamp: u64,
-    fee: Option<u64>,
-    fee_col_block: Option<u64>,
-) -> ICRC3Value {
-    let mut block_map = BTreeMap::new();
-
-    // Add timestamp
-    block_map.insert("ts".to_string(), ICRC3Value::Nat(Nat::from(timestamp)));
-
-    // Create transaction
-    let mut tx_map = BTreeMap::new();
-    tx_map.insert("op".to_string(), ICRC3Value::Text("burn".to_string()));
-
-    tx_map.insert(
-        "from".to_string(),
-        ICRC3Value::Array(vec![ICRC3Value::Blob(ByteBuf::from(from))]),
-    );
-    tx_map.insert("amt".to_string(), ICRC3Value::Nat(Nat::from(amount)));
-
-    block_map.insert("tx".to_string(), ICRC3Value::Map(tx_map));
-
-    if let Some(fee) = fee {
-        block_map.insert("fee".to_string(), ICRC3Value::Nat(Nat::from(fee)));
-    }
-
-    if let Some(fee_col_block) = fee_col_block {
-        block_map.insert(
-            "fee_col_block".to_string(),
-            ICRC3Value::Nat(Nat::from(fee_col_block)),
-        );
-    }
-
-    // Add parent hash for blocks after the first
-    if block_id > 0 {
-        let parent_hash = vec![0u8; 32]; // Simplified parent hash for testing
-        block_map.insert(
-            "phash".to_string(),
-            ICRC3Value::Blob(ByteBuf::from(parent_hash)),
-        );
-    }
-
-    ICRC3Value::Map(block_map)
-}
-
 #[cfg(test)]
 mod builder_tests {
     use super::*;
+    use candid::Principal;
     use ic_ledger_core::tokens::Tokens;
+    use ic_types::PrincipalId;
 
     fn tokens(n: u64) -> Tokens {
         Tokens::from(n)
     }
 
+    fn test_account(n: u64) -> Account {
+        Account::from(Principal::from(PrincipalId::new_user_test_id(n)))
+    }
+
     #[test]
     fn test_transfer_builder() {
-        let block = BlockBuilder::<Tokens, Vec<u8>>::new(1, 1609459200)
+        let block = BlockBuilder::<Tokens>::new(1, 1609459200)
             .with_fee(tokens(10))
-            .transfer(b"sender".to_vec(), b"receiver".to_vec(), tokens(1000))
-            .with_spender(b"spender".to_vec())
+            .transfer(test_account(1), test_account(2), tokens(1000))
+            .with_spender(test_account(3))
             .build();
 
         if let ICRC3Value::Map(block_map) = block {
@@ -466,9 +324,9 @@ mod builder_tests {
 
     #[test]
     fn test_mint_builder() {
-        let block = BlockBuilder::<Tokens, Vec<u8>>::new(0, 1609459200)
-            .with_fee_collector(b"fee_collector".to_vec())
-            .mint(b"recipient".to_vec(), tokens(5000))
+        let block = BlockBuilder::<Tokens>::new(0, 1609459200)
+            .with_fee_collector(test_account(99))
+            .mint(test_account(1), tokens(5000))
             .build();
 
         if let ICRC3Value::Map(block_map) = block {
@@ -483,9 +341,9 @@ mod builder_tests {
 
     #[test]
     fn test_burn_builder() {
-        let block = BlockBuilder::<Tokens, Vec<u8>>::new(2, 1609459200)
-            .burn(b"burner".to_vec(), tokens(500))
-            .with_spender(b"spender".to_vec())
+        let block = BlockBuilder::<Tokens>::new(2, 1609459200)
+            .burn(test_account(1), tokens(500))
+            .with_spender(test_account(3))
             .build();
 
         if let ICRC3Value::Map(block_map) = block {
@@ -499,8 +357,8 @@ mod builder_tests {
 
     #[test]
     fn test_approve_builder() {
-        let block = BlockBuilder::<Tokens, Vec<u8>>::new(3, 1609459200)
-            .approve(b"owner".to_vec(), b"spender".to_vec(), tokens(2000))
+        let block = BlockBuilder::<Tokens>::new(3, 1609459200)
+            .approve(test_account(1), test_account(2), tokens(2000))
             .with_expected_allowance(tokens(1000))
             .with_expires_at(1609459300)
             .build();
