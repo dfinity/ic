e4dde58849af16fec86ca2a7f65b9bced557ee28
Bas van Dijk <bas@dfinity.org>
2025-08-15 11:34:04 +0200

    test: support specifying explicit bazel targets to test on PRs (#6232)
    What
    ===
    
    Support specifying explicit bazel targets to test on PRs based on which
    files have been modified regardless whether they're long_tests.
    
    Why
    ===
    
    We've had quite some instances where master got broken because a PR was
    merged that broke a long_test. So we have been requesting the offending
    teams to label their PRs with `CI_ALL_BAZEL_TARGETS` more often.
    However, always labelling your PRs with `CI_ALL_BAZEL_TARGETS` defeats
    the purpose of that label. Also running *all* long_tests is often
    overkill. Worse, folks still forget to apply the label.
    
    So we need something better; something more granular and automatic.
    Hence the `BAZEL_TARGETS` file which allows specifying which
    (long_)tests to run in any case on PRs when certain files are modified.
    
    How
    ===
    
    `PULL_REQUEST_BAZEL_TARGETS` uses globbing patterns to specify which
    file modification should trigger which bazel targets. For example:
    ```
    *ic[-_]os/*    //rs/tests/nested:guestos_upgrade_smoke_test
    ```
    would mean that any change to any file under the `ic-os/` or `rs/ic_os/`
    directories should always run the
    `//rs/tests/nested:guestos_upgrade_smoke_test` regardless of whether
    that test is tagged with `long_test`.
    
    The old `ci/bazel-scripts/diff.sh` has been replaced with a new Python
    script `ci/bazel-scripts/targets.py` which returns which bazel targets
    to build or test based on an optional `--commit_range` (specified when
    `diff_only=true`), whether to skip long_tests (`--skip_long_tests`) and
    the contents of the `BAZEL_TARGETS` file.

--- ci/bazel-scripts/targets.py
+++ ci/bazel-scripts/targets.py
@@ -8,18 +8,71 @@
 #
 # If --skip_long_tests is passed, tests tagged with 'long_test' will be excluded.
 #
+# ./PULL_REQUEST_BAZEL_TARGETS is taken into account to explicitly return targets based on modified files
+# even though they're not an explicit dependency of a bazel target or are tagged as `long_test`.
+#
 # The script will print the bazel query to stderr which is useful for debugging:
 #   ci/bazel-scripts/targets.py --skip_long_tests --commit_range=master..HEAD test
-#   bazel query --keep_going '((kind(".*_test", //...)) except attr(tags, long_test, //...)) except attr(tags, manual, //...)'
+#   bazel query --keep_going '(((kind(".*_test", //...)) except attr(tags, long_test, //...)) + set(//pre-commit:shfmt-check //pre-commit:ruff-lint)) except attr(tags, manual, //...)'
 
 import argparse
 import fnmatch
 import subprocess
 import sys
+from pathlib import Path
+from typing import Set
+
+# The file specifying which bazel targets to test explicitly on PRs based on which file modifications
+# regardless of whether those targets explicitly depend on those files or whether they're tagged as `long_test`.
+PULL_REQUEST_BAZEL_TARGETS = "PULL_REQUEST_BAZEL_TARGETS"
 
 # Return all bazel targets (//...) sans the long_tests (if --skip_long_tests is specified)
 # in case any file is modified matching any of the following globs:
 all_targets_globs = ["*.bazel", "*.bzl", ".bazelrc", ".bazelversion", "mainnet-*-revisions.json", ".github/*"]
+
+
+def load_explicit_targets() -> dict[str, Set[str]]:
+    """
+    Load and parse explicit targets from the PULL_REQUEST_BAZEL_TARGETS file.
+    Returns a dictionary mapping globbing patterns
+    to a set of targets to test explicitly on PRs.
+    """
+    lines = Path(PULL_REQUEST_BAZEL_TARGETS).read_text().splitlines()
+
+    # First filter out comments and blank lines. We use gitignore-style comment handling.
+    # See: https://git-scm.com/docs/gitignore#_pattern_format
+    # * A blank line matches no files, so it can serve as a separator for readability.
+    # * A line starting with # serves as a comment. (discard it entirely).
+    # * Put a backslash ("\") in front of the first hash for patterns that begin with a hash.
+    content_lines = []
+    for line in lines:
+        if line.startswith("#") or len(line) == 0 or line.isspace():
+            continue
+        if line.startswith("\\#"):
+            content_lines.append(line[1:])  # drop the escaping backslash
+        else:
+            content_lines.append(line)
+
+    explicit_targets = []
+    for line in content_lines:
+        # Indented lines are considered part of the previous list of targets.
+        if len(line) > 0 and line[0].isspace():
+            if len(explicit_targets) == 0:
+                raise ValueError(f"Unexpected indentation in {PULL_REQUEST_BAZEL_TARGETS} for line: '{line}'")
+            targets = line.split()
+            explicit_targets[-1][1].update(targets)
+        else:
+            parts = line.split()
+            pattern = parts[0]  # Blank lines have been filtered out so we have at least a pattern.
+            targets = set(parts[1:])  # Note we accept an empty set of targets.
+            explicit_targets.append((pattern, targets))
+
+    # Turn the list of explicit targets into a dictionary to unify equivalent patterns.
+    explicit_targets_dict = {}
+    for pattern, targets in explicit_targets:
+        explicit_targets_dict[pattern] = explicit_targets_dict.get(pattern, set()) | targets
+
+    return explicit_targets_dict
 
 
 def diff_only_query(command: str, commit_range: str, skip_long_tests: bool) -> str:
@@ -53,6 +106,14 @@ def diff_only_query(command: str, commit_range: str, skip_long_tests: bool) -> s
 
     # Exclude the long_tests if requested:
     query = f"({query})" + (" except attr(tags, long_test, //...)" if skip_long_tests else "")
+
+    # Next, add the explicit targets from the PULL_REQUEST_BAZEL_TARGETS file that match the modified files:
+    explicit_targets: Set[str] = set()
+    for pattern, explicit_targets_for_pattern in load_explicit_targets().items():
+        if len(fnmatch.filter(modified_files, pattern)) > 0:
+            explicit_targets |= explicit_targets_for_pattern
+    explicit_targets_union = " ".join(explicit_targets)
+    query = f"({query}) + set({explicit_targets_union})"
 
     return query
 
