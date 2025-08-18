a09164e2dad9e9c1a0101ae514624d5fc45ef625
Mihail Jianu <mihail.jianu@dfinity.org>
2025-08-18 12:38:39 +0000

    soak

--- rs/tests/networking/canister_http_soak_test.rs
+++ rs/tests/networking/canister_http_soak_test.rs
@@ -16,11 +16,15 @@ Success::
 
 end::catalog[] */
 
+use std::time::Duration;
+
+use anyhow::anyhow;
 use anyhow::bail;
 use anyhow::Result;
 use canister_http::*;
 use canister_test::Canister;
 use dfn_candid::candid_one;
+use futures::future::join_all;
 use ic_cdk::api::call::RejectionCode;
 use ic_management_canister_types_private::{HttpMethod, TransformContext, TransformFunc};
 use ic_system_test_driver::driver::group::SystemTestGroup;
@@ -34,7 +38,9 @@ use ic_system_test_driver::util::block_on;
 use ic_types::Cycles;
 use proxy_canister::RemoteHttpRequest;
 use proxy_canister::RemoteHttpResponse;
-use proxy_canister::UnvalidatedCanisterHttpRequestArgs;
+use proxy_canister::RemoteHttpStressResponse;
+use proxy_canister::RemoteHttpStressRequest;
+use proxy_canister::UnvalidatedCanisterHttpRequestArgs; 
 use serde::{Deserialize, Serialize};
 use slog::{info, Logger};
 
@@ -69,24 +75,50 @@ pub fn test(env: TestEnv) {
             .expect("there is no node in this application subnet");
 
         let runtime = get_runtime_from_node(&node);
-        // Each requests costs ~6-7 billion cycles, and we make many thousands of requests.
-        // The default 100T cycles may not be enough.
-        let proxy_canister =
-            create_proxy_canister_with_cycles(&env, &runtime, &node, Cycles::new(u128::MAX));
+
+        let canisters: Vec<_> = (0..0)  
+            .map(|i| {
+                let canister_name = format!("canister-{}", i);
+                create_proxy_canister_with_name_and_cycles(
+                    &env,
+                    &runtime,
+                    &node,
+                    &canister_name,
+                    Cycles::new(u128::MAX),
+                )
+            })
+            .collect();
+
+        let honest_canister = create_proxy_canister_with_name_and_cycles(&env, &runtime, &node, "honest-canister", Cycles::new(u128::MAX));
 
         let webserver_ipv6 = get_universal_vm_address(&env);
 
         block_on(async {
             let url = format!("https://[{webserver_ipv6}]:20443");
-            leave_proxy_canister_running(&proxy_canister, url.clone(), logger.clone()).await;
+
+            for proxy_canister in &canisters {
+                leave_proxy_canister_running(proxy_canister, url.clone(), logger.clone(), "start_continuous_requests").await;
+            }
+            return;
+            for i in 0..10 {
+                println!("Request {} of 10", i + 1);
+                let response = do_request(&honest_canister, url.clone(), &logger, 1)
+                    .await
+                    .unwrap_or(Duration::from_secs(60));
+                println!(
+                    "Average latency for 1 concurrent request: {:?}",
+                    response
+                );
+            }
         });
     }
 }
 
-async fn leave_proxy_canister_running(proxy_canister: &Canister<'_>, url: String, logger: Logger) {
+async fn leave_proxy_canister_running(proxy_canister: &Canister<'_>, url: String, logger: Logger, method: &str) {
     ic_system_test_driver::retry_with_msg_async!(
         format!(
-            "calling start_continuous_requests of proxy canister {} with URL {}",
+            "calling {} of proxy canister {} with URL {}",
+            method,
             proxy_canister.canister_id(),
             url
         ),
@@ -97,7 +129,7 @@ async fn leave_proxy_canister_running(proxy_canister: &Canister<'_>, url: String
             let context = "There is context to be appended in body";
             let res = proxy_canister
                 .update_(
-                    "start_continuous_requests",
+                    method,
                     candid_one::<
                         Result<RemoteHttpResponse, (RejectionCode, String)>,
                         RemoteHttpRequest,
@@ -107,15 +139,16 @@ async fn leave_proxy_canister_running(proxy_canister: &Canister<'_>, url: String
                             url: url.to_string(),
                             headers: vec![],
                             body: None,
-                            transform: Some(TransformContext {
-                                function: TransformFunc(candid::Func {
-                                    principal: proxy_canister.canister_id().get().0,
-                                    method: "transform_with_context".to_string(),
-                                }),
-                                context: context.as_bytes().to_vec(),
-                            }),
+                            // transform: Some(TransformContext {
+                            //     function: TransformFunc(candid::Func {
+                            //         principal: proxy_canister.canister_id().get().0,
+                            //         method: "deterministic".to_string(),
+                            //     }),
+                            //     context: context.as_bytes().to_vec(),
+                            // }),
+                            transform: None,
                             method: HttpMethod::GET,
-                            max_response_bytes: None,
+                            max_response_bytes: Some(1000),
                             is_replicated: Some(true),
                         },
                         cycles: 500_000_000_000,
@@ -132,4 +165,57 @@ async fn leave_proxy_canister_running(proxy_canister: &Canister<'_>, url: String
     )
     .await
     .expect("Timeout on doing a canister http call to the webserver");
+}
+
+async fn do_request(
+    proxy_canister: &Canister<'_>,
+    url: String,
+    logger: &Logger,
+    concurrent_requests: u64,
+) -> Result<Duration, anyhow::Error> {
+    let context = "There is context to be appended in body";
+    let res = proxy_canister
+        .update_(
+            "send_requests_in_parallel",
+            candid_one::<
+                Result<RemoteHttpStressResponse, (RejectionCode, String)>,
+                RemoteHttpStressRequest,
+            >,
+            RemoteHttpStressRequest {
+                request: RemoteHttpRequest {
+                    request: UnvalidatedCanisterHttpRequestArgs {
+                        url,
+                        headers: vec![],
+                        body: None,
+                        transform: Some(TransformContext {
+                            function: TransformFunc(candid::Func {
+                                principal: proxy_canister.canister_id().get().0,
+                                method: "transform_with_context".to_string(),
+                            }),
+                            context: context.as_bytes().to_vec(),
+                        }),
+                        method: HttpMethod::GET,
+                        max_response_bytes: None,
+                        is_replicated: None,
+                    },
+                    cycles: 500_000_000_000,
+                },
+                count: concurrent_requests,
+            },
+        )
+        .await
+        .map_err(|e| anyhow!("Update call to proxy canister failed with {:?}", e))?;
+
+    match res {
+        Ok(ref x) if x.response.status == 200 && x.response.body.contains(context) => {
+            info!(
+                logger,
+                "All {} concurrent requests succeeded!", concurrent_requests
+            );
+            Ok(x.duration)
+        }
+        _ => {
+            bail!("Http request failed response: {:?}", res);
+        }
+    }
 }
