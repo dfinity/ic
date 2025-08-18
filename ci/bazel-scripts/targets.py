dfinity/ic

commit a95f743db48b3bb54222eed5c0dfc28602657f6b

refactor(IDX): handle case when no target branch is present (#1698)
This updates the logic in `diff.sh` to use `HEAD` when no target branch
is present. It also removes the `CI_PULL_REQUEST_TARGET_BRANCH_NAME` env
var from the workflows as it is not needed anymore.

---------

Co-authored-by: IDX GitHub Automation <IDX GitHub Automation>

.github/workflows-source/ci-main.yml
@@ -26,7 +26,6 @@ env:
   CI_JOB_URL: "${{ github.server_url }}/${{ github.repository }}/actions/runs/${{ github.run_id }}"
   CI_PIPELINE_SOURCE: ${{ github.event_name }}
   CI_PROJECT_DIR: ${{ github.workspace }}
-  CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.event.pull_request.base.ref }}
   BRANCH_NAME: ${{ github.head_ref || github.ref_name }}
   ROOT_PIPELINE_ID: ${{ github.run_id }}
   RUSTFLAGS: "--remap-path-prefix=${CI_PROJECT_DIR}=/ic"
@@ -260,6 +259,7 @@ jobs:
         env:
           BAZEL_COMMAND: "build"
           RUN_ON_DIFF_ONLY: ${{ !contains(github.event.pull_request.labels.*.name, 'CI_ALL_BAZEL_TARGETS') }}
+          MERGE_BASE_SHA: ${{ github.event.pull_request.base.sha }}
       - name: Upload build-ic.tar
         uses: actions/upload-artifact@v4
         with:

.github/workflows/ci-main.yml
@@ -22,7 +22,6 @@ env:
   CI_JOB_URL: "${{ github.server_url }}/${{ github.repository }}/actions/runs/${{ github.run_id }}"
   CI_PIPELINE_SOURCE: ${{ github.event_name }}
   CI_PROJECT_DIR: ${{ github.workspace }}
-  CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.event.pull_request.base.ref }}
   BRANCH_NAME: ${{ github.head_ref || github.ref_name }}
   ROOT_PIPELINE_ID: ${{ github.run_id }}
   RUSTFLAGS: "--remap-path-prefix=${CI_PROJECT_DIR}=/ic"
@@ -346,6 +345,7 @@ jobs:
         env:
           BAZEL_COMMAND: "build"
           RUN_ON_DIFF_ONLY: ${{ !contains(github.event.pull_request.labels.*.name, 'CI_ALL_BAZEL_TARGETS') }}
+          MERGE_BASE_SHA: ${{ github.event.pull_request.base.sha }}
       - name: Upload build-ic.tar
         uses: actions/upload-artifact@v4
         with:

.github/actions/bazel-test-all/action.yaml
@@ -68,5 +68,7 @@ runs:
           BAZEL_CI_CONFIG: ${{ inputs.BAZEL_CI_CONFIG }}
           BAZEL_EXTRA_ARGS: "${{ inputs.BAZEL_EXTRA_ARGS }} ${{ inputs.BAZEL_EXTRA_ARGS_RULES }}"
           BAZEL_STARTUP_ARGS: ${{ inputs.BAZEL_STARTUP_ARGS }}
+          CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.event.pull_request.base.ref }}
+          MERGE_BASE_SHA: ${{ github.event.pull_request.base.sha }}
           HONEYCOMB_API_TOKEN: ${{ inputs.HONEYCOMB_API_TOKEN }}
           SSH_PRIVATE_KEY: ${{ inputs.SSH_PRIVATE_KEY }}

.github/workflows-source/ci-pr-only.yml
@@ -13,7 +13,6 @@ concurrency:
 env:
   CI_PIPELINE_SOURCE: ${{ github.event_name }}
   CI_PROJECT_DIR: ${{ github.workspace }}
-  CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.event.pull_request.base.ref }}
   MERGE_BRANCH: ${{ github.event.pull_request.base.ref }}
   ORG: ${{ github.repository_owner }}
 

.github/workflows-source/release-testing.yml
@@ -19,7 +19,6 @@ env:
   CI_JOB_URL: "${{ github.server_url }}/${{ github.repository }}/actions/runs/${{ github.run_id }}"
   CI_PIPELINE_SOURCE: ${{ github.event_name }}
   CI_PROJECT_DIR: ${{ github.workspace }}
-  CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.event.pull_request.base.ref }}
   BRANCH_NAME: ${{ github.event.workflow_run.head_branch || github.ref_name }}
   ROOT_PIPELINE_ID: ${{ github.run_id }}
   RUSTFLAGS: "--remap-path-prefix=${CI_PROJECT_DIR}=/ic"

.github/workflows-source/schedule-daily.yml
@@ -14,7 +14,6 @@ env:
   CI_JOB_URL: "${{ github.server_url }}/${{ github.repository }}/actions/runs/${{ github.run_id }}"
   CI_PIPELINE_SOURCE: ${{ github.event_name }}
   CI_PROJECT_DIR: ${{ github.workspace }}
-  CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.ref_name }} # this workflow will always run on the default branch
   ROOT_PIPELINE_ID: ${{ github.run_id }}
   BUILDEVENT_DATASET: "github-ci-dfinity"
 

.github/workflows-source/schedule-hourly.yml
@@ -13,7 +13,6 @@ env:
   CI_JOB_URL: "${{ github.server_url }}/${{ github.repository }}/actions/runs/${{ github.run_id }}"
   CI_PIPELINE_SOURCE: ${{ github.event_name }}
   CI_PROJECT_DIR: ${{ github.workspace }}
-  CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.event.pull_request.base.ref }}
   ROOT_PIPELINE_ID: ${{ github.run_id }}
   RUSTFLAGS: "--remap-path-prefix=${CI_PROJECT_DIR}=/ic"
   BUILDEVENT_DATASET: "github-ci-dfinity"

.github/workflows/ci-pr-only.yml
@@ -11,7 +11,6 @@ concurrency:
 env:
   CI_PIPELINE_SOURCE: ${{ github.event_name }}
   CI_PROJECT_DIR: ${{ github.workspace }}
-  CI_PULL_REQUEST_TARGET_BRANCH_NAME: ${{ github.event.pull_request.base.ref }}
   MERGE_BRANCH: ${{ github.event.pull_request.base.ref }}
   ORG: ${{ github.repository_owner }}
 jobs:

commit 1c6e07bb3ae59da60b85b1a57730f5cdb8c67adc

fix: git diff in ci/bazel-scripts/targets.py (#6305)
What?
===

This fixes a bug introduced in
https://github.com/dfinity/ic/commit/a95f743db48b3bb54222eed5c0dfc28602657f6b
(from Sep 2024) which caused the `git diff` calculation in
`ci/bazel-scripts/diff.sh`and its successor`ci/bazel-scripts/targets.py`
to return more diffs than present in the PR because of a lacking `git
merge-base` computation. This resulted in slightly slower CI than
necessary.

Why?
===
I was monitoring how our new `ci/bazel-scripts/targets.py` script
calculates targets to test and noticed something off in PR
https://github.com/dfinity/ic/pull/6301 which only modified `Cargo.lock`
and files under `rs/sns/`. However `ci/bazel-scripts/targets.py`
calculated the following query to return which targets to test:

```
bazel query --keep_going '
  (
    ((kind(".*_test", //...)) except attr(tags, long_test, //...))
    + set(
        //rs/tests/nested:guestos_upgrade_from_latest_release_to_current  !!!
        //rs/tests/nested:guestos_upgrade_smoke_test                      !!!
        //rs/tests/nested:hostos_upgrade_smoke_test                       !!!
        //rs/tests/nested:hostos_upgrade_from_latest_release_to_current   !!!
        //rs/tests/nested:registration                                    !!!
        //pre-commit:shfmt-check                                          !!!
      )
  ) except attr(tags, manual, //...)'
```

The surprise here is that all tests are being triggered (`//...`) which
should only be the case when modifying bazel files which the PR doesn't
touch. Also the `//rs/tests/nested` and `//pre-commit:shfmt-check` tests
are being triggered explicitly! According to
`PULL_REQUEST_BAZEL_TARGETS` they should only be triggered when files
matching `*ic[-_]os/*` and `*.sh` are modified but the PR didn't touch
any of these files. So let's find out what's going wrong:

Commits of PR: https://github.com/dfinity/ic/pull/6301/commits.

Bazel-Test-All run:

.github/workflows-source/ci-main.yml
@@ -207,7 +207,7 @@ jobs:
               targets_args+=("--skip_long_tests")
             fi
             if [[ "$DIFF_ONLY" == 'true' ]]; then
-              targets_args+=("--commit_range=$BASE_SHA..$HEAD_SHA")
+              targets_args+=("--base=$BASE_SHA" "--head=$HEAD_SHA")
             fi
             "${CI_PROJECT_DIR:-}"/ci/bazel-scripts/targets.py test "${targets_args[@]}" >"$target_pattern_file"
             # if bazel targets is empty we don't need to run any tests
@@ -492,7 +492,7 @@ jobs:
             targets_args=()
             commit_range_arg=""
             if [[ "$DIFF_ONLY" == 'true' ]]; then
-              targets_args+=("--commit_range=$BASE_SHA..$HEAD_SHA")
+              targets_args+=("--base=$BASE_SHA" "--head=$HEAD_SHA")
             fi
             ci/bazel-scripts/targets.py build "${targets_args[@]}" >"$targets"
 

.github/workflows/ci-main.yml
@@ -178,7 +178,7 @@ jobs:
               targets_args+=("--skip_long_tests")
             fi
             if [[ "$DIFF_ONLY" == 'true' ]]; then
-              targets_args+=("--commit_range=$BASE_SHA..$HEAD_SHA")
+              targets_args+=("--base=$BASE_SHA" "--head=$HEAD_SHA")
             fi
             "${CI_PROJECT_DIR:-}"/ci/bazel-scripts/targets.py test "${targets_args[@]}" >"$target_pattern_file"
             # if bazel targets is empty we don't need to run any tests
@@ -479,7 +479,7 @@ jobs:
             targets_args=()
             commit_range_arg=""
             if [[ "$DIFF_ONLY" == 'true' ]]; then
-              targets_args+=("--commit_range=$BASE_SHA..$HEAD_SHA")
+              targets_args+=("--base=$BASE_SHA" "--head=$HEAD_SHA")
             fi
             ci/bazel-scripts/targets.py build "${targets_args[@]}" >"$targets"
 

ci/bazel-scripts/targets.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 #
-#   targets.py [-h] [--skip_long_tests] [--commit_range COMMIT_RANGE] {build,test}
+#   targets.py [-h] [--skip_long_tests] [--base BASE] [--head HEAD] {build,test}
 #
 # This script determines which Bazel targets should be built or tested and writes them separated by newlines to stdout.
 #
-# If --commit_range is passed only bazel targets will be included that have modified inputs within the specified git COMMIT_RANGE.
+# If --base is passed only include targets with modified inputs in `git diff --name-only --merge-base $BASE $HEAD`.
+# When --head is not provided defaults to HEAD.
 #
 # If --skip_long_tests is passed, tests tagged with 'long_test' will be excluded.
 #
@@ -29,6 +30,10 @@ PULL_REQUEST_BAZEL_TARGETS = "PULL_REQUEST_BAZEL_TARGETS"
 # Return all bazel targets (//...) sans the long_tests (if --skip_long_tests is specified)
 # in case any file is modified matching any of the following globs:
 all_targets_globs = ["*.bazel", "*.bzl", ".bazelrc", ".bazelversion", "mainnet-*-revisions.json", ".github/*"]
+
+
+def log(msg: str):
+    print(msg, file=sys.stderr)
 
 
 def load_explicit_targets() -> dict[str, Set[str]]:
@@ -75,7 +80,7 @@ def load_explicit_targets() -> dict[str, Set[str]]:
     return explicit_targets_dict
 
 
-def diff_only_query(command: str, commit_range: str, skip_long_tests: bool) -> str:
+def diff_only_query(command: str, base: str, head: str, skip_long_tests: bool) -> str:
     """
     Return a bazel query for all targets that have modified inputs in the specified git commit range. Taking into account:
     * To return all targets in case files matching all_targets_globs are modified.
@@ -83,8 +88,12 @@ def diff_only_query(command: str, commit_range: str, skip_long_tests: bool) -> s
     * To exclude long_tests if requested.
     """
     modified_files = subprocess.run(
-        ["git", "diff", "--name-only", commit_range], check=True, capture_output=True, text=True
+        ["git", "diff", "--name-only", "--merge-base", base, head], check=True, capture_output=True, text=True
     ).stdout.splitlines()
+
+    log("Calculating targets to test for the following {n} modified files:".format(n=len(modified_files)))
+    for file in modified_files:
+        log(file)
 
     # The files matching the all_targets_globs are typically not depended upon by any bazel target
     # but will determine which bazel targets there are in the first place so in case they're modified
@@ -123,31 +132,32 @@ def main():
     parser.add_argument("command", choices=["build", "test"], help="Bazel command to generate targets for")
     parser.add_argument("--skip_long_tests", action="store_true", help="Exclude tests tagged as 'long_test'")
     parser.add_argument(
-        "--commit_range",
-        help="Only include targets with modified inputs in the given git commit range. For example: 'master..HEAD'",
+        "--base",
+        help="Only include targets with modified inputs in `git diff --name-only --merge-base $BASE $HEAD`. When --head is not provided defaults to HEAD.",
     )
+    parser.add_argument("--head", help="See --base.")
     args = parser.parse_args()
 
-    # If no commit range is specified, form a query to return all targets
+    # If no base is specified, form a query to return all targets
     # but exclude those tagged with 'long_test' (in case --skip_long_tests was specified).
     # Otherwise return a query for all targets that have modified inputs in the specified
     # git commit range taking several factors into account:
     query = (
         ("//..." + (" except attr(tags, long_test, //...)" if args.skip_long_tests else ""))
-        if args.commit_range is None
-        else diff_only_query(args.command, args.commit_range, args.skip_long_tests)
+        if args.base is None
+        else diff_only_query(args.command, args.base, "HEAD" if args.head is None else args.head, args.skip_long_tests)
     )
 
     # Finally, exclude targets tagged with 'manual' to avoid running manual tests:
     query = f"({query}) except attr(tags, manual, //...)"
 
-    print(f"bazel query --keep_going '{query}'", file=sys.stderr)
+    log(f"bazel query --keep_going '{query}'")
     result = subprocess.run(["bazel", "query", "--keep_going", query], stderr=subprocess.PIPE, text=True)
 
     # As described above, when the query contains files not tracked by bazel,
     # --keep_going will ignore them but will return the special exit code 3 which we ignore:
     if result.returncode not in (0, 3):
-        print(f"Error running `bazel query --keep_going '{query}'`:\n" + result.stderr, file=sys.stderr)
+        log(f"Error running `bazel query --keep_going '{query}'`:\n" + result.stderr)
         sys.exit(result.returncode)
 
 