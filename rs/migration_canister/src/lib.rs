daad29ee951200540a4a3f7c6efc25bceafbf96a
Michael Weigelt <michael.weigelt@dfinity.org>
2025-08-20 15:11:33 +0000

    wip

--- rs/migration_canister/src/lib.rs
+++ rs/migration_canister/src/lib.rs
@@ -21,7 +21,7 @@ enum ValidatonError {
 
 struct Request {
     source: Principal,
-    source_subnet: Principal, // from call to registry `get_subnet_for_canister`
+    source_subnet: Principal,
     source_original_controllers: Vec<Principal>,
     target: Principal,
     target_subnet: Principal,
@@ -33,11 +33,11 @@ enum RequestState {
     /// Request was validated successfully.
     /// * Called registry `get_subnet_for_canister` to determine:
     ///     * Existence of source and target.
-    ///     * Subnet of source and target
+    ///     * Subnet of source and target.
     /// * Called mgmt `canister_status` to determine:
-    ///     * We are controller of source and target
+    ///     * We are controller of source and target.
     ///     * The original controllers of source and target.
-    ///     * If the target has enough cycles above the freezing threshold.
+    ///     * If the target has sufficient cycles above the freezing threshold.
     Accepted { request: Request },
 
     /// Called mgmt `update_settings` to make us the only controller.
@@ -52,7 +52,7 @@ enum RequestState {
     ///     * Source and target are stopped.
     ///     * Source is ready for migration.
     ///     * Target has no snapshots.
-    ///     * Target has sufficient cycles
+    ///     * Target has sufficient cycles above the freezing threshold.
     ///     * Source canister version is not absurdly high.
     /// * Called mgmt `canister_info` to determine the history length of source.  
     ///
@@ -64,8 +64,7 @@ enum RequestState {
         canister_history_total_num: u64,
     },
 
-    /// Called mgmt `rename_canister`. Subsequent calls have to use the explicit subnet ID, not `aaaaa-aa`.
-    ///
+    /// Called mgmt `rename_canister`. Subsequent mgmt calls have to use the explicit subnet ID, not `aaaaa-aa`.
     RenamedTarget {
         request: Request,
         stopped_since: Time,
@@ -86,4 +85,22 @@ enum RequestState {
         request: Request,
         stopped_since: Time,
     },
+
+    /// Called mgmt `delete_canister`.
+    SourceDeleted {
+        request: Request,
+        stopped_since: Time,
+    },
+
+    /// Five minutes have passed since `stopped_since` such that any messages to the
+    /// source subnet have expired by now.
+    /// Restored the controllers of the target canister (now addressed with source's id).
+    ///
+    /// Called `update_settings` to restore controllers.
+    RestoredControllers { request: Request },
+
+    /// Some transition has failed fatally.
+    /// We stay in this state until the controllers have been restored and then
+    /// transition to a `Failed` state in the `HISTORY`.
+    Failed { request: Request, reason: String },
 }
