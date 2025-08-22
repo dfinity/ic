baec76d7059f29d7cb033409900a47302a65ad72
Pierugo Pace <pierugo.pace@dfinity.org>
2025-08-22 12:33:49 +0000

    wip: add uvm setup

--- rs/tests/nested/nns_recovery_test.rs
+++ rs/tests/nested/nns_recovery_test.rs
@@ -1,11 +1,15 @@
 use anyhow::Result;
+use ic_consensus_system_test_utils::impersonate_upstreams;
 use ic_system_test_driver::{driver::group::SystemTestGroup, systest};
 use nested::{DKG_INTERVAL, SUBNET_SIZE};
 use std::time::Duration;
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
-        .with_setup(|env| nested::config(env, SUBNET_SIZE, Some(DKG_INTERVAL)))
+        .with_setup(|env| {
+            impersonate_upstreams::setup_upstreams_uvm(&env);
+            nested::config(env, SUBNET_SIZE, Some(DKG_INTERVAL))
+        })
         .add_test(systest!(nested::nns_recovery_test))
         .with_timeout_per_test(Duration::from_secs(20 * 60))
         .with_overall_timeout(Duration::from_secs(25 * 60))
