af379057b023adba638b8145fb32e18db0a3ea34
Paul H. Liu <paul@dfinity.org>
2025-08-26 23:48:21 -0700

    Address reviewer comments

--- rs/bitcoin/adapter/test_utils/src/rpc_json.rs
+++ rs/bitcoin/adapter/test_utils/src/rpc_json.rs
@@ -11,7 +11,7 @@ use std::str::FromStr;
 type AddressUnchecked = Address<NetworkUnchecked>;
 
 // Used for createrawtransaction argument.
-#[derive(Serialize, Clone, PartialEq, Eq, Debug, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct CreateRawTransactionInput {
     pub txid: Txid,
@@ -20,7 +20,7 @@ pub struct CreateRawTransactionInput {
     pub sequence: Option<u32>,
 }
 
-#[derive(Deserialize, Clone, PartialEq, Eq, Debug, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GetBalancesResultEntry {
     #[serde(with = "bitcoin::amount::serde::as_btc")]
     pub trusted: Amount,
@@ -30,7 +30,8 @@ pub struct GetBalancesResultEntry {
     pub immature: Amount,
 }
 
-#[derive(Deserialize, Clone, PartialEq, Eq, Debug, Serialize)]
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct GetBalancesResult {
     pub mine: GetBalancesResultEntry,
@@ -38,7 +39,7 @@ pub struct GetBalancesResult {
 }
 
 /// Models the result of "getblockchaininfo"
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GetBlockchainInfoResult {
     /// Current network name as defined in BIP70 (main, test, signet, regtest)
     #[serde(deserialize_with = "deserialize_bip70_network")]
@@ -80,14 +81,14 @@ pub struct GetBlockchainInfoResult {
 }
 
 /// Used to represent values that can either be a string or a string array.
-#[derive(Clone, Debug, Deserialize, PartialEq, Eq, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum StringOrStringArray {
     String(String),
     StringArray(Vec<String>),
 }
 
-#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GetMempoolEntryResult {
     /// Virtual transaction size as defined in BIP 141. This is different from actual serialized
     /// size for witness transactions as witness data is discounted.
@@ -158,7 +159,7 @@ pub struct GetMempoolEntryResult {
     pub unbroadcast: Option<bool>,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct GetMempoolEntryResultFees {
     /// Transaction fee in BTC
     #[serde(with = "bitcoin::amount::serde::as_btc")]
@@ -174,7 +175,7 @@ pub struct GetMempoolEntryResultFees {
     pub descendant: Amount,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ListUnspentResultEntry {
     pub txid: Txid,
@@ -195,7 +196,7 @@ pub struct ListUnspentResultEntry {
 }
 
 // Used for signrawtransaction argument.
-#[derive(Serialize, Clone, PartialEq, Debug, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SignRawTransactionInput {
     pub txid: Txid,
@@ -211,7 +212,7 @@ pub struct SignRawTransactionInput {
     pub amount: Option<Amount>,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SignRawTransactionResultError {
     pub txid: Txid,
@@ -221,7 +222,7 @@ pub struct SignRawTransactionResultError {
     pub error: String,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SignRawTransactionResult {
     #[serde(with = "serde_hex")]
@@ -230,13 +231,13 @@ pub struct SignRawTransactionResult {
     pub errors: Option<Vec<SignRawTransactionResultError>>,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct LoadWalletResult {
     pub name: String,
     pub warning: Option<String>,
 }
 
-#[derive(Clone, PartialEq, Eq, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct UnloadWalletResult {
     pub warning: Option<String>,
 }
@@ -285,21 +286,5 @@ pub mod serde_hex {
     pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<u8>, D::Error> {
         let hex_str: String = ::serde::Deserialize::deserialize(d)?;
         FromHex::from_hex(&hex_str).map_err(D::Error::custom)
-    }
-
-    pub mod opt {
-        use super::*;
-
-        pub fn serialize<S: Serializer>(b: &Option<Vec<u8>>, s: S) -> Result<S::Ok, S::Error> {
-            match *b {
-                None => s.serialize_none(),
-                Some(ref b) => s.serialize_str(&b.to_lower_hex_string()),
-            }
-        }
-
-        pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Option<Vec<u8>>, D::Error> {
-            let hex_str: String = ::serde::Deserialize::deserialize(d)?;
-            Ok(Some(FromHex::from_hex(&hex_str).map_err(D::Error::custom)?))
-        }
     }
 }
