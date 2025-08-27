153d2c991f758640fea38b061b26be75ce64a504
Jason Zhu <jason.zhu@dfinity.org>
2025-08-27 17:56:16 +0000

    Stop preparing routing table (in the old format) in tests

--- rs/drun/src/lib.rs
+++ rs/drun/src/lib.rs
@@ -22,8 +22,7 @@ use ic_protobuf::types::v1::PrincipalId as PrincipalIdIdProto;
 use ic_protobuf::types::v1::SubnetId as SubnetIdProto;
 use ic_registry_client::client::RegistryClientImpl;
 use ic_registry_keys::{
-    make_canister_ranges_key, make_provisional_whitelist_record_key, make_routing_table_record_key,
-    ROOT_SUBNET_ID_KEY,
+    make_canister_ranges_key, make_provisional_whitelist_record_key, ROOT_SUBNET_ID_KEY,
 };
 use ic_registry_proto_data_provider::ProtoRegistryDataProvider;
 use ic_registry_provisional_whitelist::ProvisionalWhitelist;
@@ -137,14 +136,6 @@ fn get_registry(
             &make_canister_ranges_key(CanisterId::from_u64(0)),
             registry_version,
             Some(pb_routing_table.clone()),
-        )
-        .unwrap();
-    // TODO(NNS1-3781): Remove this once routing_table is no longer used by clients.
-    data_provider
-        .add(
-            &make_routing_table_record_key(),
-            registry_version,
-            Some(pb_routing_table),
         )
         .unwrap();
     let pb_whitelist = PbProvisionalWhitelist::from(ProvisionalWhitelist::All);
