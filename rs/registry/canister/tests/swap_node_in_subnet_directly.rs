2061ad40af10b5bfaf8d19f1b13433aa3a2542c1
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-08-28 21:36:32 +0200

    moving the test back to the registry canister

--- rs/registry/canister/tests/swap_node_in_subnet_directly.rs
+++ rs/registry/canister/tests/swap_node_in_subnet_directly.rs
@@ -1,9 +1,12 @@
 use ic_base_types::PrincipalId;
-use ic_nervous_system_integration_tests::pocket_ic_helpers::{self, NnsInstaller};
+use ic_nervous_system_integration_tests::pocket_ic_helpers::nns::registry::swap_node_in_subnet_directly;
 use pocket_ic::PocketIcBuilder;
 use registry_canister::mutations::do_swap_node_in_subnet_directly::{
     SwapError, SwapNodeInSubnetDirectlyPayload,
 };
+
+use crate::common::test_helpers::install_registry_canister;
+mod common;
 
 // This test ensures that we are not enabling this feature on any network until it
 // is fully implemented.
@@ -11,17 +14,11 @@ use registry_canister::mutations::do_swap_node_in_subnet_directly::{
 // TODO(DRE-551): adapt the logic of the test to not fail if the feature is enabled.
 #[tokio::test]
 async fn ensure_feature_is_turned_off() {
-    let pocket_ic = PocketIcBuilder::new()
-        .with_nns_subnet()
-        .with_sns_subnet()
-        .build_async()
-        .await;
+    let pocket_ic = PocketIcBuilder::new().with_nns_subnet().build_async().await;
 
-    let mut nns_installer = NnsInstaller::default();
-    nns_installer.with_current_nns_canister_versions();
-    nns_installer.install(&pocket_ic).await;
+    install_registry_canister(&pocket_ic).await;
 
-    let response = pocket_ic_helpers::nns::registry::swap_node_in_subnet_directly(
+    let response = swap_node_in_subnet_directly(
         &pocket_ic,
         SwapNodeInSubnetDirectlyPayload {
             new_node_id: Some(PrincipalId::new_node_test_id(1)),
