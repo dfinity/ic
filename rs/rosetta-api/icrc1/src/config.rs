382b4014710e96505980c1b2840330fd5e2302e6
Islam El-Ashi <islam.elashi@dfinity.org>
2025-08-31 09:59:49 +0000

    .

--- rs/rosetta-api/icrc1/src/config.rs
+++ rs/rosetta-api/icrc1/src/config.rs
@@ -144,6 +144,7 @@ pub struct Args {
         note = "This parameter is deprecated. Use `multi_tokens_store_dir` instead to specify the directory where database files will be stored."
     )]
     #[arg(short = 'f', long, default_value = "/data/db.sqlite")]
+    #[allow(deprecated)]
     pub store_file: PathBuf,
 
     /// The network type that rosetta connects to.
