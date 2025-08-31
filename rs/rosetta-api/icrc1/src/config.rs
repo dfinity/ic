7799e373296a6006979e7387f5734161a7e8c27e
Islam El-Ashi <islam.elashi@dfinity.org>
2025-08-31 09:26:00 +0000

    .

--- rs/rosetta-api/icrc1/src/config.rs
+++ rs/rosetta-api/icrc1/src/config.rs
@@ -299,7 +299,7 @@ mod tests {
             port: None,
             port_file: None,
             store_type: StoreType::InMemory,
-            store_file: PathBuf::from("/test/db.sqlite"),
+            store_file: None,
             network_type: None,
             network_url: None,
             log_level: Level::INFO,
