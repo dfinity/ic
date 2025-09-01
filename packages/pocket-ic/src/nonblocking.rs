47cf920bbcc6b20932cd9535aada43c6c58e4ac2
Martin Raszyk <martin.raszyk@dfinity.org>
2025-09-01 14:53:57 +0200

    chore(PocketIC): use Option<EmptyConfig> instead of bool and Option<bool>

--- packages/pocket-ic/src/nonblocking.rs
+++ packages/pocket-ic/src/nonblocking.rs
@@ -208,7 +208,7 @@ impl PocketIc {
             log_level: log_level.map(|l| l.to_string()),
             bitcoind_addr,
             icp_features: Some(icp_features),
-            allow_incomplete_state: Some(false),
+            allow_incomplete_state: None,
             initial_time,
         };
 
