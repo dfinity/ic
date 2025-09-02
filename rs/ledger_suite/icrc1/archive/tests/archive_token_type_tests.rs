882d5a0317ae8404c3e113218277b5567f8957ab
Maciej Dfinity <maciej.modelski@dfinity.org>
2025-09-02 09:00:35 +0000

    64 to 64 to 256 tests

--- rs/ledger_suite/icrc1/archive/tests/archive_token_type_tests.rs
+++ rs/ledger_suite/icrc1/archive/tests/archive_token_type_tests.rs
@@ -88,3 +88,54 @@ fn test_master_u256_to_master_u64() {
     env.upgrade_canister(archive_id, archive_u64_wasm(), upgrade_args)
         .expect("Unable to upgrade the archive canister");
 }
+
+// TODO: The following 2 tests can be deleted when the PR that added them is part of mainnet - the intermediate
+// upgrade will not be storing the `token_type` anymore - it will be stored in the initial install.
+
+#[test]
+#[should_panic(
+    expected = "Incompatible token type, the upgraded archive token type is U64, current wasm token type is U256"
+)]
+fn test_mainnet_u64_to_master_u64_to_master_u256() {
+    let env = StateMachine::new();
+    let archive_id = env
+        .install_canister(archive_mainnet_u64_wasm(), default_init_args(), None)
+        .unwrap();
+
+    // Upgrade the archive from an older u64 wasm to the current u64 wasm.
+    // This will store the `token_type` in the archive config.
+    // The intermediate upgrade is used to verify that the `token_type` was correctly
+    // stored in the archive config - without it the last upgrade would succeed.
+    let upgrade_args = Encode!(&()).unwrap();
+    env.upgrade_canister(archive_id, archive_u64_wasm(), upgrade_args)
+        .expect("Unable to upgrade the archive canister");
+
+    // Try to upgrade the archive from using a u64 wasm to a u256 wasm
+    let upgrade_args = Encode!(&()).unwrap();
+    env.upgrade_canister(archive_id, archive_u256_wasm(), upgrade_args)
+        .expect("Unable to upgrade the archive canister");
+}
+
+#[test]
+#[should_panic(
+    expected = "Incompatible token type, the upgraded archive token type is U256, current wasm token type is U64"
+)]
+fn test_mainnet_u256_to_master_u256_to_master_u64() {
+    let env = StateMachine::new();
+    let archive_id = env
+        .install_canister(archive_mainnet_u256_wasm(), default_init_args(), None)
+        .unwrap();
+
+    // Upgrade the archive from an older u256 wasm to the current u256 wasm.
+    // This will store the `token_type` in the archive config.
+    // The intermediate upgrade is used to verify that the `token_type` was correctly
+    // stored in the archive config - without it the last upgrade would succeed.
+    let upgrade_args = Encode!(&()).unwrap();
+    env.upgrade_canister(archive_id, archive_u256_wasm(), upgrade_args)
+        .expect("Unable to upgrade the archive canister");
+
+    // Try to upgrade the archive from using a u64 wasm to a u256 wasm
+    let upgrade_args = Encode!(&()).unwrap();
+    env.upgrade_canister(archive_id, archive_u64_wasm(), upgrade_args)
+        .expect("Unable to upgrade the archive canister");
+}
