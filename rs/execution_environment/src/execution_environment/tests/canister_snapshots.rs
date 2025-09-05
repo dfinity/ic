9d85d8219fa1e667659e0cf3f5892d837c77e350
Dimitris Sarlis <dimitrios.sarlis@dfinity.org>
2025-09-05 13:14:42 +0200

    Update rs/execution_environment/src/execution_environment/tests/canister_snapshots.rs
    Co-authored-by: mraszyk <31483726+mraszyk@users.noreply.github.com>

--- rs/execution_environment/src/execution_environment/tests/canister_snapshots.rs
+++ rs/execution_environment/src/execution_environment/tests/canister_snapshots.rs
@@ -1440,7 +1440,7 @@ fn load_canister_snapshot_fails_snapshot_not_found() {
 }
 
 #[test]
-fn load_canister_snapshot_does_not_work_when_sender_does_control_originating_snapshot() {
+fn load_canister_snapshot_does_not_work_when_sender_does_not_control_originating_snapshot() {
     const CYCLES: Cycles = Cycles::new(1_000_000_000_000);
     let own_subnet = subnet_test_id(1);
     let caller_canister = canister_test_id(1);
