f5f4c03566e0f6b2f4b2383b475815a441c23953
Eero Kelly <eero.kelly@dfinity.org>
2025-09-05 22:42:05 +0000

    Filter out feature branches

--- ci/src/mainnet_revisions/mainnet_revisions.py
+++ ci/src/mainnet_revisions/mainnet_revisions.py
@@ -158,7 +158,8 @@ def get_latest_replica_version_info() -> (str, str):
 
     # Hunt for the latest ReviseElectedGuestosVersions proposal that added a version
     proposals = response["data"]
-    sorted_proposals = sorted(proposals, key=lambda x: x["executed_timestamp_seconds"], reverse=True)
+    filtered_proposals = filter(lambda x: "-base" in x["summary"], proposals)
+    sorted_proposals = sorted(filtered_proposals, key=lambda x: x["executed_timestamp_seconds"], reverse=True)
     latest_elect_proposal = next(v for v in sorted_proposals if v["payload"]["replica_version_to_elect"])
 
     version = latest_elect_proposal["payload"]["replica_version_to_elect"]
@@ -182,7 +183,8 @@ def get_latest_hostos_version_info() -> (str, str):
 
     # Hunt for the latest ReviseElectedHostosVersions proposal that added a version
     proposals = response["data"]
-    sorted_proposals = sorted(proposals, key=lambda x: x["executed_timestamp_seconds"], reverse=True)
+    filtered_proposals = filter(lambda x: "-base" in x["summary"], proposals)
+    sorted_proposals = sorted(filtered_proposals, key=lambda x: x["executed_timestamp_seconds"], reverse=True)
     latest_elect_proposal = next(v for v in sorted_proposals if v["payload"]["hostos_version_to_elect"])
 
     version = latest_elect_proposal["payload"]["hostos_version_to_elect"]
