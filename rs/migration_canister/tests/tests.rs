80ece742ea102df7cbf7b09afaa9a33c122e2859
Michael Weigelt <michael.weigelt@dfinity.org>
2025-09-05 15:57:06 +0000

    wip

--- rs/migration_canister/tests/tests.rs
+++ rs/migration_canister/tests/tests.rs
@@ -1,31 +1,44 @@
+use candid::{Encode, Principal};
+use ic_base_types::CanisterId;
+use ic_management_canister_types::CanisterSettings;
+use ic_nns_test_utils::common::build_registry_wasm;
+use pocket_ic::PocketIcBuilder;
+use registry_canister::init::RegistryCanisterInitPayload;
+use tempfile::TempDir;
+
+pub const REGISTRY_CANISTER_ID: CanisterId = CanisterId::from_u64(0);
+
 #[tokio::test]
 async fn test() {
     let state_dir = TempDir::new().unwrap();
     let state_dir = state_dir.path().to_path_buf();
 
-    let pocket_ic = PocketIcBuilder::new()
+    let pic = PocketIcBuilder::new()
         .with_state_dir(state_dir.clone())
         .with_nns_subnet()
-        .with_sns_subnet()
-        .with_ii_subnet()
+        .with_application_subnet()
         .with_application_subnet()
         .build_async()
         .await;
-    let registry_proto_path = state_dir.join("registry.proto");
-    let initial_mutations = load_registry_mutations(registry_proto_path);
-    let treasury_principal_id = *TREASURY_PRINCIPAL_ID;
 
-    // Installing NNS canisters
-    bootstrap_nns(
-        &pocket_ic,
-        vec![initial_mutations],
-        vec![(
-            treasury_principal_id.into(),
-            Tokens::from_tokens(10_000_000).unwrap(),
-        )],
-        dev_participant_id,
-        dev_neuron_id,
+    let controller = Principal::anonymous();
+
+    let registry_wasm = build_registry_wasm();
+    pic.create_canister_with_id(
+        Some(controller),
+        Some(CanisterSettings {
+            controllers: Some(vec![controller]),
+            ..Default::default()
+        }),
+        REGISTRY_CANISTER_ID.into(),
+    )
+    .await
+    .unwrap();
+    pic.install_canister(
+        REGISTRY_CANISTER_ID.into(),
+        registry_wasm.bytes(),
+        Encode!(&RegistryCanisterInitPayload::default()).unwrap(),
+        Some(controller),
     )
     .await;
-    assert!(validate_network(&pocket_ic).await.is_empty());
 }
