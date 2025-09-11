cdd0010a47ed81f826b5856949a062cc26ab4c2e
Eero Kelly <eero.kelly@dfinity.org>
2025-09-11 18:06:34 +0000

    Tidy LV setup script

--- ic-os/components/guestos/init/setup-lvs/setup-lvs.sh
+++ ic-os/components/guestos/init/setup-lvs/setup-lvs.sh
@@ -40,28 +40,28 @@ pvs /dev/mapper/vda10-crypt >/dev/null 2>&1 || (
 # Set up crypto data store if it does not exist yet.
 lvs /dev/store/shared-crypto >/dev/null 2>&1 || (
     echo "Logical volume 'shared-crypto' does not exist yet (first boot?), creating it."
-    LV_SIZE=1024M
-    retry lvcreate --yes -L "$LV_SIZE"M -n shared-crypto store
+    LV_SIZE_MB=1024
+    retry lvcreate --yes -L "$LV_SIZE_MB"M -n shared-crypto store
 )
 
 # Set up state data store if it does not exist yet.
 lvs /dev/store/shared-data >/dev/null 2>&1 || (
     echo "Logical volume 'shared-data' does not exist yet (first boot?), creating it."
     # For now, only use 25% of available capacity.
-    TOTAL_SIZE=$(($(blockdev --getsz /dev/mapper/vda10-crypt) * 512))
-    LV_SIZE=$(("$TOTAL_SIZE" / 4 / 1024 / 1024))
-    retry lvcreate --yes -L "$LV_SIZE"M -n shared-data store
+    TOTAL_SIZE_MB=$(($(blockdev --getsz /dev/mapper/vda10-crypt) * 512 / 1024 / 1024))
+    LV_SIZE_MB=$(("$TOTAL_SIZE_MB" / 4))
+    retry lvcreate --yes -L "$LV_SIZE_MB"M -n shared-data store
 )
 
 # Set up backup data store if it does not exist yet.
 lvs /dev/store/shared-backup >/dev/null 2>&1 || (
     echo "Logical volume 'shared-backup' does not exist yet (first boot?), creating it."
-    TOTAL_SIZE=$(($(blockdev --getsz /dev/mapper/vda10-crypt) * 512))
+    TOTAL_SIZE_MB=$(($(blockdev --getsz /dev/mapper/vda10-crypt) * 512 / 1024 / 1024))
     # Limit to 180G or 25% of capacity, whichever is lower.
-    LV_SIZE=$(("$TOTAL_SIZE" / 4 / 1024 / 1024))
-    LV_SIZE_LIMIT=180000
-    if [ "${LV_SIZE}" -gt "${LV_SIZE_LIMIT}" ]; then
-        LV_SIZE="${LV_SIZE_LIMIT}"
+    LV_SIZE_MB=$(("$TOTAL_SIZE_MB" / 4))
+    LV_SIZE_LIMIT_MB=180000
+    if [ "${LV_SIZE_MB}" -gt "${LV_SIZE_LIMIT_MB}" ]; then
+        LV_SIZE_MB="${LV_SIZE_LIMIT_MB}"
     fi
-    retry lvcreate --yes -L "$LV_SIZE"M -n shared-backup store
+    retry lvcreate --yes -L "$LV_SIZE_MB"M -n shared-backup store
 )
