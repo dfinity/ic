63a6e6fc192d5eb74d88c28c8be831cbe67d2689
Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>
2025-09-12 13:58:41 +0200

    Extract some types into new ic-ledger-suite-state-machine-tests-types crate

--- rs/ledger_suite/icp/test_utils/src/pocket_ic_helpers/ledger.rs
+++ rs/ledger_suite/icp/test_utils/src/pocket_ic_helpers/ledger.rs
@@ -1,17 +1,13 @@
-use ic_base_types::{CanisterId, PrincipalId};
+use ic_base_types::PrincipalId;
 use ic_ledger_core::block::{BlockIndex, BlockType};
 use ic_ledger_core::Tokens;
+use ic_nns_constants::LEDGER_CANISTER_ID;
 use icp_ledger::{
     AccountIdentifier, ArchiveInfo, Block, GetBlocksArgs, QueryBlocksResponse,
     QueryEncodedBlocksResponse, TransferArgs, MAX_BLOCKS_PER_REQUEST,
 };
 use icp_ledger::{BinaryAccountBalanceArgs, TransferError};
 use pocket_ic::PocketIc;
-
-const LEDGER_CANISTER_INDEX_IN_NNS_SUBNET: u64 = 2;
-
-pub const LEDGER_CANISTER_ID: CanisterId =
-    CanisterId::from_u64(LEDGER_CANISTER_INDEX_IN_NNS_SUBNET);
 
 pub fn account_balance(pocket_ic: &PocketIc, account: &AccountIdentifier) -> Tokens {
     super::query_or_panic(
