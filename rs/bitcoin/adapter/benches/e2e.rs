e094dc7e264e130d6cc461ccd8526ea7b9ad82c1
gregorydemay <gregory.demay@dfinity.org>
2025-09-12 16:46:24 +0200

    XC-484: bench Dogecoin

--- rs/bitcoin/adapter/benches/e2e.rs
+++ rs/bitcoin/adapter/benches/e2e.rs
@@ -1,6 +1,9 @@
-use bitcoin::{block::Header as BlockHeader, BlockHash, Network};
-use criterion::{criterion_group, criterion_main, Criterion};
-use ic_btc_adapter::{start_server, BlockchainNetwork, Config, IncomingSource};
+use bitcoin::{block::Header as BlockHeader, BlockHash};
+use criterion::measurement::Measurement;
+use criterion::{criterion_group, criterion_main, BenchmarkGroup, Criterion};
+use ic_btc_adapter::{
+    start_server, BlockchainNetwork, BlockchainState, Config, IncomingSource, MAX_HEADERS_SIZE,
+};
 use ic_btc_adapter_client::setup_bitcoin_adapter_clients;
 use ic_btc_adapter_test_utils::generate_headers;
 use ic_btc_replica_types::BitcoinAdapterRequestWrapper;
@@ -12,7 +15,10 @@ use ic_interfaces_adapter_client::Options;
 use ic_interfaces_adapter_client::RpcAdapterClient;
 use ic_logger::replica_logger::no_op_logger;
 use ic_metrics::MetricsRegistry;
-use std::path::Path;
+use rand::{CryptoRng, Rng};
+use sha2::Digest;
+use std::path::{Path, PathBuf};
+use std::sync::LazyLock;
 use tempfile::Builder;
 
 type BitcoinAdapterClient = Box<
@@ -60,7 +66,7 @@ fn prepare(
 }
 
 fn e2e(criterion: &mut Criterion) {
-    let network = Network::Regtest;
+    let network = bitcoin::Network::Regtest;
     let mut config = Config::default_with(network.into());
 
     let mut processed_block_hashes = vec![];
@@ -115,12 +121,153 @@ fn e2e(criterion: &mut Criterion) {
     });
 }
 
+/// Gives a baseline on the runtime needed to verify a proof of work which involves hashing some amount of data (the block header).
+/// For simplification, the block header is modeled as a random bytes array.
+///
+/// * In case of Bitcoin, a block header is always 80 bytes and hashed twice with SHA2-256.
+/// * In case of Dogecoin, a block header may have a variable size due to the auxiliary proof of work, but scrypt is always used to hash 80 bytes:
+///     1. If there is no auxiliary proof of work the block header is 80 bytes.
+///     2. If there is an auxiliary proof of work, part of the verification involves hashing with scrypt the parent block header, which is also 80 bytes.
+///
+///   Note that contrary to SHA2-256, the runtime of scrypt (as used in Dogecoin) is little impacted by the input size.
+fn hash_block_header(criterion: &mut Criterion) {
+    let rng = &mut ic_crypto_test_utils_reproducible_rng::reproducible_rng();
+    let params = scrypt::Params::new(10, 1, 1, 32).expect("invalid scrypt params");
+    {
+        let mut bench = criterion.benchmark_group("hash_block_header_80");
+        let header: [u8; 80] = random_header(rng);
+        scrypt_vs_double_sha256(&mut bench, &params, &header);
+    }
+
+    {
+        let mut bench = criterion.benchmark_group("hash_block_header_500");
+        let header: [u8; 500] = random_header(rng);
+        scrypt_vs_double_sha256(&mut bench, &params, &header);
+    }
+
+    {
+        let mut bench = criterion.benchmark_group("hash_block_header_1000");
+        let header: [u8; 1_000] = random_header(rng);
+        scrypt_vs_double_sha256(&mut bench, &params, &header);
+    }
+}
+
+fn scrypt_vs_double_sha256<M: Measurement>(
+    group: &mut BenchmarkGroup<'_, M>,
+    scrypt_params: &scrypt::Params,
+    header: &[u8],
+) {
+    group.bench_function("scrypt", |bench| {
+        bench.iter(|| {
+            let mut hash = [0u8; 32];
+            scrypt::scrypt(header, header, scrypt_params, &mut hash).unwrap();
+        })
+    });
+
+    group.bench_function("double-SHA2-256", |bench| {
+        bench.iter(|| {
+            let _hash = sha2::Sha256::digest(sha2::Sha256::digest(header));
+        })
+    });
+}
+
+fn random_header<const N: usize, R: Rng + CryptoRng>(rng: &mut R) -> [u8; N] {
+    let mut header = [0u8; N];
+    rng.fill_bytes(&mut header);
+    header
+}
+
+fn add_800k_block_headers(criterion: &mut Criterion) {
+    static BITCOIN_HEADERS: LazyLock<Vec<bitcoin::block::Header>> = LazyLock::new(|| {
+        let headers_data_path = PathBuf::from(
+            std::env::var("BITCOIN_MAINNET_HEADERS_DATA_PATH")
+                .expect("Failed to get test data path env variable"),
+        );
+        retrieve_headers::<bitcoin::Network>(&headers_data_path)
+    });
+    static DOGECOIN_HEADERS: LazyLock<Vec<bitcoin::dogecoin::Header>> = LazyLock::new(|| {
+        let headers_data_path = PathBuf::from(
+            std::env::var("DOGECOIN_MAINNET_HEADERS_DATA_PATH")
+                .expect("Failed to get test data path env variable"),
+        );
+        retrieve_headers::<bitcoin::dogecoin::Network>(&headers_data_path)
+    });
+    {
+        // warm-up: Call BITCOIN_HEADERS once before benchmarking to avoid biasing the first sample (lazy instantiation).
+        // Genesis block header is automatically added when instantiating BlockchainState
+        let bitcoin_headers_to_add = &BITCOIN_HEADERS.as_slice()[1..];
+        assert_eq!(bitcoin_headers_to_add.len(), 800_000);
+        let mut group = criterion.benchmark_group("bitcoin_800k");
+        group.sample_size(10);
+
+        group.bench_function("add_headers", |bench| {
+            bench.iter(|| {
+                let mut blockchain_state =
+                    BlockchainState::new(bitcoin::Network::Bitcoin, &MetricsRegistry::default());
+                // Headers are processed in chunks of at most MAX_HEADERS_SIZE entries
+                for chunk in bitcoin_headers_to_add.chunks(MAX_HEADERS_SIZE) {
+                    let (added_headers, error) = blockchain_state.add_headers(chunk);
+                    assert_eq!(error, None);
+                    assert_eq!(added_headers.len(), chunk.len())
+                }
+            })
+        });
+    }
+
+    // warm-up: Call DOGECOIN_HEADERS once before benchmarking to avoid biasing the first sample (lazy instantiation).
+    // Genesis block header is automatically added when instantiating BlockchainState
+    let dogecoin_headers_to_add = &DOGECOIN_HEADERS.as_slice()[1..];
+    assert_eq!(dogecoin_headers_to_add.len(), 800_000);
+    let mut group = criterion.benchmark_group("dogecoin_800k");
+    group.sample_size(10);
+
+    group.bench_function("add_headers", |bench| {
+        bench.iter(|| {
+            let mut blockchain_state = BlockchainState::new(
+                bitcoin::dogecoin::Network::Dogecoin,
+                &MetricsRegistry::default(),
+            );
+            // Headers are processed in chunks of at most MAX_HEADERS_SIZE entries
+            for chunk in dogecoin_headers_to_add.chunks(MAX_HEADERS_SIZE) {
+                let (added_headers, error) = blockchain_state.add_headers(chunk);
+                assert_eq!(error, None);
+                assert_eq!(added_headers.len(), chunk.len())
+            }
+        })
+    });
+}
+
+fn retrieve_headers<Network: BlockchainNetwork>(file: &Path) -> Vec<Network::Header>
+where
+    Network::Header: for<'de> serde::Deserialize<'de>,
+{
+    let decompressed_headers = decompress(file);
+    serde_json::from_slice(&decompressed_headers).unwrap_or_else(|e| {
+        panic!(
+            "Failed to retrieve headers from {}: {}",
+            file.to_string_lossy(),
+            e
+        )
+    })
+}
+
+fn decompress<P: AsRef<Path>>(location: P) -> Vec<u8> {
+    use std::io::Read;
+
+    let bytes = std::fs::read(location).unwrap();
+    let mut dec = flate2::read::GzDecoder::new(bytes.as_slice());
+    let mut decompressed = Vec::new();
+    dec.read_to_end(&mut decompressed)
+        .expect("failed to decode gzip");
+    decompressed
+}
+
 // This simulation constructs a blockchain comprising four forks, each of 2000 blocks.
 // For an extended BFS execution, the initial 1975 blocks of every branch are marked in
 // the request as being processed, with the aim to receive the last 25 blocks of each fork.
 // Performance metrics are captured from the sending of the deserialised request through
 // to receiving the response and its deserialisation.
-criterion_group!(benches, e2e);
+criterion_group!(benches, e2e, hash_block_header, add_800k_block_headers);
 
 // The benchmark can be run using:
 // bazel run //rs/bitcoin/adapter:e2e_bench
