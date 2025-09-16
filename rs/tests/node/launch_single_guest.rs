119578c7af9080c30a882db571006b3b8daae9ce
Eero Kelly <eero.kelly@dfinity.org>
2025-09-16 22:48:04 +0000

    Remove mocked tests

--- rs/tests/node/launch_single_guest.rs
+++ rs/tests/node/launch_single_guest.rs
@@ -4,12 +4,10 @@ use ic_system_test_driver::{
     driver::{group::SystemTestGroup, ic::InternetComputer, test_env::TestEnv},
     systest,
 };
-use slog::info;
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_setup(setup)
-        .add_test(systest!(test))
         .execute_from_args()?;
 
     Ok(())
@@ -20,9 +18,4 @@ fn setup(env: TestEnv) {
         .add_fast_single_node_subnet(SubnetType::System)
         .setup_and_start(&env)
         .expect("failed to setup IC under test");
-}
-
-fn test(env: TestEnv) {
-    let logger = env.logger();
-    info!(logger, "VM has launched!");
 }
--- rs/tests/node/launch_single_host.rs
+++ rs/tests/node/launch_single_host.rs
@@ -3,12 +3,10 @@ use ic_system_test_driver::{
     driver::{group::SystemTestGroup, nested::NestedNodes, test_env::TestEnv},
     systest,
 };
-use slog::info;
 
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_setup(setup)
-        .add_test(systest!(test))
         .execute_from_args()?;
 
     Ok(())
@@ -18,9 +16,4 @@ fn setup(env: TestEnv) {
     NestedNodes::new(&["Host"])
         .setup_and_start(&env)
         .expect("failed to setup IC under test");
-}
-
-fn test(env: TestEnv) {
-    let logger = env.logger();
-    info!(logger, "VM has launched!");
 }
