fae6f9d5d6493fefb50c341ef17542526d50dddf
Alexandra Zapuc <alexandra.zapuc@dfinity.org>
2025-09-17 11:06:07 +0000

    .

--- rs/pocket_ic_server/src/beta_features.rs
+++ rs/pocket_ic_server/src/beta_features.rs
@@ -8,12 +8,10 @@ pub fn hypervisor_config() -> HypervisorConfig {
     HypervisorConfig {
         embedders_config: EmbeddersConfig {
             feature_flags: FeatureFlags {
-                environment_variables: FlagStatus::Enabled,
                 ..Default::default()
             },
             ..Default::default()
         },
-        environment_variables: FlagStatus::Enabled,
         ..Default::default()
     }
 }
