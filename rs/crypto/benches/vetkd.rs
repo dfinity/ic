ce7b0ae1153f22f1f41c7b7ba5cada720790c4a7
Franz-Stefan Preiss <franzstefan@dfinity.org>
2025-09-18 10:08:15 +0000

    Test clippy

--- rs/crypto/benches/vetkd.rs
+++ rs/crypto/benches/vetkd.rs
@@ -33,8 +33,7 @@ fn vetkd_bench(criterion: &mut Criterion) {
         name: "dummy_key_name".to_string(),
     }));
 
-    let number_of_nodes = [34];
-    for subnet_size in number_of_nodes {
+    for subnet_size in [34] {
         let threshold = dkg_tag.threshold_for_subnet_of_size(subnet_size);
 
         let group = &mut criterion.benchmark_group(format!(
