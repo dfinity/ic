b8fa6a9adf2d9fee4f6d3ccee0092a55fbe726b0
Pierugo Pace <pierugo.pace@dfinity.org>
2025-09-19 08:56:08 +0000

    Revert "revert flushes, eofs, closes to see if they're really necessary"
    This reverts commit 79584d9a14dee3b6ab30e0d05c01821dd639e26e.

--- rs/tests/driver/src/driver/test_env_api.rs
+++ rs/tests/driver/src/driver/test_env_api.rs
@@ -2516,6 +2516,11 @@ pub fn scp_send_to(
                 log,
                 "scp-ed local {from_local:?} of {size:?} B to remote {to_remote:?} (written {written:?})."
             );
+            remote_file.flush()?;
+            remote_file.send_eof()?;
+            remote_file.wait_eof()?;
+            remote_file.close()?;
+            remote_file.wait_close()?;
             Ok(())
         }
     )
@@ -2545,6 +2550,11 @@ pub fn scp_recv_from(
                 log,
                 "scp-ed remote {from_remote:?} of {size:?} B to local {to_local:?}."
             );
+            remote_file.flush()?;
+            remote_file.send_eof()?;
+            remote_file.wait_eof()?;
+            remote_file.close()?;
+            remote_file.wait_close()?;
             Ok(())
         }
     )
