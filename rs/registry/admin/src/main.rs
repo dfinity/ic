4b319b35e61afbc6a9a23e73ffff22c863898731
Leo Eichhorn <leo.eichhorn@dfinity.org>
2025-09-19 11:45:37 +0000

    remove panic

--- rs/registry/admin/src/main.rs
+++ rs/registry/admin/src/main.rs
@@ -5646,7 +5646,7 @@ async fn get_node_list_since(
                     node_reward_type: NodeRewardType::try_from(
                         node_record.node_reward_type.unwrap_or_default(),
                     )
-                    .expect("Invalid node_reward_type value"),
+                    .unwrap_or(NodeRewardType::Unspecified),
                 },
             )
         })
