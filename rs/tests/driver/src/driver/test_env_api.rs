ff42a2c9f4292d3df43c4b4effd89d0fdd94fc74
Pierugo Pace <pierugo.pace@dfinity.org>
2025-09-19 14:35:39 +0000

    try dropping the channel without flushing/eof/closing

--- rs/tests/driver/src/driver/test_env_api.rs
+++ rs/tests/driver/src/driver/test_env_api.rs
@@ -2498,6 +2498,7 @@ pub fn scp_send_to(
             let mut remote_file = session.scp_send(to_remote, mode, size, None)?;
             let mut from_file = std::fs::File::open(from_local)?;
             std::io::copy(&mut from_file, &mut remote_file)?;
+            drop(remote_file);
             info!(
                 log,
                 "scp-ed local {from_local:?} of {size:?} B to remote {to_remote:?} ."
