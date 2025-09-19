54e5ca23d0fc286aa0f1ec4c2db8aff0c88e94cf
Pierugo Pace <pierugo.pace@dfinity.org>
2025-09-19 18:54:55 +0000

    should fail

--- rs/tests/driver/src/driver/test_env_api.rs
+++ rs/tests/driver/src/driver/test_env_api.rs
@@ -2495,12 +2495,6 @@ pub fn scp_send_to(
             let mut remote_file = session.scp_send(to_remote, mode, size, None)?;
             let mut from_file = std::fs::File::open(from_local)?;
             let written = std::io::copy(&mut from_file, &mut remote_file)?;
-            remote_file.flush()?;
-            remote_file.send_eof()?;
-            remote_file.wait_eof()?;
-            remote_file.close()?;
-            remote_file.wait_close()?;
-
             info!(
                 log,
                 "scp-ed local {from_local:?} of {size:?} B to remote {to_remote:?} (written {written:?})."
@@ -2531,11 +2525,6 @@ pub fn scp_recv_from(
             let size = scp_file_stat.size();
             let mut to_file = std::fs::File::create(to_local)?;
             std::io::copy(&mut remote_file, &mut to_file)?;
-            remote_file.send_eof()?;
-            remote_file.wait_eof()?;
-            remote_file.close()?;
-            remote_file.wait_close()?;
-
             info!(
                 log,
                 "scp-ed remote {from_remote:?} of {size:?} B to local {to_local:?}."
