ecfe23143aadec825c946cad0efdcf46288ee641
Max Summe <maximilian.summe@dfinity.org>
2025-09-24 16:06:26 -0700

    fix import

--- rs/nervous_system/time_helpers/src/lib.rs
+++ rs/nervous_system/time_helpers/src/lib.rs
@@ -1,6 +1,4 @@
-use std::time::Duration;
-#[cfg(not(target_arch = "wasm32"))]
-use std::time::SystemTime;
+use std::time::{Duration, SystemTime};
 
 #[cfg(target_arch = "wasm32")]
 pub fn now_nanoseconds() -> u64 {
