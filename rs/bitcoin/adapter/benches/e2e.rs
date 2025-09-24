dfinity/ic

commit 70813d8ee6823b05581aca38a7db43c252828695

fix(btc-adapter): improve btc-adapter responsiveness (#6760)
Fix XC-476

Improve the runtime behavior of btc-adapter by making blocking
operations async, including:
- `to_socket_addrs` may involve DNS lookup
- `add_header` may involve disk I/O, and also header validation could be
time consuming too.

As a result, the gRPC server run by the adapter is now more responsive
and less prone to timeouts.

`BlockchainState` has to be made interior mutable in order to support
async `add_header`.
The use of `Mutex` over `BlockchainState` is no longer needed.

This PR also cleaned up the use of tokio handlers, and fixed a problem
that caused the integration
test to be flaky.

rs/bitcoin/adapter/benches/e2e.rs
@@ -80,13 +80,7 @@ fn e2e(criterion: &mut Criterion) {
         .make(|uds_path| {
             Ok(rt.block_on(async {
                 config.incoming_source = IncomingSource::Path(uds_path.to_path_buf());
-
-                start_server(
-                    &no_op_logger(),
-                    &MetricsRegistry::default(),
-                    rt.handle(),
-                    config.clone(),
-                );
+                start_server(no_op_logger(), MetricsRegistry::default(), config.clone()).await;
                 start_client(uds_path).await
             }))
         })
@@ -193,12 +187,14 @@ fn add_800k_block_headers(criterion: &mut Criterion) {
     group.sample_size(10);
 
     group.bench_function("add_headers", |bench| {
+        let rt = tokio::runtime::Runtime::new().unwrap();
         bench.iter(|| {
-            let mut blockchain_state =
+            let blockchain_state =
                 BlockchainState::new(Network::Bitcoin, &MetricsRegistry::default());
             // Headers are processed in chunks of at most MAX_HEADERS_SIZE entries
             for chunk in bitcoin_headers_to_add.chunks(MAX_HEADERS_SIZE) {
-                let (added_headers, error) = blockchain_state.add_headers(chunk);
+                let (added_headers, error) =
+                    rt.block_on(async { blockchain_state.add_headers(chunk).await });
                 assert!(error.is_none(), "Failed to add headers: {}", error.unwrap());
                 assert_eq!(added_headers.len(), chunk.len())
             }

rs/bitcoin/adapter/bin/ic-btc-adapter/main.rs
@@ -44,11 +44,6 @@ pub async fn main() {
         start_metrics_grpc(metrics_registry.clone(), log.clone(), stream);
     }
 
-    start_server(
-        &log,
-        &metrics_registry,
-        &tokio::runtime::Handle::current(),
-        config,
-    );
-    shutdown_signal(log.clone()).await;
+    start_server(log.clone(), metrics_registry, config).await;
+    shutdown_signal(log).await;
 }

rs/bitcoin/adapter/src/addressbook.rs
@@ -42,6 +42,9 @@ pub enum AddressBookError {
         /// This field contains the max amount of addresses that can be received.
         max_amount: usize,
     },
+    /// async error thrown by tokio runtime
+    #[error("JoinError")]
+    JoinError(#[from] tokio::task::JoinError),
 }
 
 /// A convenience wrapper type for addressing results from the AddressBook.
@@ -119,22 +122,27 @@ impl AddressBook {
     }
 
     /// This function takes the DNS seeds and creates a new queue of socket addresses to connect to
-    /// for the address discovery process.
-    fn build_seed_queue(&mut self) {
+    /// for the address discovery process. It is made async because `to_socket_addrs` is a blocking
+    /// operation that may involve DNS lookup.
+    async fn build_seed_queue(&mut self) -> Result<(), tokio::task::JoinError> {
         let mut rng = StdRng::from_entropy();
-        let dns_seeds = self
-            .dns_seeds
-            .iter()
-            .map(|seed| format_addr(seed, self.port));
-        let mut addresses = dns_seeds
-            .flat_map(|seed| {
-                seed.to_socket_addrs().map_or(vec![], |v| {
-                    v.filter(|addr| !self.ipv6_only || addr.is_ipv6()).collect()
+        let ipv6_only = self.ipv6_only;
+        let tasks = self.dns_seeds.iter().map(|seed| {
+            let addr = format_addr(seed, self.port);
+            tokio::task::spawn_blocking(move || addr.to_socket_addrs())
+        });
+        let mut addresses = futures::future::try_join_all(tasks)
+            .await?
+            .into_iter()
+            .flat_map(|addr| {
+                addr.map_or(vec![], |v| {
+                    v.filter(|addr| !ipv6_only || addr.is_ipv6()).collect()
                 })
             })
             .collect::<Vec<SocketAddr>>();
         addresses.shuffle(&mut rng);
         self.seed_queue = addresses.into_iter().collect();
+        Ok(())
     }
 
     /// This function is used when the adapter idles. It clears out the seed queue and active addresses.
@@ -243,9 +251,9 @@ impl AddressBook {
 
     /// This function retrieves the next seed address from the seed queue.
     /// If no seeds are found, the seed queue is rebuilt from the DNS seeds.
-    pub fn pop_seed(&mut self) -> AddressBookResult<AddressEntry> {
+    pub async fn resolve_next_seed(&mut self) -> AddressBookResult<AddressEntry> {
         if self.seed_queue.is_empty() {
-            self.build_seed_queue();
+            self.build_seed_queue().await?;
         }
 
         let address = self
@@ -366,14 +374,17 @@ mod test {
 
     /// This function tests the `AddressManager::add_many(...)` function to ensure
     /// addresses that are not valid are skipped while adding the valid addresses.
-    #[test]
-    fn test_address_manager_add_many() {
+    #[tokio::test]
+    async fn test_address_manager_add_many() {
         let config = ConfigBuilder::default_with(Network::Bitcoin)
             .with_dns_seeds(vec![String::from("127.0.0.1")])
             .build();
         let mut book = AddressBook::new(&config, no_op_logger());
 
-        let seed = book.pop_seed().expect("there should be 1 seed");
+        let seed = book
+            .resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         let socket_1 = SocketAddr::from_str("127.0.0.1:8444").expect("bad address format");
         let address_1 = Address::new(&socket_1, ServiceFlags::NETWORK);
 
@@ -390,15 +401,18 @@ mod test {
 
     /// This function tests the `AddressBook::add_many(...)` function to ensure
     /// IPv4 addresses are skipped when in IPv6 only mode.
-    #[test]
-    fn test_address_book_add_many_ipv6_only() {
+    #[tokio::test]
+    async fn test_address_book_add_many_ipv6_only() {
         let config = ConfigBuilder::default_with(Network::Bitcoin)
             .with_dns_seeds(vec![String::from("127.0.0.1"), String::from("::1")])
             .with_ipv6_only(true)
             .build();
         let mut book = AddressBook::new(&config, no_op_logger());
 
-        let seed = book.pop_seed().expect("there should be 1 seed");
+        let seed = book
+            .resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         let socket_1 = SocketAddr::from_str("127.0.0.1:8444").expect("bad address format");
         let address_1 = Address::new(&socket_1, ServiceFlags::NETWORK);
 
@@ -416,13 +430,16 @@ mod test {
 
     /// This function tests the `AddressBook::discard(...)` function to ensure
     /// the addresses are removed from the pool.
-    #[test]
-    fn test_discard_address() {
+    #[tokio::test]
+    async fn test_discard_address() {
         let config = ConfigBuilder::default_with(Network::Bitcoin)
             .with_dns_seeds(vec![String::from("127.0.0.1"), String::from("192.168.1.1")])
             .build();
         let mut book = AddressBook::new(&config, no_op_logger());
-        let seed = book.pop_seed().expect("there should be 1 seed");
+        let seed = book
+            .resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         let socket = SocketAddr::from_str("127.0.0.1:8444").expect("bad address format");
         let address = Address::new(&socket, ServiceFlags::NETWORK);
 
@@ -465,37 +482,45 @@ mod test {
         assert_eq!(book.active_addresses.len(), 0);
     }
 
-    /// This function ensures that the [AddressBook::pop_seed](AddressBook::pop_seed) method
+    /// This function ensures that the [AddressBook::resolve_next_seed](AddressBook::resolve_next_seed) method
     /// gives the next address in queue but also pushes it to the back of the queue.
-    #[test]
-    fn test_pop_seed() {
+    #[tokio::test]
+    async fn test_resolve_next_seed() {
         let config = ConfigBuilder::default_with(Network::Signet)
             .with_dns_seeds(vec![String::from("127.0.0.1"), String::from("192.168.1.1")])
             .build();
         let mut book = AddressBook::new(&config, no_op_logger());
-        book.pop_seed().expect("there should be 1 seed");
+        book.resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         assert_eq!(book.seed_queue.len(), 1);
 
-        book.pop_seed().expect("there should be 1 seed");
+        book.resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         assert_eq!(book.seed_queue.len(), 0);
 
-        book.pop_seed().expect("pop_seed should rebuild seed queue");
+        book.resolve_next_seed()
+            .await
+            .expect("resolve_next_seed should rebuild seed queue");
         assert_eq!(book.seed_queue.len(), 1);
 
         // Remove the remaining seed address and then empty the dns_seeds.
-        book.pop_seed().expect("there should be 1 seed");
+        book.resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         book.dns_seeds.clear();
-        // `pop_seed` should now cause the AddressBookError::NoSeedAddressesFound error.
+        // `resolve_next_seed` should now cause the AddressBookError::NoSeedAddressesFound error.
         assert!(matches!(
-            book.pop_seed(),
+            book.resolve_next_seed().await,
             Err(AddressBookError::NoSeedAddressesFound)
         ));
     }
 
     /// This function tests to ensure that when the seed queue is built and IPv6 only is enabled,
     /// IPv4 seeds are filtered out.
-    #[test]
-    fn test_seeds_ipv6_only() {
+    #[tokio::test]
+    async fn test_seeds_ipv6_only() {
         let config = ConfigBuilder::default_with(Network::Signet)
             .with_dns_seeds(vec![
                 String::from("127.0.0.1"),
@@ -505,22 +530,28 @@ mod test {
             .build();
         let mut book = AddressBook::new(&config, no_op_logger());
         assert_eq!(book.seed_queue.len(), 0);
-        book.build_seed_queue();
+        book.build_seed_queue().await.unwrap();
         assert_eq!(book.seed_queue.len(), 1);
-        let addr_entry = book.pop_seed().expect("there should be 1 seed");
+        let addr_entry = book
+            .resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         assert!(addr_entry.addr().is_ipv6());
     }
 
     /// This test exercises `AddressBook::clear(...)` by checking the following:
     /// 1. If there are DNS seeds, the known, active addresses, and seed queue should be emptied.
     /// 2. If there are no DNS seeds, the seed queue and active addresses is emptied.
-    #[test]
-    fn test_clear() {
+    #[tokio::test]
+    async fn test_clear() {
         let config = ConfigBuilder::default_with(Network::Bitcoin)
             .with_dns_seeds(vec![String::from("127.0.0.1"), String::from("192.168.1.1")])
             .build();
         let mut book = AddressBook::new(&config, no_op_logger());
-        let seed = book.pop_seed().expect("there should be 1 seed");
+        let seed = book
+            .resolve_next_seed()
+            .await
+            .expect("there should be 1 seed");
         let socket = SocketAddr::from_str("127.0.0.1:8444").expect("bad address format");
         let address = Address::new(
             &socket,

rs/bitcoin/adapter/src/connectionmanager.rs
@@ -71,6 +71,9 @@ pub enum ConnectionManagerError {
     /// This can happen from recycling the DNS seed queue addresses.
     #[error("Address {0} is already connected")]
     AlreadyConnected(SocketAddr),
+    /// async error thrown by tokio runtime
+    #[error("JoinError")]
+    JoinError(#[from] tokio::task::JoinError),
 }
 
 /// This type is a simple wrapper for results created by a connection manager.
@@ -165,14 +168,15 @@ impl<Network: BlockchainNetwork> ConnectionManager<Network> {
 
     /// This function contains the actions the must occur every time the connection
     /// manager must execute actions.
-    pub fn tick(
+    pub async fn tick(
         &mut self,
         current_height: BlockHeight,
         handle: fn(StreamConfigOf<Network>) -> JoinHandle<()>,
     ) {
         self.current_height = current_height;
 
-        if let Err(ConnectionManagerError::AddressBook(err)) = self.manage_connections(handle) {
+        if let Err(ConnectionManagerError::AddressBook(err)) = self.manage_connections(handle).await
+        {
             error!(self.logger, "{}", err);
         }
     }
@@ -188,7 +192,7 @@ impl<Network: BlockchainNetwork> ConnectionManager<Network> {
     }
 
     /// This function will remove disconnects and establish new connections.
-    fn manage_connections(
+    async fn manage_connections(
         &mut self,
         handle: fn(StreamConfigOf<Network>) -> JoinHandle<()>,
     ) -> ConnectionManagerResult<()> {
@@ -203,7 +207,7 @@ impl<Network: BlockchainNetwork> ConnectionManager<Network> {
             .known_peer_addresses
             .set(self.address_book.size() as i64);
         while self.connections.len() < self.get_max_number_of_connections() {
-            self.make_connection(handle)?;
+            self.make_connection(handle).await?;
         }
         Ok(())
     }
@@ -294,13 +298,13 @@ impl<Network: BlockchainNetwork> ConnectionManager<Network> {
     }
 
     /// This function creates a new connection with a stream to a BTC node.
-    fn make_connection(
+    async fn make_connection(
         &mut self,
         handle: fn(StreamConfigOf<Network>) -> JoinHandle<()>,
     ) -> ConnectionManagerResult<()> {
         self.metrics.connections.inc();
         let address_entry_result = if !self.address_book.has_enough_addresses() {
-            self.address_book.pop_seed()
+            self.address_book.resolve_next_seed().await
         } else {
             self.address_book.pop()
         };
@@ -912,7 +916,7 @@ mod test {
             manager.get_max_number_of_connections(),
             MAX_CONNECTIONS_DURING_ADDRESS_DISCOVERY
         );
-        manager.tick(BLOCK_HEIGHT_FOR_TESTS, simple_handle);
+        manager.tick(BLOCK_HEIGHT_FOR_TESTS, simple_handle).await;
         for i in 0..4u8 {
             tokio::select! {
                 event = manager
@@ -932,7 +936,7 @@ mod test {
                 // has been disconnected.
                 continue;
             }
-            manager.tick(BLOCK_HEIGHT_FOR_TESTS, simple_handle);
+            manager.tick(BLOCK_HEIGHT_FOR_TESTS, simple_handle).await;
         }
         assert_eq!(manager.current_height, 1);
         assert_eq!(

rs/bitcoin/adapter/src/get_successors_handler.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::{HashSet, VecDeque},
-    sync::{Arc, Mutex},
+    sync::Arc,
 };
 
 use bitcoin::{BlockHash, consensus::Encodable};
@@ -86,7 +86,7 @@ pub struct GetSuccessorsResponse<Header> {
 /// Contains the functionality to respond to GetSuccessorsRequests via the RPC
 /// server.
 pub struct GetSuccessorsHandler<Network: BlockchainNetwork> {
-    state: Arc<Mutex<BlockchainState<Network>>>,
+    state: Arc<BlockchainState<Network>>,
     blockchain_manager_tx: Sender<BlockchainManagerRequest>,
     network: Network,
     metrics: GetSuccessorMetrics,
@@ -97,7 +97,7 @@ impl<Network: BlockchainNetwork> GetSuccessorsHandler<Network> {
     /// inside of the adapter when a `GetSuccessorsRequest` is received.
     pub fn new(
         network: Network,
-        state: Arc<Mutex<BlockchainState<Network>>>,
+        state: Arc<BlockchainState<Network>>,
         blockchain_manager_tx: Sender<BlockchainManagerRequest>,
         metrics_registry: &MetricsRegistry,
     ) -> Self {
@@ -124,21 +124,21 @@ impl<Network: BlockchainNetwork> GetSuccessorsHandler<Network> {
             .observe(request.processed_block_hashes.len() as f64);
 
         let (blocks, next, obsolete_blocks) = {
-            let state = self.state.lock().unwrap();
-            let anchor_height = state
+            let anchor_height = self
+                .state
                 .get_cached_header(&request.anchor)
                 .map_or(0, |cached| cached.data.height);
 
             let allow_multiple_blocks = self.network.are_multiple_blocks_allowed(anchor_height);
             let blocks = get_successor_blocks(
-                &state,
+                &self.state,
                 &request.anchor,
                 &request.processed_block_hashes,
                 allow_multiple_blocks,
                 &self.network,
             );
             let next = get_next_headers(
-                &state,
+                &self.state,
                 &request.anchor,
                 &request.processed_block_hashes,
                 &blocks,
@@ -151,8 +151,8 @@ impl<Network: BlockchainNetwork> GetSuccessorsHandler<Network> {
             // There is also a chance that they are reachable from the anchor, just not through the cache.
             // Meaning that we still need to download some other blocks first. (hence we need to free the cache).
             let mut obsolete_blocks = request.processed_block_hashes;
-            if blocks.is_empty() && state.is_block_cache_full() {
-                obsolete_blocks.extend(state.get_cached_blocks())
+            if blocks.is_empty() && self.state.is_block_cache_full() {
+                obsolete_blocks.extend(self.state.get_cached_blocks())
             }
             (blocks, next, obsolete_blocks)
         };
@@ -311,8 +311,6 @@ fn get_next_headers<Network: BlockchainNetwork>(
 mod test {
     use super::*;
 
-    use std::sync::{Arc, Mutex};
-
     use bitcoin::{Block, consensus::Decodable};
     use ic_metrics::MetricsRegistry;
     use tokio::sync::mpsc::channel;
@@ -336,7 +334,7 @@ mod test {
         let (blockchain_manager_tx, _blockchain_manager_rx) = channel(10);
         let handler = GetSuccessorsHandler::new(
             network,
-            Arc::new(Mutex::new(blockchain_state)),
+            Arc::new(blockchain_state),
             blockchain_manager_tx,
             &MetricsRegistry::default(),
         );
@@ -379,16 +377,20 @@ mod test {
         };
 
         {
-            let mut blockchain = handler.state.lock().unwrap();
-            blockchain.add_headers(&main_chain);
-            blockchain.add_headers(&side_chain);
-            blockchain.add_headers(&side_chain_2);
+            let blockchain = &handler.state;
+            blockchain.add_headers(&main_chain).await;
+            blockchain.add_headers(&side_chain).await;
+            blockchain.add_headers(&side_chain_2).await;
 
             // Add main block 2
-            blockchain.add_block(main_block_2).expect("invalid block");
+            blockchain
+                .add_block(main_block_2)
+                .await
+                .expect("invalid block");
             // Add side block 1
             blockchain
                 .add_block(side_block_1.clone())
+                .await
                 .expect("invalid block");
         }
 
@@ -444,7 +446,7 @@ mod test {
         let (blockchain_manager_tx, _blockchain_manager_rx) = channel(10);
         let handler = GetSuccessorsHandler::new(
             network,
-            Arc::new(Mutex::new(blockchain_state)),
+            Arc::new(blockchain_state),
             blockchain_manager_tx,
             &MetricsRegistry::default(),
         );
@@ -461,13 +463,15 @@ mod test {
             txdata: vec![],
         };
         {
-            let mut blockchain = handler.state.lock().unwrap();
-            blockchain.add_headers(&main_chain);
+            let blockchain = &handler.state;
+            blockchain.add_headers(&main_chain).await;
             blockchain
                 .add_block(main_block_1.clone())
+                .await
                 .expect("invalid block");
             blockchain
                 .add_block(main_block_2.clone())
+                .await
                 .expect("invalid block");
         }
         let request = GetSuccessorsRequest {
@@ -492,7 +496,7 @@ mod test {
         let (blockchain_manager_tx, _blockchain_manager_rx) = channel(10);
         let handler = GetSuccessorsHandler::new(
             network,
-            Arc::new(Mutex::new(blockchain_state)),
+            Arc::new(blockchain_state),
             blockchain_manager_tx,
             &MetricsRegistry::default(),
         );
@@ -521,17 +525,20 @@ mod test {
             txdata: vec![],
         };
         {
-            let mut blockchain = handler.state.lock().unwrap();
-            blockchain.add_headers(&main_chain);
-            blockchain.add_headers(&side_chain);
+            let blockchain = &handler.state;
+            blockchain.add_headers(&main_chain).await;
+            blockchain.add_headers(&side_chain).await;
             blockchain
                 .add_block(main_block_1.clone())
+                .await
                 .expect("invalid block");
             blockchain
                 .add_block(main_block_2.clone())
+                .await
                 .expect("invalid block");
             blockchain
                 .add_block(side_block_1.clone())
+                .await
                 .expect("invalid block");
         }
         //             |-> 1'
@@ -563,20 +570,20 @@ mod test {
         let (blockchain_manager_tx, _blockchain_manager_rx) = channel(10);
         let handler = GetSuccessorsHandler::new(
             network,
-            Arc::new(Mutex::new(blockchain_state)),
+            Arc::new(blockchain_state),
             blockchain_manager_tx,
             &MetricsRegistry::default(),
         );
         let main_chain = generate_headers(genesis_hash, genesis.time, 120, &[]);
         {
-            let mut blockchain = handler.state.lock().unwrap();
-            blockchain.add_headers(&main_chain);
+            let blockchain = &handler.state;
+            blockchain.add_headers(&main_chain).await;
             for header in main_chain {
                 let block = Block {
                     header,
                     txdata: vec![],
                 };
-                blockchain.add_block(block).expect("invalid block");
+                blockchain.add_block(block).await.expect("invalid block");
             }
         }
 
@@ -599,7 +606,7 @@ mod test {
         let (blockchain_manager_tx, _blockchain_manager_rx) = channel(10);
         let handler = GetSuccessorsHandler::new(
             network,
-            Arc::new(Mutex::new(blockchain_state)),
+            Arc::new(blockchain_state),
             blockchain_manager_tx,
             &MetricsRegistry::default(),
         );
@@ -624,22 +631,26 @@ mod test {
             txdata: vec![],
         };
         {
-            let mut blockchain = handler.state.lock().unwrap();
-            let (_, maybe_err) = blockchain.add_headers(&main_chain);
+            let blockchain = &handler.state;
+            let (_, maybe_err) = blockchain.add_headers(&main_chain).await;
             assert!(
                 maybe_err.is_none(),
                 "Error was found in main chain: {maybe_err:#?}"
             );
 
-            let (_, maybe_err) = blockchain.add_headers(&side_chain);
+            let (_, maybe_err) = blockchain.add_headers(&side_chain).await;
             assert!(
                 maybe_err.is_none(),
                 "Error was found in side chain: {maybe_err:#?}"
             );
-            blockchain.add_block(main_block_2).expect("invalid block");
+            blockchain
+                .add_block(main_block_2)
+                .await
+                .expect("invalid block");
 
             blockchain
                 .add_block(side_block_1.clone())
+                .await
                 .expect("invalid block");
         }
 
@@ -689,7 +700,7 @@ mod test {
         let (blockchain_manager_tx, _blockchain_manager_rx) = channel(10);
         let handler = GetSuccessorsHandler::new(
             network,
-            Arc::new(Mutex::new(blockchain_state)),
+            Arc::new(blockchain_state),
             blockchain_manager_tx,
             &MetricsRegistry::default(),
         );
@@ -712,16 +723,20 @@ mod test {
         };
 
         {
-            let mut blockchain = handler.state.lock().unwrap();
-            let (added_headers, _) = blockchain.add_headers(&headers);
+            let blockchain = &handler.state;
+            let (added_headers, _) = blockchain.add_headers(&headers).await;
             assert_eq!(added_headers.len(), 1);
-            let (added_headers, _) = blockchain.add_headers(&additional_headers);
+            let (added_headers, _) = blockchain.add_headers(&additional_headers).await;
             assert_eq!(added_headers.len(), 1);
 
             blockchain
                 .add_block(large_block.clone())
+                .await
                 .expect("invalid block");
-            blockchain.add_block(small_block).expect("invalid block");
+            blockchain
+                .add_block(small_block)
+                .await
+                .expect("invalid block");
         };
 
         let request = GetSuccessorsRequest {
@@ -751,7 +766,7 @@ mod test {
         let (blockchain_manager_tx, _blockchain_manager_rx) = channel(10);
         let handler = GetSuccessorsHandler::new(
             network,
-            Arc::new(Mutex::new(blockchain_state)),
+            Arc::new(blockchain_state),
             blockchain_manager_tx,
             &MetricsRegistry::default(),
         );
@@ -761,8 +776,8 @@ mod test {
             generate_large_block_blockchain(main_chain[4].block_hash(), main_chain[4].time, 1);
 
         {
-            let mut blockchain = handler.state.lock().unwrap();
-            let (added_headers, _) = blockchain.add_headers(&main_chain);
+            let blockchain = &handler.state;
+            let (added_headers, _) = blockchain.add_headers(&main_chain).await;
             assert_eq!(added_headers.len(), 5);
             let main_blocks = main_chain
                 .iter()
@@ -772,11 +787,11 @@ mod test {
                 })
                 .collect::<Vec<_>>();
             for block in main_blocks {
-                blockchain.add_block(block).unwrap();
+                blockchain.add_block(block).await.unwrap();
             }
 
             for block in &large_blocks {
-                blockchain.add_block(block.clone()).unwrap();
+                blockchain.add_block(block.clone()).await.unwrap();
             }
         };
 

commit d5c1b983f8818bae27a697b59c5a551a1af732d9

fix(adapter): benchmark hanging due to server blocking thread (#6909)
Fix bug introduced in #6760 where the benchmark `e2e` for the
Bitcoin/Dogecoin adapter was left hanging due to running the gRPC server
as a blocking task, although that task never finishes (on purpose).

rs/bitcoin/adapter/benches/e2e.rs
@@ -67,8 +67,6 @@ fn prepare(
 
 fn e2e(criterion: &mut Criterion) {
     let network = Network::Regtest;
-    let mut config = Config::default_with(network.into());
-
     let mut processed_block_hashes = vec![];
     let genesis = network.genesis_block_header();
 
@@ -78,11 +76,14 @@ fn e2e(criterion: &mut Criterion) {
 
     let (client, _temp) = Builder::new()
         .make(|uds_path| {
-            Ok(rt.block_on(async {
-                config.incoming_source = IncomingSource::Path(uds_path.to_path_buf());
-                start_server(no_op_logger(), MetricsRegistry::default(), config.clone()).await;
-                start_client(uds_path).await
-            }))
+            let mut config = Config::default_with(network.into());
+            config.incoming_source = IncomingSource::Path(uds_path.to_path_buf());
+            rt.spawn(start_server(
+                no_op_logger(),
+                MetricsRegistry::default(),
+                config,
+            ));
+            Ok(rt.block_on(async { start_client(uds_path).await }))
         })
         .unwrap()
         .into_parts();