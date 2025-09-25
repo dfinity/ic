8afdabe08ffa906d0958da1a259982635e72c3af
Andrew Battat <andrew.battat@dfinity.org>
2025-09-25 11:09:25 +0000

    Remove generate_testnet_config.rs

--- rs/tests/driver/src/driver/bootstrap.rs
+++ rs/tests/driver/src/driver/bootstrap.rs
@@ -28,11 +28,12 @@ use crate::{
     k8s::job::wait_for_job_completion,
 };
 use anyhow::{Context, Result, bail};
-use config::generate_testnet_config::{
-    GenerateTestnetConfigArgs, Ipv6ConfigType, generate_testnet_config,
-};
 use config::hostos::guestos_bootstrap_image::BootstrapOptions;
-use config_types::DeploymentEnvironment;
+use config_types::{
+    CONFIG_VERSION, DeploymentEnvironment, FixedIpv6Config, GuestOSConfig, GuestOSDevSettings,
+    GuestOSSettings, GuestOSUpgradeConfig, GuestVMType, ICOSDevSettings, ICOSSettings, Ipv4Config,
+    Ipv6Config, Logging, NetworkSettings, RecoveryConfig,
+};
 use ic_base_types::NodeId;
 use ic_prep_lib::{
     internet_computer::{IcConfig, InitializedIc, TopologyConfig},
@@ -481,41 +482,6 @@ fn create_config_disk_image(
     recovery_hash: Option<String>,
     test_env: &TestEnv,
 ) -> anyhow::Result<()> {
-    // Build GuestOS config object
-    let mut config = GenerateTestnetConfigArgs {
-        ipv6_config_type: Some(Ipv6ConfigType::RouterAdvertisement),
-        deterministic_prefix: None,
-        deterministic_prefix_length: None,
-        deterministic_gateway: None,
-        fixed_address: None,
-        fixed_gateway: None,
-        ipv4_address: None,
-        ipv4_gateway: None,
-        ipv4_prefix_length: None,
-        domain_name: None,
-        node_reward_type: None,
-        mgmt_mac: None,
-        deployment_environment: Some(DeploymentEnvironment::Testnet),
-        nns_urls: None,
-        enable_trusted_execution_environment: None,
-        use_node_operator_private_key: Some(true),
-        use_ssh_authorized_keys: Some(true),
-        recovery_hash,
-        inject_ic_crypto: Some(false),
-        inject_ic_state: Some(false),
-        inject_ic_registry_local_store: Some(false),
-        backup_retention_time_seconds: None,
-        backup_purging_interval_seconds: None,
-        malicious_behavior: None,
-        query_stats_epoch_length: None,
-        bitcoind_addr: None,
-        dogecoind_addr: None,
-        jaeger_addr: None,
-        socks_proxy: None,
-        hostname: None,
-        generate_ic_boundary_tls_cert: None,
-    };
-
     let mut bootstrap_options = BootstrapOptions {
         ic_registry_local_store: Some(
             test_env
@@ -528,95 +494,18 @@ fn create_config_disk_image(
         ..Default::default()
     };
 
-    // We've seen k8s nodes fail to pick up RA correctly, so we specify their
-    // addresses directly. Ideally, all nodes should do this, to match mainnet.
-    if InfraProvider::read_attribute(test_env) == InfraProvider::K8s {
-        let ip = format!("{}/64", node.node_config.public_api.ip());
-        let gateway = "fe80::ecee:eeff:feee:eeee".to_string();
+    let guestos_config = create_guestos_config_for_node(
+        node,
+        malicious_behavior,
+        query_stats_epoch_length,
+        ipv4_config,
+        domain_name,
+        recovery_hash,
+        test_env,
+        ic_name,
+    )?;
 
-        config.ipv6_config_type = Some(Ipv6ConfigType::Fixed);
-        config.fixed_address = Some(ip.clone());
-        config.fixed_gateway = Some(gateway.clone());
-    }
-
-    // If we have a root subnet, specify the correct NNS url.
-    if let Some(node) = test_env
-        .topology_snapshot_by_name(ic_name)
-        .root_subnet()
-        .nodes()
-        .next()
-    {
-        let nns_url = format!("http://[{}]:8080", node.get_ip_addr());
-        config.nns_urls = Some(vec![nns_url.clone()]);
-    }
-
-    if let Some(malicious_behavior) = malicious_behavior {
-        info!(
-            test_env.logger(),
-            "Node with id={} has malicious behavior={:?}", node.node_id, malicious_behavior
-        );
-        config.malicious_behavior = Some(serde_json::to_string(&malicious_behavior)?);
-    }
-
-    if let Some(query_stats_epoch_length) = query_stats_epoch_length {
-        info!(
-            test_env.logger(),
-            "Node with id={} has query_stats_epoch_length={:?}",
-            node.node_id,
-            query_stats_epoch_length
-        );
-        config.query_stats_epoch_length = Some(query_stats_epoch_length);
-    }
-
-    if let Some(ref ipv4_config) = ipv4_config {
-        info!(
-            test_env.logger(),
-            "Node with id={} is IPv4-enabled: {:?}", node.node_id, ipv4_config
-        );
-        config.ipv4_address = Some(ipv4_config.ip_addr().to_string());
-        config.ipv4_gateway = Some(ipv4_config.gateway_ip_addr().to_string());
-        config.ipv4_prefix_length = Some(ipv4_config.prefix_length().try_into().unwrap());
-    }
-
-    // if the node has a domain name, generate a certificate to be used
-    // when the node is an API boundary node.
-    if let Some(domain_name) = &node.node_config.domain {
-        config.generate_ic_boundary_tls_cert = Some(domain_name.to_string());
-    }
-
-    if let Some(ref domain_name) = domain_name {
-        info!(
-            test_env.logger(),
-            "Node with id={} has domain_name {}", node.node_id, domain_name,
-        );
-        config.domain_name = Some(domain_name.to_string());
-    }
-
-    // The bitcoin_addr specifies the local bitcoin node that the bitcoin adapter should connect to in the system test environment.
-    if let Ok(bitcoind_addr) = test_env.read_json_object::<String, _>(BITCOIND_ADDR_PATH) {
-        config.bitcoind_addr = Some(bitcoind_addr.clone());
-    }
-
-    // The dogecoind_addr specifies the local dogecoin node that the dogecoin adapter should connect to in the system test environment.
-    if let Ok(dogecoind_addr) = test_env.read_json_object::<String, _>(DOGECOIND_ADDR_PATH) {
-        config.dogecoind_addr = Some(dogecoind_addr.clone());
-    }
-
-    // The jaeger_addr specifies the local Jaeger node that the nodes should connect to in the system test environment.
-    if let Ok(jaeger_addr) = test_env.read_json_object::<String, _>(JAEGER_ADDR_PATH) {
-        config.jaeger_addr = Some(jaeger_addr.clone());
-    }
-
-    // The socks_proxy configuration indicates that a socks proxy is available to the system test environment.
-    if let Ok(socks_proxy) = test_env.read_json_object::<String, _>(SOCKS_PROXY_PATH) {
-        config.socks_proxy = Some(socks_proxy.clone());
-    }
-
-    let hostname = node.node_id.to_string();
-    config.hostname = Some(hostname.clone());
-
-    // Generate the GuestOS config and set it in bootstrap_options
-    bootstrap_options.guestos_config = Some(generate_testnet_config(config)?);
+    bootstrap_options.guestos_config = Some(guestos_config);
 
     let ssh_authorized_pub_keys_dir = test_env.get_path(SSH_AUTHORIZED_PUB_KEYS_DIR);
     if ssh_authorized_pub_keys_dir.exists() {
@@ -645,6 +534,116 @@ fn create_config_disk_image(
         bail!("Could not create sha256 of image");
     }
     Ok(())
+}
+
+/// Creates a GuestOSConfig for a node based on the provided parameters.
+fn create_guestos_config_for_node(
+    node: &InitializedNode,
+    malicious_behavior: Option<MaliciousBehavior>,
+    query_stats_epoch_length: Option<u64>,
+    ipv4_config: Option<IPv4Config>,
+    domain_name: Option<String>,
+    recovery_hash: Option<String>,
+    test_env: &TestEnv,
+    ic_name: &str,
+) -> anyhow::Result<GuestOSConfig> {
+    // Build NetworkSettings
+    let ipv6_config = if InfraProvider::read_attribute(test_env) == InfraProvider::K8s {
+        let ip = format!("{}/64", node.node_config.public_api.ip());
+        let gateway = "fe80::ecee:eeff:feee:eeee".parse::<std::net::Ipv6Addr>()?;
+        Ipv6Config::Fixed(FixedIpv6Config {
+            address: ip,
+            gateway,
+        })
+    } else {
+        Ipv6Config::RouterAdvertisement
+    };
+
+    let ipv4_config = match ipv4_config {
+        Some(config) => Some(Ipv4Config {
+            address: config.ip_addr().parse::<std::net::Ipv4Addr>()?,
+            gateway: config.gateway_ip_addr().parse::<std::net::Ipv4Addr>()?,
+            prefix_length: config.prefix_length().try_into().unwrap(),
+        }),
+        None => None,
+    };
+
+    let network_settings = NetworkSettings {
+        ipv6_config,
+        ipv4_config,
+        domain_name,
+    };
+
+    // Build ICOS settings
+    let mgmt_mac = "00:00:00:00:00:00".parse()?;
+    let deployment_environment = DeploymentEnvironment::Testnet;
+
+    let nns_urls = if let Some(node) = test_env
+        .topology_snapshot_by_name(ic_name)
+        .root_subnet()
+        .nodes()
+        .next()
+    {
+        let nns_url = format!("http://[{}]:8080", node.get_ip_addr());
+        vec![Url::parse(&nns_url)?]
+    } else {
+        vec![Url::parse("https://cloudflare.com/cdn-cgi/trace")?]
+    };
+
+    let icos_settings = ICOSSettings {
+        node_reward_type: None,
+        mgmt_mac,
+        deployment_environment,
+        logging: Logging {},
+        use_nns_public_key: false,
+        nns_urls,
+        use_node_operator_private_key: true,
+        enable_trusted_execution_environment: false,
+        use_ssh_authorized_keys: true,
+        icos_dev_settings: ICOSDevSettings::default(),
+    };
+
+    // Build GuestOSDevSettings
+    let guestos_dev_settings = GuestOSDevSettings {
+        backup_spool: None,
+        malicious_behavior,
+        query_stats_epoch_length,
+        bitcoind_addr: test_env
+            .read_json_object::<String, _>(BITCOIND_ADDR_PATH)
+            .ok(),
+        dogecoind_addr: test_env
+            .read_json_object::<String, _>(DOGECOIND_ADDR_PATH)
+            .ok(),
+        jaeger_addr: test_env
+            .read_json_object::<String, _>(JAEGER_ADDR_PATH)
+            .ok(),
+        socks_proxy: test_env
+            .read_json_object::<String, _>(SOCKS_PROXY_PATH)
+            .ok(),
+        hostname: Some(node.node_id.to_string()),
+        generate_ic_boundary_tls_cert: node.node_config.domain.clone(),
+    };
+
+    let guestos_settings = GuestOSSettings {
+        inject_ic_crypto: false,
+        inject_ic_state: false,
+        inject_ic_registry_local_store: false,
+        guestos_dev_settings,
+    };
+
+    // Assemble GuestOSConfig
+    Ok(GuestOSConfig {
+        config_version: CONFIG_VERSION.to_string(),
+        network_settings,
+        icos_settings,
+        guestos_settings,
+        guest_vm_type: GuestVMType::Default,
+        upgrade_config: GuestOSUpgradeConfig::default(),
+        trusted_execution_environment_config: None,
+        recovery_config: recovery_hash.map(|hash| RecoveryConfig {
+            recovery_hash: hash,
+        }),
+    })
 }
 
 fn node_to_config(node: &Node) -> NodeConfiguration {
