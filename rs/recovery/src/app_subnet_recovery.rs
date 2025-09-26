99ff84ed794fbed7cedeee3b2eb07828175a3ddf
Pierugo Pace <pierugo.pace@dfinity.org>
2025-09-26 08:54:36 +0000

    feat: let --wait-for-cup overwrite --upload-method

--- rs/recovery/src/app_subnet_recovery.rs
+++ rs/recovery/src/app_subnet_recovery.rs
@@ -308,13 +308,15 @@ impl RecoveryIterator<StepType, StepTypeIter> for AppSubnetRecovery {
             }
 
             StepType::WaitForCUP => {
-                if let Some(DataLocation::Remote(ip)) = self.params.upload_method {
-                    self.params.wait_for_cup_node = Some(ip);
-                } else {
-                    self.params.wait_for_cup_node = read_optional(
-                        &self.logger,
-                        "Enter IP of the node to be polled for the recovery CUP:",
-                    );
+                if self.params.wait_for_cup_node.is_none() {
+                    if let Some(DataLocation::Remote(ip)) = self.params.upload_method {
+                        self.params.wait_for_cup_node = Some(ip);
+                    } else {
+                        self.params.wait_for_cup_node = read_optional(
+                            &self.logger,
+                            "Enter IP of the node to be polled for the recovery CUP:",
+                        );
+                    }
                 }
             }
 
