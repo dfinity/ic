29c146123ffa16f2cf79769ba2bd4023ed61da12
Mihail Jianu <mihail.jianu@dfinity.org>
2025-09-30 09:57:36 +0000

    clippy

--- rs/tests/networking/canister_http_correctness_test.rs
+++ rs/tests/networking/canister_http_correctness_test.rs
@@ -1128,7 +1128,7 @@ fn test_invalid_ip(env: TestEnv) {
         &handlers,
         RemoteHttpRequest {
             request: UnvalidatedCanisterHttpRequestArgs {
-                // `2001:db8::1` is a reserved ipv6 address used in documentation and example source code. 
+                // `2001:db8::1` is a reserved ipv6 address used in documentation and example source code.
                 // See https://www.rfc-editor.org/rfc/rfc3849
                 url: "https://[2001:db8::1]".to_string(),
                 headers: vec![],
