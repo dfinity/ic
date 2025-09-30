4f1b0ca2601e07629b3492e9ed73c9d08367556e
Leo Eichhorn <leo.eichhorn@dfinity.org>
2025-09-30 14:50:47 +0000

    fix

--- rs/tests/consensus/upgrade/start_stop_test.rs
+++ rs/tests/consensus/upgrade/start_stop_test.rs
@@ -3,14 +3,11 @@ use std::time::Duration;
 use anyhow::Result;
 
 use ic_consensus_system_test_upgrade_common::{start_node, stop_node};
-use ic_consensus_system_test_utils::rw_message::install_nns_and_check_progress;
 use ic_registry_subnet_type::SubnetType;
 use ic_system_test_driver::driver::group::SystemTestGroup;
 use ic_system_test_driver::driver::ic::{InternetComputer, Subnet};
 use ic_system_test_driver::driver::test_env::TestEnv;
-use ic_system_test_driver::driver::test_env_api::{
-    GetFirstHealthyNodeSnapshot, HasTopologySnapshot,
-};
+use ic_system_test_driver::driver::test_env_api::GetFirstHealthyNodeSnapshot;
 use ic_system_test_driver::systest;
 use slog::info;
 
