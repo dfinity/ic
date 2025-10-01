dfinity/ic

commit 49faad717b7a3e3034ddff17b4c8768b60e5e65d

chore: Update Base Image Refs [2025-09-25-0804] (#6929)
Updating base container image references.

Run URL: https://github.com/dfinity/ic/actions/runs/18000948098

Co-authored-by: github-merge-queue <118344674+github-merge-queue@users.noreply.github.com>

ic-os/guestos/context/docker-base.dev
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/guestos-base-dev@sha256:c10e2a1f003d4a75e324956813d242edb6e404a48a5d41341b8dcca07a5a3538
+ghcr.io/dfinity/guestos-base-dev@sha256:d526fd54ec5d53263728401ee6b778569489fbabe00c8cd684623c87d89f91ec

ic-os/guestos/context/docker-base.prod
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/guestos-base@sha256:2814f8b040a3beb92f51b36ee65e925f69eca19c4b7cc7cdf9b247785b6f0593
+ghcr.io/dfinity/guestos-base@sha256:b9b14703cad0c8c50d46647242ea199e738214ba9b4d950a032e4e402d714c11

ic-os/hostos/context/docker-base.dev
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/hostos-base-dev@sha256:7c35f8ed302b89eb4dc3b11d9f2bb0ba020d5b30399cd0e7af83056f34339fa2
+ghcr.io/dfinity/hostos-base-dev@sha256:9ab2fac0784cc0c671dd7f8995ff616237a2c914d50e95a5668cc94f623b6f3f

ic-os/hostos/context/docker-base.prod
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/hostos-base@sha256:0c1a4edca2b6ed44398b0975c04955e943f85e3c66cd9c85e7f16ae7247b54fa
+ghcr.io/dfinity/hostos-base@sha256:3e4d7d3d7549b720aa460a05604152145f0368475eeb9a0956dd6447d90070b4

ic-os/setupos/context/docker-base.dev
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/setupos-base-dev@sha256:d069bdd1b864d65a56232b7d27a4923ab0ce2d952d27f7409b867df0d88cc752
+ghcr.io/dfinity/setupos-base-dev@sha256:8d64998a0573b405700a19975160056213d687a9a99254f293ac79fd6079aebe

ic-os/setupos/context/docker-base.prod
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/setupos-base@sha256:bddfb2c9f62f0525c8d212856fd5fc37185b233461343423e8d00b584dae58f5
+ghcr.io/dfinity/setupos-base@sha256:492ebd6209f8b5834aaf6f0905e7863528fb6e4a5c24bf2965fba72fdcfc30be

commit 8152ec6f54fb2aee4d5c955f6b63a50f03f7cf73

fix: revert base image upgrades 5833499 and 49faad7 (#7025)
49faad7 caused IC nodes that where killed ([`virsh
destroy`](https://www.libvirt.org/manpages/virsh.html#destroy)) and
restarted again to sometimes get a corrupted data partition. This caused
the
[systemd-fsck](https://www.freedesktop.org/software/systemd/man/latest/systemd-fsck@.service.html)
services on the GuestOS to fail for the data partitions:
`/dev/mapper/store-shared--crypto`, `/dev/mapper/store-shared--data` and
`/dev/mapper/var_crypt`. See:
```
root@localhost:~# systemctl | grep systemd-fsck
  systemd-fsck-root.service                                        loaded active     exited            File System Check on Root Device
  systemd-fsck@dev-mapper-store\x2dshared\x2d\x2dcrypto.service    loaded inactive   dead        start File System Check on /dev/mapper/store-shared--crypto
  systemd-fsck@dev-mapper-store\x2dshared\x2d\x2ddata.service      loaded inactive   dead        start File System Check on /dev/mapper/store-shared--data
  systemd-fsck@dev-mapper-var_crypt.service                        loaded inactive   dead        start File System Check on /dev/mapper/var_crypt
  systemd-fsck@dev-vda3.service                                    loaded active     exited            File System Check on /dev/vda3
  system-systemd\x2dfsck.slice                                     loaded active     active            Slice /system/systemd-fsck
  systemd-fsckd.socket                                             loaded active     listening         fsck to fsckd communication Socket
```
which meant those partitions would not get mounted.

It's unclear why 49faad7 is causing this flakiness. Probably some Ubuntu
update in

[`ic-os/guestos/context/Dockerfile.base`](https://github.com/dfinity/ic/blob/master/ic-os/guestos/context/Dockerfile.base)
is causing it.

We have to test if a new upgrade ([Container IC Base
Images](https://github.com/dfinity/ic/actions/workflows/container-base-images.yml))

ic-os/guestos/context/docker-base.dev
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/guestos-base-dev@sha256:5a778f5a7b104f6e47debeb65fb2493ff2759e13b8bc23c85950000f378896b5
+ghcr.io/dfinity/guestos-base-dev@sha256:c10e2a1f003d4a75e324956813d242edb6e404a48a5d41341b8dcca07a5a3538

ic-os/guestos/context/docker-base.prod
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/guestos-base@sha256:2f4bf813eccdcc01f194acbb904cd70731c6f7014879d0d832e40fffb5807195
+ghcr.io/dfinity/guestos-base@sha256:2814f8b040a3beb92f51b36ee65e925f69eca19c4b7cc7cdf9b247785b6f0593

ic-os/hostos/context/docker-base.dev
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/hostos-base-dev@sha256:06fb6c2ef9478588b6240bd850cf56f2de03cca3a4f04aaee84fc6881e192804
+ghcr.io/dfinity/hostos-base-dev@sha256:7c35f8ed302b89eb4dc3b11d9f2bb0ba020d5b30399cd0e7af83056f34339fa2

ic-os/hostos/context/docker-base.prod
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/hostos-base@sha256:6488933fcf16309a1bfbf6fecd5651c39856f86abd94ce76fdba9cc8400c0d3a
+ghcr.io/dfinity/hostos-base@sha256:0c1a4edca2b6ed44398b0975c04955e943f85e3c66cd9c85e7f16ae7247b54fa

ic-os/setupos/context/docker-base.dev
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/setupos-base-dev@sha256:f09dcc902735074986b999d424a51af3aee0965814639215b46a6616ecf68b34
+ghcr.io/dfinity/setupos-base-dev@sha256:d069bdd1b864d65a56232b7d27a4923ab0ce2d952d27f7409b867df0d88cc752

ic-os/setupos/context/docker-base.prod
@@ -1,1 +1,1 @@
-ghcr.io/dfinity/setupos-base@sha256:4c7469f2edfb76df13da2272c6997bb9baab8a5198ca43d4820369f74c477e73
+ghcr.io/dfinity/setupos-base@sha256:bddfb2c9f62f0525c8d212856fd5fc37185b233461343423e8d00b584dae58f5

rs/tests/node/BUILD.bazel
@@ -101,3 +101,16 @@ system_test(
         "@crate_index//:anyhow",
     ],
 )
+
+system_test(
+    name = "kill_start_test",
+    uses_guestos_img = True,
+    deps = [
+        # Keep sorted.
+        "//rs/registry/subnet_type",
+        "//rs/tests/consensus/upgrade:common",
+        "//rs/tests/driver:ic-system-test-driver",
+        "@crate_index//:anyhow",
+        "@crate_index//:slog",
+    ],
+)

rs/tests/node/Cargo.toml
@@ -15,6 +15,7 @@ ic-nns-constants = { path = "../../nns/constants" }
 ic-registry-canister-api = { path = "../../registry/canister/api" }
 ic-registry-subnet-type = { path = "../../registry/subnet_type" }
 ic_consensus_system_test_utils = { path = "../consensus/utils" }
+consensus-upgrade-system-tests = { path = "../consensus/upgrade" }
 ic-system-test-driver = { path = "../driver" }
 indoc = "1.0.9"
 k256 = { workspace = true }
@@ -41,3 +42,7 @@ path = "launch_single_guest.rs"
 [[bin]]
 name = "launch_single_host"
 path = "launch_single_host.rs"
+
+[[bin]]
+name = "ic-systest-kill-start"
+path = "kill_start_test.rs"

rs/tests/node/kill_start_test.rs
@@ -1,0 +1,36 @@
+// This is a regression test that checks if killing an IC node does not corrupt the filesystems of its data partitions.
+// The test deploys a single node IC, kills the node, waits a bit, and then starts the node again and checks if it comes back up healthy.
+use anyhow::Result;
+use ic_consensus_system_test_upgrade_common::{start_node, stop_node};
+use ic_registry_subnet_type::SubnetType;
+use ic_system_test_driver::driver::group::SystemTestGroup;
+use ic_system_test_driver::driver::ic::InternetComputer;
+use ic_system_test_driver::driver::test_env::TestEnv;
+use ic_system_test_driver::driver::test_env_api::GetFirstHealthyNodeSnapshot;
+use ic_system_test_driver::systest;
+use slog::info;
+use std::time::Duration;
+
+fn setup(env: TestEnv) {
+    InternetComputer::new()
+        .add_fast_single_node_subnet(SubnetType::System)
+        .setup_and_start(&env)
+        .expect("failed to setup IC under test");
+}
+
+fn test(env: TestEnv) {
+    let log = env.logger();
+    let node = env.get_first_healthy_system_node_snapshot();
+    stop_node(&log, &node);
+    info!(log, "Sleeping for 10 seconds...");
+    std::thread::sleep(Duration::from_secs(10));
+    start_node(&log, &node);
+}
+
+fn main() -> Result<()> {
+    SystemTestGroup::new()
+        .with_setup(setup)
+        .add_test(systest!(test))
+        .execute_from_args()?;
+    Ok(())
+}