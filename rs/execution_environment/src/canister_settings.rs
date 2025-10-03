ff640fdc81757ba8685b25918e8f3b6076aff6c9
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-10-03 13:49:52 +0000

    .

--- rs/execution_environment/src/canister_settings.rs
+++ rs/execution_environment/src/canister_settings.rs
@@ -133,13 +133,12 @@ impl TryFrom<CanisterSettingsArgs> for CanisterSettings {
             None => None,
         };
 
-        let log_size = match input.log_size {
-            Some(ls) => Some(NumBytes::new(
-                ls.0.to_u64()
-                    .ok_or(UpdateSettingsError::LogSizeOutOfRange { provided: ls })?,
-            )),
-            None => None,
-        };
+        let log_size = input.log_size.map(|x| {
+            NumBytes::new(
+                x.0.to_u64()
+                    .ok_or(UpdateSettingsError::LogSizeOutOfRange { provided: x })?,
+            )
+        });
 
         let wasm_memory_limit = match input.wasm_memory_limit {
             Some(limit) => {
