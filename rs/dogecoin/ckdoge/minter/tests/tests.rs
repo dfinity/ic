99d678c98725151565493cbcadc11eca8998d242
gregorydemay <gregory.demay@dfinity.org>
2025-10-03 16:45:15 +0200

    XC-495: happy path tests for get_doge_address

--- rs/dogecoin/ckdoge/minter/tests/tests.rs
+++ rs/dogecoin/ckdoge/minter/tests/tests.rs
@@ -1,7 +1,5 @@
 use candid::Principal;
-use ic_ckdoge_minter::candid_api::{
-    GetDogeAddressArgs, RetrieveDogeWithApprovalArgs, RetrieveDogeWithApprovalError,
-};
+use ic_ckdoge_minter::candid_api::{RetrieveDogeWithApprovalArgs, RetrieveDogeWithApprovalError};
 use ic_ckdoge_minter_test_utils::{
     DOGECOIN_ADDRESS_1, RETRIEVE_DOGE_MIN_AMOUNT, Setup, USER_PRINCIPAL, assert_trap,
 };
@@ -52,30 +50,77 @@ fn should_fail_withdrawal() {
     // and test failure when insufficient funds
 }
 
-#[test]
-fn should_fail_to_get_doge_address() {
-    let setup = Setup::default();
-    let minter = setup.minter();
+mod get_doge_address {
+    use candid::Principal;
+    use ic_ckdoge_minter::candid_api::GetDogeAddressArgs;
+    use ic_ckdoge_minter_test_utils::{Setup, USER_PRINCIPAL, assert_trap};
 
-    assert_trap(
-        minter.update_call_get_doge_address(
+    #[test]
+    fn should_fail_to_get_doge_address() {
+        let setup = Setup::default();
+        let minter = setup.minter();
+
+        assert_trap(
+            minter.update_call_get_doge_address(
+                USER_PRINCIPAL,
+                &GetDogeAddressArgs {
+                    owner: Some(Principal::anonymous()),
+                    subaccount: None,
+                },
+            ),
+            "owner must be non-anonymous",
+        );
+
+        assert_trap(
+            minter.update_call_get_doge_address(
+                Principal::anonymous(),
+                &GetDogeAddressArgs {
+                    owner: None,
+                    subaccount: None,
+                },
+            ),
+            "owner must be non-anonymous",
+        );
+    }
+
+    #[test]
+    fn should_get_doge_address() {
+        let setup = Setup::default();
+        let minter = setup.minter();
+
+        let address_from_caller = minter.get_doge_address(
             USER_PRINCIPAL,
-            &GetDogeAddressArgs {
-                owner: Some(Principal::anonymous()),
-                subaccount: None,
-            },
-        ),
-        "owner must be non-anonymous",
-    );
-
-    assert_trap(
-        minter.update_call_get_doge_address(
-            Principal::anonymous(),
             &GetDogeAddressArgs {
                 owner: None,
                 subaccount: None,
             },
-        ),
-        "owner must be non-anonymous",
-    );
+        );
+
+        let address_with_owner = minter.get_doge_address(
+            Principal::anonymous(),
+            &GetDogeAddressArgs {
+                owner: Some(USER_PRINCIPAL),
+                subaccount: None,
+            },
+        );
+
+        assert_eq!(address_from_caller, address_with_owner);
+        assert_eq!(
+            address_from_caller, "D95u8BQWiN21ER5LqrNwSk4WDVe25WHWHT",
+            "BUG: result of public key derivation changed!"
+        );
+
+        let address_with_subaccount = minter.get_doge_address(
+            USER_PRINCIPAL,
+            &GetDogeAddressArgs {
+                owner: None,
+                subaccount: Some([42_u8; 32]),
+            },
+        );
+        assert_ne!(address_from_caller, address_with_subaccount);
+        assert_eq!(
+            address_with_subaccount, "DPZ2c7wS53i9nGrMh25iCZABdPs718NRA9",
+            "BUG: result of public key derivation changed!"
+        );
+    }
 }
