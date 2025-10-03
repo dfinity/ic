612a647ca7ce836ecab9d4cb4fcf94c93510c35b
Andrew Battat <andrew.battat@dfinity.org>
2025-10-03 16:55:15 +0000

    Fix domain creation retry logic

--- rs/ic_os/os_tools/guest_vm_runner/src/main.rs
+++ rs/ic_os/os_tools/guest_vm_runner/src/main.rs
@@ -26,7 +26,6 @@ use tokio::time::sleep;
 use tokio_util::sync::CancellationToken;
 use virt::connect::Connect;
 use virt::domain::Domain;
-use virt::error::{ErrorDomain, ErrorNumber};
 use virt::sys::{VIR_DOMAIN_DESTROY_GRACEFUL, VIR_DOMAIN_NONE, VIR_DOMAIN_RUNNING};
 
 mod boot_args;
@@ -142,21 +141,19 @@ impl VirtualMachine {
         direct_boot: Option<DirectBoot>,
         vm_domain_name: &str,
     ) -> Result<Self> {
-        let mut retries = 3;
+        let mut retries = 10i;
         let domain = loop {
             let domain_result = Domain::create_xml(libvirt_connect, xml_config, VIR_DOMAIN_NONE);
             match domain_result {
                 Ok(domain) => break domain,
-                Err(e)
-                    if retries > 0
-                        && e.code() == ErrorNumber::OperationInvalid
-                        && e.domain() == ErrorDomain::Domain =>
-                {
+                Err(e) if retries > 0 => {
+                    println!("Domain creation failed, retrying: {}", e);
                     Self::try_destroy_existing_vm(libvirt_connect, vm_domain_name);
+                    sleep(Duration::from_secs(3)); // Wait to give libvirt a chance to clean up the domain.
                     retries -= 1;
                     continue;
                 }
-                err => err.context("Failed to create domain")?,
+                err => err.context("Failed to create domain after retries")?,
             };
         };
         Ok(Self {
@@ -170,10 +167,13 @@ impl VirtualMachine {
 
     fn try_destroy_existing_vm(libvirt_connect: &Connect, vm_domain_name: &str) {
         println!("Attempting to destroy existing '{vm_domain_name}' domain");
-        if let Err(e) = Domain::lookup_by_name(libvirt_connect, vm_domain_name)
-            .and_then(|existing| existing.destroy_flags(VIR_DOMAIN_DESTROY_GRACEFUL))
-        {
-            eprintln!("Failed to destroy existing domain: {e}");
+        if let Ok(existing_domain) = Domain::lookup_by_name(libvirt_connect, vm_domain_name) {
+            match existing_domain.destroy_flags(VIR_DOMAIN_DESTROY_GRACEFUL) {
+                Ok(_) => println!("Successfully destroyed existing domain"),
+                Err(e) => eprintln!("Failed to destroy existing domain: {e}"),
+            }
+        } else {
+            println!("No existing domain found to destroy");
         }
     }
 
