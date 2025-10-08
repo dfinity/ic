854a0997f3ac88076e7d048a9cb6a4ef819e9fad
ShahriarJavidi <shahriar.javidi@dfinity.org>
2025-10-08 09:02:28 +0000

    fix: change the return type of  to an optional result

--- rs/nns/governance/api/src/types.rs
+++ rs/nns/governance/api/src/types.rs
@@ -4399,9 +4399,28 @@ pub struct GetNeuronIndexRequest {
     page_size: Option<u32>,
 }
 
-#[derive(
-    candid::CandidType, candid::Deserialize, serde::Serialize, Debug, Default, Clone, PartialEq,
-)]
-pub struct GetNeuronIndexResponse {
-    neurons: Vec<Neuron>,
+pub mod neuron_index {
+    use super::{GovernanceError, NeuronInfo};
+
+    #[derive(
+        candid::CandidType, candid::Deserialize, serde::Serialize, Debug, Default, Clone, PartialEq,
+    )]
+    pub struct NeuronIndex {
+        neurons: Vec<NeuronInfo>,
+    }
+
+    #[derive(
+        candid::CandidType, candid::Deserialize, serde::Serialize, Debug, Clone, PartialEq,
+    )]
+    pub enum GetNeuronIndexResult {
+        Ok(NeuronIndex),
+        Err(GovernanceError),
+    }
+
+    #[derive(
+        candid::CandidType, candid::Deserialize, serde::Serialize, Debug, Default, Clone, PartialEq,
+    )]
+    pub struct GetNeuronIndexResponse {
+        result: Option<GetNeuronIndexResult>,
+    }
 }
