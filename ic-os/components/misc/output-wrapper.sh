8273992faac550d95c6cea9fadfd2f01aeb07d37
Andrew Battat <andrew.battat@dfinity.org>
2025-10-09 10:35:58 +0000

    Fix output-wrapper

--- ic-os/components/misc/output-wrapper.sh
+++ ic-os/components/misc/output-wrapper.sh
@@ -14,14 +14,17 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-if [ $# -eq 2 ]; then
-    # Single target: <target> <script>
-    TEE_TARGETS="$1"
-    SCRIPT_ARGS=("$2")
-elif [ $# -ge 3 ]; then
-    # Two targets: <target1> <target2> <script> [args...]
-    TEE_TARGETS="$1 $2"
-    SCRIPT_ARGS=("${@:3}")
+if [ $# -ge 2 ]; then
+    # Check if second argument is a terminal device
+    if [[ "$2" =~ ^/dev/tty ]]; then
+        # Two targets: <target1> <target2> <script> [args...]
+        TEE_TARGETS="$1 $2"
+        SCRIPT_ARGS=("${@:3}")
+    else
+        # Single target: <target> <script> [args...]
+        TEE_TARGETS="$1"
+        SCRIPT_ARGS=("${@:2}")
+    fi
 else
     echo "Error: Invalid number of arguments"
     exit 1
