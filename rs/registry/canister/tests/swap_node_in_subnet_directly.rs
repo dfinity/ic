21371c9c4d974520381719a7e029b2d5730d4638
Nikola Milosavljevic <73236646+NikolaMilosa@users.noreply.github.com>
2025-10-10 01:07:05 +0200

    feat: swapping functionality implementation (#7172)
    This PR adds the functionality for swapping active nodes in a subnet
    owned by single node operator. A node operator can swap one of its
    assigned nodes in the same data center with an unassigned one.
    
    At the moment this feature is completely turned off and won't be enabled
    until system tests are added.

--- rs/registry/canister/tests/swap_node_in_subnet_directly.rs
+++ rs/registry/canister/tests/swap_node_in_subnet_directly.rs
@@ -4,7 +4,9 @@ use common::test_helpers::install_registry_canister_with_payload_builder;
 use ic_base_types::PrincipalId;
 use ic_config::crypto::CryptoConfig;
 use ic_crypto_node_key_generation::generate_node_keys_once;
-use ic_nervous_system_integration_tests::pocket_ic_helpers::nns::registry::swap_node_in_subnet_directly;
+use ic_nervous_system_integration_tests::pocket_ic_helpers::nns::registry::{
+    get_value, swap_node_in_subnet_directly,
+};
 use ic_nns_test_utils::registry::{
     create_subnet_threshold_signing_pubkey_and_cup_mutations,
     invariant_compliant_mutation_as_atomic_req,
@@ -290,12 +292,14 @@ async fn subnet_rate_limited() {
 
     assert!(response.is_ok(), "Expected ok but got {response:?}");
 
-    // Make a call again which should fail because of rate limiting
+    // Make a call again which should fail because of rate limiting.
+    // Now old and new nodes are swapped in the payload because they
+    // were swaped in the previous call to the canister.
     let response = swap_node_in_subnet_directly(
         &pocket_ic,
         SwapNodeInSubnetDirectlyPayload {
-            new_node_id: Some(new_node_id.get()),
-            old_node_id: Some(old_node_id.get()),
+            new_node_id: Some(old_node_id.get()),
+            old_node_id: Some(new_node_id.get()),
         },
         operator_id,
     )
@@ -314,8 +318,8 @@ async fn subnet_rate_limited() {
     let response = swap_node_in_subnet_directly(
         &pocket_ic,
         SwapNodeInSubnetDirectlyPayload {
-            new_node_id: Some(new_node_id.get()),
-            old_node_id: Some(old_node_id.get()),
+            new_node_id: Some(old_node_id.get()),
+            old_node_id: Some(new_node_id.get()),
         },
         different_sender,
     )
@@ -376,12 +380,14 @@ async fn subnet_rate_limit_passed() {
 
     assert!(response.is_ok(), "Expected ok but got {response:?}");
 
-    // Make a call again which should fail because of rate limiting
+    // Make a call again which should fail because of rate limiting.
+    // Old and new nodes are now changed because they were swapped
+    // in a previous call.
     let response = swap_node_in_subnet_directly(
         &pocket_ic,
         SwapNodeInSubnetDirectlyPayload {
-            new_node_id: Some(new_node_id.get()),
-            old_node_id: Some(old_node_id.get()),
+            new_node_id: Some(old_node_id.get()),
+            old_node_id: Some(new_node_id.get()),
         },
         operator_id,
     )
@@ -405,8 +411,8 @@ async fn subnet_rate_limit_passed() {
     let response = swap_node_in_subnet_directly(
         &pocket_ic,
         SwapNodeInSubnetDirectlyPayload {
-            new_node_id: Some(new_node_id.get()),
-            old_node_id: Some(old_node_id.get()),
+            new_node_id: Some(old_node_id.get()),
+            old_node_id: Some(new_node_id.get()),
         },
         operator_id,
     )
@@ -433,8 +439,8 @@ async fn subnet_rate_limit_passed() {
     let response = swap_node_in_subnet_directly(
         &pocket_ic,
         SwapNodeInSubnetDirectlyPayload {
-            new_node_id: Some(new_node_id.get()),
-            old_node_id: Some(old_node_id.get()),
+            new_node_id: Some(old_node_id.get()),
+            old_node_id: Some(new_node_id.get()),
         },
         operator_id,
     )
@@ -488,5 +494,25 @@ async fn e2e_valid_swap() {
     )
     .await;
 
-    assert!(response.is_ok(), "Expected ok but got {response:?}")
+    assert!(response.is_ok(), "Expected ok but got {response:?}");
+
+    let response = get_value(&pocket_ic, make_subnet_record_key(subnet_id), None)
+        .await
+        .unwrap();
+
+    let content = match response.content.unwrap() {
+        ic_registry_transport::pb::v1::high_capacity_registry_get_value_response::Content::Value(items) => items,
+        ic_registry_transport::pb::v1::high_capacity_registry_get_value_response::Content::LargeValueChunkKeys(_) => panic!("Didn't expect large value chunk keys"),
+    };
+
+    let subnet_record = SubnetRecord::decode(content.as_slice()).unwrap();
+
+    let members: Vec<_> = subnet_record
+        .membership
+        .into_iter()
+        .map(|n| NodeId::new(PrincipalId::try_from(n).unwrap()))
+        .collect();
+
+    assert!(members.contains(&new_node_id));
+    assert!(!members.contains(&old_node_id));
 }
