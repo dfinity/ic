24104cd3f6cc489ef7176d2bf7f12af704627e10
stiegerc <christian.stieger@dfinity.org>
2025-10-13 16:38:16 +0200

    fix: Removes the bad subnet ID from the State Machine Tests split function. (#7133)
    The `seed` generates a subnet ID, specifying it manually causes issues.
    
    This line was used during debugging and is not supposed to be there.
    Since there is no test using the `split` function yet, it went in
    unnoticed.

--- rs/state_machine_tests/src/lib.rs
+++ rs/state_machine_tests/src/lib.rs
@@ -3133,7 +3133,6 @@ impl StateMachine {
             )
             .with_subnet_size(self.nodes.len())
             .with_subnet_seed(seed)
-            .with_subnet_id(ic_test_utilities_types::ids::subnet_test_id(1221))
             .with_registry_data_provider(self.registry_data_provider.clone())
             .build_with_subnets(
                 (*self.pocket_xnet.read().unwrap())
