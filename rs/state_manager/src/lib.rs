04d0c38bc35b8386be2e9c8cec8db0339656a04a
Shuo Wang <shuo.wang94@gmail.com>
2025-10-15 16:03:14 -0700

    improve metrics

--- rs/state_manager/src/lib.rs
+++ rs/state_manager/src/lib.rs
@@ -642,7 +642,7 @@ impl StateSyncMetrics {
 
         let step_duration = metrics_registry.histogram_vec(
             "state_sync_step_duration_seconds",
-            "Duration of state sync sub-steps in seconds indexed by step ('hardlink_files', 'copy_chunks', 'fetch', 'state_sync_make_checkpoint', 'preallocate', 'preallocate_directories', 'preallocate_files')",
+            "Duration of state sync sub-steps in seconds indexed by step ('hardlink_files', 'copy_chunks', 'fetch', 'state_sync_make_checkpoint', 'preallocate_directories', 'preallocate_files')",
             // 0.1s, 0.2s, 0.5s, 1s, 2s, 5s, …, 1000s, 2000s, 5000s
             decimal_buckets(-1, 3),
             &["step"],
@@ -654,7 +654,6 @@ impl StateSyncMetrics {
             LABEL_COPY_CHUNKS,
             LABEL_FETCH,
             LABEL_STATE_SYNC_MAKE_CHECKPOINT,
-            LABEL_PREALLOCATE,
             LABEL_PREALLOCATE_DIRECTORIES,
             LABEL_PREALLOCATE_FILES,
         ] {
