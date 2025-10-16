f088bb609bccd9ea2ab3ef25ae991bc710ba2fdf
Leon Tan <leon.tan@dfinity.org>
2025-10-16 12:23:30 +0000

    Make many structs in chunks_to_download named

--- rs/p2p/state_sync_manager/src/ongoing/chunks_to_download.rs
+++ rs/p2p/state_sync_manager/src/ongoing/chunks_to_download.rs
@@ -4,11 +4,18 @@ use ic_logger::{ReplicaLogger, info};
 use ic_types::NodeId;
 use std::collections::{BTreeMap, btree_map::Entry};
 
-pub(crate) struct ChunksToDownload(BTreeMap<XorDistance, (ChunkId, bool)>, ReplicaLogger);
+/// Maintains a list of chunks that are still be downloaded
+pub(crate) struct ChunksToDownload {
+    log: ReplicaLogger,
+    chunks: BTreeMap<XorDistance, ChunkToDownload>,
+}
 
 impl ChunksToDownload {
     pub(crate) fn new(log: &ReplicaLogger) -> Self {
-        Self(BTreeMap::new(), log.clone())
+        Self {
+            log: log.clone(),
+            chunks: BTreeMap::new(),
+        }
     }
 
     // Add chunks to the chunks to download list
@@ -19,14 +26,17 @@ impl ChunksToDownload {
         chunks: impl Iterator<Item = ChunkId>,
     ) -> usize {
         let mut added = 0;
-        for chunk in chunks {
-            let xor_distance = XorDistance::new(node_id, artifact_id.clone(), chunk);
+        for chunk_id in chunks {
+            let xor_distance = XorDistance::new(node_id, artifact_id.clone(), chunk_id);
 
-            if let Entry::Vacant(entry) = self.0.entry(xor_distance) {
-                entry.insert((chunk, false));
+            if let Entry::Vacant(entry) = self.chunks.entry(xor_distance) {
+                entry.insert(ChunkToDownload {
+                    id: chunk_id,
+                    downloading: false,
+                });
                 added += 1;
             } else {
-                info!(self.1, "STATE_SYNC: Attempt to double insert a chunk");
+                info!(self.log, "STATE_SYNC: Attempt to double insert a chunk");
             }
         }
 
@@ -34,33 +44,42 @@ impl ChunksToDownload {
     }
 
     pub(crate) fn next_chunk_to_download(&mut self) -> Option<ChunkId> {
-        let next_chunk = self
-            .0
+        let (_, next_chunk) = self
+            .chunks
             .iter_mut()
-            .find(|(_, (_, downloading))| !downloading)?;
+            .find(|(_, chunk)| !chunk.downloading)?;
 
-        next_chunk.1.1 = true;
-        Some(next_chunk.1.0)
+        next_chunk.downloading = true;
+        Some(next_chunk.id)
     }
 
     pub(crate) fn download_finished(&mut self, chunk_id: ChunkId) {
-        if let Some(key) = self.0.iter().find(|(_, (chunk, _))| *chunk == chunk_id) {
-            let key = key.0.clone();
-            self.0.remove(&key);
+        if let Some((key, _)) = self.chunks.iter().find(|(_, chunk)| chunk.id == chunk_id) {
+            let key = key.clone();
+            self.chunks.remove(&key);
         }
     }
 
     pub(crate) fn download_failed(&mut self, chunk_id: ChunkId) {
-        if let Some(next_chunk) = self.0.iter_mut().find(|(_, (chunk, _))| chunk == &chunk_id) {
-            next_chunk.1.1 = false;
+        if let Some((_, failed_chunk)) = self
+            .chunks
+            .iter_mut()
+            .find(|(_, chunk)| chunk.id == chunk_id)
+        {
+            failed_chunk.downloading = false;
         }
     }
 
     pub(crate) fn next_xor_distance(&self) -> Option<XorDistance> {
-        self.0.first_key_value().map(|(key, _)| key.clone())
+        self.chunks.first_key_value().map(|(key, _)| key.clone())
     }
 
     pub(crate) fn num_entries(&self) -> usize {
-        self.0.len()
+        self.chunks.len()
     }
+}
+
+struct ChunkToDownload {
+    id: ChunkId,
+    downloading: bool,
 }
