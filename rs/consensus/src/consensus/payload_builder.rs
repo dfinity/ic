6fac107b4bb351e51193e062dcbcc1625c3b27d9
Kamil Popielarz <kamil.popielarz@dfinity.org>
2025-10-17 14:29:36 +0000

    mv

--- rs/consensus/src/consensus/payload_builder.rs
+++ rs/consensus/src/consensus/payload_builder.rs
@@ -220,17 +220,16 @@ pub(crate) mod test {
     use ic_consensus_mocks::{Dependencies, dependencies};
     use ic_https_outcalls_consensus::test_utils::FakeCanisterHttpPayloadBuilder;
     use ic_interfaces_mocks::messaging::MockXNetPayloadBuilder;
-    use ic_interfaces_mocks::payload_builder::MockSelfValidatingPayloadBuilder;
+    use ic_interfaces_mocks::payload_builder::{
+        MockIngressSelector, MockSelfValidatingPayloadBuilder,
+    };
     use ic_logger::replica_logger::no_op_logger;
     use ic_test_utilities::{
         ingress_selector::FakeIngressSelector,
         self_validating_payload_builder::FakeSelfValidatingPayloadBuilder,
         xnet_payload_builder::FakeXNetPayloadBuilder,
     };
-    use ic_test_utilities_consensus::{
-        batch::{MockBatchPayloadBuilder, MockIngressSelector},
-        fake::Fake,
-    };
+    use ic_test_utilities_consensus::{batch::MockBatchPayloadBuilder, fake::Fake};
     use ic_test_utilities_registry::SubnetRecordBuilder;
     use ic_test_utilities_types::{
         ids::{node_test_id, subnet_test_id},
--- rs/interfaces/mocks/src/payload_builder.rs
+++ rs/interfaces/mocks/src/payload_builder.rs
@@ -1,5 +1,8 @@
-use ic_interfaces::self_validating_payload::SelfValidatingPayloadBuilder;
-use ic_types::{NumBytes, batch::ValidationContext};
+use ic_interfaces::{
+    ingress_manager::IngressSelector, self_validating_payload::SelfValidatingPayloadBuilder,
+    validation::ValidationResult,
+};
+use ic_types::{Height, NumBytes, batch::ValidationContext};
 
 use mockall::mock;
 
@@ -25,4 +28,32 @@ mock! {
             ic_interfaces::self_validating_payload::SelfValidatingPayloadValidationError,
         >;
     }
+}
+
+mock! {
+   pub IngressSelector {}
+
+   impl IngressSelector for IngressSelector {
+     fn get_ingress_payload(
+         &self,
+         past_ingress: &dyn ic_interfaces::ingress_manager::IngressSetQuery,
+         context: &ValidationContext,
+         byte_limit: NumBytes,
+     ) -> ic_types::batch::IngressPayload;
+
+     fn validate_ingress_payload(
+         &self,
+         payload: &ic_types::batch::IngressPayload,
+         past_ingress: &dyn ic_interfaces::ingress_manager::IngressSetQuery,
+         context: &ValidationContext,
+     ) -> ValidationResult<ic_interfaces::ingress_manager::IngressPayloadValidationError>;
+
+     fn filter_past_payloads(
+         &self,
+         past_payloads: &[(Height, ic_types::Time, ic_types::consensus::Payload)],
+         context: &ValidationContext,
+     ) -> ic_types::ingress::IngressSets;
+
+     fn request_purge_finalized_messages(&self, message_ids: Vec<ic_types::artifact::IngressMessageId>);
+   }
 }
