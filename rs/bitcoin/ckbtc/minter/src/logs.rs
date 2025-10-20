835c14420866d9986832f25f9b688608e434c204
gregorydemay <gregory.demay@dfinity.org>
2025-10-20 10:51:25 +0200

    XC-495: add canlog as dep

--- rs/bitcoin/ckbtc/minter/src/logs.rs
+++ rs/bitcoin/ckbtc/minter/src/logs.rs
@@ -1,7 +1,25 @@
+use candid::Deserialize;
+use canlog::{GetLogFilter, LogFilter};
+use canlog_derive::LogPriorityLevels;
 use ic_canister_log::declare_log_buffer;
+use serde::Serialize;
 
 // High-priority messages.
 declare_log_buffer!(name = P0, capacity = 1000);
 
 // Low-priority info messages.
 declare_log_buffer!(name = P1, capacity = 1000);
+
+#[derive(LogPriorityLevels, Serialize, Deserialize, PartialEq, Debug, Copy, Clone)]
+pub enum Priority {
+    #[log_level(capacity = 1000, name = "INFO")]
+    Info,
+    #[log_level(capacity = 1000, name = "DEBUG")]
+    Debug,
+}
+
+impl GetLogFilter for Priority {
+    fn get_log_filter() -> LogFilter {
+        LogFilter::ShowAll
+    }
+}
