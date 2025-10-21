a05760faff05b0015cf0c9661a6a9cd978305982
Paul H. Liu <paul@dfinity.org>
2025-10-21 02:20:54 -0700

    Change to simple_http since minreq doesn't support IPv6

--- rs/bitcoin/adapter/test_utils/src/rpc_client.rs
+++ rs/bitcoin/adapter/test_utils/src/rpc_client.rs
@@ -238,12 +238,12 @@ impl<T: RpcClientType> RpcClient<T> {
     /// Create a RPC client using the given [Network], url and [Auth].
     pub fn new(network: T, url: &str, auth: Auth) -> Result<Self> {
         let (user, pass) = auth.get_user_pass()?;
-        let transport = jsonrpc::minreq_http::Builder::new()
+        let transport = jsonrpc::simple_http::Builder::new()
             .timeout(std::time::Duration::from_secs(60))
             .url(url)
             .map_err(|e| RpcError::JsonRpc(e.into()))?;
         let transport = if let Some(user) = user {
-            transport.basic_auth(user, pass)
+            transport.auth(user, pass)
         } else {
             transport
         };
