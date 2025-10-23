3d35648e2baef7b8a29588ae14c23412b4b37d03
Andrew Battat <andrew.battat@dfinity.org>
2025-10-23 17:31:11 +0000

    Use /sbin/nologin for limited console user, then launch limited-console-shell program

--- cpp/infogetty-cpp/infogetty.cc
+++ cpp/infogetty-cpp/infogetty.cc
@@ -190,18 +190,31 @@ main(int argc, char** argv)
     sigaction(SIGQUIT, &saved_quit, NULL);
     sigaction(SIGINT, &saved_int, NULL);
 
-    // Drop into shell. We do this via the "login" binary which establishes
-    // everything nicely to have a login session.
-    // When the shell terminates, systemd will respawn us and we will take
-    // over the terminal again.
-    const char* cmdline[] = {
-        "/usr/bin/login",
-        "-f",
-        "-p",
-        opts.login_user.c_str(),
-        0
-    };
-    check_panic_errno(::execve(cmdline[0], const_cast<char**>(cmdline), environ), opts.tty_dev, "execve login failed");
+    // For limited-console user, run a program directly without login
+    // For other users (like root), use the login process
+    if (opts.login_user == "limited-console") {
+        // Use su to run the program as the limited-console user
+        const char* cmdline[] = {
+            "/bin/su",
+            "-s", "/opt/ic/bin/limited-console-shell",
+            "limited-console",
+            0
+        };
+        check_panic_errno(::execve(cmdline[0], const_cast<char**>(cmdline), environ), opts.tty_dev, "execve limited-console-shell failed");
+    } else {
+        // Drop into shell. We do this via the "login" binary which establishes
+        // everything nicely to have a login session.
+        // When the shell terminates, systemd will respawn us and we will take
+        // over the terminal again.
+        const char* cmdline[] = {
+            "/usr/bin/login",
+            "-f",
+            "-p",
+            opts.login_user.c_str(),
+            0
+        };
+        check_panic_errno(::execve(cmdline[0], const_cast<char**>(cmdline), environ), opts.tty_dev, "execve login failed");
+    }
 
     return 1;
 }
