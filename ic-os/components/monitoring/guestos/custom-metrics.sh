4162a2c9bd81327d43c3794e3171be17270ac8a9
Ruediger Birkner <ruediger.birkner@dfinity.org>
2025-10-23 13:36:56 +0000

    add sev_active as metric

--- ic-os/components/monitoring/guestos/custom-metrics.sh
+++ ic-os/components/monitoring/guestos/custom-metrics.sh
@@ -58,22 +58,39 @@ function update_node_operator_private_key_metric() {
         "gauge"
 }
 
-function update_tee_enabled_metric() {
-    tee_enabled_str=$(get_config_value '.icos_settings.enable_trusted_execution_environment')
+function update_tee_metrics() {
+    # Define the metric metadata once.
+    local metric_family="guestos_tee"
+    clear_metrics "$metric_family"
 
-    if [[ "${tee_enabled_str}" == "true" ]]; then
-        tee_enabled=1
-    elif [[ "${tee_enabled_str}" == "false" ]]; then
-        tee_enabled=0
+    tee_configuration_str=$(get_config_value '.icos_settings.enable_trusted_execution_environment')
+
+    if [[ "${tee_configuration_str}" == "true" ]]; then
+        tee_configured=1
+    elif [[ "${tee_configuration_str}" == "false" ]]; then
+        tee_configured=0
     else
         # Default/fallback if the key is missing or empty
-        tee_enabled=0
+        tee_configured=0
     fi
 
-    write_metric "guestos_tee_enabled" \
-        "${tee_enabled}" \
+    write_metric_header "$metric_family" \
+        "guestos_tee_configured" \
+        "Indicates whether the virtual machine is configured to run in a Trusted Execution Environment (1 = TEE configured, 0 = TEE not configured)" \
+        "gauge"
+    append_metric "$metric_family" "guestos_tee_configured" "" "${tee_configured}"
+
+    if /opt/ic/bin/sev_active; then
+        tee_active=1
+    else
+        tee_active=0
+    fi
+
+    write_metric_header "$metric_family" \
+        "guestos_tee_active" \
         "Indicates whether the virtual machine is running in a Trusted Execution Environment (1 = TEE enabled, 0 = not in TEE)" \
         "gauge"
+    append_metric "$metric_family" "guestos_tee_active" "" "${tee_active}"
 }
 
 function main() {
@@ -81,7 +98,7 @@ function main() {
     update_guestos_boot_action_metric
     update_config_version_metric
     update_node_operator_private_key_metric
-    update_tee_enabled_metric
+    update_tee_metrics
 }
 
 main
