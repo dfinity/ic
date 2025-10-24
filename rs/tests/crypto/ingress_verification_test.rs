f23f7a74b93f7d536b12310606c58a9d33d4d91a
Jack Lloyd <jack.lloyd@dfinity.org>
2025-10-24 21:17:46 +0000

    Delegation with target tests (incomplete)

--- rs/tests/crypto/ingress_verification_test.rs
+++ rs/tests/crypto/ingress_verification_test.rs
@@ -13,12 +13,12 @@ use ic_system_test_driver::systest;
 use ic_system_test_driver::util::{
     UniversalCanister, block_on, expiry_time, random_ed25519_identity, sign_query, sign_update,
 };
-use ic_types::Time;
 use ic_types::crypto::SignedBytesWithoutDomainSeparator;
 use ic_types::messages::{
     Blob, Delegation, HttpCallContent, HttpCanisterUpdate, HttpQueryContent, HttpRequestEnvelope,
     HttpUserQuery, SignedDelegation,
 };
+use ic_types::{CanisterId, PrincipalId, Time};
 use rand::{CryptoRng, Rng};
 use reqwest::{StatusCode, Url};
 use slog::{debug, info};
@@ -26,7 +26,11 @@ use slog::{debug, info};
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_setup(setup)
-        .add_parallel(SystemTestSubGroup::new().add_test(systest!(requests_with_delegations)))
+        .add_parallel(
+            SystemTestSubGroup::new()
+                .add_test(systest!(requests_with_delegations))
+                .add_test(systest!(requests_with_delegations_with_targets)),
+        )
         .execute_from_args()?;
     Ok(())
 }
@@ -154,21 +158,7 @@ pub fn requests_with_delegations(env: TestEnv) {
                     identities.push(GenericIdentity::new(id_type, rng));
                 }
 
-                let delegation_expiry =
-                    Time::from_nanos_since_unix_epoch(expiry_time().as_nanos() as u64);
-
-                let mut delegations = Vec::with_capacity(delegation_count);
-
-                if delegation_count > 0 {
-                    for i in 1..=delegation_count {
-                        let delegation =
-                            Delegation::new(identities[i].public_key(), delegation_expiry);
-
-                        let signed_delegation = sign_delegation(delegation, &identities[i - 1]);
-
-                        delegations.push(signed_delegation);
-                    }
-                }
+                let delegations = create_delegations(&identities);
 
                 let sender = &identities[0];
                 let signer = &identities[identities.len() - 1];
@@ -187,6 +177,148 @@ pub fn requests_with_delegations(env: TestEnv) {
             }
         }
     });
+}
+
+pub fn requests_with_delegations_with_targets(env: TestEnv) {
+    let logger = env.logger();
+    let node = env.get_first_healthy_node_snapshot();
+    let agent = node.build_default_agent();
+    let rng = &mut reproducible_rng();
+    block_on({
+        async move {
+            let node_url = node.get_public_url();
+            debug!(logger, "Selected replica"; "url" => format!("{}", node_url));
+
+            let canister =
+                UniversalCanister::new_with_retries(&agent, node.effective_canister_id(), &logger)
+                    .await;
+
+            debug!(
+                logger,
+                "Installed Universal Canister";
+                "canister_id" => format!("{:?}", canister.canister_id())
+            );
+
+            let canister_id = canister.canister_id();
+
+            let test_info = TestInformation {
+                url: node_url,
+                canister_id,
+            };
+
+            let mut test_delegation_with_targets = async |targets: &[Vec<Principal>]| -> (StatusCode, StatusCode) {
+                let delegation_count = targets.len();
+
+                let mut identities = Vec::with_capacity(delegation_count + 1);
+
+                for _ in 0..(delegation_count + 1) {
+                    let id_type = GenericIdentityType::random(rng);
+                    identities.push(GenericIdentity::new(id_type, rng));
+                }
+
+                let delegations = create_delegations_with_targets(&identities, &targets);
+
+                let sender = &identities[0];
+                let signer = &identities[identities.len() - 1];
+
+                let query_result = query_delegation(&test_info, sender, signer, &delegations).await;
+                let update_result =
+                    update_delegation(&test_info, sender, signer, &delegations).await;
+
+                (query_result, update_result)
+            };
+
+            let accepted = (StatusCode::from_u16(200).unwrap(), StatusCode::from_u16(202).unwrap());
+
+            // Test Scenario:
+            //
+            // Two delegations each with a singleton target containing the requested canister ID;
+            assert_eq!(test_delegation_with_targets(&[vec![canister_id], vec![canister_id]]).await,
+                       accepted);
+
+            // Test Scenario:
+            //
+            // Delegation targets containing the requested canister ID multiple times;
+            assert_eq!(test_delegation_with_targets(&[vec![canister_id, canister_id]]).await,
+                       accepted);
+
+            // Test Scenario:
+            //
+            // One delegation containing a singleton target containing the requested
+            // canister ID and one delegation containing no target restriction (for both
+            // ordering of these two delegations);
+            assert_eq!(test_delegation_with_targets(&[vec![canister_id], vec![]]).await, accepted);
+            assert_eq!(test_delegation_with_targets(&[vec![], vec![canister_id]]).await, accepted);
+
+            // Test Scenario:
+            //
+            // Up to 1000 different targets (incl. arbitrary principals) containing the requested canister ID;
+
+            // TODO
+            // test_delegation_with_targets(&[vec![random_principals_including(canister_id, 1000)]]);
+
+            // TODO
+            // with the mgmt canister principal as the target for mgmt canister calls.
+        }
+    });
+}
+
+fn create_delegations(identities: &[GenericIdentity]) -> Vec<SignedDelegation> {
+    let delegation_expiry = Time::from_nanos_since_unix_epoch(expiry_time().as_nanos() as u64);
+
+    let delegation_count = identities.len() - 1;
+    let mut delegations = Vec::with_capacity(delegation_count);
+
+    if delegation_count > 0 {
+        for i in 1..=delegation_count {
+            let delegation = Delegation::new(identities[i].public_key(), delegation_expiry);
+
+            let signed_delegation = sign_delegation(delegation, &identities[i - 1]);
+
+            delegations.push(signed_delegation);
+        }
+    }
+
+    delegations
+}
+
+fn canister_id_from_principal(p: &Principal) -> CanisterId {
+    CanisterId::unchecked_from_principal(PrincipalId::from(*p))
+}
+
+fn create_delegations_with_targets(
+    identities: &[GenericIdentity],
+    targets: &[Vec<Principal>],
+) -> Vec<SignedDelegation> {
+    let delegation_expiry = Time::from_nanos_since_unix_epoch(expiry_time().as_nanos() as u64);
+
+    let delegation_count = identities.len() - 1;
+    let mut delegations = Vec::with_capacity(delegation_count);
+
+    if delegation_count > 0 {
+        for i in 1..=delegation_count {
+            let delegation = if targets[i - 1].is_empty() {
+                Delegation::new(
+                    identities[i].public_key(),
+                    delegation_expiry)
+            } else {
+                Delegation::new_with_targets(
+                    identities[i].public_key(),
+                    delegation_expiry,
+                    targets[i - 1]
+                        .iter()
+                        .map(canister_id_from_principal)
+                        .collect::<Vec<CanisterId>>()
+                )
+            };
+
+            let signed_delegation = sign_delegation(delegation, &identities[i - 1]);
+
+            delegations.push(signed_delegation);
+        }
+    }
+
+    delegations
 }
 
 fn sign_delegation(delegation: Delegation, identity: &GenericIdentity) -> SignedDelegation {
@@ -251,7 +383,7 @@ async fn query_delegation(
     let signature = signer.sign_query(&content);
 
     status_of_request(
-        &test,
+        test,
         "query",
         content,
         sender.public_key(),
@@ -281,7 +413,7 @@ async fn update_delegation(
     let signature = signer.sign_update(&content);
 
     status_of_request(
-        &test,
+        test,
         "call",
         content,
         sender.public_key(),
