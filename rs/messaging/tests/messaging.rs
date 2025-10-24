5ce4d485d31397b7f3235c223ebf411639c4d439
Christian Stieger <christian.stieger@dfinity.org>
2025-10-24 20:06:56 +0000

    .

--- rs/messaging/tests/messaging.rs
+++ rs/messaging/tests/messaging.rs
@@ -8,8 +8,24 @@ use proptest::prelude::ProptestConfig;
 
 #[test]
 fn test_canister_can_be_stopped_with_hanging_call_on_stalled_subnet() {
-    let (subnet1, subnet2) =
+    let (mut subnet1, subnet2) =
         two_test_subnets(TestSubnetConfig::default(), TestSubnetConfig::default());
+
+    // A call to be sent as ingress to `subnet1`,
+    // which then makes a best-effort call to `subnet2`.
+    let call = Call {
+        receiver: subnet1.canisters()[0],
+        call_bytes: 345,
+        reply_bytes: 567,
+        timeout_secs: None,
+        downstream_calls: vec![Call {
+            receiver: subnet2.canisters()[0],
+            call_bytes: 678,
+            reply_bytes: 543,
+            timeout_secs: Some(10),
+            downstream_calls: vec![],
+        }],
+    };
 }
 
 #[test_strategy::proptest(ProptestConfig::with_cases(3))]
