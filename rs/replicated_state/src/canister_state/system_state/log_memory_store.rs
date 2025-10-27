9acdde4906f49a58a5d3e78c73cfd9896b9148a4
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-10-27 14:59:20 +0000

    .

--- rs/replicated_state/src/canister_state/system_state/log_memory_store.rs
+++ rs/replicated_state/src/canister_state/system_state/log_memory_store.rs
@@ -1,4 +1,5 @@
 use crate::{PageMap, page_map::PageAllocatorFileDescriptor};
+use ic_types::CanisterLog;
 use ic_validate_eq::ValidateEq;
 use ic_validate_eq_derive::ValidateEq;
 use std::sync::Arc;
@@ -34,5 +35,37 @@ impl LogMemoryStore {
 
     pub fn page_map_mut(&mut self) -> &mut PageMap {
         &mut self.data
+    }
+
+    pub fn clear(&mut self) {
+        // TODO.
+    }
+
+    pub fn capacity(&self) -> usize {
+        0 // TODO.
+    }
+
+    pub fn used_space(&self) -> usize {
+        0 // TODO.
+    }
+
+    pub fn next_id(&self) -> u64 {
+        0 // TODO.
+    }
+
+    pub fn records(&self, filter: Option<FetchCanisterLogsFilter>) -> Vec<CanisterLogRecord> {
+        vec![] // TODO.
+    }
+
+    pub fn append_delta_log(&mut self, _delta_log: &mut CanisterLog) {
+        // TODO: preserve record sizes, advance next_idx, append records.
+    }
+
+    fn push_delta_log_size(&mut self, size: usize) {
+        // TODO.
+    }
+
+    pub fn take_delta_log_sizes(&mut self) -> Vec<usize> {
+        vec![] // TODO.
     }
 }
