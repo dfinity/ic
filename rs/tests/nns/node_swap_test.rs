1852c73789aa2c25148482e7d7cf934848e9acb6
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-10-27 20:30:54 +0100

    refactoring and extending to check if the node joins the subnet from the nodes perspective

--- rs/tests/nns/node_swap_test.rs
+++ rs/tests/nns/node_swap_test.rs
@@ -10,11 +10,14 @@ use ic_system_test_driver::driver::group::SystemTestGroup;
 use ic_system_test_driver::driver::ic::{InternetComputer, Subnet};
 use ic_system_test_driver::driver::test_env::TestEnv;
 use ic_system_test_driver::driver::test_env_api::{
-    HasTopologySnapshot, IcNodeContainer, NnsCustomizations,
+    HasTopologySnapshot, IcNodeContainer, IcNodeSnapshot, NnsCustomizations,
 };
+use ic_system_test_driver::retry_with_msg_async;
 use ic_system_test_driver::util::block_on;
+use ic_types::SubnetId;
 use registry_canister::init::RegistryCanisterInitPayloadBuilder;
 use registry_canister::mutations::do_swap_node_in_subnet_directly::SwapNodeInSubnetDirectlyPayload;
+use slog::{Logger, info};
 
 const OVERALL_TIMEOUT: Duration = Duration::from_secs(60 * 60);
 
@@ -56,6 +59,10 @@ fn setup(env: TestEnv) {
 }
 
 fn test(env: TestEnv) {
+    block_on(test_inner(env))
+}
+
+async fn test_inner(env: TestEnv) {
     let snapshot = env.topology_snapshot();
 
     let unassigned_node = snapshot.unassigned_nodes().next().unwrap();
@@ -76,40 +83,86 @@ fn test(env: TestEnv) {
     let sender = Sender::from_keypair(&TEST_USER1_KEYPAIR.clone());
     let agent = Agent::new(url, sender);
 
-    block_on(async move {
-        agent.root_key().await.unwrap();
+    agent.root_key().await.unwrap();
 
-        let response = agent
-            .execute_update(
-                &REGISTRY_CANISTER_ID,
-                &REGISTRY_CANISTER_ID,
-                "swap_node_in_subnet_directly",
-                Encode!(&payload).unwrap(),
-                vec![],
-            )
-            .await;
+    let response = agent
+        .execute_update(
+            &REGISTRY_CANISTER_ID,
+            &REGISTRY_CANISTER_ID,
+            "swap_node_in_subnet_directly",
+            Encode!(&payload).unwrap(),
+            vec![],
+        )
+        .await;
 
-        assert!(
-            response.as_ref().is_ok(),
-            "Expected the call to swap node in subnet directly to be ok but got: {response:?}"
-        );
+    assert!(
+        response.as_ref().is_ok(),
+        "Expected the call to swap node in subnet directly to be ok but got: {response:?}"
+    );
 
-        snapshot.block_for_newer_registry_version().await.unwrap();
+    snapshot.block_for_newer_registry_version().await.unwrap();
 
-        let new_unassigned_node = snapshot.unassigned_nodes().next().unwrap();
-        // Expect the new unassigned node to be the previously assigned one
-        // which should happen if the canister didn't return any errors.
-        assert_eq!(new_unassigned_node.node_id, assigned_node.node_id);
+    let new_unassigned_node = snapshot.unassigned_nodes().next().unwrap();
+    // Expect the new unassigned node to be the previously assigned one
+    // which should happen if the canister didn't return any errors.
+    assert_eq!(new_unassigned_node.node_id, assigned_node.node_id);
 
-        // Expect the previously unassigned node to be a member of
-        // a subnet it was directly swapped into.
-        assert!(
-            snapshot
-                .subnets()
-                .next()
-                .unwrap()
-                .nodes()
-                .any(|node| node.node_id == unassigned_node.node_id)
-        );
-    });
+    // Expect the previously unassigned node to be a member of
+    // a subnet it was directly swapped into.
+    let new_node_in_subnet = snapshot
+        .subnets()
+        .next()
+        .unwrap()
+        .nodes()
+        .find(|node| node.node_id == unassigned_node.node_id)
+        .expect("Expected the unassigned node to become a member of a subnet");
+    let subnet_id = subnet.subnet_id;
+
+    retry_with_msg_async!(
+        "Check that the node joined the subnt",
+        &env.logger(),
+        Duration::from_secs(600),
+        Duration::from_secs(5),
+        || async {
+            ensure_node_in_subnet(new_node_in_subnet.clone(), subnet_id, env.logger()).await
+        }
+    )
+    .await
+    .unwrap()
+}
+
+async fn ensure_node_in_subnet(
+    node: IcNodeSnapshot,
+    subnet_id: SubnetId,
+    logger: Logger,
+) -> Result<()> {
+    let dashboard_url = format!("http://[{}]:7070", node.get_ip_addr());
+    info!(
+        logger,
+        "Will fetch information about the node from the dashboard at: {dashboard_url}"
+    );
+
+    let response = reqwest::get(dashboard_url)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let prefix = "subnet id: ";
+
+    let subnet_from_dashboard = response
+        .lines()
+        .find(|l| l.starts_with(prefix))
+        .map(|s| s.strip_prefix(prefix).unwrap())
+        .unwrap()
+        .trim();
+
+    if subnet_from_dashboard != subnet_id.to_string() {
+        return Err(anyhow::anyhow!(
+            "Expected subnet to be {subnet_id} but got: {subnet_from_dashboard}"
+        ));
+    }
+
+    Ok(())
 }
