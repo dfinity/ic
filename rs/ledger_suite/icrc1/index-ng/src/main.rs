42072ed7d75b4ab90f26a11f351cb0023c059eef
Maciej Dfinity <maciej.modelski@dfinity.org>
2025-10-30 10:30:30 +0000

    build fix

--- rs/ledger_suite/icrc1/index-ng/src/main.rs
+++ rs/ledger_suite/icrc1/index-ng/src/main.rs
@@ -992,9 +992,7 @@ fn get_accounts(block: &Block<Tokens>) -> Vec<Account> {
         Operation::Mint { to, .. } => vec![to],
         Operation::Transfer { from, to, .. } => vec![from, to],
         Operation::Approve { from, .. } => vec![from],
-        Operation::FeeCollector { .. } => {
-            panic!("not implemented")
-        }
+        Operation::FeeCollector { .. } => vec![],
     }
 }
 
