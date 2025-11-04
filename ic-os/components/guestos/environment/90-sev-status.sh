3b462e49d57eadc48e061a96b43d3cfab04874ce
David Frank <david.frank@dfinity.org>
2025-11-04 22:53:17 +0800

    clarify comment

--- ic-os/components/guestos/environment/90-sev-status.sh
+++ ic-os/components/guestos/environment/90-sev-status.sh
@@ -2,9 +2,11 @@
 
 # Set SEV_ACTIVE environment variable based on systemd-detect-virt output.
 # SEV detection involves querying the CPU using cpuid which goes through the HostOS.
-# A malicious HostOS could intercept this call. By querying the CPU only once early
+# A malicious HostOS could intercept this call, but by querying the CPU only once early
 # in the boot process and setting an environment variable, each service in the GuestOS
-# will see the same value.
+# will see the same value. Therefore, faking the initial cpuid is not an attack vector,
+# as it would not allow selective manipulation of SEV status between services — it is
+# equivalent of enabling/disabling SEV in the GuestOS VM config.
 
 case $(systemd-detect-virt --cvm) in
   sev|sev-es|sev-snp)
