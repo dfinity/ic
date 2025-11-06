042048e76acfbc2ade633b1f9c752d6b436221f8
Andrew Battat <andrew.battat@dfinity.org>
2025-11-06 19:19:33 +0000

    Log GuestOS console logs from HostOS

--- rs/tests/nested/src/util.rs
+++ rs/tests/nested/src/util.rs
@@ -347,6 +347,10 @@ pub fn block_on_bash_script_and_log<N: SshSession>(log: &Logger, node: &N, cmd: 
 /// Logs guestos diagnostics, used in the event of test failure
 pub fn try_logging_guestos_diagnostics(host: &NestedVm, logger: &Logger) {
     info!(logger, "Logging GuestOS diagnostics...");
+
+    info!(logger, "GuestOS console logs...");
+    block_on_bash_script_and_log(logger, host, "cat /var/log/libvirt/qemu/guestos-serial.log");
+
     match host.get_guest_ssh() {
         Ok(guest) => {
             let diagnostics = vec![
