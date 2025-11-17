8043c38110c8f3066b1741d36fda2bf7a28a2058
Pietro <pietro.di.marco@dfinity.org>
2025-11-17 20:39:17 +0100

    Enable performance based rewards governance

--- rs/nns/governance/src/lib.rs
+++ rs/nns/governance/src/lib.rs
@@ -216,7 +216,7 @@ thread_local! {
 }
 
 thread_local! {
-    static ARE_PERFORMANCE_BASED_REWARDS_ENABLED: Cell<bool> = const { Cell::new(false) };
+    static ARE_PERFORMANCE_BASED_REWARDS_ENABLED: Cell<bool> = const { Cell::new(true) };
 }
 
 pub(crate) fn are_performance_based_rewards_enabled() -> bool {
