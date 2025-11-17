0ddf4d0273d71d48f29f58aece308a3a302cdc5b
Kamil Popielarz <kamil.popielarz@dfinity.org>
2025-11-17 09:36:22 +0000

    rev

--- rs/tests/run-p8s.sh
+++ rs/tests/run-p8s.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/env nix-shell
+#! nix-shell run-p8s.nix -i bash
 
 # TODO: Rewrite this to use docker instead of Nix
 # See: https://dfinity.atlassian.net/browse/VER-1941
@@ -76,7 +78,6 @@ done
 
 PROMETHEUS_PORT="${PROMETHEUS_PORT:-9090}"
 GRAFANA_PORT="${GRAFANA_PORT:-3000}"
-GRAFANA="/usr"
 
 info() { echo "$*"; }
 warn() { info "$*" 1>&2; }
