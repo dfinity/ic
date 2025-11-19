b68bd59e9cc09214b27a4a0bae0a2607634c5f0b
Jack Lloyd <jack.lloyd@dfinity.org>
2025-11-19 04:56:01 -0500

    chore(crypto): CRP-2410 Remove redundant Ed25519 DER functions from basic_sig (#7693)
    The ic-ed25519 crate has equivalent functions and there are just a
    couple of users of the old basic_sig functions remaining - rewrite the
    callers to use ic-ed25519 directly (which they already depended on
    indirectly, since basic_sig/ed25519 is implemented by calling
    ic-ed25519) and remove the old functions.

--- rs/crypto/internal/crypto_service_provider/src/vault/local_csp_vault/tls/mod.rs
+++ rs/crypto/internal/crypto_service_provider/src/vault/local_csp_vault/tls/mod.rs
@@ -148,20 +148,15 @@ impl<R: Rng + CryptoRng, S: SecretKeyStore, C: SecretKeyStore, P: PublicKeyStore
 
         match &secret_key {
             CspSecretKey::TlsEd25519(secret_key_der) => {
-                let secret_key_bytes =
-                    ic_crypto_internal_basic_sig_ed25519::secret_key_from_pkcs8_v1_der(
-                        &secret_key_der.bytes,
-                    )
+                let secret_key = ic_ed25519::PrivateKey::deserialize_pkcs8(secret_key_der.bytes.expose_secret())
                     .map_err(|e| {
                         CspTlsSignError::MalformedSecretKey {
                             error: format!("Failed to convert TLS secret key DER from key store to Ed25519 secret key: {e:?}")
                     }})?;
 
+                let signature = secret_key.sign_message(message);
                 let signature_bytes =
-                    ic_crypto_internal_basic_sig_ed25519::sign(message, &secret_key_bytes)
-                        .map_err(|e| CspTlsSignError::SigningFailed {
-                            error: format!("{e}"),
-                        })?;
+                    ic_crypto_internal_basic_sig_ed25519::types::SignatureBytes(signature);
 
                 Ok(CspSignature::Ed25519(signature_bytes))
             }
