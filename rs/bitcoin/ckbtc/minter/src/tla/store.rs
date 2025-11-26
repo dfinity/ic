94c2a4f8bc5830f20df59e871c05a4b2e1ffcafa
Ognjen Maric <ognjen.maric@dfinity.org>
2025-11-26 17:55:35 +0100

    Only allow storing data if the tla feature is enabled

--- rs/bitcoin/ckbtc/minter/src/tla/store.rs
+++ rs/bitcoin/ckbtc/minter/src/tla/store.rs
@@ -2,14 +2,15 @@ use local_key::task_local;
 use std::sync::{Arc, Mutex, RwLock};
 use tla_instrumentation::{InstrumentationState, UpdateTrace};
 
+#[cfg(feature = "tla")]
 task_local! {
     pub static TLA_INSTRUMENTATION_STATE: InstrumentationState;
     pub static TLA_TRACES_LKEY: Arc<Mutex<Vec<UpdateTrace>>>;
 }
 
-#[cfg(target_family = "wasm")]
+#[cfg(all(target_family = "wasm", feature = "tla"))]
 pub static TLA_TRACES_MUTEX: Option<RwLock<Vec<UpdateTrace>>> = Some(RwLock::new(vec![]));
 
-#[cfg(not(target_family = "wasm"))]
+#[cfg(all(not(target_family = "wasm"), feature = "tla"))]
 pub static TLA_TRACES_MUTEX: Option<RwLock<Vec<UpdateTrace>>> = None;
 
