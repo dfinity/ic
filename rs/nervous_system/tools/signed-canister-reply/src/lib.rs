46d66c787b7df03c690fda8d3e50a95dabadfaf5
Daniel Wong <daniel.wong@dfinity.org>
2025-11-27 16:50:54 +0100

    Deleted some unnecessary code.

--- rs/nervous_system/tools/signed-canister-reply/src/lib.rs
+++ rs/nervous_system/tools/signed-canister-reply/src/lib.rs
@@ -168,27 +168,6 @@ async fn download_signed_proposal(
 }
 
 fn verify_signed_proposal(agent: &Agent, certificate: Certificate) -> Vec<u8> {
-    // This is copied from ic-admin.
-    const IC_ROOT_PUBLIC_KEY_BASE64: &str = r#"MIGCMB0GDSsGAQQBgtx8BQMBAgEGDCsGAQQBgtx8BQMCAQNhAIFMDm7HH6tYOwi9gTc8JVw8NxsuhIY8mKTx4It0I10U+12cDNVG2WhfkToMCyzFNBWDv0tDkuRn25bWW5u0y3FxEvhHLg1aTRRQX/10hLASkQkcX4e5iINGP5gJGguqrg=="#;
-    // This is copied from rs/embedders.
-    const IC_ROOT_KEY: &[u8; 133] = b"\x30\x81\x82\x30\x1d\x06\x0d\x2b\x06\x01\x04\x01\x82\xdc\x7c\x05\x03\x01\x02\x01\x06\x0c\x2b\x06\x01\x04\x01\x82\xdc\x7c\x05\x03\x02\x01\x03\x61\x00\x81\x4c\x0e\x6e\xc7\x1f\xab\x58\x3b\x08\xbd\x81\x37\x3c\x25\x5c\x3c\x37\x1b\x2e\x84\x86\x3c\x98\xa4\xf1\xe0\x8b\x74\x23\x5d\x14\xfb\x5d\x9c\x0c\xd5\x46\xd9\x68\x5f\x91\x3a\x0c\x0b\x2c\xc5\x34\x15\x83\xbf\x4b\x43\x92\xe4\x67\xdb\x96\xd6\x5b\x9b\xb4\xcb\x71\x71\x12\xf8\x47\x2e\x0d\x5a\x4d\x14\x50\x5f\xfd\x74\x84\xb0\x12\x91\x09\x1c\x5f\x87\xb9\x88\x83\x46\x3f\x98\x09\x1a\x0b\xaa\xae";
-    assert_eq!(
-        &base64::decode(IC_ROOT_PUBLIC_KEY_BASE64).unwrap(),
-        IC_ROOT_KEY
-    );
-    assert_eq!(
-        agent.read_root_key(),
-        base64::decode(IC_ROOT_PUBLIC_KEY_BASE64).unwrap()
-    );
-
-    let _corrupt_root_key = {
-        let mut result = agent.read_root_key();
-        let last_index = result.len() - 1;
-        result[last_index] = 0;
-        result
-    };
-    // agent.set_root_key(corrupt_root_key);
-
     let governance_principal = Principal::from_text("rrkah-fqaaa-aaaaa-aaaaq-cai").unwrap();
 
     agent
@@ -203,24 +182,6 @@ fn verify_signed_proposal(agent: &Agent, certificate: Certificate) -> Vec<u8> {
          be verified later.",
     );
     eprintln!();
-
-    /*
-    let paths = certificate
-        .tree
-        .list_paths()
-        .into_iter()
-        .map(|path| {
-            path
-                .into_iter()
-                .map(|segment| {
-                    String::from_utf8_lossy(segment.as_bytes())
-                        .into_owned()
-                })
-                .collect::<Vec<String>>()
-                .join("    ")
-        })
-        .collect::<Vec<_>>();
-    */
 
     let request_status = RequestStatus::try_from_tree(certificate.tree).unwrap();
     assert_eq!(&request_status.status, "replied");
