0e3f9765d5888ff6dd85db55672b8384713d42f5
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-12-01 14:53:43 +0100

    refactoring

--- rs/registry/canister/src/mutations/node_management/do_approve_add_node_payload.rs
+++ rs/registry/canister/src/mutations/node_management/do_approve_add_node_payload.rs
@@ -1,25 +1,22 @@
 use std::fmt::Display;
 
 use candid::CandidType;
-use hex::FromHexError;
+use ic_types::PrincipalId;
 use prost::Message;
 use serde::{Deserialize, Serialize};
 
-static SHA256_BYTES_LEN: usize = 32;
-
 #[derive(Clone, Eq, PartialEq, CandidType, Deserialize, Message, Serialize)]
 pub struct ApproveAddNodePayload {
-    /// Represents the hash of the payload sent by the
+    /// Represents the expected node ID sent by the
     /// node operator that will be used to map an incoming
     /// request from the node to register itself.
     #[prost(message, optional, tag = "1")]
-    pub new_payload_hash_hex: Option<String>,
+    pub node_id: Option<PrincipalId>,
 }
 
 #[derive(Debug, PartialEq)]
 pub enum ApprovePayloadError {
-    InvalidPayloadHex(FromHexError),
-    InvalidPayloadLength { received_length: usize },
+    MissingNodeId,
 }
 
 impl Display for ApprovePayloadError {
@@ -28,11 +25,8 @@ impl Display for ApprovePayloadError {
             f,
             "{}",
             match self {
-                ApprovePayloadError::InvalidPayloadLength { received_length } => format!(
-                    "Received invalid payload. `new_payload_hash_hex` should contain {SHA256_BYTES_LEN} bytes, but got {received_length} bytes."
-                ),
-                ApprovePayloadError::InvalidPayloadHex(hex_error) =>
-                    format!("Received invalid payload. Error from decoding hex bytes: {hex_error}"),
+                ApprovePayloadError::MissingNodeId =>
+                    "Received invalid payload. `node_id` must be specified.".to_string(),
             }
         )
     }
@@ -40,19 +34,8 @@ impl Display for ApprovePayloadError {
 
 impl ApproveAddNodePayload {
     pub fn validate(&self) -> Result<(), ApprovePayloadError> {
-        let received_hex = self
-            .new_payload_hash_hex
-            .as_ref()
-            .ok_or(ApprovePayloadError::InvalidPayloadLength { received_length: 0 })?;
-
-        let received_bytes_len = hex::decode(received_hex)
-            .map(|bytes| bytes.len())
-            .map_err(ApprovePayloadError::InvalidPayloadHex)?;
-
-        if received_bytes_len != SHA256_BYTES_LEN {
-            return Err(ApprovePayloadError::InvalidPayloadLength {
-                received_length: received_bytes_len,
-            });
+        if self.node_id.is_none() {
+            return Err(ApprovePayloadError::MissingNodeId);
         }
 
         Ok(())
@@ -65,58 +48,18 @@ mod tests {
 
     #[test]
     fn disallow_empty_payload() {
-        let payload = ApproveAddNodePayload {
-            new_payload_hash_hex: None,
-        };
+        let payload = ApproveAddNodePayload { node_id: None };
 
         let result = payload.validate();
 
-        let expected_err = ApprovePayloadError::InvalidPayloadLength { received_length: 0 };
+        let expected_err = ApprovePayloadError::MissingNodeId;
         assert_eq!(result, Err(expected_err))
-    }
-
-    #[test]
-    fn disallow_wrong_size_of_payloads() {
-        for payload_size in [10, 42] {
-            let payload = ApproveAddNodePayload {
-                new_payload_hash_hex: Some("a".repeat(payload_size)),
-            };
-
-            let result = payload.validate();
-
-            let expected_err = ApprovePayloadError::InvalidPayloadLength {
-                // Two chars in a hex byte
-                received_length: payload_size / 2,
-            };
-
-            assert_eq!(result, Err(expected_err))
-        }
-    }
-
-    #[test]
-    fn disallow_wrong_chars_in_hex() {
-        for disallowed in 'g'..='z' {
-            let payload = ApproveAddNodePayload {
-                // No need to send over more than one byte as it is enough to fail
-                new_payload_hash_hex: Some(disallowed.to_string().repeat(2)),
-            };
-
-            let result = payload.validate();
-
-            let expected_err =
-                ApprovePayloadError::InvalidPayloadHex(FromHexError::InvalidHexCharacter {
-                    c: disallowed,
-                    index: 0,
-                });
-
-            assert_eq!(result, Err(expected_err));
-        }
     }
 
     #[test]
     fn valid_payload_size() {
         let payload = ApproveAddNodePayload {
-            new_payload_hash_hex: Some("a".repeat(SHA256_BYTES_LEN * 2)),
+            node_id: Some(PrincipalId::new_node_test_id(1)),
         };
 
         let result = payload.validate();
