264c84b0a0a96350640d68466cb7d368971268c1
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-12-02 11:51:21 +0000

    adapting test logic

--- rs/registry/canister/tests/data_migration_test.rs
+++ rs/registry/canister/tests/data_migration_test.rs
@@ -1,9 +1,13 @@
 use common::test_helpers::install_registry_canister_with_payload_builder;
+use ic_agent::agent::AgentBuilder;
+use ic_agent::identity::AnonymousIdentity;
 use ic_interfaces_registry::ZERO_REGISTRY_VERSION;
 use ic_nervous_system_integration_tests::pocket_ic_helpers::nns::registry::{
     apply_mutations_for_test, get_value,
 };
+use ic_nns_constants::REGISTRY_CANISTER_ID;
 use ic_protobuf::registry::subnet::v1::SubnetListRecord;
+use ic_registry_nns_data_provider::registry::RegistryCanister;
 use ic_registry_transport::pb::v1::RegistryMutation;
 use ic_registry_transport::{delete, upsert};
 use pocket_ic::PocketIcBuilder;
@@ -13,6 +17,98 @@ use registry_canister::init::RegistryCanisterInitPayloadBuilder;
 use crate::common::test_helpers::upgrade_registry_canister;
 
 mod common;
+
+trait DataMigrationAssert {
+    async fn assert_expected_changes(&self, new_mutations: &Vec<RegistryMutation>);
+}
+
+struct TestScenario {
+    name: String,
+    on_hash: String,
+    asserts: Vec<Box<dyn DataMigrationAssert>>,
+}
+
+struct Setup {
+    pocket_ic: PocketIc,
+    mainnet_mutation_batches: Vec<Vec<RegistryMutation>>,
+    mainnet_module_hash: String,
+}
+
+impl Setup {
+    async fn new(pocket_ic: PocketIc) -> Self {
+        Self {
+            mainnet_mutation_batches: Self::fetch_all_mainnet_changes().await,
+            pocket_ic,
+            mainnet_module_hash: Self::fetch_current_module_hash().await,
+        }
+    }
+
+    async fn fetch_all_mainnet_changes() -> Vec<Vec<RegistryMutation>> {
+        let mut mutation_batches = vec![];
+        let mainnet_registry = RegistryCanister::new(vec!["https://ic0.app".parse().unwrap()]);
+
+        let latest_version_on_mainnet = mainnet_registry.get_latest_version().await.unwrap();
+
+        let mut local_version = ZERO_REGISTRY_VERSION;
+
+        loop {
+            match local_version.get().cmp(&latest_version_on_mainnet) {
+                std::cmp::Ordering::Less => {}
+                std::cmp::Ordering::Equal => break,
+                std::cmp::Ordering::Greater => panic!(
+                    "Impossible, the local version was in front of the registry on mainnet. Local {local_version}, remote {latest_version_on_mainnet}"
+                ),
+            }
+
+            // The registry itself maps the limit of deltas to
+            // fit into the message, so we just need to send
+            // them in the exact same batches, mapped.
+            let (records, _) = mainnet_registry
+                .get_changes_since_as_registry_records(local_version.get())
+                .await
+                .unwrap();
+
+            let new_version = records.last().map(|r| r.version);
+            mutation_batches.push(
+                records
+                    .into_iter()
+                    .map(|r| match r.value {
+                        None => delete(r.key.as_bytes()),
+                        Some(val) => upsert(r.key.as_bytes(), val.as_slice()),
+                    })
+                    .collect(),
+            );
+
+            local_version = match new_version {
+                Some(v) => v,
+                None => break,
+            }
+        }
+
+        mutation_batches
+    }
+
+    async fn fetch_current_module_hash() -> String {
+        let agent = AgentBuilder::default()
+            .with_url("https://ic0.app")
+            .with_identity(AnonymousIdentity {})
+            .build()
+            .unwrap();
+
+        std::str::from_utf8(
+            &agent
+                .read_state_canister_module_hash(REGISTRY_CANISTER_ID.get().0)
+                .await
+                .unwrap(),
+        )?
+        .trim()
+        .to_string()
+    }
+
+    async fn upgrade(&self) -> Vec<RegistryMutation> {
+        vec![]
+    }
+}
 
 #[tokio::test]
 async fn test_mainnet_data() {
