ed65ac10850bb35420fdfaa9364cc050092dee09
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-12-03 01:48:49 +0000

    mapping what needs to be done

--- rs/registry/canister/src/mutations/node_management/do_approve_add_node_payload.rs
+++ rs/registry/canister/src/mutations/node_management/do_approve_add_node_payload.rs
@@ -1,9 +1,112 @@
-use std::fmt::Display;
+use std::{
+    cell::RefCell,
+    collections::{HashMap, VecDeque},
+    fmt::Display,
+};
 
 use candid::CandidType;
 use ic_types::PrincipalId;
 use prost::Message;
 use serde::{Deserialize, Serialize};
+
+use crate::registry::Registry;
+
+struct NodeApprovalManager {
+    approved: HashMap<PrincipalId, VecDeque<PrincipalId>>,
+}
+
+impl NodeApprovalManager {
+    fn new() -> Self {
+        Self {
+            approved: HashMap::new(),
+        }
+    }
+}
+
+thread_local! {
+    static NODE_APPROVAL_MANAGER: RefCell<NodeApprovalManager> = RefCell::new(NodeApprovalManager::new());
+}
+
+impl Registry {
+    /// Called by the node operators in order to approve nodes for onboarding.
+    pub fn do_approve_add_node_payload(&mut self, payload: ApproveAddNodePayload) {
+        NODE_APPROVAL_MANAGER.with_borrow_mut(|manager| {
+            self.do_approve_add_node_payload_inner(payload, dfn_core::api::caller(), manager)
+                .unwrap_or_else(|e| panic!("{e}"));
+        })
+    }
+
+    // TODO: Implement a way to fetch a node operator id for a given node id.
+    //
+    // This will be used from `do_add_node` to verify the following things:
+    // * Is the node that is trying to onboard to the network approved to do so?
+    // * If it is, to what node operator should it be linked to?
+    //
+    // NOTE: This function should hold an invariant check that will check if
+    // somehow the given node is approved with multiple node operators and error
+    // out if it is. If the whole feature is implemented properly, this shouldn't
+    // happen, but assuming that the first occurance of the node id in any of the
+    // node operators approved node may be faulty.
+
+    // TODO: Implement a way to remove a node id from the queue of approved nodes
+    // for the node operator.
+    //
+    // NOTE: This should be called as the very last step of the `do_add_node`,
+    // after the registry verified all other invariant checks since this call
+    // is irreversible and if the node registration process fails later, that
+    // node operator would have inaccurate approved nodes for onboarding.
+
+    fn do_approve_add_node_payload_inner(
+        &mut self,
+        payload: ApproveAddNodePayload,
+        caller: PrincipalId,
+        node_approval_manager: &mut NodeApprovalManager,
+    ) -> Result<(), ApprovePayloadError> {
+        // TODO: Verify payload integrity
+
+        // TODO: Prevent approving a node that has already been queued for registration.
+
+        // TODO: Duplication Check: Ensure node is not already in the registry or
+        // already queued.
+
+        // TODO: Ensure `caller` is a registered node operator.
+
+        // TODO: Calculate the remaining spare capacity the node operator and
+        // update the approved nodes for onboarding for the node operator.
+        //
+        // NOTICE: Keep in mind that this code will be used by UTOPIA and capacity
+        // calculations are done differently there and on a regular IC.
+        //
+        // Spare capacity (in this context) is calculated as follows:
+        //    spare = total_capacity - (nodes_in_registry + nodes_currently_approved)
+        //
+        // Two scenarios are possible here:
+        // * If spare > 0: Prepend new ID to the queue.
+        // * If spare == 0: Prepend new ID and pop the back (remove stale approval).
+        //
+        // This approach is deemed acceptable because the window between
+        // approving a node for registration and its actual registration
+        // will be small. This approach also removes the need for a cron
+        // job that will clean the approved node ids that were queued due to
+        // an error or just won't ever complete.
+
+        // TODO: Enforce a hard cap on the number of nodes a node operator
+        // is onboarding.
+        //
+        // This is a safety measure regardless of the theoretical maximum.
+
+        // TODO: Handle edge case for calculating spares.
+        //
+        // It is possible that total_capacity gets changed over time, i.e.
+        // with `do_update_node_operator` which happens via proposals. While
+        // increasing the total_capacity (by increasing max_rewardable_nodes)
+        // is not a problem, decreasing the total_capacity could cause an
+        // underflow and the capacity formula could allow having more
+        // approved nodes for onboarding than possible.
+
+        Ok(())
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub enum ApprovePayloadError {
