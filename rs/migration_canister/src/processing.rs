c68b3fce6e8ac91f09dc20083417f93d3c636304
Martin Raszyk <martin.raszyk@dfinity.org>
2025-12-04 13:16:18 +0100

    harden

--- rs/migration_canister/src/processing.rs
+++ rs/migration_canister/src/processing.rs
@@ -713,7 +713,9 @@ mod test {
 
         state = controller_recovery(&mut ic00, state, canister_id, new_controllers.clone()).await;
         assert_eq!(state, ControllerRecoveryState::TotalNumChangesBefore(0));
-        state = controller_recovery(&mut ic00, state, canister_id, new_controllers).await;
+        state = controller_recovery(&mut ic00, state, canister_id, new_controllers.clone()).await;
         assert_eq!(state, ControllerRecoveryState::Done);
+
+        assert_eq!(*ic00.controllers.get(&canister_id).unwrap(), new_controllers);
     }
 }
