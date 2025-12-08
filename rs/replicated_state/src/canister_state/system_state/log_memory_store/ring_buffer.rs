80d4a315393f5d1c5911e527bb74b6d88b053fee
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2025-12-08 12:17:05 +0000

    total_allocated_bytes clean

--- rs/replicated_state/src/canister_state/system_state/log_memory_store/ring_buffer.rs
+++ rs/replicated_state/src/canister_state/system_state/log_memory_store/ring_buffer.rs
@@ -77,12 +77,9 @@ impl RingBuffer {
     /// Returns the total allocated bytes for the ring buffer
     /// including header, index table and data region.
     pub fn total_allocated_bytes(&self) -> usize {
-        let header = self.io.load_header();
-        let memory_usage = HEADER_SIZE.get() as usize
+        HEADER_SIZE.get() as usize
             + header.index_table_pages as usize * PAGE_SIZE
-            + header.data_capacity.get() as usize;
-        // TODO: temporarely remove from counting the first 3 pages of log memory store.
-        memory_usage.saturating_sub(3 * PAGE_SIZE)
+            + header.data_capacity.get() as usize
     }
 
     /// Returns the data capacity of the ring buffer.
