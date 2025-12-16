352b9c909d22117b2730e4e70201fd0f39ae3eba
gregorydemay <gregory.demay@dfinity.org>
2025-12-16 16:06:07 +0100

    DEFI-2561: explode structure

---
+++ rs/dogecoin/ckdoge/minter/src/test_fixtures/mod.rs
@@ -0,0 +1,9 @@
+use crate::fees::DogecoinFeeEstimator;
+use crate::lifecycle::init::Network;
+
+pub mod arbitrary;
+
+pub fn dogecoin_fee_estimator() -> DogecoinFeeEstimator {
+    const RETRIEVE_DOGE_MIN_AMOUNT: u64 = 50 * 100_000_000;
+    DogecoinFeeEstimator::new(Network::Mainnet, RETRIEVE_DOGE_MIN_AMOUNT)
+}
