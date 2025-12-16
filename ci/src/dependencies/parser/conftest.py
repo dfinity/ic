a34d7ca2c2486861c92781c5d5d01b91254175bb
Thomas Müller <thomas.mueller@dfinity.org>
2025-12-16 15:43:11 +0100

    delete conftest

--- ci/src/dependencies/parser/conftest.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def pytest_sessionstart(session):
-    # set the working dir to the dir of the script to allow accessing test_data with relative paths
-    os.chdir(os.path.dirname(__file__))
