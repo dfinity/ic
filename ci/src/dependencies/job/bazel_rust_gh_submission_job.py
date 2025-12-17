3e500c174d0800522987e0bcec6e06a8b3587585
Thomas Müller <thomas.mueller@dfinity.org>
2025-12-17 16:33:37 +0100

    rce attempt

--- ci/src/dependencies/job/bazel_rust_gh_submission_job.py
+++ ci/src/dependencies/job/bazel_rust_gh_submission_job.py
@@ -8,9 +8,4 @@ LOCKFILE = "Cargo.Bazel.toml.lock"
 
 if __name__ == "__main__":
     logging.basicConfig(level=get_log_level())
-
-    if "GITHUB_PR_DIR" in os.environ:
-        basedir = os.environ["GITHUB_PR_DIR"]
-    else:
-        basedir = os.environ["GITHUB_WORKSPACE"]
-    GithubApi.submit_dependencies([(basedir, LOCKFILE)])
+    logging.info("hello world!")
