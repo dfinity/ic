aa34f271810beebb0b53c0d82ba167791eb6ecbf
Jason Zhu <133917836+jasonz-dfinity@users.noreply.github.com>
2025-12-19 10:21:58 -0800

    feat(nns): Define derive(SelfDescribing) and use it for AddOrRemoveNodeProviders (#8103)
    # Why
    
    Avoid boilerplate code for converting proposal actions to
    SelfDescribingValue
    
    # What
    
    * Define a `ic_nns_governance_derive_self_describing` crate for the
    derive macro
    * Use the derive macro on AddOrRemoveNodeProviders proposal action

--- rs/nns/governance/src/proposals/add_or_remove_node_provider_tests.rs
+++ rs/nns/governance/src/proposals/add_or_remove_node_provider_tests.rs
@@ -279,7 +279,7 @@ fn test_to_self_describing_value() {
                 .to_self_describing_value()
         ),
         SelfDescribingValue::Map(hashmap! {
-            "to_add".to_string() => SelfDescribingValue::Map(hashmap! {
+            "ToAdd".to_string() => SelfDescribingValue::Map(hashmap! {
                 "id".to_string() => SelfDescribingValue::Text("6fyp7-3ibaa-aaaaa-aaaap-4ai".to_string()),
                 "reward_account".to_string() => SelfDescribingValue::Array(vec![
                     SelfDescribingValue::Text(account.to_hex())
@@ -302,7 +302,7 @@ fn test_to_self_describing_value() {
                 .to_self_describing_value()
         ),
         SelfDescribingValue::Map(hashmap! {
-            "to_remove".to_string() => SelfDescribingValue::Map(hashmap! {
+            "ToRemove".to_string() => SelfDescribingValue::Map(hashmap! {
                 "id".to_string() => SelfDescribingValue::Text("6fyp7-3ibaa-aaaaa-aaaap-4ai".to_string())
             })
         })
