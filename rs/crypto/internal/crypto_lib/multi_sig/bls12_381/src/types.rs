978a49ce081989b6082eefc22b0177434cbc1d3b
Jack Lloyd <jack.lloyd@dfinity.org>
2025-12-19 14:47:02 -0500

    Bring back base64 Debug impls

--- rs/crypto/internal/crypto_lib/multi_sig/bls12_381/src/types.rs
+++ rs/crypto/internal/crypto_lib/multi_sig/bls12_381/src/types.rs
@@ -4,6 +4,7 @@ use ic_crypto_internal_bls12_381_type::{G1Affine, G2Affine, Scalar};
 use ic_crypto_secrets_containers::SecretArray;
 use serde::{Deserialize, Serialize};
 use zeroize::{Zeroize, ZeroizeOnDrop};
+use std::fmt;
 
 #[cfg(test)]
 pub mod arbitrary;
@@ -41,40 +42,64 @@ impl SecretKeyBytes {
 }
 
 // Note: This is needed to keep sensitive material from getting Debug logged.
-impl std::fmt::Debug for SecretKeyBytes {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+impl fmt::Debug for SecretKeyBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "REDACTED")
     }
 }
 
 /// Wrapper for a serialized individual signature.
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct IndividualSignatureBytes(pub [u8; IndividualSignatureBytes::SIZE]);
 ic_crypto_internal_types::derive_serde!(IndividualSignatureBytes, IndividualSignatureBytes::SIZE);
 impl IndividualSignatureBytes {
     pub const SIZE: usize = G1Affine::BYTES;
 }
 
+impl fmt::Debug for IndividualSignatureBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", base64::encode(&self.0[..]))
+    }
+}
+
 /// Wrapper for a serialized proof of possession.
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct PopBytes(pub [u8; PopBytes::SIZE]);
 ic_crypto_internal_types::derive_serde!(PopBytes, PopBytes::SIZE);
 impl PopBytes {
     pub const SIZE: usize = G1Affine::BYTES;
 }
 
+impl fmt::Debug for PopBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", &self.0[..])
+    }
+}
+
 /// Wrapper for a serialized combined signature.
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct CombinedSignatureBytes(pub [u8; CombinedSignatureBytes::SIZE]);
 ic_crypto_internal_types::derive_serde!(CombinedSignatureBytes, CombinedSignatureBytes::SIZE);
 impl CombinedSignatureBytes {
     pub const SIZE: usize = G1Affine::BYTES;
 }
 
+impl fmt::Debug for CombinedSignatureBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", base64::encode(&self.0[..]))
+    }
+}
+
 /// Wrapper for a serialized public key.
-#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+#[derive(Copy, Clone, Eq, PartialEq)]
 pub struct PublicKeyBytes(pub [u8; PublicKeyBytes::SIZE]);
 ic_crypto_internal_types::derive_serde!(PublicKeyBytes, PublicKeyBytes::SIZE);
 impl PublicKeyBytes {
     pub const SIZE: usize = G2Affine::BYTES;
+}
+
+impl fmt::Debug for PublicKeyBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", &self.0[..])
+    }
 }
