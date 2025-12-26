4ec352c34d4fc67e500217989f77b01792dc153b
Andrew Battat <andrew.battat@dfinity.org>
2025-12-26 22:53:05 +0000

    Rename config_tool and organize config_tool and config_types under a single ic_os/config

--- ic-os/components/guestos/init/init-config/init-config.sh
+++ ic-os/components/guestos/init/init-config/init-config.sh
@@ -87,7 +87,7 @@ mkdir -p /run/config/bootstrap
 tar xf /mnt/config/ic-bootstrap.tar -C /run/config/bootstrap
 cp /run/config/bootstrap/config.json /run/config/config.json
 chown ic-replica:nogroup /run/config/config.json
-/opt/ic/bin/config populate-nns-public-key
+/opt/ic/bin/config_tool populate-nns-public-key
 
 # Create file under /run/config/guest_vm_type, this can be used to add ConditionPathExists conditions to systemd units
 guest_vm_type="$(jq -r ".guest_vm_type" /run/config/config.json)"
