ee265bbe57cedcc15ceb9887606f21b07594fdce
Maciej Dfinity <maciej.modelski@dfinity.org>
2026-01-05 15:36:07 +0000

    add comments

--- packages/icrc-ledger-types/src/icrc107/set_fee_collector.rs
+++ packages/icrc-ledger-types/src/icrc107/set_fee_collector.rs
@@ -3,6 +3,9 @@ use serde::Serialize;
 
 use super::super::icrc1::account::Account;
 
+/// The arguments for the
+/// [ICRC-107 `icrc107_set_fee_collector`](https://github.com/dfinity/ICRC/blob/main/ICRCs/ICRC-107/ICRC-107.md#icrc107_set_fee_collector)
+/// endpoint.
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct SetFeeCollectorArgs {
     #[serde(default)]
@@ -10,6 +13,9 @@ pub struct SetFeeCollectorArgs {
     pub created_at_time: u64,
 }
 
+/// The error return type for the
+/// [ICRC-107 `icrc107_set_fee_collector`](https://github.com/dfinity/ICRC/blob/main/ICRCs/ICRC-107/ICRC-107.md#icrc107_set_fee_collector)
+/// endpoint.
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub enum SetFeeCollectorError {
     AccessDenied(String),
