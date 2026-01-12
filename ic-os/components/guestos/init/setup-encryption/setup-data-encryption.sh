4fc7a46e93ce387c3c085076eb02fb4d3d82bb5a
Igor Novgorodov <igor.novgorodov@dfinity.org>
2026-01-12 15:13:05 +0000

    Add encrypted partition UUID migration

--- ic-os/components/guestos/init/setup-encryption/setup-data-encryption.sh
+++ ic-os/components/guestos/init/setup-encryption/setup-data-encryption.sh
@@ -8,6 +8,12 @@ BASE_DEVICE=$(lsblk -nld --bytes -o SIZE,NAME | sort -n -r | head -n1 | cut -d' 
 PART_UUID="231213c6-ec9e-11f0-b45f-b7bbea44aaf0"
 STORE_DEVICE="/dev/disk/by-partuuid/${PART_UUID}"
 
+# Migration step: change the UUID of the partition to the persistent one
+if [ ! -e ${STORE_DEVICE} ] && [ -e /dev/${BASE_DEVICE}10 ]; then
+    sfdisk --force --no-reread --part-uuid /dev/${BASE_DEVICE} 10 ${PART_UUID}
+    partprobe /dev/${BASE_DEVICE}
+fi
+
 if [ -e ${STORE_DEVICE} ]; then
     /opt/ic/bin/guest_disk crypt-open store ${STORE_DEVICE}
     exit 0
