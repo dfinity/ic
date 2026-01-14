c4381c860ff6657b34d55b65c523ceded3774383
Carly Gundy <carlene.gundy@dfinity.org>
2026-01-14 10:59:17 +0100

    refactor(IDX): add skeleton for container targets

--- ci/container/build-image.sh
+++ ci/container/build-image.sh
@@ -2,11 +2,14 @@
 
 set -eEuo pipefail
 
+BUILD_TARGET="devenv"
+
 usage() {
     echo "Build ic-build docker image."
     echo " "
     echo "Options:"
-    echo "-h, --help   show brief help"
+    echo "-h, --help        show brief help"
+    echo "--target <name>   set Docker build target (default: devenv)"
 }
 
 while test $# -gt 0; do
@@ -15,13 +18,17 @@ while test $# -gt 0; do
             usage >&2
             exit 0
             ;;
+        --target)
+            shift
+            BUILD_TARGET="$1"
+            ;;
         *)
             echo "unknown argument: $1" >&2
             usage >&2
             exit 1
             ;;
-
     esac
+    shift
 done
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
@@ -39,6 +46,7 @@ else
 fi
 
 DOCKER_BUILDKIT=1 docker "${ARGS[@]}" build "${BUILD_ARGS[@]}" \
+    --target "$BUILD_TARGET" \
     -t ic-build:"$DOCKER_IMG_TAG" \
     -t ghcr.io/dfinity/ic-build:"$DOCKER_IMG_TAG" \
     -t ghcr.io/dfinity/ic-build:latest \
