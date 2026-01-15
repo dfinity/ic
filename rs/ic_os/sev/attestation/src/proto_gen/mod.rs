96fb197da087bd5fdf0abcce568e5b6ff83a1be8
Andrew Battat <andrew.battat@dfinity.org>
2026-01-15 00:29:36 +0000

    Pre-generate protobuf code

---
+++ rs/ic_os/sev/attestation/src/proto_gen/mod.rs
@@ -0,0 +1,10 @@
+// @generated
+// This module contains pre-generated protobuf code.
+// To regenerate, run: bazel build //rs/ic_os/sev/attestation:build_script
+// and copy the output from bazel-bin/rs/ic_os/sev/attestation/build_script.out_dir/attestation.rs
+// to src/proto_gen/attestation.rs
+
+#[allow(clippy::all)]
+mod attestation;
+
+pub use attestation::*;
