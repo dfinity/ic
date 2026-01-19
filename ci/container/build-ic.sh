9955db91bd2e02bf5c9bbbe004a71e45f530e040
Carly Gundy <carlene.gundy@dfinity.org>
2026-01-19 12:39:43 +0000

    fix env var detection and update error message

--- ci/container/build-ic.sh
+++ ci/container/build-ic.sh
@@ -15,8 +15,8 @@ export ROOT_DIR="$(git rev-parse --show-toplevel)"
 # If running in the wrong container, abort.
 # if not running in a container, drop into the correct container.
 if ([ -e /.dockerenv ] || [ -e /run/.containerenv ] || [ -n "${CI_JOB_NAME:-}" ]); then
-    if [ -n "${DFINITY_CONTAINER:-}" ]; then
-        echo "ERROR: Detected container environment, but not running in the DFINITY container!"
+    if [ -z "${DFINITY_CONTAINER:-}" ]; then
+        echo "ERROR: Detected container environment, but not running in the DFINITY container. Please exit your container and rerun the script."
         exit 1
     fi
 else
