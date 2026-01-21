0838d6e426986fb5c98a5e3fdf4b444f82545cf9
Jason Zhu <jason.zhu@dfinity.org>
2026-01-21 00:28:38 +0000

    Make decode_candid_args_to_self_describing_value consistent with recent changes

--- rs/nns/governance/src/proposals/execute_nns_function_tests.rs
+++ rs/nns/governance/src/proposals/execute_nns_function_tests.rs
@@ -234,9 +234,7 @@ async fn test_to_self_describing_uninstall_code() {
         SelfDescribingValue::from(result.value.unwrap()),
         SelfDescribingValue::Map(hashmap! {
             "canister_id".to_string() => SelfDescribingValue::from(target_canister.to_string()),
-            "sender_canister_version".to_string() => SelfDescribingValue::Array(vec![
-                SelfDescribingValue::from(42_u64),
-            ]),
+            "sender_canister_version".to_string() => SelfDescribingValue::from(42_u64),
         })
     );
 }
