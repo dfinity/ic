dbc5b366cb722e4b20dfe190cbd87ae4749c88f2
Carly Gundy <47304080+cgundy@users.noreply.github.com>
2026-01-22 15:03:18 +0100

    keep comment

--- ci/container/container-run.sh
+++ ci/container/container-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-## Currently this script only supports podman as container runtime
+## This script only supports podman as container runtime
 
 eprintln() {
     echo "$@" >&2
