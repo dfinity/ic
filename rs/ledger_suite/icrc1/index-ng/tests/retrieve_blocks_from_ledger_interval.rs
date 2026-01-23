0cba7ce2558196094662973d89a87f084c501c55
Mathieu Ducroux <mathieu.ducroux@dfinity.org>
2026-01-23 12:13:34 +0100

    Update retrieve_blocks_from_ledger_interval.rs

--- rs/ledger_suite/icrc1/index-ng/tests/retrieve_blocks_from_ledger_interval.rs
+++ rs/ledger_suite/icrc1/index-ng/tests/retrieve_blocks_from_ledger_interval.rs
@@ -18,6 +18,7 @@ use proptest::prelude::Strategy;
 use proptest::test_runner::TestRunner;
 use std::time::{Duration, SystemTime};
 
+#[allow(dead_code)]
 mod common;
 
 /// Corresponds to ic_icrc1_index_ng::DEFAULT_MAX_WAIT_TIME_IN_SECS
