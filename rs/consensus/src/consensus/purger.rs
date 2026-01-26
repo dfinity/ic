ec431587e8dc1ed986bb43993406352411b35448
Martin Raszyk <martin.raszyk@dfinity.org>
2026-01-26 14:02:51 +0100

    flip

--- rs/consensus/src/consensus/purger.rs
+++ rs/consensus/src/consensus/purger.rs
@@ -318,11 +318,12 @@ impl Purger {
     fn purge_replicated_state_by_finalized_certified_height(&self, pool: &PoolReader<'_>) {
         let height = pool.get_finalized_tip().context.certified_height;
 
+        self.state_manager
+            .update_latest_subnet_certified_height(height);
+
         let extra_heights_to_keep = get_pending_idkg_cup_heights(pool);
         self.state_manager
             .remove_inmemory_states_below(height, &extra_heights_to_keep);
-        self.state_manager
-            .update_latest_subnet_certified_height(height);
         trace!(
             self.log,
             "Purge replicated states below [memory] {:?}, height to keep: {:?}",
