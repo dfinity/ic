07c99a5f495e51eaa9f50d4c50304a9d9b048929
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2026-01-30 10:16:29 +0000

    div_ceil

--- rs/execution_environment/src/query_handler/tests.rs
+++ rs/execution_environment/src/query_handler/tests.rs
@@ -491,12 +491,12 @@ fn queries_to_frozen_canisters_are_rejected() {
     //
     // 300_000_002_460 cycles are needed as prepayment for max install_code instructions
     //       5_000_000 cycles are needed for update call execution
-    //         690_250 cycles are needed for log memory store (arm64-linux)
+    //         690_250 cycles are needed for log memory store (arm64-linux 4 KiB OS page)
     //          41_070 cycles are needed to cover freeze_threshold_cycles
     //                 of the canister history memory usage (134 bytes)
-    let log_memory_store_platform_factor = 690_250 / 4096;
-    let log_memory_store_cycles = log_memory_store_platform_factor * PAGE_SIZE;
-    let low_cycles = Cycles::new(300_005_633_530) + Cycles::from(log_memory_store_cycles);
+    let log_memory_store_platform_factor = 690_250_u128.div_ceil(4096);
+    let log_memory_store_cycles = log_memory_store_platform_factor * PAGE_SIZE as u128;
+    let low_cycles = Cycles::new(300_005_633_530) + Cycles::new(log_memory_store_cycles);
 
     let canister_a = test.universal_canister_with_cycles(low_cycles).unwrap();
     test.update_freezing_threshold(canister_a, freezing_threshold)
