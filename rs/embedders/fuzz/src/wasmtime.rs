eebd851530dc2504555e764f880429cb9b073885
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2026-01-31 17:42:53 +0000

    init

--- rs/embedders/fuzz/src/wasmtime.rs
+++ rs/embedders/fuzz/src/wasmtime.rs
@@ -1,7 +1,10 @@
 use crate::ic_wasm::ICWasmModule;
 use ic_config::embedders::Config as EmbeddersConfig;
+use ic_embedders::wasmtime_embedder::system_api::ApiType;
 use ic_test_utilities_embedders::WasmtimeInstanceBuilder;
+use ic_test_utilities_types::ids::user_test_id;
 use ic_types::methods::{FuncRef, WasmMethod};
+use ic_types::{Cycles, messages::CallContextId, time::UNIX_EPOCH};
 use libfuzzer_sys::Corpus;
 use std::collections::BTreeSet;
 
@@ -15,6 +18,13 @@ pub fn run_fuzzer(module: ICWasmModule) -> Corpus {
     let instance_result = WasmtimeInstanceBuilder::new()
         .with_wasm(wasm)
         .with_config(config)
+        .with_api_type(ApiType::update(
+            UNIX_EPOCH,
+            vec![1_u8; 10],
+            Cycles::new(10_000_000_000),
+            user_test_id(24).get(),
+            CallContextId::from(0),
+        ))
         .try_build();
     let mut instance = match instance_result {
         Ok(instance) => instance,
