aa8921e0e74946f75d37b831e4a2c7f2c5397b29
Eero Kelly <eero.kelly@dfinity.org>
2026-02-02 20:40:16 +0000

    Revert "chore: speed up build-bootloader-tree (#8645)"
    This reverts commit 0642ae803fcf09292e89883875d050a6798c740b.

--- ic-os/bootloader/build-bootloader-tree.sh
+++ ic-os/bootloader/build-bootloader-tree.sh
@@ -4,16 +4,10 @@
 # the /boot/grub and /boot/efi portions. From this, the grub and
 # efi partitions of the disk image can be built.
 
-set -euo pipefail
-
-# when set, cleanup() will `rm -f` it
-CONTAINER_NAME=
+set -exo pipefail
 
 cleanup() {
-    if [ -n "${CONTAINER_NAME:-}" ]; then
-        echo >&2 "warning: have to force delete container"
-        podman rm -f "$CONTAINER_NAME"
-    fi
+    podman rm -f "${CONTAINER}"
     rm -rf "${TMP_DIR}"
 }
 trap cleanup EXIT
@@ -34,7 +28,6 @@ TMP_DIR=$(mktemp -d -t build-image-XXXXXXXXXXXX)
 
 BASE_IMAGE="ghcr.io/dfinity/library/ubuntu@sha256:6015f66923d7afbc53558d7ccffd325d43b4e249f41a6e93eef074c9505d2233"
 
-echo >&2 "building container"
 podman build --no-cache --iidfile "${TMP_DIR}/iidfile" - <<<"
     FROM $BASE_IMAGE
     USER root:root
@@ -54,12 +47,7 @@ podman build --no-cache --iidfile "${TMP_DIR}/iidfile" - <<<"
 
 IMAGE_ID=$(cut -d':' -f2 <"${TMP_DIR}/iidfile")
 
-echo >&2 "exporting container /build/boot"
-# track container with unique name in case  `podman rm -f` is needed
-# (we use TMP_DIR to avoid a dependency on uuidgen & co)
-CONTAINER_NAME=$(md5sum <<<"$TMP_DIR" | cut -d' ' -f1)
-# run the container which will print a deterministic tarball of /build/boot to stdout
-podman run --rm --name "$CONTAINER_NAME" "${IMAGE_ID}" \
-    tar cf - --sort=name --owner=root:0 --group=root:0 '--mtime=UTC 1970-01-01 00:00:00' -C /build boot \
-    >"${OUT_FILE:?out file missing}"
-CONTAINER_NAME= # unset CONTAINER_NAME so that cleanup() doesn't try to rm it
+CONTAINER=$(podman run -d "${IMAGE_ID}")
+
+podman export "${CONTAINER}" | tar --strip-components=1 -C "${TMP_DIR}" -x build
+tar cf "${OUT_FILE}" --sort=name --owner=root:0 --group=root:0 "--mtime=UTC 1970-01-01 00:00:00" -C "${TMP_DIR}" boot
