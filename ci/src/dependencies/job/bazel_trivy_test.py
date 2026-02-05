2249d26142ced70923ace82468aed672f0967f89
Thomas Müller <thomas.mueller@dfinity.org>
2026-02-05 14:58:07 +0100

    qa: write minimal bazel test file

---
+++ ci/src/dependencies/job/bazel_trivy_test.py
@@ -0,0 +1,36 @@
+import json
+import logging
+import os
+import pathlib
+
+from scanner.process_executor import ProcessExecutor
+
+PROJECT_ROOT = pathlib.Path(
+    os.environ.get("CI_PROJECT_DIR", pathlib.Path(__file__).absolute().parent.parent.parent.parent.parent)
+)
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    path = PROJECT_ROOT.parent / "tmu-trivy-debug/ic-os/guestos/envs/prod"
+    if not path.is_dir():
+        raise RuntimeError(f"path {path} is invalid")
+    json_file_path = f"{path.resolve()}/findings.json"
+    hash_file_path = f"{path.resolve()}/file-hashes.txt"
+    command = (
+        f"bazel run vuln-scan -- --output-path {json_file_path} --format json --hash-output-path {hash_file_path}"
+    )
+    trivy_output = ProcessExecutor.execute_command(command, path.resolve(), {})
+    if os.path.exists(json_file_path):
+        with open(json_file_path, "r") as file:
+            if os.fstat(file.fileno()).st_size == 0:
+                logging.error(f"trivy scan attempt failed (file size 0) with output:\n{trivy_output}")
+            else:
+                trivy_data = json.load(file)
+                logging.info(trivy_data)
+    else:
+        logging.error(f"trivy scan attempt failed ({json_file_path} doesn't exists) with output:\n{trivy_output}")
+
+
+if __name__ == "__main__":
+    main()
