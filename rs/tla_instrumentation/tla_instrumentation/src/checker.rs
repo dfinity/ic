aba08e18cbad71c793b618474a837f45bd185f14
Nicolas Mattia <nicolas.mattia@dfinity.org>
2026-02-06 16:47:00 +0100

    chore: migrate build to Bazel 8.5.1 (#8602)
    This bumps our bazel version in `.bazelversion` and upgrades a few
    rules, macros and targets that need adapting:
    
    * In general, `~` was replaced with `+` in (canonical) labels
    * Some bazel modules dependencies were updated
    * The `apalache-mc` binary was wrapped in a `java_binary` to let Bazel
    handle the call to `java` (the old way broke in Bazel 8, and this new
    way breaks in other ways in Bazel 7 so was not done separately)
    
    NOTE: the Java update seemingly surfaced a race condition in the
    apalache/TLA tests. When multiple threads/processes try to access the
    TLA modules concurrently some accesses error out. For this reason we set
    a hard limit (1) on the number of apalache processes.
    
    ---------
    
    Co-authored-by: IDX GitHub Automation <infra+github-automation@dfinity.org>
    Co-authored-by: IDX GitHub Automation <>
    Co-authored-by: Bas van Dijk <bas@dfinity.org>

--- rs/tla_instrumentation/tla_instrumentation/src/checker.rs
+++ rs/tla_instrumentation/tla_instrumentation/src/checker.rs
@@ -156,17 +156,22 @@ fn run_apalache(
     // TODO: There's a race condition when running multiple instances of Apalache in parallel,
     // as they all seem to try to write to the same file in /tmp. So we create a new temporary
     // directory for each run of Apalache based on the Bazel-provided temporary directory
-    // and then feed that to the JRE, using the Apalache-specific JVM_ARGS environment variable.
+    // and then feed that to the JRE.
+    // https://github.com/tlaplus/tlaplus/issues/688
     let tmp_subdir_str = unique_tmp_dir();
-    // Construct the JVM_ARGS value so that Apalache uses the new temporary subdirectory
-    let jvm_args = format!("-Djava.io.tmpdir={tmp_subdir_str}");
+    // Construct JVM args so that Apalache uses the new temporary subdirectory as well as
+    // some memory options from apalache-mc's bash wrapper
+    // https://github.com/apalache-mc/apalache/blob/18e8a0b748b913c7df66013226b15660ee670301/src/universal/bin/apalache-mc#L48-L48
+    let jvm_args = format!(
+        "-Djava.io.tmpdir={tmp_subdir_str} -Xmx4096m  -XX:+UseG1GC -XX:G1PeriodicGCInterval=600000 -XX:+G1PeriodicGCInvokesConcurrent"
+    );
 
     cmd.arg("check")
         .arg(format!("--init={init_predicate}"))
         .arg(format!("--next={next_predicate}"))
         .arg("--length=1")
         .arg(tla_module)
-        .env("JVM_ARGS", jvm_args);
+        .env("JDK_JAVA_OPTIONS", jvm_args);
     cmd.status()
         .map_err(|e| ApalacheError::SetupError(e.to_string()))
         .and_then(|e| {
