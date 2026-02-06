98cb0f1b1f8c8eb0c4918beabd738a06256661f0
Thomas Müller <thomas.mueller@dfinity.org>
2026-02-06 10:57:18 +0100

    qa: use dep-scan

--- ci/src/dependencies/config/bazel_trivy_periodic.py
+++ ci/src/dependencies/config/bazel_trivy_periodic.py
@@ -1,1 +1,18 @@
-REPOS_TO_SCAN = []
+from model.project import Project
+from model.repository import Repository
+from model.team import Team
+
+REPOS_TO_SCAN = [
+    Repository(
+        "ic",
+        "https://github.com/dfinity/ic",
+        [
+            Project(
+                name="guestos",
+                path="tmu-trivy-debug/ic-os/guestos/envs/prod",  # TODO: change to master
+                link="https://github.com/dfinity/ic/tree/tmu-trivy-debug/ic-os/guestos/context",  # TODO: change to master
+                owner=Team.NODE_TEAM,
+            ),
+        ],
+    )
+]
