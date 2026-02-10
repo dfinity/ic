840f2c9fa46f575752295ac89f4a1090436b62e1
Kamil Popielarz <kamil.popielarz@dfinity.org>
2026-02-10 08:09:09 +0000

    fix tests

--- rs/nns/integration_tests/src/subnet_handler.rs
+++ rs/nns/integration_tests/src/subnet_handler.rs
@@ -35,6 +35,7 @@ fn test_submit_and_accept_update_subnet_proposal() {
             let initial_subnet_record = SubnetRecord {
                 membership: vec![],
                 max_ingress_bytes_per_message: 60 * 1024 * 1024,
+                max_ingress_bytes_per_block: 4 * 1024 * 1024,
                 max_ingress_messages_per_block: 1000,
                 max_block_payload_size: 4 * 1024 * 1024,
                 unit_delay_millis: 500,
@@ -153,6 +154,7 @@ fn test_submit_and_accept_update_subnet_proposal() {
                 SubnetRecord {
                     membership: vec![],
                     max_ingress_bytes_per_message: 10 * 1024 * 1024,
+                    max_ingress_bytes_per_block: 4 * 1024 * 1024,
                     max_ingress_messages_per_block: 1000,
                     max_block_payload_size: 4 * 1024 * 1024,
                     unit_delay_millis: 500,
