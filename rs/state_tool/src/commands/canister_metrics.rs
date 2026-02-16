e4349a6a039e12f0e541ab21da72ee945f721d8c
Kamil Popielarz <kamil.popielarz@dfinity.org>
2026-02-16 14:08:52 +0000

    println

--- rs/state_tool/src/commands/canister_metrics.rs
+++ rs/state_tool/src/commands/canister_metrics.rs
@@ -63,5 +63,7 @@ pub fn get(checkpoint_dir: PathBuf, output_path: &Path) -> Result<(), String> {
         .map_err(|err| format!("Failed to write row: {err}"))?;
     }
 
+    println!("Number of canisters: {}", replicated_state.canister_states().len());
+
     Ok(())
 }
