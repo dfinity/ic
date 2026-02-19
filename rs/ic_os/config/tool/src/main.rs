b5c7b9f32a86d99a2d161c1bb52845c11ed9111a
David Frank <david.frank@dfinity.org>
2026-02-19 03:32:42 +0100

    fix: Populate NNS key early on
    #8562 moved populating the NNS key to bootstrap-ic-node. However, that is too late. The guest-upgrade-client service in the Upgrade GuestVM needs the NNS key already before bootstrap-ic-node could run. So guest-upgrade-client fails without this change, which prevents SEV nodes from upgrading.

--- rs/ic_os/config/tool/src/main.rs
+++ rs/ic_os/config/tool/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Error, Result, bail};
 use clap::{Parser, Subcommand};
-use config_tool::guestos::bootstrap_ic_node::bootstrap_ic_node;
+use config_tool::guestos::bootstrap_ic_node::{bootstrap_ic_node, populate_nns_public_key};
 use config_tool::guestos::generate_ic_config;
 use config_tool::serialize_and_write_config;
 use config_tool::setupos::config_ini::{ConfigIniSettings, get_config_ini_settings};
@@ -60,6 +60,12 @@ pub enum Commands {
             value_name = "node_operator_private_key.pem"
         )]
         node_operator_private_key_path: PathBuf,
+    },
+    PopulateNnsPublicKey {
+        #[arg(long, default_value = config_tool::DEFAULT_GUESTOS_CONFIG_OBJECT_PATH, value_name = "config-guestos.json")]
+        guestos_config_json_path: PathBuf,
+        #[arg(long, default_value = config_tool::DEFAULT_BOOTSTRAP_DIR, value_name = "bootstrap_dir")]
+        bootstrap_dir: PathBuf,
     },
     /// Dumps OS configuration
     DumpOSConfig {
@@ -228,6 +234,14 @@ pub fn main() -> Result<()> {
                 config_tool::deserialize_config(&guestos_config_json_path)?;
 
             bootstrap_ic_node(&bootstrap_dir, guestos_config)
+        }
+        Some(Commands::PopulateNnsPublicKey {
+            guestos_config_json_path,
+            bootstrap_dir,
+        }) => {
+            let guestos_config: GuestOSConfig =
+                config_tool::deserialize_config(&guestos_config_json_path)?;
+            populate_nns_public_key(&bootstrap_dir, &guestos_config)
         }
         Some(Commands::GenerateICConfig {
             guestos_config_json_path,
