2f613f3a290e063ae511c14fe7d18b7d3b97c77b
Kamil Popielarz <kamil.popielarz@dfinity.org>
2026-02-24 16:17:24 +0000

    ref

--- rs/tests/consensus/subnet_recovery/sr_app_failover_nodes_test.rs
+++ rs/tests/consensus/subnet_recovery/sr_app_failover_nodes_test.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
 use ic_consensus_system_test_subnet_recovery::common::{
-    setup_failover_nodes as setup, test_without_chain_keys as test,
+    CupCorruption, setup_failover_nodes as setup, test_without_chain_keys as test,
 };
 use ic_system_test_driver::driver::group::SystemTestGroup;
 use ic_system_test_driver::systest;
@@ -9,7 +9,7 @@ use ic_system_test_driver::systest;
 fn main() -> Result<()> {
     SystemTestGroup::new()
         .with_setup(setup)
-        .add_test(systest!(test))
+        .add_test(systest!(test; CupCorruption::CorruptedIncludingInvalidNiDkgId))
         .without_assert_no_replica_restarts()
         // The test corrupts the CUPs, so it's expected that the following error metric will be
         // non-zero.
--- rs/tests/consensus/subnet_recovery/sr_app_no_upgrade_with_chain_keys_test.rs
+++ rs/tests/consensus/subnet_recovery/sr_app_no_upgrade_with_chain_keys_test.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
 use ic_consensus_system_test_subnet_recovery::common::{
-    CHAIN_KEY_SUBNET_RECOVERY_TIMEOUT, setup_same_nodes_chain_keys as setup,
+    CHAIN_KEY_SUBNET_RECOVERY_TIMEOUT, CupCorruption, setup_same_nodes_chain_keys as setup,
     test_no_upgrade_with_chain_keys as test,
 };
 use ic_system_test_driver::driver::group::SystemTestGroup;
@@ -12,7 +12,9 @@ fn main() -> Result<()> {
         .with_timeout_per_test(CHAIN_KEY_SUBNET_RECOVERY_TIMEOUT)
         .with_setup(setup)
         .without_assert_no_replica_restarts()
-        .add_test(systest!(test))
+        // A corrupted CUP whose NiDkgId can still be parsed can tell nodes to which subnet they
+        // belong to, see the recovery CUP, and thus allow the recovery on the same nodes
+        .add_test(systest!(test; CupCorruption::CorruptedWithValidNiDkgId))
         // The test corrupts the CUPs, so it's expected that the following error metric will be
         // non-zero.
         .remove_metrics_to_check("orchestrator_cup_deserialization_failed_total")
