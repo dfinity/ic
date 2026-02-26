7a6e6343b6262a67b5fa6d89467dad2cfe56a389
Dimitris Sarlis <dimitrios.sarlis@dfinity.org>
2026-02-26 14:21:11 +0200

    feat: Check subnet admins in deterministic state machine (#8912)
    This PR builds on top of https://github.com/dfinity/ic/pull/8731 to add
    the checks related to the new subnet admins field and actions they can
    perform on canisters. Specifically, the subnet admins can `stop`,
    `start`, `uninstall`, `delete` and get the status of canisters but they
    cannot for example `install_code` on them.
    
    The field is read from the registry and stored in the `NetworkTopology`
    which can then be read by the Deterministic State Machine to perform any
    necessary checks. A new function `validate_controller_or_subnet_admin`
    is added that performs the required checks. This function replaces the
    old one in the endpoints that are allowed to be called by subnet admins.
    In case the subnet admins field is empty, this function returns the same
    error as before (for backward compatibility), otherwise a new error is
    returned that captures the fact that the caller is neither a controller
    or subnet admin.
    
    Given that the ability to update the admins list is still under
    development (see https://github.com/dfinity/ic/pull/8911), this PR
    should currently act a no-op as all subnets have an empty subnet admins
    list.
    
    ---------
    
    Co-authored-by: mraszyk <31483726+mraszyk@users.noreply.github.com>

--- rs/state_manager/tests/state_manager.rs
+++ rs/state_manager/tests/state_manager.rs
@@ -5493,6 +5493,7 @@ fn certified_read_can_certify_node_public_keys_since_v12() {
                 subnet_features: SubnetFeatures::default(),
                 chain_keys_held: BTreeSet::new(),
                 cost_schedule: CanisterCyclesCostSchedule::Normal,
+                subnet_admins: BTreeSet::new(),
             },
         );
 
@@ -5891,6 +5892,7 @@ fn certified_read_can_exclude_canister_ranges() {
                     subnet_features: SubnetFeatures::default(),
                     chain_keys_held: BTreeSet::new(),
                     cost_schedule: CanisterCyclesCostSchedule::Normal,
+                    subnet_admins: BTreeSet::new(),
                 },
             );
             routing_table
