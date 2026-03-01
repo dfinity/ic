d1a81e6ac90c67179f7a093bbe0d09533633aae7
Franz-Stefan Preiss <franzstefan@dfinity.org>
2026-03-01 01:23:22 +0100

    refactor(flexible-outcalls): remove obsolete subnet_size param from context's generate_from_args (#9037)
    Removes the obsolete `subnet_size` param from
    `CanisterHttpRequestContext::generate_from_args` and use
    `node_ids.len()` instead, while rejecting empty node_ids with
    `NoNodesAvailableForDelegation`, so as to make the API less confusing.
    
    This is done because in production, `subnet_size` and `node_ids.len()`
    are the same, except for the special (critical error) case when the
    subnet record's `membership` is empty:
    * `node_ids` is derived from the registry's subnet record's membership
    [here](https://github.com/dfinity/ic/blob/4bb874fc5cc90e25ef1b003f932a48da40eb34a6/rs/messaging/src/message_routing.rs#L862-L869)
    (de-duplicated via `BTreeSet`)
    * `subnet_size` is also calculated from the registry's subnet record's
    membership
    [here](https://github.com/dfinity/ic/blob/master/rs/messaging/src/message_routing.rs#L905-L919)
    (de-duplicated via `BTreeSet`), **BUT** there is a fallback when the
    membership is empty, namely to `SMALL_APP_SUBNET_MAX_SIZE` (13) with a
    warn! log (while the `node_ids` remain empty).
    * Also in some tests, the two [can
    diverge](https://github.com/dfinity/ic/blob/4bb874fc5cc90e25ef1b003f932a48da40eb34a6/rs/test_utilities/execution_environment/src/lib.rs#L206-L215),
    which is irrelevant here.

--- rs/execution_environment/src/execution_environment.rs
+++ rs/execution_environment/src/execution_environment.rs
@@ -1104,7 +1104,6 @@ impl ExecutionEnvironment {
                                         request.as_ref(),
                                         args,
                                         &registry_settings.node_ids,
-                                        registry_settings.subnet_size,
                                         rng,
                                     ) {
                                         Err(err) => ExecuteSubnetMessageResult::Finished {
