b30bb030476fc5303c375b80b55d90ed8cc12f52
kpop-dfinity <125868903+kpop-dfinity@users.noreply.github.com>
2026-03-02 18:59:13 +0100

    test: also check that orchestrator error metrics are not increased (#8949)
    Added checks that none of 
    - `orchestrator_cup_deserialization_failed_total`,
    - `orchestrator_state_removal_failed_total`,
    - `orchestrator_tasks_failed_total`,
    
    
    error metrics were increased during the test. 
    
    Also:
    - made it a bit more explicit that some of the tests are corrupting CUPs
    - disabled the block size limit in `dual_workload_test` - it was too
    small and was generating a critical error

--- rs/tests/consensus/subnet_recovery/sr_app_no_upgrade_enable_chain_keys_test.rs
+++ rs/tests/consensus/subnet_recovery/sr_app_no_upgrade_enable_chain_keys_test.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 
 use ic_consensus_system_test_subnet_recovery::common::{
-    CHAIN_KEY_SUBNET_RECOVERY_TIMEOUT, setup_same_nodes as setup,
+    CHAIN_KEY_SUBNET_RECOVERY_TIMEOUT, CupCorruption, setup_same_nodes as setup,
     test_no_upgrade_with_chain_keys as test,
 };
 use ic_system_test_driver::driver::group::SystemTestGroup;
@@ -12,7 +12,7 @@ fn main() -> Result<()> {
         .with_timeout_per_test(CHAIN_KEY_SUBNET_RECOVERY_TIMEOUT)
         .with_setup(setup)
         .without_assert_no_replica_restarts()
-        .add_test(systest!(test))
+        .add_test(systest!(test; CupCorruption::NotCorrupted))
         .execute_from_args()?;
     Ok(())
 }
