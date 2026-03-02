aef2bddaafc056471d6d01c6780ba167e3deb437
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2026-03-02 12:00:49 +0000

    fix: fix division by zero in scheduler

--- rs/execution_environment/src/scheduler.rs
+++ rs/execution_environment/src/scheduler.rs
@@ -762,10 +762,12 @@ impl SchedulerImpl {
             max_instructions_executed_per_thread =
                 max_instructions_executed_per_thread.max(instructions_executed);
 
-            self.metrics.compute_utilization_per_core.observe(
-                instructions_executed.get() as f64
-                    / round_limits_per_thread.instructions.get() as f64,
-            );
+            let divisor = round_limits_per_thread.instructions.get();
+            if divisor > 0 {
+                // Avoid division by zero.
+                let value = instructions_executed.get() as f64 / divisor as f64;
+                self.metrics.compute_utilization_per_core.observe(value);
+            }
 
             // Propagate the metrics from `execution_round_inner_iteration_thread`
             // to `execution_round_inner_iteration`.
