17280091791c4b8f35a865e04b92c4e3cfbbf04d
Andrew Battat <andrew.battat@dfinity.org>
2026-03-03 21:02:59 +0000

    Add comment regarding stripping ANSI color/escape codes

--- ic-os/components/monitoring/hostos/export-guestos-serial-logs.sh
+++ ic-os/components/monitoring/hostos/export-guestos-serial-logs.sh
@@ -4,6 +4,7 @@
 
 set -e
 
+# Strip ANSI color/escape codes from serial console output before forwarding to journald
 tail -F /var/log/libvirt/qemu/guestos-serial.log | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g; s/\[[0-9]\+;[0-9;]*m//g' | systemd-cat -t guestos-serial -p info &
 tail -F /var/log/libvirt/qemu/upgrade-guestos-serial.log | sed 's/\x1b\[[0-9;]*[a-zA-Z]//g; s/\[[0-9]\+;[0-9;]*m//g' | systemd-cat -t upgrade-guestos-serial -p info &
 wait
