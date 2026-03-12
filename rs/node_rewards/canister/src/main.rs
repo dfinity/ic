bbb47e8453c127f933aab4075303577929295de2
IDX GitHub Automation <infra+github-automation@dfinity.org>
2026-03-12 16:31:17 +0000

    Automatically fixing code for linting and formatting issues

--- rs/node_rewards/canister/src/main.rs
+++ rs/node_rewards/canister/src/main.rs
@@ -2,7 +2,9 @@ use ic_cdk::api::in_replicated_execution;
 use ic_cdk::{init, post_upgrade, pre_upgrade, query, update};
 use ic_http_types::{HttpRequest, HttpResponse, HttpResponseBuilder};
 use ic_nervous_system_canisters::registry::RegistryCanister;
-use ic_nervous_system_timer_task::{RecurringAsyncTask, RecurringSyncTask, TimerTaskMetricsRegistry};
+use ic_nervous_system_timer_task::{
+    RecurringAsyncTask, RecurringSyncTask, TimerTaskMetricsRegistry,
+};
 use ic_nns_constants::GOVERNANCE_CANISTER_ID;
 use ic_node_rewards_canister::canister::NodeRewardsCanister;
 use ic_node_rewards_canister::storage::{
