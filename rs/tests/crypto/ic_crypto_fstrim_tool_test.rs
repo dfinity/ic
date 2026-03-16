9e265172c373a5fccc70e850df6ac861cb79e9ee
IDX GitHub Automation <infra+github-automation@dfinity.org>
2026-03-16 10:51:11 +0000

    Automatically fixing code for linting and formatting issues

--- rs/tests/crypto/ic_crypto_fstrim_tool_test.rs
+++ rs/tests/crypto/ic_crypto_fstrim_tool_test.rs
@@ -121,8 +121,7 @@ fn retrieve_fstrim_metrics(node: &IcNodeSnapshot, logger: &Logger) -> FsTrimMetr
 }
 
 fn initialize_fstrim_metrics(node: &IcNodeSnapshot, logger: &Logger) {
-    const INITIALIZE_FSTRIM_METRICS_CMD: &str =
-        "sudo systemctl start setup-fstrim-tool.service";
+    const INITIALIZE_FSTRIM_METRICS_CMD: &str = "sudo systemctl start setup-fstrim-tool.service";
     info!(
         logger,
         "initializing fstrim metrics using command: {}", INITIALIZE_FSTRIM_METRICS_CMD
