e3870c8aa53b27bbc959e5662a9bd2afd6316925
IDX GitHub Automation <infra+github-automation@dfinity.org>
2026-03-17 14:02:32 +0000

    Automatically fixing code for linting and formatting issues

--- pre-commit/do-not-merge-check.sh
+++ pre-commit/do-not-merge-check.sh
@@ -5,7 +5,7 @@ set -ueo pipefail
 cd "${BUILD_WORKSPACE_DIRECTORY:?Expected to run from bazel}"
 
 ret="0"
-git grep -I -i -n -E "DO[^\w]?NOT[^\w]?MERGE" . ":(exclude)pre-commit/BUILD.bazel" ":(exclude)pre-commit/do-not-merge-check.sh" ":(exclude).github/workflows/ci-pr-only.yml"|| ret="$?"
+git grep -I -i -n -E "DO[^\w]?NOT[^\w]?MERGE" . ":(exclude)pre-commit/BUILD.bazel" ":(exclude)pre-commit/do-not-merge-check.sh" ":(exclude).github/workflows/ci-pr-only.yml" || ret="$?"
 
 case "$ret" in
     0)
