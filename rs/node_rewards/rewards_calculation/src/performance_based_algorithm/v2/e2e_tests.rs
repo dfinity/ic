09665462654fc60f3460dc5b75b5919bcdfc141f
IDX GitHub Automation <infra+github-automation@dfinity.org>
2026-03-17 14:08:32 +0000

    Automatically fixing code for linting and formatting issues

--- rs/node_rewards/rewards_calculation/src/performance_based_algorithm/v2/e2e_tests.rs
+++ rs/node_rewards/rewards_calculation/src/performance_based_algorithm/v2/e2e_tests.rs
@@ -1,8 +1,10 @@
-use crate::performance_based_algorithm::{PerformanceBasedAlgorithm, PerformanceBasedAlgorithmInputProvider};
 use crate::performance_based_algorithm::test_utils::{
     test_node_id, test_provider_id, test_subnet_id,
 };
 use crate::performance_based_algorithm::v2::RewardsCalculationV2;
+use crate::performance_based_algorithm::{
+    PerformanceBasedAlgorithm, PerformanceBasedAlgorithmInputProvider,
+};
 use crate::types::{NodeMetricsDailyRaw, RewardableNode};
 use chrono::NaiveDate;
 use futures::FutureExt;
