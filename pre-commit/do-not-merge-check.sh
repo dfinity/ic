d71ca79571015a66f06052bbfcc816d4964c9ab3
Nicolas Mattia <nicolas.mattia@dfinity.org>
2026-03-17 12:41:29 +0100

    chore: Run linters with 'bazel run'
    Those checks were previously included as bazel tests, but required all
    sandboxing to be disabled and could not actually track their inputs.
    
    Instead we now run them with `bazel run` so that they have access to the
    workspace without any workarounds.

---
+++ pre-commit/do-not-merge-check.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+cd "${BUILD_WORKSPACE_DIRECTORY:?Expected to run from bazel}"
+
+ret="0"
+git grep -I -i -n -E "DO[^\w]?NOT[^\w]?MERGE" . ":(exclude)pre-commit/BUILD.bazel" ":(exclude)pre-commit/do-not-merge-check.sh" ":(exclude).github/workflows/ci-pr-only.yml"|| ret="$?"
+
+case "$ret" in
+    0)
+        echo "[-] Cannot merge - DO NOT MERGE present in this MR" >&2
+        exit 1
+        ;;
+    1)
+        exit 0
+        ;;
+    *)
+        echo "The command exited with the code $ret" >&2
+        exit "$ret"
+        ;;
+esac
