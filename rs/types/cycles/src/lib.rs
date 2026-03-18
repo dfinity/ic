987cc4d6f173cb6618922ca3cd1d0ffee32e4e53
Dimitris Sarlis <sarlis.dim@gmail.com>
2026-03-18 08:45:14 +0000

    chore: Make modules in cycles crate private

--- rs/types/cycles/src/lib.rs
+++ rs/types/cycles/src/lib.rs
@@ -1,6 +1,6 @@
-pub mod cycles;
-pub mod cycles_use_case;
-pub mod nominal_cycles;
+mod cycles;
+mod cycles_use_case;
+mod nominal_cycles;
 
 pub use cycles::Cycles;
 pub use cycles_use_case::CyclesUseCase;
