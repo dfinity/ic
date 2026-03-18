333c47b2e791036fd64cc5761e2c1ce1504c52cd
Daniel Wong <daniel.wong@dfinity.org>
2026-03-18 10:55:43 +0000

    Splay out the many clippy flags, explain them, and put them into the recommended fix command.

--- ci/scripts/rust-lint.sh
+++ ci/scripts/rust-lint.sh
@@ -5,12 +5,35 @@ cd "${CI_PROJECT_DIR:-$(git rev-parse --show-toplevel)}"
 
 cargo fmt -- --check
 
-if ! cargo clippy --locked --all-features --workspace --all-targets --keep-going -- \
-    -D warnings \
-    -D clippy::all \
-    -D clippy::mem_forget \
-    -D clippy::unseparated_literal_suffix \
-    -A clippy::uninlined_format_args; then
+clippy_args=(
+    # Do not modify Cargo.lock.
+    --locked
+
+    # For comprehensiveness.
+    --all-features
+    --workspace
+    --all-targets
+
+    # Don't stop at the first error, but rather, report ALL of them.
+    --keep-going
+
+    # Everything after this is forwarded from cargo to the clippy binary itself.
+    --
+
+    # Be strict.
+    --deny warnings
+    --deny clippy::all
+
+    # Ban std::mem::forget, because it is a memory leak hazard.
+    --deny clippy::mem_forget
+
+    # Require 42_u64, as opposed to 42u64. Because spaces good.
+    --deny clippy::unseparated_literal_suffix
+
+    # Allow format!("{}", x) instead of format!("{x}").
+    --allow clippy::uninlined_format_args
+)
+if ! cargo clippy "${clippy_args[@]}"; then
     # Don't just explode: provide a solution. Our job is to provide
     # solid gold, not raw ore.
     echo ""
@@ -22,7 +45,7 @@ if ! cargo clippy --locked --all-features --workspace --all-targets --keep-going
     echo -ne "\033[1;32m" # Start green.
     echo "To automatically fix many of these, run:"
     echo ""
-    echo "    cargo clippy --fix"
+    echo "    cargo clippy --fix ${clippy_args[@]}"
     echo -ne "\033[0m" # Clear formatting.
     echo ""
     echo "========================================"
