cd651a7f7122804016cceadba3d9675e50d9f1a9
Daniel Wong <daniel.wong@dfinity.org>
2026-03-18 10:02:11 +0000

    Enforce 42_u64, not 42u64. This will be merged into master once all currently non-compliant code is fixed.

--- ci/scripts/rust-lint.sh
+++ ci/scripts/rust-lint.sh
@@ -2,12 +2,32 @@
 set -xeuo pipefail
 
 cd "${CI_PROJECT_DIR:-$(git rev-parse --show-toplevel)}"
+
 cargo fmt -- --check
-cargo clippy --locked --all-features --workspace --all-targets --keep-going -- \
+
+if ! cargo clippy --locked --all-features --workspace --all-targets --keep-going -- \
     -D warnings \
     -D clippy::all \
     -D clippy::mem_forget \
-    -A clippy::uninlined_format_args
+    -D clippy::unseparated_literal_suffix \
+    -A clippy::uninlined_format_args; then
+    # Don't just explode: provide a solution. Our job is to provide
+    # solid gold, not raw ore.
+    echo ""
+    echo "========================================"
+    echo -ne "\033[1;31m"  # Start red.
+    echo "Clippy violations found!"
+    echo -ne "\033[0m"     # Clear formatting.
+    echo ""
+    echo -ne "\033[1;32m"  # Start green.
+    echo "To automatically fix many of these, run:"
+    echo ""
+    echo "    cargo clippy --fix"
+    echo -ne "\033[0m"     # Clear formatting.
+    echo ""
+    echo "========================================"
+    exit 1
+fi
 
 if cargo tree --workspace --depth 1 -e features | grep -q 'serde feature "rc"'; then
     echo 'The serde "rc" feature seems to be enabled. Instead, the module "serde_arc" in "ic-utils" should be used.'
