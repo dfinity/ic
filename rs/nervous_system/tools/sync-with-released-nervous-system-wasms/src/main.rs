8f25817572e11e01b73812717147f01a109517a8
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2026-03-18 13:52:41 +0000

    Set test_filename to None for internet_identity_backend in rs/nervous_system/tools/sync-with-released-nervous-system-wasms/src/main.rs

--- rs/nervous_system/tools/sync-with-released-nervous-system-wasms/src/main.rs
+++ rs/nervous_system/tools/sync-with-released-nervous-system-wasms/src/main.rs
@@ -59,7 +59,7 @@ const EXTERNAL_CANISTER_NAME_TO_INFO: [(&str, ExternalCanisterInfo); 8] = [
             repository: "dfinity/internet-identity",
             tag_name_prefix: None,
             filename: "internet_identity_production.wasm.gz",
-            test_filename: Some("internet_identity_production.wasm.gz"),
+            test_filename: None,
             canister_id: IDENTITY_CANISTER_ID,
         },
     ),
