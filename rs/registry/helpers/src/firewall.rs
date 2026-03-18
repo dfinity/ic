af1b74404ee1914f9a03d06b5c0a672566581114
Pierugo Pace <pierugo.pace@dfinity.org>
2026-03-18 15:26:05 +0000

    test: reput test

--- rs/registry/helpers/src/firewall.rs
+++ rs/registry/helpers/src/firewall.rs
@@ -117,3 +117,95 @@ impl<T: RegistryClient + ?Sized> FirewallRegistry for T {
         Vec::from_iter(ip_addresses)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ic_protobuf::registry::node::v1::{ConnectionEndpoint, NodeRecord};
+    use ic_registry_client_fake::FakeRegistryClient;
+    use ic_registry_keys::make_node_record_key;
+    use ic_registry_proto_data_provider::ProtoRegistryDataProvider;
+    use ic_types::PrincipalId;
+    use std::sync::Arc;
+
+    // Helper function to create a registry client with the provided information.
+    fn create_test_registry_client(
+        registry_version: RegistryVersion,
+        node_records: Vec<(NodeId, NodeRecord)>,
+    ) -> Arc<dyn RegistryClient> {
+        let data_provider = Arc::new(ProtoRegistryDataProvider::new());
+
+        for (node_id, node_record) in node_records.into_iter() {
+            data_provider
+                .add(
+                    &make_node_record_key(node_id),
+                    registry_version,
+                    Some(node_record),
+                )
+                .unwrap();
+        }
+
+        let registry = Arc::new(FakeRegistryClient::new(data_provider));
+        registry.update_to_latest_version();
+        registry as Arc<dyn RegistryClient>
+    }
+
+    #[test]
+    fn test_get_available_ip_addresses_for_node_ids() {
+        let test_ip_addrs: Vec<IpAddr> = vec![
+            "1::".parse().unwrap(),
+            "2::".parse().unwrap(),
+            "3::".parse().unwrap(),
+            "4::".parse().unwrap(),
+            "10.0.0.1".parse().unwrap(),
+            "10.0.0.2".parse().unwrap(),
+            "192.168.0.1".parse().unwrap(),
+        ];
+        let test_records: Vec<(NodeId, NodeRecord)> = test_ip_addrs
+            .iter()
+            .cloned()
+            .enumerate()
+            .map(|(id, ip)| {
+                (
+                    NodeId::from(PrincipalId::new_node_test_id(id as u64)),
+                    NodeRecord {
+                        http: Some(ConnectionEndpoint {
+                            ip_addr: ip.to_string(),
+                            port: 8080,
+                        }),
+                        xnet: Some(ConnectionEndpoint {
+                            ip_addr: ip.to_string(),
+                            port: 2457,
+                        }),
+                        ..Default::default()
+                    },
+                )
+            })
+            .collect();
+
+        let version = RegistryVersion::from(2);
+
+        let registry = create_test_registry_client(version, test_records.clone());
+        let ip_addrs = registry.get_available_ip_addresses_for_node_ids(
+            test_records
+                .iter()
+                .map(|(node_id, _)| *node_id)
+                .chain(
+                    // Add some node IDs that do not exist in the registry to ensure they are
+                    // skipped without error.
+                    [
+                        NodeId::from(PrincipalId::new_node_test_id(999)),
+                        NodeId::from(PrincipalId::new_node_test_id(1000)),
+                    ],
+                )
+                .collect::<Vec<_>>(),
+            version,
+        );
+
+        // Compare as sets since the order of IP addresses is not guaranteed.
+        assert_eq!(
+            HashSet::<IpAddr>::from_iter(ip_addrs),
+            HashSet::<IpAddr>::from_iter(test_ip_addrs)
+        );
+    }
+}
