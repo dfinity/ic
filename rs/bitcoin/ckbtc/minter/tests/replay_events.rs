b09df6568896b2a5c4a0db64971ffaa55f8dd890
Grégory Demay <gregory.demay@dfinity.org>
2026-03-19 15:40:24 +0000

    DEFI-2697: update ckBTC events

--- rs/bitcoin/ckbtc/minter/tests/replay_events.rs
+++ rs/bitcoin/ckbtc/minter/tests/replay_events.rs
@@ -104,7 +104,7 @@ async fn should_replay_events_for_mainnet() {
         .expect("Failed to check invariants");
 
     assert_eq!(state.btc_network, Network::Mainnet);
-    assert_eq!(state.get_total_btc_managed(), 28_947_158_445);
+    assert_eq!(state.get_total_btc_managed(), 28_544_468_650);
 }
 
 #[tokio::test]
