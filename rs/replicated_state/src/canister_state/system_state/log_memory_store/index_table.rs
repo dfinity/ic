7f03523905e8fbea3a840270af0bc16197cc061c
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2026-03-19 15:17:54 +0000

    rm debug assert

--- rs/replicated_state/src/canister_state/system_state/log_memory_store/index_table.rs
+++ rs/replicated_state/src/canister_state/system_state/log_memory_store/index_table.rs
@@ -98,7 +98,6 @@ impl IndexTable {
         let entries = if entries.is_empty() {
             vec![IndexEntry::invalid(); entries_count]
         } else {
-            debug_assert_eq!(entries.len(), entries_count, "Entry count mismatch",);
             entries
         };
         Self {
