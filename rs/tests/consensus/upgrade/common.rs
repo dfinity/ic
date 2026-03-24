b4ee85b89e60c197aaab25f61179a483172cb59a
Pierugo Pace <pierugo.pace@dfinity.org>
2026-03-24 12:45:00 +0000

    revert upgrade changes

--- rs/tests/consensus/upgrade/common.rs
+++ rs/tests/consensus/upgrade/common.rs
@@ -27,7 +27,7 @@ use ic_consensus_threshold_sig_system_test_utils::run_chain_key_signature_test;
 use ic_management_canister_types::{CanisterId, TakeCanisterSnapshotArgs};
 use ic_management_canister_types_private::MasterPublicKeyId;
 use ic_registry_subnet_type::SubnetType;
-use ic_system_test_driver::util::create_agent;
+use ic_system_test_driver::util::{LogStream, create_agent};
 use ic_system_test_driver::{
     driver::{test_env::TestEnv, test_env_api::*},
     util::{MessageCanister, block_on},
@@ -287,6 +287,10 @@ async fn upgrade_to(
     target_version: &ReplicaVersion,
     logger: &Logger,
 ) {
+    let log_streams = LogStream::open(healthy_nodes.iter().cloned())
+        .await
+        .unwrap();
+
     info!(
         logger,
         "Upgrading subnet {} to {}", subnet_id, target_version
@@ -298,36 +302,27 @@ async fn upgrade_to(
         "Checking that all nodes produced a log indicating that the orchestrator has gracefully shut down...",
     );
 
-    // Concurrently assert that all orchestrators shut down gracefully
-    try_join_all(healthy_nodes.iter().map(|node| {
-        let session = node.block_on_ssh_session().unwrap();
-        session.set_timeout(5 * 60 * 1000);
-        let node_id = node.node_id;
-        tokio::task::spawn_blocking(move || {
-            if !JournalStreamer::new(session)
-                .follow()
-                .max_lines(1)
-                .search("Orchestrator shut down gracefully")
-                .is_ok_and(|matches| !matches.is_empty())
-            {
-                panic!("{node_id} did not log that the orchestrator has shut down gracefully");
-            }
+    // Concurrently assert that all orchestrators shut down gracefully and fetch the latest computed
+    // root hash from logs of each node
+    let graceful_stops_handle = try_join_all(healthy_nodes.iter().map(|node| {
+        let node_cl = node.clone();
+        tokio::task::spawn_blocking(move || assert_orchestrator_stopped_gracefully(&node_cl))
+    }));
+    let fetch_hashes_handle = {
+        let logger_cl = logger.clone();
+        tokio::spawn(async move {
+            fetch_latest_computed_root_hashes_from_logs(&logger_cl, log_streams).await
         })
-    }))
-    .await
-    .unwrap();
+    };
+
+    let (graceful_stops_result, fetch_hashes_result) =
+        tokio::join!(graceful_stops_handle, fetch_hashes_handle);
+
+    // Ensure that all nodes gracefully stopped
+    graceful_stops_result.unwrap();
     info!(logger, "All orchestrators shut down the tasks gracefully");
 
-    for node in &healthy_nodes {
-        assert_assigned_replica_version(node, target_version, logger.clone());
-    }
-
-    info!(
-        logger,
-        "Checking that at least n - f nodes produced a log displaying the latest computed root hash before rebooting..."
-    );
-    // Fetch the latest computed root hash from logs of each node
-    let state_hashes_from_logs = find_latest_computed_root_hashes_from_logs(logger, healthy_nodes);
+    let state_hashes_from_logs = fetch_hashes_result.unwrap();
     // Find all nodes that logged the same latest computed root hash and pick the most common one
     let mut state_hashes_counts = BTreeMap::new();
     for (node_id, hash) in state_hashes_from_logs.iter() {
@@ -356,6 +351,9 @@ async fn upgrade_to(
         most_common_hash
     );
 
+    for node in healthy_nodes {
+        assert_assigned_replica_version(&node, target_version, logger.clone());
+    }
     info!(
         logger,
         "Successfully upgraded subnet {} to {}", subnet_id, target_version
@@ -388,48 +386,65 @@ pub fn start_node(logger: &Logger, app_node: &IcNodeSnapshot) {
     info!(logger, "Node started: {}", app_node.get_ip_addr());
 }
 
+/// Fetches the latest computed state root hash from the node logs by continously searching for
+/// matching log entries until the log stream ends (which indicates that all nodes rebooted).
 /// Returns the last computed root hash found in the logs for every node.
-fn find_latest_computed_root_hashes_from_logs(
+///
+/// This function will never return if an upgrade is not scheduled.
+async fn fetch_latest_computed_root_hashes_from_logs(
     logger: &Logger,
-    nodes: Vec<IcNodeSnapshot>,
+    mut log_streams: LogStream,
 ) -> BTreeMap<NodeId, String> {
-    let computed_root_hash_regex = regex::Regex::new(
-        r#"Computed root hash CryptoHash\(0x([a-f0-9]{64})\) of state @([0-9]*)"#,
-    )
-    .unwrap();
+    let computed_root_hash_regex =
+        regex::Regex::new(r#"Computed root hash CryptoHash\(0x([a-f0-9]{64})\) of state @(\d*)"#)
+            .unwrap();
 
     let mut latest_root_hash_per_node = BTreeMap::new();
-    for node in nodes {
-        let matches = JournalStreamer::new(node.block_on_ssh_session().unwrap())
-            .previous_boot()
-            .search(computed_root_hash_regex.as_str())
-            .expect("Failed to fetch log entries");
-        let last_hash = matches
-            .into_iter()
-            .map(|entry| {
-                let caps = computed_root_hash_regex
-                    .captures(&entry)
-                    .expect("Should match the regex");
-                let hash = caps.get(1).unwrap().as_str().to_string();
-                let height = caps.get(2).unwrap().as_str().parse::<u64>().ok().unwrap();
-                info!(
-                    logger,
-                    "Found computed root hash log entry for node {} @{}: {}",
-                    node.node_id,
-                    height,
-                    hash
-                );
-                hash
+    while let Ok((node, entry)) = log_streams
+        .find(|_, line| computed_root_hash_regex.is_match(line))
+        .await
+    {
+        let (computed_root_hash, height) = computed_root_hash_regex
+            .captures(&entry)
+            .and_then(|caps| {
+                let hash_group = caps.get(1)?.as_str().to_string();
+                let height_group = caps.get(2)?.as_str().parse::<u64>().ok()?;
+                Some((hash_group, height_group))
             })
-            .next_back()
-            .unwrap_or_else(|| {
-                panic!(
-                    "No log entry with computed root hash found for node {}",
-                    node.node_id
-                )
-            });
-        latest_root_hash_per_node.insert(node.node_id, last_hash);
+            .expect("Failed to extract computed root hash from log entry");
+
+        info!(
+            logger,
+            "Found computed root hash log entry for node {} @{}: {}",
+            node.node_id,
+            height,
+            computed_root_hash
+        );
+
+        latest_root_hash_per_node.insert(node.node_id, computed_root_hash);
     }
 
     latest_root_hash_per_node
+}
+
+/// Asserts that the orchestrator has shut down gracefully by searching for a specific log entry.
+/// Panics if the log entry is not found but the log stream ends (which indicates the node
+/// rebooted).
+///
+/// We use a bash script instead of connecting to the log stream endpoint because as the
+/// orchestrator is shutting down, the endpoint might close right away without letting us the
+/// chance to read the relevant log entry. In constrast, the SSH connection remains open longer.
+///
+/// This function will never return if an upgrade is not scheduled.
+fn assert_orchestrator_stopped_gracefully(node: &IcNodeSnapshot) {
+    let session = node.block_on_ssh_session().unwrap();
+    session.set_timeout(5 * 60 * 1000);
+    if !JournalStreamer::new(session)
+        .follow()
+        .max_lines(1)
+        .search("Orchestrator shut down gracefully")
+        .is_ok_and(|matches| !matches.is_empty())
+    {
+        panic!("Orchestrator did not shut down gracefully");
+    }
 }
