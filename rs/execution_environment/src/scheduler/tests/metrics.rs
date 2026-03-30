7fd6b5a1055b875e6eeb4d3d14118f1fa4f916de
Alin Sinpalean <alin.sinpalean@dfinity.org>
2026-03-30 12:38:39 +0000

    chore: [DSM-103] Drop compute allocation violations metric
    The metric aims to be a sort of critical error counter for instances where a canister with a compute allocation of N has not been executed for more than 100/N rounds (e.g. a canister with a compute allocation of 25 should be executed at least every 4 rounds).
    
    I'm pretty sure this is not even an invariant for the basic (non-DTS), single core scheduler. While the paper shows that a canister with a compute allocation of N will be scheduled at least N rounds out of 100 (or exactly N rounds, if all compute allocation is allocated), the N rounds are only approximetely evenly spaced. In simulations, sometimes canisters went up to 300+ compute allocation, i.e. they were 3+ rounds "late".
    
    Regardless, in the presence of DTS execution; prioritization of completing already started DTS executions over starting new ones; and aborting DTS executions; a canister can take a lot longer to be scheduled than 100/N rounds.
    
    Finally, once we start scheduling active canisters only, the fact that a canister was not executed for arbitrarily many rounds (because it had no inputs to execute) will become totally meaningless.

--- rs/execution_environment/src/scheduler/tests/metrics.rs
+++ rs/execution_environment/src/scheduler/tests/metrics.rs
@@ -200,8 +200,6 @@ fn can_record_metrics_for_a_round() {
     );
     assert_eq!(metrics.round_finalization_ingress.get_sample_count(), 1);
     assert_eq!(metrics.round_finalization_charge.get_sample_count(), 1);
-    // Compute allocation violation is not observed for newly created canisters.
-    assert_eq!(metrics.canister_compute_allocation_violation.get(), 0);
 
     // `2 * scheduler_cores` messages were executed.
     assert_eq!(
@@ -234,8 +232,6 @@ fn can_record_metrics_for_a_round() {
     let metrics = &test.scheduler().metrics;
     // The canister age metric should be observed now.
     assert_eq!(metrics.canister_age.get_sample_sum() as i64, 12);
-    // Compute allocation violation should also be observed now.
-    assert_eq!(metrics.canister_compute_allocation_violation.get(), 1);
 }
 
 /// Check that when a canister is scheduled and can't prepay for execution, the
