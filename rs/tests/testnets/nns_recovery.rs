dcffb732e3201de932d8c4e031e0ebf9a6f8c8a1
Pierugo Pace <pierugo.pace@dfinity.org>
2026-04-02 10:20:41 +0000

    fix

--- rs/tests/testnets/nns_recovery.rs
+++ rs/tests/testnets/nns_recovery.rs
@@ -32,7 +32,7 @@
 use anyhow::Result;
 use ic_consensus_system_test_subnet_recovery::utils::{
     BACKUP_USERNAME, SshKeys, break_nodes, get_ssh_keys_for_user,
-    node_with_highest_certification_share_height,
+    node_with_highest_cert_share_and_cup_heights,
 };
 use ic_limits::DKG_INTERVAL_HEIGHT;
 use ic_nested_nns_recovery_common::{
@@ -144,7 +144,7 @@ fn log_instructions(env: TestEnv) {
     };
 
     loop {
-        let (_, highest_cert_share) = node_with_highest_certification_share_height(
+        let (_, highest_cert_share) = node_with_highest_cert_share_and_cup_heights(
             &env.topology_snapshot().root_subnet(),
             &logger,
         );
