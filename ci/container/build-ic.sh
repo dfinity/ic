87d05cf52363c10374f4dc3cc08399fb1b045b98
Carly Gundy <carlene.gundy@dfinity.org>
2026-04-08 15:51:08 +0200

    test autofix

--- ci/container/build-ic.sh
+++ ci/container/build-ic.sh
@@ -8,7 +8,8 @@ if [ "${BUILD_IC_NESTED:-}" == 1 ]; then
     echo "$0 nested, aborting"
     exit 1
 fi
-export BUILD_IC_NESTED=1
+export 
+BUILD_IC_NESTED=1
 
 export ROOT_DIR="$(git rev-parse --show-toplevel)"
 
