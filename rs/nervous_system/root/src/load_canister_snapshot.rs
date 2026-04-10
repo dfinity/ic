cccfe203d6299b4aca6994460ae0c8f15681026a
Daniel Wong <daniel.wong@dfinity.org>
2026-04-10 16:27:42 +0000

    Make snapshot proposals safe for Governance, which would otherwise deadlock.

--- rs/nervous_system/root/src/load_canister_snapshot.rs
+++ rs/nervous_system/root/src/load_canister_snapshot.rs
@@ -1,8 +1,10 @@
-use crate::private::exclusively_stop_and_start_canister;
+use crate::{LOG_PREFIX, private::exclusively_stop_and_start_canister};
 use candid::CandidType;
 use ic_base_types::{CanisterId, PrincipalId, SnapshotId};
+use ic_cdk::{futures::spawn_017_compat, println};
 use ic_management_canister_types_private::LoadCanisterSnapshotArgs;
 use ic_nervous_system_clients::management_canister_client::ManagementCanisterClient;
+use ic_nns_constants::GOVERNANCE_CANISTER_ID;
 use serde::Deserialize;
 
 #[derive(Clone, Eq, PartialEq, Hash, Debug, CandidType, Deserialize)]
@@ -26,71 +28,145 @@ pub struct LoadCanisterSnapshotError {
     pub description: String,
 }
 
+/// Restores a canister to a previously taken snapshot, an earlier state,
+/// which includes its memory (normal and stable) and code.
+///
+/// When the target canister is Governance, the operation is performed in the
+/// background and this function returns immediately with a placeholder `Ok`
+/// response. This avoids a deadlock: Root would need to stop Governance, but
+/// Governance cannot stop while it is waiting for Root to reply.
 pub async fn load_canister_snapshot(
     load_canister_snapshot_request: LoadCanisterSnapshotRequest,
-    management_canister_client: &mut impl ManagementCanisterClient,
+    management_canister_client: impl ManagementCanisterClient + 'static,
 ) -> LoadCanisterSnapshotResponse {
     let operation_description = format!("{:?}", load_canister_snapshot_request);
+    // Retain our own copy, because the async block will take the original.
+    let operation_description_for_log = operation_description.clone();
 
-    let LoadCanisterSnapshotRequest {
-        canister_id,
-        snapshot_id,
-    } = load_canister_snapshot_request;
+    // Used later, but calculated now, because the main async block is going
+    // to take load_canister_snapshot_request.
+    let targets_governance =
+        load_canister_snapshot_request.canister_id == PrincipalId::from(GOVERNANCE_CANISTER_ID);
 
-    let snapshot_id = match SnapshotId::try_from(snapshot_id) {
-        Ok(ok) => ok,
-        Err(err) => {
-            return LoadCanisterSnapshotResponse::Err(LoadCanisterSnapshotError {
+    let operation = async move {
+        // Construct the request that we will be sending to the Management canister
+        // in the next (large) statement.
+        let load_canister_snapshot_args =
+            LoadCanisterSnapshotArgs::try_from(load_canister_snapshot_request)?;
+
+        // Call the Management canister (but make sure we are not already
+        // operating on the canister, and also, stop before and start
+        // again after).
+        let result = exclusively_stop_and_start_canister::<_, _, _>(
+            load_canister_snapshot_args.get_canister_id(),
+            &operation_description,
+            true, // stop_before
+            || async {
+                management_canister_client
+                    .load_canister_snapshot(load_canister_snapshot_args)
+                    .await
+            },
+        )
+        .await;
+
+        // Handle errors.
+        // This line explicitly shows that no data is thrown away.
+        let _: () = result
+            // Handle errors from exclusively_stop_and_start_canister.
+            .map_err(|err| LoadCanisterSnapshotError {
                 code: None,
-                description: format!("Invalid snapshot ID: {err}"),
-            });
-        }
+                description: format!("{err}"),
+            })?
+            // Handle errors from calling management_canister_client.
+            .map_err(|(code, description)| LoadCanisterSnapshotError {
+                code: Some(code),
+                description,
+            })?;
+
+        Ok(LoadCanisterSnapshotOk {})
     };
 
-    let canister_id = match CanisterId::try_from(canister_id) {
-        Ok(ok) => ok,
-        Err(err) => {
-            return LoadCanisterSnapshotResponse::Err(LoadCanisterSnapshotError {
+    // Log result.
+    let operation = async move {
+        let result = LoadCanisterSnapshotResponse::from(operation.await);
+        println!("{LOG_PREFIX}{operation_description_for_log}: {result:?}");
+        result
+    };
+
+    // In the case of Governance, do the operation in the background.
+    // This is necessary to avoid deadlock:
+    //
+    // 1. the Governance canister calls the Root canister's load_canister_snapshot
+    //    method, but
+    // 2. the first thing load_canister_snapshot does (via
+    //    exclusively_stop_and_start_canister) is to stop the target canister,
+    //    which in this case, is Governance, but
+    // 3. Governance is waiting for the call in step 1 to return.
+    //
+    // So, what we have is Governance waiting for Root to reply in order to stop, but Root
+    // is waiting for Governance to stop before it can reply. Circular waiting, i.e. deadlock.
+    if targets_governance {
+        spawn_017_compat(async move {
+            // It is ok to throw this away, because it is already logged above.
+            let _: LoadCanisterSnapshotResponse = operation.await;
+
+            // Because spawn_017_compat does not know what to do with
+            // a LoadCanisterSnapshotResponse.
+            ()
+        });
+
+        // Even though we do not yet know that the operation will succeed,
+        // we return Ok here, because we also do not know that it will fail.
+        // The important thing is that we launched the operation. That's
+        // the definition of success in the special case of Governance.
+        return LoadCanisterSnapshotResponse::Ok(LoadCanisterSnapshotOk {});
+    }
+
+    operation.await
+}
+
+impl TryFrom<LoadCanisterSnapshotRequest> for LoadCanisterSnapshotArgs {
+    type Error = LoadCanisterSnapshotError;
+
+    fn try_from(request: LoadCanisterSnapshotRequest) -> Result<Self, LoadCanisterSnapshotError> {
+        let LoadCanisterSnapshotRequest {
+            canister_id,
+            snapshot_id,
+        } = request;
+
+        let canister_id =
+            CanisterId::try_from(canister_id).map_err(|err| LoadCanisterSnapshotError {
                 code: None,
                 description: format!("Invalid canister ID: {err}"),
-            });
-        }
-    };
+            })?;
 
-    let result = exclusively_stop_and_start_canister::<_, _, _>(
-        canister_id,
-        &operation_description,
-        true, // stop_before
-        || async {
-            let load_canister_snapshot_args = LoadCanisterSnapshotArgs::new(
-                canister_id,
-                snapshot_id,
-                management_canister_client.canister_version(),
-            );
-
-            management_canister_client
-                .load_canister_snapshot(load_canister_snapshot_args)
-                .await
-        },
-    )
-    .await;
-
-    let result = match result {
-        Ok(ok) => ok,
-        Err(err) => {
-            let description = format!("{err}");
-            return LoadCanisterSnapshotResponse::Err(LoadCanisterSnapshotError {
+        let snapshot_id =
+            SnapshotId::try_from(snapshot_id).map_err(|err| LoadCanisterSnapshotError {
                 code: None,
-                description,
-            });
-        }
-    };
+                description: format!("Invalid snapshot ID: {err}"),
+            })?;
 
-    match result {
-        Ok(()) => LoadCanisterSnapshotResponse::Ok(LoadCanisterSnapshotOk {}),
-        Err((code, description)) => LoadCanisterSnapshotResponse::Err(LoadCanisterSnapshotError {
-            code: Some(code),
-            description,
-        }),
+        Ok(LoadCanisterSnapshotArgs::new(
+            canister_id,
+            snapshot_id,
+            None,
+        ))
+    }
+}
+
+impl From<LoadCanisterSnapshotError> for LoadCanisterSnapshotResponse {
+    fn from(err: LoadCanisterSnapshotError) -> Self {
+        LoadCanisterSnapshotResponse::Err(err)
+    }
+}
+
+impl From<Result<LoadCanisterSnapshotOk, LoadCanisterSnapshotError>>
+    for LoadCanisterSnapshotResponse
+{
+    fn from(result: Result<LoadCanisterSnapshotOk, LoadCanisterSnapshotError>) -> Self {
+        match result {
+            Ok(ok) => LoadCanisterSnapshotResponse::Ok(ok),
+            Err(err) => LoadCanisterSnapshotResponse::Err(err),
+        }
     }
 }
--- rs/nervous_system/root/src/take_canister_snapshot.rs
+++ rs/nervous_system/root/src/take_canister_snapshot.rs
@@ -1,8 +1,10 @@
-use crate::private::exclusively_stop_and_start_canister;
+use crate::{LOG_PREFIX, private::exclusively_stop_and_start_canister};
 use candid::CandidType;
 use ic_base_types::{CanisterId, PrincipalId, SnapshotId};
+use ic_cdk::{futures::spawn_017_compat, println};
 use ic_management_canister_types_private::{CanisterSnapshotResponse, TakeCanisterSnapshotArgs};
 use ic_nervous_system_clients::management_canister_client::ManagementCanisterClient;
+use ic_nns_constants::GOVERNANCE_CANISTER_ID;
 use serde::Deserialize;
 
 #[derive(Clone, Eq, PartialEq, Hash, Debug, CandidType, Deserialize)]
@@ -30,103 +32,170 @@ pub struct TakeCanisterSnapshotError {
     pub description: String,
 }
 
+/// A snapshot consists of a canister's memory (normal and stable) and code.
+/// Taking a snapshot simply means that we capture that data atomically.
+/// A snapshot can then later be used (usually for disaster recovery)
+/// to restore the canister to the state it was in when the snapshot was taken.
+///
+/// When the target canister is Governance, the operation is performed in the
+/// background and this function returns immediately with a placeholder `Ok`
+/// response (fields zeroed). This avoids a deadlock: Root would need to stop
+/// Governance, but Governance cannot stop while it is waiting for Root to reply.
 pub async fn take_canister_snapshot(
     take_canister_snapshot_request: TakeCanisterSnapshotRequest,
-    management_canister_client: &mut impl ManagementCanisterClient,
+    management_canister_client: impl ManagementCanisterClient + 'static,
 ) -> TakeCanisterSnapshotResponse {
     let operation_description = format!("{:?}", take_canister_snapshot_request);
+    // Retain our own copy, because the async block will take the original.
+    let operation_description_for_log = operation_description.clone();
 
-    let TakeCanisterSnapshotRequest {
-        canister_id,
-        replace_snapshot,
-    } = take_canister_snapshot_request;
+    // Used later, but calculated now, because the main async block is going
+    // to take take_canister_snapshot_request.
+    let targets_governance =
+        take_canister_snapshot_request.canister_id == PrincipalId::from(GOVERNANCE_CANISTER_ID);
 
-    let replace_snapshot = match replace_snapshot {
-        None => None,
-        Some(snapshot_id) => {
-            let snapshot_id = match SnapshotId::try_from(&snapshot_id) {
-                Ok(ok) => ok,
-                Err(err) => {
-                    return TakeCanisterSnapshotResponse::Err(TakeCanisterSnapshotError {
-                        code: None,
-                        description: format!("Invalid snapshot ID ({snapshot_id:02X?}): {err}"),
-                    });
-                }
-            };
+    let operation = async move {
+        // Construct the request that we will be sending to the Management canister
+        // in the next (large) statement.
+        let take_canister_snapshot_args =
+            TakeCanisterSnapshotArgs::try_from(take_canister_snapshot_request)?;
 
-            Some(snapshot_id)
-        }
-    };
+        // Call the Management canister (but make sure we are not already
+        // operating on the canister, and also, stop before and start
+        // again after).
+        let result = exclusively_stop_and_start_canister(
+            take_canister_snapshot_args.get_canister_id(),
+            &operation_description,
+            true, // stop_before
+            || async {
+                management_canister_client
+                    .take_canister_snapshot(take_canister_snapshot_args)
+                    .await
+            },
+        )
+        .await;
 
-    let canister_id = match CanisterId::try_from(canister_id) {
-        Ok(id) => id,
-        Err(e) => {
-            return TakeCanisterSnapshotResponse::Err(TakeCanisterSnapshotError {
-                code: None,
-                description: format!("Invalid canister ID: {:?}", e),
-            });
-        }
-    };
-
-    let result = exclusively_stop_and_start_canister(
-        canister_id,
-        &operation_description,
-        true, // stop_before
-        || async {
-            let canister_id = PrincipalId::from(canister_id);
-
-            let take_canister_snapshot_args = TakeCanisterSnapshotArgs {
-                canister_id,
-                replace_snapshot,
-                uninstall_code: None,
-                sender_canister_version: management_canister_client.canister_version(),
-            };
-
-            management_canister_client
-                .take_canister_snapshot(take_canister_snapshot_args)
-                .await
-        },
-    )
-    .await;
-
-    let result = match result {
-        Ok(ok) => ok,
-        Err(err) => {
-            return TakeCanisterSnapshotResponse::Err(TakeCanisterSnapshotError {
+        // Handle errors.
+        let snapshot: CanisterSnapshotResponse = result
+            // Handle errors from exclusively_stop_and_start_canister.
+            .map_err(|err| TakeCanisterSnapshotError {
                 code: None,
                 description: format!("{err}"),
-            });
-        }
+            })?
+            // Handle errors from calling management_canister_client.
+            .map_err(|(code, description)| TakeCanisterSnapshotError {
+                code: Some(code),
+                description,
+            })?;
+
+        Ok(TakeCanisterSnapshotOk::from(snapshot))
     };
 
-    match result {
-        Ok(result) => {
-            let result =
-                convert_from_canister_snapshot_response_to_take_canister_snapshot_ok(result);
-            TakeCanisterSnapshotResponse::Ok(result)
-        }
+    // Log result.
+    let operation = async move {
+        let result = TakeCanisterSnapshotResponse::from(operation.await);
+        println!("{LOG_PREFIX}{operation_description_for_log}: {result:?}");
+        result
+    };
 
-        Err((code, description)) => TakeCanisterSnapshotResponse::Err(TakeCanisterSnapshotError {
-            code: Some(code),
-            description,
-        }),
+    // In the case of Governance, do the operation in the background.
+    // This is necessary to avoid deadlock:
+    //
+    // 1. the Governance canister calls the Root canister's take_canister_snapshot
+    //    method, but
+    // 2. the first thing take_canister_snapshot does (via
+    //    exclusively_stop_and_start_canister) is to stop the target canister,
+    //    which in this case, is Governance, but
+    // 3. Governance is waiting for the call in step 1 to return.
+    //
+    // So, what we have is Governance waiting for Root to reply in order to stop, but Root
+    // is waiting for Governance to stop before it can reply. Circular waiting, i.e. deadlock.
+    if targets_governance {
+        spawn_017_compat(async move {
+            // It is ok to throw this away, because it is already logged above.
+            let _: TakeCanisterSnapshotResponse = operation.await;
+
+            // Because spawn_017_compat does not know what to do with
+            // a TakeCanisterSnapshotResponse.
+            ()
+        });
+
+        // Even though we do not yet know that the operation will succeed,
+        // we return Ok here, because we also do not know that it will fail.
+        // The important thing is that we launched the operation. That's
+        // the definition of success in the special case of Governance.
+        return TakeCanisterSnapshotResponse::Ok(TakeCanisterSnapshotOk {
+            id: vec![],
+            taken_at_timestamp: 0,
+            total_size: 0,
+        });
+    }
+
+    operation.await
+}
+
+impl TryFrom<TakeCanisterSnapshotRequest> for TakeCanisterSnapshotArgs {
+    type Error = TakeCanisterSnapshotError;
+
+    fn try_from(request: TakeCanisterSnapshotRequest) -> Result<Self, TakeCanisterSnapshotError> {
+        let TakeCanisterSnapshotRequest {
+            canister_id,
+            replace_snapshot,
+        } = request;
+
+        let canister_id =
+            CanisterId::try_from(canister_id).map_err(|err| TakeCanisterSnapshotError {
+                code: None,
+                description: format!("Invalid canister ID: {err}"),
+            })?;
+
+        let replace_snapshot = replace_snapshot
+            .map(|snapshot_id| {
+                SnapshotId::try_from(&snapshot_id).map_err(|err| TakeCanisterSnapshotError {
+                    code: None,
+                    description: format!("Invalid snapshot ID: {err}"),
+                })
+            })
+            .transpose()?;
+
+        Ok(TakeCanisterSnapshotArgs::new(
+            canister_id,
+            replace_snapshot,
+            None, // uninstall_code
+            None, // sender_canister_version
+        ))
     }
 }
 
-fn convert_from_canister_snapshot_response_to_take_canister_snapshot_ok(
-    response: CanisterSnapshotResponse,
-) -> TakeCanisterSnapshotOk {
-    let CanisterSnapshotResponse {
-        id,
-        taken_at_timestamp,
-        total_size,
-    } = response;
+impl From<TakeCanisterSnapshotError> for TakeCanisterSnapshotResponse {
+    fn from(err: TakeCanisterSnapshotError) -> Self {
+        TakeCanisterSnapshotResponse::Err(err)
+    }
+}
 
-    let id = id.to_vec();
+impl From<Result<TakeCanisterSnapshotOk, TakeCanisterSnapshotError>>
+    for TakeCanisterSnapshotResponse
+{
+    fn from(result: Result<TakeCanisterSnapshotOk, TakeCanisterSnapshotError>) -> Self {
+        match result {
+            Ok(ok) => TakeCanisterSnapshotResponse::Ok(ok),
+            Err(err) => TakeCanisterSnapshotResponse::Err(err),
+        }
+    }
+}
 
-    TakeCanisterSnapshotOk {
-        id,
-        taken_at_timestamp,
-        total_size,
+impl From<CanisterSnapshotResponse> for TakeCanisterSnapshotOk {
+    fn from(response: CanisterSnapshotResponse) -> Self {
+        let CanisterSnapshotResponse {
+            id,
+            taken_at_timestamp,
+            total_size,
+        } = response;
+
+        TakeCanisterSnapshotOk {
+            id: id.to_vec(),
+            taken_at_timestamp,
+            total_size,
+        }
     }
 }
