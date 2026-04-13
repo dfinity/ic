724dc7cc6e40d81381663f0c31e9428dfe6bd2b6
Mathias Björkqvist <mathias.bjorkqvist@dfinity.org>
2026-04-13 22:50:53 +0200

    Cleanup

--- packages/icrc-ledger-types/src/icrc1/account.rs
+++ packages/icrc-ledger-types/src/icrc1/account.rs
@@ -248,9 +248,6 @@ mod tests {
     use proptest::strategy::Strategy;
     use std::str::FromStr;
 
-    #[cfg(feature = "storable")]
-    use ic_stable_structures::Storable;
-
     use crate::icrc1::account::{
         Account, ICRC1TextReprError, principal_to_subaccount, subaccount_to_principal,
         try_from_subaccount_to_principal,
@@ -259,15 +256,6 @@ mod tests {
     pub fn principal_strategy() -> impl Strategy<Value = Principal> {
         let bytes_strategy = prop::collection::vec(0..=255_u8, 29);
         bytes_strategy.prop_map(|bytes| Principal::from_slice(bytes.as_slice()))
-    }
-
-    pub fn account_strategy() -> impl Strategy<Value = Account> {
-        let bytes_strategy = prop::option::of(prop::collection::vec(0..=255_u8, 32));
-        let principal_strategy = principal_strategy();
-        (bytes_strategy, principal_strategy).prop_map(|(bytes, principal)| Account {
-            owner: principal,
-            subaccount: bytes.map(|x| x.as_slice().try_into().unwrap()),
-        })
     }
 
     #[test]
@@ -406,25 +394,6 @@ mod tests {
         );
     }
 
-    #[cfg(feature = "storable")]
-    #[test]
-    fn test_account_serialization() {
-        use proptest::{prop_assert_eq, proptest};
-
-        fn account_strategy() -> impl Strategy<Value = Account> {
-            let bytes_strategy = prop::option::of(prop::collection::vec(0..=255_u8, 32));
-            let principal_strategy = principal_strategy();
-            (bytes_strategy, principal_strategy).prop_map(|(bytes, principal)| Account {
-                owner: principal,
-                subaccount: bytes.map(|x| x.as_slice().try_into().unwrap()),
-            })
-        }
-
-        proptest!(|(account in account_strategy())| {
-            prop_assert_eq!(Account::from_bytes(account.to_bytes()), account);
-        })
-    }
-
     #[test]
     fn test_principal_to_subaccount() {
         use proptest::{prop_assert_eq, proptest};
@@ -471,42 +440,66 @@ mod tests {
     }
 
     #[cfg(feature = "storable")]
-    #[test]
-    fn test_account_serialization_stability() {
-        let owner =
-            Principal::from_str("k2t6j-2nvnp-4zjm3-25dtz-6xhaa-c7boj-5gayf-oj3xs-i43lp-teztq-6ae")
-                .unwrap();
-        let subaccount = Some(
-            hex::decode("0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20")
-                .unwrap()
-                .try_into()
-                .unwrap(),
-        );
-        let mut accounts = vec![Account { owner, subaccount }];
-        let mut serialized_accounts = vec![hex::decode("82581db56bf994b37ae8e79f5ce000be1727a6060ae4eef24736b7cc999c3c0258200102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20").unwrap()];
-        let owner =
-            Principal::from_str("gjfkw-yiolw-ncij7-yzhg2-gq6ec-xi6jy-feyni-g26f4-x7afk-thx6z-6ae")
-                .unwrap();
-        let subaccount = Some(
-            hex::decode("0000000000000000000000000000000000000000000000000000000000000000")
-                .unwrap()
-                .try_into()
-                .unwrap(),
-        );
-        accounts.push(Account { owner, subaccount });
-        serialized_accounts.push(hex::decode("82581d0e5d9a2427f8c9cda343c415d1e4e0a4c3506d78bcbfc0554cf7f67c0258200000000000000000000000000000000000000000000000000000000000000000").unwrap());
+    mod storable_tests {
+        use super::*;
+        use ic_stable_structures::Storable;
+        use proptest::{prop_assert_eq, proptest};
 
-        let owner = Principal::from_str("2chl6-4hpzw-vqaaa-aaaaa-c").unwrap();
-        let subaccount = None;
-        accounts.push(Account { owner, subaccount });
-        serialized_accounts.push(hex::decode("8149efcdab000000000001").unwrap());
+        fn account_strategy() -> impl Strategy<Value = Account> {
+            let bytes_strategy = prop::option::of(prop::collection::vec(0..=255_u8, 32));
+            let principal_strategy = principal_strategy();
+            (bytes_strategy, principal_strategy).prop_map(|(bytes, principal)| Account {
+                owner: principal,
+                subaccount: bytes.map(|x| x.as_slice().try_into().unwrap()),
+            })
+        }
 
-        for (i, account) in accounts.iter().enumerate() {
-            assert_eq!(account.to_bytes(), serialized_accounts[i].clone());
-            assert_eq!(
-                *account,
-                Account::from_bytes(std::borrow::Cow::Owned(serialized_accounts[i].clone()))
+        #[test]
+        fn test_account_serialization() {
+            proptest!(|(account in account_strategy())| {
+                prop_assert_eq!(Account::from_bytes(account.to_bytes()), account);
+            })
+        }
+
+        #[test]
+        fn test_account_serialization_stability() {
+            let owner = Principal::from_str(
+                "k2t6j-2nvnp-4zjm3-25dtz-6xhaa-c7boj-5gayf-oj3xs-i43lp-teztq-6ae",
+            )
+            .unwrap();
+            let subaccount = Some(
+                hex::decode("0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20")
+                    .unwrap()
+                    .try_into()
+                    .unwrap(),
             );
+            let mut accounts = vec![Account { owner, subaccount }];
+            let mut serialized_accounts = vec![hex::decode("82581db56bf994b37ae8e79f5ce000be1727a6060ae4eef24736b7cc999c3c0258200102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20").unwrap()];
+            let owner = Principal::from_str(
+                "gjfkw-yiolw-ncij7-yzhg2-gq6ec-xi6jy-feyni-g26f4-x7afk-thx6z-6ae",
+            )
+            .unwrap();
+            let subaccount = Some(
+                hex::decode("0000000000000000000000000000000000000000000000000000000000000000")
+                    .unwrap()
+                    .try_into()
+                    .unwrap(),
+            );
+            accounts.push(Account { owner, subaccount });
+            serialized_accounts.push(hex::decode("82581d0e5d9a2427f8c9cda343c415d1e4e0a4c3506d78bcbfc0554cf7f67c0258200000000000000000000000000000000000000000000000000000000000000000").unwrap());
+
+            let owner = Principal::from_str("2chl6-4hpzw-vqaaa-aaaaa-c").unwrap();
+            let subaccount = None;
+            accounts.push(Account { owner, subaccount });
+            serialized_accounts.push(hex::decode("8149efcdab000000000001").unwrap());
+
+            for (i, account) in accounts.iter().enumerate() {
+                assert_eq!(account.to_bytes(), serialized_accounts[i].clone());
+                assert_eq!(
+                    *account,
+                    Account::from_bytes(std::borrow::Cow::Owned(serialized_accounts[i].clone()))
+                );
+            }
         }
     }
 }
