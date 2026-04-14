45edbd856e39d6b75f891c259ba8141f557d3401
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2026-04-14 07:03:42 +0000

    chore: re-enable all management canister benchmarks

--- rs/execution_environment/benches/management_canister/main.rs
+++ rs/execution_environment/benches/management_canister/main.rs
@@ -11,14 +11,12 @@ use criterion::{Criterion, criterion_group, criterion_main};
 
 fn all_benchmarks(c: &mut Criterion) {
     canister_logging::canister_logging_benchmark(c);
-
-    // TODO: uncomment once the work on canister_logging_benchmark is done, to avoid spending time on running all benchmarks.
-    // canister_snapshots::benchmark(c);
-    // create_canisters::create_canisters_benchmark(c);
-    // ecdsa::ecdsa_benchmark(c);
-    // http_request::http_request_benchmark(c);
-    // install_code::install_code_benchmark(c);
-    // update_settings::update_settings_benchmark(c);
+    canister_snapshots::benchmark(c);
+    create_canisters::create_canisters_benchmark(c);
+    ecdsa::ecdsa_benchmark(c);
+    http_request::http_request_benchmark(c);
+    install_code::install_code_benchmark(c);
+    update_settings::update_settings_benchmark(c);
 }
 
 criterion_group!(benchmarks, all_benchmarks);
