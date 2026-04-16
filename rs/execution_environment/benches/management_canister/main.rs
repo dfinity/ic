dd467751a2ae34a44eaa5d7b2c26e4de2929be6c
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2026-04-16 14:19:56 +0000

    enable all benchmarks

--- rs/execution_environment/benches/management_canister/main.rs
+++ rs/execution_environment/benches/management_canister/main.rs
@@ -11,14 +11,12 @@ use criterion::{Criterion, criterion_group, criterion_main};
 
 fn all_benchmarks(c: &mut Criterion) {
     canister_logging::canister_logging_benchmark(c);
-
-    // TODO: do not submit! Uncomment before merging.
-    // canister_snapshots::benchmark(c);
-    // create_canisters::create_canisters_benchmark(c);
-    // ecdsa::ecdsa_benchmark(c);
-    // http_request::http_request_benchmark(c);
-    // install_code::install_code_benchmark(c);
-    // update_settings::update_settings_benchmark(c);
+    canister_snapshots::benchmark(c);
+    create_canisters::create_canisters_benchmark(c);
+    ecdsa::ecdsa_benchmark(c);
+    http_request::http_request_benchmark(c);
+    install_code::install_code_benchmark(c);
+    update_settings::update_settings_benchmark(c);
 }
 
 criterion_group!(benchmarks, all_benchmarks);
