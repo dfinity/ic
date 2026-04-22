352cd3b8c29d96e1dc04f8b2ff6e3a9e193638e5
Daniel Wong <daniel.wong@dfinity.org>
2026-04-22 11:52:20 +0000

    Added support for storing batch proposals to governance.proto.

--- rs/nns/governance/src/gen/ic_nns_governance.pb.v1.rs
+++ rs/nns/governance/src/gen/ic_nns_governance.pb.v1.rs
@@ -440,7 +440,7 @@ pub struct Proposal {
     /// take.
     #[prost(
         oneof = "proposal::Action",
-        tags = "10, 12, 13, 14, 15, 16, 17, 18, 19, 21, 29, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34"
+        tags = "10, 12, 13, 14, 15, 16, 17, 18, 19, 21, 29, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35"
     )]
     pub action: ::core::option::Option<proposal::Action>,
 }
@@ -559,7 +559,36 @@ pub mod proposal {
         /// Create a canister in a (possibly non-NNS) subnet and install code into it.
         #[prost(message, tag = "34")]
         CreateCanisterAndInstallCode(super::CreateCanisterAndInstallCode),
+        /// Execute multiple proposal actions sequentially. Stops at first failure.
+        #[prost(message, tag = "35")]
+        Batch(super::Batch),
     }
+}
+/// Multiple actions to execute sequentially. Stop on first failure.
+///
+/// No recursion, i.e. Batch cannot contain batch.
+///
+/// # Topic
+///
+/// All constituent actions must have the same topic.
+///
+/// This has the topic of its constituents.
+#[derive(
+    candid::CandidType,
+    candid::Deserialize,
+    serde::Serialize,
+    comparable::Comparable,
+    Clone,
+    PartialEq,
+    ::prost::Message,
+)]
+pub struct Batch {
+    /// The steps to take.
+    /// Proposal includes some fields that are superfluous in this context
+    /// (title, summary, etc). The only part we use here is the `action` oneof.
+    /// The others will be left blank.
+    #[prost(message, repeated, tag = "1")]
+    pub actions: ::prost::alloc::vec::Vec<Proposal>,
 }
 /// Take a canister snapshot.
 #[derive(
