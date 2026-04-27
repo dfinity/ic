f6a8755d120874ece79cf5c6c37cfa5fdcd14a38
Daniel Wong <daniel.wong@dfinity.org>
2026-04-27 10:43:19 +0000

    added --nocache_test_results to preview so that if you run a second time to regenerate, the result is not empty.

--- filter_relaxed_8y_gang_preview.sh
+++ filter_relaxed_8y_gang_preview.sh
@@ -6,6 +6,7 @@ bazel test \
   --test_env=SSH_AUTH_SOCK \
   --test_output=streamed \
   --test_arg=--nocapture \
+  --nocache_test_results \
   //rs/nns/integration_tests:upgrade_canisters_with_golden_nns_state \
 | grep PREVIEW_RELAXED_EIGHT_YEAR_GANG \
 | sed 's/.*PREVIEW_RELAXED_EIGHT_YEAR_GANG //' \
