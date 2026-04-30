e8b1cb5ea03ef39e789b9397beee0e34774eff21
Martin Raszyk <martin.raszyk@dfinity.org>
2026-04-30 07:39:39 +0000

    add create_execution_state benchmark

---
+++ rs/execution_environment/benches/management_canister/create_execution_state.rs
@@ -0,0 +1,52 @@
+use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
+use ic_config::execution_environment::{SUBNET_CALLBACK_SOFT_LIMIT, SUBNET_MEMORY_RESERVATION};
+use ic_execution_environment::{CompilationCostHandling, RoundLimits, as_round_instructions};
+use ic_interfaces::execution_environment::SubnetAvailableMemory;
+use ic_test_utilities_execution_environment::ExecutionTestBuilder;
+use ic_test_utilities_types::ids::canister_test_id;
+use ic_types::NumInstructions;
+use ic_wasm_types::CanisterModule;
+
+const MAX_NUM_INSTRUCTIONS: NumInstructions = NumInstructions::new(500_000_000_000);
+
+pub fn benchmark(c: &mut Criterion) {
+    let canister_id = canister_test_id(1);
+    let wasm = wat::parse_str("(module)").unwrap();
+    c.bench_function("create_execution_state/wasm:empty", |b| {
+        b.iter_batched(
+            || {
+                let exec_test = ExecutionTestBuilder::new().build();
+                let tmpdir = tempfile::TempDir::new().unwrap();
+                (exec_test, tmpdir)
+            },
+            |(exec_test, tmpdir)| {
+                let hypervisor = exec_test.hypervisor_deprecated();
+                let mut round_limits = RoundLimits {
+                    instructions: as_round_instructions(MAX_NUM_INSTRUCTIONS),
+                    subnet_available_memory: SubnetAvailableMemory::new_for_testing(
+                        i64::MAX,
+                        i64::MAX,
+                        i64::MAX,
+                    ),
+                    subnet_available_callbacks: SUBNET_CALLBACK_SOFT_LIMIT as i64,
+                    compute_allocation_used: 0,
+                    subnet_memory_reservation: SUBNET_MEMORY_RESERVATION,
+                };
+                hypervisor
+                    .create_execution_state(
+                        CanisterModule::new(wasm.clone()),
+                        tmpdir.path().to_path_buf(),
+                        canister_id,
+                        &mut round_limits,
+                        CompilationCostHandling::CountFullAmount,
+                    )
+                    .1
+                    .unwrap();
+            },
+            BatchSize::SmallInput,
+        );
+    });
+}
+
+criterion_group!(benchmarks, benchmark);
+criterion_main!(benchmarks);
