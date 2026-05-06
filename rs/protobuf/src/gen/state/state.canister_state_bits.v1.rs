f49d15bf05f89441480422595e5656a37a5a9fff
Dimitris Sarlis <sarlis.dim@gmail.com>
2026-05-06 09:21:26 +0000

    chore: Remove deprecated protobuf fields

--- rs/protobuf/src/gen/state/state.canister_state_bits.v1.rs
+++ rs/protobuf/src/gen/state/state.canister_state_bits.v1.rs
@@ -94,21 +94,14 @@ pub struct Callback {
     pub cycles_sent: ::core::option::Option<super::super::queues::v1::Cycles>,
     #[prost(message, optional, tag = "7")]
     pub respondent: ::core::option::Option<super::super::super::types::v1::CanisterId>,
-    #[prost(message, optional, tag = "8")]
-    pub prepayment_for_response_execution: ::core::option::Option<super::super::queues::v1::Cycles>,
-    #[prost(message, optional, tag = "9")]
-    pub prepayment_for_response_transmission:
-        ::core::option::Option<super::super::queues::v1::Cycles>,
     /// If non-zero, this is a best-effort call.
     #[prost(uint32, tag = "10")]
     pub deadline_seconds: u32,
-    /// To replace `prepayment_for_response_execution`.
     #[prost(message, optional, tag = "11")]
-    pub prepayment_for_response_execution_compound:
+    pub prepayment_for_response_execution:
         ::core::option::Option<super::super::queues::v1::CompoundCycles>,
-    /// To replace `prepayment_for_response_transmission`.
     #[prost(message, optional, tag = "12")]
-    pub prepayment_for_response_transmission_compound:
+    pub prepayment_for_response_transmission:
         ::core::option::Option<super::super::queues::v1::CompoundCycles>,
     #[prost(message, optional, tag = "13")]
     pub prepayment_for_call_transmission:
@@ -324,14 +317,8 @@ pub mod execution_task {
     pub struct AbortedExecution {
         /// The execution cost that has already been charged from the canister.
         /// Retried execution does not have to pay for it again.
-        #[prost(message, optional, tag = "4")]
-        pub prepaid_execution_cycles:
-            ::core::option::Option<super::super::super::queues::v1::Cycles>,
-        /// The execution cost that has already been charged from the canister.
-        /// Retried execution does not have to pay for it again.
-        /// This field will replace the existing `prepaid_execution_cycles`.
         #[prost(message, optional, tag = "7")]
-        pub prepaid_execution_compound_cycles:
+        pub prepaid_execution_cycles:
             ::core::option::Option<super::super::super::queues::v1::CompoundCycles>,
         #[prost(oneof = "aborted_execution::Input", tags = "1, 6, 3, 5")]
         pub input: ::core::option::Option<aborted_execution::Input>,
@@ -359,18 +346,12 @@ pub mod execution_task {
     }
     #[derive(Clone, PartialEq, ::prost::Message)]
     pub struct AbortedInstallCode {
-        /// The execution cost that has already been charged from the canister.
-        /// Retried execution does not have to pay for it again.
-        #[prost(message, optional, tag = "3")]
-        pub prepaid_execution_cycles:
-            ::core::option::Option<super::super::super::queues::v1::Cycles>,
         #[prost(uint64, optional, tag = "4")]
         pub call_id: ::core::option::Option<u64>,
         /// The execution cost that has already been charged from the canister.
         /// Retried execution does not have to pay for it again.
-        /// Will replace the existing `prepaid_execution_cycles`.
         #[prost(message, optional, tag = "5")]
-        pub prepaid_execution_compound_cycles:
+        pub prepaid_execution_cycles:
             ::core::option::Option<super::super::super::queues::v1::CompoundCycles>,
         #[prost(oneof = "aborted_install_code::Message", tags = "1, 2")]
         pub message: ::core::option::Option<aborted_install_code::Message>,
--- rs/replicated_state/src/canister_state/system_state/proto.rs
+++ rs/replicated_state/src/canister_state/system_state/proto.rs
@@ -1,8 +1,6 @@
 use super::*;
 use ic_protobuf::proxy::{ProxyDecodeError, try_from_option_field};
 use ic_protobuf::state::canister_state_bits::v1 as pb;
-use ic_protobuf::state::queues::v1::CompoundCycles as PbCompoundCycles;
-use ic_types_cycles::{CanisterCyclesCostSchedule, CompoundCycles, Instructions};
 
 impl From<&CanisterStatus> for pb::canister_state_bits::CanisterStatus {
     fn from(item: &CanisterStatus) -> Self {
@@ -100,12 +98,7 @@ impl From<&ExecutionTask> for pb::ExecutionTask {
                     task: Some(pb::execution_task::Task::AbortedExecution(
                         pb::execution_task::AbortedExecution {
                             input: Some(input),
-                            prepaid_execution_cycles: Some(
-                                (prepaid_execution_cycles.real()).into(),
-                            ),
-                            prepaid_execution_compound_cycles: Some(PbCompoundCycles::from(
-                                *prepaid_execution_cycles,
-                            )),
+                            prepaid_execution_cycles: Some((*prepaid_execution_cycles).into()),
                         },
                     )),
                 }
@@ -125,12 +118,7 @@ impl From<&ExecutionTask> for pb::ExecutionTask {
                         pb::execution_task::AbortedInstallCode {
                             message: Some(message),
                             call_id: Some(call_id.get()),
-                            prepaid_execution_cycles: Some(
-                                (prepaid_execution_cycles.real()).into(),
-                            ),
-                            prepaid_execution_compound_cycles: Some(PbCompoundCycles::from(
-                                *prepaid_execution_cycles,
-                            )),
+                            prepaid_execution_cycles: Some((*prepaid_execution_cycles).into()),
                         },
                     )),
                 }
@@ -146,14 +134,6 @@ impl TryFrom<pb::ExecutionTask> for ExecutionTask {
         let task = value
             .task
             .ok_or(ProxyDecodeError::MissingField("ExecutionTask::task"))?;
-        // cost_schedule should ideally be read from the checkpoint, however there
-        // is no easy access to it here (will need to be propagated from `StateManager`).
-        // Given that the current state is that the values for `Cycles` and `NominalCycles`
-        // should still match and that they should be 0 on `Free` schedule, we can use `Normal`
-        // without any loss (to maintain values on subnets with `Normal` schedule).
-        // This code will be removed anyway when the new fields are set and the old fields
-        // containing just `Cycles` can be retired.
-        let cost_schedule = CanisterCyclesCostSchedule::Normal;
         let task = match task {
             pb::execution_task::Task::AbortedExecution(aborted) => {
                 use pb::execution_task::{
@@ -193,15 +173,10 @@ impl TryFrom<pb::ExecutionTask> for ExecutionTask {
                         CanisterMessageOrTask::Task(task)
                     }
                 };
-                let prepaid_execution_cycles = match aborted.prepaid_execution_compound_cycles {
-                    Some(value) => CompoundCycles::try_from(value)?,
-                    None => {
-                        let prepaid_execution_cycles = aborted
-                            .prepaid_execution_cycles
-                            .map_or_else(Cycles::zero, |c| c.into());
-                        CompoundCycles::<Instructions>::new(prepaid_execution_cycles, cost_schedule)
-                    }
-                };
+                let prepaid_execution_cycles = try_from_option_field(
+                    aborted.prepaid_execution_cycles,
+                    "AbortedExecution::prepaid_execution_cycles",
+                )?;
                 ExecutionTask::AbortedExecution {
                     input,
                     prepaid_execution_cycles,
@@ -216,15 +191,10 @@ impl TryFrom<pb::ExecutionTask> for ExecutionTask {
                     Message::Request(v) => CanisterCall::Request(Arc::new(v.try_into()?)),
                     Message::Ingress(v) => CanisterCall::Ingress(Arc::new(v.try_into()?)),
                 };
-                let prepaid_execution_cycles = match aborted.prepaid_execution_compound_cycles {
-                    Some(value) => CompoundCycles::try_from(value)?,
-                    None => {
-                        let prepaid_execution_cycles = aborted
-                            .prepaid_execution_cycles
-                            .map_or_else(Cycles::zero, |c| c.into());
-                        CompoundCycles::<Instructions>::new(prepaid_execution_cycles, cost_schedule)
-                    }
-                };
+                let prepaid_execution_cycles = try_from_option_field(
+                    aborted.prepaid_execution_cycles,
+                    "AbortedExecution::prepaid_execution_cycles",
+                )?;
                 let call_id = aborted.call_id.ok_or(ProxyDecodeError::MissingField(
                     "AbortedInstallCode::call_id",
                 ))?;
