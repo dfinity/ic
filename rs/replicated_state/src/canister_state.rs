9be6310bcbd7b5f0509a98ee1549daa0e982df5f
Alin Sinpalean <58422065+alin-at-dfinity@users.noreply.github.com>
2026-05-08 16:17:25 +0200

    feat: [DSM-103] Active-canister-only scheduling (#10094)
    Only schedule active canisters instead of all canisters. This needs to
    be done on every scheduler loop iteration (as opposed to once per round
    followed by filtering per iteration), to account for canisters going
    idle / becoming active from one iteration to the next. But scheduling
    priorities continue to be updated only once, at the end of the round.
    
    The sequence of `RoundScheduler` calls goes from:
    
    ```
    apply_scheduling_strategy()  // Produce the actual schedule
    loop {
        charge_idle_canisters()
        start_iteration()        // Filter for active canisters
        // Execute
        end_iteration()          // Update stats (fully executed, etc.)
    }
    finish_round()               // Update scheduling priorities
    ```
    
    to a simplified:
    
    ```
    new()                        // Basic constructor
    loop {
        start_iteration()        // Prune SubnetSchedule, produce the actual schedule
        // Execute
        end_iteration()          // Update stats (fully executed, etc.)
    }
    finish_round()               // Update scheduling priorities
    ```
    
    `SubnetSchedule` no longer holds a `CanisterPriority` entry for every
    canister on the subnet. It only holds entries for:
     * active canisters (that have inputs),
     * long execution canisters,
    * heap delta or install code rate limited canisters (while their
    respective debits are consumed) and
    * idle canisters with accumulated priorities (AP) outside the `[0, 100]`
    range (because we want to "recall" that a canister was scheduled too
    much/too little even after it has been idle for a few rounds).
    
    Idle canisters with negative AP accumulate priority same as active
    canisters, until they reach zero; and idle canisters with > 100 AP are
    charged as "fully executed" every round (they do end the round with no
    inputs) until their AP is burned down to zero. Idle canisters with `[0,
    100]` AP are pruned from the `SubnetSchedule` at the beginning of the
    next round, in `start_iteration()`, once we know for sure that they are
    still idle. As a result, there is no more need to charge truly idle
    canisters every round.
    
    Finally, because we were already enforcing soft upper and lower AP
    bounds via exponential decay outside the `[-2000, 500]` range, there is
    no need to periodically reset AP, so that logic was dropped.

--- rs/replicated_state/src/canister_state.rs
+++ rs/replicated_state/src/canister_state.rs
@@ -275,6 +275,20 @@ impl CanisterState {
         self.system_state.queues().has_output()
     }
 
+    /// Returns true if the canister must be present in the `SubnetSchedule`,
+    /// regardless of new inputs or accumulated priority.
+    ///
+    /// This is different from "should be scheduled in an iteration", which also
+    /// considers whether the canister has inputs / tasks or non-zero AP / priority
+    /// credit. It is strictly about ensuring that canisters are retained in the
+    /// subnet schedule for as long as they have long-running executions or heap
+    /// delta or install code debits.
+    pub fn must_be_in_schedule(&self) -> bool {
+        self.scheduler_state.heap_delta_debit.get() > 0
+            || self.scheduler_state.install_code_debit.get() > 0
+            || self.has_long_execution_or_install_code()
+    }
+
     /// See `SystemState::push_output_request` for documentation.
     pub fn push_output_request(
         &mut self,
