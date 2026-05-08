64525da4f6b19d5c3764528681964cdf100d26cf
Martin Raszyk <martin.raszyk@dfinity.org>
2026-05-08 09:24:24 +0000

    consistent test fixes

--- rs/execution_environment/src/canister_manager/tests.rs
+++ rs/execution_environment/src/canister_manager/tests.rs
@@ -4672,12 +4672,11 @@ fn resource_saturation_scaling_works_in_create_canister() {
 fn update_settings_can_set_reserved_cycles_limit() {
     const CYCLES: Cycles = Cycles::new(1_000_000_000_000_000);
     const CAPACITY: u64 = 20_000_000_000;
-    const THRESHOLD: u64 = CAPACITY / 2;
 
     let mut test = ExecutionTestBuilder::new()
         .with_subnet_execution_memory(CAPACITY)
         .with_subnet_memory_reservation(0)
-        .with_subnet_memory_threshold(THRESHOLD)
+        .with_subnet_memory_threshold(0)
         .build();
 
     let canister_id = test
@@ -4685,6 +4684,7 @@ fn update_settings_can_set_reserved_cycles_limit() {
             CYCLES,
             CanisterSettingsArgsBuilder::new()
                 .with_reserved_cycles_limit(1)
+                .with_log_memory_limit(0)
                 .build(),
         )
         .unwrap();
@@ -7978,12 +7978,11 @@ fn create_canister_fails_with_reserved_cycles_limit_exceeded() {
 fn create_canister_can_set_reserved_cycles_limit() {
     const CYCLES: Cycles = Cycles::new(1_000_000_000_000_000);
     const CAPACITY: u64 = 20_000_000_000;
-    const THRESHOLD: u64 = CAPACITY / 2;
 
     let mut test = ExecutionTestBuilder::new()
         .with_subnet_execution_memory(CAPACITY)
         .with_subnet_memory_reservation(0)
-        .with_subnet_memory_threshold(THRESHOLD)
+        .with_subnet_memory_threshold(0)
         .build();
 
     let uc = test
@@ -7994,6 +7993,7 @@ fn create_canister_can_set_reserved_cycles_limit() {
     // empty canister is zero, setting the reserved cycles limit should succeed.
     let settings = CanisterSettingsArgsBuilder::new()
         .with_reserved_cycles_limit(1)
+        .with_log_memory_limit(0)
         .build();
     let args = CreateCanisterArgs {
         settings: Some(settings),
