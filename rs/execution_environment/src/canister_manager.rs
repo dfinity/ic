dea7390e7996b58486fc496c347acb8ce658f08b
Martin Raszyk <martin.raszyk@dfinity.org>
2026-05-12 09:08:48 +0000

    chore: account for heap delta increase due to canister log buffer resize

--- rs/execution_environment/src/canister_manager.rs
+++ rs/execution_environment/src/canister_manager.rs
@@ -632,6 +632,7 @@ impl CanisterManager {
 
         validate_controller(canister, &sender)?;
 
+        let old_log_store_memory_usage = canister.log_memory_store_memory_usage();
         self.validate_and_update_canister_settings(
             canister,
             round_limits,
@@ -642,6 +643,9 @@ impl CanisterManager {
             cost_schedule,
             Some(metrics),
         )?;
+        let heap_delta_increase = canister
+            .log_memory_store_memory_usage()
+            .saturating_sub(&old_log_store_memory_usage);
 
         canister.system_state.bump_canister_version();
         let new_controllers = match settings.controllers() {
@@ -693,8 +697,7 @@ impl CanisterManager {
         Ok(CanisterManagerResponse {
             canister_id: canister.canister_id(),
             reply: Some(EmptyBlob.encode()),
-            // TODO(DSM-123): set `heap_delta_increase` due to canister log buffer resize
-            heap_delta_increase: NumBytes::new(0),
+            heap_delta_increase,
             unflushed_checkpoint_op: None,
             deleted_call_context_responses: vec![],
             stop_call_id_to_remove: None,
@@ -1515,6 +1518,15 @@ impl CanisterManager {
 
         if specified_id.is_none() {
             state.metadata.commit_new_canister_id(new_canister_id);
+        }
+
+        let heap_delta = new_canister.log_memory_store_memory_usage();
+        state.metadata.heap_delta_estimate += heap_delta;
+        if self.config.rate_limiting_of_heap_delta == FlagStatus::Enabled {
+            new_canister.scheduler_state.heap_delta_debit = new_canister
+                .scheduler_state
+                .heap_delta_debit
+                .saturating_add(&heap_delta);
         }
 
         // Add new canister to the replicated state.
