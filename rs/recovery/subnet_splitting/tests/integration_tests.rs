5e75e8c62550911bef3740a6679bfc1447408580
Martin Raszyk <martin.raszyk@dfinity.org>
2026-05-27 06:53:15 +0000

    test fixes

--- rs/recovery/subnet_splitting/tests/integration_tests.rs
+++ rs/recovery/subnet_splitting/tests/integration_tests.rs
@@ -175,15 +175,15 @@ fn load_metrics_e2e_test() {
         // Accept up to 10% error. The precise values are not important here and they're very sensitive
         // to the changes to the replicated state / execution. It's mostly a sanity check that the
         // returned values are not too ridiculous and they might have to be updated once in a while.
-        assert_near!(states_sizes_bytes.source, 4572692, 0.1);
-        assert_near!(states_sizes_bytes.destination, 4572736, 0.1);
-        assert_near!(instructions_executed.source, 7664789, 0.1);
-        assert_near!(instructions_executed.destination, 7663691, 0.1);
+        assert_near!(states_sizes_bytes.source, 4571292, 0.1);
+        assert_near!(states_sizes_bytes.destination, 4571290, 0.1);
+        assert_near!(instructions_executed.source, 8295290, 0.1);
+        assert_near!(instructions_executed.destination, 8296084, 0.1);
         assert_eq!(
             ingress_messages_executed,
             Estimates {
-                source: 20,
-                destination: 19,
+                source: 19,
+                destination: 20,
             }
         );
         assert_eq!(
@@ -196,8 +196,8 @@ fn load_metrics_e2e_test() {
         assert_eq!(
             local_subnet_messages_executed_upper_bound,
             Estimates {
-                source: 15,
-                destination: 13,
+                source: 14,
+                destination: 14,
             }
         );
         assert_eq!(
@@ -210,8 +210,8 @@ fn load_metrics_e2e_test() {
         assert_eq!(
             heartbeats_and_global_timers_executed,
             Estimates {
-                source: 276,
-                destination: 418,
+                source: 366,
+                destination: 328,
             }
         );
         // Check if the split finder found a split satisfying the load constraints
