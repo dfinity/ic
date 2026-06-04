c900df9056bca7b7eccac1af3e5a8a5cf41ce0d3
IDX GitHub Automation <infra+github-automation@dfinity.org>
2026-06-04 13:37:40 +0000

    Automatically fixing code for linting and formatting issues

--- rs/registry/canister/src/get_subnet.rs
+++ rs/registry/canister/src/get_subnet.rs
@@ -58,8 +58,9 @@ impl TryFrom<SubnetRecordPb> for SubnetRecord {
                 .unwrap_or(CanisterCyclesCostSchedulePb::Unspecified);
         let canister_cycles_cost_schedule = match cost_schedule_pb {
             CanisterCyclesCostSchedulePb::Free => CanisterCyclesCostSchedule::Free,
-            CanisterCyclesCostSchedulePb::Normal
-            | CanisterCyclesCostSchedulePb::Unspecified => CanisterCyclesCostSchedule::Normal,
+            CanisterCyclesCostSchedulePb::Normal | CanisterCyclesCostSchedulePb::Unspecified => {
+                CanisterCyclesCostSchedule::Normal
+            }
         };
 
         let chain_key_config = pb
