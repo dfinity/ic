7e5ff24dbfdf0b893e44b1fa4d4ffc2079783d00
IDX GitHub Automation <infra+github-automation@dfinity.org>
2026-06-15 10:26:07 +0000

    Automatically fixing code for linting and formatting issues

--- rs/registry/canister/src/mutations/routing_table.rs
+++ rs/registry/canister/src/mutations/routing_table.rs
@@ -303,7 +303,8 @@ impl Registry {
         subnet_id: SubnetId,
     ) -> Vec<RegistryMutation> {
         self.modify_routing_table(version, |routing_table| {
-            let target_in_routing_table = routing_table.iter().any(|(_range, sid)| *sid == subnet_id);
+            let target_in_routing_table =
+                routing_table.iter().any(|(_range, sid)| *sid == subnet_id);
             for canister_id in canister_ids {
                 routing_table.assign_canister(canister_id, subnet_id);
             }
