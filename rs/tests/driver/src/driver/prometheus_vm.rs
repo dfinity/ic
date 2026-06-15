20dd12379a23c19bf88a1c5f132b918b82133c25
Bas van Dijk <bas@dfinity.org>
2026-06-15 18:19:24 +0000

    chore: upgrade UVM and P8s images

--- rs/tests/driver/src/driver/prometheus_vm.rs
+++ rs/tests/driver/src/driver/prometheus_vm.rs
@@ -42,7 +42,7 @@ const PROMETHEUS_VM_NAME: &str = "prometheus";
 /// Following through to the "Upload UVM images to S3" job and copying the <SHA256-HASH> from the line:
 /// upload: ../../../../../nix/store/...-nixos-disk-image-out-refs-discarded/nixos.img.zst to s3://dfinity-download/farm/prometheus-vm/<SHA256-HASH>/x86_64-linux/prometheus-vm.img.zst
 const DEFAULT_PROMETHEUS_VM_IMG_SHA256: &str =
-    "49ecc84e46e5cc5b970fc9cf94aa3decd03161642c91f4d1153955110e0ee13f";
+    "bdd9bd976e3210f382ae0810d13230dba13a660b2420662bdcb284f989842daa";
 
 fn get_default_prometheus_vm_img_url() -> String {
     format!(
