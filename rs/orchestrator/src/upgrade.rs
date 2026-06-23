8f3a7403ddd0239216816a7b025e9fa41029a00b
Pierugo Pace <pierugo.pace@dfinity.org>
2026-06-23 14:23:23 +0000

    fix: fix outdated log+docs

--- rs/orchestrator/src/upgrade.rs
+++ rs/orchestrator/src/upgrade.rs
@@ -429,9 +429,9 @@ impl Upgrade {
             .await
             .map_err(OrchestratorError::FileDownloadError)?;
         if let Err(e) = self.stop_children() {
-            // Even though we fail to stop the replica, we should still
+            // Even though we fail to stop child processes, we should still
             // replace the registry local store, so we simply issue a warning.
-            warn!(self.logger, "Failed to stop replica with error {:?}", e);
+            warn!(self.logger, "Failed to stop children with error {:?}.", e);
         }
         let new_local_store = LocalStoreImpl::new(local_store_location);
         self.registry_replicator
