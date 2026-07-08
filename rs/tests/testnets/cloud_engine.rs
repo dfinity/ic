8567f2c715c96af16cc1b3b79dd1b93b0af00944
nikolamilosa <nikola.milosavljevic@dfinity.org>
2026-07-08 00:50:14 +0000

    stripping out uneeded things

--- rs/tests/testnets/cloud_engine.rs
+++ rs/tests/testnets/cloud_engine.rs
@@ -14,7 +14,7 @@
 // You can setup this testnet by executing the following commands (preferably from a devenv in dm1-idx1):
 //
 //   $ ./ci/tools/docker-run
-//   $ bazel run //rs/tests/testnets:cloud_engine --test_tmpdir=./cloud_engine --test_env=DC=dm1-dmz -- --keepalive
+//   $ bazel run //rs/tests/testnets:cloud_engine --test_tmpdir=./cloud_engine -- --keepalive
 //
 // The --test_tmpdir=./cloud_engine will store the remaining test output in the specified directory.
 // This is useful to have access to in case you need to SSH into an IC node for example like:
@@ -42,7 +42,6 @@ use ic_protobuf::registry::{
 };
 use ic_registry_subnet_type::SubnetType;
 use ic_system_test_driver::driver::{
-    farm::HostFeature,
     group::SystemTestGroup,
     ic::{InternetComputer, Node, NodeOperatorConfig, Subnet},
     ic_gateway_vm::{HasIcGatewayVm, IC_GATEWAY_VM_NAME, IcGatewayVm},
@@ -57,19 +56,12 @@ use nns_dapp::{
     set_authorized_subnets,
 };
 use std::collections::BTreeMap;
-use std::net::Ipv4Addr;
 
 /// Cycles to fund the demo application canister with. 300T cycles.
 const DEMO_CANISTER_CYCLES: u128 = 300_000_000_000_000;
 
 /// Cycles to fund the whale application canister with. 100_000T (100 quadrillion) cycles.
 const WHALE_CANISTER_CYCLES: u128 = 100_000_000_000_000_000;
-
-/// dm1-dmz datacenter and network constants
-const DM1_DMZ_DC: &str = "dm1-dmz";
-const DM1_DMZ_NETWORK: Ipv4Addr = Ipv4Addr::new(23, 142, 184, 224);
-const DM1_DMZ_PREFIX: u8 = 28;
-const DM1_DMZ_GATEWAY: Ipv4Addr = Ipv4Addr::new(23, 142, 184, 238);
 
 /// Node providers used in this testnet. Each data center is owned by exactly
 /// one node provider (1 node provider per DC). Providers can own multiple DCs
@@ -283,10 +275,7 @@ fn main() -> Result<()> {
 }
 
 pub fn setup(env: TestEnv) {
-    let dm1_dmz_features = vec![HostFeature::DC(DM1_DMZ_DC.to_string()), HostFeature::DMZ];
-
     let mut ic = InternetComputer::new()
-        .with_required_host_features(dm1_dmz_features.clone())
         .add_subnet(
             Subnet::new(SubnetType::System)
                 .add_nodes(1)
@@ -394,23 +383,8 @@ pub fn setup(env: TestEnv) {
         env.topology_snapshot(),
         nns_dapp_customizations(),
     );
-    // deploy ic-gateway on dm1-dmz with static IPv4
-    let mut ic_gateway_vm =
-        IcGatewayVm::new(IC_GATEWAY_VM_NAME).with_required_host_features(dm1_dmz_features);
-    if let Ok(ic_gw_ipv4) = std::env::var("IC_GW_IPV4") {
-        let ip: Ipv4Addr = ic_gw_ipv4
-            .parse()
-            .unwrap_or_else(|e| panic!("invalid IC_GW_IPV4 address '{ic_gw_ipv4}': {e}"));
-        let mask: u32 = !((1_u32 << (32 - DM1_DMZ_PREFIX)) - 1);
-        assert_eq!(
-            u32::from(ip) & mask,
-            u32::from(DM1_DMZ_NETWORK),
-            "IC_GW_IPV4 address {ip} is not within {DM1_DMZ_NETWORK}/{DM1_DMZ_PREFIX}"
-        );
-        let address = format!("{ip}/{DM1_DMZ_PREFIX}");
-        ic_gateway_vm = ic_gateway_vm.with_ipv4_config(&address, &DM1_DMZ_GATEWAY.to_string());
-    }
-    ic_gateway_vm
+    // Deploy ic-gateway on any Farm host (no public/DMZ access needed).
+    IcGatewayVm::new(IC_GATEWAY_VM_NAME)
         .start(&env)
         .expect("failed to setup ic-gateway");
     let ic_gateway = env.get_deployed_ic_gateway(IC_GATEWAY_VM_NAME).unwrap();
