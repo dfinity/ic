ac7049baa0c394d7adddab63a0126204e02c4535
Pierugo Pace <pierugo.pace@dfinity.org>
2026-07-29 11:27:27 +0000

    perf: reduce size of enum variant

--- rs/types/types/src/consensus/dkg.rs
+++ rs/types/types/src/consensus/dkg.rs
@@ -792,12 +792,11 @@ pub enum DkgPayloadCreationError {
 }
 
 /// Reasons for why a dkg payload might be invalid.
-#[allow(clippy::large_enum_variant)]
 #[derive(PartialEq, Debug)]
 pub enum InvalidDkgPayloadReason {
     CryptoError(CryptoError),
     DkgVerifyDealingError(DkgVerifyDealingError),
-    MismatchedDkgSummary(DkgSummary, DkgSummary),
+    MismatchedDkgSummary(Box<DkgSummary>, Box<DkgSummary>),
     MissingDkgConfigForDealing,
     DkgStartHeightDoesNotMatchParentBlock,
     DkgSummaryAtNonStartHeight(Height),
