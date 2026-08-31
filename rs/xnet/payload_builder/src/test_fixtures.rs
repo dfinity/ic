cd16ff8cf6819913868f576906d2741ffc4f425b
Alin Sinpalean <alin.sinpalean@dfinity.org>
2026-08-31 08:24:13 +0000

    Revert PAYLOAD_BYTES_LIMIT (in XNet payload builder tests) to 4 MB.

--- rs/xnet/payload_builder/src/test_fixtures.rs
+++ rs/xnet/payload_builder/src/test_fixtures.rs
@@ -45,7 +45,7 @@ pub(crate) const SRC_CANISTER: u64 = 2;
 pub(crate) const DST_CANISTER: u64 = 3;
 pub(crate) const CALLBACK_ID: u64 = 4;
 
-pub(crate) const PAYLOAD_BYTES_LIMIT: NumBytes = NumBytes::new(POOLED_SLICE_BYTE_SIZE_MAX as u64);
+pub(crate) const PAYLOAD_BYTES_LIMIT: NumBytes = NumBytes::new(4 << 20);
 
 pub(crate) const LOCAL_NODE_1_OPERATOR_1: NodeId = NODE_1;
 pub(crate) const REMOTE_NODE_1_OPERATOR_1: NodeId = NODE_2;
