ceceaae16518806bcf31f0575a21000f76e5253e
Eero Kelly <eero.kelly@dfinity.org>
2026-09-21 23:59:28 +0000

    Sanitize GuestOS terminal output, for HostOS

--- ic-os/components/misc/logging.sh
+++ ic-os/components/misc/logging.sh
@@ -15,3 +15,11 @@ write_log() {
 
     logger -t "${SCRIPT}" "${message}"
 }
+
+sanitize_output() {
+    LC_ALL=C sed --unbuffered \
+        -e 's|\x1b\[[0-?]*[ -/]*[@-~]||g' \
+        -e 's|\x1b[]P_^][^\x1b\x07]*\(\x07\|\x1b\\\)\?||g' \
+        -e 's|\x1b[ -/]*[0-~]||g' \
+        | LC_ALL=C tr -cd '\011\012\040-\176'
+}
